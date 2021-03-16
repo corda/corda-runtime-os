a80406289b281fddf4927f443b170e6970d58f0d
LWogan <lorcan.wogan@r3.com>
2021-03-16 11:15:59 +0000

    import base and use the context logger in hello world. publish to artifactory

--- helloworld/src/main/kotlin/net/corda/sample/hello/HelloWorld.kt
+++ helloworld/src/main/kotlin/net/corda/sample/hello/HelloWorld.kt
@@ -1,6 +1,19 @@
 package net.corda.sample.hello
 
+import net.corda.v5.base.util.contextLogger
+
 
 fun main() {
-    println("hello world")
+    val helloWorld = HelloWorld()
+    helloWorld.logHello()
+}
+
+class HelloWorld  {
+    companion object {
+        private val log = contextLogger()
+    }
+
+    fun logHello() {
+        log.info("Hello world!")
+    }
 }
