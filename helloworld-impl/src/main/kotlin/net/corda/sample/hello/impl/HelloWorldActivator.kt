1bef161b2306b466aadbd41c8d32d58e14321d04
LWogan <lorcan.wogan@r3.com>
2021-03-31 16:23:32 +0100

    CORE-1017 Bundle activator interface class is not required when using annotations

--- helloworld-impl/src/main/kotlin/net/corda/sample/hello/impl/HelloWorldActivator.kt
+++ helloworld-impl/src/main/kotlin/net/corda/sample/hello/impl/HelloWorldActivator.kt
@@ -1,6 +1,5 @@
 package net.corda.sample.hello.impl
 
-import org.osgi.framework.BundleActivator
 import org.osgi.framework.BundleContext
 import org.osgi.service.component.annotations.Activate
 import org.osgi.service.component.annotations.Component
@@ -9,20 +8,18 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
 @Component(immediate = true)
-class HelloWorldActivator : BundleActivator {
+class HelloWorldActivator {
     private companion object {
         private val logger: Logger = LoggerFactory.getLogger(HelloWorldActivator::class.java)
     }
 
     @Activate
-    override fun start(context: BundleContext) {
+    fun start(context: BundleContext) {
         logger.info("Starting!")
-        println("Starting! WooHoo!")
     }
 
     @Deactivate
-    override fun stop(context: BundleContext) {
+    fun stop(context: BundleContext) {
         logger.info("Stopping!")
-        println("Stopping! Boo!!!")
     }
 }
