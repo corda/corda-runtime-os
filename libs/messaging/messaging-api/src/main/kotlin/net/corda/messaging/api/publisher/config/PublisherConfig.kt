f3f410a52f904d5a5e832f7366e9831d22434b0c
LWogan <lorcan.wogan@r3.com>
2021-04-14 14:35:21 +0100

    typos

--- libs/messaging/messaging-api/src/main/kotlin/net/corda/messaging/api/publisher/config/PublisherConfig.kt
+++ libs/messaging/messaging-api/src/main/kotlin/net/corda/messaging/api/publisher/config/PublisherConfig.kt
@@ -3,9 +3,9 @@ package net.corda.messaging.api.publisher.config
 /**
  * Class to store the required params to create a Publisher.
  * [clientId] is an identifier of the source of a record.
- * [instanceId] required for transactional publishing where order is importany. Leave null for idempotent producer
+ * [instanceId] required for transactional publishing where order is important.
  * [topic] to publish to
  */
 data class PublisherConfig (val clientId: String,
-                            val instanceId: Int?,
+                            val instanceId: Int,
                             val eventTopic: String)
