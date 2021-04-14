13f6639f6f5646af7d770e65db5eadf7f27724df
LWogan <lorcan.wogan@r3.com>
2021-04-14 14:49:38 +0100

    revert back for now. kafka api only allows one at a time.

--- libs/messaging/messaging-api/src/main/kotlin/net/corda/messaging/api/publisher/Publisher.kt
+++ libs/messaging/messaging-api/src/main/kotlin/net/corda/messaging/api/publisher/Publisher.kt
@@ -9,8 +9,8 @@ import net.corda.messaging.api.records.Record
 interface Publisher<K, V> {
 
     /**
-     * Publish a list of [records]
+     * Publish a [record]
      * @return a corda future indicating where the publish to a topic was successful.
      */
-    fun publish(records: List<Record<K, V>>) : CordaFuture<Boolean>
+    fun publish(record: Record<K, V>) : CordaFuture<Boolean>
 }
