4001c35faf1d1aa99c27b147654d8e28e808b98e
LWogan <lorcan.wogan@r3.com>
2021-04-23 12:42:49 +0100

    reorder param and default instanceId to null.

--- libs/messaging/messaging/src/main/kotlin/net/corda/messaging/api/publisher/config/PublisherConfig.kt
+++ libs/messaging/messaging/src/main/kotlin/net/corda/messaging/api/publisher/config/PublisherConfig.kt
@@ -3,9 +3,9 @@ package net.corda.messaging.api.publisher.config
 /**
  * Class to store the required params to create a Publisher.
  * @property clientId Is an identifier of the source of a record.
- * @property instanceId Required for transactional publishing where order is important. If this is null then transactions are not used.
  * @property topic To events publish to.
+ * @property instanceId Unique id required for transactional publishing. If this is null then transactions are not used. Defaults to null.
  */
 data class PublisherConfig (val clientId: String,
-                            val instanceId: Int?,
-                            val topic: String)
+                            val topic: String,
+                            val instanceId: Int? = null)
