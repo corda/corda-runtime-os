7d0e66d6fe37904986e90f956625caa5f8a85fb6
LWogan <lorcan.wogan@r3.com>
2021-04-27 08:54:55 +0100

    CORE-1124 additional units tests

--- libs/messaging/kafka-messaging-impl/src/test/kotlin/net/corda/messaging/kafka/publisher/KafkaPublisherTest.kt
+++ libs/messaging/kafka-messaging-impl/src/test/kotlin/net/corda/messaging/kafka/publisher/KafkaPublisherTest.kt
@@ -1,5 +1,6 @@
 package net.corda.messaging.kafka.subscription.net.corda.messaging.kafka.publisher
 
+import com.nhaarman.mockito_kotlin.*
 import com.typesafe.config.Config
 import com.typesafe.config.ConfigFactory
 import com.typesafe.config.ConfigValueFactory
@@ -9,24 +10,23 @@ import net.corda.messaging.kafka.properties.KafkaProperties.Companion.KAFKA_TOPI
 import net.corda.messaging.kafka.properties.KafkaProperties.Companion.PRODUCER_CLOSE_TIMEOUT
 import net.corda.messaging.kafka.publisher.KafkaPublisher
 import org.apache.kafka.clients.producer.MockProducer
-import org.apache.kafka.common.serialization.ByteArraySerializer
-import org.apache.kafka.common.serialization.StringSerializer
+import org.apache.kafka.common.errors.InvalidProducerEpochException
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.assertj.core.api.Assertions.assertThat
+import org.mockito.Mockito
+import java.time.Duration
 
 class KafkaPublisherTest {
     private lateinit var publisherConfig : PublisherConfig
     private lateinit var kafkaPublisher : KafkaPublisher<String, ByteArray>
-    private lateinit var producer : MockProducer<String, ByteArray>
     private lateinit var kafkaConfig: Config
+    private val producer : MockProducer<String, ByteArray> = mock()
 
     @BeforeEach
     fun beforeEach() {
         publisherConfig  = PublisherConfig("clientId", "topic")
         kafkaConfig = ConfigFactory.empty().withValue(PRODUCER_CLOSE_TIMEOUT, ConfigValueFactory.fromAnyRef(1))
         kafkaConfig = kafkaConfig.withValue(KAFKA_TOPIC_PREFIX, ConfigValueFactory.fromAnyRef("prefix"))
-        producer = MockProducer(true, null, StringSerializer(), ByteArraySerializer())
     }
 
     @Test
@@ -34,19 +34,45 @@ class KafkaPublisherTest {
         kafkaPublisher = KafkaPublisher(publisherConfig, kafkaConfig, producer)
 
         val record = Record("topic", "key1", "value1".toByteArray())
-        val fut = kafkaPublisher.publish(record)
-        assertThat(fut.get())
+        kafkaPublisher.publish(record)
+        verify(producer, times(1)).send(any(), any())
+        verify(producer, times(0)).beginTransaction()
+        verify(producer, times(0)).commitTransaction()
+
     }
 
     @Test
     fun testTransactionPublish() {
-        producer.initTransactions()
         publisherConfig  = PublisherConfig("clientId", "topic", 1)
-
         kafkaPublisher = KafkaPublisher(publisherConfig, kafkaConfig, producer)
 
         val record = Record("topic", "key1", "value1".toByteArray())
-        val fut = kafkaPublisher.publish(record)
-        assertThat(fut.get())
+        kafkaPublisher.publish(record)
+        verify(producer, times(1)).send(any(), any())
+        verify(producer, times(1)).beginTransaction()
+        verify(producer, times(1)).commitTransaction()
+    }
+
+    @Test
+    fun testSafeClose() {
+        kafkaPublisher = KafkaPublisher(publisherConfig, kafkaConfig, producer)
+
+        kafkaPublisher.safeClose()
+        verify(producer, times(1)).close(Mockito.any(Duration::class.java))
+    }
+
+    @Test
+    fun testRetryTransactions() {
+        doThrow(InvalidProducerEpochException("")).whenever(producer).beginTransaction()
+
+        publisherConfig  = PublisherConfig("clientId", "topic", 1)
+        kafkaPublisher = KafkaPublisher(publisherConfig, kafkaConfig, producer)
+
+        val record = Record("topic", "key1", "value1".toByteArray())
+        kafkaPublisher.publish(record)
+        verify(producer, times(0)).send(any(), any())
+        verify(producer, times(0)).commitTransaction()
+        verify(producer, times(1)).beginTransaction()
+        verify(producer, times(1)).abortTransaction()
     }
 }
