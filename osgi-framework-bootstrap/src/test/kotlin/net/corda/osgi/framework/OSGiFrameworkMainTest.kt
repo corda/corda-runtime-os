2e6d656ae535906b12e2af82b61fc8c264bf3209
Luca <Debiasi>
2021-05-13 16:26:44 +0100

    CORE-1035: WIP: setting dev workbench...

---
+++ osgi-framework-bootstrap/src/test/kotlin/net/corda/osgi/framework/OSGiFrameworkMainTest.kt
@@ -0,0 +1,12 @@
+package net.corda.osgi.framework
+
+import org.junit.jupiter.api.Test
+
+class OSGiFrameworkMainTest {
+
+    @Test
+    fun main() {
+        OSGiFrameworkMain.main(arrayOf())
+    }
+
+}
