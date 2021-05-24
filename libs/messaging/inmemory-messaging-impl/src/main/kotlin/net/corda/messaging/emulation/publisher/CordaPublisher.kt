3b83f282f0d3e2b39a8fb84b8cb4ad98448a8684
LWogan <lorcan.wogan@r3.com>
2021-05-24 08:50:59 +0100

    CORE-1128 code review updates

---
+++ libs/messaging/inmemory-messaging-impl/src/main/kotlin/net/corda/messaging/emulation/publisher/CordaPublisher.kt
@@ -0,0 +1,79 @@
+package net.corda.messaging.emulation.publisher
+
+import com.typesafe.config.Config
+import net.corda.messaging.api.exception.CordaMessageAPIFatalException
+import net.corda.messaging.api.publisher.Publisher
+import net.corda.messaging.api.records.Record
+import net.corda.messaging.emulation.publisher.factory.CordaPublisherFactory.Companion.PUBLISHER_CLIENT_ID
+import net.corda.messaging.emulation.publisher.factory.CordaPublisherFactory.Companion.PUBLISHER_INSTANCE_ID
+import net.corda.messaging.emulation.publisher.factory.CordaPublisherFactory.Companion.PUBLISHER_TOPIC
+import net.corda.messaging.emulation.topic.service.TopicService
+import net.corda.v5.base.concurrent.CordaFuture
+import net.corda.v5.base.internal.concurrent.OpenFuture
+import net.corda.v5.base.internal.concurrent.openFuture
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+/**
+ * In-memory corda publisher. Emulates CordaKafkaPublisher.
+ * @property config config to store relevant information.
+ * @property topicService service to interact with the in-memory storage of topics.
+ */
+class CordaPublisher<K : Any, V : Any> (
+    private val config: Config,
+    private val topicService: TopicService
+    ) : Publisher<K, V> {
+
+    private companion object {
+        private val log: Logger = LoggerFactory.getLogger(this::class.java)
+    }
+
+    private val topic = config.getString(PUBLISHER_TOPIC)
+    private val clientId = config.getString(PUBLISHER_CLIENT_ID)
+    private val instanceId = if (config.hasPath(PUBLISHER_INSTANCE_ID)) config.getInt(PUBLISHER_INSTANCE_ID) else null
+
+    @Suppress("TooGenericExceptionCaught")
+    override fun publish(records: List<Record<K, V>>): List<CordaFuture<Boolean>> {
+        return try {
+            topicService.addRecords(records)
+            getFutures(records.size)
+        } catch (ex: Exception) {
+            getFutures(records.size, ex)
+        }
+    }
+
+    /**
+     * Generate result for publish to topics.
+     * In-memory publish is always done as a transaction.
+     * [Publisher] api expects each record to be sent separately if transaction is not enabled.
+     * Emulate multiple sends by copying transaction result to a list of futures [size] times.
+     */
+    private fun getFutures(size: Int, ex: Exception? = null) : List<OpenFuture<Boolean>> {
+        val futures = mutableListOf<OpenFuture<Boolean>>()
+        val future = openFuture<Boolean>()
+        futures.add(future)
+
+        if (ex != null) {
+            val message = "Corda publisher clientId $clientId, instanceId $instanceId, " +
+                    "for topic $topic failed to send record."
+            log.error(message, ex)
+            future.setException(CordaMessageAPIFatalException(message, ex))
+        } else {
+            future.set(true)
+        }
+
+        //if not a transaction emulate multiple sends
+        if (instanceId == null) {
+            repeat(size - 1) {
+                futures.add(future)
+            }
+        }
+
+        return futures
+    }
+
+    override fun close() {
+        log.info("Closing Corda publisher clientId $clientId, instanceId $instanceId, " +
+                "for topic $topic.")
+    }
+}
---
+++ libs/messaging/inmemory-messaging-impl/src/main/kotlin/net/corda/messaging/emulation/topic/model/Topic.kt
@@ -0,0 +1,122 @@
+package net.corda.messaging.emulation.topic.model
+
+import net.corda.messaging.api.exception.CordaMessageAPIFatalException
+import net.corda.messaging.api.records.Record
+import net.corda.v5.base.util.contextLogger
+import net.corda.v5.base.util.debug
+import org.slf4j.Logger
+import java.util.*
+import java.util.concurrent.locks.ReentrantLock
+import kotlin.collections.HashMap
+import kotlin.math.abs
+import kotlin.math.max
+
+/**
+ * Object to store records and track consumer group offsets.
+ * Topics have a maximum size. When max size is reached oldest records are deleted.
+ * Topics have locks which must be obtained to read or write.
+ * Consumers subscribing and producers writing to topics automatically create topics if they do not exist.
+ */
+class Topic (private val topicName : String, private val maxSize: Int) {
+
+    companion object {
+        private val log: Logger = contextLogger()
+    }
+
+    val lock = ReentrantLock()
+    private var newestRecordOffset = 0L
+    private var oldestRecordOffset = 0L
+    private val records = LinkedList<RecordMetadata>()
+
+    //tracks the consumerGroup offset. The offset is the offset of the next record to be retrieved
+    private val consumerGroupOffset = HashMap<String, Long>()
+
+    /**
+     * Subscribe the [consumerGroup] to this [topicName] applying the [offsetStrategy]
+     */
+    fun subscribe(consumerGroup: String, offsetStrategy: OffsetStrategy) {
+        when (offsetStrategy) {
+            OffsetStrategy.EARLIEST -> {
+                consumerGroupOffset[consumerGroup] = oldestRecordOffset
+            }
+            OffsetStrategy.LATEST -> {
+                consumerGroupOffset[consumerGroup] = newestRecordOffset
+            }
+        }
+    }
+
+    /**
+     * Add this [record] to this [topicName].
+     * If [records] max size is reached, delete the oldest record
+     */
+    fun addRecord(record: Record<*, *>) {
+        if (records.size == maxSize) {
+            records.removeFirst()
+            oldestRecordOffset = records.first.offset
+            log.debug { "Max record count reached for topic $topicName. Deleting oldest record with offset $oldestRecordOffset." }
+        }
+        records.add(RecordMetadata(newestRecordOffset, record))
+        newestRecordOffset++
+    }
+
+    /**
+     * Get [pollSize] number of records from the [topicName] for this [consumerGroup].
+     * If [autoCommitOffset] is set to true then update the [consumerGroupOffset] with the latest poll position.
+     */
+    fun getRecords(consumerGroup : String, pollSize: Int, autoCommitOffset: Boolean) : List<RecordMetadata> {
+        var consumerOffset = consumerGroupOffset[consumerGroup]
+            ?: throw CordaMessageAPIFatalException("Consumer $consumerGroup is not subscribed to ")
+
+        if (records.isEmpty()) {
+            return emptyList()
+        }
+
+        val nextRecordIndex = getConsumerNextRecordIndex(consumerOffset, consumerGroup)
+        if (nextRecordIndex > records.size) {
+            return emptyList()
+        }
+        val iterator = records.listIterator(nextRecordIndex)
+        val polledRecords = mutableListOf<RecordMetadata>()
+
+        repeat(pollSize) {
+            if (iterator.hasNext()) {
+                val record = iterator.next()
+                polledRecords.add(record)
+                consumerOffset = max(consumerOffset, record.offset) + 1
+            }
+        }
+
+        if (autoCommitOffset) {
+            commitOffset(consumerGroup, consumerOffset)
+        }
+
+        return polledRecords
+    }
+
+    /**
+     * Commit the [offset] of a record for this [consumerGroup]
+     * to the [topicName]
+     */
+    fun commitOffset(consumerGroup: String, offset: Long) {
+        consumerGroupOffset[consumerGroup] = offset
+    }
+
+    /**
+     * Calculate the index in [records] for the [consumerGroup] offset.
+     * Log a warning if there records missed due to records max size reached.
+     */
+    private fun getConsumerNextRecordIndex(consumerOffset: Long, consumerGroup: String): Int {
+        val firstRecord = records.first
+        val oldestRecordIndex = records.indexOf(firstRecord)
+        val oldestRecordOffset = firstRecord.offset
+
+        var indexOffset = (consumerOffset - oldestRecordOffset).toInt()
+        if (indexOffset < 0) {
+            log.warn ("Records beginning at offset $consumerOffset for consumer $consumerGroup on topic $topicName were " +
+                    "deleted. Record loss count ${abs(indexOffset)}")
+            indexOffset = 0
+        }
+
+        return oldestRecordIndex + indexOffset
+    }
+}
