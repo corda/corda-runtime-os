34c26b07f7e06d198145873b355489deb7a01519
Luca Debiasi <66948975+luca-debiasi-r3@users.noreply.github.com>
2021-05-31 11:06:21 +0100

    CORE 1035 add an interface for the entry/exit point of the application (#29)

--- goodbyeworld/src/main/kotlin/net/corda/sample/goodbye/GoodbyeWorld.kt
+++ goodbyeworld/src/main/kotlin/net/corda/sample/goodbye/GoodbyeWorld.kt
@@ -1,21 +1,60 @@
 package net.corda.sample.goodbye
 
-import net.corda.sample.api.hello.HelloWorld
-import org.osgi.service.component.annotations.Reference
+import net.corda.osgi.api.Application
+import net.corda.osgi.api.Shutdown
+import org.osgi.framework.BundleActivator
+import org.osgi.framework.BundleContext
+import org.osgi.framework.FrameworkUtil
+import org.osgi.framework.ServiceReference
+import org.osgi.service.component.annotations.Activate
+import org.osgi.service.component.annotations.Component
+import org.osgi.service.component.annotations.Deactivate
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
-fun main() {
-    // Some bootstrap code goes here
+@Component(immediate = true)
+class GoodbyeWorld : BundleActivator, Application {
 
-    GoodbyeWorld().callHelloWorld()
-}
-
-class GoodbyeWorld {
-
-    @Reference
-    var helloWorld: HelloWorld? = null
-
-    fun callHelloWorld() {
-        helloWorld?.sayHello() ?: println("We couldn't find hello world!!!")
+    private companion object {
+        private val logger: Logger = LoggerFactory.getLogger(GoodbyeWorld::class.java)
     }
+
+    init {
+        logger.info("INIT")
+    }
+
+    @Activate
+    override fun start(context: BundleContext?) {
+        logger.info("START")
+    }
+
+    override fun startup(args: Array<String>) {
+        logger.info("START-UP")
+        Thread.sleep(1000)
+        Thread {
+            shutdownOSGiFramework()
+        }.start()
+    }
+
+    override fun shutdown() {
+        logger.info("SHUTDOWN")
+    }
+
+    private fun shutdownOSGiFramework() {
+        val bundleContext: BundleContext? = FrameworkUtil.getBundle(GoodbyeWorld::class.java).bundleContext
+        if (bundleContext != null) {
+            val shutdownServiceReference: ServiceReference<Shutdown>? =
+                bundleContext.getServiceReference(Shutdown::class.java)
+            if (shutdownServiceReference != null) {
+                bundleContext.getService(shutdownServiceReference)?.shutdown(bundleContext.bundle)
+            }
+        }
+    }
+
+    @Deactivate
+    override fun stop(context: BundleContext?) {
+        logger.info("STOP")
+    }
+
 }
 
--- osgi-framework-bootstrap/framework-app-tester/src/main/kotlin/net/corda/osgi/framework/apptester/AppTester.kt
+++ osgi-framework-bootstrap/framework-app-tester/src/main/kotlin/net/corda/osgi/framework/apptester/AppTester.kt
@@ -1,15 +1,49 @@
 package net.corda.osgi.framework.apptester
 
+import net.corda.osgi.api.Application
+import net.corda.osgi.api.Shutdown
 import org.osgi.framework.BundleActivator
 import org.osgi.framework.BundleContext
+import org.osgi.framework.FrameworkUtil
+import org.osgi.framework.ServiceReference
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
-class AppTester: BundleActivator {
+class AppTester: Application, BundleActivator {
+
+    private companion object {
+        private val logger: Logger = LoggerFactory.getLogger(AppTester::class.java)
+    }
 
     override fun start(context: BundleContext?) {
-        println("START")
+        logger.info("START")
+    }
+
+    override fun startup(args: Array<String>) {
+        logger.info("START-UP")
+        Thread.sleep(1000)
+        Thread {
+            shutdownOSGiFramework()
+        }.start()
+    }
+
+    override fun shutdown() {
+        logger.info("SHUTDOWN")
+        shutdownOSGiFramework()
+    }
+
+    private fun shutdownOSGiFramework() {
+        val bundleContext: BundleContext? = FrameworkUtil.getBundle(this.javaClass).bundleContext
+        if (bundleContext != null) {
+            val shutdownServiceReference: ServiceReference<Shutdown>? =
+                bundleContext.getServiceReference(Shutdown::class.java)
+            if (shutdownServiceReference != null) {
+                bundleContext.getService(shutdownServiceReference)?.shutdown(bundleContext.bundle)
+            }
+        }
     }
 
     override fun stop(context: BundleContext?) {
-        println("STOP")
+        logger.info("STOP")
     }
 }
