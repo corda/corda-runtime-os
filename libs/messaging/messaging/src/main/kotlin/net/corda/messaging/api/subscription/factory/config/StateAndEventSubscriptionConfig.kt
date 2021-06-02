17d0f0fa42e9851d63a34a39c1b6db88d0e2b8bc
Ryan Fowler <fowlerrr@users.noreply.github.com>
2021-06-02 14:37:58 +0100

    CORE-1127: Define the State and Event pattern interface (#44)
    * CORE-1127: Define the State and Event pattern interface
    
    Co-authored-by: James Higgs <45565019+JamesHR3@users.noreply.github.com>

--- libs/messaging/messaging/src/main/kotlin/net/corda/messaging/api/subscription/factory/config/StateAndEventSubscriptionConfig.kt
+++ libs/messaging/messaging/src/main/kotlin/net/corda/messaging/api/subscription/factory/config/StateAndEventSubscriptionConfig.kt
@@ -3,11 +3,14 @@ package net.corda.messaging.api.subscription.factory.config
 /**
  * Class to store the required params to create a StateAndEventSubscription.
  * @property groupName The unique ID for a group of consumers.
- * @property instanceId Required for transactional publishing where order and exactly once semantics are important.
- * @property stateTopic Topic to get state for a given event.
- * @property eventTopic Topic to get events from.
+ * @property instanceId Required for transactional publishing. Id must be unique across worker instances
+ * and subscription instances within the same consumer group.
+ * @property stateTopic Topic on which states will be available
+ * @property eventTopic Topic on which events will arrive from.
  */
-data class StateAndEventSubscriptionConfig (val groupName:String,
-                               val instanceId:Int,
-                               val stateTopic:String,
-                               val eventTopic:String)
+data class StateAndEventSubscriptionConfig(
+    val groupName: String,
+    val instanceId: Int,
+    val stateTopic: String,
+    val eventTopic: String,
+)
