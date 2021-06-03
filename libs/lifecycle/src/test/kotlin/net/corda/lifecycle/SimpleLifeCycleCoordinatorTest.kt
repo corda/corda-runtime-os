4c177b93c258e4338189ea62161093a533b022b4
Luca <Debiasi>
2021-06-03 08:56:34 +0100

    CORE-1105: essential unit tests introduced.

--- libs/lifecycle/src/test/kotlin/net/corda/lifecycle/SimpleLifeCycleCoordinatorTest.kt
+++ libs/lifecycle/src/test/kotlin/net/corda/lifecycle/SimpleLifeCycleCoordinatorTest.kt
@@ -13,21 +13,79 @@ internal class SimpleLifeCycleCoordinatorTest {
 
         const val BATCH_SIZE: Int = 128
 
-        const val TIMEOUT: Long = 10000L
+        const val TIMEOUT: Long = 1000L
 
         val logger: Logger = LoggerFactory.getLogger(SimpleLifeCycleCoordinatorTest::class.java)
     }
 
+    interface PostEvent : LifeCycleEvent
+
     @Test
     fun cancelTimer() {
+        val startLatch = CountDownLatch(1)
+        val key = "kill_me_softly"
+        val timerLatch = CountDownLatch(1)
+        SimpleLifeCycleCoordinator(BATCH_SIZE, TIMEOUT) { lifeCycleEvent: LifeCycleEvent ->
+            logger.debug("processEvent $lifeCycleEvent")
+            when (lifeCycleEvent) {
+                is StartEvent -> startLatch.countDown()
+                is TimerEvent -> {
+                    fail("${lifeCycleEvent}.${lifeCycleEvent.key} cancelled but executed! ")
+                }
+            }
+        }.use { coordinator ->
+            coordinator.start()
+            assertTrue(startLatch.await(TIMEOUT * 2, TimeUnit.MILLISECONDS))
+            coordinator.setTimer(key, TIMEOUT / 2) {
+                object : TimerEvent {
+                    override val key: String
+                        get() = key
+                }
+            }
+            coordinator.cancelTimer(key)
+            assertFalse(timerLatch.await(TIMEOUT, TimeUnit.MILLISECONDS))
+        }
     }
 
     @Test
     fun postEvent() {
+        val postLatch = CountDownLatch(1)
+        SimpleLifeCycleCoordinator(BATCH_SIZE, TIMEOUT) { lifeCycleEvent: LifeCycleEvent ->
+            when (lifeCycleEvent) {
+                is PostEvent -> postLatch.countDown()
+            }
+        }.use { coordinator ->
+            coordinator.start()
+            coordinator.postEvent(object : PostEvent {})
+            assertTrue(postLatch.await(TIMEOUT, TimeUnit.MILLISECONDS))
+        }
     }
 
     @Test
     fun setTimer() {
+        val startLatch = CountDownLatch(1)
+        val key = "wait_for_me"
+        val timerLatch = CountDownLatch(1)
+        SimpleLifeCycleCoordinator(BATCH_SIZE, TIMEOUT) { lifeCycleEvent: LifeCycleEvent ->
+            logger.debug("processEvent $lifeCycleEvent")
+            when (lifeCycleEvent) {
+                is StartEvent -> startLatch.countDown()
+                is TimerEvent -> {
+                    timerLatch.countDown()
+                    assertEquals(key, lifeCycleEvent.key)
+                }
+            }
+        }.use { coordinator ->
+            coordinator.start()
+            assertTrue(startLatch.await(TIMEOUT * 2, TimeUnit.MILLISECONDS))
+            coordinator.setTimer(key, TIMEOUT / 2) {
+                object : TimerEvent {
+                    override val key: String
+                        get() = key
+                }
+            }
+            assertTrue(timerLatch.await(TIMEOUT, TimeUnit.MILLISECONDS))
+        }
     }
 
     @Test
