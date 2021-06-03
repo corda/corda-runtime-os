8aebf2aa65df2cdd8f9f3e262d32bd01f407668f
Schife <codreanu.r@gmail.com>
2021-06-03 11:09:34 +0100

    CORE-1543 cleanup

--- libs/configuration/configuration-write/src/main/java/net/corda/libs/configuration/write/kafka/factory/package-info.java
+++
@@ -1,4 +0,0 @@
-@Export
-package net.corda.libs.configuration.write.kafka.factory;
-
-import org.osgi.annotation.bundle.Export;
--- libs/kafka-utils/topic-admin/src/main/java/net/corda/libs/kafka/topic/utils/impl/factory/package-info.java
+++
@@ -1,4 +0,0 @@
-@Export
-package net.corda.libs.kafka.topic.utils.impl.factory;
-
-import org.osgi.annotation.bundle.Export;
