dac966751bccd34d010707e886502ed0bd81097d
Schife <codreanu.r@gmail.com>
2021-06-04 13:40:24 +0100

    CORE-1543 application bootstrapping

--- applications/tools/kafka-setup/src/main/kotlin/net/corda/tools/kafka/KafkaConfigUploader.kt
+++ applications/tools/kafka-setup/src/main/kotlin/net/corda/tools/kafka/KafkaConfigUploader.kt
@@ -2,22 +2,32 @@ package net.corda.tools.kafka
 
 import net.corda.comp.kafka.config.write.KafkaConfigWrite
 import net.corda.comp.kafka.topic.admin.KafkaTopicAdmin
+import net.corda.osgi.api.Application
+import org.osgi.service.component.annotations.Activate
+import org.osgi.service.component.annotations.Component
 import org.osgi.service.component.annotations.Reference
 import java.io.File
 import kotlin.system.exitProcess
 
-@Reference(service = KafkaTopicAdmin::class)
-private lateinit var topicAdmin: KafkaTopicAdmin
-@Reference(service = KafkaConfigWrite::class)
-private lateinit var configWriter: KafkaConfigWrite
+@Component
+class KafkaConfigUploader @Activate constructor(
+    @Reference(service = KafkaTopicAdmin::class)
+    private var topicAdmin: KafkaTopicAdmin,
+    @Reference(service = KafkaConfigWrite::class)
+private var configWriter: KafkaConfigWrite
+) : Application {
 
+    override fun startup(args: Array<String>) {
+        if (args.size != 3) {
+            println("Required command line arguments: kafkaProps topicTemplate typesafeconfig")
+            exitProcess(1)
+        }
 
-fun main(args: Array<String>) {
-    if (args.size != 3) {
-        println("Required command line arguments: kafkaProps topicTemplate typesafeconfig")
-        exitProcess(1)
+        val topic = topicAdmin.createTopic(File(args[0]).readText(), File(args[1]).readText())
+        configWriter.updateConfig(topic.getString("topicName"), File(args[2]).readText())
     }
 
-    val topic = topicAdmin.createTopic(File(args[0]).readText(), File(args[1]).readText())
-    configWriter.updateConfig(topic.getString("topicName"), File(args[2]).readText())
+    override fun shutdown() {
+        TODO("Not yet implemented")
+    }
 }
