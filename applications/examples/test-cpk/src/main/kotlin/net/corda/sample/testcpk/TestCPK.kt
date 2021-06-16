bf7a1ddf0152307e875b4931abb86e79aea300f8
Luca <Debiasi>
2021-06-16 12:12:16 +0100

    CORE-1645: KDoc created.

--- applications/examples/test-cpk/src/main/kotlin/net/corda/sample/testcpk/TestCPK.kt
+++ applications/examples/test-cpk/src/main/kotlin/net/corda/sample/testcpk/TestCPK.kt
@@ -1,8 +1,12 @@
 package net.corda.sample.testcpk
 
+
+/**
+ * Dummy CordApp used in the `:applications:examples:test-sandbox`.
+ */
 class TestCPK {
 
     init {
-        println("net.corda.sample.testcpk.TestCPK INIT")
+        println("net.corda.sample.testcpk.TestCPK Initialized.")
     }
 }
--- applications/examples/test-sandbox/src/main/kotlin/net/corda/sample/testsandbox/TestSandboxApplication.kt
+++ applications/examples/test-sandbox/src/main/kotlin/net/corda/sample/testsandbox/TestSandboxApplication.kt
@@ -12,6 +12,19 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import java.nio.file.Paths
 
+/**
+ * This class is the entry point of the didactic application showing how to run CordApps from a bootable JAR.
+ *
+ * The CordApp used in this demo application is in the module `:applications:examples:test-cpk`.
+ *
+ * Run
+ *
+ * ```
+ * ./gradlew :applications:examples:test-cpk:cpk
+ * ```
+ *
+ * to create tke CPK artifact used in this Application.
+ */
 @Component(immediate = true)
 class TestSandboxApplication : Application {
 
@@ -19,16 +32,32 @@ class TestSandboxApplication : Application {
 
         private val logger: Logger = LoggerFactory.getLogger(TestSandboxApplication::class.java)
 
+        /**
+         * Relative path to the directory where the CPK artifact is created
+         * from the `:applications:examples:test-cpk` module.
+         */
         private const val PATH = "applications/examples/test-cpk/build/libs"
 
     } //~ companion object
 
+    /**
+     * Reference to the [InstallService] set automatically because both [InstallService] and
+     * this class are annotated as `@Component` and they are published OSGi services.
+     */
     @Reference
     private var installService: InstallService? = null
 
+    /**
+     * Reference to the [SandboxService] set automatically because both [SandboxService] and
+     * this class are annotated as `@Component` and they are published OSGi services.
+     */
     @Reference
     private var sandboxService: SandboxService? = null
 
+    /**
+     * Coordinator used start the [TestSandbox] as a Corda component, only when [installService] and [sandboxService]
+     * are set because service publishing and wiring is done before [startup] is called.
+     */
     private val coordinator = SimpleLifeCycleCoordinator(1, 1000L) { event, _ ->
         val testSandbox = TestSandbox(Paths.get(PATH), installService!!, sandboxService!!)
         when (event) {
@@ -41,19 +70,30 @@ class TestSandboxApplication : Application {
         }
     }
 
+    /**
+     * This method is called by after the OSGI framework wired all bundles and services.
+     *
+     * Properties annotated with `@Reference` are already set when this method is called.
+     */
     override fun startup(args: Array<String>) {
-        logger.info("Start-up...")
+        logger.info("Start-up($args).")
         if (installService != null) {
             logger.info("Install service active.")
             if (sandboxService != null) {
                 logger.info("Sandbox service active.")
                 coordinator.start()
+                logger.info("Press [CTRL+C] to stop the application...")
             }
         }
     }
 
+    /**
+     * This method is called when the bootable JAr is requested to terminate by the application itself or the
+     * operating system because the JVM terminates.
+     */
     override fun shutdown() {
         logger.info("Shutdown.")
         coordinator.stop()
     }
+
 }
