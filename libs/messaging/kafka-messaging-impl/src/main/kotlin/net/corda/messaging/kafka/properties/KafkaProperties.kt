d99032c9ca96833efb631e6bb457a51b691a4c22
Ryan Fowler <ryan.fowler@r3.com>
2021-06-23 17:53:12 +0100

    CORE-1740: Check for the correct config in CordaKafkaConsumerImpl

--- libs/messaging/kafka-messaging-impl/src/main/kotlin/net/corda/messaging/kafka/properties/KafkaProperties.kt
+++ libs/messaging/kafka-messaging-impl/src/main/kotlin/net/corda/messaging/kafka/properties/KafkaProperties.kt
@@ -29,9 +29,12 @@ class KafkaProperties {
         const val CONSUMER_CLOSE_TIMEOUT = "consumer.$CLOSE_TIMEOUT"
         const val CONSUMER_THREAD_STOP_TIMEOUT = "consumer.thread.stop.timeout"
         const val CONSUMER_POLL_AND_PROCESS_RETRIES = "consumer.processor.retries"
+        const val POLL_TIMEOUT = "poll.timeout"
         const val CONSUMER_POLL_TIMEOUT = "consumer.poll.timeout"
         const val CONSUMER_SUBSCRIBE_MAX_RETRIES = "consumer.subscribe.retries"
         const val CONSUMER_COMMIT_OFFSET_MAX_RETRIES = "consumer.commit.retries"
+        const val SUBSCRIBE_MAX_RETRIES = "subscribe.retries"
+        const val COMMIT_OFFSET_MAX_RETRIES = "commit.retries"
 
         const val GROUP_INSTANCE_ID = CommonClientConfigs.GROUP_INSTANCE_ID_CONFIG
         const val PRODUCER_CLIENT_ID = "producer.${CommonClientConfigs.CLIENT_ID_CONFIG}"
