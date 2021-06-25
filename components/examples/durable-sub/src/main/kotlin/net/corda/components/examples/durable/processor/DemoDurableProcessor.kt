6ca7085153ce7535f800b238fc978948b7d0adde
LWogan <lorcan.wogan@r3.com>
2021-06-25 15:16:50 +0100

    CORE-1325 basic log console output when something is gone wrong

--- components/examples/durable-sub/src/main/kotlin/net/corda/components/examples/durable/processor/DemoDurableProcessor.kt
+++ components/examples/durable-sub/src/main/kotlin/net/corda/components/examples/durable/processor/DemoDurableProcessor.kt
@@ -47,6 +47,7 @@ class DemoDurableProcessor(
             val newPublisherSet = eventRecordValue == 1
             if (expectedNextValues[key] != null && expectedNextValues[key] != eventRecordValue && !newPublisherSet) {
                 log.error("Wrong record found! Expected to find ${expectedNextValues[key]} but found $eventRecordValue")
+                println("Wrong record received by Durable processor! Expected to find ${expectedNextValues[key]} but found $eventRecordValue")
             }
             expectedNextValues[key] = eventRecordValue + 1
 
