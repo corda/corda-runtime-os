feb3a189e70ae600aeec081b82c46126774ab5d8
LWogan <lorcan.wogan@r3.com>
2021-06-25 11:55:58 +0100

    CORE-1325 use default state topic

--- components/examples/state-plus-event-sub/src/main/kotlin/net/corda/components/examples/stateevent/RunStateEventSub.kt
+++ components/examples/state-plus-event-sub/src/main/kotlin/net/corda/components/examples/stateevent/RunStateEventSub.kt
@@ -1,7 +1,6 @@
 package net.corda.components.examples.stateevent
 
 import com.typesafe.config.Config
-import com.typesafe.config.ConfigValueFactory
 import net.corda.components.examples.stateevent.processor.DemoStateAndEventProcessor
 import net.corda.data.demo.DemoRecord
 import net.corda.data.demo.DemoStateRecord
@@ -26,9 +25,6 @@ class RunStateEventSub(
         val log: Logger = contextLogger()
         const val groupName = "stateEventGroup"
         const val eventTopic = "EventTopic"
-        const val stateTopic = "StateTopic"
-        const val STATE_TOPIC = "messaging.pattern.stateAndEvent.stateConsumer.topic.name"
-
     }
 
     private var subscription: StateAndEventSubscription<String, DemoStateRecord, DemoRecord>? = null
@@ -47,7 +43,6 @@ class RunStateEventSub(
         if (!isRunning) {
             log.info("Creating state and event subscription")
             val processor = DemoStateAndEventProcessor(killProcessOnRecord, delayOnNext)
-            config = config.withValue(STATE_TOPIC, ConfigValueFactory.fromAnyRef(stateTopic))
 
             subscription = subscriptionFactory.createStateAndEventSubscription(
                 SubscriptionConfig(groupName, eventTopic, instanceId),
