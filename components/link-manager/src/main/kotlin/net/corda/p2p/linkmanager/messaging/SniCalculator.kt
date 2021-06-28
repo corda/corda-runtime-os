b0725239f05fe0ca46b14f067b3f1d2e0c7ea9c5
Will Vigor <william.vigor@r3.com>
2021-06-28 15:07:28 +0100

    Add .p2p.corda.net suffix to SNI for classic Corda Identity
    This is intentionally different to Corda 4.

--- components/link-manager/src/main/kotlin/net/corda/p2p/linkmanager/messaging/SniCalculator.kt
+++ components/link-manager/src/main/kotlin/net/corda/p2p/linkmanager/messaging/SniCalculator.kt
@@ -11,11 +11,12 @@ class SniCalculator {
         private const val HASH_ALGO = "SHA-256"
         private const val HASH_TRUNCATION_SIZE = 63 //Truncate to 63 characters (as RCF 1035)
         private const val ADDRESS_DELIMITER = ":"
+        private const val CLASSIC_CORDA_SNI_SUFFIX = ".p2p.corda.net" //This is intentionally different to Corda 4
 
         fun calculateSni(peer: HoldingIdentity, address: String): String {
             //Classic Corda Identity
             return if (peer.groupId == null) {
-                sha256Hash(peer.x500Name.toByteArray()).toString().take(HASH_TRUNCATION_SIZE).toLowerCase()
+                sha256Hash(peer.x500Name.toByteArray()).toString().take(HASH_TRUNCATION_SIZE).toLowerCase() + CLASSIC_CORDA_SNI_SUFFIX
             } else {
                 //We assume address is made up of the HostName and Port e.g. MyNode:80
                 address.split(ADDRESS_DELIMITER)[0]
