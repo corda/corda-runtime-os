3d6f95b9f38fdefc9666230211c3db41fca03eb9
Luca <Debiasi>
2021-06-28 09:29:17 +0100

    CORE-1645: Documentation edited to reflect gradle changes not including Log4j2 in all subprojects by default.

--- applications/examples/test-cpk/src/main/kotlin/net/corda/sample/testcpk/TestCPK.kt
+++ applications/examples/test-cpk/src/main/kotlin/net/corda/sample/testcpk/TestCPK.kt
@@ -1,16 +1,18 @@
 package net.corda.sample.testcpk
 
+//import org.slf4j.LoggerFactory
+
 /**
  * Dummy CordApp used in the `:applications:examples:test-sandbox`.
  */
-class TestCPK {
+class TestCPK: Runnable {
 
     init {
         println("net.corda.sample.testcpk.TestCPK INIT.")
     }
 
-    fun test() {
-        println("net.corda.sample.testcpk.TestCPK TEST")
-    }
 
+    override fun run() {
+        println("net.corda.sample.testcpk.TestCPK RUN.")
+    }
 }
