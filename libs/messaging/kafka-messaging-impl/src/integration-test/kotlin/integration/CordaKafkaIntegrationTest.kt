0e542ebc4bbef423853025fd927a4eaba6cd50b5
LWogan <lorcan.wogan@r3.com>
2021-06-30 16:46:40 +0100

    CORE-1690 draft

--- libs/messaging/kafka-messaging-impl/src/integration-test/kotlin/integration/CordaKafkaIntegrationTest.kt
+++ libs/messaging/kafka-messaging-impl/src/integration-test/kotlin/integration/CordaKafkaIntegrationTest.kt
@@ -30,7 +30,6 @@ class CordaKafkaIntegrationTest {
     private companion object {
         const val CLIENT_ID = "integrationTestPublisher"
         const val BOOTSTRAP_SERVERS_VALUE = "localhost:9092"
-        const val BOOTSTRAP_SERVERS = "bootstrap.servers"
         const val TOPIC_PREFIX = "messaging.topic.prefix"
         const val KAFKA_COMMON_BOOTSTRAP_SERVER = "messaging.kafka.common.bootstrap.servers"
         const val PUBLISHER_TOPIC1 = "PublisherTopic1"
