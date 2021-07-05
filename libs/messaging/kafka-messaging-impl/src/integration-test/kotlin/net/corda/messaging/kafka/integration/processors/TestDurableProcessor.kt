e4114d9e51154ef2bc7aeba496b17e3403c8ad47
LWogan <lorcan.wogan@r3.com>
2021-07-05 11:16:03 +0100

    CORE-1690 integration tests

--- libs/messaging/kafka-messaging-impl/src/integration-test/kotlin/net/corda/messaging/kafka/integration/processors/TestDurableProcessor.kt
+++ libs/messaging/kafka-messaging-impl/src/integration-test/kotlin/net/corda/messaging/kafka/integration/processors/TestDurableProcessor.kt
@@ -1,11 +1,13 @@
-package integration.processors
+package net.corda.messaging.kafka.integration.processors
 
 import net.corda.data.demo.DemoRecord
 import net.corda.messaging.api.processor.DurableProcessor
 import net.corda.messaging.api.records.Record
 import java.util.concurrent.CountDownLatch
 
-class TestDurableProcessor(private val latch: CountDownLatch) : DurableProcessor<String, DemoRecord> {
+class TestDurableProcessor(
+    private val latch: CountDownLatch, private val outputTopic: String? = null
+) : DurableProcessor<String, DemoRecord> {
     override val keyClass: Class<String>
         get() = String::class.java
     override val valueClass: Class<DemoRecord>
@@ -15,6 +17,11 @@ class TestDurableProcessor(private val latch: CountDownLatch) : DurableProcessor
         for (event in events) {
             latch.countDown()
         }
-        return emptyList()
+
+        return if (outputTopic != null) {
+            listOf(Record(outputTopic, "durableOutputKey", DemoRecord(1)))
+        } else {
+            emptyList<Record<String, DemoRecord>>()
+        }
     }
 }
--- libs/messaging/kafka-messaging-impl/src/integration-test/kotlin/net/corda/messaging/kafka/integration/subscription/PubsubSubscriptionIntegrationTest.kt
+++ libs/messaging/kafka-messaging-impl/src/integration-test/kotlin/net/corda/messaging/kafka/integration/subscription/PubsubSubscriptionIntegrationTest.kt
@@ -1,19 +1,21 @@
-package integration
+package net.corda.messaging.kafka.integration.subscription
 
 import com.typesafe.config.Config
 import com.typesafe.config.ConfigFactory
 import com.typesafe.config.ConfigValueFactory
-import net.corda.data.demo.DemoRecord
 import net.corda.messaging.api.publisher.Publisher
 import net.corda.messaging.api.publisher.config.PublisherConfig
 import net.corda.messaging.api.publisher.factory.PublisherFactory
-import net.corda.messaging.api.records.Record
 import net.corda.messaging.api.subscription.factory.SubscriptionFactory
 import net.corda.messaging.api.subscription.factory.config.SubscriptionConfig
-import integration.processors.TestDurableProcessor
-import org.assertj.core.api.Assertions.assertThat
+import net.corda.messaging.kafka.integration.IntegrationTestProperties.Companion.BOOTSTRAP_SERVERS_VALUE
+import net.corda.messaging.kafka.integration.IntegrationTestProperties.Companion.KAFKA_COMMON_BOOTSTRAP_SERVER
+import net.corda.messaging.kafka.integration.IntegrationTestProperties.Companion.TOPIC_PREFIX
+import net.corda.messaging.kafka.integration.getRecords
+import net.corda.messaging.kafka.integration.processors.TestPubsubProcessor
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Timeout
 import org.junit.jupiter.api.extension.ExtendWith
 import org.osgi.test.common.annotation.InjectService
 import org.osgi.test.junit5.service.ServiceExtension
@@ -21,18 +23,15 @@ import java.util.concurrent.CountDownLatch
 import java.util.concurrent.TimeUnit
 
 @ExtendWith(ServiceExtension::class)
-class CordaKafkaIntegrationTest {
+class PubsubSubscriptionIntegrationTest {
 
     private lateinit var publisherConfig: PublisherConfig
     private lateinit var publisher: Publisher
     private lateinit var kafkaConfig: Config
 
     private companion object {
-        const val CLIENT_ID = "integrationTestPublisher"
-        const val BOOTSTRAP_SERVERS_VALUE = "localhost:9092"
-        const val TOPIC_PREFIX = "messaging.topic.prefix"
-        const val KAFKA_COMMON_BOOTSTRAP_SERVER = "messaging.kafka.common.bootstrap.servers"
-        const val PUBLISHER_TOPIC1 = "PublisherTopic1"
+        const val CLIENT_ID = "integrationTestPubsubPublisher"
+        const val PUBSUB_TOPIC1 = "PubsubTopic1"
     }
 
     @InjectService(timeout = 4000)
@@ -40,7 +39,6 @@ class CordaKafkaIntegrationTest {
 
     @InjectService(timeout = 4000)
     lateinit var subscriptionFactory: SubscriptionFactory
-
 
     @BeforeEach
     fun beforeEach() {
@@ -50,36 +48,25 @@ class CordaKafkaIntegrationTest {
     }
 
     @Test
-    fun testPublishAndDurableConsume() {
+    @Timeout(value = 10000, unit = TimeUnit.MILLISECONDS)
+    fun `start pubsub subscription and publish records`() {
+        val latch = CountDownLatch(1)
+        val pubsubSub = subscriptionFactory.createPubSubSubscription(
+            SubscriptionConfig("pubsub2", PUBSUB_TOPIC1),
+            TestPubsubProcessor(latch),
+            null,
+            kafkaConfig
+        )
+        pubsubSub.start()
+
         publisherConfig = PublisherConfig(CLIENT_ID)
         publisher = publisherFactory.createPublisher(publisherConfig, kafkaConfig)
-        val futures = publisher.publish(getRecords(PUBLISHER_TOPIC1, 5, 2))
-        assertThat(futures.size).isEqualTo(10)
-        futures.forEach { it.get(10, TimeUnit.SECONDS) }
-        publisher.close()
 
-        val latch = CountDownLatch(10)
-        val durableSub = subscriptionFactory.createDurableSubscription(
-            SubscriptionConfig("durable1", PUBLISHER_TOPIC1, 1),
-            TestDurableProcessor(latch),
-            kafkaConfig,
-            null
-        )
-        durableSub.start()
-
-        latch.await(15, TimeUnit.SECONDS)
-        durableSub.stop()
-        assertThat(durableSub.isRunning).isEqualTo(false)
-    }
-
-    private fun getRecords(topic: String, recordCount: Int, keyCount: Int): List<Record<*, *>> {
-        val records = mutableListOf<Record<*, *>>()
-        for (i in 1..keyCount) {
-            val key = "key$i"
-            for (j in 1..recordCount) {
-                records.add(Record(topic, key, DemoRecord(j)))
-            }
+        while (latch.count > 0) {
+            publisher.publish(getRecords(PUBSUB_TOPIC1, 10, 2))
         }
-        return records
+
+        publisher.close()
+        pubsubSub.stop()
     }
 }
