f68b7c331204b09670e6716f4cd7529f6751795a
Luca <Debiasi>
2021-07-08 10:47:23 +0100

    CORE-1899: Test burstEvents, burstTimers, cancelTimer, getBatchSize, getTimeout, postHandledButRethrowErrorEvent, postUnhandledErrorEvent

--- libs/lifecycle/src/test/kotlin/net/corda/lifecycle/SimpleLifeCycleCoordinatorTest.kt
+++ libs/lifecycle/src/test/kotlin/net/corda/lifecycle/SimpleLifeCycleCoordinatorTest.kt
@@ -233,7 +233,6 @@ internal class SimpleLifeCycleCoordinatorTest {
         }
     }
 
-    @Disabled
     @ParameterizedTest
     @Timeout(value = 60, unit = TimeUnit.SECONDS)
     @ValueSource(ints = [5])
