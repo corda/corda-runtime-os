aeee6cade73f8083106f11d6473dec145164c97e
Dimos Raptis <dimos.raptis@r3.com>
2021-07-08 15:19:04 +0100

    Implement record/offset cleanup based on timestamp for time-based retention

--- libs/messaging/db-messaging-impl/src/integration-test/kotlin/net/corda/messaging/db/persistence/DbAccessProviderTestBase.kt
+++ libs/messaging/db-messaging-impl/src/integration-test/kotlin/net/corda/messaging/db/persistence/DbAccessProviderTestBase.kt
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import java.sql.DriverManager
+import java.time.Instant
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 abstract class DbAccessProviderTestBase {
@@ -273,6 +274,41 @@ abstract class DbAccessProviderTestBase {
 
         val nonExistingRecord = dbAccessProvider.getRecord(topic1, 1, 10)
         assertThat(nonExistingRecord).isNull()
+    }
+
+    @Test
+    fun `can delete records before timestamp successfully`() {
+        val committedOffset = 3L
+        val writtenRecords = listOf(
+            RecordDbEntry(topic1, 1, 1, "key-1".toByteArray(), "value-1".toByteArray()),
+            RecordDbEntry(topic1, 1, 2, "key-2".toByteArray(), "value-2".toByteArray()),
+            RecordDbEntry(topic1, 1, 3, "key-3".toByteArray(), "value-3".toByteArray())
+        )
+        dbAccessProvider.writeOffsetsAndRecordsAtomically(topic1, consumer1, mapOf(1 to committedOffset), writtenRecords) {}
+
+        val readRecordsBeforeCleanup = dbAccessProvider.readRecords(topic1, listOf(FetchWindow(1, 1, 3, 10)))
+        assertThat(readRecordsBeforeCleanup).containsAll(writtenRecords)
+
+        dbAccessProvider.deleteRecordsOlderThan(topic1, Instant.now())
+
+        val readRecordsAfterCleanup = dbAccessProvider.readRecords(topic1, listOf(FetchWindow(1, 1, 3, 10)))
+        assertThat(readRecordsAfterCleanup).isEmpty()
+    }
+
+    @Test
+    fun `can delete offsets before timestamp successfully`() {
+        val offsets = mapOf(
+            1 to 3L,
+            2 to 5L
+        )
+        dbAccessProvider.writeOffsets(topic1, consumer1, offsets)
+
+        assertThat(dbAccessProvider.getMaxCommittedOffset(topic1, consumer1, setOf(1, 2))).isEqualTo(offsets)
+        dbAccessProvider.deleteOffsetsOlderThan(topic1, Instant.now())
+        assertThat(dbAccessProvider.getMaxCommittedOffset(topic1, consumer1, setOf(1, 2))).isEqualTo(mapOf(
+            1 to null,
+            2 to null
+        ))
     }
 
 }
--- libs/messaging/db-messaging-impl/src/integration-test/kotlin/net/corda/messaging/db/util/DbUtils.kt
+++ libs/messaging/db-messaging-impl/src/integration-test/kotlin/net/corda/messaging/db/util/DbUtils.kt
@@ -16,6 +16,7 @@ class DbUtils {
         val createOffsetsTableStmt = "CREATE TABLE ${DbSchema.CommittedOffsetsTable.TABLE_NAME} " +
                 "(${DbSchema.CommittedOffsetsTable.TOPIC_COLUMN_NAME} VARCHAR, ${DbSchema.CommittedOffsetsTable.CONSUMER_GROUP_COLUMN_NAME} VARCHAR, " +
                 "${DbSchema.CommittedOffsetsTable.PARTITION_COLUMN_NAME} INT, ${DbSchema.CommittedOffsetsTable.COMMITTED_OFFSET_COLUMN_NAME} BIGINT, " +
+                "${DbSchema.CommittedOffsetsTable.OFFSET_TIMESTAMP_COLUMN_NAME} TIMESTAMP, " +
                 "PRIMARY KEY (${DbSchema.CommittedOffsetsTable.TOPIC_COLUMN_NAME}, ${DbSchema.CommittedOffsetsTable.CONSUMER_GROUP_COLUMN_NAME}, ${DbSchema.CommittedOffsetsTable.PARTITION_COLUMN_NAME}, ${DbSchema.CommittedOffsetsTable.COMMITTED_OFFSET_COLUMN_NAME}));"
 
         val cleanupOffsetsTableStmt = "DELETE FROM ${DbSchema.CommittedOffsetsTable.TABLE_NAME}"
@@ -41,6 +42,7 @@ class DbUtils {
             val createOffsetsTableStmt = "CREATE TABLE ${DbSchema.CommittedOffsetsTable.TABLE_NAME} " +
                     "(${DbSchema.CommittedOffsetsTable.TOPIC_COLUMN_NAME} VARCHAR(200), ${DbSchema.CommittedOffsetsTable.CONSUMER_GROUP_COLUMN_NAME} VARCHAR(200), " +
                     "${DbSchema.CommittedOffsetsTable.PARTITION_COLUMN_NAME} INT, ${DbSchema.CommittedOffsetsTable.COMMITTED_OFFSET_COLUMN_NAME} BIGINT, " +
+                    "${DbSchema.CommittedOffsetsTable.OFFSET_TIMESTAMP_COLUMN_NAME} DATETIME, " +
                     "PRIMARY KEY (${DbSchema.CommittedOffsetsTable.TOPIC_COLUMN_NAME}, ${DbSchema.CommittedOffsetsTable.CONSUMER_GROUP_COLUMN_NAME}, ${DbSchema.CommittedOffsetsTable.PARTITION_COLUMN_NAME}, ${DbSchema.CommittedOffsetsTable.COMMITTED_OFFSET_COLUMN_NAME}));"
 
             val createTopicsTableStmt = "CREATE TABLE ${DbSchema.TopicsTable.TABLE_NAME} " +
@@ -62,6 +64,7 @@ class DbUtils {
             val createOffsetsTableStmt = "CREATE TABLE ${DbSchema.CommittedOffsetsTable.TABLE_NAME} " +
                     "(${DbSchema.CommittedOffsetsTable.TOPIC_COLUMN_NAME} VARCHAR(200), ${DbSchema.CommittedOffsetsTable.CONSUMER_GROUP_COLUMN_NAME} VARCHAR(200), " +
                     "${DbSchema.CommittedOffsetsTable.PARTITION_COLUMN_NAME} INT, ${DbSchema.CommittedOffsetsTable.COMMITTED_OFFSET_COLUMN_NAME} NUMBER, " +
+                    "${DbSchema.CommittedOffsetsTable.OFFSET_TIMESTAMP_COLUMN_NAME} TIMESTAMP, " +
                     "PRIMARY KEY (${DbSchema.CommittedOffsetsTable.TOPIC_COLUMN_NAME}, ${DbSchema.CommittedOffsetsTable.CONSUMER_GROUP_COLUMN_NAME}, ${DbSchema.CommittedOffsetsTable.PARTITION_COLUMN_NAME}, ${DbSchema.CommittedOffsetsTable.COMMITTED_OFFSET_COLUMN_NAME}))"
 
             val createTopicsTableStmt = "CREATE TABLE ${DbSchema.TopicsTable.TABLE_NAME} " +
