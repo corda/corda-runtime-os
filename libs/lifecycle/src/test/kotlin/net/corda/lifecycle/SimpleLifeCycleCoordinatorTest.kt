f42da2c8331ba4630fa66b8704de99356cd0b015
Luca <Debiasi>
2021-07-08 13:55:48 +0100

    CORE-1899: Test setTimer, startAndStopLoop...

--- libs/lifecycle/src/test/kotlin/net/corda/lifecycle/SimpleLifeCycleCoordinatorTest.kt
+++ libs/lifecycle/src/test/kotlin/net/corda/lifecycle/SimpleLifeCycleCoordinatorTest.kt
@@ -239,7 +239,7 @@ internal class SimpleLifeCycleCoordinatorTest {
         }
     }
 
-    @Disabled
+    //@Disabled
     @ParameterizedTest
     @Timeout(value = 60, unit = TimeUnit.SECONDS)
     @ValueSource(ints = [5])
