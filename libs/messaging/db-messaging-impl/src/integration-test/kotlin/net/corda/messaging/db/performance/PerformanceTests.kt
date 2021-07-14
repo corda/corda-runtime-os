57af6831aadaaa1da47d4a9fefface77e6fce0a4
Dimos Raptis <dimos.raptis@r3.com>
2021-07-14 19:00:48 +0100

    Add annotation that was removed by accident

--- libs/messaging/db-messaging-impl/src/integration-test/kotlin/net/corda/messaging/db/performance/PerformanceTests.kt
+++ libs/messaging/db-messaging-impl/src/integration-test/kotlin/net/corda/messaging/db/performance/PerformanceTests.kt
@@ -24,6 +24,7 @@ import net.corda.v5.base.util.seconds
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito
 import java.nio.ByteBuffer
