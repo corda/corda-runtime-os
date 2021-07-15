a12b678f621278a8c67e4be603d814615ab1645d
LWogan <lorcan.wogan@r3.com>
2021-07-15 14:41:32 +0100

    CORE-1367 serialize keys using avro. use kafka default serializers when key is string or bytearray

--- libs/messaging/kafka-messaging-impl/src/main/kotlin/net/corda/messaging/kafka/publisher/CordaAvroSerializer.kt
+++ libs/messaging/kafka-messaging-impl/src/main/kotlin/net/corda/messaging/kafka/publisher/CordaAvroSerializer.kt
@@ -2,14 +2,34 @@ package net.corda.messaging.kafka.publisher
 
 import net.corda.schema.registry.AvroSchemaRegistry
 import org.apache.kafka.common.serialization.Serializer
+import org.apache.kafka.common.serialization.StringSerializer
 
 class CordaAvroSerializer<T : Any>(
     private val schemaRegistry: AvroSchemaRegistry
 ) : Serializer<T> {
+
+    companion object {
+        const val STRING_MAGIC = "corda-string-"
+        const val BYTE_ARRAY_MAGIC = "corda-bytearray-"
+    }
+
     override fun serialize(topic: String?, data: T?): ByteArray? {
-        if (data == null) {
-            return null
+        return when {
+            data == null -> {
+                null
+            }
+            data.javaClass == String::class.java -> {
+                val stringSerializer = StringSerializer()
+                val serializedMagic = stringSerializer.serialize(topic, STRING_MAGIC)
+                val serializedData = stringSerializer.serialize(topic, data as String)
+                serializedMagic + serializedData
+            }
+            data.javaClass == ByteArray::class.java -> {
+                BYTE_ARRAY_MAGIC.toByteArray() + data as ByteArray
+            }
+            else -> {
+                schemaRegistry.serialize(data).array()
+            }
         }
-        return schemaRegistry.serialize(data).array()
     }
 }
