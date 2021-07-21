d4d239f2551a9e9c7477b99e00ea35856f2a7d5c
Luca <Debiasi>
2021-07-21 17:41:44 +0100

    CORE-1549: Flask integrated.

---
+++ testing/cordapps/test-cpk/src/main/kotlin/net/corda/sample/testcpk/TestCPK.kt
@@ -0,0 +1,15 @@
+package net.corda.sample.testcpk
+
+/**
+ * Dummy CordApp used in the `:applications:examples:test-sandbox`.
+ */
+class TestCPK: Runnable {
+
+    init {
+        println("TestCPK INIT.")
+    }
+
+    override fun run() {
+        println("TestCPK RUN.")
+    }
+}
