07fb3bfdabd0d7fd567a1a6473eccbfcf66da64b
Ryan Fowler <ryan.fowler@r3.com>
2021-07-27 11:38:48 +0100

    Remove demo app for another PR

--- components/examples/run-flow/src/main/java/net/corda/components/examples/runflow/package-info.java
+++
@@ -1,4 +0,0 @@
-@Export
-package net.corda.components.examples.runflow;
-
-import org.osgi.annotation.bundle.Export;
--- components/examples/run-flow/src/main/java/net/corda/components/examples/runflow/processor/package-info.java
+++
@@ -1,4 +0,0 @@
-@Export
-package net.corda.components.examples.runflow.processor;
-
-import org.osgi.annotation.bundle.Export;
