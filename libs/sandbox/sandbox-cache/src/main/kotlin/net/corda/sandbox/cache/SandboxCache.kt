ace5cad396355f6f9faa8edb2dfb81245a96625e
Ryan Fowler <ryan.fowler@r3.com>
2021-07-27 11:38:48 +0100

    Remove demo app for another PR

--- libs/sandbox/sandbox-cache/src/main/kotlin/net/corda/sandbox/cache/SandboxCache.kt
+++ libs/sandbox/sandbox-cache/src/main/kotlin/net/corda/sandbox/cache/SandboxCache.kt
@@ -9,7 +9,5 @@ data class FlowMetadata(val name: String, val key: FlowKey)
 
 interface SandboxCache {
     fun getSandboxGroupFor(identity: HoldingIdentity, flow: FlowMetadata): SandboxGroup
-//    fun getSandboxById(id: FlowMetadata): SandboxGroup
-
     fun loadCpbs(CPBs: List<Path>)
 }
