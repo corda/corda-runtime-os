51d563ac3ea583ae0298608fa31bb1a7ac0bb913
Schife <codreanu.r@gmail.com>
2021-07-29 09:35:53 +0100

    CORE-2039 PR comments

--- libs/serialization/serializers/src/main/kotlin/net/corda/serializers/PrivateKeySerializer.kt
+++ libs/serialization/serializers/src/main/kotlin/net/corda/serializers/PrivateKeySerializer.kt
@@ -5,12 +5,7 @@ import net.corda.kryoserialization.CheckpointInternalCustomSerializer
 import net.corda.kryoserialization.CheckpointOutput
 import net.corda.v5.crypto.Crypto
 import java.security.PrivateKey
-import java.security.PublicKey
 
-/* This file contains Serializers for types which are defined in the Crypto module.
- * We do this here as we don't want crypto to depend on serialization (or serialization-kryo
- * to depend on Crypto).
-*/
 class PrivateKeySerializer : CheckpointInternalCustomSerializer<PrivateKey> {
     override fun write(output: CheckpointOutput, obj: PrivateKey) {
         output.writeBytesWithLength(obj.encoded)
@@ -18,16 +13,5 @@ class PrivateKeySerializer : CheckpointInternalCustomSerializer<PrivateKey> {
 
     override fun read(input: CheckpointInput, type: Class<PrivateKey>): PrivateKey {
         return Crypto.decodePrivateKey(input.readBytesWithLength())
-    }
-}
-
-/** For serialising a public key */
-class PublicKeySerializer : CheckpointInternalCustomSerializer<PublicKey> {
-    override fun write(output: CheckpointOutput, obj: PublicKey) {
-        output.writeBytesWithLength(obj.encoded)
-    }
-
-    override fun read(input: CheckpointInput, type: Class<PublicKey>): PublicKey {
-        return Crypto.decodePublicKey(input.readBytesWithLength())
     }
 }
