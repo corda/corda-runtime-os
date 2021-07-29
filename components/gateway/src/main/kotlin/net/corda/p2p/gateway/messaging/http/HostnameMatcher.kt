d499358cf1f4b63dbcc3ec1614561a7dfbd80694
bogdan.paunescu <bogdan.paunescu@r3.com>
2021-07-29 19:58:30 +0100

    [CORE-1749]-implement host name matching for alt subject names; add unit tests

--- components/gateway/src/main/kotlin/net/corda/p2p/gateway/messaging/http/HostnameMatcher.kt
+++ components/gateway/src/main/kotlin/net/corda/p2p/gateway/messaging/http/HostnameMatcher.kt
@@ -1,26 +1,32 @@
-@file:Suppress("JAVA_MODULE_DOES_NOT_EXPORT_PACKAGE")
 package net.corda.p2p.gateway.messaging.http
 
 import net.corda.nodeapi.internal.crypto.x509
 import net.corda.p2p.NetworkType
 import net.corda.v5.application.identity.CordaX500Name
+import org.apache.commons.validator.routines.DomainValidator
+import org.apache.commons.validator.routines.InetAddressValidator
 import org.slf4j.LoggerFactory
 import java.security.KeyStore
 import javax.net.ssl.SNIHostName
 import javax.net.ssl.SNIMatcher
 import javax.net.ssl.SNIServerName
 import javax.net.ssl.StandardConstants
-import sun.security.util.HostnameChecker
-import sun.security.util.HostnameChecker.TYPE_TLS
-import java.lang.NumberFormatException
-import java.security.cert.CertificateException
+import java.net.InetAddress
+import java.net.UnknownHostException
 import java.security.cert.X509Certificate
 
-class HostnameMatcher(private val keyStore: KeyStore) : SNIMatcher(0) {
+class HostnameMatcher(private val keyStore: KeyStore) : SNIMatcher(1) {
 
-    private val logger = LoggerFactory.getLogger(HostnameMatcher::class.java)
+    companion object {
+        private val logger = LoggerFactory.getLogger(HostnameMatcher::class.java)
 
-    var matchedAlias: String?  = null
+        private const val ALTNAME_DNS = 2
+        private const val ALTNAME_IP = 7
+    }
+
+
+
+    var matchedAlias: String? = null
         private set
     var matchedServerName: String? = null
         private set
@@ -37,12 +43,7 @@ class HostnameMatcher(private val keyStore: KeyStore) : SNIMatcher(0) {
                 val certificate = keyStore.getCertificate(alias).x509
                 val cordaX500Name = CordaX500Name.build(certificate.subjectX500Principal)
                 val c4SniValue = SniCalculator.calculateSni(cordaX500Name.toString(), NetworkType.CORDA_4, "")
-                val c5Check = try {
-                    HostnameChecker.getInstance(TYPE_TLS).match(serverNameString, certificate)
-                    true
-                } catch (e: CertificateException) {
-                    false
-                }
+                val c5Check = match(serverNameString, certificate)
 
                 if (serverNameString == c4SniValue || cordaX500Name.commonName == serverNameString || c5Check) {
                     matchedAlias = alias
@@ -58,39 +59,44 @@ class HostnameMatcher(private val keyStore: KeyStore) : SNIMatcher(0) {
     }
 
     private fun match(name: String, certificate: X509Certificate): Boolean {
-        if (isIpAddress(name)) {
+        return if (InetAddressValidator.getInstance().isValid(name)) {
             matchIp(name, certificate)
         } else {
             matchDNS(name, certificate)
         }
     }
 
-    private fun isIpAddress(str: String): Boolean {
-        return isIpV4Address(str) || isIpV6Address(str)
-    }
+//    private fun isIpAddress(str: String): Boolean {
+//        return isIpV4Address(str) || isIpV6Address(str)
+//    }
+//
+//    private fun isIpV4Address(str: String): Boolean {
+//        val octets = str.split(".")
+//        if (octets.size != 4)
+//            return false
+//        try {
+//            octets.forEach {
+//                val intValue = it.toInt()
+//                if (intValue < 1 || intValue > 255)
+//                    return false
+//            }
+//        } catch (e: NumberFormatException) {
+//            return false
+//        }
+//        return true
+//    }
+//
+//    private fun isIpV6Address(str: String): Boolean {
+//        // Tricky to do
+//        return false
+//    }
 
-    private fun isIpV4Address(str: String): Boolean {
-        val octets = str.split(".")
-        if (octets.size != 4)
-            return false
-        try {
-            octets.forEach {
-                val intValue = it.toInt()
-                if (intValue < 1 || intValue > 255)
-                    return false
-            }
-        } catch (e: NumberFormatException) {
-            return false
-        }
-        return true
-    }
-
-    private fun isIpV6Address(str: String): Boolean {
-        // A lot more tricky to do
-        return false
-    }
-
-    private fun matchIp(name: String, certificate: X509Certificate): Boolean {
+    /**
+     * RFC2818: In some cases, the URI is specified as an IP address rather than a
+     * hostname. In this case, the iPAddress subjectAltName must be present
+     * in the certificate and must exactly match the IP in the URI.
+     */
+    private fun matchIp(hostIP: String, certificate: X509Certificate): Boolean {
         val names = certificate.subjectAlternativeNames
         if (names.isEmpty()) {
             logger.debug("No subject alternative names found in the certificate")
@@ -98,13 +104,143 @@ class HostnameMatcher(private val keyStore: KeyStore) : SNIMatcher(0) {
         }
 
         names.forEach {
-            if (7 == it[0]) {
-                val address = it[1
+            if (ALTNAME_IP == it[0]) {
+                val altNameAsIP = it[1] as String
+                if (hostIP == altNameAsIP) {
+                    return true
+                } else {
+                    // Perhaps it's IPv6, in which case we need to ensure equality in case of abbreviated or long form
+                    try {
+                        if (InetAddress.getByName(hostIP) == InetAddress.getByName(altNameAsIP)) {
+                            return true
+                        }
+                    } catch (e: UnknownHostException) {
+                        logger.debug(e.message)
+                    } catch (e: SecurityException) {
+                        logger.debug(e.message)
+                    }
+                }
             }
         }
+
+        return false
     }
 
-    private fun matchDNS(name: String, certificate: X509Certificate): Boolean {
+    /**
+     * RFC2818: If a subjectAltName extension of type dNSName is present, that MUST
+     * be used as the identity. Otherwise, the (most specific) Common Name
+     * field in the Subject field of the certificate MUST be used. Although
+     * the use of the Common Name is existing practice, it is deprecated and
+     * Certification Authorities are encouraged to use the dNSName instead.
+     *
+     * If more than one identity of a given type is present in
+     * the certificate (e.g., more than one dNSName name, a match in any one
+     * of the set is considered acceptable.) Names may contain the wildcard
+     * character * which is considered to match any single domain name
+     * component or component fragment. E.g., *.a.com matches foo.a.com but
+     * not bar.foo.a.com. f*.com matches foo.com but not bar.com.
+     */
+    private fun matchDNS(hostName: String, certificate: X509Certificate): Boolean {
+        val names = certificate.subjectAlternativeNames
+        if (names.isEmpty()) {
+            logger.debug("No subject alternative names found in the certificate")
+            return false
+        }
+        var usingDNS = false
+        names.forEach {
+            if(ALTNAME_DNS == it[0]) {
+                usingDNS = true
+                val altName = it[1] as String
+                if (matchWithWildcard(hostName.toLowerCase(), altName.toLowerCase())) {
+                    return true
+                }
+            }
+        }
 
+        if (usingDNS) {
+            // If subject alternate names contain DNS names and none match, we don't check the CN
+            return false
+        }
+
+        val cn = CordaX500Name.build(certificate.subjectX500Principal).commonName
+        if (matchWithWildcard(hostName.toLowerCase(), cn?.toLowerCase())) {
+            return true
+        }
+
+        return false
+    }
+
+    internal fun matchWithWildcard(expectedName: String, altName: String?): Boolean {
+        altName?.let {
+            // Straightforward check
+            if (expectedName == altName)
+                return true
+
+            // Check if wildcards exist and if they are legal
+            if (illegalWildcard(altName)) {
+                return false
+            }
+
+            val expectedNameSections = expectedName.split(".")
+            val altNameSections = altName.split(".")
+
+            // Wildcard can only be used for one group
+            if (expectedNameSections.size != altNameSections.size) {
+                return false
+            }
+
+            expectedNameSections.forEachIndexed { idx, expectedGroup ->
+                // Wildcard can be used for entire group or only part of it
+                val wildCardIndex = altNameSections[idx].lastIndexOf('*')
+                if (wildCardIndex != -1) {
+                    // Check if the expected group starts with the characters behind the wildcard
+                    // Covers the case of entire group wildcard as well as partial group wildcard (f*.bar)
+                    val beforeWildcard = altNameSections[idx].substring(0, wildCardIndex)
+                    if (!expectedGroup.startsWith(beforeWildcard)) {
+                        return false
+                    }
+                } else {
+                    if (expectedGroup != altNameSections[idx]) {
+                        return false
+                    }
+                }
+
+            }
+
+            return true
+        }
+
+        return false
+    }
+
+    internal fun illegalWildcard(name: String): Boolean {
+        val wildcardIndex = name.lastIndexOf('*')
+
+        // DNS name does not contain wildcard
+        if (wildcardIndex == -1) {
+            return false
+        }
+
+        // DNS name can only have one wildcard
+        if (name.substring(0, wildcardIndex).lastIndexOf('*') != -1) {
+            return true
+        }
+
+        // DNS name cannot be just the wildcard
+        if ("*" == name || "*." == name) {
+            return true
+        }
+
+        // Wildcard character cannot not be the last and needs to be followed by a dot ("*.net")
+        if (wildcardIndex == name.length - 1 || name[wildcardIndex + 1] != '.') {
+            return true
+        }
+
+        val nameAfterWildcard = name.substring(wildcardIndex + 2)
+        // Name after wildcard group must be a valid domain
+        if (!DomainValidator.getInstance().isValid("filler.$nameAfterWildcard")) {
+            return true
+        }
+        return false
     }
 }
--- components/gateway/src/test/kotlin/net/corda/p2p/gateway/messaging/http/HostnameMatcherTest.kt
+++ components/gateway/src/test/kotlin/net/corda/p2p/gateway/messaging/http/HostnameMatcherTest.kt
@@ -1,10 +1,15 @@
+@file:Suppress("JAVA_MODULE_DOES_NOT_EXPORT_PACKAGE")
 package net.corda.p2p.gateway.messaging.http
 
+import net.corda.nodeapi.internal.crypto.getX509Certificate
 import net.corda.p2p.NetworkType
 import net.corda.v5.application.identity.CordaX500Name
+import org.apache.commons.validator.routines.DomainValidator
 import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
+import sun.security.util.HostnameChecker
+import sun.security.util.HostnameChecker.TYPE_TLS
 import java.io.FileInputStream
 import java.security.KeyStore
 import javax.net.ssl.SNIHostName
@@ -24,13 +29,36 @@ class HostnameMatcherTest {
 
     @Test
     fun `C5 SNI match`() {
+        // Because the tool used to create this certificate (tinycert.org) performs validations over subject alt names,
+        // only happy paths can be tested using the certificate as input
         val keyStore: KeyStore = KeyStore.getInstance("JKS").also {
             it.load(FileInputStream(javaClass.classLoader.getResource("sslkeystore_c5.jks")!!.file), "password".toCharArray())
         }
         val matcher = HostnameMatcher(keyStore)
-        assertTrue(matcher.matches(SNIHostName("chip.net")))
-        assertTrue(matcher.matches(SNIHostName("www.chip.net")))
-        assertTrue(matcher.matches(SNIHostName("127.0.0.1")))
         assertFalse(matcher.matches(SNIHostName("alice.net")))
+        assertTrue(matcher.matches(SNIHostName("www.TesT.com")))
+        assertTrue(matcher.matches(SNIHostName("www.test.co.uk")))
+        assertTrue(matcher.matches(SNIHostName("alice.test.net"))) // matches with *.test.net
+        assertTrue(matcher.matches(SNIHostName("bob.test.net"))) // matches with *.test.net
+        assertTrue(matcher.matches(SNIHostName("10.11.12.13")))
+        assertTrue(matcher.matches(SNIHostName("2001:db8:3333:4444:cccc:dddd:eeee:ffff"))) // regular IPv6
+        assertTrue(matcher.matches(SNIHostName("2001:0db8:3c4d:0015:0000:0000:1a2f:1a2b"))) // regular IPv6 which can be abbreviated
+        assertTrue(matcher.matches(SNIHostName("2001:db8:3c4d:15::1a2f:1a2b"))) // abbreviated IPv6
+        assertTrue(matcher.matches(SNIHostName("2001:db8:3c4d:15:0:0:1a2f:1a2b"))) // another abbreviation of the above IPv6 address
+
+        // Certificate tool wouldn't allow using a DNS name with only part of a group wildcarded, even though the RFC allows it
+        // We test this match using the direct HostnameMatcher method
+        assertTrue(matcher.matchWithWildcard("alice.test.net", "al*.test.net"))
+
+        // Use of wildcard
+        assertTrue(matcher.illegalWildcard("*"))
+        assertTrue(matcher.illegalWildcard("*."))
+//        assertTrue(matcher.illegalWildcard("*.*.net")) // Should this be legal?
+//        assertTrue(matcher.illegalWildcard("www.*.foo.bar.net")) // Should this be legal?
+        assertTrue(matcher.illegalWildcard("www.*foo.bar.net"))
+        assertTrue(matcher.illegalWildcard("www.f*o.bar.net"))
+        assertTrue(matcher.illegalWildcard("www.foo.*"))
+        assertTrue(matcher.illegalWildcard("www.foo.ne*"))
+        assertTrue(matcher.illegalWildcard("www.foo.*et"))
     }
 }
