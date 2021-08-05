a60264f897b4e8324daf4a952e6c6fb56dd9f0fb
bpaunescu <bogdan.paunescu@r3.com>
2021-08-05 17:54:51 +0100

    [CORE-1283]-Gateway - message sending/receiving (#60)
    * [CORE-1283]-bump corda-base version to latest RC; add netty and caffeine version numbers to gradle props
    
    * [CORE-1283]-bump netty version to match Corda
    
    * [CORE-1283]-initial commit for Gateway HTTP components
    
    * [CORE-1283]-connection manager using caffeine cache
    
    * [CORE-1283]-add basic processing for upstream messages
    
    * [CORE-1283]-fix merge issues due to LifeCycle changes
    
    * [CORE-1283]-HttpClient channel is closed explicitly without waiting for close to be done on server side
    
    * [CORE-1283]-address detekt issues
    
    * [CORE-1283]-add stubs for gateway tests
    
    * [CORE-1283]-tests
    
    * [CORE-1283]-fix merging issues
    
    * [CORE-1283]-add gateway integration tests; add request validation; clean-up
    
    * [CORE-1283]-address detekt issues
    
    * [CORE-1283]-tweak logging
    
    * [CORE-1283]-fix issues post branch sync
    
    * [CORE-1283]-session management
    
    * [CORE-1283]-LinkInMessage between gateways instead of LinkOutMessage
    
    * [CORE-1283]-fix flaky test
    
    * [CORE-1283]-backpressure, delivery retry
    
    * [CORE-1283]-fix test, set debug logging
    
    * [CORE-1283]-tweak logging
    
    * [CORE-1283]-refactor channel handler
    
    * [CORE-1283]-http client no longer reconnects
    
    * [CORE-1283]-Gateway sets started status correctly
    
    * [CORE-1283]-fix detekt issues
    
    * [CORE-1283]-fix API issues post release branch sync
    
    * [CORE-1283]-move gateway tests to integration tests
    
    * [CORE-1283]-use correct topic names from shared avro schema
    
    * [CORE-1283]-refactor out duplicated code
    
    * [CORE-1283]-remove HttpMessage.release() method
    
    * [CORE-1283]-harmonize HttpHelper code
    
    * [CORE-1283]-remove unused property
    
    * [CORE-1283]-replace usage of NetworkHostAndPort with URI and SocketAddress
    
    * [CORE-1283]-move constants to configuration objects; clean-up kdocs
    
    * CORE-1283: Remove observables
    
    * CORE-1283: Asynchronous HTTP client
    
    * CORE-1283: Start and stop outboundMessageProcessor
    
    * [NOTICK]-sync with release branch
    
    * [NOTICK]-sync with release branch
    
    * [NOTICK]-detekt wildcard imports
    
    * CORE-1283: Remove unused dependency
    
    * CORE-1283: Rename Lifecycle
    
    Co-authored-by: Denis Rekalov <denis.rekalov@r3.com>

---
+++ components/gateway/src/main/kotlin/net/corda/p2p/gateway/messaging/internal/OutboundMessageHandler.kt
@@ -0,0 +1,96 @@
+package net.corda.p2p.gateway.messaging.internal
+
+import com.typesafe.config.ConfigFactory
+import io.netty.handler.codec.http.HttpResponseStatus
+import net.corda.lifecycle.Lifecycle
+import net.corda.messaging.api.processor.EventLogProcessor
+import net.corda.messaging.api.publisher.Publisher
+import net.corda.messaging.api.publisher.config.PublisherConfig
+import net.corda.messaging.api.publisher.factory.PublisherFactory
+import net.corda.messaging.api.records.EventLogRecord
+import net.corda.messaging.api.records.Record
+import net.corda.p2p.LinkInMessage
+import net.corda.p2p.LinkOutMessage
+import net.corda.p2p.gateway.Gateway.Companion.PUBLISHER_ID
+import net.corda.p2p.gateway.messaging.ConnectionManager
+import net.corda.p2p.gateway.messaging.http.HttpEventListener
+import net.corda.p2p.gateway.messaging.http.HttpMessage
+import net.corda.p2p.schema.Schema.Companion.LINK_IN_TOPIC
+import org.slf4j.LoggerFactory
+import java.io.IOException
+import java.net.URI
+import java.nio.ByteBuffer
+
+/**
+ * This is an implementation of an [EventLogProcessor] used to consume messages from a P2P message subscription. The received
+ * events are processed and fed into the HTTP pipeline. No records will be produced by this processor as a result.
+ */
+class OutboundMessageHandler(private val connectionPool: ConnectionManager,
+                             private val publisherFactory: PublisherFactory
+) : EventLogProcessor<String, LinkOutMessage>, Lifecycle, HttpEventListener {
+    companion object {
+        private val logger = LoggerFactory.getLogger(OutboundMessageHandler::class.java)
+    }
+
+    private var p2pInPublisher: Publisher? = null
+
+    private var started = false
+    override val isRunning: Boolean
+        get() = started
+
+    override fun start() {
+        logger.info("Starting P2P message sender")
+        p2pInPublisher = publisherFactory.createPublisher(PublisherConfig(PUBLISHER_ID), ConfigFactory.empty())
+        connectionPool.addListener(this)
+        started = true
+    }
+
+    override fun stop() {
+        logger.info("Stopping P2P message sender")
+        started = false
+        connectionPool.removeListener(this)
+        p2pInPublisher?.close()
+        p2pInPublisher = null
+    }
+
+    override fun onNext(events: List<EventLogRecord<String, LinkOutMessage>>): List<Record<*, *>> {
+        events.forEach { evt ->
+            evt.value?.let { peerMessage ->
+                val destination = URI.create(peerMessage.header.address)
+                val message = LinkInMessage(peerMessage.payload).toByteBuffer().array()
+                connectionPool.acquire(destination).write(message)
+            }
+        }
+        return emptyList()
+    }
+
+    /**
+     * Handler for P2P messages sent back as a result of a request. Typically, these responses have no payloads and serve
+     * as an indication of successful receipt on the other end. In case of a session request message, the response will
+     * contain information which then needs to be forwarded to the LinkManager
+     */
+    override fun onMessage(message: HttpMessage) {
+        logger.debug("Processing response message from ${message.source} with status ${message.statusCode}")
+        if (HttpResponseStatus.OK == message.statusCode) {
+            // response messages should have empty payloads unless they are part of the initial session handshake
+            if (message.payload.isNotEmpty()) {
+                try {
+                    // Attempt to deserialize as an early check. Shouldn't forward unrecognised message types
+                    LinkInMessage.fromByteBuffer(ByteBuffer.wrap(message.payload))
+                    val record = Record(LINK_IN_TOPIC, "key", message)
+                    p2pInPublisher?.publish(listOf(record))
+                } catch (e: IOException) {
+                    logger.warn("Invalid message received. Cannot deserialize")
+                    logger.debug(e.stackTraceToString())
+                }
+            }
+        } else {
+            logger.warn("Something went wrong with peer processing an outbound message. Peer response status ${message.statusCode}")
+        }
+    }
+
+    override val keyClass: Class<String>
+        get() = String::class.java
+    override val valueClass: Class<LinkOutMessage>
+        get() = LinkOutMessage::class.java
+}
