ad2def76d3dac84a7402bdcc0984e98ff43d6fd9
James Higgs <james.higgs@r3.com>
2021-08-26 16:55:12 +0100

    Add a check for negative batch size

--- libs/lifecycle/lifecycle-impl/src/main/kotlin/net/corda/lifecycle/impl/LifecycleCoordinatorFactoryImpl.kt
+++ libs/lifecycle/lifecycle-impl/src/main/kotlin/net/corda/lifecycle/impl/LifecycleCoordinatorFactoryImpl.kt
@@ -4,6 +4,7 @@ import net.corda.lifecycle.LifecycleCoordinator
 import net.corda.lifecycle.LifecycleCoordinatorFactory
 import net.corda.lifecycle.LifecycleCoordinatorName
 import net.corda.lifecycle.LifecycleEventHandler
+import net.corda.lifecycle.LifecycleException
 import net.corda.lifecycle.impl.registry.LifecycleRegistryImpl
 import org.osgi.service.component.annotations.Component
 import org.osgi.service.component.annotations.ServiceScope
@@ -18,6 +19,12 @@ class LifecycleCoordinatorFactoryImpl : LifecycleCoordinatorFactory {
         batchSize: Int,
         handler: LifecycleEventHandler
     ): LifecycleCoordinator {
+        if (batchSize <= 0) {
+            throw LifecycleException(
+                "Failed to create a lifecycle coordinator with name $name as the batch size is less" +
+                        " than 1. (Provided batch size was $batchSize)"
+            )
+        }
         val coordinator = LifecycleCoordinatorImpl(name, batchSize, registry, handler)
         registry.registerCoordinator(name, coordinator)
         return coordinator
