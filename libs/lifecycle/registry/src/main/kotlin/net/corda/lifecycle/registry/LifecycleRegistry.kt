5fb2eee06f7f4b402908bbbc0c880bd1673a4c22
James Higgs <45565019+JamesHR3@users.noreply.github.com>
2021-08-27 15:19:12 +0100

    CORE-2034: Introduce the lifecycle registry and API/Implementation split (#183)
    * CORE-2034: Add a lifecycle registry and implement the API
    
    * Remove event driven status updates
    
    * CORE-2034: Move to an API + implementation bundle for lifecycle
    
    * Remove unneeded files from the top level lifecycle folder
    
    * Add reason to status update and provide registry updates on coordinator status change
    
    * Change the coordinator name to use a structured type
    
    * Add a test case to verify the instance id
    
    * Add functionality for registering by name
    
    * Add a default parameter for reason and update testing
    
    * Add a check when updating statuses that the coordinator exists
    
    * Add a check for negative batch size
    
    * Implement a basic form of closing a coordinator
    
    * Fix OSGi annotation imports and some formatting errors

---
+++ libs/lifecycle/registry/src/main/kotlin/net/corda/lifecycle/registry/LifecycleRegistry.kt
@@ -0,0 +1,27 @@
+package net.corda.lifecycle.registry
+
+import net.corda.lifecycle.LifecycleCoordinatorName
+
+/**
+ * Obtain information about the current running status of coordinators in the system.
+ *
+ * This is used to monitor the status of components in the system using the lifecycle library. On creating a new
+ * coordinator, it is added to the registry under the provided name. Data about the current status of the coordinators
+ * in the system can then be queried for. Coordinators are added to the registry automatically on creation.
+ *
+ * Adding multiple coordinators under the same name is supported to cope with component reuse. An optional scope
+ * parameter can be provided at coordinator creation time to declare what purpose this instance of the component is for.
+ * If this is omitted, the registry will assign an integer suffix for every coordinator supplied after the first.
+ *
+ * This interface is concerned with providing the read side of component data and should be used only by monitoring
+ * components.
+ */
+interface LifecycleRegistry {
+
+    /**
+     * Obtain the current status of all objects.
+     *
+     * @return A map of coordinator names to their current statuses.
+     */
+    fun componentStatus(): Map<LifecycleCoordinatorName, CoordinatorStatus>
+}
