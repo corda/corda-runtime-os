bf30f52aef3f3273ae80c34a71e34eaa39b50764
driessamyn <dries.samyn@r3.com>
2021-09-01 16:23:42 +0100

    Initial commit of corda ORM abstraction.

---
+++ libs/db/orm/src/main/kotlin/net/corda/orm/EntityManagerFactoryFactory.kt
@@ -0,0 +1,22 @@
+package net.corda.orm
+
+import javax.persistence.EntityManagerFactory
+
+/**
+ * EntityManagerFactory factory
+ */
+interface EntityManagerFactoryFactory {
+    /**
+     * Create [EntityManagerFactory] for the given JPA [entities] and target data source
+     *
+     * @param persistenceUnitName
+     * @param entities to be managed by the [EntityManagerFactory]
+     * @param configuration for the target data source
+     * @return [EntityManagerFactory]
+     */
+    fun createEntityManagerFactory(
+        persistenceUnitName: String,
+        entities: List<Class<*>>,
+        configuration: EntityManagerConfiguration
+    ): EntityManagerFactory
+}
