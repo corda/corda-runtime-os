954788cef949043345d5174652e37787789de011
RazvanCodreanu <razvan.codreanu@r3.com>
2021-09-02 16:21:05 +0100

    CORE-2520 remove processor method from api

--- libs/messaging/messaging/src/main/kotlin/net/corda/messaging/api/subscription/RPCResponder.kt
+++ libs/messaging/messaging/src/main/kotlin/net/corda/messaging/api/subscription/RPCResponder.kt
@@ -1,18 +1,12 @@
 package net.corda.messaging.api.subscription
 
 import net.corda.lifecycle.Lifecycle
-import net.corda.messaging.api.processor.RPCResponderProcessor
 
 interface RPCResponder<TREQ, TRESP> : Lifecycle {
 
     /**
-     * Register a processor for the [RPCResponder] to handle all inbound requests
-     */
-    fun registerProcessor(rpcResponderProcessor: RPCResponderProcessor<TREQ, TRESP>): AutoCloseable
-
-    /**
      * Send response via RPC
      */
-    fun sendResponse(req: TREQ)
+    fun sendResponse(req: TRESP)
 
 }
