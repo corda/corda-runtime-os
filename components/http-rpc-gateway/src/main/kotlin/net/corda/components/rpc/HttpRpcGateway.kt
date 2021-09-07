0856e4094486af05c577670802ad16ecedd21ce9
Viktor Kolomeyko <Viktor.Kolomeyko@r3.com>
2021-09-07 08:36:09 +0100

    CORE-2562: First stub on HTTP RPC Gateway component

---
+++ components/http-rpc-gateway/src/main/kotlin/net/corda/components/rpc/HttpRpcGateway.kt
@@ -0,0 +1,76 @@
+package net.corda.components.rpc
+
+import com.typesafe.config.Config
+import net.corda.libs.configuration.read.ConfigListener
+import net.corda.libs.configuration.read.ConfigReadService
+import net.corda.libs.configuration.read.factory.ConfigReadServiceFactory
+import net.corda.lifecycle.Lifecycle
+import net.corda.lifecycle.LifecycleCoordinator
+import net.corda.lifecycle.LifecycleEvent
+import net.corda.v5.base.exceptions.CordaRuntimeException
+import net.corda.v5.base.util.contextLogger
+import org.osgi.service.component.annotations.Reference
+import org.slf4j.Logger
+
+
+class ConfigReceivedEvent(val currentConfigurationSnapshot: Map<String, Config>) : LifecycleEvent
+class MessagingConfigUpdateEvent(val currentConfigurationSnapshot: Map<String, Config>) : LifecycleEvent
+
+class HttpRpcGateway(
+    private val lifeCycleCoordinator: LifecycleCoordinator,
+    @Reference(service = ConfigReadServiceFactory::class)
+    private val readServiceFactory: ConfigReadServiceFactory
+) : Lifecycle {
+
+    companion object {
+        private val log: Logger = contextLogger()
+        const val MESSAGING_CONFIG: String = "corda.messaging"
+    }
+
+    private var receivedSnapshot = false
+
+    private var configReadService: ConfigReadService? = null
+    private var sub: AutoCloseable? = null
+    private var bootstrapConfig: Config? = null
+
+    override val isRunning: Boolean
+    get() = receivedSnapshot
+
+    fun start(bootstrapConfig: Config) {
+        this.bootstrapConfig = bootstrapConfig
+        this.start()
+    }
+
+    override fun start() {
+        if(bootstrapConfig != null) {
+            configReadService = readServiceFactory.createReadService(bootstrapConfig!!)
+            val lister = ConfigListener { changedKeys: Set<String>, currentConfigurationSnapshot: Map<String, Config> ->
+                if (!receivedSnapshot) {
+                    if (changedKeys.contains(MESSAGING_CONFIG)) {
+                        log.info("Config read service config snapshot received")
+                        receivedSnapshot = true
+                        lifeCycleCoordinator.postEvent(ConfigReceivedEvent(currentConfigurationSnapshot))
+                    }
+                } else {
+                    log.info("Config read service config update received")
+                    if (changedKeys.contains(MESSAGING_CONFIG)) {
+                        log.info("Config update contains kafka config")
+                        lifeCycleCoordinator.postEvent(MessagingConfigUpdateEvent(currentConfigurationSnapshot))
+                    }
+                }
+
+            }
+            sub = configReadService!!.registerCallback(lister)
+            configReadService!!.start()
+        } else {
+            val message = "Use the other start method available and pass in the bootstrap configuration"
+            log.error(message)
+            throw CordaRuntimeException(message)
+        }
+    }
+
+    override fun stop() {
+        sub?.close()
+        sub = null
+    }
+}
