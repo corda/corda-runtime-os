08fb2264a096f8c895652a8c00d2614a036ee147
Ryan Fowler <ryan.fowler@r3.com>
2021-09-07 14:25:19 +0100

    CORE-2110, CORE-2517: Improve Kryo access and usage

--- libs/serialization/serialization-kryo/src/main/kotlin/net/corda/kryoserialization/impl/CheckpointSerializationServiceImpl.kt
+++ libs/serialization/serialization-kryo/src/main/kotlin/net/corda/kryoserialization/impl/CheckpointSerializationServiceImpl.kt
@@ -2,7 +2,6 @@ package net.corda.kryoserialization.impl
 
 import net.corda.serialization.CheckpointSerializationService
 import net.corda.serialization.CheckpointSerializer
-import net.corda.v5.base.types.sequence
 
 class CheckpointSerializationServiceImpl(
     private val serializer: CheckpointSerializer
@@ -13,6 +12,6 @@ class CheckpointSerializationServiceImpl(
     }
 
     override fun <T : Any> deserialize(bytes: ByteArray, clazz: Class<T>): T {
-        return serializer.deserialize(bytes.sequence(), clazz)
+        return serializer.deserialize(bytes, clazz)
     }
 }
