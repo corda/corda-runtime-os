dc08e0c0c3931714205d1e158dc2aa5bad918fbb
LWogan <lorcan.wogan@r3.com>
2021-09-08 17:14:47 +0100

    CORE-1711 some more tests

--- libs/messaging/kafka-messaging-impl/src/test/kotlin/net/corda/messaging/kafka/subscription/consumer/wrapper/StateAndEventConsumerImplTest.kt
+++ libs/messaging/kafka-messaging-impl/src/test/kotlin/net/corda/messaging/kafka/subscription/consumer/wrapper/StateAndEventConsumerImplTest.kt
@@ -2,10 +2,10 @@ package net.corda.messaging.kafka.subscription.consumer.wrapper
 
 import com.typesafe.config.Config
 import net.corda.messaging.api.subscription.listener.StateAndEventListener
-import net.corda.messaging.kafka.subscription.consumer.listener.StateAndEventRebalanceListenerTest
 import net.corda.messaging.kafka.subscription.consumer.wrapper.impl.StateAndEventConsumerImpl
 import net.corda.messaging.kafka.subscription.factory.SubscriptionMapFactory
 import org.apache.kafka.common.TopicPartition
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.mockito.kotlin.any
 import org.mockito.kotlin.doAnswer
@@ -13,6 +13,7 @@ import org.mockito.kotlin.mock
 import org.mockito.kotlin.times
 import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
+import java.time.Clock
 
 class StateAndEventConsumerImplTest {
 
@@ -29,6 +30,35 @@ class StateAndEventConsumerImplTest {
 
         verify(eventConsumer, times(1)).close(any())
         verify(stateConsumer, times(1)).close(any())
+    }
+
+    @Test
+    fun testGetValue() {
+        val (stateAndEventListener, eventConsumer, stateConsumer, config, mapFactory, partitions) = setupMocks()
+        val partitionState = StateAndEventPartitionState(mutableMapOf(partitions.first().partition() to mutableMapOf("key1" to Pair(Long.MIN_VALUE, "value1"))), mutableMapOf(partitions.first().partition() to Long.MAX_VALUE))
+        val consumer = StateAndEventConsumerImpl(config, mapFactory, eventConsumer, stateConsumer, partitionState, stateAndEventListener)
+        val valueKey1 = consumer.getValue("key1")
+        assertThat(valueKey1).isEqualTo("value1")
+        val valueKey2 = consumer.getValue("key2")
+        assertThat(valueKey2).isNull()
+    }
+
+
+    @Test
+    fun testOnProcessorStateUpdated() {
+        val (stateAndEventListener, eventConsumer, stateConsumer, config, mapFactory, partitions) = setupMocks()
+        val partitionId = partitions.first().partition()
+        val partitionState = StateAndEventPartitionState(mutableMapOf(partitions.first().partition() to mutableMapOf("key1" to Pair(Long.MIN_VALUE, "value1"))), mutableMapOf(partitions.first().partition() to Long.MAX_VALUE))
+        val consumer = StateAndEventConsumerImpl(config, mapFactory, eventConsumer, stateConsumer, partitionState, stateAndEventListener)
+
+        consumer.onProcessorStateUpdated(mutableMapOf(partitions.first().partition() to mutableMapOf("key1" to null, "key2" to "value2")), Clock.systemUTC())
+
+
+        val currentStates = partitionState.currentStates
+        assertThat(currentStates[partitionId]?.get("key1")).isNull()
+        assertThat(currentStates[partitionId]?.get("key2")).isNotNull
+
+        verify(stateAndEventListener, times(1)).onPostCommit(any())
     }
 
 
