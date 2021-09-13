0b3af184875f73382a00519e2f09ed8c6f481d8e
Alexey Kadyrov <67952405+alexey-kadyrov-r3@users.noreply.github.com>
2021-09-13 10:12:53 +0100

    added defaultSignatureScheme to CryptoServiceConfig with default ECDSA_SECP256R1_CODE_NAME (#64)

--- cipher-suite/src/main/kotlin/net/corda/v5/cipher/suite/config/CryptoServiceConfig.kt
+++ cipher-suite/src/main/kotlin/net/corda/v5/cipher/suite/config/CryptoServiceConfig.kt
@@ -1,11 +1,13 @@
 package net.corda.v5.cipher.suite.config
 
+import net.corda.v5.cipher.suite.schemes.ECDSA_SECP256R1_CODE_NAME
 import java.time.Duration
 
 class CryptoServiceConfig(
     val serviceName: String = DEFAULT_SERVICE_NAME,
     val timeout: Duration = Duration.ofSeconds(5),
-    val serviceConfig: Map<String, Any?> = mapOf()
+    val serviceConfig: Map<String, Any?> = mapOf(),
+    val defaultSignatureScheme: String = ECDSA_SECP256R1_CODE_NAME
 ) {
     companion object {
         const val DEFAULT_SERVICE_NAME = "default"
