5afcd3a1009d2cc74ff58828f85359c31a622e07
Yiftach Kaplan <yiftach.kaplan@r3.com>
2021-09-14 17:41:22 +0100

    Simplyfy the durable subscription test

--- libs/messaging/inmemory-messaging-impl/src/integration-test/kotlin/net/corda/messaging/emulation/subscription/durable/DurableSubscriptionIntegrationTests.kt
+++ libs/messaging/inmemory-messaging-impl/src/integration-test/kotlin/net/corda/messaging/emulation/subscription/durable/DurableSubscriptionIntegrationTests.kt
@@ -4,6 +4,7 @@ import net.corda.messaging.api.processor.DurableProcessor
 import net.corda.messaging.api.publisher.config.PublisherConfig
 import net.corda.messaging.api.publisher.factory.PublisherFactory
 import net.corda.messaging.api.records.Record
+import net.corda.messaging.api.subscription.Subscription
 import net.corda.messaging.api.subscription.factory.SubscriptionFactory
 import net.corda.messaging.api.subscription.factory.config.SubscriptionConfig
 import org.assertj.core.api.Assertions.assertThat
@@ -11,13 +12,13 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.osgi.test.common.annotation.InjectService
 import org.osgi.test.junit5.service.ServiceExtension
+import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.TimeUnit
 import java.util.concurrent.atomic.AtomicInteger
 
 @ExtendWith(ServiceExtension::class)
 class DurableSubscriptionIntegrationTests {
-
     data class Value(val data: Int)
     data class Key(val data: Long)
 
@@ -29,60 +30,78 @@ class DurableSubscriptionIntegrationTests {
     @InjectService(timeout = 4000)
     lateinit var publisherFactory: PublisherFactory
 
-    // each topic will send 3 times more events than the previous one.
-    val counter = CountDownLatch(1 + 3 + 9 + 27)
+    private val numberOfMessagesToSend = 10
+    private val numberOfMessagesToReplyForEachMessage = 2
+    private val numberOfSubscribers = 3
 
-    inner class Subscription(private val index: Int) {
-        val config = SubscriptionConfig(
-            eventTopic = "durable.integration.test.topic.$index",
-            groupName = "durable.integration.test.group.$index",
-        )
-        val receivedEvents = AtomicInteger(0)
-        private val subscriptions by lazy {
-            (1..4).map {
-                subscriptionFactory.createDurableSubscription(
-                    subscriptionConfig = config,
-                    processor = object : DurableProcessor<Key, Value> {
-                        override fun onNext(events: List<Record<Key, Value>>): List<Record<*, *>> {
-                            return events.flatMap {
-                                receivedEvents.incrementAndGet()
-                                counter.countDown()
-                                (1..3).map { Record("durable.integration.test.topic.${index + 1}", Key(it.toLong()), Value(3)) }
-                            }
+    private val index = AtomicInteger(0)
+    private val receivedValues = ConcurrentHashMap.newKeySet<Int?>()
+
+    private val otherConfig = SubscriptionConfig(
+        eventTopic = "durable.integration.test.other.topic",
+        groupName = "durable.integration.test.other.group"
+    )
+    private val config = SubscriptionConfig(
+        eventTopic = "durable.integration.test.topic",
+        groupName = "durable.integration.test.group",
+    )
+
+    private val counter = CountDownLatch(numberOfMessagesToSend * numberOfMessagesToReplyForEachMessage)
+
+    private fun subscription(): Subscription<Key, Value> {
+        return subscriptionFactory.createDurableSubscription(
+            subscriptionConfig = config,
+            processor = object : DurableProcessor<Key, Value> {
+                override fun onNext(events: List<Record<Key, Value>>): List<Record<*, *>> {
+                    return events.flatMap {
+                        (1..numberOfMessagesToReplyForEachMessage).map {
+                            Record(
+                                otherConfig.eventTopic,
+                                Key(it.toLong()),
+                                Value(index.incrementAndGet())
+                            )
                         }
+                    }
+                }
 
-                        override val keyClass = Key::class.java
-                        override val valueClass = Value::class.java
-                    },
-                    partitionAssignmentListener = null
-                )
-            }
-        }
-
-        fun start() {
-            subscriptions.forEach {
-                it.start()
-            }
-        }
-        fun stop() {
-            subscriptions.forEach {
-                it.stop()
-            }
-        }
+                override val keyClass = Key::class.java
+                override val valueClass = Value::class.java
+            },
+            partitionAssignmentListener = null
+        )
     }
 
-    private val subscriptions by lazy {
-        (1..4).map { it -> Subscription(it).also { it.start() } }
+    private fun otherSubscription(): Subscription<Key, Value> {
+        return subscriptionFactory.createDurableSubscription(
+            subscriptionConfig = otherConfig,
+            processor = object : DurableProcessor<Key, Value> {
+                override fun onNext(events: List<Record<Key, Value>>): List<Record<*, *>> {
+                    events.forEach { event ->
+                        receivedValues.add(event.value?.data)
+                        counter.countDown()
+                    }
+                    return emptyList()
+                }
+
+                override val keyClass = Key::class.java
+                override val valueClass = Value::class.java
+            },
+            partitionAssignmentListener = null
+        )
     }
 
     @Test
     fun `test durable subscription`() {
-        subscriptions.forEach {
+        val subscriptions = (1..numberOfSubscribers).flatMap {
+            listOf(subscription(), otherSubscription())
+        }.onEach {
             it.start()
         }
 
         val publisherConfig = PublisherConfig(clientId)
-        val records = listOf(Record(subscriptions[0].config.eventTopic, Key(0), Value(1)))
+        val records = (1..numberOfMessagesToSend).map {
+            Record(config.eventTopic, Key(0), Value(1))
+        }
         publisherFactory.createPublisher(publisherConfig).use {
             it.publish(records)
         }
@@ -93,6 +112,6 @@ class DurableSubscriptionIntegrationTests {
             it.stop()
         }
 
-        assertThat(subscriptions.map { it.receivedEvents.get() }).isEqualTo(listOf(1, 3, 9, 27))
+        assertThat(receivedValues).containsAll((1..(numberOfMessagesToSend * numberOfMessagesToReplyForEachMessage)))
     }
 }
