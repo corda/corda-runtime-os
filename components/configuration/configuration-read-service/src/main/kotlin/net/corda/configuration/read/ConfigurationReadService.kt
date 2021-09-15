16abbca5838176f74f62bdab56ebbba0b79fc50d
James Higgs <45565019+JamesHR3@users.noreply.github.com>
2021-09-15 10:54:31 +0100

    CORE-2500: Add a configuration read component (#205)
    * CORE-2500: Add a configuration read component
    
    * Create a flow service component sketch
    
    * Update to fix build issues after rebase

---
+++ components/configuration/configuration-read-service/src/main/kotlin/net/corda/configuration/read/ConfigurationReadService.kt
@@ -0,0 +1,46 @@
+package net.corda.configuration.read
+
+import com.typesafe.config.Config
+import net.corda.lifecycle.Lifecycle
+
+/**
+ * A service managing the configuration in the process.
+ *
+ * The [ConfigurationReadService] is responsible for reading configuration from the message bus and exposing this to
+ * other services in the system. Configuration is expected to be dynamic, so services must be prepared to respond to
+ * configuration changes.
+ *
+ * Note that calling start or stop on this service is idempotent, so it is safe for other services to call start to make
+ * sure that the service is up.
+ */
+interface ConfigurationReadService : Lifecycle {
+
+    /**
+     * Register for configuration updates.
+     *
+     * The provided handler will be invoked when the first batch of configuration becomes available, or on registration
+     * if the configuration is already available. After this, the handler will be invoked every time the configuration
+     * changes.
+     *
+     * The returned handle may be closed to unregister from the configuration read service.
+     *
+     * @param configHandler The user configuration handler. See [ConfigurationHandler].
+     * @return A handle for this registration, which may be closed to unregister from the configuration read service.
+     */
+    fun registerForUpdates(configHandler: ConfigurationHandler) : AutoCloseable
+
+    /**
+     * Provide bootstrap configuration to the configuration service.
+     *
+     * This should be called by the application, providing enough initial configuration to connect to the message bus
+     * and retrieve the full configuration. Other services will not need to call this.
+     *
+     * Calling this multiple times will result in an error. An error is also thrown if the configuration does not allow
+     * the configuration read service to access the full configuration.
+     *
+     * @param config The bootstrap configuration to connect to the message bus.
+     * @throws ConfigurationException If the bootstrap configuration is provided after a connection has been
+     *                                established, or if the configuration does not allow access.
+     */
+    fun bootstrapConfig(config: Config)
+}
