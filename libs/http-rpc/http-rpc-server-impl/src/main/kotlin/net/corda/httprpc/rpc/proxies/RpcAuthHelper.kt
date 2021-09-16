05e2e4c294a99a16e7a040bf4109dc257b62d7cd
Dan Newton <dan.newton@r3.com>
2021-09-16 14:19:53 +0100

    CORE-2617 Remove remaining usages of `RPCOps` from `corda-api`
    `RPCOps` has been removed and an internal version `RpcOps` is defined in
    `:http-rpc`.

--- libs/http-rpc/http-rpc-server-impl/src/main/kotlin/net/corda/httprpc/rpc/proxies/RpcAuthHelper.kt
+++ libs/http-rpc/http-rpc-server-impl/src/main/kotlin/net/corda/httprpc/rpc/proxies/RpcAuthHelper.kt
@@ -2,7 +2,7 @@
 
 package net.corda.httprpc.rpc.proxies
 
-import net.corda.v5.application.messaging.RPCOps
+import net.corda.v5.httprpc.api.RpcOps
 import java.lang.reflect.Method
 
 
@@ -13,7 +13,7 @@ object RpcAuthHelper {
 
     fun methodFullName(clazz: Class<*>, methodName: String): String {
         require(clazz.isInterface) { "Must be an interface: $clazz" }
-        require(RPCOps::class.java.isAssignableFrom(clazz)) { "Must be assignable from RPCOps: $clazz" }
+        require(RpcOps::class.java.isAssignableFrom(clazz)) { "Must be assignable from RPCOps: $clazz" }
         return clazz.name + INTERFACE_SEPARATOR + methodName
     }
 }
