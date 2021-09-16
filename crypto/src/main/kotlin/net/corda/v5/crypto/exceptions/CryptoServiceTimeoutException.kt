b9f2cbd6f6e9e3d9a115475191d583ae5c5b7fa1
Alexey Kadyrov <67952405+alexey-kadyrov-r3@users.noreply.github.com>
2021-09-16 18:56:28 +0100

    Added constructor with cause to CryptoServiceTimeoutException (#77)

--- crypto/src/main/kotlin/net/corda/v5/crypto/exceptions/CryptoServiceTimeoutException.kt
+++ crypto/src/main/kotlin/net/corda/v5/crypto/exceptions/CryptoServiceTimeoutException.kt
@@ -4,5 +4,8 @@ import net.corda.v5.base.annotations.CordaSerializable
 import java.time.Duration
 
 @CordaSerializable
-class CryptoServiceTimeoutException(timeout: Duration) :
-    CryptoServiceException("Timed-out while waiting for ${timeout.toMillis()} milliseconds")
+class CryptoServiceTimeoutException(timeout: Duration, cause: Throwable?) :
+    CryptoServiceException("Timed-out while waiting for ${timeout.toMillis()} milliseconds", cause) {
+
+    constructor(timeout: Duration) : this(timeout, null)
+}
