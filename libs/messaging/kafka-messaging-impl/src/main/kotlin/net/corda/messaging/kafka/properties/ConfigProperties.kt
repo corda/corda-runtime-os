a75226cf425a44c80cc8a2f3eb015d59f5fa668e
Razvan Codreanu <52859362+Schife@users.noreply.github.com>
2021-09-17 17:09:40 +0100

    CORE-2521 RPC subscription and sender implementation (#221)
    * CORE-2521 initial pass at rpc subscription and associated factories
    
    * CORE-2521 integrate with new avro envelope objects
    
    * CORE-2521 response processing for the sender side
    
    * CORE-2521 new response status
    
    * CORE-2521 subscription test and serialization and deserialization rework
    
    * CORE-2521 initial pass at the rpc integration test
    
    * CORE-2521 version upgrade for jboss
    
    * CORE-2521 hibernate exclusion

--- libs/messaging/kafka-messaging-impl/src/main/kotlin/net/corda/messaging/kafka/properties/ConfigProperties.kt
+++ libs/messaging/kafka-messaging-impl/src/main/kotlin/net/corda/messaging/kafka/properties/ConfigProperties.kt
@@ -12,6 +12,7 @@ class ConfigProperties {
 
         const val TOPIC_PREFIX = "topic.prefix"
         const val TOPIC_NAME = "topic.name"
+        const val RESPONSE_TOPIC = "responseTopic"
 
         const val KAFKA_PRODUCER = "producer"
         const val KAFKA_CONSUMER = "consumer"
@@ -27,6 +28,8 @@ class ConfigProperties {
         const val PATTERN_STATEANDEVENT = "messaging.pattern.stateAndEvent"
         const val PATTERN_EVENTLOG = "messaging.pattern.eventLog"
         const val PATTERN_RANDOMACCESS = "messaging.pattern.randomAccess"
+        const val PATTERN_RPC_SENDER = "messaging.pattern.rpcSender"
+        const val PATTERN_RPC_RESPONDER = "messaging.pattern.rpcResponder"
 
         const val CLOSE_TIMEOUT = "close.timeout"
         const val PRODUCER_CLOSE_TIMEOUT = "producer.$CLOSE_TIMEOUT"
