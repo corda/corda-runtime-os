c7a92bb8d4cbc5e05da65d259e44cd0febf454ed
Dimos Raptis <dimos.raptis@r3.com>
2021-09-20 16:34:55 +0100

    CORE-2471: Add schemas/topics for stub network map for link manager

--- data/topic-schema/src/main/kotlin/net/corda/p2p/schema/TestSchema.kt
+++ data/topic-schema/src/main/kotlin/net/corda/p2p/schema/TestSchema.kt
@@ -3,5 +3,6 @@ package net.corda.p2p.schema
 class TestSchema {
     companion object {
         const val CRYPTO_KEYS_TOPIC = "p2p.crypto.keys"
+        const val NETWORK_MAP_TOPIC = "p2p.netmap"
     }
 }
