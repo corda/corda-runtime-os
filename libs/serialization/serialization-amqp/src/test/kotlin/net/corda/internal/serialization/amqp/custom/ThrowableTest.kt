82c65e65dc63d8bd2c192d1fb56612c0e8c5e179
Andrei Palade <2119610+palade@users.noreply.github.com>
2021-09-20 16:39:52 +0100

    CORE-2477: Serialization roundtrip tests for specific types (#254)
    Co-authored-by: Andrei Palade <palade@users.noreply.github.com>

--- libs/serialization/serialization-amqp/src/test/kotlin/net/corda/internal/serialization/amqp/custom/ThrowableTest.kt
+++ libs/serialization/serialization-amqp/src/test/kotlin/net/corda/internal/serialization/amqp/custom/ThrowableTest.kt
@@ -1,6 +1,6 @@
 package net.corda.internal.serialization.amqp.custom
 
-import net.corda.internal.serialization.amqp.custom.ReusableSerialiseDeserializeAssert.Companion.serializeDeserialize
+import net.corda.internal.serialization.amqp.ReusableSerialiseDeserializeAssert.Companion.serializeDeserialize
 import net.corda.v5.base.exceptions.CordaRuntimeException
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
