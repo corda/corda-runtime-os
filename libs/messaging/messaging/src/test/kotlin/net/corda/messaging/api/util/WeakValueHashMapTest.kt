2d11c0da8d1efb20101a5a247914d77f5842a68b
Schife <codreanu.r@gmail.com>
2021-09-27 10:24:50 +0100

    CORE-2749 unit test for the weak value hash map

---
+++ libs/messaging/messaging/src/test/kotlin/net/corda/messaging/api/util/WeakValueHashMapTest.kt
@@ -0,0 +1,35 @@
+package net.corda.messaging.api.util
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import java.util.concurrent.CompletableFuture
+
+class WeakValueHashMapTest {
+
+    @Test
+    fun `test WeakValueHashMap functionality and garbage collection`() {
+        val map = WeakValueHashMap<String, CompletableFuture<String>>()
+        var future = CompletableFuture<String>()
+        val futureToRemove = CompletableFuture<String>()
+
+        //test base functionality
+        map["future"] = future
+        map["remove"] = futureToRemove
+        Assertions.assertEquals(future, map["future"])
+        Assertions.assertTrue(map.containsKey("future"))
+        map.remove("remove")
+        Assertions.assertNull(map["remove"])
+        Assertions.assertEquals(1, map.size)
+        Assertions.assertFalse(map.isEmpty())
+
+        //test garbage collection
+        future.complete("completed")
+        //new assignment so we orphan the old future
+        future = CompletableFuture<String>()
+        future.complete("completed yet again")
+        System.gc()
+        Thread.sleep(1000)
+        Assertions.assertTrue(map.isEmpty())
+
+    }
+}
