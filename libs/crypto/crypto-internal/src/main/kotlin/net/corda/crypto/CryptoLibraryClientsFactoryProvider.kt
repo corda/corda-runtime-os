2422662cc5035da43f7732528da6fcabbd1a536f
Alexey Kadyrov <alexey.kadyrov@r3.com>
2021-10-06 13:39:38 +0100

    updated api version and added KDocs

--- libs/crypto/crypto-internal/src/main/kotlin/net/corda/crypto/CryptoLibraryClientsFactoryProvider.kt
+++ libs/crypto/crypto-internal/src/main/kotlin/net/corda/crypto/CryptoLibraryClientsFactoryProvider.kt
@@ -1,8 +1,12 @@
 package net.corda.crypto
 
+/**
+ * Provides operations to create [CryptoLibraryClientsFactory] for environments such as p2p components where
+ * the caller already has the member id.
+ */
 interface CryptoLibraryClientsFactoryProvider : AutoCloseable {
-    fun create(
-        memberId: String,
-        requestingComponent: String
-    ): CryptoLibraryClientsFactory
+    /**
+     * Creates an instance of [CryptoLibraryClientsFactory]
+     */
+    fun create(memberId: String, requestingComponent: String): CryptoLibraryClientsFactory
 }
--- libs/crypto/crypto-internal/src/main/kotlin/net/corda/crypto/CryptoLibrarySandboxClientsFactoryProvider.kt
+++ libs/crypto/crypto-internal/src/main/kotlin/net/corda/crypto/CryptoLibrarySandboxClientsFactoryProvider.kt
@@ -1,5 +1,12 @@
 package net.corda.crypto
 
+/**
+ * Provides operations to create [CryptoLibraryClientsFactory] for sandboxed (state machine, flows) environments where
+ * the current member id have to inferred from the ambient context.
+ */
 interface CryptoLibrarySandboxClientsFactoryProvider : AutoCloseable {
+    /**
+     * Creates an instance of [CryptoLibraryClientsFactory]
+     */
     fun create(requestingComponent: String): CryptoLibraryClientsFactory
 }
