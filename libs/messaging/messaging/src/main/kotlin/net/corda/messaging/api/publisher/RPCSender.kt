855605162aaff132cac2c9748eb257fbd156b0e3
Razvan Codreanu <52859362+Schife@users.noreply.github.com>
2021-10-06 11:51:35 +0100

    CORE-2523 Improved error handling and integration tests around non golden path (#311)
    * CORE-2523 error handling and integration tests
    
    * CORE-2523 more integration tests and error handling

--- libs/messaging/messaging/src/main/kotlin/net/corda/messaging/api/publisher/RPCSender.kt
+++ libs/messaging/messaging/src/main/kotlin/net/corda/messaging/api/publisher/RPCSender.kt
@@ -7,6 +7,12 @@ import java.util.concurrent.CompletableFuture
 /**
  * Interface for posting requests of type [TREQ] and receiving responses of type [TRESP]
  * RPCSender instances can be created via the [PublisherFactory].
+ *
+ * The sender contains a subscription for reading back responses. Depending on the response status, the future will be
+ * completed normally, exceptionally or be cancelled.
+ * Clients should expect the future to throw a CordaRPCAPIResponderException if it was completed exceptionally or a
+ * CancellationException if it was cancelled when calling get()/getOrThrow() on future
+ *
  */
 interface RPCSender<TREQ, TRESP>: Lifecycle {
 
@@ -17,6 +23,9 @@ interface RPCSender<TREQ, TRESP>: Lifecycle {
      *
      * The future represents the promise of a response and is completed when said response is received
      * It may error if the response fails or if the request times out
+     *
+     * @throws CordaRPCAPISenderException if there are no partitions available when attempting to send or if any errors
+     * occur during the publishing process
      *
      * The client is responsible for retries in the event of a failure or timeout
      */
