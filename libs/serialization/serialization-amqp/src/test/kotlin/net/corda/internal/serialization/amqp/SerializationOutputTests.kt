9dfce4b432376f50601ab1a7d82f32cfd91b9846
Joseph Zuniga-Daly <joseph.zunigadaly@r3.com>
2021-10-07 16:44:32 +0100

    Delete test zoned date time serialize

--- libs/serialization/serialization-amqp/src/test/kotlin/net/corda/internal/serialization/amqp/SerializationOutputTests.kt
+++ libs/serialization/serialization-amqp/src/test/kotlin/net/corda/internal/serialization/amqp/SerializationOutputTests.kt
@@ -661,18 +661,6 @@ class SerializationOutputTests {
     }
     
     @Test
-    fun `test zoned date time serialize`() {
-        val factory = SerializerFactoryBuilder.build(AllWhitelist)
-        factory.register(net.corda.internal.serialization.amqp.custom.ZonedDateTimeSerializer(factory))
-
-        val factory2 = SerializerFactoryBuilder.build(AllWhitelist)
-        factory2.register(net.corda.internal.serialization.amqp.custom.ZonedDateTimeSerializer(factory2))
-
-        val obj = ZonedDateTime.now()
-        serdes(obj, factory, factory2)
-    }
-
-    @Test
     fun `test offset time serialize`() {
         val factory = SerializerFactoryBuilder.build(AllWhitelist)
         factory.register(net.corda.internal.serialization.amqp.custom.OffsetTimeSerializer(factory))
