22e43a318d5d52fee43b973d67fef95b60c1b907
joeldudleyr3 <joel.dudley@r3.com>
2021-10-15 12:31:44 +0100

    Removes unneeded local permissions tests.

--- libs/security-manager/osgi-integration-tests/local-permissions/src/main/java/net/corda/securitymanager/localpermissions/package-info.java
+++
@@ -1,4 +0,0 @@
-@Export
-package net.corda.securitymanager.localpermissions;
-
-import org.osgi.annotation.bundle.Export;
--- libs/security-manager/osgi-integration-tests/src/integrationTest/kotlin/net/corda/securitymanager/osgi/BundlePermissionTests.kt
+++ libs/security-manager/osgi-integration-tests/src/integrationTest/kotlin/net/corda/securitymanager/osgi/BundlePermissionTests.kt
@@ -1,6 +1,5 @@
 package net.corda.securitymanager.osgi
 
-import net.corda.securitymanager.localpermissions.LocalPermissions
 import net.corda.securitymanager.osgiinvoker.OsgiInvoker
 import org.junit.jupiter.api.Assertions.assertDoesNotThrow
 import org.junit.jupiter.api.Assertions.assertThrows
@@ -30,20 +29,12 @@ class BundlePermissionTests {
 
         lateinit var sandboxedOsgiInvoker: OsgiInvoker
 
-        @InjectService(timeout = 1000)
-        lateinit var unsandboxedLocalPermissions: LocalPermissions
-
-        lateinit var sandboxedLocalPermisions: LocalPermissions
-
         @Suppress("unused")
         @JvmStatic
         @BeforeAll
         fun setup() {
             val sandboxedOsgiInvokerBundle = createSandboxedBundle(unsandboxedOsgiInvoker::class.java)
             sandboxedOsgiInvoker = retrieveServiceFromBundle(sandboxedOsgiInvokerBundle, OsgiInvoker::class.java)
-
-            val sandboxedLocalPermissionsBundle = createSandboxedBundle(unsandboxedLocalPermissions::class.java)
-            sandboxedLocalPermisions = retrieveServiceFromBundle(sandboxedLocalPermissionsBundle, LocalPermissions::class.java)
         }
 
         /** Returns a sandboxed [Bundle] installed from the same source as the bundle containing [classFromBundle].  */
@@ -200,80 +191,6 @@ class BundlePermissionTests {
     fun `sandboxed bundle has service permissions`() {
         assertDoesNotThrow {
             sandboxedOsgiInvoker.getService()
-        }
-    }
-
-    @Test
-    fun `sandboxed local permissions bundle has context permissions`() {
-        // The local permissions file grants this bundle "context" permissions.
-        assertDoesNotThrow {
-            sandboxedLocalPermisions.getBundleContext()
-        }
-    }
-
-    @Test
-    fun `sandboxed local permissions bundle bundle does not have execute permissions`() {
-        assertThrows(AccessControlException::class.java) {
-            sandboxedLocalPermisions.startBundle()
-        }
-    }
-
-    @Test
-    fun `sandboxed local permissions bundle does not have lifecycle permissions`() {
-        // Although the local permissions file grants this bundle "lifecycle" permissions, these are still denied by
-        // `CordaSecurityManager`.
-        assertThrows(AccessControlException::class.java) {
-            sandboxedLocalPermisions.installBundle()
-        }
-    }
-
-    @Test
-    fun `sandboxed local permissions bundle does not have listener permissions`() {
-        assertThrows(AccessControlException::class.java) {
-            sandboxedLocalPermisions.addListener()
-        }
-    }
-
-    @Test
-    fun `sandboxed local permissions bundle does not have class permissions`() {
-        assertThrows(ClassNotFoundException::class.java) {
-            sandboxedLocalPermisions.loadClass()
-        }
-    }
-
-    @Test
-    fun `sandboxed local permissions bundle does not have metadata permissions`() {
-        assertThrows(AccessControlException::class.java) {
-            sandboxedLocalPermisions.getLocation()
-        }
-    }
-
-    @Test
-    fun `sandboxed local permissions bundle does not have resolve permissions`() {
-        assertThrows(AccessControlException::class.java) {
-            sandboxedLocalPermisions.refreshBundles()
-        }
-    }
-
-    @Test
-    fun `sandboxed local permissions bundle does not have adapt permissions`() {
-        assertThrows(AccessControlException::class.java) {
-            sandboxedLocalPermisions.adaptBundle()
-        }
-    }
-
-    @Test
-    fun `sandboxed local permissions bundle does not have service permissions`() {
-        assertThrows(NullPointerException::class.java) {
-            sandboxedLocalPermisions.getService()
-        }
-    }
-
-    @Test
-    fun `sandboxed local permissions bundle does not have file permissions`() {
-        // Any permission not explicitly granted in the local permissions file is denied.
-        assertThrows(AccessControlException::class.java) {
-            sandboxedLocalPermisions.readFile()
         }
     }
 }
