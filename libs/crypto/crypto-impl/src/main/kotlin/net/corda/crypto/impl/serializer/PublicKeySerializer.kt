c61c45bb13c66b243a172d0bf27f9c83ab7ea02e
Joseph Zuniga-Daly <joseph.zunigadaly@r3.com>
2021-10-20 15:53:11 +0100

    Extract CipherSchemeMetadata into a lazy private property

--- libs/crypto/crypto-impl/src/main/kotlin/net/corda/crypto/impl/serializer/PublicKeySerializer.kt
+++ libs/crypto/crypto-impl/src/main/kotlin/net/corda/crypto/impl/serializer/PublicKeySerializer.kt
@@ -1,15 +1,17 @@
 package net.corda.crypto.impl.serializer
 
 import net.corda.crypto.impl.CipherSchemeMetadataProviderImpl
+import net.corda.v5.cipher.suite.CipherSchemeMetadata
 import net.corda.v5.serialization.SerializationCustomSerializer
 import java.security.PublicKey
 
 /**
  * A serializer that writes out a public key in X.509 format.
  */
-class PublicKeySerializer
-    : SerializationCustomSerializer<PublicKey, ByteArray> {
+class PublicKeySerializer : SerializationCustomSerializer<PublicKey, ByteArray> {
+
+    private val cipherSchemeMetadata: CipherSchemeMetadata by lazy { CipherSchemeMetadataProviderImpl().getInstance() }
+
     override fun toProxy(obj: PublicKey): ByteArray = obj.encoded
-    override fun fromProxy(proxy: ByteArray): PublicKey =
-        CipherSchemeMetadataProviderImpl().getInstance().decodePublicKey(proxy)
+    override fun fromProxy(proxy: ByteArray): PublicKey = cipherSchemeMetadata.decodePublicKey(proxy)
 }
