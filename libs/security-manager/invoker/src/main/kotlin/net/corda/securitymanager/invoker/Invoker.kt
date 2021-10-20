eaa0685a04c202b84a7efea85d3c17dbf087e163
joeldudleyr3 <joel.dudley@r3.com>
2021-10-20 17:50:03 +0100

    Checkpoint.

--- libs/security-manager/invoker/src/main/kotlin/net/corda/securitymanager/invoker/Invoker.kt
+++
@@ -1,7 +0,0 @@
-package net.corda.securitymanager.invoker
-
-/** Used to execute actions within the context of the bundle containing the implementation of this interface. */
-interface Invoker {
-    /** Executes the provided lambda. */
-    fun performAction(lambda: () -> Unit)
-}
