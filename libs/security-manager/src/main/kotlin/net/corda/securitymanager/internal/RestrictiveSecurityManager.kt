8b8679fc51b0691a5594525f9191dcdeedd975c4
joeldudleyr3 <joel.dudley@r3.com>
2021-10-20 17:55:02 +0100

    Simplifies RestrictiveSecurityManager startup.

--- libs/security-manager/src/main/kotlin/net/corda/securitymanager/internal/RestrictiveSecurityManager.kt
+++ libs/security-manager/src/main/kotlin/net/corda/securitymanager/internal/RestrictiveSecurityManager.kt
@@ -1,8 +1,6 @@
 package net.corda.securitymanager.internal
 
 import net.corda.securitymanager.SecurityManagerException
-import org.osgi.framework.FrameworkUtil
-import org.osgi.service.cm.ConfigurationAdmin
 import org.osgi.service.component.annotations.Activate
 import org.osgi.service.component.annotations.Component
 import org.osgi.service.component.annotations.Reference
@@ -17,8 +15,6 @@ import org.osgi.service.permissionadmin.PermissionInfo
 import java.security.AllPermission
 import java.security.Permission
 
-// TODO - Does this name even make sense anymore?
-
 /** A [CordaSecurityManager] that grants sandbox code a very limited set of permissions. */
 @Component(service = [RestrictiveSecurityManager::class])
 class RestrictiveSecurityManager @Activate constructor(
@@ -31,21 +27,13 @@ class RestrictiveSecurityManager @Activate constructor(
         private val allPermInfo = PermissionInfo(AllPermission::class.java.name, ALL, ALL)
     }
 
-    /**
-     * Performs two sets of permission updates:
-     *
-     *  * Grants all permissions to the `ConfigurationAdmin` service. For reasons unknown, the permissive Java
-     *   security policy that is applied on framework start-up is not extended to this service
-     *
-     * // TODO - Update.
-     *  * Denies all permissions to sandbox bundles, except some minimal permissions required to set up OSGi bundles
-     *
-     *  These permissions work in tandem with the OSGi hooks defined in the `sandbox` module to prevent sandbox bundles
-     *  from performing illegal actions.
-     */
+    /** Grants all permissions to all bundles. */
     override fun start() {
-        grantConfigAdminPermissions(permissionAdmin)
-        restrictSandboxBundlePermissions(conditionalPermissionAdmin)
+        val grantAllPermissions = conditionalPermissionAdmin.newConditionalPermissionInfo(
+            null, null, arrayOf(allPermInfo), ALLOW
+        )
+
+        updateConditionalPerms(setOf(grantAllPermissions))
     }
 
     /** Clears any permissions from the [ConditionalPermissionAdmin]. */
@@ -75,28 +63,6 @@ class RestrictiveSecurityManager @Activate constructor(
 
         updateConditionalPerms(setOf(condPermInfo), clear = false)
     }
-
-    /** Grants all permissions to the [ConfigurationAdmin] service. */
-    private fun grantConfigAdminPermissions(permissionAdmin: PermissionAdmin) {
-        permissionAdmin.setPermissions(
-            FrameworkUtil.getBundle(ConfigurationAdmin::class.java).location,
-            arrayOf(allPermInfo)
-        )
-    }
-
-    // TODO: Update description and method name.
-    /**
-     * Denies all permissions to bundles matching the [SANDBOX_SECURITY_DOMAIN_FILTER], except some minimal permissions
-     * required to set up OSGi bundles (i.e. [packagePermission], [capabilityPermission] and [servicePermission]).
-     */
-    private fun restrictSandboxBundlePermissions(conditionalPermissionAdmin: ConditionalPermissionAdmin) {
-        val grantAllPermissions = conditionalPermissionAdmin.newConditionalPermissionInfo(
-            null, null, arrayOf(allPermInfo), ALLOW
-        )
-
-        updateConditionalPerms(setOf(grantAllPermissions))
-    }
-
     /**
      * Adds the [perms] to the start of [ConditionalPermissionAdmin]'s permissions list.
      *
