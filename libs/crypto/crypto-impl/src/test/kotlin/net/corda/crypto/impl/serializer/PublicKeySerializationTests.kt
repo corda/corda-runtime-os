4e3aca5dbc716c5c6193afbaf2dbe7399f11c7d4
Alexey Kadyrov <67952405+alexey-kadyrov-r3@users.noreply.github.com>
2021-10-22 12:07:13 +0100

    NOTICK - changed key serialiser to get encoder on each call (negligible performance implication from ~6M/sec down to ~3M/sec) (#390)

--- libs/crypto/crypto-impl/src/test/kotlin/net/corda/crypto/impl/serializer/PublicKeySerializationTests.kt
+++ libs/crypto/crypto-impl/src/test/kotlin/net/corda/crypto/impl/serializer/PublicKeySerializationTests.kt
@@ -3,22 +3,22 @@ package net.corda.crypto.impl.serializer
 import net.corda.crypto.CryptoLibraryFactory
 import net.corda.v5.cipher.suite.KeyEncodingService
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Timeout
 import org.mockito.kotlin.mock
-import java.security.KeyPairGenerator
 import java.security.PublicKey
 import kotlin.test.assertEquals
 
 class PublicKeySerializationTests {
-
     @Test
-    fun `test custom serializers on public key`() {
-        val algorithm = "RSA"
-
-        // Generate new key for testing
-        val publicKey: PublicKey = KeyPairGenerator.getInstance(algorithm).genKeyPair().public
-
+    @Timeout(5)
+    fun `Should serialize and then deserialize public key`() {
+        val encodedPublicKey = ByteArray(1)
+        val publicKey = mock<PublicKey> {
+            on { it.encoded }.thenReturn(encodedPublicKey)
+        }
         val keyEncodingService = mock<KeyEncodingService> {
-            on { it.decodePublicKey(publicKey.encoded) }.thenReturn(publicKey)
+            on { it.decodePublicKey(encodedPublicKey) }.thenReturn(publicKey)
+            on { it.encodeAsByteArray(publicKey) }.thenReturn(encodedPublicKey)
         }
         val cryptoLibraryFactory = mock<CryptoLibraryFactory> {
             on { it.getKeyEncodingService() }.thenReturn(keyEncodingService)
