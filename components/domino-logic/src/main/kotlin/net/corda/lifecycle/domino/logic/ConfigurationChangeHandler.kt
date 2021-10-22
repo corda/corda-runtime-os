c4238e3a014c843e4656ef90473f74df2c388be8
Will Vigor <william.vigor@r3.com>
2021-10-22 16:04:49 +0100

    Change ConfigurationChangeHandler

--- components/domino-logic/src/main/kotlin/net/corda/lifecycle/domino/logic/ConfigurationChangeHandler.kt
+++ components/domino-logic/src/main/kotlin/net/corda/lifecycle/domino/logic/ConfigurationChangeHandler.kt
@@ -7,53 +7,11 @@ import net.corda.lifecycle.LifecycleCoordinatorFactory
 import net.corda.lifecycle.domino.logic.util.ResourcesHolder
 import net.corda.v5.base.util.contextLogger
 
-abstract class ConfigurationAwareTile<C>(
-    private val configurationReaderService: ConfigurationReadService,
-    private val key: String,
-    private val configFactory: (Config) -> C
+abstract class ConfigurationChangeHandler<C>(
+    val configurationReaderService: ConfigurationReadService,
+    val key: String,
+    val configFactory: (Config) -> C
 ) {
-    companion object {
-        private val logger = contextLogger()
-    }
-
-    @Volatile
-    private var lastConfiguration: C? = null
-
-    @Volatile
-    private var configSet = false
-
-    private inner class Handler : ConfigurationHandler {
-        override fun onNewConfiguration(changedKeys: Set<String>, config: Map<String, Config>) {
-            if (changedKeys.contains(key)) {
-                val newConfiguration = config[key]
-                if (newConfiguration != null) {
-                    callFromCoordinator {
-                        applyNewConfiguration(newConfiguration)
-                        configSet = true
-                        return@callFromCoordinator configSet
-                    }
-                }
-            }
-        }
-    }
-
-    private fun applyNewConfiguration(newConfiguration: Config) {
-        @Suppress("TooGenericExceptionCaught")
-        try {
-            val configuration = configFactory(newConfiguration)
-            logger.info("Got configuration $name")
-            if (configuration == lastConfiguration) {
-                logger.info("Configuration had not changed $name")
-                return
-            } else {
-                applyNewConfiguration(configuration, lastConfiguration, resources)
-                lastConfiguration = configuration
-                logger.info("Reconfigured $name")
-            }
-        } catch (e: Throwable) {
-            gotError(e)
-        }
-    }
 
     abstract fun applyNewConfiguration(newConfiguration: C, oldConfiguration: C?, resources: ResourcesHolder)
 
