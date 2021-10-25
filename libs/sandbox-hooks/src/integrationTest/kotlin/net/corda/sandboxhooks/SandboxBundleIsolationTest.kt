b2acb4a96fca284e46967a1616fc95ce9aa5b052
joeldudleyr3 <joel.dudley@r3.com>
2021-10-25 18:16:51 +0100

    Simplifies sandbox bundle isolation tests.

--- libs/sandbox-hooks/src/integrationTest/kotlin/net/corda/sandboxhooks/SandboxBundleIsolationTest.kt
+++ libs/sandbox-hooks/src/integrationTest/kotlin/net/corda/sandboxhooks/SandboxBundleIsolationTest.kt
@@ -1,8 +1,7 @@
 package net.corda.sandboxhooks
 
 import net.corda.sandbox.Sandbox
-import org.assertj.core.api.AbstractListAssert
-import org.assertj.core.api.ObjectAssert
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.osgi.framework.Bundle
@@ -18,24 +17,17 @@ class SandboxBundleIsolationTest {
         @InjectService(timeout = 1000)
         lateinit var sandboxLoader: SandboxLoader
 
-        fun assertThat(bundles: List<Bundle>) = BundleListAssertions(bundles)
-
-        class BundleListAssertions(bundles: List<Bundle>)
-            : AbstractListAssert<BundleListAssertions, List<Bundle>, Bundle, ObjectAssert<Bundle>>(bundles, BundleListAssertions::class.java) {
-            override fun toAssert(value: Bundle?, description: String?) = ObjectAssert(value!!) // Never called.
-
-            override fun newAbstractIterableAssert(iterable: Iterable<Bundle>): BundleListAssertions {
-                return BundleListAssertions(iterable as List<Bundle>)
-            }
-
-            fun existsOnlyInsideSandbox(symbolicName: String, sandbox: Sandbox): BundleListAssertions {
-                return anyMatch { bundle ->
-                    // A bundle with this symbolic name exists in target sandbox.
-                    bundle.symbolicName == symbolicName && sandboxLoader.containsBundle(bundle, sandbox)
-                }.noneMatch { bundle ->
-                    // No bundle with this symbolic name exists in a different sandbox.
-                    bundle.symbolicName == symbolicName && !sandboxLoader.containsBundle(bundle, sandbox)
-                }
+        /**
+         * At least one of the provided bundles has the given [symbolicName] and is contained in the [sandbox].
+         *
+         * There is no bundle out of the provided bundles that has the given [symbolicName] but that is not
+         * contained in the [sandbox].
+         */
+        fun bundlesWithGivenNameAreFromSandbox(bundles: List<Bundle>, symbolicName: String, sandbox: Sandbox): Boolean {
+            return bundles.any { bundle ->
+                bundle.symbolicName == symbolicName && sandboxLoader.containsBundle(bundle, sandbox)
+            } && bundles.none { bundle ->
+                bundle.symbolicName == symbolicName && !sandboxLoader.containsBundle(bundle, sandbox)
             }
         }
     }
@@ -43,22 +35,18 @@ class SandboxBundleIsolationTest {
     @Test
     fun sandboxCanSeeItsOwnBundlesAndMainBundlesInTheSameSandboxGroupOnly() {
         val thisGroup = sandboxLoader.group1
+        val otherGroup = sandboxLoader.group2
         val sandbox1 = thisGroup.getSandbox(sandboxLoader.cpk1.metadata.id)
         val sandbox2 = thisGroup.getSandbox(sandboxLoader.cpk2.metadata.id)
 
         // This flow returns all bundle events visible to this bundle.
         val bundles = sandboxLoader.runFlow<List<Bundle>>(BUNDLES1_FLOW_CLASS, thisGroup)
 
-        // CPK1 should be able to see its own bundles, and CPK2's "main" jar bundle, but nothing from CPK3.
-        assertThat(bundles)
-            .anyMatch { sandboxLoader.containsBundle(it, thisGroup) }
-            .noneMatch { sandboxLoader.containsBundle(it, sandboxLoader.group2) }
+        assertTrue(bundles.any { bundle -> sandboxLoader.containsBundle(bundle, thisGroup) })
+        assertTrue(bundles.none { bundle -> sandboxLoader.containsBundle(bundle, otherGroup) })
 
-            // CPK1 can see both its own and CPK2's "main" bundles.
-            .existsOnlyInsideSandbox(sandboxLoader.cpk1.metadata.id.name, sandbox1)
-            .existsOnlyInsideSandbox(sandboxLoader.cpk2.metadata.id.name, sandbox2)
-
-            // Only CPK1 can see its own library bundle.
-            .existsOnlyInsideSandbox(LIBRARY_BUNDLE_SYMBOLIC_NAME, sandbox1)
+        assertTrue(bundlesWithGivenNameAreFromSandbox(bundles, sandboxLoader.cpk1.metadata.id.name, sandbox1))
+        assertTrue(bundlesWithGivenNameAreFromSandbox(bundles, sandboxLoader.cpk2.metadata.id.name, sandbox2))
+        assertTrue(bundlesWithGivenNameAreFromSandbox(bundles, LIBRARY_BUNDLE_SYMBOLIC_NAME, sandbox1))
     }
 }
