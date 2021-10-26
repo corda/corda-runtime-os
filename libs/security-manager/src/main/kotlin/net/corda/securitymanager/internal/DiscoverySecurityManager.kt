beaa377b7db9a0888d76fdc0aa98f824f51aac12
Joel Dudley <joel.dudley@r3.com>
2021-10-26 13:21:24 +0100

    CORE-2829: Allows permissions to be requested or denied via SecurityManagerService. (#375)
    * Adds  method to SecurityManagerService.
    
    * Allows additional permissions to be granted to bundles.
    
    * Provides separate methods for starting the restrictive and discovery security managers.
    
    * Simplifies RestrictiveSecurityManager startup.

--- libs/security-manager/src/main/kotlin/net/corda/securitymanager/internal/DiscoverySecurityManager.kt
+++ libs/security-manager/src/main/kotlin/net/corda/securitymanager/internal/DiscoverySecurityManager.kt
@@ -1,22 +1,25 @@
 package net.corda.securitymanager.internal
 
 import org.osgi.service.component.annotations.Component
+import java.security.Permission
 
 // TODO - CORE-2828: Update [DiscoverySecurityManager] to write out updated permissions file.
 
 /**
  * A [CordaSecurityManager] that grants sandbox code all permissions.
  *
- * While active, for any permission check that would fail under the [RestrictiveSecurityManager], it writes out the
- * corresponding permission to an updated permissions file.
+ * While active, for any permission check performed by user code, it writes out the corresponding permission to an
+ * updated permissions file.
  *
- * This security manager is not secure in production.
+ * This security manager is not secure for production use.
  */
 @Component(service = [DiscoverySecurityManager::class])
-class DiscoverySecurityManager: CordaSecurityManager, SecurityManager() {
+class DiscoverySecurityManager : CordaSecurityManager, SecurityManager() {
     override fun start() {
         System.setSecurityManager(this)
     }
 
     override fun stop() = Unit
+    override fun grantPermissions(filter: String, perms: Collection<Permission>) = Unit
+    override fun denyPermissions(filter: String, perms: Collection<Permission>) = Unit
 }
