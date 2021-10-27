fa10dcdb514af73b57a48e95bc21d4677f406cb2
Kyriakos Tharrouniatis <kyriakos.tharrouniatis@r3.com>
2021-10-27 11:58:00 +0100

    Replace `ClassTagService` with `SandboxContextService` in `TypeResolver`

--- libs/serialization/serialization-amqp/src/main/kotlin/net/corda/internal/serialization/osgi/TypeResolver.kt
+++ libs/serialization/serialization-amqp/src/main/kotlin/net/corda/internal/serialization/osgi/TypeResolver.kt
@@ -1,7 +1,7 @@
 package net.corda.internal.serialization.osgi
 
-import net.corda.classinfo.ClassTagService
 import net.corda.internal.serialization.amqp.asClass
+import net.corda.sandbox.SandboxContextService
 import org.osgi.service.component.annotations.Activate
 import org.osgi.service.component.annotations.Component
 import org.osgi.service.component.annotations.Reference
@@ -10,13 +10,13 @@ import java.lang.reflect.Type
 @Component(immediate = true, service = [TypeResolver::class])
 class TypeResolver @Activate constructor(
         @Reference
-        classTagService: ClassTagService
+        sandboxContextService: SandboxContextService
 ) {
 
     companion object {
-        private var classTagService: ClassTagService? = null
+        private var sandboxContextService: SandboxContextService? = null
 
-        fun getClassTagFor(type: Type): String = classTagService!!.getClassTag(type.asClass(), isStaticTag = false)
+        fun getClassTagFor(type: Type): String = sandboxContextService!!.getClassTag(type.asClass(), isStaticTag = false)
 
         fun resolve(className: String, classLoader: ClassLoader): Class<*> {
             return try {
@@ -32,6 +32,6 @@ class TypeResolver @Activate constructor(
     }
 
     init {
-        TypeResolver.classTagService = classTagService
+        TypeResolver.sandboxContextService = sandboxContextService
     }
 }
