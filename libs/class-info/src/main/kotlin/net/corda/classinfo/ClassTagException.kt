59d24da9aa7012e19c16f3615dda69e3c379b2b6
Kyriakos Tharrouniatis <kyriakos.tharrouniatis@r3.com>
2021-10-27 12:16:10 +0100

    Delete `class-info` module

--- libs/class-info/src/main/kotlin/net/corda/classinfo/ClassTagException.kt
+++
@@ -1,4 +0,0 @@
-package net.corda.classinfo
-
-/** Thrown if an exception occurs related to retrieval of class information. */
-class ClassTagException(message: String?, cause: Throwable? = null) : RuntimeException(message, cause)
