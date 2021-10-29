1f7f15a6d2f7840ee7e1512443429557b97c322e
Alexey Kadyrov <67952405+alexey-kadyrov-r3@users.noreply.github.com>
2021-10-29 12:07:47 +0100

    Removing obsolete crypto configuration functionality (#118)

--- cipher-suite/src/main/kotlin/net/corda/v5/cipher/suite/config/CryptoLibraryConfig.kt
+++ cipher-suite/src/main/kotlin/net/corda/v5/cipher/suite/config/CryptoLibraryConfig.kt
@@ -1,28 +1,12 @@
 package net.corda.v5.cipher.suite.config
 
-import net.corda.v5.crypto.exceptions.CryptoConfigurationException
-
 /**
- * Defines the minimum crypto library configuration, the keys are mix of member ids and other configuration
- * such as RPC, cache, etc., the latter values are internal to the crypto library implementation.
+ * Defines the crypto library configuration, such as RPC, cache, etc.,
+ * most values are internal to the crypto library implementation.
  */
-interface CryptoLibraryConfig : Map<String, Any?> {
-    companion object {
-        /**
-         * The key which is used for the members which don't have their own specific configuration.
-         */
-        const val DEFAULT_MEMBER_KEY = "default"
-    }
-
-    /**
-     * Returns the member config, if the member id is not found then
-     * it tries to return the value using 'default' key and if that is not found then
-     * it throws [CryptoConfigurationException] exception
-     */
-    fun getMember(memberId: String): CryptoMemberConfig
-}
+interface CryptoLibraryConfig : Map<String, Any?>
 
 /**
- * Similar ot typesafe's hasPath where it returns true if the key is present, and it's not null
+ * Similar to typesafe's hasPath where it returns true if the key is present, and it's not null
  */
 fun Map<String, Any?>.hasPath(key: String) : Boolean = get(key) != null
