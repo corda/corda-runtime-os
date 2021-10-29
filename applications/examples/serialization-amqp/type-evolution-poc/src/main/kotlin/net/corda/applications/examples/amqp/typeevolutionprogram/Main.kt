375b46a75b3770b72e4a92d0e7d171b22a126151
Joseph Zuniga-Daly <joseph.zunigadaly@r3.com>
2021-10-29 15:18:32 +0100

    Work in progress

--- applications/examples/serialization-amqp/type-evolution-poc/src/main/kotlin/net/corda/applications/examples/amqp/typeevolutionprogram/Main.kt
+++ applications/examples/serialization-amqp/type-evolution-poc/src/main/kotlin/net/corda/applications/examples/amqp/typeevolutionprogram/Main.kt
@@ -1,75 +1,79 @@
 package net.corda.applications.examples.amqp.typeevolutionprogram
 
-import net.corda.internal.serialization.AMQP_STORAGE_CONTEXT
 import net.corda.internal.serialization.AllWhitelist
-import net.corda.internal.serialization.AlwaysAcceptEncodingWhitelist
-import net.corda.internal.serialization.SerializationContextImpl
 import net.corda.internal.serialization.amqp.DeserializationInput
 import net.corda.internal.serialization.amqp.SerializationOutput
 import net.corda.internal.serialization.amqp.SerializerFactoryBuilder
-import net.corda.internal.serialization.amqp.amqpMagic
 import net.corda.osgi.api.Application
-import net.corda.v5.base.types.ByteSequence
-import net.corda.v5.base.util.contextLogger
-import net.corda.v5.serialization.SerializationContext
-import net.corda.v5.serialization.annotations.CordaSerializationTransformRename
-import net.corda.v5.serialization.annotations.CordaSerializationTransformRenames
-import net.corda.v5.serialization.annotations.DeprecatedConstructorForDeserialization
-import org.osgi.framework.BundleContext
+import net.corda.osgi.api.Shutdown
+import net.corda.sandbox.SandboxCreationService
+import org.osgi.framework.FrameworkUtil
 import org.osgi.service.component.annotations.Activate
+import org.osgi.service.cm.ConfigurationAdmin
 import org.osgi.service.component.annotations.Component
+import org.osgi.service.component.annotations.Reference
+import org.osgi.service.component.annotations.ReferenceCardinality
 import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import java.util.Hashtable
 
 val factory = SerializerFactoryBuilder.build(AllWhitelist)
 val output = SerializationOutput(factory)
 val input = DeserializationInput(factory)
 
-
-//
-//net.corda.osgi.api Application.kt public interface Application : AutoCloseable
-//The osgi-framework-bootstrap module calls startup of the class implementing this interface as entry point of the application and shutdown before to stop the OSGi framework.
-//NOTE: To distribute an application as a bootable JAR built with the corda.common.app plugin, only one class must implement this interface because that class is the entry point of the application.
-//The class implementing this interface must define an OSGi component and register as an OSGi service.
-//EXAMPLE
-//The code shows for to implement the Application interface and inject in the constructor a OSGi service/component annotated the @Reference.
-//See the README.md file of the buildSrc module for the common-appplugin for more infos.
-//import net.corda.comp.kafka.topic.admin.KafkaTopicAdmin
-//import net.corda.osgi.api.Application
-//import org.osgi.service.component.annotations.Activate
-//import org.osgi.service.component.annotations.Component
-//import org.osgi.service.component.annotations.Reference
-//
-//@Component(immediate = true)
-//class App @Activate constructor(
-//    @Reference(service = KafkaTopicAdmin::class)
-//    private var kafkaTopicAdmin: KafkaTopicAdmin,
-//): Application {
-//
-//    override fun startup(args: Array<String>) {
-//        println("startup with ${kafkaTopicAdmin}")
-//    }
-//
-//    override fun shutdown() {
-//        println("shutdown")
-//    }
-//}
-
-@Component(immediate = true)
-class Main : Application {
+@Component
+class Main @Activate constructor(
+    @Reference
+    private val shutDownService: Shutdown,
+    @Reference
+    private val sandboxCreationService: SandboxCreationService,
+    @Reference
+    private val configurationAdmin: ConfigurationAdmin
+) : Application {
     private companion object {
-        private val logger: Logger = contextLogger()
+        private val consoleLogger: Logger = LoggerFactory.getLogger("Console")
     }
 
-    init {
-        logger.info("INIT")
-    }
+    private val bundle = FrameworkUtil.getBundle(this::class.java)
+    private val bundleContext = bundle.bundleContext
+
+    private fun exit() = shutDownService.shutdown(bundle)
 
     override fun startup(args: Array<String>) {
 //    saveResourceFiles()
 
+        val conf = configurationAdmin.getConfiguration(ConfigurationAdmin::class.java.name, null)
+        conf?.update(Hashtable(mapOf("baseDirectory" to "/Users/josephzuniga-daly/Documents/git/corda-runtime-os/applications/examples/serialization-amqp/type-evolution-poc/tmp")))
+
+        printBundleList()
+        printServiceList()
+
+        sandboxCreationService.createPublicSandbox()
+
+        exit()
     }
 
-    override fun shutdown() = Unit
+    private fun printServiceList() {
+        val allServiceReferences = bundleContext.getAllServiceReferences(null, null)
+        consoleLogger.info("getAllServiceReferences:")
+        consoleLogger.info("------------------------")
+        allServiceReferences.forEach { consoleLogger.info(it.toString()) }
+    }
+
+    private fun printBundleList() {
+        val allBundles = bundleContext.bundles.toList()
+
+        consoleLogger.info("allBundles:")
+        consoleLogger.info("------------------------")
+        allBundles.forEach {
+            consoleLogger.info(it.toString())
+            it.registeredServices?.forEach { service -> consoleLogger.info("---> $service") }
+        }
+    }
+
+    override fun shutdown() {
+        consoleLogger.info("Shutting down")
+    }
 }
 
 
