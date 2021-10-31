d6086bd20290e88e5d826940ddd557a53f231fa7
Joseph Zuniga-Daly <joseph.zunigadaly@r3.com>
2021-10-31 21:52:53 +0000

    Sandbox group loading cpk

--- applications/examples/serialization-amqp/type-evolution-poc/src/main/kotlin/net/corda/applications/examples/amqp/typeevolutionprogram/Main.kt
+++ applications/examples/serialization-amqp/type-evolution-poc/src/main/kotlin/net/corda/applications/examples/amqp/typeevolutionprogram/Main.kt
@@ -1,11 +1,14 @@
 package net.corda.applications.examples.amqp.typeevolutionprogram
 
+import net.corda.install.InstallService
 import net.corda.internal.serialization.AllWhitelist
+import net.corda.internal.serialization.ByteBufferOutputStream
 import net.corda.internal.serialization.amqp.DeserializationInput
 import net.corda.internal.serialization.amqp.SerializationOutput
 import net.corda.internal.serialization.amqp.SerializerFactoryBuilder
 import net.corda.osgi.api.Application
 import net.corda.osgi.api.Shutdown
+import net.corda.packaging.CPI
 import net.corda.sandbox.SandboxCreationService
 import org.osgi.framework.FrameworkUtil
 import org.osgi.service.cm.ConfigurationAdmin
@@ -14,6 +17,9 @@ import org.osgi.service.component.annotations.Component
 import org.osgi.service.component.annotations.Reference
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
+import java.nio.file.Path
 import java.util.Hashtable
 
 val factory = SerializerFactoryBuilder.build(AllWhitelist)
@@ -27,7 +33,9 @@ class Main @Activate constructor(
     @Reference
     private val sandboxCreationService: SandboxCreationService,
     @Reference
-    private val configurationAdmin: ConfigurationAdmin
+    private val configurationAdmin: ConfigurationAdmin,
+    @Reference
+    private val installService: InstallService,
 ) : Application {
     private companion object {
         private val consoleLogger: Logger = LoggerFactory.getLogger("Console")
@@ -43,15 +51,26 @@ class Main @Activate constructor(
 
         val conf = configurationAdmin.getConfiguration(ConfigurationAdmin::class.java.name, null)
         val tmpDir = args[0]
-        val cpk = args[1]
+        val cpk = Path.of(args[1])
 
-        conf?.update(Hashtable(mapOf("baseDirectory" to tmpDir)))
+        conf?.update(Hashtable(mapOf("baseDirectory" to tmpDir,
+            "platformVersion" to 999,
+            "blacklistedKeys" to emptyList<Any>())))
 
-        printBundleList()
-        printServiceList()
+//        printBundleList()
+//        printServiceList()
 
 //        sandboxCreationService.createPublicSandbox()
 //        sandboxCreationService.createSandboxGroup()
+
+        val outputStream = ByteArrayOutputStream()
+        CPI.assemble(outputStream, "cpi", "1.0", listOf(cpk))
+        consoleLogger.info("loadCpb")
+        val loadCpb: CPI = installService.loadCpb(ByteArrayInputStream(outputStream.toByteArray()))
+        consoleLogger.info("createSandboxGroup")
+        val sandboxGroup = sandboxCreationService.createSandboxGroup(loadCpb.cpks.map { it.metadata.hash })
+
+        consoleLogger.info(sandboxGroup.toString())
 
         exit()
     }
