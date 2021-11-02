5814d84ab7ba0a7c00eccc349915ba509a588465
Emily Bowe <emily.bowe@r3.com>
2021-11-02 12:18:10 +0000

    changed jenkinsfile to create db, applied @disabled to SQL again

--- libs/messaging/db-messaging-impl/src/integration-test/kotlin/net/corda/messaging/db/persistence/DbAccessProviderSQLServerTest.kt
+++ libs/messaging/db-messaging-impl/src/integration-test/kotlin/net/corda/messaging/db/persistence/DbAccessProviderSQLServerTest.kt
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Disabled
 import org.testcontainers.containers.MSSQLServerContainer
 import java.sql.DriverManager
 
-//@Disabled("Disabled for CI until we have a shared database hosted by the infrastructure team. See INFRA-1485")
+@Disabled("Disabled for CI until we have a shared database hosted by the infrastructure team. See INFRA-1485")
 class DbAccessProviderSQLServerTest: DbAccessProviderTestBase() {
 
     private val sqlServer = MSSQLServerContainer<Nothing>("mcr.microsoft.com/mssql/server:2017-latest").apply { this.acceptLicense() }
