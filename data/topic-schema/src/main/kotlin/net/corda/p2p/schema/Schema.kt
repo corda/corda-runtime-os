3ad95756a66d481f74452b2d2e6bf9455dd04ac0
Viktor Kolomeyko <viktor.kolomeyko@r3.com>
2021-11-04 15:02:56 +0000

    CORE-2840: Introduce constants for RPC permissions topics (#125)
    * CORE-2840: Introduce constants for RPC permissions topics
    
    * CORE-2840: Update to correct response topic name

--- data/topic-schema/src/main/kotlin/net/corda/p2p/schema/Schema.kt
+++ data/topic-schema/src/main/kotlin/net/corda/p2p/schema/Schema.kt
@@ -8,5 +8,12 @@ class Schema {
         const val LINK_OUT_TOPIC = "link.out"
         const val LINK_IN_TOPIC = "link.in"
         const val SESSION_OUT_PARTITIONS = "session.out.partitions"
+
+        // RPC Permissions
+        const val RPC_PERM_MGMT_REQ_TOPIC = "rpc.permissions.management"
+        const val RPC_PERM_MGMT_RESP_TOPIC = "rpc.permissions.management.resp"
+        const val RPC_PERM_USER_TOPIC = "rpc.permissions.user"
+        const val RPC_PERM_GROUP_TOPIC = "rpc.permissions.group"
+        const val RPC_PERM_ROLE_TOPIC = "rpc.permissions.role"
     }
 }
