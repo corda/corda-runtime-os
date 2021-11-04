be583a99fcc85fb5e47a9c187d094c895e6d20f4
Will Vigor <william.vigor@r3.com>
2021-11-04 15:10:00 +0000

    Fix GatewayTest are merging Yiftack's changes

--- components/gateway/src/test/kotlin/net/corda/p2p/gateway/messaging/ReconfigurableConnectionManagerTest.kt
+++ components/gateway/src/test/kotlin/net/corda/p2p/gateway/messaging/ReconfigurableConnectionManagerTest.kt
@@ -1,61 +1,67 @@
 package net.corda.p2p.gateway.messaging
 
-import com.typesafe.config.Config
-import net.corda.configuration.read.ConfigurationHandler
 import net.corda.configuration.read.ConfigurationReadService
 import net.corda.lifecycle.LifecycleCoordinator
 import net.corda.lifecycle.LifecycleCoordinatorFactory
-import net.corda.lifecycle.LifecycleEvent
 import net.corda.lifecycle.LifecycleEventHandler
 import net.corda.lifecycle.StartEvent
-import net.corda.p2p.gateway.Gateway.Companion.CONFIG_KEY
+import net.corda.lifecycle.domino.logic.DominoTile
+import net.corda.lifecycle.domino.logic.util.ResourcesHolder
 import net.corda.p2p.gateway.messaging.http.DestinationInfo
 import net.corda.p2p.gateway.messaging.http.HttpEventListener
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import org.mockito.Mockito
 import org.mockito.kotlin.any
 import org.mockito.kotlin.argumentCaptor
 import org.mockito.kotlin.doAnswer
 import org.mockito.kotlin.doReturn
-import org.mockito.kotlin.eq
+import org.mockito.kotlin.doThrow
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.never
 import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
 import java.net.URI
-import java.time.Duration
 
 class ReconfigurableConnectionManagerTest {
     private val manager = mock<ConnectionManager>()
     private val handler = argumentCaptor<LifecycleEventHandler>()
-    private val event = argumentCaptor<LifecycleEvent>()
     private val coordinator = mock<LifecycleCoordinator> {
         on { start() } doAnswer {
             handler.lastValue.processEvent(StartEvent(), mock)
-        }
-        on { postEvent(event.capture()) } doAnswer {
-            handler.lastValue.processEvent(event.lastValue, mock)
         }
     }
     private val factory = mock<LifecycleCoordinatorFactory> {
         on { createCoordinator(any(), handler.capture()) } doReturn coordinator
     }
-
-    private val configHandler = argumentCaptor<ConfigurationHandler>()
     private val service = mock<ConfigurationReadService> {
-        on { registerForUpdates(configHandler.capture()) } doReturn mock()
+        on { registerForUpdates(any()) } doReturn mock()
     }
     private val listener = mock<HttpEventListener>()
-    private val configuration = mock<Config> {
-        on {getInt(any())} doReturn 5
-        on {getLong(any())} doReturn 5L
-        on {getString(any())} doReturn ""
-        on {getDuration(any())} doReturn Duration.ofMillis(1)
-        on {getBoolean(any())} doReturn true
-        on {getConfig(any())} doReturn this.mock
-        on {getEnum(eq(RevocationConfigMode::class.java), any())} doReturn RevocationConfigMode.OFF
+    private val configuration = mock<GatewayConfiguration> {
+        on { sslConfig } doReturn mock()
+    }
+    private val badConfigurationException = RuntimeException("Bad Config")
+    private val badConfiguration = mock<GatewayConfiguration> {
+        on { sslConfig } doThrow(badConfigurationException)
+    }
+
+    private val resourcesHolder = mock<ResourcesHolder>()
+    private lateinit var configHandler: ReconfigurableConnectionManager.ConnectionManagerConfigChangeHandler
+    private val lifecycleLockLambdaCaptor = argumentCaptor<() -> Any>()
+    private val dominoTile = Mockito.mockConstruction(DominoTile::class.java) { mock, context ->
+        whenever(mock.withLifecycleLock(lifecycleLockLambdaCaptor.capture())).doAnswer {lifecycleLockLambdaCaptor.lastValue.invoke()}
+        @Suppress("UNCHECKED_CAST")
+        configHandler = (context.arguments()[4] as ReconfigurableConnectionManager.ConnectionManagerConfigChangeHandler)
     }
 
     private val connectionManager = ReconfigurableConnectionManager(factory, service, listener) { manager }
+
+    @AfterEach
+    fun cleanUp() {
+        dominoTile.close()
+    }
 
     @Test
     fun `acquire will throw an exception if configuration is not ready`() {
@@ -66,11 +72,9 @@ class ReconfigurableConnectionManagerTest {
 
     @Test
     fun `acquire will call the manager`() {
-        //val resources = ResourcesHolder()
+        val resources = ResourcesHolder()
         connectionManager.start()
-
-        configHandler.lastValue.onNewConfiguration(setOf(CONFIG_KEY), mapOf(CONFIG_KEY to configuration))
-        //connectionManager.applyNewConfiguration(configuration, null, resources)
+        configHandler.applyNewConfiguration(configuration, null, resources)
         val info = DestinationInfo(
             URI("http://www.r3.com:3000"),
             "",
@@ -86,41 +90,53 @@ class ReconfigurableConnectionManagerTest {
     }
 
     @Test
-    fun `Config update will close manager`() {
+    fun `applyNewConfiguration will close manager`() {
         connectionManager.start()
-        configHandler.lastValue.onNewConfiguration(setOf(CONFIG_KEY), mapOf(CONFIG_KEY to configuration))
+        configHandler.applyNewConfiguration(configuration, null, resourcesHolder)
 
-        val secondConfiguration = mock<Config> {
-            on {getInt(any())} doReturn 66
-            on {getLong(any())} doReturn 5L
-            on {getString(any())} doReturn ""
-            on {getDuration(any())} doReturn Duration.ofMillis(1)
-            on {getBoolean(any())} doReturn true
-            on {getConfig(any())} doReturn this.mock
-            on {getEnum(eq(RevocationConfigMode::class.java), any())} doReturn RevocationConfigMode.SOFT_FAIL
+        val secondConfiguration = mock<GatewayConfiguration> {
+            on { sslConfig } doReturn mock()
         }
-        configHandler.lastValue.onNewConfiguration(setOf(CONFIG_KEY), mapOf(CONFIG_KEY to secondConfiguration))
+        configHandler.applyNewConfiguration(secondConfiguration, configuration, resourcesHolder)
 
         verify(manager).close()
     }
 
     @Test
-    fun `Config update will not close the manager if same configuration`() {
-        connectionManager.start()
-        configHandler.lastValue.onNewConfiguration(setOf(CONFIG_KEY), mapOf(CONFIG_KEY to configuration))
+    fun `applyNewConfiguration sets configApplied`() {
+        configHandler.applyNewConfiguration(configuration, null, resourcesHolder)
 
-        configHandler.lastValue.onNewConfiguration(setOf(CONFIG_KEY), mapOf(CONFIG_KEY to configuration))
+        verify(dominoTile.constructed().last()).configApplied(DominoTile.ConfigUpdateResult.Success)
+    }
+
+    @Test
+    fun `applyNewConfiguration sets configApplied if bad config`() {
+        configHandler.applyNewConfiguration(badConfiguration, null, resourcesHolder)
+
+        verify(dominoTile.constructed().last()).configApplied(DominoTile.ConfigUpdateResult.Error(badConfigurationException))
+    }
+
+    @Test
+    fun `applyNewConfiguration will not close the manager if same configuration`() {
+        val sslConfiguration = mock<SslConfiguration>()
+        val firstConfiguration = mock<GatewayConfiguration> {
+            on { sslConfig } doReturn sslConfiguration
+        }
+        val secondConfiguration = mock<GatewayConfiguration> {
+            on { sslConfig } doReturn sslConfiguration
+        }
+        connectionManager.start()
+        configHandler.applyNewConfiguration(firstConfiguration, null, resourcesHolder)
+
+        configHandler.applyNewConfiguration(secondConfiguration, secondConfiguration, resourcesHolder)
 
         verify(manager, never()).close()
     }
 
     @Test
-    fun `close will close the manager`() {
-        connectionManager.start()
-        configHandler.lastValue.onNewConfiguration(setOf(CONFIG_KEY), mapOf(CONFIG_KEY to configuration))
+    fun `applyNewConfiguration keeps the manager in the resource holder`() {
+        configHandler.applyNewConfiguration(configuration, null, resourcesHolder)
 
-        connectionManager.close()
-
-        verify(manager).close()
+        verify(resourcesHolder).keep(manager)
     }
 }
