598e889f4344953e886e8837f1d7d8cc139dac39
Viktor Kolomeyko <Viktor.Kolomeyko@r3.com>
2021-11-04 11:28:34 +0000

    CORE-2840: Change implementation of `PermissionServiceImpl`

--- libs/permissions/permission-validation-impl/src/main/kotlin/net/corda/libs/permission/PermissionServiceImpl.kt
+++ libs/permissions/permission-validation-impl/src/main/kotlin/net/corda/libs/permission/PermissionServiceImpl.kt
@@ -1,62 +1,144 @@
 package net.corda.libs.permission
 
+import net.corda.data.permissions.Group
+import net.corda.data.permissions.PermissionType
+import net.corda.data.permissions.Role
 import net.corda.data.permissions.User
 import net.corda.messaging.api.subscription.CompactedSubscription
 import net.corda.messaging.api.subscription.factory.SubscriptionFactory
 import net.corda.messaging.api.subscription.factory.config.SubscriptionConfig
+import net.corda.v5.base.util.contextLogger
+import net.corda.v5.base.util.debug
+import java.util.concurrent.locks.ReentrantLock
+import kotlin.concurrent.withLock
 
 class PermissionServiceImpl(
         private val subscriptionFactory: SubscriptionFactory,
-        private val userTopicProcessor: UserTopicProcessor
+        private val userTopicProcessor: UserTopicProcessor,
+        private val groupTopicProcessor: GroupTopicProcessor,
+        private val roleTopicProcessor: RoleTopicProcessor
 ) : PermissionService {
 
     companion object {
-        private const val USER_PERMISSION_TOPIC = "user-permissions-topic"
+
+        private val logger = contextLogger()
+
+        // Those are to be removed in favor of constants from Corda-API
+        private const val RPC_PERM_USER_TOPIC = "rpc.permissions.user"
+        private const val RPC_PERM_GROUP_TOPIC = "rpc.permissions.group"
+        private const val RPC_PERM_ROLE_TOPIC = "rpc.permissions.role"
+
         internal const val CONSUMER_GROUP = "PERMISSION_SERVICE"
     }
 
     @Volatile
-    private var stopped = false
+    private var running = false
 
-    private var subscription: CompactedSubscription<String, User>? = null
+    private val lock = ReentrantLock()
 
-    override fun authorizeUser(requestId: String, loginName: String, permission: String): Boolean {
+    private var subscriptions: Triple<
+                CompactedSubscription<String, User>,
+                CompactedSubscription<String, Group>,
+                CompactedSubscription<String, Role>
+            >? = null
 
-        val user = userTopicProcessor.getUser(loginName) ?: return false
-
-        if (!user.enabled) return false
-
-        return false
-
-        // Review given new topics definition
-        //val permissionRequested = PermissionUrl.fromUrl(permission).permissionRequested
-        //return user.roleIds.flatMap { it.permissions }.any { it.type == PermissionType.ALLOW &&
-        // it.permissionString == permissionRequested }
-    }
 
     override val isRunning: Boolean
-        get() = !stopped
+        get() = running
 
     override fun start() {
-            if (subscription == null) {
-                subscription =
-                        subscriptionFactory.createCompactedSubscription(
-                                SubscriptionConfig(
-                                        CONSUMER_GROUP,
-                                        USER_PERMISSION_TOPIC
-                                ),
-                                userTopicProcessor
-                        )
-                subscription!!.start()
-                stopped = false
+        lock.withLock {
+            if (subscriptions == null) {
+                val userSubscription =
+                    subscriptionFactory.createCompactedSubscription(
+                        SubscriptionConfig(
+                            CONSUMER_GROUP,
+                            RPC_PERM_USER_TOPIC
+                        ),
+                        userTopicProcessor
+                    ).also { it.start() }
+                val groupSubscription =
+                    subscriptionFactory.createCompactedSubscription(
+                        SubscriptionConfig(
+                            CONSUMER_GROUP,
+                            RPC_PERM_GROUP_TOPIC
+                        ),
+                        groupTopicProcessor
+                    ).also { it.start() }
+                val roleSubscription =
+                    subscriptionFactory.createCompactedSubscription(
+                        SubscriptionConfig(
+                            CONSUMER_GROUP,
+                            RPC_PERM_ROLE_TOPIC
+                        ),
+                        roleTopicProcessor
+                    ).also { it.start() }
+                subscriptions = Triple(userSubscription, groupSubscription, roleSubscription)
+                running = true
             }
+        }
     }
 
     override fun stop() {
-            if (!stopped) {
-                subscription?.stop()
-                subscription = null
-                stopped = true
+        lock.withLock {
+            if (running) {
+                subscriptions?.toList()?.forEach { it.stop() }
+                subscriptions = null
+                running = false
             }
+        }
+    }
+
+    override fun authorizeUser(requestId: String, loginName: String, permission: String): Boolean {
+
+        logger.debug { "Checking permissions for $permission for user $loginName" }
+
+        val user = userTopicProcessor.getUser(loginName) ?: return false
+
+        if (!user.enabled) {
+            logger.debug { "User $loginName is disabled" }
+            return false
+        }
+
+        return performCheckRec(user.roleIds, user.parentGroupId, permission)
+    }
+
+    private tailrec fun performCheckRec(roleIds: Collection<String>, parentGroupId: String?, permission: String): Boolean {
+
+        logger.debug { "Checking permissions for: $permission - $roleIds - $parentGroupId" }
+
+        if (roleIds.isEmpty() && parentGroupId == null) {
+            logger.debug { "Roles are empty and no parent group left" }
+            return false
+        }
+
+        // Should we report roles that cannot be found?
+        val roles = roleIds.mapNotNull { roleTopicProcessor.getRole(it) }
+
+        val permissionRequested = PermissionUrl.fromUrl(permission).permissionRequested
+        val allPermissions = roles.flatMap { it.permissions }
+
+        // Perform checks, with deny taking priority over allow
+        val (denies, allows) = allPermissions.partition { it.type == PermissionType.DENY }
+        if (denies.any { it.permissionString == permissionRequested }) {
+            logger.debug { "Explicitly denied by: '${denies.first { it.permissionString == permissionRequested }}'" }
+            return false
+        }
+        if (allows.any { it.permissionString == permissionRequested }) {
+            logger.debug { "Explicitly allowed by: '${allows.first { it.permissionString == permissionRequested }}'" }
+            return true
+        }
+
+        // If we could not reach decision yet, try referring to the parent
+        if (parentGroupId == null) {
+            return false
+        }
+        val parentGroup = groupTopicProcessor.getGroup(parentGroupId)
+        if (parentGroup == null) {
+            logger.warn("Group with id: '$parentGroupId' cannot be found")
+            return false
+        }
+        val rolesIdsForGroup = parentGroup.roleIds
+        return performCheckRec(rolesIdsForGroup, parentGroup.parentGroupId, permission)
     }
 }
