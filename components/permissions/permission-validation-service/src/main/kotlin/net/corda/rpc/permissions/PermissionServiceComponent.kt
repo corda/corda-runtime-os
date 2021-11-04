6f2198a6def1c47b5f42d4c0dae8a0ece3566eae
Viktor Kolomeyko <Viktor.Kolomeyko@r3.com>
2021-11-04 17:51:09 +0000

    CORE-2840: Changes to component

--- components/permissions/permission-validation-service/src/main/kotlin/net/corda/rpc/permissions/PermissionServiceComponent.kt
+++ components/permissions/permission-validation-service/src/main/kotlin/net/corda/rpc/permissions/PermissionServiceComponent.kt
@@ -1,28 +1,27 @@
 package net.corda.rpc.permissions
 
-import net.corda.libs.permission.PermissionService
+import net.corda.libs.permission.PermissionValidator
 import net.corda.libs.permission.factory.PermissionServiceFactory
 import net.corda.lifecycle.Lifecycle
 import org.osgi.service.component.annotations.Reference
 
-class PermissionServiceComponent  (
-        @Reference(service = PermissionServiceFactory::class)
-        private val permissionServiceFactory: PermissionServiceFactory
+class PermissionServiceComponent(
+    @Reference(service = PermissionServiceFactory::class)
+    private val permissionServiceFactory: PermissionServiceFactory
 ) : Lifecycle {
 
-    private var receivedSnapshot = false
-
-    private var permissionService: PermissionService? = null
+    @Volatile
+    private var permissionValidator: PermissionValidator? = null
 
     override val isRunning: Boolean
-        get() = receivedSnapshot
+        get() = permissionValidator.let { it?.isRunning ?: false }
 
     override fun start() {
-       permissionService?.stop()
-        permissionService = permissionServiceFactory.createPermissionService()
+        permissionValidator?.stop()
+        permissionValidator = permissionServiceFactory.createPermissionService()
     }
 
     override fun stop() {
-      permissionService?.stop()
+        permissionValidator?.stop()
     }
 }
