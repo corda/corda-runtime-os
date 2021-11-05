775488065ae4f1a8eeb42000bb575bae175c8929
pnemeth <peter.nemeth@r3.com>
2021-11-05 14:57:35 +0000

    CORE-2840 permission modules creation (#381)
    * permission modules creation
    
    * refactorings
    
    * add PermissionTopicProcessorTest
    
    * update interface; add tests
    
    * refactoring permissionService WIP
    
    * CORE-2840: Compilation fixes after rebase to the latest
    
    * CORE-2840: Fix Permissions component
    
    * CORE-2840: Fix Permissions Service Impl library
    
    * CORE-2840: Remove explicit version of Avro schema
    
    * CORE-2840: Resolve missing dependency
    
    * CORE-2840: Remove unused import
    
    * CORE-2840: Address code review comment by @conalsmith-r3
    
    * CORE-2840: Making Detekt happier
    
    * CORE-2840: Rename modules as per suggestion from @conalsmith-r3
    
    * CORE-2840: Rename to avoid duplicating class names
    
    * CORE-2840: Topic processors re-jig
    
    * CORE-2840: Change implementation of `PermissionServiceImpl`
    
    * CORE-2840: Fix wiring for `PermissionServiceFactoryImpl` and the unit test
    
    * CORE-2840: Changes to component
    
    * CORE-2840: Rename `PermissionService` to `PermissionValidator`
    
    * CORE-2840: Remove `VisibleForTesting`
    
    * CORE-2840: Reformat
    
    * CORE-2840: Use `ConcurrentMap`
    
    * CORE-2840: Rename test
    
    * CORE-2840: Remove use of `Triple`
    
    * CORE-2840: Use wildcard matching
    
    * CORE-2840: Perform wildcard matching
    
    * CORE-2840: Make `start/stop` in `PermissionServiceComponent` idempotent.
    
    * CORE-2840: Use topic constants from Corda API
    
    Co-authored-by: Viktor Kolomeyko <Viktor.Kolomeyko@r3.com>

---
+++ libs/permissions/permission-validation-impl/src/test/kotlin/net/corda/libs/permission/PermissionValidatorImplTest.kt
@@ -0,0 +1,91 @@
+package net.corda.libs.permission
+
+import net.corda.data.permissions.ChangeDetails
+import net.corda.data.permissions.Permission
+import net.corda.data.permissions.PermissionType
+import net.corda.data.permissions.Role
+import net.corda.data.permissions.User
+import net.corda.messaging.api.subscription.factory.SubscriptionFactory
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import org.mockito.kotlin.mock
+import java.time.Instant
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+class PermissionValidatorImplTest {
+
+    companion object {
+        private val userProcessor = UserTopicProcessor()
+        private val groupProcessor = GroupTopicProcessor()
+        private val roleProcessor = RoleTopicProcessor()
+        private val subsFactory: SubscriptionFactory = mock()
+        private val permissionService = PermissionValidatorImpl(subsFactory, userProcessor, groupProcessor, roleProcessor)
+
+        private const val virtualNode = "f39d810f-6ee6-4742-ab7c-d1fe274ab85e"
+        private const val permissionString = "flow/start/com.myapp.MyFlow"
+
+        private const val permissionUrlRequest = "https://host:1234/node-rpc/5e0a07a6-c25d-413a-be34-647a792f4f58/${permissionString}"
+
+        private val permission = Permission("5e0a07a6-c25d-413a-be34-647a792f4f58", 1,
+                ChangeDetails(Instant.now(), "changeUser"),
+                virtualNode,
+                permissionString,
+                PermissionType.ALLOW)
+
+        private val permissionDenied = Permission("5e0a07a6-c25d-413a-be34-647a792f4f58", 1,
+                ChangeDetails(Instant.now(), "changeUser"),
+                virtualNode,
+                permissionString,
+                PermissionType.DENY)
+
+        private val role = Role("roleId1", 1,
+            ChangeDetails(Instant.now(), "changeUser"), "STARTFLOW-MYFLOW", listOf(permission))
+        private val roleWithPermDenied = Role("roleId2", 1,
+            ChangeDetails(Instant.now(), "changeUser"), "STARTFLOW-MYFLOW", listOf(permissionDenied))
+        private val user = User("user1", 1, ChangeDetails(Instant.now(), "changeUser"), "full name", true,
+            "hashedPassword", "saltValue", false, null, null, listOf(role).map { it.id })
+        private val userWithPermDenied = User("userWithPermDenied", 1, ChangeDetails(Instant.now(), "changeUser"), "full name", true,
+            "hashedPassword", "saltValue", false, null, null, listOf(roleWithPermDenied).map { it.id })
+        private val disabledUser = User("disabledUser", 1, ChangeDetails(Instant.now(), "changeUser"), "full name", false,
+            "hashedPassword", "saltValue",false, null, null, listOf(role).map { it.id })
+
+        @BeforeAll
+        @JvmStatic
+        fun setUp() {
+
+            listOf(role, roleWithPermDenied).associateBy { it.id }.let {
+                roleProcessor.onSnapshot(it)
+            }
+
+            listOf(user, disabledUser, userWithPermDenied).associateBy { it.id }.let {
+                userProcessor.onSnapshot(it)
+            }
+        }
+    }
+
+    @Test
+    fun `User with proper permission will be authorized`() {
+
+        assertTrue(permissionService.authorizeUser("requestId", user.id, permissionUrlRequest))
+    }
+
+    @Test
+    fun `Non-existing user will not be authorized`() {
+
+        assertFalse(permissionService.authorizeUser("requestId", "user2", permissionUrlRequest))
+    }
+
+    @Test
+    fun `Disabled user will not be authorized`() {
+
+        assertFalse(permissionService.authorizeUser("requestId", disabledUser.id, permissionUrlRequest))
+    }
+    @Test
+    fun `User with proper permission set to DENY will not be authorized`() {
+
+        assertFalse(permissionService.authorizeUser("requestId", userWithPermDenied.id, permissionUrlRequest))
+    }
+
+    // More tests are to be added which verify group related permissions
+}
