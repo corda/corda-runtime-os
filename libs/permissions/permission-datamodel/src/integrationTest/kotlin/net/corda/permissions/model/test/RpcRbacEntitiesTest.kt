aa69a0882aa991295b6874e264e932e25121fd2a
Viktor Kolomeyko <viktor.kolomeyko@r3.com>
2021-11-11 10:26:48 +0000

    CORE-1975: Improve `RpcRbacEntitiesTest` (#465)
    * CORE-1975: Improve `RpcRbacEntitiesTest`
    
    By doing more compile time checks and avoiding using reflection for classes instantiation
    
    Currently fails with `java.lang.ClassNotFoundException: net.corda.permissions.model.RbacDboFactory not found by permission-datamodel-tests [48]`
    
    * CORE-1975: Move the test to a dedicated package that does not clash with another module
    
    * CORE-1975: Remove use of Factory that is now redundant
    
    * CORE-1975: Switch a dependency to runtime
    
    * CORE-1975: Improvements to the test to allow it to run repeatedly under Postgres
    
    Also improvements to trigger local run over Postgres when only port is specified to sensibly default the rest.

---
+++ libs/permissions/permission-datamodel/src/integrationTest/kotlin/net/corda/permissions/model/test/RpcRbacEntitiesTest.kt
@@ -0,0 +1,123 @@
+package net.corda.permissions.model.test
+
+import net.corda.db.admin.LiquibaseSchemaMigrator
+import net.corda.db.admin.impl.ClassloaderChangeLog
+import net.corda.db.schema.Schema
+import net.corda.db.testkit.DbUtils
+import net.corda.orm.EntityManagerConfiguration
+import net.corda.orm.EntityManagerFactoryFactory
+import net.corda.permissions.model.ChangeAudit
+import net.corda.permissions.model.Group
+import net.corda.permissions.model.GroupProperty
+import net.corda.permissions.model.Permission
+import net.corda.permissions.model.Role
+import net.corda.permissions.model.RoleGroupAssociation
+import net.corda.permissions.model.RolePermissionAssociation
+import net.corda.permissions.model.RoleUserAssociation
+import net.corda.permissions.model.User
+import net.corda.permissions.model.UserProperty
+import net.corda.test.util.LoggingUtils.emphasise
+import net.corda.v5.base.util.contextLogger
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.osgi.framework.FrameworkUtil
+import org.osgi.test.common.annotation.InjectService
+import org.osgi.test.junit5.service.ServiceExtension
+import java.io.StringWriter
+import java.time.Instant
+import java.util.*
+import javax.persistence.EntityManagerFactory
+
+@ExtendWith(ServiceExtension::class)
+class RpcRbacEntitiesTest {
+    companion object {
+        private val logger = contextLogger()
+
+        @InjectService
+        lateinit var entityManagerFactoryFactory: EntityManagerFactoryFactory
+        @InjectService
+        lateinit var lbm: LiquibaseSchemaMigrator
+
+        lateinit var emf: EntityManagerFactory
+
+        private val dbConfig: EntityManagerConfiguration = DbUtils.getEntityManagerConfiguration("rbac")
+
+        @Suppress("unused")
+        @JvmStatic
+        @BeforeAll
+        fun setupEntities() {
+
+            val schemaClass = Schema::class.java
+            val bundle = FrameworkUtil.getBundle(schemaClass)
+            logger.info("RBAC schema bundle $bundle".emphasise())
+
+            logger.info("Create Schema for ${dbConfig.dataSource.connection.metaData.url}".emphasise())
+            val fullName = schemaClass.packageName + ".rbac"
+            val resourcePrefix = fullName.replace('.', '/')
+            val cl = ClassloaderChangeLog(
+                linkedSetOf(
+                    ClassloaderChangeLog.ChangeLogResourceFiles(
+                        fullName,
+                        listOf("$resourcePrefix/migration/rpc-rbac-creation-v1.0.xml"),
+                        classLoader = schemaClass.classLoader
+                    )
+                )
+            )
+            StringWriter().use {
+                lbm.createUpdateSql(dbConfig.dataSource.connection, cl, it)
+                logger.info("Schema creation SQL: $it")
+            }
+            lbm.updateDb(dbConfig.dataSource.connection, cl)
+
+            logger.info("Create Entities".emphasise())
+
+            emf = entityManagerFactoryFactory.create(
+                "RPC RBAC",
+                listOf(
+                    User::class.java,
+                    Group::class.java,
+                    Role::class.java,
+                    Permission::class.java,
+                    UserProperty::class.java,
+                    GroupProperty::class.java,
+                    ChangeAudit::class.java,
+                    RoleUserAssociation::class.java,
+                    RoleGroupAssociation::class.java,
+                    RolePermissionAssociation::class.java
+                ),
+                dbConfig
+            )
+        }
+
+        @Suppress("unused")
+        @AfterAll
+        @JvmStatic
+        fun done() {
+            if (this::emf.isInitialized) {
+                emf.close()
+            }
+        }
+    }
+
+    @Test
+    fun `test user creation`() {
+        val em = emf.createEntityManager()
+        try {
+            em.transaction.begin()
+            val id = UUID.randomUUID().toString()
+            val user = User(
+                id, Instant.now(), "fullName", "loginName-$id", true,
+                "saltValue", "hashedPassword", null, null)
+            em.persist(user)
+            em.transaction.commit()
+
+            val retrievedUser = em.createQuery("from User where id = '$id'", user.javaClass).singleResult
+            Assertions.assertThat(retrievedUser).isEqualTo(user)
+        } finally {
+            em.close()
+        }
+    }
+}
