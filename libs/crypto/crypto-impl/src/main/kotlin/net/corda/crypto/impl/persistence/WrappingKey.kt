7c9f708788b881a3ca1a02ac351939c0ef6f2031
Viktor Kolomeyko <Viktor.Kolomeyko@r3.com>
2021-11-17 14:18:29 +0000

    CORE-3057: Move default/dev `salt` and `password`back to `WrappingKey`

--- libs/crypto/crypto-impl/src/main/kotlin/net/corda/crypto/impl/persistence/WrappingKey.kt
+++ libs/crypto/crypto-impl/src/main/kotlin/net/corda/crypto/impl/persistence/WrappingKey.kt
@@ -2,6 +2,7 @@ package net.corda.crypto.impl.persistence
 
 import net.corda.crypto.PasswordEncodeUtils.AES_KEY_LENGTH
 import net.corda.crypto.PasswordEncodeUtils.encodePassPhrase
+import net.corda.v5.base.util.contextLogger
 import net.corda.v5.cipher.suite.CipherSchemeMetadata
 import org.bouncycastle.asn1.pkcs.PrivateKeyInfo
 import java.security.PrivateKey
@@ -17,6 +18,9 @@ class WrappingKey(
     private val schemeMetadata: CipherSchemeMetadata,
 ) {
     companion object {
+
+        private val logger = contextLogger()
+
         private const val IV_LENGTH = 16
 
         private const val AES_PROVIDER = "SunJCE"
@@ -24,7 +28,21 @@ class WrappingKey(
         private const val TRANSFORMATION = "AES/CBC/PKCS5Padding"
 
         fun deriveMasterKey(schemeMetadata: CipherSchemeMetadata, originalPassphrase: String?, originalSalt: String?): WrappingKey {
-            val encoded = encodePassPhrase(originalPassphrase, originalSalt)
+
+            val passphrase = if (originalPassphrase.isNullOrBlank()) {
+                logger.warn("Please specify the passphrase in the configuration, for now will be using the dev value!")
+                "PASSPHRASE"
+            } else {
+                originalPassphrase
+            }
+            val salt = if (originalSalt.isNullOrBlank()) {
+                logger.warn("Please specify the salt in the configuration, for now will be using the dev value!")
+                "SALT"
+            } else {
+                originalSalt
+            }
+
+            val encoded = encodePassPhrase(passphrase, salt)
             return WrappingKey(
                 schemeMetadata = schemeMetadata,
                 master = SecretKeySpec(encoded, WRAPPING_KEY_ALGORITHM)
