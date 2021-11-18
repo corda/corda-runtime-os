e8ac11c329401277234f43133de3bd8b6bed29bf
Joseph Zuniga-Daly <joseph.zunigadaly@r3.com>
2021-11-18 14:27:36 +0000

    Configure testSerializationContext with a mock sandbox group

--- libs/serialization/serialization-amqp/src/test/kotlin/net/corda/internal/serialization/amqp/testutils/TestSerializationContext.kt
+++ libs/serialization/serialization-amqp/src/test/kotlin/net/corda/internal/serialization/amqp/testutils/TestSerializationContext.kt
@@ -5,8 +5,19 @@ package net.corda.internal.serialization.amqp.testutils
 import net.corda.internal.serialization.AllWhitelist
 import net.corda.internal.serialization.SerializationContextImpl
 import net.corda.internal.serialization.amqp.amqpMagic
+import net.corda.packaging.CPK
+import net.corda.sandbox.SandboxGroup
 import net.corda.serialization.SerializationContext
+import net.corda.v5.base.util.uncheckedCast
 
+private class MockSandboxGroup : SandboxGroup {
+        override val cpks: Collection<CPK> = emptyList()
+        override fun <T : Any> loadClassFromMainBundles(className: String, type: Class<T>): Class<out T> =
+                uncheckedCast(Class.forName(className))
+        override fun getClass(className: String, serialisedClassTag: String) = Class.forName(className)
+        override fun getStaticTag(klass: Class<*>): String = "S;bundle;sandbox"
+        override fun getEvolvableTag(klass: Class<*>) = "E;bundle;sandbox"
+}
 
 val testSerializationContext: SerializationContext
         get() = SerializationContextImpl(
@@ -16,4 +27,4 @@ val testSerializationContext: SerializationContext
                 objectReferencesEnabled = false,
                 useCase = SerializationContext.UseCase.Testing,
                 encoding = null
-        )
+        ).withSandboxGroup(MockSandboxGroup())
