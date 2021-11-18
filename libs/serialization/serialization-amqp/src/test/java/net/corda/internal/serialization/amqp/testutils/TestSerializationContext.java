211ae56a80fe11ea26f95392d81cce1a863e1161
Joseph Zuniga-Daly <joseph.zunigadaly@r3.com>
2021-11-18 11:51:54 +0000

    Make TestSerializationContext#testSerializationContext return the version defined in kotlin

--- libs/serialization/serialization-amqp/src/test/java/net/corda/internal/serialization/amqp/testutils/TestSerializationContext.java
+++ libs/serialization/serialization-amqp/src/test/java/net/corda/internal/serialization/amqp/testutils/TestSerializationContext.java
@@ -1,23 +1,8 @@
 package net.corda.internal.serialization.amqp.testutils;
 
-import net.corda.internal.serialization.AllWhitelist;
-import net.corda.internal.serialization.SerializationContextImpl;
 import net.corda.serialization.SerializationContext;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import static net.corda.internal.serialization.amqp.SchemaKt.amqpMagic;
 
 public class TestSerializationContext {
 
-    private static final Map<Object, Object> serializationProperties = new HashMap<>();
-
-    public static SerializationContext testSerializationContext = new SerializationContextImpl(
-        amqpMagic,
-        AllWhitelist.INSTANCE,
-        serializationProperties,
-        false,
-        SerializationContext.UseCase.Testing,
-        null);
+    public static SerializationContext testSerializationContext = TestSerializationContextKt.getTestSerializationContext();
 }
