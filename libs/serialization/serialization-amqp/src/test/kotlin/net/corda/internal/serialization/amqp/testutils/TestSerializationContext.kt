608d9938f6a2fa8e24520af2f64e1772ba6ea130
Joseph Zuniga-Daly <joseph.zunigadaly@r3.com>
2021-11-18 11:57:53 +0000

    Turn testSerializationContext into a getter so we build a new context for each test

--- libs/serialization/serialization-amqp/src/test/kotlin/net/corda/internal/serialization/amqp/testutils/TestSerializationContext.kt
+++ libs/serialization/serialization-amqp/src/test/kotlin/net/corda/internal/serialization/amqp/testutils/TestSerializationContext.kt
@@ -7,11 +7,13 @@ import net.corda.internal.serialization.SerializationContextImpl
 import net.corda.internal.serialization.amqp.amqpMagic
 import net.corda.serialization.SerializationContext
 
-val testSerializationContext: SerializationContext = SerializationContextImpl(
-        preferredSerializationVersion = amqpMagic,
-        whitelist = AllWhitelist,
-        properties = mutableMapOf(),
-        objectReferencesEnabled = false,
-        useCase = SerializationContext.UseCase.Testing,
-        encoding = null
-)
+
+val testSerializationContext: SerializationContext
+        get() = SerializationContextImpl(
+                preferredSerializationVersion = amqpMagic,
+                whitelist = AllWhitelist,
+                properties = mutableMapOf(),
+                objectReferencesEnabled = false,
+                useCase = SerializationContext.UseCase.Testing,
+                encoding = null
+        )
