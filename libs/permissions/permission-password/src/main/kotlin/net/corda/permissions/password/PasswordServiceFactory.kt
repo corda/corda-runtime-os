abf74ff98223ed6e322d6029927f22ab294c3da0
Viktor Kolomeyko <viktor.kolomeyko@r3.com>
2021-11-19 09:51:06 +0000

    CORE-3057: Create module for RPC password hashing verification (#485)
    * CORE-3057: First stub on password encoding
    
    Still to do:
    - Try use "wrap"/"unwrap" to avoid exposing "master"
    - OSGi export as necessary
    
    * CORE-3057: OSGi import and refactoring of `WrappingKey`
    
    * CORE-3057: Re-jig dependencies
    
    * CORE-3057: Move utility function to to `:crypto:crypto-internal` module
    
    * CORE-3057: Move default/dev `salt` and `password`back to `WrappingKey`
    
    * CORE-3057: Add verification that salt and hashed value change every time even for the same password.
    
    * CORE-3057: Split unit test into 3 as suggested by @conalsmith-r3

---
+++ libs/permissions/permission-password/src/main/kotlin/net/corda/permissions/password/PasswordServiceFactory.kt
@@ -0,0 +1,12 @@
+package net.corda.permissions.password
+
+import net.corda.permissions.password.impl.PasswordServiceImpl
+import org.osgi.service.component.annotations.Component
+import java.security.SecureRandom
+
+@Component(service = [PasswordServiceFactory::class])
+class PasswordServiceFactory {
+    fun createPasswordService(secureRandom: SecureRandom): PasswordService {
+        return PasswordServiceImpl(secureRandom)
+    }
+}
