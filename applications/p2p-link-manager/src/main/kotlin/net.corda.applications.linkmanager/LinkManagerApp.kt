d4b1ceaf38d49f130cff8e9fbefe1242a47fbd88
Yiftach Kaplan <yiftach.kaplan@r3.com>
2021-11-23 15:54:46 +0000

    why?

--- applications/p2p-link-manager/src/main/kotlin/net.corda.applications.linkmanager/LinkManagerApp.kt
+++ applications/p2p-link-manager/src/main/kotlin/net.corda.applications.linkmanager/LinkManagerApp.kt
@@ -64,7 +64,7 @@ class LinkManagerApp @Activate constructor(
                 thread(isDaemon = true) {
                     while (!linkmanager.isRunning) {
                         consoleLogger.info("Waiting for link manager to start...")
-                        Thread.sleep(10000)
+                        Thread.sleep(1000)
                     }
                     consoleLogger.info("Link manager is running")
                 }
--- applications/tools/p2p-test/p2p-layer-deployment/src/main/kotlin/net/corda/p2p/deployment/ConfiguratorPod.kt
+++ applications/tools/p2p-test/p2p-layer-deployment/src/main/kotlin/net/corda/p2p/deployment/ConfiguratorPod.kt
@@ -3,6 +3,7 @@ package net.corda.p2p.deployment
 class ConfiguratorPod(kafkaServers: String) : Pod() {
     override val app = "configurator"
     // YIFT: Replace with actual image
+    //override val image = "corda-os-docker-dev.software.r3.com/corda-os-configuration-publisher:latest"
     override val image = "azul/zulu-openjdk-alpine:11"
     override val command = listOf("sleep", "infinity")
     override val environmentVariables = mapOf("KAFKA_SERVERS" to kafkaServers)
