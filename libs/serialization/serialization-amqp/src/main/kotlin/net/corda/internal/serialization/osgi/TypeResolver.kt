3797dccd316c721b1b74dc5999016ef33efdb14f
Joseph Zuniga-Daly <joseph.zunigadaly@r3.com>
2021-11-24 10:06:03 +0000

    Remove unused method

--- libs/serialization/serialization-amqp/src/main/kotlin/net/corda/internal/serialization/osgi/TypeResolver.kt
+++ libs/serialization/serialization-amqp/src/main/kotlin/net/corda/internal/serialization/osgi/TypeResolver.kt
@@ -4,9 +4,6 @@ import net.corda.sandbox.SandboxGroup
 
 object TypeResolver {
 
-    fun resolve(className: String, classLoader: ClassLoader): Class<*>
-        = Class.forName(className, false, classLoader)
-
     fun resolve(className: String, sandboxGroup: SandboxGroup): Class<*>
             = sandboxGroup.loadClassFromMainBundles(className, Any::class.java)
 }
