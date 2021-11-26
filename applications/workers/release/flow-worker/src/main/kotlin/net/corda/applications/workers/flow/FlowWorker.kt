41d5e58a989f49362d2f1793339d1b6cbd8134d4
joeldudleyr3 <joel.dudley@r3.com>
2021-11-26 13:55:26 +0000

    Adds health monitor.

--- applications/workers/release/flow-worker/src/main/kotlin/net/corda/applications/workers/flow/FlowWorker.kt
+++ applications/workers/release/flow-worker/src/main/kotlin/net/corda/applications/workers/flow/FlowWorker.kt
@@ -1,33 +1,77 @@
 package net.corda.applications.workers.flow
 
-import net.corda.applications.common.ConfigHelper.Companion.getBootstrapConfig
 import net.corda.osgi.api.Application
-import net.corda.processors.flow.FlowProcessor
 import org.osgi.service.component.annotations.Component
 import picocli.CommandLine
+import java.io.File
 
+// TODO - Document.
 @Component(service = [Application::class])
 @Suppress("unused")
 class FlowWorker: Application {
+    private val healthMonitor = HealthMonitor()
+
+    init {
+        healthMonitor.setIsHealthy()
+    }
 
     @Suppress("SpreadOperator")
     override fun startup(args: Array<String>) {
-        val parameters = CliParameters()
-        CommandLine(parameters).parseArgs(*args)
-        // TODO - Is this just a debug method? Where is this config coming from?
-        val bootstrapConfig = getBootstrapConfig(parameters.instanceId.toInt())
-        FlowProcessor().startup(bootstrapConfig)
+        println("flow worker starting")
+
+        // We comment this out for now, to avoid having to pass in any config.
+//        val parameters = CliParameters()
+//        CommandLine(parameters).parseArgs(*args)
+//        // TODO - Is this just a debug method? Where is this config coming from?
+//        val bootstrapConfig = getBootstrapConfig(parameters.instanceId.toInt())
+//        FlowProcessor().startup(bootstrapConfig)
+
+        // Sets the worker to unhealthy after 30 seconds.
+        var x = 0
+        while (true) {
+            Thread.sleep(1000)
+            println(x)
+            x++
+            if (x > 30) {
+                healthMonitor.setIsUnhealthy()
+            }
+            continue
+        }
     }
 
     override fun shutdown() = Unit
 }
 
-// TODO - Define this in a common library.
+// TODO - Make this part of the common worker libraries.
 class CliParameters {
     @CommandLine.Option(names = ["--instanceId"], description = ["InstanceId for this worker"])
     lateinit var instanceId: String
 }
 
-// TODO - Override logging.
+// TODO - Implement Docker-friendly logging.
+    // TODO - Allow default Docker-friendly logging to be overridden.
 
-// TODO - Provide health monitoring.
+// TODO - Make this part of the common worker libraries.
+/**
+ * Allows the health of the worker to be monitored by external processes.
+ *
+ * The worker is considered healthy if and only if a file exists at `/tmp/worker_is_healthy`. Otherwise, the worker is
+ * considered unhealthy. Docker Healthcheck and Kubernetes liveness probes can therefore monitor the health of the
+ * worker using the command `cat /tmp/worker_is_healthy`, which will return 0 if the file exists, and 1 otherwise.
+ *
+ * Since this file does not exist initially, the worker starts in an unhealthy state. Health checks should therefore be
+ * delayed until an initial call to [setIsHealthy].
+ */
+class HealthMonitor {
+    companion object {
+        // TODO - Move to constants file.
+        private const val healthCheckFileName = "/tmp/worker_is_healthy"
+        private val healthCheckFile = File(healthCheckFileName)
+    }
+
+    /** Marks the worker as healthy. */
+    fun setIsHealthy() = healthCheckFile.createNewFile()
+
+    /** Marks the worker as unhealthy. */
+    fun setIsUnhealthy() = healthCheckFile.delete()
+}
