e11d06caaa25ba11e98597a5bc5d85aec1688349
Yiftach Kaplan <yiftach.kaplan@r3.com>
2021-11-26 14:38:22 +0000

    Move link

--- applications/tools/p2p-test/p2p-layer-deployment/src/main/resources/sql/create_table.sql
+++ applications/tools/p2p-test/p2p-layer-deployment/src/main/resources/sql/create_table.sql
@@ -1,1 +1,1 @@
-/home/yift/corda-runtime-os/applications/tools/p2p-test/app-simulator/src/test/resources/postgres-docker/docker-postgresql-init-scripts/create_table.sql
+../../../../../../p2p-test/app-simulator/src/test/resources/postgres-docker/docker-postgresql-init-scripts/create_table.sql
