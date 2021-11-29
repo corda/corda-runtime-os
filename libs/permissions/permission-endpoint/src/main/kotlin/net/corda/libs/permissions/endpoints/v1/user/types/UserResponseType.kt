799c9eda918c71be19e9eb411cfe51777e4b1833
Conal Smith <68279385+conalsmith-r3@users.noreply.github.com>
2021-11-29 17:12:01 +0000

    CORE-3026 Permission RPC Ops Service (#501)
    * CORE-3026 Permission RPC Ops Service
    This component listens to status updates from the permission service. When UP, it will create the endpoints for User, Group and Role operations. It gets a reference to the permission manager and permission validator and sets these on the endpoint to be used. Certain operations may require the permission validator for additional validation hence why it is present. The endpoint delegates to the permission manager to perform read and write operations. This component should be included in the HTTP Rpc gateway component and exposes a getter to obtain all permission rpcOps endpoints when the service is UP.
    
    * CORE-3027 Add tests to setting manager/validator to null when permission service is down. Fix a few detekt errors.
    
    * CORE-3026 Permission RPC Ops Service - make endpoint implement PluggableRpcOps, remove virtualNodId from endpoint and request dtos and types.
    
    * CORE-3026 Permission RPC Ops Service - fix unit test
    
    * CORE-3026 Make endpoints an OSGi component so they automatically get picked up by the PluggableRPCOps discovery logic.
    Adding permissionManager and permissionValidator as vars to interface to allow us to set these by the component when permission service is ready.
    
    * CORE-3026 Remove endpoint impl export
    
    * CORE-3026: Illustrate proposed changes for PR #501
    
    In particular:
    1. Delete module `permission-validation` and move interfaces into module `permission-manager` and a dedicated package.
    Having two separate modules is logical, but rather inconvenient as 3rd module will have to be created for `PermissionService`.
    2. Delete module `:permission-rpcops-service`as  redundant
    3. Create `PermissionService` interface in the `:lib` area
    4. Tidy-up `UserEndpoint` interface
    5. Tidy-up `UserEndpointImpl`
    
    * CORE-3026: Make `UserEndpointImpl` a component
    
    * CORE-3026: Restore `:permission-validation` module
    
    As it is kind of "innocent" if we are not introducing `PermissionService`.
    
    Co-authored-by: Viktor Kolomeyko <Viktor.Kolomeyko@r3.com>

--- libs/permissions/permission-endpoint/src/main/kotlin/net/corda/libs/permissions/endpoints/v1/user/types/UserResponseType.kt
+++ libs/permissions/permission-endpoint/src/main/kotlin/net/corda/libs/permissions/endpoints/v1/user/types/UserResponseType.kt
@@ -1,37 +1,49 @@
-package net.corda.libs.permissions.manager.request
+package net.corda.libs.permissions.endpoints.v1.user.types
 
 import java.time.Instant
 
 /**
- * Request object for creating a User in the permission system.
+ * Response type representing a User to be returned to the caller.
  */
-data class CreateUserRequestDto(
+data class UserResponseType(
+
     /**
-     * ID of the user making the request.
+     * Id of the User.
      */
-    val requestedBy: String,
+    val id: String,
+
+    /**
+     * Version of the user.
+     */
+    val version: Int,
+
+    /**
+     * Time the user was last updated.
+     */
+    val updateTimestamp: Instant,
+
     /**
      * Full name of the User.
      */
     val fullName: String,
+
     /**
      * Login name of the User. Acts as a unique identifier.
      */
     val loginName: String,
+
     /**
      * Whether this user should be enabled or not.
      */
     val enabled: Boolean,
-    /**
-     * The initial password if the user is not to be set up with SSO providers.
-     */
-    val initialPassword: String?,
+
     /**
      * If the User account used basic authentication, the time in which it expires.
      */
     val passwordExpiry: Instant?,
+
     /**
      * The group to which the User belongs.
      */
-    val parentGroup: String?,
+    val parentGroup: String?
 )
