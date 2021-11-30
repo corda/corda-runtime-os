cef1b3a1641e8172a20cc20ffa106858f244f11d
Viktor Kolomeyko <Viktor.Kolomeyko@r3.com>
2021-11-30 16:12:12 +0000

    CORE-3121: Apply feedback from @conalsmith-r3

--- applications/http-rpc-gateway/deploy/create-topics.sh
+++ applications/http-rpc-gateway/deploy/create-topics.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-kafka_topics='docker exec kafka /opt/bitnami/kafka/bin/kafka-topics.sh --bootstrap-server kafka:9092'
+kafka_topics='/opt/bitnami/kafka/bin/kafka-topics.sh --bootstrap-server kafka:9092'
 create_kafka_topics="$kafka_topics --partitions 1 --replication-factor 1"
 
 topic_prefix='rpc.permissions'
@@ -8,15 +8,15 @@ topic_prefix='rpc.permissions'
 if [[ "$*" == "delete" ]]
   then
     echo "Delete all $topic_prefix topics"
-    $kafka_topics --delete --topic "$topic_prefix.*"
+    docker exec kafka bash -c "$kafka_topics --delete --topic \"$topic_prefix.*\""
 fi
 
 echo "(Re)create topics"
-$create_kafka_topics --create --topic $topic_prefix.management
-$create_kafka_topics --create --topic $topic_prefix.management.resp
-$create_kafka_topics --create --topic $topic_prefix.user --config "cleanup.policy=compact"
-$create_kafka_topics --create --topic $topic_prefix.group --config "cleanup.policy=compact"
-$create_kafka_topics --create --topic $topic_prefix.role --config "cleanup.policy=compact"
+docker exec kafka bash -c "$create_kafka_topics --create --topic $topic_prefix.management"
+docker exec kafka bash -c "$create_kafka_topics --create --topic $topic_prefix.management.resp"
+docker exec kafka bash -c "$create_kafka_topics --create --topic $topic_prefix.user --config \"cleanup.policy=compact\""
+docker exec kafka bash -c "$create_kafka_topics --create --topic $topic_prefix.group --config \"cleanup.policy=compact\""
+docker exec kafka bash -c "$create_kafka_topics --create --topic $topic_prefix.role --config \"cleanup.policy=compact\""
 
 echo "Topics Created:"
-$kafka_topics --list
+docker exec kafka bash -c "$kafka_topics --list"
