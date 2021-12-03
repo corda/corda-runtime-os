27908ec26aeab413061cddf3ba792999529711eb
joeldudleyr3 <joel.dudley@r3.com>
2021-12-03 15:49:35 +0000

    Adds short and long command line flags.

--- applications/workers/worker-common/src/main/kotlin/net/corda/applications/workers/workercommon/DefaultWorkerParams.kt
+++ applications/workers/worker-common/src/main/kotlin/net/corda/applications/workers/workercommon/DefaultWorkerParams.kt
@@ -7,15 +7,15 @@ import kotlin.random.Random
 /** The startup parameters handled by all workers. */
 class DefaultWorkerParams {
     @Suppress("Unused")
-    @Option(names = ["--instanceId"], description = ["The Kafka instance ID for this worker."])
+    @Option(names = ["-i", "--instanceId"], description = ["The Kafka instance ID for this worker."])
     var instanceId = Random.nextInt().absoluteValue
 
-    @Option(names = ["--disableHealthMonitor"], description = ["Disables the health monitor."])
+    @Option(names = ["-d", "--disableHealthMonitor"], description = ["Disables the health monitor."])
     var disableHealthMonitor = false
 
-    @Option(names = ["--healthMonitorPort"], description = ["The port for the health monitor."])
+    @Option(names = ["-p", "--healthMonitorPort"], description = ["The port for the health monitor."])
     var healthMonitorPort = HEALTH_MONITOR_PORT
 
-    @Option(names = ["-c"], description = ["Additional parameters for the processor."])
+    @Option(names = ["-c", "--additionalParams"], description = ["Additional parameters for the processor."])
     var additionalParams = emptyMap<String, String>()
 }
