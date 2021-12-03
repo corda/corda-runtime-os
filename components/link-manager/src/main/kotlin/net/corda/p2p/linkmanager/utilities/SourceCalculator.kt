ee50fb01b22be6d018a9592a1283249234bfeab8
Emily Bowe <emily.bowe@r3.com>
2021-12-03 10:21:02 +0000

    remove hashing from SourceCalculator

--- components/link-manager/src/main/kotlin/net/corda/p2p/linkmanager/utilities/SourceCalculator.kt
+++ components/link-manager/src/main/kotlin/net/corda/p2p/linkmanager/utilities/SourceCalculator.kt
@@ -8,9 +8,6 @@ import java.security.MessageDigest
 
 object SourceCalculator {
 
-    private const val HASH_ALGO = "SHA-256"
-    private const val HASH_TRUNCATION_SIZE = 32
-
     fun calculateSource(
         x500Name: String
     ): String {
@@ -18,15 +15,6 @@ object SourceCalculator {
             X500Name(x500Name).rdNs.flatMap { it.typesAndValues.asList() }.sortedBy { it.type.toString() }
                 .groupBy(AttributeTypeAndValue::getType, AttributeTypeAndValue::getValue)
                 .mapValues { it.value[0].toString() }.map { it }.joinToString(", ")
-        sha256Hash(x500NameSorted.toByteArray()).toHex().take(HASH_TRUNCATION_SIZE).toLowerCase()
         return x500NameSorted
-    }
-
-    private fun sha256Hash(bytes: ByteArray): ByteArray {
-        val provider = BouncyCastleProvider()
-        val messageDigest = MessageDigest.getInstance(HASH_ALGO, provider)
-        messageDigest.reset()
-        messageDigest.update(bytes)
-        return messageDigest.digest()
     }
 }
