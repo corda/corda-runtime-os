fbf517d12a5c3e259b3c8a341200dd715e20fa83
Ryan Fowler <fowlerrr@users.noreply.github.com>
2021-12-06 13:29:02 +0000

    CORDA-3154: Introduce the Corda Serializer/Deserializer patterns in the API. (#170)

---
+++ data/avro-schema/src/main/kotlin/net/corda/data/CordaDeserializer.kt
@@ -0,0 +1,14 @@
+package net.corda.data
+
+/**
+ * Defines the interface for Message Bus deserialization.  The underlying mechanism may differ.
+ */
+interface CordaDeserializer<T> {
+    /**
+     * Deserialize the given [data] into an object of type [T]
+     *
+     * @param data the serialized byte stream representing the data
+     * @return the object represented by [data] or null if unsuccessful
+     */
+    fun deserialize(data: ByteArray): T?
+}
