00d70ef33dba0b40d4c9e4d6b960f8d631111e7b
Dries Samyn <dries.samyn@r3.com>
2021-12-07 18:19:11 +0000

    NOTICK - Fix CPI test for Windows (#172)
    * Close streams.
    
    * Close the CPI apparently :(
    
    * Replace \ with / in map key to support windows.
    
    * PR Feedback
    
    * PR Feedback

--- packaging/src/test/kotlin/net/corda/packaging/CPITests.kt
+++ packaging/src/test/kotlin/net/corda/packaging/CPITests.kt
@@ -2,6 +2,7 @@ package net.corda.packaging
 
 import net.corda.v5.crypto.DigestAlgorithmName
 import net.corda.v5.crypto.SecureHash
+import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
@@ -14,43 +15,50 @@ import java.security.DigestInputStream
 import java.security.MessageDigest
 import java.util.TreeSet
 
-//This is to avoid extracting the CPK archive in every single test case,
+// This is to avoid extracting the CPK archive in every single test case,
 // no test case writes anything to the filesystem, nor alters the state of the test class instance;
 // this makes it safe to use the same instance for all test cases (test case execution order is irrelevant)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class CPITests {
 
-    private lateinit var testDir : Path
+    private lateinit var testDir: Path
 
-    private lateinit var flowsCPKPath : Path
-    private lateinit var workflowCPKPath : Path
-    private lateinit var contractCPKPath : Path
-    private lateinit var testCPIPath : Path
-    private lateinit var contractCPK : CPK.Metadata
-    private lateinit var workflowCPK : CPK.Metadata
-    private lateinit var flowsCPK : CPK.Metadata
+    private lateinit var flowsCPKPath: Path
+    private lateinit var workflowCPKPath: Path
+    private lateinit var contractCPKPath: Path
+    private lateinit var testCPIPath: Path
+    private lateinit var contractCPK: CPK.Metadata
+    private lateinit var workflowCPK: CPK.Metadata
+    private lateinit var flowsCPK: CPK.Metadata
     private lateinit var testCPI: CPI
     private val cpiName = "Test CPI"
     private val cpiVersion = "1.5"
-    private lateinit var cpiHash : SecureHash
+    private lateinit var cpiHash: SecureHash
 
     @BeforeAll
-    fun setup(@TempDir junitTestDir : Path) {
+    fun setup(@TempDir junitTestDir: Path) {
         testDir = junitTestDir
 
         flowsCPKPath = Path.of(URI(System.getProperty("net.corda.flows.cpk")))
-        flowsCPK = CPK.Metadata.from(Files.newInputStream(flowsCPKPath), flowsCPKPath.toString())
+        flowsCPK = Files.newInputStream(flowsCPKPath).use { CPK.Metadata.from(it, flowsCPKPath.toString()) }
         testCPIPath = testDir.resolve("test.cpi")
         workflowCPKPath = Path.of(URI(System.getProperty("net.corda.packaging.test.workflow.cpk")))
         contractCPKPath = Path.of(URI(System.getProperty("net.corda.packaging.test.contract.cpk")))
-        contractCPK = CPK.Metadata.from(Files.newInputStream(contractCPKPath), contractCPKPath.toString())
-        workflowCPK = CPK.Metadata.from(Files.newInputStream(workflowCPKPath), workflowCPKPath.toString())
-        CPI.assemble(Files.newOutputStream(testCPIPath), cpiName, cpiVersion, listOf(workflowCPKPath, contractCPKPath))
+        contractCPK = Files.newInputStream(contractCPKPath).use { CPK.Metadata.from(it, contractCPKPath.toString()) }
+        workflowCPK = Files.newInputStream(workflowCPKPath).use { CPK.Metadata.from(it, workflowCPKPath.toString()) }
+        Files.newOutputStream(testCPIPath).use {
+            CPI.assemble(it, cpiName, cpiVersion, listOf(workflowCPKPath, contractCPKPath))
+        }
         val md = MessageDigest.getInstance(DigestAlgorithmName.SHA2_256.name)
         testCPI = DigestInputStream(Files.newInputStream(testCPIPath), md).use { inputStream ->
-             CPI.from(inputStream, testDir.resolve("cpi_expansion_dir"))
+            CPI.from(inputStream, testDir.resolve("cpi_expansion_dir"))
         }
         cpiHash = SecureHash(DigestAlgorithmName.SHA2_256.name, md.digest())
+    }
+
+    @AfterAll
+    fun teardown() {
+        testCPI.close()
     }
 
     @Test
