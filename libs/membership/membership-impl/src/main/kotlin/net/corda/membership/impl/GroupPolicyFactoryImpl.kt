5448bd6678740d58eccfc7cc939bcf9e78371137
Charlie Crean <charlie.crean@r3.com>
2021-12-09 12:32:37 +0000

    Added group policy factory interface and implementation for parsing the group policy from a json string

---
+++ libs/membership/membership-impl/src/main/kotlin/net/corda/membership/impl/GroupPolicyFactoryImpl.kt
@@ -0,0 +1,32 @@
+package net.corda.membership.impl
+
+import com.typesafe.config.ConfigException
+import com.typesafe.config.ConfigFactory
+import net.corda.membership.GroupPolicy
+import net.corda.membership.GroupPolicyFactory
+import net.corda.v5.base.util.contextLogger
+
+class GroupPolicyFactoryImpl : GroupPolicyFactory {
+    companion object {
+        private val logger = contextLogger()
+    }
+
+    override fun createGroupPolicy(groupPolicyJson: String): GroupPolicy {
+        return GroupPolicyImpl(
+            try {
+                if (groupPolicyJson.isBlank()) {
+                    logger.error("Group policy file is empty.")
+                    emptyMap()
+                } else {
+                    ConfigFactory
+                        .parseString(groupPolicyJson)
+                        .root()
+                        .unwrapped()
+                }
+            } catch (e: ConfigException.Parse) {
+                logger.error("Group policy file failed to parse. Check the file format.")
+                emptyMap()
+            }
+        )
+    }
+}
