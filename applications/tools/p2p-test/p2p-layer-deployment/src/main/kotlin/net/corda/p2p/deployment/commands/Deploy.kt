d0927e40a37cf7a61f4e593db51c4cff4ea1b91a
Yiftach Kaplan <yiftach.kaplan@r3.com>
2021-12-10 17:53:10 +0000

    Revert to working tag

--- applications/tools/p2p-test/p2p-layer-deployment/src/main/kotlin/net/corda/p2p/deployment/commands/Deploy.kt
+++ applications/tools/p2p-test/p2p-layer-deployment/src/main/kotlin/net/corda/p2p/deployment/commands/Deploy.kt
@@ -87,7 +87,7 @@ class Deploy : Runnable {
         names = ["-t", "--tag"],
         description = ["The docker name of the tag to pull"]
     )
-    private var tag = "5.0.0.0-beta-1639153101369"
+    private var tag = "5.0.0.0-beta-1639131609697"
 
     @Option(
         names = ["--lm-conf", "--link-manager-config"],
