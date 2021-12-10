2187b7cb41a5adf04dd9efcd2e06a9ad16c55978
Charlie Crean <charlie.crean@r3.com>
2021-12-10 17:31:40 +0000

    Initial commit of class outline for membership read component

---
+++ components/membership/membership-impl/src/main/kotlin/net/corda/membership/impl/read/MembershipGroupReaderImpl.kt
@@ -0,0 +1,34 @@
+package net.corda.membership.impl.read
+
+import net.corda.membership.CPIWhiteList
+import net.corda.membership.GroupPolicy
+import net.corda.membership.impl.read.cache.MemberListCache
+import net.corda.membership.read.MembershipGroupReader
+import net.corda.v5.membership.GroupParameters
+import net.corda.v5.membership.identity.MemberInfo
+import net.corda.v5.membership.identity.MemberX500Name
+
+class MembershipGroupReaderImpl(
+    override val groupId: String,
+    override val owningMember: MemberX500Name,
+    override val policy: GroupPolicy,
+    private val memberListCache: MemberListCache
+) : MembershipGroupReader {
+
+    private val memberList: List<MemberInfo>
+        get() = memberListCache.get(groupId, owningMember)
+
+    override val groupParameters: GroupParameters
+        get() = TODO("Not yet implemented")
+    override val cpiWhiteList: CPIWhiteList
+        get() = TODO("Not yet implemented")
+
+    override fun lookup(publicKeyHash: ByteArray): MemberInfo? {
+        TODO("Not yet implemented")
+    }
+
+    override fun lookup(name: MemberX500Name): MemberInfo? =
+        memberList.singleOrNull {
+            it.name == name
+        }
+}
---
+++ components/membership/membership-impl/src/main/kotlin/net/corda/membership/impl/read/cache/MemberDataCache.kt
@@ -0,0 +1,38 @@
+package net.corda.membership.impl.read.cache
+
+import net.corda.v5.membership.identity.MemberX500Name
+
+/**
+ * Interface for classes which are to be used as caches for group member data.
+ * This assumes data is stored and retrieved based on the holding identity of the member owning the data.
+ */
+interface MemberDataCache<T> {
+    /**
+     * Get a cached instance of [T], or null if no cached value exists.
+     * Cache lookups are done using a member's holding identity.
+     *
+     * @param groupId The membership group ID as a [String].
+     * @param memberX500Name The [MemberX500Name] of the member who owns the cached data.
+     */
+    fun get(groupId: String, memberX500Name: MemberX500Name): T?
+
+    /**
+     * Add a new, or update an existing cache entry which is an instance of [T].
+     * The member's holding identity is used for cache storage.
+     *
+     * @param groupId The membership group ID as a [String].
+     * @param memberX500Name The [MemberX500Name] of the member who owns the cached data.
+     * @param data The data to cache
+     */
+    fun put(groupId: String, memberX500Name: MemberX500Name, data: T)
+
+    /**
+     * Clear all in-memory data.
+     */
+    fun clear()
+}
+
+/**
+ * An extension of [MemberDataCache] which is used when the data to be cached for a member is a list of data.
+ */
+interface MemberDataListCache<T> : MemberDataCache<List<T>>
