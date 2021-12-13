f35258c62ad4bfae9063f114688ceb19b975f051
Charlie Crean <charlie.crean@r3.com>
2021-12-13 16:13:10 +0000

    Refactored the member data cache to be more generic. Was considering a group policy cache in front of the group policy parsing but group policy is currently only parsed when creating a group reader so the policy is already cached with that. If in the future we have different caching requirements for group policy we could probably use MemberDataCache for this.

--- components/membership/membership-persistence-impl/src/main/kotlin/net/corda/membership/impl/read/cache/MemberDataCache.kt
+++ components/membership/membership-persistence-impl/src/main/kotlin/net/corda/membership/impl/read/cache/MemberDataCache.kt
@@ -5,6 +5,7 @@ import net.corda.v5.membership.identity.MemberX500Name
 /**
  * Interface for classes which are to be used as caches for group member data.
  * This assumes data is stored and retrieved based on the holding identity of the member owning the data.
+ * Only one object of type [T] is cached per holding identity.
  */
 interface MemberDataCache<T> {
     /**
@@ -25,4 +26,23 @@ interface MemberDataCache<T> {
      * @param data The data to cache
      */
     fun put(groupId: String, memberX500Name: MemberX500Name, data: T)
+
+    /**
+     * Basic member data in-memory cache implementation.
+     * Data is stored as a map from group ID to map of member name to [T].
+     */
+    class Impl<T> : MemberDataCache<T> {
+
+        private val cache: MutableMap<String, MutableMap<MemberX500Name, T>> = mutableMapOf()
+
+        private fun getCachedData(groupId: String): MutableMap<MemberX500Name, T> =
+            cache.getOrPut(groupId) { mutableMapOf() }
+
+        override fun get(groupId: String, memberX500Name: MemberX500Name): T? =
+            getCachedData(groupId)[memberX500Name]
+
+        override fun put(groupId: String, memberX500Name: MemberX500Name, data: T) {
+            getCachedData(groupId)[memberX500Name] = data
+        }
+    }
 }
