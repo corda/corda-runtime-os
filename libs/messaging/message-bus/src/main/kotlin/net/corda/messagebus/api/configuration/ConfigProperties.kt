5cec57b3cf46fa2b811937bb8a981041ae4c4dd1
Ryan Fowler <ryan.fowler@r3.com>
2021-12-13 10:22:01 +0000

    CORE-2899: Add Message Bus API for restructuring pattern code.

--- libs/messaging/message-bus/src/main/kotlin/net/corda/messagebus/api/configuration/ConfigProperties.kt
+++ libs/messaging/message-bus/src/main/kotlin/net/corda/messagebus/api/configuration/ConfigProperties.kt
@@ -3,21 +3,19 @@ package net.corda.messagebus.api.configuration
 class ConfigProperties {
     companion object {
         const val TOPIC = "topic"
-        const val DEAD_LETTER_QUEUE_SUFFIX = "topic.deadLetterQueueSuffix"
+        const val TOPIC_PREFIX = "topic.prefix"
+        const val TOPIC_NAME = "topic.name"
+
         const val GROUP = "group"
+        const val GROUP_INSTANCE_ID = "group.instance.id"
+
         const val INSTANCE_ID = "instanceId"
         const val CLIENT_ID_COUNTER = "clientIdCounter"
 
-        const val TOPIC_PREFIX = "topic.prefix"
-        const val TOPIC_NAME = "topic.name"
         const val RESPONSE_TOPIC = "responseTopic"
 
-        const val KAFKA_PRODUCER = "producer"
-        const val KAFKA_CONSUMER = "consumer"
-        const val STATE_CONSUMER = "stateConsumer"
-        const val EVENT_CONSUMER = "eventConsumer"
-
-        const val MESSAGING_KAFKA = "messaging.kafka"
+        const val CORDA_PRODUCER = "producer"
+        const val CORDA_CONSUMER = "consumer"
 
         const val CLOSE_TIMEOUT = "close.timeout"
         const val PRODUCER_CLOSE_TIMEOUT = "producer.$CLOSE_TIMEOUT"
@@ -33,17 +31,9 @@ class ConfigProperties {
         const val COMMIT_OFFSET_MAX_RETRIES = "commit.retries"
 
         const val GROUP_ID = "consumer.id"
-        const val GROUP_INSTANCE_ID = "group.instance.id"
         const val PRODUCER_CLIENT_ID = "producer.client.id"
         const val CONSUMER_GROUP_ID = "consumer.group.id"
         const val CONSUMER_MAX_POLL_INTERVAL = "consumer.max.poll.interval.ms"
         const val PRODUCER_TRANSACTIONAL_ID = "producer.transactional.id"
-
-        const val STATE_TOPIC_NAME = "$STATE_CONSUMER.$TOPIC_NAME"
-
-        const val EVENT_GROUP_ID = "$EVENT_CONSUMER.$CONSUMER_GROUP_ID"
-        val EVENT_CONSUMER_THREAD_STOP_TIMEOUT = CONSUMER_THREAD_STOP_TIMEOUT.replace("consumer", "eventConsumer")
-        val EVENT_CONSUMER_POLL_AND_PROCESS_RETRIES = CONSUMER_POLL_AND_PROCESS_RETRIES.replace("consumer", "eventConsumer")
-        val EVENT_CONSUMER_CLOSE_TIMEOUT = CONSUMER_CLOSE_TIMEOUT.replace("consumer", "eventConsumer")
     }
 }
