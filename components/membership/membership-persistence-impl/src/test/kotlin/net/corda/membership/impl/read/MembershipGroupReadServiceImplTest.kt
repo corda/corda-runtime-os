a739686bf2428994f728d3fcb10ef2c5b217e6a6
Charlie Crean <charlie.crean@r3.com>
2021-12-13 15:12:41 +0000

    Tidy up

--- components/membership/membership-persistence-impl/src/test/kotlin/net/corda/membership/impl/read/MembershipGroupReadServiceImplTest.kt
+++ components/membership/membership-persistence-impl/src/test/kotlin/net/corda/membership/impl/read/MembershipGroupReadServiceImplTest.kt
@@ -6,6 +6,7 @@ import net.corda.messaging.api.processor.CompactedProcessor
 import net.corda.messaging.api.subscription.CompactedSubscription
 import net.corda.messaging.api.subscription.factory.SubscriptionFactory
 import net.corda.packaging.CPI
+import net.corda.v5.base.exceptions.CordaRuntimeException
 import net.corda.v5.membership.identity.MemberX500Name
 import net.corda.virtualnode.HoldingIdentity
 import net.corda.virtualnode.VirtualNodeInfo
@@ -18,6 +19,8 @@ import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.mockito.kotlin.any
+import org.mockito.kotlin.doAnswer
+import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.eq
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.times
@@ -26,74 +29,44 @@ import org.mockito.kotlin.whenever
 
 class MembershipGroupReadServiceImplTest {
 
-    private val cpiInfoReader: CpiInfoReader = mock()
-    private val virtualNodeInfoReader: VirtualNodeInfoReaderComponent = mock()
-    private val subscriptionFactory: SubscriptionFactory = mock()
-
     private lateinit var membershipGroupReadService: MembershipGroupReadServiceImpl
 
-    private val testGroupId = "GROUP_ID"
-    private val testMemberName = MemberX500Name("Alice", "London", "GB")
-    private var memberListSubIsRunning = false
+    private val groupId = "GROUP_ID"
+    private val memberName = MemberX500Name("Alice", "London", "GB")
 
-    /**
-     * Set mocks before each test.
-     */
+    private lateinit var cpiInfoReader: CpiInfoReader
+    private lateinit var virtualNodeInfoReader : VirtualNodeInfoReaderComponent
+    private lateinit var subscriptionFactory: SubscriptionFactory
+
     @BeforeEach
     fun setUp() {
+        configureMocks()
         membershipGroupReadService = MembershipGroupReadServiceImpl(
             cpiInfoReader,
             virtualNodeInfoReader,
             subscriptionFactory
         )
-        val cpi: CPI.Identifier = mock()
-        val virtualNodeInfo = VirtualNodeInfo(HoldingIdentity(testMemberName.toString(), testGroupId), cpi)
-        val cpiMetadata: CPI.Metadata = mock()
-        val mockMemberListSub: CompactedSubscription<String, SignedMemberInfo> = mock()
-
-        whenever(virtualNodeInfoReader.get(any())).thenReturn(virtualNodeInfo)
-        whenever(cpiInfoReader.get(eq(cpi))).thenReturn(cpiMetadata)
-        // default group policy to empty string
-        whenever(cpiMetadata.groupPolicy).thenReturn("")
-        whenever(
-            subscriptionFactory.createCompactedSubscription(
-                any(),
-                any<CompactedProcessor<String, SignedMemberInfo>>(),
-                any()
-            )
-        ).thenReturn(mockMemberListSub)
-        whenever(mockMemberListSub.isRunning).thenAnswer {
-            memberListSubIsRunning
-        }
-        whenever(mockMemberListSub.start()).thenAnswer {
-            memberListSubIsRunning = true
-            Unit
-        }
-        whenever(mockMemberListSub.stop()).thenAnswer {
-            memberListSubIsRunning = false
-            Unit
-        }
     }
 
     @Test
     fun `Group reader cannot be retrieved if service hasn't started yet`() {
-        assertThrows<NullPointerException> {
-            membershipGroupReadService.getGroupReader(testGroupId, testMemberName)
+        assertThrows<CordaRuntimeException> {
+            membershipGroupReadService.getGroupReader(groupId, memberName)
         }
     }
 
     @Test
     fun `Group reader can be retrieved if service has started`() {
         membershipGroupReadService.start()
-        membershipGroupReadService.getGroupReader(testGroupId, testMemberName)
+        membershipGroupReadService.getGroupReader(groupId, memberName)
     }
 
     @Test
     fun `Group reader cannot be retrieved if service has stopped`() {
         membershipGroupReadService.start()
         membershipGroupReadService.stop()
-        assertThrows<NullPointerException> {
-            membershipGroupReadService.getGroupReader(testGroupId, testMemberName)
+        assertThrows<CordaRuntimeException> {
+            membershipGroupReadService.getGroupReader(groupId, memberName)
         }
     }
 
@@ -118,18 +91,18 @@ class MembershipGroupReadServiceImplTest {
     @Test
     fun `Group reader is returned from the cache if already created`() {
         membershipGroupReadService.start()
-        val lookup1 = membershipGroupReadService.getGroupReader(testGroupId, testMemberName)
-        val lookup2 = membershipGroupReadService.getGroupReader(testGroupId, testMemberName)
+        val lookup1 = membershipGroupReadService.getGroupReader(groupId, memberName)
+        val lookup2 = membershipGroupReadService.getGroupReader(groupId, memberName)
         assertEquals(lookup1, lookup2)
     }
 
     @Test
     fun `Group reader cache is cleared after restarting the service`() {
         membershipGroupReadService.start()
-        val lookup1 = membershipGroupReadService.getGroupReader(testGroupId, testMemberName)
+        val lookup1 = membershipGroupReadService.getGroupReader(groupId, memberName)
         membershipGroupReadService.stop()
         membershipGroupReadService.start()
-        val lookup2 = membershipGroupReadService.getGroupReader(testGroupId, testMemberName)
+        val lookup2 = membershipGroupReadService.getGroupReader(groupId, memberName)
         assertNotEquals(lookup1, lookup2)
     }
 
@@ -160,22 +133,59 @@ class MembershipGroupReadServiceImplTest {
         membershipGroupReadService.start()
 
         // create and cache group reader
-        val reader1 = membershipGroupReadService.getGroupReader(testGroupId, testMemberName)
+        val reader1 = membershipGroupReadService.getGroupReader(groupId, memberName)
         // get reader from cache
-        val reader2 = membershipGroupReadService.getGroupReader(testGroupId, testMemberName)
+        val reader2 = membershipGroupReadService.getGroupReader(groupId, memberName)
         assertEquals(reader1, reader2)
 
         // handle config event
         membershipGroupReadService.handleConfigEvent(mock())
 
         // next reader should be new since cache should have been reset
-        val reader3 = membershipGroupReadService.getGroupReader(testGroupId, testMemberName)
+        val reader3 = membershipGroupReadService.getGroupReader(groupId, memberName)
         // get reader from cache
-        val reader4 = membershipGroupReadService.getGroupReader(testGroupId, testMemberName)
+        val reader4 = membershipGroupReadService.getGroupReader(groupId, memberName)
         assertEquals(reader3, reader4)
 
         // current reader should not equal previous reader from before handling the config
         assertNotEquals(reader1, reader3)
     }
 
+    @Test
+    fun `Service is running after handling a config event`() {
+        assertFalse(membershipGroupReadService.isRunning)
+        membershipGroupReadService.start()
+        assertTrue(membershipGroupReadService.isRunning)
+        membershipGroupReadService.handleConfigEvent(mock())
+        assertTrue(membershipGroupReadService.isRunning)
+    }
+
+    private fun configureMocks() {
+        var memberListSubIsRunning = false
+
+        val cpi: CPI.Identifier = mock()
+        val virtualNodeInfo = VirtualNodeInfo(HoldingIdentity(memberName.toString(), groupId), cpi)
+        val cpiMetadata = mock<CPI.Metadata>().apply {
+            doReturn("").whenever(this).groupPolicy
+        }
+        val mockMemberListSub = mock<CompactedSubscription<String, SignedMemberInfo>>().apply {
+            doAnswer { memberListSubIsRunning = true }.whenever(this).start()
+            doAnswer { memberListSubIsRunning = false }.whenever(this).stop()
+            doAnswer { memberListSubIsRunning }.whenever(this).isRunning
+        }
+
+        cpiInfoReader = mock<CpiInfoReader>().apply {
+            doReturn(cpiMetadata).whenever(this).get(eq(cpi))
+        }
+        virtualNodeInfoReader = mock<VirtualNodeInfoReaderComponent>().apply {
+            doReturn(virtualNodeInfo).whenever(this).get(any())
+        }
+        subscriptionFactory = mock<SubscriptionFactory>().apply {
+            doReturn(mockMemberListSub).whenever(this).createCompactedSubscription(
+                any(),
+                any<CompactedProcessor<String, SignedMemberInfo>>(),
+                any()
+            )
+        }
+    }
 }
