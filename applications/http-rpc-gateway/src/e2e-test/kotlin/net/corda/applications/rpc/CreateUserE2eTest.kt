7512ed9eea0c9e39c682714048ca33032db7eea0
Viktor Kolomeyko <Viktor.Kolomeyko@r3.com>
2021-12-15 10:07:45 +0000

    CORE-1976: Directory structure for E2E tests

---
+++ applications/http-rpc-gateway/src/e2e-test/kotlin/net/corda/applications/rpc/CreateUserE2eTest.kt
@@ -0,0 +1,12 @@
+package net.corda.applications.rpc
+
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+class CreateUserE2eTest {
+
+    @Test
+    fun test() {
+        assertTrue(true)
+    }
+}
