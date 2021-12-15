ab9e13e53c5d98e0062160920cb3635f43babfeb
Viktor Kolomeyko <Viktor.Kolomeyko@r3.com>
2021-12-15 12:21:22 +0000

    CORE-1976: Finish the first test

--- applications/http-rpc-gateway/src/e2e-test/kotlin/net/corda/applications/rpc/CreateUserE2eTest.kt
+++ applications/http-rpc-gateway/src/e2e-test/kotlin/net/corda/applications/rpc/CreateUserE2eTest.kt
@@ -3,8 +3,12 @@ package net.corda.applications.rpc
 import net.corda.applications.rpc.http.TestHttpInterfaceProperty
 import net.corda.httprpc.client.exceptions.MissingRequestedResourceException
 import net.corda.libs.permissions.endpoints.v1.user.UserEndpoint
+import net.corda.libs.permissions.endpoints.v1.user.types.CreateUserType
 import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.assertj.core.api.SoftAssertions.assertSoftly
 import org.junit.jupiter.api.Test
+import java.time.Instant
+import java.time.temporal.ChronoUnit.DAYS
 
 class CreateUserE2eTest {
 
@@ -12,13 +16,31 @@ class CreateUserE2eTest {
 
     @Test
     fun testCreateAndGet() {
-        httpInterface.clientFor(UserEndpoint::class.java).use {
+        httpInterface.clientFor(UserEndpoint::class.java).use { client ->
             val userName = httpInterface.uniqueName
-            val proxy = it.start().proxy
+            val proxy = client.start().proxy
 
             // Check the user does not exist yet
             assertThatThrownBy { proxy.getUser(userName) }.isInstanceOf(MissingRequestedResourceException::class.java)
                 .hasMessageContaining("$userName not found")
+
+            // Create user
+            val password = httpInterface.uniqueName
+            val passwordExpirySet = Instant.now().plus(1, DAYS).truncatedTo(DAYS)
+            with(proxy.createUser(CreateUserType(userName, userName, true, password, passwordExpirySet, null))) {
+                assertSoftly {
+                    it.assertThat(loginName).isEqualTo(userName)
+                    it.assertThat(passwordExpiry).isEqualTo(passwordExpirySet)
+                }
+            }
+
+            // Check the user does exist now
+            with(proxy.getUser(userName)) {
+                assertSoftly {
+                    it.assertThat(loginName).isEqualTo(userName)
+                    it.assertThat(passwordExpiry).isEqualTo(passwordExpirySet)
+                }
+            }
         }
     }
 }
