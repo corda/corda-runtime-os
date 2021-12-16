a059f0e969fd0763b3c90b6a0af420859d9790b5
Conal Smith <conal.smith@r3.com>
2021-12-16 14:51:45 +0000

    CORE-1978 Add e2e test for role creation and role not already exist validation

---
+++ applications/http-rpc-gateway/src/e2e-test/kotlin/net/corda/applications/rpc/CreateRoleE2eTest.kt
@@ -0,0 +1,57 @@
+package net.corda.applications.rpc
+
+import net.corda.applications.rpc.http.TestToolkitProperty
+import net.corda.httprpc.client.exceptions.InternalErrorException
+import net.corda.libs.permissions.endpoints.v1.role.RoleEndpoint
+import net.corda.libs.permissions.endpoints.v1.role.types.CreateRoleType
+import net.corda.test.util.eventually
+import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.assertj.core.api.SoftAssertions.assertSoftly
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
+
+class CreateRoleE2eTest {
+
+    private val testToolkit by TestToolkitProperty()
+
+    @Test
+    fun testCreateAndGet() {
+        testToolkit.httpClientFor(RoleEndpoint::class.java).use { client ->
+            val name = testToolkit.uniqueName
+            val proxy = client.start().proxy
+
+            // Create role
+            val createRoleType = CreateRoleType(name, null)
+
+            val roleId = with(proxy.createRole(createRoleType)) {
+                assertSoftly {
+                    it.assertThat(roleName).isEqualTo(name)
+                    it.assertThat(version).isEqualTo(0L)
+                    it.assertThat(groupVisibility).isNull()
+                    it.assertThat(permissions).isEmpty()
+                }
+                id
+            }
+
+            // Check the role does exist now. The distribution of Role record may take some time to complete on the
+            // message bus, hence use of `eventually` along with `assertDoesNotThrow`.
+            eventually {
+                assertDoesNotThrow {
+                    with(proxy.getRole(roleId)) {
+                        assertSoftly {
+                            it.assertThat(roleName).isEqualTo(name)
+                            it.assertThat(version).isEqualTo(0L)
+                            it.assertThat(groupVisibility).isNull()
+                            it.assertThat(permissions).isEmpty()
+                        }
+                    }
+                }
+            }
+
+            // Try to create a role with the same name again and verify that it fails
+            assertThatThrownBy { proxy.createRole(CreateRoleType(name, null)) }
+                .isInstanceOf(InternalErrorException::class.java)
+                .hasMessageContaining("Failed to create new role: $name as they already exist")
+        }
+    }
+}
