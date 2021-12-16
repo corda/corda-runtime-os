c40327b7a0c2042aa491346097a70875a8e7e871
Emily Bowe <emily.bowe@r3.com>
2021-12-16 20:44:02 +0000

    tests passing to address not initiating sessions or replying to session initiations from peers for empty partitions

--- components/link-manager/src/test/kotlin/net/corda/p2p/linkmanager/LinkManagerTest.kt
+++ components/link-manager/src/test/kotlin/net/corda/p2p/linkmanager/LinkManagerTest.kt
@@ -1025,7 +1025,7 @@ class LinkManagerTest {
     }
 
     @Test
-    fun `DRAFTOutboundMessageProcessor produces only a LinkManagerSentMarker if partitions empty`() {
+    fun `OutboundMessageProcessor gives warning if partitions empty`() {
         val mockSessionManager = Mockito.mock(SessionManager::class.java)
         val sessionInitMessage = LinkOutMessage()
         Mockito.`when`(mockSessionManager.processOutboundMessage(any())).thenReturn(
@@ -1036,19 +1036,23 @@ class LinkManagerTest {
             mockSessionManager,
             hostingMap,
             netMap,
-            assignedListener(listOf(1)),
+            assignedListener(emptyList()),
         )
-        val messages = listOf(EventLogRecord(TOPIC, KEY, AppMessage(authenticatedMessage(
-            FIRST_SOURCE, FIRST_DEST, "0", MESSAGE_ID)), 0, 0)
+        val messages = listOf(
+            EventLogRecord(
+                TOPIC, KEY, AppMessage(
+                    authenticatedMessage(
+                        FIRST_SOURCE, FIRST_DEST, "0", MESSAGE_ID
+                    )
+                ), 0, 0
+            )
         )
         val records = processor.onNext(messages)
-
         assertThat(records).hasSize(messages.size)
 
-        assertThat(records).filteredOn { it.topic == P2P_OUT_MARKERS }.hasSize(messages.size)
-            .allSatisfy { assertThat(it.key).isEqualTo(MESSAGE_ID) }
-            .extracting<AppMessageMarker> { it.value as AppMessageMarker }
-            .allSatisfy { assertThat(it.marker).isInstanceOf(LinkManagerSentMarker::class.java) }
+        loggingInterceptor.assertSingleWarning(
+            "No partitions exist"
+        )
     }
 
 }
