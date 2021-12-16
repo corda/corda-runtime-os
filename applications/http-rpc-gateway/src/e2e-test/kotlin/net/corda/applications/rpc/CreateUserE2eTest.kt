3f9b306277a237fc9783ee81dff27cc168870844
Viktor Kolomeyko <viktor.kolomeyko@r3.com>
2021-12-16 12:56:32 +0000

    CORE-1976: Extend create `User` E2E test (#593)
    By checking that the user with the same login name cannot be created.
    Also make the test more stable by using `eventually` on the inherently asynchronous operation.

--- applications/http-rpc-gateway/src/e2e-test/kotlin/net/corda/applications/rpc/CreateUserE2eTest.kt
+++ applications/http-rpc-gateway/src/e2e-test/kotlin/net/corda/applications/rpc/CreateUserE2eTest.kt
@@ -1,12 +1,15 @@
 package net.corda.applications.rpc
 
 import net.corda.applications.rpc.http.TestToolkitProperty
+import net.corda.httprpc.client.exceptions.InternalErrorException
 import net.corda.httprpc.client.exceptions.MissingRequestedResourceException
 import net.corda.libs.permissions.endpoints.v1.user.UserEndpoint
 import net.corda.libs.permissions.endpoints.v1.user.types.CreateUserType
+import net.corda.test.util.eventually
 import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.assertj.core.api.SoftAssertions.assertSoftly
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
 import java.time.Instant
 import java.time.temporal.ChronoUnit.DAYS
 
@@ -22,26 +25,37 @@ class CreateUserE2eTest {
 
             // Check the user does not exist yet
             assertThatThrownBy { proxy.getUser(userName) }.isInstanceOf(MissingRequestedResourceException::class.java)
-                .hasMessageContaining("$userName not found")
+                .hasMessageContaining("User $userName not found")
 
             // Create user
             val password = testToolkit.uniqueName
             val passwordExpirySet = Instant.now().plus(1, DAYS).truncatedTo(DAYS)
-            with(proxy.createUser(
-                    CreateUserType(userName, userName, true, password, passwordExpirySet, null))) {
+            val createUserType = CreateUserType(userName, userName, true, password, passwordExpirySet, null)
+
+            with(proxy.createUser(createUserType)) {
                 assertSoftly {
                     it.assertThat(loginName).isEqualTo(userName)
                     it.assertThat(passwordExpiry).isEqualTo(passwordExpirySet)
                 }
             }
 
-            // Check the user does exist now
-            with(proxy.getUser(userName)) {
-                assertSoftly {
-                    it.assertThat(loginName).isEqualTo(userName)
-                    it.assertThat(passwordExpiry).isEqualTo(passwordExpirySet)
+            // Check the user does exist now. The distribution of User record may take some time to complete on the
+            // message bus, hence use of `eventually` along with `assertDoesNotThrow`.
+            eventually {
+                assertDoesNotThrow {
+                    with(proxy.getUser(userName)) {
+                        assertSoftly {
+                            it.assertThat(loginName).isEqualTo(userName)
+                            it.assertThat(passwordExpiry).isEqualTo(passwordExpirySet)
+                        }
+                    }
                 }
             }
+
+            // Try to create a user with the same login name again and verify that it fails
+            assertThatThrownBy { proxy.createUser(createUserType.copy(fullName = "Alice")) }
+                .isInstanceOf(InternalErrorException::class.java)
+                .hasMessageContaining("Failed to create new user: $userName as they already exist")
         }
     }
 }
