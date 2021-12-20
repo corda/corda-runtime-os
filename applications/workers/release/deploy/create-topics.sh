c7c69cb4acba8264e9725e1a7fd8a0cca7a3d8e8
joeldudleyr3 <joel.dudley@r3.com>
2021-12-20 18:00:30 +0000

    Cleans up the worker deploy scripts.

--- applications/workers/release/deploy/create-topics.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-kafka_command='docker exec kafka /opt/bitnami/kafka/bin/kafka-topics.sh --bootstrap-server kafka:9092'
-kafka_flags="--partitions 1 --replication-factor 1 --create --topic"
-
-echo "Creating topics."
-$kafka_command $kafka_flags config-update-request --config "cleanup.policy=compact"
-$kafka_command $kafka_flags config --config "cleanup.policy=compact"
--- applications/workers/release/deploy/delete-topics.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-kafka_command='docker exec kafka /opt/bitnami/kafka/bin/kafka-topics.sh --bootstrap-server kafka:9092'
-
-echo "Deleting topics."
-$kafka_command --delete --topic "config"
-$kafka_command --delete --topic "config-update-request"
