b0fe8bbbb11ea657eec685813bfffb667fdafc4c
Viktor Kolomeyko <Viktor.Kolomeyko@r3.com>
2021-12-29 12:57:18 +0000

    CORE-1980: Add changes to persistence layer

--- libs/permissions/permission-storage-writer-impl/src/main/kotlin/net/corda/libs/permissions/storage/writer/impl/role/impl/RoleWriterImpl.kt
+++ libs/permissions/permission-storage-writer-impl/src/main/kotlin/net/corda/libs/permissions/storage/writer/impl/role/impl/RoleWriterImpl.kt
@@ -1,16 +1,20 @@
 package net.corda.libs.permissions.storage.writer.impl.role.impl
 
+import net.corda.data.permissions.management.role.AddPermissionToRoleRequest
 import java.time.Instant
 import java.util.UUID
 import javax.persistence.EntityManagerFactory
 import net.corda.data.permissions.management.role.CreateRoleRequest
+import net.corda.data.permissions.management.role.RemovePermissionFromRoleRequest
 import net.corda.libs.permissions.storage.common.converter.toAvroRole
 import net.corda.libs.permissions.storage.writer.impl.role.RoleWriter
 import net.corda.orm.utils.transaction
 import net.corda.permissions.model.ChangeAudit
 import net.corda.permissions.model.Group
+import net.corda.permissions.model.Permission
 import net.corda.permissions.model.RPCPermissionOperation
 import net.corda.permissions.model.Role
+import net.corda.permissions.model.RolePermissionAssociation
 import net.corda.v5.base.util.contextLogger
 import net.corda.v5.base.util.debug
 import net.corda.data.permissions.Role as AvroRole
@@ -59,6 +63,84 @@ class RoleWriterImpl(
             entityManager.persist(auditLog)
 
             log.info("Successfully created new role: $roleName.")
+
+            role.toAvroRole()
+        }
+    }
+
+    override fun addPermissionToRole(request: AddPermissionToRoleRequest, requestUserId: String): AvroRole {
+
+        log.debug { "Received request to add permission to a role: $request" }
+
+        return entityManagerFactory.transaction { entityManager ->
+
+            val role = requireNotNull(entityManager.find(Role::class.java, request.roleId)) {
+                "Unable to find Role with Id: ${request.roleId}"
+            }
+
+            val permission = requireNotNull(entityManager.find(Permission::class.java, request.permissionId)) {
+                "Unable to find Permission with Id: ${request.permissionId}"
+            }
+
+            val updateTimestamp = Instant.now()
+
+            val rolePermissionAssociation =
+                RolePermissionAssociation(UUID.randomUUID().toString(), role, permission, updateTimestamp)
+            role.rolePermAssociations.add(rolePermissionAssociation)
+
+            // Note: We are not making checks whether a permission is already associated with a role.
+            // Should this be the case, there is a unique constraint (`rpc_role_perm_rel_uc1`) which will not let
+            // this happen.
+            entityManager.merge(role)
+
+            val auditLog = ChangeAudit(
+                id = UUID.randomUUID().toString(),
+                updateTimestamp = updateTimestamp,
+                actorUser = requestUserId,
+                changeType = RPCPermissionOperation.ADD_PERMISSION_TO_ROLE,
+                details = "Role '${role.id}' got permission assigned '${permission.id}' by '$requestUserId'."
+            )
+
+            entityManager.persist(auditLog)
+
+            log.info("Successfully added permission to a role: $request.")
+
+            role.toAvroRole()
+        }
+    }
+
+    override fun removePermissionFromRole(request: RemovePermissionFromRoleRequest, requestUserId: String): AvroRole {
+        log.debug { "Received request to remove permission from a role: $request" }
+
+        return entityManagerFactory.transaction { entityManager ->
+
+            val role = requireNotNull(entityManager.find(Role::class.java, request.roleId)) {
+                "Unable to find Role with Id: ${request.roleId}"
+            }
+
+            val rolePermissionAssociation =
+                requireNotNull(role.rolePermAssociations.find { it.permission.id == request.permissionId }) {
+                    "Permission with Id: ${request.permissionId} is not associated with a role: ${role.id}."
+                }
+
+            val updateTimestamp = Instant.now()
+
+            role.rolePermAssociations.remove(rolePermissionAssociation)
+
+            entityManager.merge(role)
+            entityManager.remove(rolePermissionAssociation)
+
+            val auditLog = ChangeAudit(
+                id = UUID.randomUUID().toString(),
+                updateTimestamp = updateTimestamp,
+                actorUser = requestUserId,
+                changeType = RPCPermissionOperation.DELETE_PERMISSION_FROM_ROLE,
+                details = "Role '${role.id}' got permission removed '${request.permissionId}' by '$requestUserId'."
+            )
+
+            entityManager.persist(auditLog)
+
+            log.info("Successfully removed permission from a role: $request.")
 
             role.toAvroRole()
         }
