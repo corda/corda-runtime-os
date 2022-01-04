ec3e0660f58eb6469cdd7d9e6c0bc387dbbea477
Emily Bowe <emily.bowe@r3.com>
2022-01-04 17:19:49 +0000

    tidy up message, tests passing

--- components/link-manager/src/main/kotlin/net/corda/p2p/linkmanager/LinkManager.kt
+++ components/link-manager/src/main/kotlin/net/corda/p2p/linkmanager/LinkManager.kt
@@ -274,7 +274,8 @@ class LinkManager(@Reference(service = SubscriptionFactory::class)
         private fun recordsForNewSession(state: SessionState.NewSessionNeeded): List<Record<String, *>> {
             val partitions = inboundAssignmentListener.getCurrentlyAssignedPartitions(LINK_IN_TOPIC).toList()
             return if(partitions.isEmpty()) {
-                logger.warn("No partitions from topic ${LINK_IN_TOPIC} are currently assigned to the inbound message processor.")
+                logger.warn("No partitions from topic $LINK_IN_TOPIC are currently assigned to the inbound message processor." +
+                        " Session ${state.sessionId} will not be initiated.")
                 emptyList()
             } else {
                 val records = mutableListOf<Record<String, *>>()
