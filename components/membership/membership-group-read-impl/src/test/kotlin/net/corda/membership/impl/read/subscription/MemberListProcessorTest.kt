555f41cafa09406412183078aee80c7bb5ec335b
Charlie Crean <69511999+charlieR3@users.noreply.github.com>
2022-01-12 16:47:09 +0000

    CORE-2931 Kafka based implementation of member info read service (#565)

---
+++ components/membership/membership-group-read-impl/src/test/kotlin/net/corda/membership/impl/read/subscription/MemberListProcessorTest.kt
@@ -0,0 +1,29 @@
+package net.corda.membership.impl.read.subscription
+
+import net.corda.data.membership.SignedMemberInfo
+import net.corda.membership.impl.read.cache.MembershipGroupReadCache
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.mockito.kotlin.mock
+
+class MemberListProcessorTest {
+    private lateinit var memberListProcessor: MemberListProcessor
+
+    private val membershipGroupReadCache: MembershipGroupReadCache = mock()
+
+    @BeforeEach
+    fun setUp() {
+        memberListProcessor = MemberListProcessor(membershipGroupReadCache)
+    }
+
+    @Test
+    fun `Key class is String`() {
+        assertEquals(String::class.java, memberListProcessor.keyClass)
+    }
+
+    @Test
+    fun `Value class is SignedMemberInfo`() {
+        assertEquals(SignedMemberInfo::class.java, memberListProcessor.valueClass)
+    }
+}
