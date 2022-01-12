b22ac677d2940334540b4bda84fb37bb6044520a
Viktor Kolomeyko <Viktor.Kolomeyko@r3.com>
2022-01-12 13:26:00 +0000

    CORE-3217: Adjust DB constants

--- libs/permissions/permission-storage-common/src/main/kotlin/net/corda/libs/permissions/storage/common/ConfigKeys.kt
+++ libs/permissions/permission-storage-common/src/main/kotlin/net/corda/libs/permissions/storage/common/ConfigKeys.kt
@@ -3,8 +3,9 @@ package net.corda.libs.permissions.storage.common
 object ConfigKeys {
     const val BOOTSTRAP_CONFIG = "corda.boot"
 
-    const val DB_CONFIG_KEY = "db_config"
-    const val DB_URL = "db_url"
-    const val DB_USER = "db_user"
-    const val DB_PASSWORD = "db_password"
+    // Similar to `net.corda.processors.db.internal.ConstantsKt.CONFIG_JDBC_URL`
+    const val DB_CONFIG_KEY = "database.cluster"
+    const val DB_URL = "jdbc.url"
+    const val DB_USER = "user"
+    const val DB_PASSWORD = "pass"
 }
