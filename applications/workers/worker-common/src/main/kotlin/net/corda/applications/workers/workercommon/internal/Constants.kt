0965e741fd41d3c065580d61085d4ae6f07628aa
Owen Stanford <92725587+owenstanford@users.noreply.github.com>
2022-01-12 17:39:33 +0000

    CORE-3223 removed option for configuring the config.topic. (#654)
    * CORE-3223 - Removed duplication of configuration keys
    - Replaced optional config.topic configuration with fixed value the API topic schemas
    - Fixed configuration issues preventing the flow worker demo app from running
    - Fixed minor issue in the file based CPK cache causing file locking on Windows.

--- applications/workers/worker-common/src/main/kotlin/net/corda/applications/workers/workercommon/internal/Constants.kt
+++ applications/workers/worker-common/src/main/kotlin/net/corda/applications/workers/workercommon/internal/Constants.kt
@@ -6,7 +6,5 @@ internal const val HTTP_HEALTH_ROUTE = "/isHealthy"
 internal const val HTTP_READINESS_ROUTE = "/isReady"
 internal const val HEALTH_MONITOR_PORT = 7000
 
-internal const val INSTANCE_ID_PATH = "instanceId"
-internal const val TOPIC_PREFIX_PATH = "topicPrefix"
 internal const val MSG_CONFIG_PATH = "messaging"
 internal const val CUSTOM_CONFIG_PATH = "custom"
