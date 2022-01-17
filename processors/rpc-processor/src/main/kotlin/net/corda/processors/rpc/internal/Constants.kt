ade45dc7d95b88c57c1e279bff99aa0808796e07
Viktor Kolomeyko <viktor.kolomeyko@r3.com>
2022-01-17 15:31:05 +0000

    CORE-3217:  Align configuration keys (#694)
    Aligns the use of HTTP RPC timeout value across Config operations and RPC RBAC.

--- processors/rpc-processor/src/main/kotlin/net/corda/processors/rpc/internal/Constants.kt
+++ processors/rpc-processor/src/main/kotlin/net/corda/processors/rpc/internal/Constants.kt
@@ -3,7 +3,10 @@ package net.corda.processors.rpc.internal
 internal val CONFIG_HTTP_RPC =
     """address="0.0.0.0:8888"
         context.description="Exposing RPCOps interfaces as OpenAPI WebServices"
-        context.title="HTTP RPC"""".trimIndent()
-internal const val CONFIG_CONFIG_MGMT_REQUEST_TIMEOUT = "timeout.millis=\"10000\""
+        context.title="HTTP RPC"
+        sso.azureAd.clientId="f4a37d97-c561-4367-97fd-b10d44ceae24"
+        sso.azureAd.tenantId="a4be1f2e-2d10-4195-87cd-736aca9b672c"
+        maxContentLength=100000
+        endpoint.timeoutMs=12000""".trimIndent()
 
 internal const val CLIENT_ID_RPC_PROCESSOR = "rpc.processor"
