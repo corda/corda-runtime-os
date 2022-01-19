39aaf26a6873da198aebd5ba90b9306253e096c2
James Higgs <45565019+JamesHR3@users.noreply.github.com>
2022-01-19 14:12:29 +0000

    CORE-3485: Remove out of date example code (#715)
    * CORE-3485: Remove all examples from components and libs, most from applications

--- applications/tools/config-reader/src/main/java/net/corda/tools/config/reader/package-info.java
+++
@@ -1,4 +0,0 @@
-@Export
-package net.corda.tools.config.reader;
-
-import org.osgi.annotation.bundle.Export;
--- components/examples/config-reader/src/main/java/net/corda/components/examples/config/reader/package-info.java
+++
@@ -1,4 +0,0 @@
-@Export
-package net.corda.components.examples.config.reader;
-
-import org.osgi.annotation.bundle.Export;
