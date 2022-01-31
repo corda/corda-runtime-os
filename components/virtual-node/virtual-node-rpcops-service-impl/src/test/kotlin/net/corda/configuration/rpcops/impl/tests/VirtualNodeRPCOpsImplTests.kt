d4f1e03e0a89f8ae06a23ede9d3cb46a06ab99e7
Kyriakos Tharrouniatis <kyriakos.tharrouniatis@r3.com>
2022-01-31 10:10:49 +0000

    Bring back deleted tests

--- components/virtual-node/virtual-node-rpcops-service-impl/src/test/kotlin/net/corda/configuration/rpcops/impl/tests/VirtualNodeRPCOpsImplTests.kt
+++ components/virtual-node/virtual-node-rpcops-service-impl/src/test/kotlin/net/corda/configuration/rpcops/impl/tests/VirtualNodeRPCOpsImplTests.kt
@@ -20,6 +20,7 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.mockito.kotlin.any
 import org.mockito.kotlin.mock
+import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
 import java.nio.ByteBuffer
 import java.time.Duration
@@ -43,6 +44,35 @@ class VirtualNodeRPCOpsImplTests {
         "holdingIdHash"
     )
     private val httpRequestTimeoutDuration = Duration.ofMillis(1000.toLong())
+
+//    @Test
+//    fun `createAndStartRPCSender starts new RPC sender`() {
+//        val (rpcSender, vnodeRPCOps) = getVirtualNodeRPCOps()
+//
+//        vnodeRPCOps.createAndStartRPCSender(mock())
+//
+//        verify(rpcSender).start()
+//    }
+
+//    @Test
+//    fun `createAndStartRPCSender closes existing RPC sender if one exists`() {
+//        val (rpcSender, vnodeRPCOps) = getVirtualNodeRPCOps()
+//
+//        vnodeRPCOps.createAndStartRPCSender(mock())
+//        vnodeRPCOps.createAndStartRPCSender(mock())
+//
+//        verify(rpcSender).close()
+//    }
+
+//    @Test
+//    fun `stop closes existing RPC sender if one exists`() {
+//        val (rpcSender, vnodeRPCOps) = getVirtualNodeRPCOps()
+//
+//        vnodeRPCOps.createAndStartRPCSender(mock())
+//        vnodeRPCOps.stop()
+//
+//        verify(rpcSender).close()
+//    }
 
     @Test
     fun `createVirtualNode throws HttpApiException if response is failure`() {
