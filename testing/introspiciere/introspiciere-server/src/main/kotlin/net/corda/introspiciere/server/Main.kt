8c55562a41b8d3af352408875f719b6b763f7fbd
David Beales <david.beales@r3.com>
2022-02-02 21:32:48 +0000

    Update for corda cli 1.6.0-beta

--- testing/introspiciere/introspiciere-server/src/main/kotlin/net/corda/introspiciere/server/Main.kt
+++ testing/introspiciere/introspiciere-server/src/main/kotlin/net/corda/introspiciere/server/Main.kt
@@ -9,17 +9,27 @@ import net.corda.introspiciere.core.addidentity.CryptoKeySenderImpl
 
 fun main() {
     val app = Javalin.create().start(7070)
-
-    val servers = System.getenv("KAFKA_BROKERS")?.split(",") ?: listOf("kafka-service:9092")
+    val servers = System.getenv("KAFKA_BROKERS")?.split(",") ?: listOf("alpha-bk-1:9092")
     val kafka = SimpleKafkaClient(servers)
 
     app.get("/helloworld") { ctx ->
         try {
             val greeting = HelloWorld().greeting()
             ctx.result(greeting)
-        } catch (ex: Exception) {
+        } catch (t: Throwable) {
             throw InternalServerErrorResponse(details = mapOf(
-                "exception" to ex.stackTraceToString()
+                "Exception" to t.stackTraceToString()
+            ))
+        }
+    }
+
+    app.get("/topics") { ctx ->
+        try {
+            val topics = kafka.fetchTopics()
+            ctx.result(topics)
+        } catch (t: Throwable) {
+            throw InternalServerErrorResponse(details = mapOf(
+                "Exception" to t.stackTraceToString()
             ))
         }
     }
@@ -29,9 +39,9 @@ fun main() {
             val input = ctx.bodyAsClass<CreateKeysAndAddIdentityInteractor.Input>()
             CreateKeysAndAddIdentityInteractor(CryptoKeySenderImpl(kafka)).execute(input)
             ctx.result("OK")
-        } catch (ex: Exception) {
+        } catch (t: Throwable) {
             throw InternalServerErrorResponse(details = mapOf(
-                "exception" to ex.stackTraceToString()
+                "Exception" to t.stackTraceToString()
             ))
         }
     }
