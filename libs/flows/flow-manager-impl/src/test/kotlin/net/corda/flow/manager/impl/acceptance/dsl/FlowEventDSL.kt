e0758d924167b3e343af0e865edbcd40a89e0daf
Dan Newton <dan.newton@r3.com>
2022-02-02 15:00:14 +0000

    CORE-3301 Remove `FlowEventDSL` interface

--- libs/flows/flow-manager-impl/src/test/kotlin/net/corda/flow/manager/impl/acceptance/dsl/FlowEventDSL.kt
+++ libs/flows/flow-manager-impl/src/test/kotlin/net/corda/flow/manager/impl/acceptance/dsl/FlowEventDSL.kt
@@ -5,36 +5,12 @@ import net.corda.data.flow.state.Checkpoint
 import net.corda.messaging.api.processor.StateAndEventProcessor
 import net.corda.messaging.api.records.Record
 import net.corda.schema.Schemas
-import java.util.UUID
 
 fun flowEventDSL(dsl: FlowEventDSL.() -> Unit) {
-    FlowEventDSLImpl().run(dsl)
+    FlowEventDSL().run(dsl)
 }
 
-// need to take the output of a processing step and process any flow events for each reprocess marker there is
-interface FlowEventDSL {
-
-    // currently im adding the flow event
-    // might want to switch to allowing input payload
-    // if doing payload i need to pass in flow key or flow id for simplicity
-    fun input(event: FlowEvent)
-
-    fun inputLastOutputEvent()
-
-    fun flowFiber(fiber: MockFlowFiber): MockFlowFiber
-
-    fun flowFiber(flowId: String = UUID.randomUUID().toString(), fiber: MockFlowFiber.() -> Unit): MockFlowFiber
-
-    fun startedFlowFiber(flowId: String = UUID.randomUUID().toString(), fiber: MockFlowFiber.() -> Unit): MockFlowFiber
-
-    fun processOne(): StateAndEventProcessor.Response<Checkpoint>
-
-    // could return just the last state instead if that is more useful
-    fun processAll(): List<StateAndEventProcessor.Response<Checkpoint>>
-}
-
-// not thread safe
-class FlowEventDSLImpl : FlowEventDSL {
+class FlowEventDSL {
 
     private val processor = mockFlowEventProcessor()
 
@@ -43,34 +19,34 @@ class FlowEventDSLImpl : FlowEventDSL {
     private var checkpoints = mutableMapOf<String, Checkpoint>()
     private var outputFlowEvents = mutableListOf<FlowEvent>()
 
-    override fun input(event: FlowEvent) {
+    fun input(event: FlowEvent) {
         inputFlowEvents += event
     }
 
-    override fun inputLastOutputEvent() {
+    fun inputLastOutputEvent() {
         inputFlowEvents += ProcessLastOutputFlowEvent
     }
 
-    override fun flowFiber(fiber: MockFlowFiber): MockFlowFiber {
+    fun flowFiber(fiber: MockFlowFiber): MockFlowFiber {
         processor.addFlowFiber(fiber)
         return fiber
     }
 
-    override fun flowFiber(flowId: String, fiber: MockFlowFiber.() -> Unit): MockFlowFiber {
+    fun flowFiber(flowId: String, fiber: MockFlowFiber.() -> Unit): MockFlowFiber {
         return MockFlowFiber(flowId).apply {
             fiber(this)
             processor.addFlowFiber(this)
         }
     }
 
-    override fun startedFlowFiber(flowId: String, fiber: MockFlowFiber.() -> Unit): MockFlowFiber {
+    fun startedFlowFiber(flowId: String, fiber: MockFlowFiber.() -> Unit): MockFlowFiber {
         val (mockFlowFiber, response) = processor.startFlow(flowId)
         updateDSLStateWithEventResponse(flowId, response)
         fiber(mockFlowFiber)
         return mockFlowFiber
     }
 
-    override fun processOne(): StateAndEventProcessor.Response<Checkpoint> {
+    fun processOne(): StateAndEventProcessor.Response<Checkpoint> {
         val event = when (val input = checkNotNull(inputFlowEvents.removeFirstOrNull()) { "No input flow events have been setup" }) {
             ProcessLastOutputFlowEvent -> {
                 checkNotNull(outputFlowEvents.removeFirstOrNull()) {
@@ -87,7 +63,7 @@ class FlowEventDSLImpl : FlowEventDSL {
         ).also { updateDSLStateWithEventResponse(flowId, it) }
     }
 
-    override fun processAll(): List<StateAndEventProcessor.Response<Checkpoint>> {
+    fun processAll(): List<StateAndEventProcessor.Response<Checkpoint>> {
         // Copy [inputs] and throw it away for code simplicity
         return inputFlowEvents.toList().map { processOne() }
     }
