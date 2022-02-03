b07fd78a5087176b1cc418d57a8940ab8ac65a11
Nikolett Nagy <61757742+nikinagy@users.noreply.github.com>
2022-02-03 17:07:30 +0000

    CORE-3169 - Create and send SignedMemberInfo via Kafka upon static registration (#748)

--- libs/membership/membership-identity-impl/src/main/kotlin/net/corda/membership/identity/MemberInfoHelpers.kt
+++ libs/membership/membership-identity-impl/src/main/kotlin/net/corda/membership/identity/MemberInfoHelpers.kt
@@ -1,9 +1,13 @@
 package net.corda.membership.identity
 
 import net.corda.data.KeyValuePairList
+import net.corda.v5.crypto.DigestAlgorithmName
+import net.corda.v5.crypto.DigestService
+import net.corda.v5.crypto.MerkleTree
 import net.corda.v5.membership.identity.MGMContext
 import net.corda.v5.membership.identity.MemberContext
 import net.corda.v5.membership.identity.MemberInfo
+import java.nio.ByteBuffer
 import java.util.SortedMap
 
 /**
@@ -32,4 +36,16 @@ fun KeyValuePairList.toSortedMap(): SortedMap<String, String?> {
     // (to avoid malicious attacks where extra data is attached to the end of the context)
     validateKeyOrder(this)
     return this.items.map { it.key to it.value }.toMap().toSortedMap()
+}
+
+/**
+ * Builds the Merkle tree used for the mgm's signature.
+ * The leaves are the [MemberContext] and [MGMContext].
+ */
+fun buildMerkleTree(memberContext: ByteBuffer, mgmContext: ByteBuffer, digestService: DigestService): MerkleTree {
+    val leaves = mutableListOf(
+        digestService.hash(memberContext.array(), DigestAlgorithmName.SHA2_256),
+        digestService.hash(mgmContext.array(), DigestAlgorithmName.SHA2_256)
+    )
+    return MerkleTree.getMerkleTree(leaves, DigestAlgorithmName.SHA2_256, digestService)
 }
