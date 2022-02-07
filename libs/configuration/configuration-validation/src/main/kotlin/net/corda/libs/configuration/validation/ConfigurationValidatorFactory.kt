01c0350866c106749af32c0e405f5e2206fea7d9
James Higgs <james.higgs@r3.com>
2022-02-07 09:13:32 +0000

    CORE-3210: Minor test tweaks and a couple of compilation errors

--- libs/configuration/configuration-validation/src/main/kotlin/net/corda/libs/configuration/validation/ConfigurationValidatorFactory.kt
+++ libs/configuration/configuration-validation/src/main/kotlin/net/corda/libs/configuration/validation/ConfigurationValidatorFactory.kt
@@ -3,7 +3,7 @@ package net.corda.libs.configuration.validation
 import net.corda.libs.configuration.validation.impl.ConfigurationValidatorImpl
 import net.corda.schema.configuration.provider.SchemaProviderFactory
 
-class ConfigurationValidatorFactory {
+object ConfigurationValidatorFactory {
 
     fun getConfigValidator(): ConfigurationValidator {
         val schemaProvider = SchemaProviderFactory.getSchemaProvider()
