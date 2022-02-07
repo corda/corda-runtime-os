4d97b5ab4513e1caf6bfd6a45c648b7cd740d313
driessamyn <dries.samyn@r3.com>
2022-02-07 10:54:08 +0000

    Updated KDocs

--- components/db/db-connection-manager/src/main/kotlin/net/corda/db/connection/manager/DbConnectionManager.kt
+++ components/db/db-connection-manager/src/main/kotlin/net/corda/db/connection/manager/DbConnectionManager.kt
@@ -10,11 +10,19 @@ import javax.persistence.EntityManagerFactory
 /**
  * The [DbConnectionManager] is responsible for creating and managing [EntityManagerFactory] objects
  * for databases for which connection details itself are stored in the cluster DB.
- * [EntityManagerFactory] object could be created on demand or returned from a cache.
+ * Internally, the [EntityManagerFactory] objects could be cached, so the [getOrCreateEntityManagerFactory] method
+ * could create the [EntityManagerFactory] on demand or return one from a cache.
  *
- * The [DbConnectionManager] needs to be configured with enough data do be able to connect to the cluster DB
+ * The [DbConnectionManager] needs to be configured to be able to connect to the cluster DB
  * in order to fetch other DBs' connection details.
- * The [EntityManagerFactory] for the cluster DB itself is also exposed.
+ * The [EntityManagerFactory] for the cluster DB itself is also cached and exposed.
+ *
+ * The [getOrCreateEntityManagerFactory] method can be invoked with a [CordaDb] and [DbPrivilege] parameter to
+ * support cases for "well known" DBs (e.g. RBAC). For DBs that are dynamically added during the lifecycle of the
+ * Corda cluster, e.g. VNode Vault or VNode Crypto DBs, the name, [DbPrivilege] and [JpaEntitiesSet] must be provided,
+ * where name is a unique name identifying the DB (e.g. vnode_123_vault) and [JpaEntitiesSet] contains all the JPA
+ * entities managed for this DB. The [JpaEntitiesSet] could be a "fixed" set (e.g. vnode crypto DB), or a dynamic set
+ * depending on the CorDapps related to the VNode.
  *
  * The [DbConnectionManager] will transition to [LifecycleStatus.UP] once it has checked it has access to the cluster DB.
  *
@@ -25,8 +33,11 @@ interface DbConnectionManager : Lifecycle {
     /**
      * Get an instance of [EntityManagerFactory] for the named [db] from cache or create one if necessary.
      *
-     * @param db Predefined [db] to be used.
-     * @return
+     * @param db Name of the DB to use.
+     * @param privilege [DbPrivilege] required (DML or DDL).
+     * @return [EntityManagerFactory] from cache, or created on demand.
+     * @throws [DBConfigurationException] if connection details for the requested DB/Privilege does not exist
+     *              or if entities associated to the DB are not defined.
      */
     fun getOrCreateEntityManagerFactory(db: CordaDb, privilege: DbPrivilege): EntityManagerFactory
 
@@ -34,21 +45,26 @@ interface DbConnectionManager : Lifecycle {
      * Get an instance of [EntityManagerFactory] for the connection ID. Use cache or create one if necessary.
      *
      * @param name name for the connection to be used.
-     * @param privilege required.
-     * @param entitiesSet to used with the EntityManager.
-     * @return
+     * @param privilege [DbPrivilege] required (DML or DDL).
+     * @param entitiesSet Set of all entities managed by [javax.persistence.EntityManager]s created by the
+     *                  [EntityManagerFactory] returned
+     * @return [EntityManagerFactory] from cache, or created on demand.
+     * @throws [DBConfigurationException] if connection details for the requested DB/Privilege does not exist.
      */
-    fun getOrCreateEntityManagerFactory(name: String, privilege: DbPrivilege, entitiesSet: JpaEntitiesSet): EntityManagerFactory
+    fun getOrCreateEntityManagerFactory(name: String, privilege: DbPrivilege, entitiesSet: JpaEntitiesSet):
+            EntityManagerFactory
 
     /**
      * Persist new DB connection with given [config].
      *
      * Replaces if the connection already exists.
+     * The [name] and [privilege] pair are unique in the DB Connections Configuration database.
      *
-     * @param name name for the connection to be used.
-     * @param privilege required.
+     * @param name Name of the DB to use.
+     * @param privilege [DbPrivilege] (DML or DDL).
      * @param config smart config object to be used to create a DataSource using the
-     *      [DataSourceFactory.createFromConfig] extension method
+     *      [DataSourceFactory.createFromConfig] extension method. This should contain all JDBC connection details
+     *      needed to create a [javax.sql.DataSource] from [DataSourceFactory].
      * @param description (optional)
      * @param name of the actor responsible for the insert or update.
      */
