a19a8a3c28e3c3d302f671e143720cf6bfb136c5
Kyriakos Tharrouniatis <kyriakos.tharrouniatis@r3.com>
2022-02-08 15:27:05 +0000

    Add/ update KDocs

--- libs/virtual-node/cpi-upload-manager/src/main/kotlin/net/corda/libs/cpiupload/CpiUploadManagerException.kt
+++ libs/virtual-node/cpi-upload-manager/src/main/kotlin/net/corda/libs/cpiupload/CpiUploadManagerException.kt
@@ -3,6 +3,6 @@ package net.corda.libs.cpiupload
 import net.corda.v5.base.exceptions.CordaRuntimeException
 
 /**
- * Thrown from within CpiUploadManager management operations.
+ * Thrown from within [CpiUploadManager] management operations.
  */
 class CpiUploadManagerException(message: String, e: Exception? = null) : CordaRuntimeException(message, e)
--- libs/virtual-node/cpi-upload-manager/src/main/kotlin/net/corda/libs/cpiupload/CpiUploadManagerFactory.kt
+++ libs/virtual-node/cpi-upload-manager/src/main/kotlin/net/corda/libs/cpiupload/CpiUploadManagerFactory.kt
@@ -5,6 +5,9 @@ import net.corda.data.chunking.ChunkAck
 import net.corda.libs.configuration.SmartConfig
 import net.corda.messaging.api.publisher.RPCSender
 
+/**
+ * Used to create instances of [CpiUploadManager].
+ */
 interface CpiUploadManagerFactory {
     fun create(config: SmartConfig, rpcSender: RPCSender<Chunk, ChunkAck>): CpiUploadManager
 }
