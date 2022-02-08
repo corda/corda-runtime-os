51d499b3af16c4bf2c9e932ede536fb19e634c75
Peter McKinney <pmck91@users.noreply.github.com>
2022-02-08 12:16:46 +0000

    Testing systemLambda on Jenkins

--- app/src/test/kotlin/net/corda/cli/application/BootTest.kt
+++ app/src/test/kotlin/net/corda/cli/application/BootTest.kt
@@ -1,13 +1,21 @@
 package net.corda.cli.application
 
+import com.github.stefanbirkner.systemlambda.SystemLambda
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
+import picocli.CommandLine
 
 class BootTest {
 
     @Test
     fun testNoArgs() {
 
-        assertTrue(true)
+        val outText = SystemLambda.tapSystemErrNormalized {
+            CommandLine(
+                App()
+            ).execute("")
+        }
+
+        assertTrue(outText.contains("Usage: corda [COMMAND]"))
     }
 }
