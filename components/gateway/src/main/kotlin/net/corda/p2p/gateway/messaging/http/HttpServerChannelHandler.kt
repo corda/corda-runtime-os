82d0932c365e19bc79c9993eec6c51edd389d866
Emily Bowe <emily.bowe@r3.com>
2022-02-09 13:43:12 +0000

    replacing field of responseCode - hold the value and keep checking if http response is ok

--- components/gateway/src/main/kotlin/net/corda/p2p/gateway/messaging/http/HttpServerChannelHandler.kt
+++ components/gateway/src/main/kotlin/net/corda/p2p/gateway/messaging/http/HttpServerChannelHandler.kt
@@ -20,22 +20,21 @@ import java.lang.IndexOutOfBoundsException
 
 class HttpServerChannelHandler(private val serverListener: HttpServerListener,
                                private val logger: Logger): BaseHttpChannelHandler(serverListener, logger) {
+    private var responseCode: HttpResponseStatus? = null
+
     /**
      * Reads the HTTP objects into a [ByteBuf] and publishes them to all subscribers
      */
     @Suppress("ComplexMethod")
     override fun channelRead0(ctx: ChannelHandlerContext, msg: HttpObject) {
         if (msg is HttpRequest) {
-            val responseCode = msg.validate()
+            responseCode = msg.validate()
             if (responseCode != HttpResponseStatus.OK) {
                 logger.warn ("Received invalid HTTP request from ${ctx.channel().remoteAddress()}\n" +
                         "Protocol version: ${msg.protocolVersion()}\n" +
                         "Hostname: ${msg.headers()[HttpHeaderNames.HOST]?:"unknown"}\n" +
                         "Request URI: ${msg.uri()}\n and the response code was $responseCode.")
-                val response = createResponse(null, responseCode)
-                ctx.writeAndFlush(response)
-                    .addListener(ChannelFutureListener.CLOSE)
-                return
+
             }
 
             logger.debug ("Received HTTP request from ${ctx.channel().remoteAddress()}\n" +
@@ -43,8 +42,11 @@ class HttpServerChannelHandler(private val serverListener: HttpServerListener,
                     "Hostname: ${msg.headers()[HttpHeaderNames.HOST]?:"unknown"}\n" +
                     "Request URI: ${msg.uri()}\n" +
                     "Content length: ${msg.headers()[HttpHeaderNames.CONTENT_LENGTH]}\n")
+
             // initialise byte array to read the request into
-            allocateBodyBuffer(ctx, msg.headers()[HttpHeaderNames.CONTENT_LENGTH].toInt())
+            if (responseCode!! == HttpResponseStatus.OK) {
+                allocateBodyBuffer(ctx, msg.headers()[HttpHeaderNames.CONTENT_LENGTH].toInt())
+            }
 
             if (HttpUtil.is100ContinueExpected(msg)) {
                 send100Continue(ctx)
@@ -53,7 +55,7 @@ class HttpServerChannelHandler(private val serverListener: HttpServerListener,
 
         if (msg is HttpContent) {
             val content = msg.content()
-            if (content.isReadable) {
+            if (content.isReadable && responseCode!! == HttpResponseStatus.OK) {
                 logger.debug("Reading message content into local buffer of size ${content.readableBytes()}")
                 try {
                     readBytesIntoBodyBuffer(content)
@@ -68,10 +70,21 @@ class HttpServerChannelHandler(private val serverListener: HttpServerListener,
         if (msg is LastHttpContent) {
             logger.debug("Read end of response body $msg")
             val returnByteArray = readBytesFromBodyBuffer()
-            val sourceAddress = ctx.channel().remoteAddress()
-            val targetAddress = ctx.channel().localAddress()
-            serverListener.onRequest(HttpRequest(returnByteArray, sourceAddress, targetAddress))
+
+            when(responseCode) {
+                HttpResponseStatus.OK -> {
+                    val sourceAddress = ctx.channel().remoteAddress()
+                    val targetAddress = ctx.channel().localAddress()
+                    serverListener.onRequest(HttpRequest(returnByteArray, sourceAddress, targetAddress))
+                }
+                else -> {
+                    val response = createResponse(null, responseCode!!)
+                    ctx.writeAndFlush(response)
+                }
+            }
+
             releaseBodyBuffer()
+            responseCode = null
         }
     }
 
