33bc1603b7a6f238985485f7479da09f8c21f2d0
Ryan Fowler <fowlerrr@users.noreply.github.com>
2022-02-11 16:00:08 +0000

    CORE-3020: Add DB Message Bus implementation for Producer (#772)
    * CORE-3020: DB implementation of message bus
    
    This first PR contains just the producer side.

--- libs/messaging/db-messaging-impl/src/main/kotlin/net/corda/messaging/db/persistence/DBAccessProvider.kt
+++ libs/messaging/db-messaging-impl/src/main/kotlin/net/corda/messaging/db/persistence/DBAccessProvider.kt
@@ -102,36 +102,6 @@ interface DBAccessProvider: Lifecycle {
 
 }
 
-data class RecordDbEntry(val topic: String, val partition: Int, val offset: Long, val key: ByteArray, val value: ByteArray?) {
-    @Suppress("ComplexMethod")
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as RecordDbEntry
-
-        if (topic != other.topic) return false
-        if (partition != other.partition) return false
-        if (offset != other.offset) return false
-        if (!key.contentEquals(other.key)) return false
-        if (value != null) {
-            if (other.value == null) return false
-            if (!value.contentEquals(other.value)) return false
-        } else if (other.value != null) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = topic.hashCode()
-        result = 31 * result + partition
-        result = 31 * result + offset.hashCode()
-        result = 31 * result + key.contentHashCode()
-        result = 31 * result + (value?.contentHashCode() ?: 0)
-        return result
-    }
-}
-
 /**
  * @property partition the partition to fetch records from.
  * @param startOffset the offset after which records can be fetched (inclusive).
