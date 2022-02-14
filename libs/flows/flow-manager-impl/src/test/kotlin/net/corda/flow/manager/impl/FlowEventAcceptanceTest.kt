3ad8151f892fa0c28b51a83c68d84f689583d7e8
Dan Newton <dan.newton@r3.com>
2022-02-14 18:57:50 +0000

    CORE-3301 Update acceptance test

--- libs/flows/flow-manager-impl/src/test/kotlin/net/corda/flow/manager/impl/FlowEventAcceptanceTest.kt
+++ libs/flows/flow-manager-impl/src/test/kotlin/net/corda/flow/manager/impl/FlowEventAcceptanceTest.kt
@@ -13,26 +13,21 @@ import org.junit.jupiter.api.Test
 class FlowEventAcceptanceTest {
 
     @Test
-    fun `receiving multiple wakeup events does not alter a flow's checkpoint`() {
+    fun `forcing a checkpoint schedules a wakeup event`() {
         flowEventDSL {
 
             startedFlowFiber {
-                repeatSuspension(FlowIORequest.ForceCheckpoint, 3)
+                repeatSuspension(FlowIORequest.ForceCheckpoint, 1)
             }
 
             inputLastOutputEvent()
-            inputLastOutputEvent()
-            inputLastOutputEvent()
 
-            val responses = processAll()
+            val response = processOne()
+            val outputPayloads = response.filterOutputFlowTopicEventPayloads()
 
-            val checkpoints = responses.map { it.updatedState }
-            val outputEvents = responses.map { it.responseEvents.single().value as FlowEvent }
-
-            assertEquals(3, checkpoints.size)
-            assertEquals(1, checkpoints.toSet().size)
-            assertEquals(3, outputEvents.size)
-            assertEquals(3, outputEvents.map { it.payload }.filterIsInstance<Wakeup>().size)
+            assertEquals(net.corda.data.flow.state.waiting.Wakeup(), response.updatedState?.flowState?.waitingFor?.value)
+            assertEquals(1, outputPayloads.size)
+            assertTrue(outputPayloads.single() is Wakeup)
         }
     }
 
