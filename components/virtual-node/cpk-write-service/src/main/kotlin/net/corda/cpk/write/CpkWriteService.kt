d29a39f706796016a033918471706842a660d59c
Kyriakos Tharrouniatis <kyriakos.tharrouniatis@r3.com>
2022-02-21 22:46:57 +0000

    Forgotten file

--- components/virtual-node/cpk-write-service/src/main/kotlin/net/corda/cpk/write/CpkWriteService.kt
+++ components/virtual-node/cpk-write-service/src/main/kotlin/net/corda/cpk/write/CpkWriteService.kt
@@ -2,11 +2,10 @@ package net.corda.cpk.write
 
 import net.corda.cpk.write.types.CpkChunk
 import net.corda.lifecycle.Lifecycle
-import java.util.concurrent.CompletableFuture
 
 interface CpkWriteService : Lifecycle {
     /**
      * Puts CPK chunks to kafka.
       */
-    fun putAll(cpkChunks: List<CpkChunk>): List<CompletableFuture<Unit>>
+    fun putAll(cpkChunks: List<CpkChunk>)
 }
