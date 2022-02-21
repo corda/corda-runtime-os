f3de039f48c033f9e693a63381d56e75040d72d8
Kyriakos Tharrouniatis <kyriakos.tharrouniatis@r3.com>
2022-02-21 15:39:58 +0000

    Align `CpkWriteService` interface

--- components/virtual-node/cpk-write-service/src/main/kotlin/net/corda/cpk/write/CpkWriteService.kt
+++ components/virtual-node/cpk-write-service/src/main/kotlin/net/corda/cpk/write/CpkWriteService.kt
@@ -1,13 +1,11 @@
 package net.corda.cpk.write
 
+import net.corda.cpk.write.types.CpkChunk
 import net.corda.lifecycle.Lifecycle
-import net.corda.packaging.CPK
-import java.io.InputStream
 
 interface CpkWriteService : Lifecycle {
-    /** put the cpk into some implementation specific backing storage */
-    fun put(cpkMetadata: CPK.Metadata, inputStream: InputStream)
-
-    /** remove the cpk from some implementation specific backing storage */
-    fun remove(cpkMetadata: CPK.Metadata)
+    /**
+     * Puts CPK chunks to kafka.
+      */
+    fun putIfAbsent(cpkInfo: List<CpkChunk>): Boolean
 }
