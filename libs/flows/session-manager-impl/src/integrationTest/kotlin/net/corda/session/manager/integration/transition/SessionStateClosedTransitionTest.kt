dbf4a96e20652653f9859f349d3d1934a62256f4
LWogan <lorcan.wogan@r3.com>
2022-02-22 13:58:50 +0000

    CORE-3919 additional tests to verify transitions

---
+++ libs/flows/session-manager-impl/src/integrationTest/kotlin/net/corda/session/manager/integration/transition/SessionStateClosedTransitionTest.kt
@@ -0,0 +1,100 @@
+package net.corda.session.manager.integration.transition
+
+import net.corda.data.flow.event.MessageDirection
+import net.corda.data.flow.event.session.SessionAck
+import net.corda.data.flow.state.session.SessionState
+import net.corda.data.flow.state.session.SessionStateType
+import net.corda.session.manager.impl.SessionManagerImpl
+import net.corda.session.manager.impl.buildSessionState
+import net.corda.session.manager.integration.SessionMessageType
+import net.corda.session.manager.integration.helper.generateMessage
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+import java.time.Instant
+
+class SessionStateClosedTransitionTest {
+
+    private val sessionManager = SessionManagerImpl()
+    private val instant = Instant.now()
+    
+    //TODO - this should be an error
+    @Test
+    fun `Send session init when in state closed`() {
+        val sessionState = buildClosedState()
+        
+        val sessionEvent = generateMessage(SessionMessageType.INIT, instant)
+        val outputState = sessionManager.processMessageToSend(sessionState, sessionState, sessionEvent, instant)
+        Assertions.assertThat(outputState.status).isEqualTo(SessionStateType.CLOSED)
+    }
+
+    @Test
+    fun `Send data when in state closed`() {
+        val sessionState = buildClosedState()
+
+        val sessionEvent = generateMessage(SessionMessageType.DATA, instant)
+        val outputState = sessionManager.processMessageToSend(sessionState, sessionState, sessionEvent, instant)
+        Assertions.assertThat(outputState.status).isEqualTo(SessionStateType.ERROR)
+    }
+
+    @Test
+    fun `Send close when in state closed`() {
+        val sessionState = buildClosedState()
+
+        val sessionEvent = generateMessage(SessionMessageType.CLOSE, instant)
+        val outputState = sessionManager.processMessageToSend(sessionState, sessionState, sessionEvent, instant)
+        Assertions.assertThat(outputState.status).isEqualTo(SessionStateType.ERROR)
+    }
+
+    @Test
+    fun `Receive duplicate init when in state closed`() {
+        val sessionState = buildClosedState()
+
+        val sessionEvent = generateMessage(SessionMessageType.INIT, instant, MessageDirection.INBOUND)
+        sessionEvent.sequenceNum = 1
+        val outputState = sessionManager.processMessageReceived(sessionState, sessionState, sessionEvent, instant)
+        Assertions.assertThat(outputState.status).isEqualTo(SessionStateType.CLOSED)
+    }
+
+    @Test
+    fun `Receive new data when in state closed`() {
+        val sessionState = buildClosedState()
+
+        val sessionEvent = generateMessage(SessionMessageType.DATA, instant, MessageDirection.INBOUND)
+        sessionEvent.sequenceNum = 2
+        val outputState = sessionManager.processMessageReceived(sessionState, sessionState, sessionEvent, instant)
+        Assertions.assertThat(outputState.status).isEqualTo(SessionStateType.ERROR)
+    }
+
+    //TODO - this should be ok
+    @Test
+    fun `Receive duplicate close when in state closed`() {
+        val sessionState = buildClosedState()
+
+        val sessionEvent = generateMessage(SessionMessageType.CLOSE, instant, MessageDirection.INBOUND)
+        sessionEvent.sequenceNum = 1
+        val outputState = sessionManager.processMessageReceived(sessionState, sessionState, sessionEvent, instant)
+        Assertions.assertThat(outputState.status).isEqualTo(SessionStateType.ERROR)
+    }
+
+    @Test
+    fun `Receive ack for close when in state closed`() {
+        val sessionState = buildClosedState()
+
+        val sessionEvent = generateMessage(SessionMessageType.ACK, instant, MessageDirection.INBOUND)
+        (sessionEvent.payload as SessionAck).sequenceNum = 2
+
+        val outputState = sessionManager.processMessageReceived(sessionState, sessionState, sessionEvent, instant)
+        Assertions.assertThat(outputState.status).isEqualTo(SessionStateType.CLOSED)
+    }
+
+
+    private fun buildClosedState(): SessionState {
+        return buildSessionState(
+            SessionStateType.CLOSED,
+            1,
+            listOf(),
+            2,
+            listOf()
+        )
+    }
+}
---
+++ libs/flows/session-manager-impl/src/integrationTest/kotlin/net/corda/session/manager/integration/transition/SessionStateCreatedTransitionTest.kt
@@ -0,0 +1,101 @@
+package net.corda.session.manager.integration.transition
+
+import net.corda.data.flow.event.MessageDirection
+import net.corda.data.flow.event.session.SessionAck
+import net.corda.data.flow.state.session.SessionState
+import net.corda.data.flow.state.session.SessionStateType
+import net.corda.session.manager.impl.SessionManagerImpl
+import net.corda.session.manager.impl.buildSessionState
+import net.corda.session.manager.integration.SessionMessageType
+import net.corda.session.manager.integration.helper.generateMessage
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+import java.time.Instant
+
+class SessionStateCreatedTransitionTest {
+
+    private val sessionManager = SessionManagerImpl()
+    private val instant = Instant.now()
+
+    //TODO - this should be an error
+    @Test
+    fun `Send session init when in state created`() {
+        val sessionState = buildCreatedState()
+
+        val sessionEvent = generateMessage(SessionMessageType.INIT, instant)
+        val outputState = sessionManager.processMessageToSend(sessionState, sessionState, sessionEvent, instant)
+        Assertions.assertThat(outputState.status).isEqualTo(SessionStateType.CREATED)
+    }
+
+    @Test
+    fun `Send data when in state created`() {
+        val sessionState = buildCreatedState()
+
+        val sessionEvent = generateMessage(SessionMessageType.DATA, instant)
+        val outputState = sessionManager.processMessageToSend(sessionState, sessionState, sessionEvent, instant)
+        Assertions.assertThat(outputState.status).isEqualTo(SessionStateType.ERROR)
+    }
+
+    @Test
+    fun `Send close when in state created`() {
+        val sessionState = buildCreatedState()
+
+        val sessionEvent = generateMessage(SessionMessageType.CLOSE, instant)
+        val outputState = sessionManager.processMessageToSend(sessionState, sessionState, sessionEvent, instant)
+        Assertions.assertThat(outputState.status).isEqualTo(SessionStateType.ERROR)
+    }
+
+    //TODO this should really be an error
+    @Test
+    fun `Session Initiatitor receives init back`() {
+        val sessionState = buildCreatedState()
+
+        val sessionEvent = generateMessage(SessionMessageType.INIT, instant, MessageDirection.INBOUND)
+        sessionEvent.sequenceNum = 1
+        val outputState = sessionManager.processMessageReceived(sessionState, sessionState, sessionEvent, instant)
+        Assertions.assertThat(outputState.status).isEqualTo(SessionStateType.CREATED)
+    }
+
+    @Test
+    fun `Session Initiatitor receives data back`() {
+        val sessionState = buildCreatedState()
+
+        val sessionEvent = generateMessage(SessionMessageType.DATA, instant, MessageDirection.INBOUND)
+        sessionEvent.sequenceNum = 1
+        val outputState = sessionManager.processMessageReceived(sessionState, sessionState, sessionEvent, instant)
+        Assertions.assertThat(outputState.status).isEqualTo(SessionStateType.CREATED)
+    }
+
+    @Test
+    fun `Session Initiator receives close back`() {
+        val sessionState = buildCreatedState()
+
+        val sessionEvent = generateMessage(SessionMessageType.CLOSE, instant, MessageDirection.INBOUND)
+        sessionEvent.sequenceNum = 1
+        val outputState = sessionManager.processMessageReceived(sessionState, sessionState, sessionEvent, instant)
+        Assertions.assertThat(outputState.status).isEqualTo(SessionStateType.CLOSING)
+    }
+
+    @Test
+    fun `Session Initiator receives ack back`() {
+        val sessionState = buildCreatedState()
+        val sessionEvent = generateMessage(SessionMessageType.ACK, instant, MessageDirection.INBOUND)
+        (sessionEvent.payload as SessionAck).sequenceNum = 1
+
+        val outputState = sessionManager.processMessageReceived(sessionState, sessionState, sessionEvent, instant)
+        Assertions.assertThat(outputState.status).isEqualTo(SessionStateType.CONFIRMED)
+    }
+
+    private fun buildCreatedState(): SessionState {
+        val sentSessionInit = generateMessage(SessionMessageType.INIT, instant)
+        sentSessionInit.sequenceNum = 1
+
+        return buildSessionState(
+            SessionStateType.CREATED,
+            0,
+            listOf(),
+            1,
+            listOf(sentSessionInit)
+        )
+    }
+}
