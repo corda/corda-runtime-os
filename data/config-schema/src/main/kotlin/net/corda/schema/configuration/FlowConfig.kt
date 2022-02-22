fc57139b84bb041cf3ade16a0a68fd7b8c752492
Lorcan Wogan <69468264+LWogan@users.noreply.github.com>
2022-02-22 14:19:59 +0000

    CORE-3904 timestamp of last/sent received message for heartbeat check (#254)
    * CORE-3904 timestamp of last sent message for heartbeat check
    
    * CORE-3904 add default config
    
    * CORE-3904 add docs, switch long to timestamp

--- data/config-schema/src/main/kotlin/net/corda/schema/configuration/FlowConfig.kt
+++ data/config-schema/src/main/kotlin/net/corda/schema/configuration/FlowConfig.kt
@@ -2,4 +2,5 @@ package net.corda.schema.configuration
 
 object FlowConfig {
         const val SESSION_MESSAGE_RESEND_WINDOW = "session.messageResendWindow"
+        const val SESSION_HEARTBEAT_TIMEOUT_WINDOW = "session.heartbeatTimeout"
 }
