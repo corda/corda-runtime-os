968941e2498a3b75bdc286bdcdaa694dece1814e
Kyriakos Tharrouniatis <kyriakos.tharrouniatis@r3.com>
2022-02-23 13:58:12 +0000

    Generate equals and hashCode for bytes

--- components/virtual-node/cpk-write-service-impl/src/main/kotlin/net/corda/cpk/write/impl/services/db/CpkChecksumData.kt
+++ components/virtual-node/cpk-write-service-impl/src/main/kotlin/net/corda/cpk/write/impl/services/db/CpkChecksumData.kt
@@ -2,8 +2,25 @@ package net.corda.cpk.write.impl.services.db
 
 import net.corda.v5.crypto.SecureHash
 
-@Suppress("warnings")
 data class CpkChecksumData(
     val checksum: SecureHash,
     val bytes: ByteArray
-)
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as CpkChecksumData
+
+        if (checksum != other.checksum) return false
+        if (!bytes.contentEquals(other.bytes)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = checksum.hashCode()
+        result = 31 * result + bytes.contentHashCode()
+        return result
+    }
+}
