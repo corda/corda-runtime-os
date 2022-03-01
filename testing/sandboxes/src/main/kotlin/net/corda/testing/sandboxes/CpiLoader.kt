9aff8475a16f82e55eb1930bf973d0a6579d05af
Chris Rankin <chris.rankin@r3.com>
2022-03-01 11:21:17 +0000

    NOTICK: Minor fixes for sandbox tests.

--- testing/sandboxes/src/main/kotlin/net/corda/testing/sandboxes/CpiLoader.kt
+++ testing/sandboxes/src/main/kotlin/net/corda/testing/sandboxes/CpiLoader.kt
@@ -2,16 +2,16 @@ package net.corda.testing.sandboxes
 
 import net.corda.libs.packaging.CpiIdentifier
 import net.corda.libs.packaging.CpiMetadata
-import java.util.concurrent.CompletableFuture
 import net.corda.packaging.CPI
+import java.util.concurrent.CompletableFuture
 
 interface CpiLoader {
     fun loadCPI(resourceName: String): CPI
-    fun unloadCPI(id: CpiIdentifier)
+    fun unloadCPI(cpi: CPI)
 
-    fun getAll(): CompletableFuture<List<CpiMetadata>>
-    fun get(id: CpiIdentifier): CompletableFuture<CpiMetadata?>
-    fun remove(id: CpiIdentifier)
+    fun getAllCpiMetadata(): CompletableFuture<List<CpiMetadata>>
+    fun getCpiMetadata(id: CpiIdentifier): CompletableFuture<CpiMetadata?>
+    fun removeCpiMetadata(id: CpiIdentifier)
 
     fun stop()
 }
