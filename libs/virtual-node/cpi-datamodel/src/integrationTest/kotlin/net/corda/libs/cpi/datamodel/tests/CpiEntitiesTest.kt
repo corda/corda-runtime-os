48491b29f53cfea9532ccea999201ef0a64730c0
Kyriakos Tharrouniatis <kyriakos.tharrouniatis@r3.com>
2022-03-02 09:23:15 +0000

    CORE-3874 Load CPK from DB and publish chunks to Kafka (#875)
    Load CPK from DB and publish chunks to Kafka

--- libs/virtual-node/cpi-datamodel/src/integrationTest/kotlin/net/corda/libs/cpi/datamodel/tests/CpiEntitiesTest.kt
+++ libs/virtual-node/cpi-datamodel/src/integrationTest/kotlin/net/corda/libs/cpi/datamodel/tests/CpiEntitiesTest.kt
@@ -10,31 +10,33 @@ import net.corda.libs.cpi.datamodel.CpiMetadataEntityKey
 import net.corda.libs.cpi.datamodel.CpkDataEntity
 import net.corda.libs.cpi.datamodel.CpkMetadataEntity
 import net.corda.libs.cpi.datamodel.CpkMetadataEntityKey
+import net.corda.libs.cpi.datamodel.findCpkChecksumsNotIn
+import net.corda.libs.cpi.datamodel.findCpkDataEntity
 import net.corda.orm.EntityManagerConfiguration
 import net.corda.orm.impl.EntityManagerFactoryFactoryImpl
 import net.corda.orm.utils.transaction
 import net.corda.orm.utils.use
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assumptions
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import java.util.UUID
+import javax.persistence.EntityManagerFactory
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class CpiEntitiesIntegrationTest {
-    val dbConfig: EntityManagerConfiguration
+    private val dbConfig: EntityManagerConfiguration
 
     init {
         // comment this out if you want to run the test against a real postgres
         //  NOTE: the blob storage doesn't work in HSQL, hence skipping the majority of the test.
-//        System.setProperty("postgresPort", "5432")
+        System.setProperty("postgresPort", "5432")
         dbConfig = DbUtils.getEntityManagerConfiguration("cpi_db")
     }
     @BeforeAll
     private fun prepareDatabase() {
-
-
         val dbChange = ClassloaderChangeLog(
             linkedSetOf(
                 ClassloaderChangeLog.ChangeLogResourceFiles(
@@ -217,5 +219,114 @@ class CpiEntitiesIntegrationTest {
         assertThat(loadedCpi.cpks).containsExactlyInAnyOrder(
             cpkEntity, cpkEntity2
         )
+    }
+
+    private fun cleanUpDb() {
+        val emFactory = EntityManagerFactoryFactoryImpl().create(
+            "test_unit",
+            CpiEntities.classes.toList(),
+            dbConfig
+        )
+
+        emFactory.transaction {
+            it.createQuery("DELETE FROM ${CpkMetadataEntity::class.simpleName}").executeUpdate()
+
+            it.createQuery("DELETE FROM ${CpkDataEntity::class.simpleName}").executeUpdate()
+        }
+    }
+
+    private fun insertCpkChecksums(cpkChecksums: List<String>, emFactory: EntityManagerFactory) {
+        emFactory.transaction {
+            cpkChecksums.forEach { cpkChecksum ->
+                it.persist(
+                    CpkDataEntity(
+                        cpkChecksum,
+                        byteArrayOf(0x01, 0x02, 0x03)
+                    )
+                )
+            }
+        }
+    }
+
+    @Test
+    fun ` on findCpkChecksumsNotIn an empty set returns all results`() {
+        cleanUpDb()
+        val cpkChecksums = listOf(
+            "SHA-256:BFD76C0EBBD006FEE583410547C1887B0292BE76D582D96C242D2A792723E3FA",
+            "SHA-256:BFD76C0EBBD006FEE583410547C1887B0292BE76D582D96C242D2A792723E3FB",
+            "SHA-256:BFD76C0EBBD006FEE583410547C1887B0292BE76D582D96C242D2A792723E3FC"
+        )
+
+        val emFactory = EntityManagerFactoryFactoryImpl().create(
+            "test_unit",
+            CpiEntities.classes.toList(),
+            dbConfig
+        )
+
+        insertCpkChecksums(cpkChecksums, emFactory)
+
+        val fetchedCpkChecksums =
+        emFactory.transaction {
+            it.findCpkChecksumsNotIn(emptyList())
+        }
+
+        assertEquals(cpkChecksums[0], fetchedCpkChecksums[0])
+        assertEquals(cpkChecksums[1], fetchedCpkChecksums[1])
+        assertEquals(cpkChecksums[2], fetchedCpkChecksums[2])
+    }
+
+    @Test
+    fun ` on findCpkChecksumsNotIn a checksum set returns all but this set`() {
+        cleanUpDb()
+        val cpkChecksums = listOf(
+            "SHA-256:BFD76C0EBBD006FEE583410547C1887B0292BE76D582D96C242D2A792723E3FA",
+            "SHA-256:BFD76C0EBBD006FEE583410547C1887B0292BE76D582D96C242D2A792723E3FB",
+            "SHA-256:BFD76C0EBBD006FEE583410547C1887B0292BE76D582D96C242D2A792723E3FC"
+        )
+
+        val emFactory = EntityManagerFactoryFactoryImpl().create(
+            "test_unit",
+            CpiEntities.classes.toList(),
+            dbConfig
+        )
+
+        insertCpkChecksums(cpkChecksums, emFactory)
+
+        val fetchedCpkChecksums =
+            emFactory.transaction {
+                it.findCpkChecksumsNotIn(listOf(
+                    "SHA-256:BFD76C0EBBD006FEE583410547C1887B0292BE76D582D96C242D2A792723E3FA",
+                    "SHA-256:BFD76C0EBBD006FEE583410547C1887B0292BE76D582D96C242D2A792723E3FB"
+                ))
+            }
+
+        assertEquals(cpkChecksums[2], fetchedCpkChecksums[0])
+    }
+
+    @Test
+    fun `finds CPK data entity`() {
+        cleanUpDb()
+        val cpkChecksums = listOf(
+            "SHA-256:BFD76C0EBBD006FEE583410547C1887B0292BE76D582D96C242D2A792723E3FA"
+        )
+
+        val emFactory = EntityManagerFactoryFactoryImpl().create(
+            "test_unit",
+            CpiEntities.classes.toList(),
+            dbConfig
+        )
+
+        insertCpkChecksums(cpkChecksums, emFactory)
+
+        val cpkDataEntity =
+            emFactory.transaction {
+                it.findCpkDataEntity(cpkChecksums[0])
+            }
+
+        val expectedCpkDataEntity = CpkDataEntity(
+            "SHA-256:BFD76C0EBBD006FEE583410547C1887B0292BE76D582D96C242D2A792723E3FA",
+            byteArrayOf(0x01, 0x02, 0x03)
+        )
+        assertEquals(expectedCpkDataEntity, cpkDataEntity)
     }
 }
