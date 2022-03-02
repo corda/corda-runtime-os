e5ff4e122e0efb409daa8ef7f19186ed54c6ffec
nikinagy <nikolett.nagy@r3.com>
2022-03-02 09:40:40 +0000

    add failing scenarios

--- applications/workers/release/rpc-worker/src/e2eTest/kotlin/net/corda/applications/workers/rpc/StaticRegistrationE2eTest.kt
+++ applications/workers/release/rpc-worker/src/e2eTest/kotlin/net/corda/applications/workers/rpc/StaticRegistrationE2eTest.kt
@@ -4,23 +4,41 @@ import net.corda.applications.workers.rpc.http.TestToolkitProperty
 import net.corda.membership.httprpc.v1.MemberRegistrationRpcOps
 import net.corda.membership.httprpc.v1.types.request.MemberRegistrationRequest
 import net.corda.membership.httprpc.v1.types.request.RegistrationAction
+import net.corda.v5.base.exceptions.CordaRuntimeException
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 class StaticRegistrationE2eTest {
     private val testToolkit by TestToolkitProperty()
 
+    private val nonExistentId = "failure"
+
     @Test
-    fun test() {
+    fun `using a non-existent holding identity id results in exception during registration`() {
         testToolkit.httpClientFor(MemberRegistrationRpcOps::class.java).use { client ->
             val proxy = client.start().proxy
 
-            proxy.startRegistration(
-                MemberRegistrationRequest(
-                    "test",
-                    RegistrationAction.REQUEST_JOIN
+            val ex = assertThrows<CordaRuntimeException> {
+                proxy.startRegistration(
+                    MemberRegistrationRequest(
+                        nonExistentId,
+                        RegistrationAction.REQUEST_JOIN
+                    )
                 )
-            )
+            }
+            assert(ex.message!!.contains("MembershipRegistrationException"))
+        }
+    }
 
+    @Test
+    fun `using a non-existent holding identity id results in exception during registration status check`() {
+        testToolkit.httpClientFor(MemberRegistrationRpcOps::class.java).use { client ->
+            val proxy = client.start().proxy
+
+            val ex = assertThrows<CordaRuntimeException> {
+                proxy.checkRegistrationProgress(nonExistentId)
+            }
+            assert(ex.message!!.contains("MembershipRegistrationException"))
         }
     }
 }
