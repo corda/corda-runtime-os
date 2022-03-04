eaa6066f11a8c4c1e5273e154e3575b88334ba81
Charlie Crean <69511999+charlieR3@users.noreply.github.com>
2022-03-04 17:08:21 +0000

    CORE-4127 Tooling to retrieve group member lists for verification of membership client (#942)

--- components/membership/membership-group-read/src/main/kotlin/net/corda/membership/read/MembershipGroupReader.kt
+++ components/membership/membership-group-read/src/main/kotlin/net/corda/membership/read/MembershipGroupReader.kt
@@ -7,33 +7,40 @@ import net.corda.v5.base.types.MemberX500Name
 import net.corda.v5.membership.MemberInfo
 
 /**
- * Provides group information for a specific group and member. The view of group information may vary based on the
- * requesting member.
+ * Provides group information for a specific group and member. The view of group information is for the member
+ * represented by [owningMember] within the group represented by [groupId]
  */
 interface MembershipGroupReader {
     /**
-     * The ID of the group for which this service provides functionality for.
+     * The ID of the group for which this service provides data on.
      */
     val groupId: String
 
     /**
-     * The member for which this group information is visible for.
+     * The member X500 name for which this service provides data on.
      */
     val owningMember: MemberX500Name
 
     /**
-     * The current group parameters for this group.
+     * The current group parameters for the group represented by [groupId].
      */
     val groupParameters: GroupParameters
 
     /**
-     * The CPI whitelist for this group.
+     * The CPI whitelist for the group represented by [groupId].
      */
     val cpiWhiteList: CPIWhiteList
 
     /**
-     * Looks up a group member of the specified group by the public key SHA-256 hash
-     * belonging to the member.
+     * Returns a list of all visible [MemberInfo]s for the member represented by [owningMember]
+     * within the group represented by [groupId].
+     */
+    fun lookup(): Collection<MemberInfo>
+
+    /**
+     * Looks up a group member matching the public key SHA-256 hash as visible by the member represented
+     * by [owningMember] within the group represented by [groupId].
+     *
      * If the member is not found then the null value is returned.
      *
      * @param publicKeyHash Public key hash for the member to lookup.
@@ -41,8 +48,9 @@ interface MembershipGroupReader {
     fun lookup(publicKeyHash: PublicKeyHash): MemberInfo?
 
     /**
-     * Looks up a group member of the specified group by the MemberX500Name
-     * belonging to the member.
+     * Looks up a group member matching the [MemberX500Name] as visible by the member represented
+     * by [owningMember] within the group represented by [groupId].
+     *
      * If the member is not found then the null value is returned.
      *
      * @param name MemberX500Name of the member to lookup.
