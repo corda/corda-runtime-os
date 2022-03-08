44008aaf04c8b57b1c42830618d58dd9edd791e8
Miljenko Brkic <miljenko.brkic@r3.com>
2022-03-08 14:41:36 +0000

    CORE-2980 RPC api to return list of CPIs uploaded to the cluster

--- components/virtual-node/cpi-upload-rpcops-service/src/main/kotlin/net/corda/cpi/upload/endpoints/v1/CpiUploadRPCOpsImpl.kt
+++ components/virtual-node/cpi-upload-rpcops-service/src/main/kotlin/net/corda/cpi/upload/endpoints/v1/CpiUploadRPCOpsImpl.kt
@@ -2,10 +2,16 @@ package net.corda.cpi.upload.endpoints.v1
 
 import net.corda.cpi.upload.endpoints.common.CpiUploadRPCOpsHandler
 import net.corda.cpi.upload.endpoints.service.CpiUploadRPCOpsService
+import net.corda.cpiinfo.read.CpiInfoReadService
 import net.corda.httprpc.PluggableRPCOps
 import net.corda.httprpc.exception.ResourceNotFoundException
 import net.corda.libs.cpiupload.CpiUploadStatus
+import net.corda.libs.cpiupload.endpoints.v1.CpiIdentifier
+import net.corda.libs.cpiupload.endpoints.v1.CpiMetadata
 import net.corda.libs.cpiupload.endpoints.v1.CpiUploadRPCOps
+import net.corda.libs.cpiupload.endpoints.v1.CpkIdentifier
+import net.corda.libs.cpiupload.endpoints.v1.CpkMetadata
+import net.corda.libs.cpiupload.endpoints.v1.HTTPGetCPIsResponse
 import net.corda.lifecycle.Lifecycle
 import net.corda.lifecycle.LifecycleCoordinatorFactory
 import net.corda.lifecycle.createCoordinator
@@ -20,7 +26,9 @@ class CpiUploadRPCOpsImpl @Activate constructor(
     @Reference(service = LifecycleCoordinatorFactory::class)
     coordinatorFactory: LifecycleCoordinatorFactory,
     @Reference(service = CpiUploadRPCOpsService::class)
-    private val cpiUploadRPCOpsService: CpiUploadRPCOpsService
+    private val cpiUploadRPCOpsService: CpiUploadRPCOpsService,
+    @Reference(service = CpiInfoReadService::class)
+    private val cpiInfoReadService: CpiInfoReadService
 ) : CpiUploadRPCOps, PluggableRPCOps<CpiUploadRPCOps>, Lifecycle {
     companion object {
         val log = contextLogger()
@@ -38,7 +46,10 @@ class CpiUploadRPCOpsImpl @Activate constructor(
 
     override val isRunning get() = coordinator.isRunning
 
-    override fun start() = coordinator.start()
+    override fun start() {
+        coordinator.start()
+        cpiInfoReadService.start()
+    }
 
     override fun stop() = coordinator.close()
 
@@ -58,5 +69,31 @@ class CpiUploadRPCOpsImpl @Activate constructor(
             CpiUploadStatus.NO_SUCH_REQUEST_ID -> throw ResourceNotFoundException("No such request id '$id'")
             else -> return CpiUploadRPCOps.Status(status.toString())
         }
+    }
+
+    override fun getAllCpis(): HTTPGetCPIsResponse {
+        val cpis = cpiInfoReadService.getAll().map{ cpiMetadata ->
+            CpiMetadata(
+                createCpiIdentifier(cpiMetadata.id),
+                cpiMetadata.hash.toString(),
+                cpiMetadata.cpks.map{ createCpkMetadata(it) },
+                cpiMetadata.groupPolicy)
+        }
+
+        return HTTPGetCPIsResponse(cpis)
+    }
+
+    private fun createCpiIdentifier(cpiId: net.corda.libs.packaging.CpiIdentifier): CpiIdentifier {
+        return CpiIdentifier(cpiId.name, cpiId.version, cpiId.signerSummaryHash.toString())
+    }
+
+    private fun createCpkIdentifier(cpkId: net.corda.libs.packaging.CpkIdentifier): CpkIdentifier {
+        return CpkIdentifier(cpkId.name, cpkId.version, cpkId.signerSummaryHash.toString())
+    }
+
+    private fun createCpkMetadata(cpk: net.corda.libs.packaging.CpkMetadata): CpkMetadata {
+        return CpkMetadata(
+            createCpkIdentifier(cpk.id), cpk.mainBundle, cpk.libraries, cpk.dependencies.map{ createCpkIdentifier(it) },
+            cpk.type.toString(), cpk.hash.toString())
     }
 }
