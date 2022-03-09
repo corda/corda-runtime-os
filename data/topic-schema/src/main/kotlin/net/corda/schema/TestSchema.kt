c7bcd6ffaae06e6f82661d5d45517100d1873323
Yiftach Kaplan <67583323+yift-r3@users.noreply.github.com>
2022-03-09 14:02:55 +0000

    CORE-3702: Add hosting topic and message to the P2P testing schema (#266)
    This change adds a hosting topic and message to be used by the Link Manager.

--- data/topic-schema/src/main/kotlin/net/corda/schema/TestSchema.kt
+++ data/topic-schema/src/main/kotlin/net/corda/schema/TestSchema.kt
@@ -8,5 +8,6 @@ class TestSchema {
          */
         const val CRYPTO_KEYS_TOPIC = "p2p.crypto.keys"
         const val NETWORK_MAP_TOPIC = "p2p.netmap"
+        const val HOSTED_MAP_TOPIC = "p2p.hosted.identities"
     }
 }
