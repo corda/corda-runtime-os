ac84f5fd540a998451368d056ad7cb3fc943df02
Yiftach Kaplan <yiftach.kaplan@r3.com>
2022-03-10 09:37:12 +0000

    Use the latest tag

--- applications/tools/p2p-test/p2p-layer-deployment/src/main/kotlin/net/corda/p2p/deployment/commands/Deploy.kt
+++ applications/tools/p2p-test/p2p-layer-deployment/src/main/kotlin/net/corda/p2p/deployment/commands/Deploy.kt
@@ -95,7 +95,7 @@ class Deploy : Runnable {
         names = ["-t", "--tag"],
         description = ["The docker name of the tag to pull"]
     )
-    private var tag = "5.0.0.0-beta-1646650286154"
+    private var tag = "5.0.0.0-beta-1646903521823"
 
     @Option(
         names = ["--lm-conf", "--link-manager-config"],
