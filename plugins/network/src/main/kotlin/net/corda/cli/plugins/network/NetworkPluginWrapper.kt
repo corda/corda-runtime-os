64c4bec818eefa3eb3b1bcd396b54518d498215c
Yash Nabar <yash.nabar@r3.com>
2022-03-10 14:27:13 +0000

    CORE-3206: Add network plugin

---
+++ plugins/network/src/main/kotlin/net/corda/cli/plugins/network/NetworkPluginWrapper.kt
@@ -0,0 +1,52 @@
+package net.corda.cli.plugins.network
+
+import net.corda.cli.api.CordaCliPlugin
+import net.corda.cli.api.serviceUsers.HttpServiceUser
+import net.corda.cli.api.services.HttpService
+import org.pf4j.Extension
+import org.pf4j.Plugin
+import org.pf4j.PluginWrapper
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import picocli.CommandLine
+import picocli.CommandLine.Mixin
+
+class NetworkPluginWrapper(wrapper: PluginWrapper) : Plugin(wrapper) {
+    companion object {
+        private val logger: Logger = LoggerFactory.getLogger(NetworkPlugin::class.java)
+    }
+
+    override fun start() {
+        logger.debug("Network plugin started.")
+    }
+
+    override fun stop() {
+        logger.debug("Network plugin stopped.")
+    }
+
+    @Extension
+    @CommandLine.Command(
+        name = "network",
+        mixinStandardHelpOptions = true,
+        description = ["Plugin for deploying and managing a network."]
+    )
+    class NetworkPlugin : CordaCliPlugin, HttpServiceUser {
+        @Mixin
+        override lateinit var service: HttpService
+
+        @CommandLine.Command(
+            name = "members-list",
+            description = ["Shows the list of members on the network."]
+        )
+        fun getMembersList(
+            @CommandLine.Option(
+                names = ["-v", "--vnode-id"],
+                arity = "1",
+                description = ["Virtual node ID"]
+            ) virtualNodeId: String?
+        ) {
+            require(virtualNodeId != null)
+            service.get("/members?vnode-id=$virtualNodeId")
+        }
+    }
+}
