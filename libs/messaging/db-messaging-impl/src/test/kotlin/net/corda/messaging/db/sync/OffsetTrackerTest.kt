79ce5f4f6ad9c847ad395793ea592cc52f1ace32
Dimos Raptis <dimos.raptis@r3.com>
2022-03-14 11:27:46 +0000

    Detekt

--- libs/messaging/db-messaging-impl/src/test/kotlin/net/corda/messaging/db/sync/OffsetTrackerTest.kt
+++ libs/messaging/db-messaging-impl/src/test/kotlin/net/corda/messaging/db/sync/OffsetTrackerTest.kt
@@ -14,7 +14,6 @@ import org.mockito.kotlin.mock
 import java.util.concurrent.Callable
 import java.util.concurrent.Executors
 import java.util.concurrent.ScheduledExecutorService
-import java.util.concurrent.ScheduledFuture
 
 class OffsetTrackerTest {
 
