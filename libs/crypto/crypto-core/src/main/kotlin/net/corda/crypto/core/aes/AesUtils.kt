41d545e87bd1448e1fd3141eeac490635af18a9d
Alexey Kadyrov <alexey.kadyrov@r3.com>
2022-03-16 16:56:02 +0000

    Added HMAC calculation for streams

--- libs/crypto/crypto-core/src/main/kotlin/net/corda/crypto/core/aes/AesUtils.kt
+++ libs/crypto/crypto-core/src/main/kotlin/net/corda/crypto/core/aes/AesUtils.kt
@@ -2,15 +2,34 @@ package net.corda.crypto.core.aes
 
 import net.corda.crypto.core.ManagedSecret
 import net.corda.crypto.core.ManagedSecret.Companion.HMAC_ALGORITHM
+import java.io.InputStream
 import javax.crypto.Mac
 import javax.crypto.spec.SecretKeySpec
 
+const val STREAM_BUFFER_SIZE = DEFAULT_BUFFER_SIZE
+
 /**
- * Calculates HMAC of the [data].
+ * Calculates HMAC of the [data] [ByteArray].
  */
 fun ManagedSecret.hmacOf(data: ByteArray, algorithm: String = HMAC_ALGORITHM): ByteArray {
     val secretKeySpec = SecretKeySpec(secret, algorithm)
     val mac = Mac.getInstance(algorithm)
     mac.init(secretKeySpec)
     return mac.doFinal(data)
+}
+
+/**
+ * Calculates HMAC of the [inputStream] [InputStream].
+ */
+fun ManagedSecret.hmacOf(inputStream : InputStream, algorithm: String = HMAC_ALGORITHM): ByteArray {
+    val secretKeySpec = SecretKeySpec(secret, algorithm)
+    val mac = Mac.getInstance(algorithm)
+    mac.init(secretKeySpec)
+    val buffer = ByteArray(STREAM_BUFFER_SIZE)
+    while(true) {
+        val read = inputStream.read(buffer)
+        if(read <= 0) break
+        mac.update(buffer, 0, read)
+    }
+    return mac.doFinal()
 }
--- libs/crypto/crypto-core/src/test/kotlin/net/corda/crypto/core/aes/AesUtilsTests.kt
+++ libs/crypto/crypto-core/src/test/kotlin/net/corda/crypto/core/aes/AesUtilsTests.kt
@@ -3,6 +3,7 @@ package net.corda.crypto.core.aes
 import net.corda.crypto.core.ManagedSecret
 import org.junit.jupiter.api.Assertions.assertArrayEquals
 import org.junit.jupiter.api.Test
+import java.io.ByteArrayInputStream
 import java.time.Instant
 import kotlin.random.Random
 import kotlin.test.assertEquals
@@ -10,7 +11,7 @@ import kotlin.test.assertFalse
 
 class AesUtilsTests {
     @Test
-    fun `Should generate same HMAC(SHA256) for the same data`() {
+    fun `Should generate same HMAC(SHA256) for the same byte array data`() {
         val random = Random(Instant.now().toEpochMilli())
         val data = random.nextBytes(random.nextInt(1, 193))
         val secret = ManagedSecret.generate()
@@ -22,7 +23,7 @@ class AesUtilsTests {
     }
 
     @Test
-    fun `Should generate different HMAC(SHA256) for different data`() {
+    fun `Should generate different HMAC(SHA256) for different byte array data`() {
         val random = Random(Instant.now().toEpochMilli())
         val data1 = random.nextBytes(193)
         val data2 = random.nextBytes(193)
@@ -35,7 +36,7 @@ class AesUtilsTests {
     }
 
     @Test
-    fun `Should generate different HMAC(SHA256) for the different secrets but same data`() {
+    fun `Should generate different HMAC(SHA256) for the different secrets but same byte array data`() {
         val random = Random(Instant.now().toEpochMilli())
         val data = random.nextBytes(random.nextInt(1, 193))
         val secret1 = ManagedSecret.generate()
@@ -46,4 +47,65 @@ class AesUtilsTests {
         assertEquals(32, hmac2.size)
         assertFalse(hmac1.contentEquals(hmac2))
     }
-}
+
+    @Test
+    fun `Should generate same HMAC(SHA256) for the short length stream data`() {
+        val random = Random(Instant.now().toEpochMilli())
+        for ( i in 1..100) {
+            val data = random.nextBytes(i)
+            val secret = ManagedSecret.generate()
+            val stream = ByteArrayInputStream(data)
+            val hmac1 = secret.hmacOf(data)
+            val hmac2 = secret.hmacOf(stream)
+            assertEquals(32, hmac1.size)
+            assertEquals(32, hmac2.size)
+            assertArrayEquals(hmac1, hmac2)
+        }
+    }
+
+    @Test
+    fun `Should generate same HMAC(SHA256) for the medium length stream data`() {
+        val random = Random(Instant.now().toEpochMilli())
+        for ( i in 1..100) {
+            val len = random.nextInt(375, 2074)
+            val data = random.nextBytes(len)
+            val secret = ManagedSecret.generate()
+            val stream = ByteArrayInputStream(data)
+            val hmac1 = secret.hmacOf(data)
+            val hmac2 = secret.hmacOf(stream)
+            assertEquals(32, hmac1.size)
+            assertEquals(32, hmac2.size)
+            assertArrayEquals(hmac1, hmac2)
+        }
+    }
+
+    @Test
+    fun `Should generate same HMAC(SHA256) for the large length stream data`() {
+        val random = Random(Instant.now().toEpochMilli())
+        for ( i in 1..100) {
+            val len = random.nextInt(37_794, 63_987)
+            val data = random.nextBytes(len)
+            val secret = ManagedSecret.generate()
+            val stream = ByteArrayInputStream(data)
+            val hmac1 = secret.hmacOf(data)
+            val hmac2 = secret.hmacOf(stream)
+            assertEquals(32, hmac1.size)
+            assertEquals(32, hmac2.size)
+            assertArrayEquals(hmac1, hmac2)
+        }
+    }
+
+    @Test
+    fun `Should generate same HMAC(SHA256) for the streams with sizes around buffer size`() {
+        val random = Random(Instant.now().toEpochMilli())
+        for ( len in (STREAM_BUFFER_SIZE - 5)..(STREAM_BUFFER_SIZE + 5)) {
+            val data = random.nextBytes(len)
+            val secret = ManagedSecret.generate()
+            val stream = ByteArrayInputStream(data)
+            val hmac1 = secret.hmacOf(data)
+            val hmac2 = secret.hmacOf(stream)
+            assertEquals(32, hmac1.size)
+            assertEquals(32, hmac2.size)
+            assertArrayEquals(hmac1, hmac2)
+        }
+    }}
