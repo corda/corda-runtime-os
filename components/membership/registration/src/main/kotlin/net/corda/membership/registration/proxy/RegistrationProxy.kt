48fd740932d9136c56c9b4369b101386bb29d6b9
Yash Nabar <yash.nabar@r3.com>
2022-03-23 11:50:45 +0000

    CORE-4088: Separate RegistrationProxy interface

--- components/membership/registration/src/main/kotlin/net/corda/membership/registration/proxy/RegistrationProxy.kt
+++ components/membership/registration/src/main/kotlin/net/corda/membership/registration/proxy/RegistrationProxy.kt
@@ -1,9 +1,23 @@
 package net.corda.membership.registration.proxy
 
+import net.corda.lifecycle.Lifecycle
 import net.corda.membership.registration.MemberRegistrationService
+import net.corda.membership.registration.MembershipRequestRegistrationResult
+import net.corda.virtualnode.HoldingIdentity
 
 /**
- * API for registering a holding identity with the appropriate instance of [MemberRegistrationService].
- * Implementations of this proxy must coordinate lifecycles of all [MemberRegistrationService]s.
+ * Proxy for registering a holding identity with the appropriate instance of [MemberRegistrationService].
+ * Implementations of this interface must coordinate lifecycles of all [MemberRegistrationService]s.
  */
-interface RegistrationProxy : MemberRegistrationService
+interface RegistrationProxy : Lifecycle {
+    /**
+     * Retrieves the appropriate instance of [MemberRegistrationService] for a holding identity as specified in the CPI
+     * configuration, and forwards the registration request to it.
+     *
+     * @param member The holding identity of the virtual node requesting registration.
+     *
+     * @return The status of the registration request as reported by the [MemberRegistrationService].
+     * NOT_SUBMITTED is returned if something goes wrong while creating the request.
+     */
+    fun register(member: HoldingIdentity): MembershipRequestRegistrationResult
+}
