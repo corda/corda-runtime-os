d53baadab5c192af71655c222b0aeda8d94b92e0
LWogan <lorcan.wogan@r3.com>
2022-03-25 07:29:41 +0000

    CORE-3781 dans pr comments

--- libs/messaging/message-bus/src/main/kotlin/net/corda/messagebus/api/constants/ConsumerRoles.kt
+++ libs/messaging/message-bus/src/main/kotlin/net/corda/messagebus/api/constants/ConsumerRoles.kt
@@ -6,30 +6,13 @@ package net.corda.messagebus.api.constants
  * constants here should reflect the paths that the config can be found in enforced/default config files for the various
  * bus implementations.
  */
-enum class ConsumerRoles {
-    PUBSUB {
-        override val configPath = "pubsub.consumer"
-    },
-    COMPACTED {
-        override val configPath = "compacted.consumer"
-    },
-    DURABLE {
-        override val configPath = "durable.consumer"
-    },
-    SAE_STATE {
-        override val configPath = "stateAndEvent.stateConsumer"
-    },
-    SAE_EVENT {
-        override val configPath = "stateAndEvent.eventConsumer"
-    },
-    EVENT_LOG {
-        override val configPath = "eventLog.consumer"
-    },
-    RPC_SENDER {
-        override val configPath = "rpcSender.consumer"
-    },
-    RPC_RESPONDER {
-        override val configPath = "rpcResponder.consumer"
-    };
-    abstract val configPath: String
+enum class ConsumerRoles(val configPath: String) {
+    PUBSUB ("pubsub.consumer"),
+    COMPACTED ("compacted.consumer"),
+    DURABLE ("durable.consumer"),
+    SAE_STATE ("stateAndEvent.stateConsumer"),
+    SAE_EVENT ("stateAndEvent.eventConsumer"),
+    EVENT_LOG ("eventLog.consumer"),
+    RPC_SENDER ("rpcSender.consumer"),
+    RPC_RESPONDER("rpcResponder.consumer")
 }
--- libs/messaging/message-bus/src/main/kotlin/net/corda/messagebus/api/constants/ProducerRoles.kt
+++ libs/messaging/message-bus/src/main/kotlin/net/corda/messagebus/api/constants/ProducerRoles.kt
@@ -6,24 +6,11 @@ package net.corda.messagebus.api.constants
  * constants here should reflect the paths that the config can be found in enforced/default config files for the various
  * bus implementations.
  */
-enum class ProducerRoles {
-    PUBLISHER {
-        override val configPath = "publisher.producer"
-    },
-    DURABLE {
-        override val configPath = "durable.producer"
-    },
-    SAE_PRODUCER {
-        override val configPath = "stateAndEvent.producer"
-    },
-    EVENT_LOG {
-        override val configPath = "eventLog.producer"
-    },
-    RPC_SENDER {
-        override val configPath = "rpcSender.producer"
-    },
-    RPC_RESPONDER {
-        override val configPath = "rpcResponder.producer"
-    };
-    abstract val configPath: String
+enum class ProducerRoles(val configPath: String) {
+    PUBLISHER("publisher.producer"),
+    DURABLE("durable.producer"),
+    SAE_PRODUCER("stateAndEvent.producer"),
+    EVENT_LOG("eventLog.producer"),
+    RPC_SENDER("rpcSender.producer"),
+    RPC_RESPONDER("rpcResponder.producer")
 }
