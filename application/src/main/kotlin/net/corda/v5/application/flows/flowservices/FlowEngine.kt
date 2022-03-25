d8aad8168de7d0bf3a2dc0b073d36a30417ce5be
Owen Stanford <92725587+owenstanford@users.noreply.github.com>
2022-03-25 15:00:34 +0000

    CORE-3767 Added virtual node x500 name to the flow engine (#296)
    - Adds the Virtual node X500 name to flow engine
    - Removes superfluous flow API

--- application/src/main/kotlin/net/corda/v5/application/flows/flowservices/FlowEngine.kt
+++ application/src/main/kotlin/net/corda/v5/application/flows/flowservices/FlowEngine.kt
@@ -2,14 +2,13 @@ package net.corda.v5.application.flows.flowservices
 
 import net.corda.v5.application.flows.Flow
 import net.corda.v5.application.flows.FlowException
-import net.corda.v5.application.flows.FlowExternalOperation
 import net.corda.v5.application.flows.InitiatingFlow
-import net.corda.v5.application.flows.KilledFlowException
-import net.corda.v5.application.flows.FlowId
 import net.corda.v5.application.injection.CordaFlowInjectable
 import net.corda.v5.base.annotations.DoNotImplement
 import net.corda.v5.base.annotations.Suspendable
+import net.corda.v5.base.types.MemberX500Name
 import java.time.Duration
+import java.util.*
 
 @DoNotImplement
 interface FlowEngine : CordaFlowInjectable {
@@ -17,33 +16,12 @@ interface FlowEngine : CordaFlowInjectable {
      * Returns a wrapped [UUID][java.util.UUID] object that identifies this flow or it's top level instance (i.e. subflows have the same
      * identifier as their parents).
      */
-    val flowId: FlowId
+    val flowId: UUID
 
     /**
-     * Returns `true` when the current [Flow] has been killed (has received a command to halt its progress and terminate).
-     *
-     * Check this property in long-running computation loops to exit a flow that has been killed:
-     * ```
-     * while (!isKilled) {
-     *   // do some computation
-     * }
-     * ```
-     *
-     * Ideal usage would include throwing a [KilledFlowException] which will lead to the termination of the flow:
-     * ```
-     * for (item in list) {
-     *   if (isKilled) {
-     *     throw KilledFlowException(flowId)
-     *   }
-     *   // do some computation
-     * }
-     * ```
-     *
-     * Note, once the [isKilled] flag is set to `true` the flow may terminate once it reaches the next API function marked with the
-     * @[Suspendable] annotation. Therefore, it is possible to write a flow that does not interact with the [isKilled] flag while still
-     * terminating correctly.
+     * Returns the x500 name of the current virtual node executing the flow.
      */
-    val isKilled: Boolean
+    val virtualNodeName: MemberX500Name
 
     /**
      * Invokes the given subflow. This function returns once the subflow completes successfully with the result
@@ -61,39 +39,6 @@ interface FlowEngine : CordaFlowInjectable {
     fun <R> subFlow(subLogic: Flow<R>): R
 
     /**
-     * Helper function that throws a [KilledFlowException] if the current [Flow] has been killed.
-     *
-     * Call this function in long-running computation loops to exit a flow that has been killed:
-     * ```
-     * for (item in list) {
-     *   checkFlowIsNotKilled()
-     *   // do some computation
-     * }
-     * ```
-     *
-     * See the [isKilled] property for more information.
-     */
-    @Suspendable
-    fun checkFlowIsNotKilled()
-
-    /**
-     * Helper function that throws a [KilledFlowException] if the current [Flow] has been killed. The provided message is added to the
-     * thrown [KilledFlowException].
-     *
-     * Call this function in long-running computation loops to exit a flow that has been killed:
-     * ```
-     * for (item in list) {
-     *   checkFlowIsNotKilled { "The flow $flowId was killed while iterating through the list of items" }
-     *   // do some computation
-     * }
-     * ```
-     *
-     * See the [isKilled] property for more information.
-     */
-    @Suspendable
-    fun checkFlowIsNotKilled(lazyMessage: () -> Any)
-
-    /**
      * Suspends the flow and only wakes it up after at least [duration] time has passed.
      *
      * Warning: long sleeps and in general long running flows are highly discouraged, as there is currently no
@@ -103,15 +48,5 @@ interface FlowEngine : CordaFlowInjectable {
      */
     @Suspendable
     fun sleep(duration: Duration)
-
-    /**
-     * Suspends a flow until [FlowExternalOperation] is completed. Once the [FlowExternalOperation] gets completed the flow will resume and
-     * return result [R].
-     *
-     * @param operation The [FlowExternalOperation] to be executed asynchronously.
-     * @return The result [R] of [FlowExternalOperation.execute].
-     */
-    @Suspendable
-    fun <R : Any> await(operation: FlowExternalOperation<R>): R
 }
 
--- application/src/test/java/net/corda/v5/application/flows/flowservices/FlowEngineJavaApiTest.java
+++ application/src/test/java/net/corda/v5/application/flows/flowservices/FlowEngineJavaApiTest.java
@@ -1,17 +1,11 @@
 package net.corda.v5.application.flows.flowservices;
 
-import kotlin.jvm.functions.Function0;
 import net.corda.v5.application.flows.Flow;
-import net.corda.v5.application.flows.FlowExternalOperation;
-import net.corda.v5.application.flows.FlowId;
 import org.assertj.core.api.Assertions;
-import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.Test;
-
 import java.time.Duration;
 import java.util.UUID;
 
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
@@ -24,23 +18,13 @@ public class FlowEngineJavaApiTest {
 
     @Test
     public void getFlowId() {
-        FlowId test = new FlowId(UUID.randomUUID());
+        UUID test = UUID.randomUUID();
         when(flowEngine.getFlowId()).thenReturn(test);
 
-        FlowId result = flowEngine.getFlowId();
+        UUID result = flowEngine.getFlowId();
 
         Assertions.assertThat(result).isNotNull();
         Assertions.assertThat(result).isEqualTo(test);
-    }
-
-    @Test
-    public void isKilled() {
-        when(flowEngine.isKilled()).thenReturn(true);
-
-        Boolean result = flowEngine.isKilled();
-
-        Assertions.assertThat(result).isNotNull();
-        Assertions.assertThat(result).isEqualTo(true);
     }
 
     @Test
@@ -57,54 +41,9 @@ public class FlowEngineJavaApiTest {
     }
 
     @Test
-    public void checkFlowIsNotKilled() {
-        flowEngine.checkFlowIsNotKilled();
-        verify(flowEngine, times(1)).checkFlowIsNotKilled();
-    }
-
-    @Test
-    public void checkFlowIsNotKilledLazyMessage() {
-        Function0<Integer> lambda = () -> 1;
-        flowEngine.checkFlowIsNotKilled(lambda);
-        verify(flowEngine, times(1)).checkFlowIsNotKilled(lambda);
-    }
-
-    @Test
-    public void checkFlowIsNotKilledLazyMessageWithLambda() {
-        flowEngine.checkFlowIsNotKilled(() -> 1);
-        verify(flowEngine, times(1)).checkFlowIsNotKilled(any());
-    }
-
-    @Test
     public void sleep() {
         Duration duration = Duration.ZERO;
         flowEngine.sleep(duration);
         verify(flowEngine, times(1)).sleep(duration);
-    }
-
-    @Test
-    public void await() {
-        MyFlowExternalOperation flowExternalOperation = new MyFlowExternalOperation("testProperty");
-        doReturn("testProperty").when(flowEngine).await(flowExternalOperation);
-
-        String result = flowEngine.await(flowExternalOperation);
-
-        Assertions.assertThat(result).isNotNull();
-        Assertions.assertThat(result).isEqualTo("testProperty");
-        verify(flowEngine, times(1)).await(flowExternalOperation);
-    }
-
-    static class MyFlowExternalOperation implements FlowExternalOperation<String> {
-
-        private final String property;
-
-        MyFlowExternalOperation(String property) {
-            this.property = property;
-        }
-
-        @Override
-        public String execute(@NotNull String deduplicationId) {
-            return property;
-        }
     }
 }
