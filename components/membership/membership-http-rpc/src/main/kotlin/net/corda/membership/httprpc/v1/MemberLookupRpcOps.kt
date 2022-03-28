85ef487dc84a2c6b91de97d746b6de18ee3fd354
Yash Nabar <yash.nabar@r3.com>
2022-03-28 17:40:30 +0100

    CORE-3205: Update descriptions

--- components/membership/membership-http-rpc/src/main/kotlin/net/corda/membership/httprpc/v1/MemberLookupRpcOps.kt
+++ components/membership/membership-http-rpc/src/main/kotlin/net/corda/membership/httprpc/v1/MemberLookupRpcOps.kt
@@ -23,17 +23,17 @@ interface MemberLookupRpcOps : RpcOps {
     fun lookup(
         @HttpRpcQueryParameter(description = "ID of the holding identity to be checked.")
         holdingIdentityId: String,
-        @HttpRpcQueryParameter(name="cn", description = "CN attribute of the X.500 name to filter members by", required = false)
+        @HttpRpcQueryParameter(name="cn", description = "Common Name (CN) attribute of the X.500 name to filter members by", required = false)
         commonName: String? = null,
-        @HttpRpcQueryParameter(name="o", description = "O attribute of the X.500 name to filter members by", required = false)
+        @HttpRpcQueryParameter(name="o", description = "Organisation (O) attribute of the X.500 name to filter members by", required = false)
         organisation: String? = null,
-        @HttpRpcQueryParameter(name="ou", description = "OU attribute of the X.500 name to filter members by", required = false)
+        @HttpRpcQueryParameter(name="ou", description = "Organisation Unit (OU) attribute of the X.500 name to filter members by", required = false)
         organisationUnit: String? = null,
-        @HttpRpcQueryParameter(name="l", description = "L attribute of the X.500 name to filter members by", required = false)
+        @HttpRpcQueryParameter(name="l", description = "Locality (L) attribute of the X.500 name to filter members by", required = false)
         locality: String? = null,
-        @HttpRpcQueryParameter(name="st", description = "ST attribute of the X.500 name to filter members by", required = false)
+        @HttpRpcQueryParameter(name="st", description = "State (ST) attribute of the X.500 name to filter members by", required = false)
         state: String? = null,
-        @HttpRpcQueryParameter(name="c", description = "C attribute of the X.500 name to filter members by", required = false)
+        @HttpRpcQueryParameter(name="c", description = "Country (C) attribute of the X.500 name to filter members by", required = false)
         country: String? = null
     ): RpcMemberInfoList
 }
