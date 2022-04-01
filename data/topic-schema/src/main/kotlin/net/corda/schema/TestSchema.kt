8be5ddac20b3110c54b662b36ef8ad6d63e5c006
Yiftach Kaplan <67583323+yift-r3@users.noreply.github.com>
2022-04-01 14:04:54 +0100

    CORE-3732: Add test app topic (#315)

--- data/topic-schema/src/main/kotlin/net/corda/schema/TestSchema.kt
+++ data/topic-schema/src/main/kotlin/net/corda/schema/TestSchema.kt
@@ -10,5 +10,6 @@ class TestSchema {
         const val GROUP_POLICIES_TOPIC = "p2p.group.policies"
         const val MEMBER_INFO_TOPIC = "p2p.members.info"
         const val HOSTED_MAP_TOPIC = "p2p.hosted.identities"
+        const val APP_RECEIVED_MESSAGES_TOPIC = "p2p.app.received_msg"
     }
 }
