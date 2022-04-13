fcf15a7a9b471a7bcfd8d5fd39c804bd055f25ee
Barry Lapthorn <barry.lapthorn@r3.com>
2022-04-13 13:46:01 +0100

    CORE-4095 db sandbox/persist entities
    Basic code, with a few unit tests, and the basic OSGI
    integration tests set up as well.
    
    Remove getUniqueObject extension method (fails to work with mocks)
    
    Slight hack to register custom serializers for the entities until
    I figure out how the serializers work.

---
+++ testing/cpbs/extendable-cpb/testing-dogs-cpk/src/main/kotlin/net/corda/testing/cpks/dogs/Dog.kt
@@ -0,0 +1,24 @@
+package net.corda.testing.cpks.dogs
+
+import net.corda.v5.base.annotations.CordaSerializable
+import java.time.Instant
+import java.util.UUID
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.Id
+
+@CordaSerializable
+@Entity
+data class Dog(
+    @Id
+    @Column
+    val id: UUID,
+    @Column
+    val name: String,
+    @Column
+    val birthdate: Instant,
+    @Column
+    val owner: String
+) {
+    constructor() : this(id = UUID.randomUUID(), name = "", birthdate = Instant.now(), owner = "")
+}
