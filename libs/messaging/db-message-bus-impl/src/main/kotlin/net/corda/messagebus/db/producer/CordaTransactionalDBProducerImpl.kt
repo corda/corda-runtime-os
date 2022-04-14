bb5b725e7fc82077af8169abac45f59127794e38
James Higgs <james.higgs@r3.com>
2022-04-14 16:40:30 +0100

    NOTICK: Further tidy up

--- libs/messaging/db-message-bus-impl/src/main/kotlin/net/corda/messagebus/db/producer/CordaTransactionalDBProducerImpl.kt
+++ libs/messaging/db-message-bus-impl/src/main/kotlin/net/corda/messagebus/db/producer/CordaTransactionalDBProducerImpl.kt
@@ -143,7 +143,6 @@ class CordaTransactionalDBProducerImpl(
                 transaction
             )
         }
-        println("Writing offsets: \n${offsets.joinToString("\n")}")
         dbAccess.writeOffsets(offsets)
     }
 
