4258b25ddb49aaa2a8cbc34b68704924bfaaffdd
Will Vigor <william.vigor@r3.com>
2022-04-14 17:14:58 +0100

    Remove unneeded logging

--- components/gateway/src/main/kotlin/net/corda/p2p/gateway/messaging/internal/OutboundMessageHandler.kt
+++ components/gateway/src/main/kotlin/net/corda/p2p/gateway/messaging/internal/OutboundMessageHandler.kt
@@ -98,7 +98,6 @@ internal class OutboundMessageHandler(
                         peerMessage.header.address
                     )
                     val messageId = UUID.randomUUID().toString()
-                    logger.info("Sending message with $messageId")
                     val gatewayMessage = GatewayMessage(messageId, peerMessage.payload)
                     val expectedX500Name = if (NetworkType.CORDA_4 == peerMessage.header.destinationNetworkType) {
                         X500Name(peerMessage.header.destinationIdentity.x500Name)
@@ -123,7 +122,6 @@ internal class OutboundMessageHandler(
 
     private fun scheduleHandleResponse(pendingRequest: PendingRequest) {
         threadPool.schedule({
-            logger.info("Handle response.")
             val (response, error) = try {
                 val response = pendingRequest.future.get(0, TimeUnit.MILLISECONDS)
                 response to null
