2f48db8e5ce85f97b95434f00dfac254275e004d
Ben McMahon <benmcmahon100@gmail.com>
2022-04-22 11:10:18 +0100

    NOTICK: Move plugins to runtime-os repo (#1032)
    * Move to new bootstrap process
    
    * Make db host required
    
    * Make auto bootstrap optional
    
    * Simplify bootstrap logic
    
    * Fix chart to pull right image
    
    * Add membership worker

--- processors/db-processor/src/main/kotlin/net/corda/processors/db/internal/DBProcessorImpl.kt
+++ processors/db-processor/src/main/kotlin/net/corda/processors/db/internal/DBProcessorImpl.kt
@@ -11,8 +11,6 @@ import net.corda.db.admin.impl.ClassloaderChangeLog
 import net.corda.db.admin.impl.ClassloaderChangeLog.ChangeLogResourceFiles
 import net.corda.db.connection.manager.DbAdmin
 import net.corda.db.connection.manager.DbConnectionManager
-import net.corda.db.connection.manager.dbFallbackConfig
-import net.corda.db.core.DbPrivilege
 import net.corda.db.schema.CordaDb
 import net.corda.db.schema.DbSchema
 import net.corda.libs.configuration.SmartConfig
@@ -35,7 +33,6 @@ import net.corda.permissions.storage.reader.PermissionStorageReaderService
 import net.corda.permissions.storage.writer.PermissionStorageWriterService
 import net.corda.processors.db.DBProcessor
 import net.corda.processors.db.DBProcessorException
-import net.corda.schema.configuration.ConfigKeys
 import net.corda.v5.base.util.contextLogger
 import net.corda.v5.base.util.debug
 import net.corda.virtualnode.write.db.VirtualNodeWriteService
@@ -43,7 +40,6 @@ import org.osgi.service.component.annotations.Activate
 import org.osgi.service.component.annotations.Component
 import org.osgi.service.component.annotations.Reference
 import java.sql.SQLException
-import java.util.UUID
 import javax.sql.DataSource
 import net.corda.schema.configuration.ConfigKeys.DB_CONFIG
 
@@ -136,9 +132,6 @@ class DBProcessorImpl @Activate constructor(
                 if (event.registration == dbManagerRegistrationHandler) {
                     log.info("DB Connection Manager has been initialised")
 
-                    // TODO - remove this when cluster bootstrapping is implemented
-                    tempDbInitProcess(bootstrapConfig!!)
-
                     // ready to continue bootstrapping processor
                     log.info("Bootstrapping Config Write Service with instance ID: $instanceId")
                     configWriteService.startProcessing(
@@ -168,64 +161,6 @@ class DBProcessorImpl @Activate constructor(
                 log.error("Unexpected event $event!")
             }
         }
-    }
-
-    private fun tempDbInitProcess(config: SmartConfig) {
-        log.info("Running Cluster DB Migration")
-        migrateDatabase(dbConnectionManager.getClusterDataSource(), listOf(
-            "net/corda/db/schema/config/db.changelog-master.xml"
-        ))
-
-        val dbConfig = config.getConfig(DB_CONFIG).withFallback(dbFallbackConfig)
-
-        // Creating RBAC DB configurations
-        if(null == dbConnectionManager.getDataSource(CordaDb.RBAC.persistenceUnitName, DbPrivilege.DDL)) {
-            val ddlRbacUser = "rbac_ddl"
-            val ddlRbacPassword = UUID.randomUUID().toString()
-            dbAdmin.createDbAndUser(
-                CordaDb.RBAC.persistenceUnitName,
-                DbSchema.RPC_RBAC,
-                ddlRbacUser,
-                ddlRbacPassword,
-                dbConfig.getString(ConfigKeys.JDBC_URL),
-                DbPrivilege.DDL,
-                config.factory
-            )
-        }
-
-        if(null == dbConnectionManager.getDataSource(CordaDb.RBAC.persistenceUnitName, DbPrivilege.DML)) {
-            val dmlRbacUser = "rbac_dml"
-            val dmlRbacPassword = UUID.randomUUID().toString()
-            dbAdmin.createDbAndUser(
-                CordaDb.RBAC.persistenceUnitName,
-                DbSchema.RPC_RBAC,
-                dmlRbacUser,
-                dmlRbacPassword,
-                dbConfig.getString(ConfigKeys.JDBC_URL),
-                DbPrivilege.DML,
-                config.factory
-            )
-        }
-
-        log.info("Running RBAC DB Migration")
-        /** TODO - this is a bit hacky
-         *   We can't really use the DDL user created above because it does not have CREATE privileges
-         *   on the public schema, therefore, it cannot create new schemas and our Liquibase migration
-         *   currently has a IF NOT EXIST CREATE SCHEMA ... which fails if the permission is missing.
-         *   For this reason, for VNode Vault and Crypto DBs, we should not use schemas but assume the
-         *   "default" schema that is specified in the connection details.
-         *
-         *   For RBAC, CONFIG etc, we need to have a discussion and decision on how we handle this.
-         *   Maybe it doesn't actually make sense to keep DDL connection details for RBAC, and maybe we
-         *   can always assume the DB Migrations for system tables (i.e. not vault) are always handled
-         *   externally?
-         *
-         *   Until then, just use the cluster DB.
-         */
-        migrateDatabase(
-            dbConnectionManager.getClusterDataSource(),
-            listOf("net/corda/db/schema/rbac/db.changelog-master.xml"),
-            DbSchema.RPC_RBAC)
     }
 
     /**
