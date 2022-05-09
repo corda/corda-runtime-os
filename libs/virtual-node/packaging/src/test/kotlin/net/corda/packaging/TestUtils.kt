2b4722931f4fdd84f2760e65852835fad04b287e
Joseph Zuniga-Daly <joseph.zunigadaly@r3.com>
2022-05-09 18:00:20 +0100

    CORE-4354: Refactor V1 packaging code to prepare for V2
    - Move packaging-avro-converters from corda-api into corda-runtime-os
    - Move packaging module from corda-api to corda-runtime-os

---
+++ libs/virtual-node/packaging/src/test/kotlin/net/corda/packaging/TestUtils.kt
@@ -0,0 +1,67 @@
+package net.corda.packaging
+
+import org.osgi.framework.Constants.BUNDLE_SYMBOLICNAME
+import org.osgi.framework.Constants.BUNDLE_VERSION
+import java.util.jar.Manifest
+
+/**
+ * Test utilities shared across multiple `install` tests.
+ */
+internal object TestUtils {
+    internal const val MANIFEST_DUMMY_BUNDLE_SYMBOLIC_NAME = "DummyBundleSymbolicName"
+    internal const val MANIFEST_DUMMY_BUNDLE_VERSION = "DummyBundleVersion"
+    internal const val MANIFEST_DUMMY_MIN_PLATFORM_VERSION = "222"
+    internal const val MANIFEST_DUMMY_TARGET_PLATFORM_VERSION = "333"
+    internal val MANIFEST_CONTRACT_INFO = ManifestCordappInfo(
+            "contractName", "contractVendor", 444, "contractLicence")
+    internal val MANIFEST_WORKFLOW_INFO = ManifestCordappInfo(
+            "workflowName", "workflowVendor", 555, "workflowLicence")
+
+    internal const val MANIFEST_DUMMY_CONTRACTS = "contractClassOne, contractClassTwo"
+    internal const val MANIFEST_DUMMY_FLOWS = "flowClassOne, flowClassTwo"
+
+    /** Creates a dummy [Manifest] containing the CorDapp-specific values provided. */
+    @Suppress("LongParameterList")
+    internal fun createDummyCordappManifest(
+            bundleSymbolicName: String? = MANIFEST_DUMMY_BUNDLE_SYMBOLIC_NAME,
+            bundleVersion: String? = MANIFEST_DUMMY_BUNDLE_VERSION,
+            minPlatformVersion: String? = MANIFEST_DUMMY_MIN_PLATFORM_VERSION,
+            targetPlatformVersion: String? = MANIFEST_DUMMY_TARGET_PLATFORM_VERSION,
+            contractShortName: String? = MANIFEST_CONTRACT_INFO.shortName,
+            contractVendor: String? = MANIFEST_CONTRACT_INFO.vendor,
+            contractVersionId: String? = MANIFEST_CONTRACT_INFO.versionId.toString(),
+            contractLicence: String? = MANIFEST_CONTRACT_INFO.licence,
+            workflowShortName: String? = MANIFEST_WORKFLOW_INFO.shortName,
+            workflowVendor: String? = MANIFEST_WORKFLOW_INFO.vendor,
+            workflowVersionId: String? = MANIFEST_WORKFLOW_INFO.versionId.toString(),
+            workflowLicence: String? = MANIFEST_WORKFLOW_INFO.licence,
+            contracts: String? = MANIFEST_DUMMY_CONTRACTS,
+            flows: String? = MANIFEST_DUMMY_FLOWS
+    ): Manifest {
+        val manifestHeaders = listOf(BUNDLE_SYMBOLICNAME, BUNDLE_VERSION,
+                CordappManifest.MIN_PLATFORM_VERSION,
+                CordappManifest.TARGET_PLATFORM_VERSION,
+                CordappManifest.CORDAPP_CONTRACT_NAME,
+                CordappManifest.CORDAPP_CONTRACT_VENDOR,
+                CordappManifest.CORDAPP_CONTRACT_VERSION,
+                CordappManifest.CORDAPP_CONTRACT_LICENCE,
+                CordappManifest.CORDAPP_WORKFLOW_NAME,
+                CordappManifest.CORDAPP_WORKFLOW_VENDOR,
+                CordappManifest.CORDAPP_WORKFLOW_VERSION,
+                CordappManifest.CORDAPP_WORKFLOW_LICENCE,
+                CordappManifest.CORDAPP_CONTRACTS,
+                CordappManifest.CORDAPP_FLOWS)
+        val manifestValues = listOf(bundleSymbolicName, bundleVersion, minPlatformVersion, targetPlatformVersion,
+                contractShortName, contractVendor, contractVersionId, contractLicence, workflowShortName,
+                workflowVendor, workflowVersionId, workflowLicence, contracts, flows)
+        val manifestHeadersAndValues = manifestHeaders
+                .zip(manifestValues)
+                .filter { (_, value) -> value != null }
+
+        val manifestString = manifestHeadersAndValues.joinToString("\n", postfix = "\n") { (header, value) ->
+            "$header: $value"
+        }
+
+        return Manifest(manifestString.byteInputStream())
+    }
+}
---
+++ libs/virtual-node/packaging/src/test/kotlin/net/corda/packaging/versioning/VersionComparatorFuzzerTests.kt
@@ -0,0 +1,109 @@
+package net.corda.packaging.versioning
+
+import net.corda.packaging.VersionComparator
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.Test
+import kotlin.random.Random
+
+// Remove this annotation to run 100,000 fuzz tests to see if anything is broken.
+@Disabled
+class VersionComparatorFuzzerTests {
+    companion object {
+        private val randomPool: List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9') + listOf(':', '-', '!')
+        private val numberPool: List<Char> = ('0'..'9') + emptyList()
+        private val alphaPool: List<Char> = ('a'..'z') + ('A'..'Z')
+        const val TOTAL = 100000
+
+        fun randomString(length: Int): String = randomChars(length, randomPool)
+
+        private fun randomNumber(length: Int): String = randomChars(length, numberPool)
+
+        private fun randomAlpha(): String = randomChars(Random.nextInt(1, 10), alphaPool)
+
+        private fun randomChars(length: Int, charPool: List<Char>): String = (1..length)
+            .map { Random.nextInt(0, charPool.size) }
+            .map(charPool::get)
+            .joinToString("")
+
+        private fun randomVersion(): String =
+            (1..Random.nextInt(1, 4))
+                .joinToString(".") { randomNumber(Random.nextInt(1, 6)) }
+
+        private fun randomEpochAndVersion(): String =
+            listOf(randomAlpha(), randomVersion()).joinToString(":")
+
+        private fun randomVersionAndRelease(): String =
+            listOf(randomVersion(), randomAlpha()).joinToString("-")
+
+        private fun randomFullVersion(): String =
+            listOf(randomEpochAndVersion(), randomAlpha()).joinToString("-")
+    }
+
+    @Suppress("TooGenericExceptionCaught")
+    private fun testVersion(v1: String, v2: String) {
+        try {
+            // we don't know whether the result = -1 or +1 (or even zero)
+            val result = VersionComparator.cmp(v1, v2)
+            val oppositeResult = VersionComparator.cmp(v2, v1)
+
+            // Check equality
+            Assertions.assertEquals(0, VersionComparator.cmp(v1, v1), "when testing with \"$v1\", \"$v2\"")
+            Assertions.assertEquals(0, VersionComparator.cmp(v2, v2), "when testing with \"$v1\", \"$v2\"")
+
+            // But we know the opposite result (+1, 0, -1) plus original result (-1, 0, +1) must be zero
+            // Ensures comparison is commutative
+            Assertions.assertEquals(0, result + oppositeResult, "when testing with \"$v1\", \"$v2\"")
+        } catch (e: Exception) {
+            // Catch and print out the failing strings for testing
+            Assertions.fail("when calling  VersionComparator.ncmp(\"$v1\", \"$v2\")")
+        }
+    }
+
+    @Test
+    fun `fuzz completely random strings`() {
+        for (i in 1..20) {
+            for (j in 1..TOTAL) {
+                val v1 = randomString(i)
+                val v2 = randomString(i)
+                testVersion(v1, v2)
+            }
+        }
+    }
+
+    @Test
+    fun `fuzz version strings only`() {
+        for (i in 1..TOTAL) {
+            val v1 = randomVersion()
+            val v2 = randomVersion()
+            testVersion(v1, v2)
+        }
+    }
+
+    @Test
+    fun `fuzz epoch-version strings only`() {
+        for (i in 1..TOTAL) {
+            val v1 = randomEpochAndVersion()
+            val v2 = randomEpochAndVersion()
+            testVersion(v1, v2)
+        }
+    }
+
+    @Test
+    fun `fuzz version-release strings only`() {
+        for (i in 1..TOTAL) {
+            val v1 = randomVersionAndRelease()
+            val v2 = randomVersionAndRelease()
+            testVersion(v1, v2)
+        }
+    }
+
+    @Test
+    fun `fuzz full version strings only`() {
+        for (i in 1..TOTAL) {
+            val v1 = randomFullVersion()
+            val v2 = randomVersionAndRelease()
+            testVersion(v1, v2)
+        }
+    }
+}
