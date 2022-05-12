a967ad94dc982ddaa28adec726c8a2383533e37e
Joseph Zuniga-Daly <joseph.zunigadaly@r3.com>
2022-05-12 17:43:18 +0100

    Delete Cpi.assemble, CpiBuilder and com.typesafe:config dependency

--- libs/packaging/packaging/src/main/kotlin/net/corda/packaging/Cpi.kt
+++ libs/packaging/packaging/src/main/kotlin/net/corda/packaging/Cpi.kt
@@ -1,14 +1,11 @@
 package net.corda.packaging
 
-import net.corda.packaging.internal.CpiBuilder
 import net.corda.packaging.internal.CpiIdentifierImpl
 import net.corda.packaging.internal.CpiLoader
 import net.corda.packaging.internal.CpiMetadataImpl
 import net.corda.packaging.internal.jarSignatureVerificationEnabledByDefault
 import net.corda.v5.crypto.SecureHash
 import java.io.InputStream
-import java.io.OutputStream
-import java.io.Reader
 import java.nio.file.Path
 
 interface Cpi : AutoCloseable {
@@ -38,40 +35,6 @@ interface Cpi : AutoCloseable {
                  cpiLocation : String? = null,
                  verifySignature : Boolean = jarSignatureVerificationEnabledByDefault()) : Cpi =
             CpiLoader.loadCpi(inputStream, expansionLocation, cpiLocation, verifySignature)
-
-        /**
-         * Creates a new a [Cpi] archive containing the CPKs specified in [cpkFiles] and dumps it to [outputStream]
-         * (which is guaranteed to be closed when the method returns), this method will also attempt to resolve all
-         * the included CPK dependencies and raises [DependencyResolutionException] if any dependency is not met.
-         * If [useSignatures] is false, the CPK signatures won't be verified and won't be taken in consideration
-         * in the dependency resolution (an unsigned CPK or a CPK signed by a different key will be considered
-         * an equally good candidate to meet a dependency).
-         * A [metadataReader] can also be specified to include an HOCON configuration file with metadata
-         * in the generated archive.
-         *
-         * @param name the CPI name
-         * @param version CPI version
-         * @param destination the [OutputStream] where the CPI wil be written
-         * @param cpkFiles a collection of [Path]s to CPK files
-         * @param cpkArchives a collection of directories that will be treated as a CPK file repositories for
-         * the purpose of dependency resolution (missing CPK file dependencies will be looked up there)
-         * @param metadataReader an optional [Reader] providing the membership metadata file
-         * @param signingParams
-         * @param useSignatures whether to verify and use jar signatures on CPK file (it can affect dependency
-         * resolution as [Cpk.Identifier.signerSummaryHash] won't be parsed if [useSignatures] is false)
-         */
-        @JvmStatic
-        @JvmOverloads
-        @Suppress("LongParameterList")
-        fun assemble(destination : OutputStream,
-                     name : String,
-                     version : String,
-                     cpkFiles: Iterable<Path>,
-                     cpkArchives: Iterable<Path> = emptyList(),
-                     metadataReader: Reader? = null,
-                     useSignatures: Boolean = jarSignatureVerificationEnabledByDefault(),
-                     groupPolicy: String? = null
-        ) = CpiBuilder(name, version, cpkFiles, cpkArchives, metadataReader, useSignatures, groupPolicy).build(destination)
     }
 
     /**
