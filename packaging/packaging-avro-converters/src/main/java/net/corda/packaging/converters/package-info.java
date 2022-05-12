981abbcf5722ddb6e4894caa48165f3a1d77944e
Joseph Zuniga-Daly <joseph.zunigadaly@r3.com>
2022-05-12 13:23:55 +0100

    CORE-4354: Refactor V1 packaging code to prepare for V2 (#361)
    - Move packaging-avro-converters from corda-api into corda-runtime-os
    - Move packaging module from corda-api to corda-runtime-os
    - Remove unused typesafeConfigVersion gradle variable
    - Change cordaApiRevision to 96

--- packaging/packaging-avro-converters/src/main/java/net/corda/packaging/converters/package-info.java
+++
@@ -1,4 +0,0 @@
-@Export
-package net.corda.packaging.converters;
-
-import org.osgi.annotation.bundle.Export;
--- packaging/src/main/kotlin/net/corda/packaging/ManifestCordappInfo.kt
+++
@@ -1,8 +0,0 @@
-package net.corda.packaging
-
-/** Information on a contract or workflow CorDapp in a [CordappManifest]. */
-data class ManifestCordappInfo(
-        val shortName: String?,
-        val vendor: String?,
-        val versionId: Int?,
-        val licence: String?)
