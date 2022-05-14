51abfe7b99ecb1efd700213b12c8285060fc0cdd
Alexey Kadyrov <alexey.kadyrov@r3.com>
2022-05-14 20:29:17 +0100

    increasing default timeout

--- libs/crypto/crypto-impl/src/main/kotlin/net/corda/crypto/impl/config/CryptoConfigUtils.kt
+++ libs/crypto/crypto-impl/src/main/kotlin/net/corda/crypto/impl/config/CryptoConfigUtils.kt
@@ -148,7 +148,7 @@ fun SmartConfigFactory.createDefaultCryptoConfig(
                             makeSecret(softKey.passphrase).root().unwrapped()
                         ),
                         CryptoSoftPersistenceConfig::retries.name to "0",
-                        CryptoSoftPersistenceConfig::timeoutMills.name to "10000"
+                        CryptoSoftPersistenceConfig::timeoutMills.name to "20000"
                     )
                 )
             )
