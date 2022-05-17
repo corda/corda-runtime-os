c46444f35ebee21522e939440d23231a85cd0e34
Kyriakos Tharrouniatis <kyriakos.tharrouniatis@r3.com>
2022-05-17 10:46:44 +0100

    Fix test

--- components/reconciliation/reconciliation-impl/src/test/kotlin/net/corda/reconciliation/ReconcilerImplTest.kt
+++ components/reconciliation/reconciliation-impl/src/test/kotlin/net/corda/reconciliation/ReconcilerImplTest.kt
@@ -19,7 +19,7 @@ class ReconcilerImplTest {
                 String::class.java,
                 Int::class.java,
                 mock(),
-                mock()
+                1000L
             )
         assertEquals(
             "${ReconcilerImpl::class.java.name}<${String::class.java.name}, ${Int::class.java.name}>",
