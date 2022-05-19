8cbf833a7f2c15b9e23767fbf38d56cf510212ba
Viktor Kolomeyko <viktor.kolomeyko@r3.com>
2022-05-19 13:30:52 +0100

    CORE-4465: `corda-api` changes to support force CPI upload (#353)

---
+++ data/avro-schema/src/main/kotlin/net/corda/data/chunking/PropertyKeys.kt
@@ -0,0 +1,9 @@
+package net.corda.data.chunking
+
+object PropertyKeys {
+
+    /**
+     * Boolean property specifying whether same data content can re-upload again
+     */
+    const val FORCE_UPLOAD = "forceUpload"
+}
