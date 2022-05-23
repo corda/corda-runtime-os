db5c66d62218a7a1c1d5eb0e78c4e2e306a41e06
Dan Newton <dan.newton@r3.com>
2022-05-23 12:10:17 +0100

    CORE-4851 Discard orphaned `Wakeup` events (#1336)
    If a flow does not exist and a `Wakeup` event is received, then discard
    the event. It is highly likely that a `Wakeup` received this way was due
    to a flow finishing but still having a `Wakeup` event in the message
    queue, that was either scheduled by the flow or by the flow heartbeating
    mechanism.

---
+++ components/flow/flow-service/src/integrationTest/kotlin/net/corda/flow/testing/tests/WakeupAcceptanceTest.kt
@@ -0,0 +1,66 @@
+package net.corda.flow.testing.tests
+
+import net.corda.flow.fiber.FlowIORequest
+import net.corda.flow.testing.context.FlowServiceTestBase
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.jupiter.api.parallel.Execution
+import org.junit.jupiter.api.parallel.ExecutionMode
+import org.osgi.test.junit5.service.ServiceExtension
+
+@ExtendWith(ServiceExtension::class)
+@Execution(ExecutionMode.SAME_THREAD)
+class WakeupAcceptanceTest : FlowServiceTestBase() {
+
+    @Test
+    fun `Receiving a wakeup event for a flow that does not exist discards the event`() {
+        `when` {
+            wakeupEventReceived(FLOW_ID1)
+        }
+
+        then {
+            expectOutputForFlow(FLOW_ID1) {
+                flowDidNotResume()
+                noFlowEvents()
+            }
+        }
+    }
+
+    @Test
+    fun `Receiving a wakeup event for a flow that finished discards the event`() {
+        given {
+            startFlowEventReceived(FLOW_ID1, REQUEST_ID1, ALICE_HOLDING_IDENTITY, CPI1, "flow start data")
+                .suspendsWith(FlowIORequest.FlowFinished("done"))
+        }
+
+        `when` {
+            wakeupEventReceived(FLOW_ID1)
+        }
+
+        then {
+            expectOutputForFlow(FLOW_ID1) {
+                flowDidNotResume()
+                noFlowEvents()
+            }
+        }
+    }
+
+    @Test
+    fun `Receiving a wakeup event for a flow that failed discards the event`() {
+        given {
+            startFlowEventReceived(FLOW_ID1, REQUEST_ID1, ALICE_HOLDING_IDENTITY, CPI1, "flow start data")
+                .suspendsWith(FlowIORequest.FlowFailed(RuntimeException("done")))
+        }
+
+        `when` {
+            wakeupEventReceived(FLOW_ID1)
+        }
+
+        then {
+            expectOutputForFlow(FLOW_ID1) {
+                flowDidNotResume()
+                noFlowEvents()
+            }
+        }
+    }
+}
