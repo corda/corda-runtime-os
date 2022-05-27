b58b6cba25859f1fbba0abc31506ac913c608a45
driessamyn <dries.samyn@r3.com>
2022-05-27 17:22:21 +0100

    Change structure of the CPI/CPK metadata entities so that we can eagerly fetch everything in one query when using findAllCpiMetadata. Also simplified the CPI "Reset" code to not need the deletes.

---
+++ libs/virtual-node/cpi-datamodel/src/main/kotlin/net/corda/libs/cpi/datamodel/CpiCpkEntity.kt
@@ -0,0 +1,65 @@
+package net.corda.libs.cpi.datamodel
+
+import net.corda.db.schema.DbSchema
+import java.io.Serializable
+import java.time.Instant
+import javax.persistence.CascadeType
+import javax.persistence.Column
+import javax.persistence.Embeddable
+import javax.persistence.EmbeddedId
+import javax.persistence.Entity
+import javax.persistence.JoinColumn
+import javax.persistence.JoinColumns
+import javax.persistence.OneToOne
+import javax.persistence.PreUpdate
+import javax.persistence.Table
+import javax.persistence.Version
+
+
+/**
+ * Cpi/cpk mapping table.
+ */
+@Entity
+@Table(name = "cpi_cpk", schema = DbSchema.CONFIG)
+data class CpiCpkEntity(
+    @EmbeddedId
+    private val Id: CpiCpkKey,
+    @Column(name = "cpk_file_name", nullable = false)
+    val cpkFileName: String,
+    @OneToOne(cascade = [CascadeType.MERGE, CascadeType.PERSIST])
+    @JoinColumns(
+        JoinColumn(
+            name = "cpk_file_checksum",
+            referencedColumnName = "file_checksum",
+            insertable = false,
+            updatable = false
+        )
+    )
+    val metadata: CpkMetadataEntity,
+) {
+    @Version
+    @Column(name = "entity_version", nullable = false)
+    var entityVersion: Int = 0
+    // Initial population of this TS is managed on the DB itself
+    @Column(name = "insert_ts", insertable = false, updatable = true)
+    var insertTimestamp: Instant? = null
+    @Column(name = "is_deleted", nullable = false)
+    var isDeleted: Boolean = false
+
+    @PreUpdate
+    fun onUpdate() {
+        insertTimestamp = Instant.now()
+    }
+}
+
+@Embeddable
+data class CpiCpkKey(
+    @Column(name = "cpi_name")
+    private val name: String,
+    @Column(name = "cpi_version")
+    private val version: String,
+    @Column(name = "cpi_signer_summary_hash")
+    private val signerSummaryHash: String,
+    @Column(name = "cpk_file_checksum")
+    private val cpkFileChecksum: String,
+): Serializable
