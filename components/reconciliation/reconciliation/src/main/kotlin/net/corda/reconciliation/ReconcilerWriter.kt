d3980a6863df7bda34ef54ba28966d8d0b04005a
Kyriakos Tharrouniatis <kyriakos.tharrouniatis@r3.com>
2022-05-31 19:00:04 +0100

    Update KDoc

--- components/reconciliation/reconciliation/src/main/kotlin/net/corda/reconciliation/ReconcilerWriter.kt
+++ components/reconciliation/reconciliation/src/main/kotlin/net/corda/reconciliation/ReconcilerWriter.kt
@@ -3,11 +3,8 @@ package net.corda.reconciliation
 import net.corda.lifecycle.LifecycleCoordinatorName
 
 /**
- * Publishes record values on Kafka, that are out of sync on Kafka compared to their Db state,
+ * Publishes records to Kafka, that are out of sync on Kafka compared to their DB state,
  * identified from the reconciliation process.
- *
- * For now we are only publishing the value of the record because our DTOs contain both the key
- * and the value, therefore they also contain the key.
  */
 interface ReconcilerWriter<K : Any, V : Any> {
     /**
