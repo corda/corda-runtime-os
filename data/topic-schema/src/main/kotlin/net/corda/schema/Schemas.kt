3813aa4f060ec9d54c94d6699c3b4f984bcc06a4
Ben McMahon <benmcmahon100@gmail.com>
2022-06-02 14:14:59 +0100

    Add new P2P topic (#393)

--- data/topic-schema/src/main/kotlin/net/corda/schema/Schemas.kt
+++ data/topic-schema/src/main/kotlin/net/corda/schema/Schemas.kt
@@ -114,6 +114,7 @@ class Schemas {
             const val P2P_OUT_TOPIC = "p2p.out"
             const val P2P_OUT_MARKERS = "p2p.out.markers"
             const val P2P_IN_TOPIC = "p2p.in"
+            const val P2P_HOSTED_IDENTITIES_TOPIC = "p2p.hosted.identities"
             const val LINK_OUT_TOPIC = "link.out"
             const val LINK_IN_TOPIC = "link.in"
             const val SESSION_OUT_PARTITIONS = "session.out.partitions"
