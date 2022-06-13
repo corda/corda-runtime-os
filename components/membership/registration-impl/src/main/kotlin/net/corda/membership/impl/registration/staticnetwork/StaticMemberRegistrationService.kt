491a938b519fac9814816260b83c0a655b37f727
Dimos Raptis <dimos.raptis@r3.com>
2022-06-13 11:39:02 +0100

    NOTICK - Add remaining changes needed so that p2p can work with static networks

--- components/membership/registration-impl/src/main/kotlin/net/corda/membership/impl/registration/staticnetwork/StaticMemberRegistrationService.kt
+++ components/membership/registration-impl/src/main/kotlin/net/corda/membership/impl/registration/staticnetwork/StaticMemberRegistrationService.kt
@@ -79,6 +79,24 @@ class StaticMemberRegistrationService @Activate constructor(
         private val logger: Logger = contextLogger()
         private val endpointUrlIdentifier = ENDPOINT_URL.substringBefore("-")
         private val endpointProtocolIdentifier = ENDPOINT_PROTOCOL.substringBefore("-")
+
+        private const val DUMMY_CERTIFICATE = """
+            -----BEGIN CERTIFICATE-----
+            MIIBKjCB0qADAgECAgEDMAoGCCqGSM49BAMCMBAxDjAMBgNVBAYTBVVLIENOMB4X
+            DTIyMDYxMzEwMDIwM1oXDTIyMDcxMzEwMDIwM1owEDEOMAwGA1UEBhMFVUsgQ04w
+            WTATBgcqhkjOPQIBBggqhkjOPQMBBwNCAASe7Nywwu2zF8x+C6754R9nEkQR8d4W
+            qogGkYq6Sv69QuuN1YX3mK/Kw64U/marhwyksLvCC3iZs6hn8+p/DgiLox0wGzAZ
+            BgNVHREBAf8EDzANggtleGFtcGxlLmNvbTAKBggqhkjOPQQDAgNHADBEAiAletny
+            LjfTXpntpsuM3QUKoqViPyT8OraIXx+x79BqnQIgTejO2fShMBuVF1ininmwYcY7
+            nOEL3FPCmO4TaDct7E0=
+            -----END CERTIFICATE-----
+        """
+        private const val DUMMY_PUBLIC_SESSION_KEY = """
+            -----BEGIN PUBLIC KEY-----
+            MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAE4SBc982Pox28yb29m9EpdcUOU9ei
+            +N5ihAvKeWRt6Mew2k5TGSiJeDao30siZQ/1lF2nES98/QAE3CTceXh//w==
+            -----END PUBLIC KEY-----
+        """
     }
 
     // Handler for lifecycle events
@@ -208,9 +226,9 @@ class StaticMemberRegistrationService @Activate constructor(
             memberId,
             memberId,
             // we don't have certs yet
-            listOf(""),
+            listOf(DUMMY_CERTIFICATE),
             // and we don't have the session initiation public key
-            ""
+            DUMMY_PUBLIC_SESSION_KEY
         )
 
         return Record(
