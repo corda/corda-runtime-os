ae632dedc0b8ead35ae02f08fbb20323a628e9ae
James Higgs <james.higgs@r3.com>
2022-06-14 18:08:31 +0100

    CORE-5102: Fix an integration test compilation error

--- components/flow/flow-service/src/integrationTest/kotlin/net/corda/flow/testing/fakes/FakeRPCRequestData.kt
+++ components/flow/flow-service/src/integrationTest/kotlin/net/corda/flow/testing/fakes/FakeRPCRequestData.kt
@@ -1,14 +1,14 @@
 package net.corda.flow.testing.fakes
 
 import net.corda.v5.application.flows.RPCRequestData
-import net.corda.v5.application.marshalling.JsonMarshallingService
+import net.corda.v5.application.marshalling.MarshallingService
 
 class FakeRPCRequestData: RPCRequestData {
     override fun getRequestBody(): String {
         return ""
     }
 
-    override fun <T> getRequestBodyAs(jsonMarshallingService: JsonMarshallingService, clazz: Class<T>): T {
+    override fun <T> getRequestBodyAs(marshallingService: MarshallingService, clazz: Class<T>): T {
         TODO("Not yet implemented")
     }
 }
