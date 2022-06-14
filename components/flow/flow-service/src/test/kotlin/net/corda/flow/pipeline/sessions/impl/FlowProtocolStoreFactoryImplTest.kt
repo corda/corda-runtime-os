5958d8a9f3589114a3176e84a480a4df15b05c9e
James Higgs <james.higgs@r3.com>
2022-06-14 12:24:39 +0100

    CORE-5102: Add further test and update the CorDapp manifest

--- components/flow/flow-service/src/test/kotlin/net/corda/flow/pipeline/sessions/impl/FlowProtocolStoreFactoryImplTest.kt
+++ components/flow/flow-service/src/test/kotlin/net/corda/flow/pipeline/sessions/impl/FlowProtocolStoreFactoryImplTest.kt
@@ -1,5 +1,6 @@
 package net.corda.flow.pipeline.sessions.impl
 
+import net.corda.flow.pipeline.exceptions.FlowProcessingException
 import net.corda.libs.packaging.core.CordappManifest
 import net.corda.libs.packaging.core.CpiMetadata
 import net.corda.libs.packaging.core.CpkMetadata
@@ -7,11 +8,14 @@ import net.corda.sandbox.SandboxGroup
 import net.corda.v5.application.flows.Flow
 import net.corda.v5.application.flows.InitiatedBy
 import net.corda.v5.application.flows.InitiatingFlow
+import net.corda.v5.application.flows.RPCRequestData
+import net.corda.v5.application.flows.RPCStartableFlow
 import net.corda.v5.application.flows.ResponderFlow
 import net.corda.v5.application.flows.Subflow
 import net.corda.v5.application.messaging.FlowSession
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.whenever
 
@@ -20,6 +24,8 @@ class FlowProtocolStoreFactoryImplTest {
     companion object {
         private const val INITIATING_FLOW = "initiating-flow"
         private const val INITIATED_FLOW = "initiated-flow"
+        private const val RPC_FLOW = "rpc-flow"
+        private const val BAD_RESPONDER = "bad-responder"
 
         private const val PROTOCOL = "protocol"
     }
@@ -29,13 +35,28 @@ class FlowProtocolStoreFactoryImplTest {
         val cpiMetadata = makeMockCPIMetadata(
             listOf(
                 CpkFlowClassNameLists(listOf(INITIATING_FLOW), listOf(), listOf()),
-                CpkFlowClassNameLists(listOf(), listOf(), listOf(INITIATED_FLOW))
+                CpkFlowClassNameLists(listOf(RPC_FLOW, INITIATED_FLOW), listOf(RPC_FLOW), listOf(INITIATED_FLOW))
             )
         )
         val sandboxGroup = makeMockSandboxGroup()
         val protocolStore = FlowProtocolStoreFactoryImpl().create(sandboxGroup, cpiMetadata)
         assertEquals(Pair(PROTOCOL, listOf(1)), protocolStore.protocolsForInitiator(INITIATING_FLOW, mock()))
         assertEquals(INITIATED_FLOW, protocolStore.responderForProtocol(PROTOCOL, listOf(1), mock()))
+        assertEquals(Pair(PROTOCOL, listOf(1)), protocolStore.protocolsForInitiator(RPC_FLOW, mock()))
+    }
+
+    @Test
+    fun `adding two responders with the same protocol results in an error`() {
+        val cpiMetadata = makeMockCPIMetadata(
+            listOf(
+                CpkFlowClassNameLists(listOf(INITIATING_FLOW, BAD_RESPONDER), listOf(), listOf(BAD_RESPONDER)),
+                CpkFlowClassNameLists(listOf(RPC_FLOW, INITIATED_FLOW), listOf(RPC_FLOW), listOf(INITIATED_FLOW))
+            )
+        )
+        val sandboxGroup = makeMockSandboxGroup()
+        assertThrows<FlowProcessingException> {
+            FlowProtocolStoreFactoryImpl().create(sandboxGroup, cpiMetadata)
+        }
     }
 
     private fun makeMockCPIMetadata(flows: List<CpkFlowClassNameLists>): CpiMetadata {
@@ -63,8 +84,14 @@ class FlowProtocolStoreFactoryImplTest {
                 Flow::class.java
             )
         ).thenReturn(MyInitiatingFlow::class.java)
-        whenever(sandboxGroup.loadClassFromMainBundles(INITIATED_FLOW, ResponderFlow::class.java)).thenReturn(
+        whenever(sandboxGroup.loadClassFromMainBundles(INITIATED_FLOW, Flow::class.java)).thenReturn(
             MyResponderFlow::class.java
+        )
+        whenever(sandboxGroup.loadClassFromMainBundles(RPC_FLOW, Flow::class.java)).thenReturn(
+            MyRPCFlow::class.java
+        )
+        whenever(sandboxGroup.loadClassFromMainBundles(BAD_RESPONDER, Flow::class.java)).thenReturn(
+            BadResponderFlow::class.java
         )
         return sandboxGroup
     }
@@ -77,6 +104,19 @@ class FlowProtocolStoreFactoryImplTest {
 
     @InitiatedBy(protocol = PROTOCOL)
     private class MyResponderFlow : ResponderFlow {
+        override fun call(session: FlowSession) {
+        }
+    }
+
+    @InitiatingFlow(protocol = PROTOCOL)
+    private class MyRPCFlow : RPCStartableFlow {
+        override fun call(requestBody: RPCRequestData): String {
+            return ""
+        }
+    }
+
+    @InitiatedBy(protocol = PROTOCOL)
+    private class BadResponderFlow : ResponderFlow {
         override fun call(session: FlowSession) {
         }
     }
