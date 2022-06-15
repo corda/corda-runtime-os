e3e7468928df14ade77ca30684364971c84f3997
Sagar Nenwani <sagar.nenwani@r3.com>
2022-06-15 18:09:05 +0100

    PublicKeyConverterTest

--- libs/crypto/crypto-impl/src/test/kotlin/net/corda/crypto/impl/converter/PublicKeyConverterTest.kt
+++ libs/crypto/crypto-impl/src/test/kotlin/net/corda/crypto/impl/converter/PublicKeyConverterTest.kt
@@ -2,6 +2,7 @@ package net.corda.crypto.impl.converter
 
 import net.corda.layeredpropertymap.CustomPropertyConverter
 import net.corda.layeredpropertymap.testkit.LayeredPropertyMapMocks
+import net.corda.membership.impl.MemberContextImpl
 import net.corda.v5.base.exceptions.ValueNotFoundException
 import net.corda.v5.base.util.parse
 import net.corda.v5.base.util.parseList
