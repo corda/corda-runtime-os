4e58a771b771a7723d0017551e1aeb1f7d78bde7
Lajos Veres <lajos.veres@r3.com>
2022-06-17 10:56:09 +0100

    CORE-5119 Merkle: commenting

--- libs/crypto/merkle/src/main/kotlin/net/corda/crypto/merkle/MerkleTreeImpl.kt
+++ libs/crypto/merkle/src/main/kotlin/net/corda/crypto/merkle/MerkleTreeImpl.kt
@@ -25,6 +25,7 @@ internal class MerkleTreeImpl(
         fun nextHigherPower2(value: Int): Int {
             require(value > 0) { "nextHigherPower2 requires positive value" }
             require(value <= 0x40000000) { "nextHigherPower2 requires smaller value" }
+            // Fill (v-1)'s binary digits with ones after the highest position one.
             var v = value - 1
             v = v or (v ushr 1)
             v = v or (v ushr 2)
@@ -48,6 +49,17 @@ internal class MerkleTreeImpl(
         }
     }
 
+    /**
+     * We calculate the tree's elements here from starting with the lowest level and progressing level by level towards
+     * the root element.
+     *
+     * hashes contain the current level what we are process
+     * hashSet contains the next level what we are calculating now
+     *
+     * If any level has odd number of elements, the last one just gets lifted to the next level without
+     * more hashing.
+     */
+
     private val nodeHashes: List<List<SecureHash>> by lazy(LazyThreadSafetyMode.PUBLICATION) {
         val hashSet = mutableListOf<List<SecureHash>>()
         var hashes = leafHashes
@@ -61,7 +73,7 @@ internal class MerkleTreeImpl(
                     nodeHashes += digestProvider.nodeHash(depthCounter, hashes[i], hashes[i + 1])
                 }
             }
-            if ((hashes.size and 1) == 1) {
+            if ((hashes.size and 1) == 1) { // Non-paired last elements of odd lists, just get lifted one level upper.
                 nodeHashes += hashes.last()
             }
             hashes = nodeHashes
@@ -79,26 +91,55 @@ internal class MerkleTreeImpl(
         nodeHashes.last().single()
     }
 
+    /**
+     * createAuditProof creates the proof for a set of leaf indices.
+     * Similarly to the Merkle tree building it walks through the tree from the leaves towards the root.
+     * The proof contains
+     * - the data what we are proving with their positions and nonce. (leaves)
+     * - enough hashes of the other elements to be able to reconstruct the tree.
+     * - the size of the tree.
+     *
+     * The extra hashes order is quite important to fill the gaps between the subject elements.
+     *
+     * We'll need to calculate the node's hashes on the routes from the subject elements
+     * towards the tree's root element in the verification process.
+     * We'll contain the indices of these route elements in inPath for the level what we are processing.
+     * Through the processing of a level we'll add a set of hashes to the proof, and we'll calculate the next
+     * level's in route elements (next iteration's inPath) in the newInPath variable.
+     *
+     * When we process a level of the tree, we pair the elements, and we'll have these cases:
+     * - Both elements of the pair are in route, which means they'll be calculated in the verification, so
+     *   their parent on the next level will be in route as well, and we do not need to add their hashes.
+     * - None of the elements of the pair are in route, which means their parent won't be either, also
+     *   we do not need to add their hashes, since their parent hash will be enough to cover them.
+     * - Only one of the elements are in route, which means their parent will be in route, and also we need to
+     *   add the other element's hash to the proof.
+     *
+     * If a level has odd number of elements, then the last element is essentially the both or none case.
+     * So we do not need to add its hash, and its parent node will be on the route if and only if it was on route.
+     */
     @Suppress("NestedBlockDepth", "ComplexMethod")
     override fun createAuditProof(leafIndices: List<Int>): MerkleProof {
         require(leafIndices.isNotEmpty()) { "Proof requires at least one leaf" }
         require(leafIndices.all { it >= 0 && it < leaves.size }) { "Leaf indices out of bounds" }
-        var inPath = List(leaves.size) { it in leafIndices }
+        var inPath = List(leaves.size) { it in leafIndices }    // Initialize inPath with the input elements
         val outputHashes = mutableListOf<SecureHash>()
         var level = 0
         while (inPath.size > 1) {
-            val newInPath = mutableListOf<Boolean>()
+            val newInPath = mutableListOf<Boolean>()            // This will contain the next
+                                                                // level's in route element's indices
             for (i in inPath.indices step 2) {
-                if (i <= inPath.size - 2) {
-                    newInPath += inPath[i] || inPath[i + 1]
-                    if (!inPath[i] && inPath[i + 1]) {
+                if (i <= inPath.size - 2) {                     // We still have a pair to process.
+                    newInPath += inPath[i] || inPath[i + 1]     // If any of them in route, then their parent will be
+                    if (!inPath[i] && inPath[i + 1]) {          // We need to add a hash for the "Only one" cases.
                         outputHashes += nodeHashes[level][i]
                     } else if (inPath[i] && !inPath[i + 1]) {
                         outputHashes += nodeHashes[level][i + 1]
                     }
                 }
             }
-            if ((inPath.size and 1) == 1) {
+            if ((inPath.size and 1) == 1) {                     // If the level has odd number of elements,
+                                                                // the last one is still to be processed.
                 newInPath += inPath.last()
             }
             inPath = newInPath
