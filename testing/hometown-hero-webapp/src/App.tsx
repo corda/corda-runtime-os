4c3d55427b75566fa2663408eb890c4f7626b1ee
Evgeny-Timoshin-R3 <evgeny.timoshin@r3.com>
2022-06-20 11:36:12 +0100

    initial project setup

---
+++ testing/hometown-hero-webapp/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.scss';
+
+import { PageHeader } from '@r3/r3-tooling-design-system/exports';
+
+function App() {
+    return (
+        <div className="App">
+            <PageHeader size={'large'}>Hometown Hero UI</PageHeader>
+        </div>
+    );
+}
+
+export default App;
---
+++ testing/hometown-hero-webapp/src/main.tsx
@@ -0,0 +1,12 @@
+import './index.scss';
+import '@r3/r3-tooling-design-system/index.scss';
+
+import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
