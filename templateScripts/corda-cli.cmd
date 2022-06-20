6c01c817efe6da1493c59dc1d4265bcfbf4d725d
Peter McKinney <pmck91@users.noreply.github.com>
2022-06-20 15:17:35 +0100

    accidental file change revert

--- templateScripts/corda-cli.cmd
+++ templateScripts/corda-cli.cmd
@@ -3,9 +3,7 @@
 set DIRNAME=%~dp0
 if "%DIRNAME%" == "" set DIRNAME=.
 set APP_BASE_NAME=%~n0
-set APP_HOME=%DIRNAME%..
-
-
+set APP_HOME=%DIRNAME%..\..
 
 @rem Resolve any "." and ".." in APP_HOME to make it shorter.
 for %%i in ("%APP_HOME%") do set APP_HOME=%%~fi
@@ -13,4 +11,4 @@ for %%i in ("%APP_HOME%") do set APP_HOME=%%~fi
 SET binDir="%APP_HOME%\app\build\libs"
 SET pluginsDir="%APP_HOME%\build\plugins"
 
-java -Dpf4j.pluginsDir=%pluginsDir% -jar %binDir%\corda-cli-0.0.1-SNAPSHOT.jar %*
+java -Dpf4j.pluginsDir=%pluginsDir% -jar %binDir%\corda-cli-VERSION.jar %*
