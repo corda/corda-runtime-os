228be8118772d8f3af9cef97ae8a43361fda54ce
Kyriakos Tharrouniatis <kyriakos.tharrouniatis@r3.com>
2022-06-21 09:02:52 +0100

    Update `VersionedRecord` KDoc

--- components/reconciliation/reconciliation/src/main/kotlin/net/corda/reconciliation/VersionedRecord.kt
+++ components/reconciliation/reconciliation/src/main/kotlin/net/corda/reconciliation/VersionedRecord.kt
@@ -1,9 +1,18 @@
 package net.corda.reconciliation
 
 /**
- * Input data for reconciliation, returned by (db and Kafka) [ReconcilerReader]s. Non nullable [value]s
- * means Kafka records with deleted values (null`ed values) are filtered out and cannot be returned by
- * [ReconcilerReader]s.
+ * Versioned records are returned by DB and Kafka [ReconcilerReader]s. They are used to determine
+ * which records need updating on Kafka. A DB versioned record needs to be published on Kafka when:
+ *
+ * * It does not exist on Kafka.
+ * * It's DB version is higher than it's Kafka version.
+ * * It is deleted in the DB but exists on Kafka (i.e. it's value is not null on Kafka compacted topic).
+ *
+ * Note1: DB records that are to be read as part of reconciliation need to be soft DB deleted and
+ * [VersionedRecord.isDeleted] needs to be populated accordingly.
+ *
+ * Note2: Non nullable [value]s means Kafka records with deleted values (null`ed values) are
+ * filtered out and are not returned by Kafka [ReconcilerReader]s.
  */
 interface VersionedRecord<K, V> {
     val version: Int
