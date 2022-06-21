34b819170829fe1669ec4aef2f945ab1410463c1
Kyriakos Tharrouniatis <kyriakos.tharrouniatis@r3.com>
2022-06-21 09:50:11 +0100

    Apply PR feedback

--- components/reconciliation/reconciliation/src/main/kotlin/net/corda/reconciliation/VersionedRecord.kt
+++ components/reconciliation/reconciliation/src/main/kotlin/net/corda/reconciliation/VersionedRecord.kt
@@ -2,11 +2,11 @@ package net.corda.reconciliation
 
 /**
  * Versioned records are returned by DB and Kafka [ReconcilerReader]s. They are used to determine
- * which records need updating on Kafka. A DB versioned record needs to be updated on Kafka when:
+ * which records need updating in Kafka. A DB versioned record needs to be updated in Kafka when:
  *
- * * It does not exist on Kafka.
- * * It's DB version is higher than it's Kafka version.
- * * It is deleted in the DB but exists on Kafka (i.e. it's value is not null on Kafka compacted topic).
+ * - It does not exist in Kafka.
+ * - Its DB version is higher than its Kafka version.
+ * - It is deleted in the DB but exists in Kafka (i.e. its value is not null in Kafka compacted topic).
  *
  * Note1: DB records that are to be read as part of reconciliation need to be soft DB deleted and
  * [VersionedRecord.isDeleted] needs to be populated accordingly.
