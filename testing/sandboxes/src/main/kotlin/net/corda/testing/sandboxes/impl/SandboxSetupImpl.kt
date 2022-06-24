56456a636c6cd7d230c2491ccd51338b437954c0
Oisín Mac Fhearaí <oisin.macfhearai@r3.com>
2022-06-24 12:21:06 +0100

    Cleanup:
    * Remove some probably-currently-unneeded requirements from consensual ledger gradle build file + test.bndrun.
    * Remove -impl from SandboxGroupContextComponentImpl and test SandboxSetupImpl.

--- testing/sandboxes/src/main/kotlin/net/corda/testing/sandboxes/impl/SandboxSetupImpl.kt
+++ testing/sandboxes/src/main/kotlin/net/corda/testing/sandboxes/impl/SandboxSetupImpl.kt
@@ -53,7 +53,6 @@ class SandboxSetupImpl @Activate constructor(
             "net.corda.kotlin-stdlib-jdk8.osgi-bundle",
             "net.corda.ledger",
             "net.corda.ledger-consensual",
-            "net.corda.ledger-consensual-impl",
             "net.corda.membership",
             "net.corda.persistence",
             "net.corda.serialization",
