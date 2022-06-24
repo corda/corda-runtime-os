511df63da62ce9a43b46863db427e2c3299d60ed
Owen Stanford <92725587+owenstanford@users.noreply.github.com>
2022-06-24 12:02:49 +0100

    CORE-5173 add flow smoke tests (#1494)
    CORE-5173 Add three basic e2e smoke tests for flows (start flow, fail flow, flow to flow session comms)
    CORE-5259 Fixed message generation bug in the flow mapper session handler

---
+++ applications/workers/workers-smoketest/src/smokeTest/kotlin/net/corda/applications/workers/smoketest/Utils.kt
@@ -0,0 +1,24 @@
+package net.corda.applications.workers.smoketest
+
+import java.net.URI
+
+const val X500_BOB = "CN=Bob, OU=Application, O=R3, L=London, C=GB"
+const val X500_ALICE = "CN=Alice, OU=Application, O=R3, L=London, C=GB"
+const val CPI_NAME = "flow-worker-dev"
+
+const val USERNAME = "admin"
+const val PASSWORD = "admin"
+const val GROUP_ID = "placeholder"
+
+val CLUSTER_URI = URI(System.getProperty("rpcHost"))
+
+
+// BUG:  Not sure if we should be requiring clients to use a method similar to this because we
+// return a full hash (64 chars?) but the same API only accepts the first 12 chars.
+fun truncateLongHash(shortHash:String):String {
+    return shortHash.substring(0,12)
+}
+
+
+
+
