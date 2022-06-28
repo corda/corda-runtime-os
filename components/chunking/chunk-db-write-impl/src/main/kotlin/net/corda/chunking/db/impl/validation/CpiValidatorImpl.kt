8d31f711b6729b66ec77ef3bfc428ee19fc2720f
Kyriakos Tharrouniatis <kyriakos.tharrouniatis@r3.com>
2022-06-28 14:11:05 +0100

    Add CPI verification library call site only to compile - currently not working so switched off

--- components/chunking/chunk-db-write-impl/src/main/kotlin/net/corda/chunking/db/impl/validation/CpiValidatorImpl.kt
+++ components/chunking/chunk-db-write-impl/src/main/kotlin/net/corda/chunking/db/impl/validation/CpiValidatorImpl.kt
@@ -1,5 +1,10 @@
 package net.corda.chunking.db.impl.validation
 
+import java.io.FileNotFoundException
+import java.nio.file.Files
+import java.nio.file.Path
+import java.security.KeyStore
+import java.security.cert.X509Certificate
 import net.corda.chunking.RequestId
 import net.corda.chunking.db.impl.persistence.ChunkPersistence
 import net.corda.chunking.db.impl.persistence.StatusPublisher
@@ -7,9 +12,9 @@ import net.corda.cpiinfo.write.CpiInfoWriteService
 import net.corda.libs.packaging.Cpi
 import net.corda.libs.packaging.core.CpiMetadata
 import net.corda.utilities.time.Clock
+import net.corda.libs.packaging.verify.verifyCpi
 import net.corda.v5.base.util.contextLogger
 import net.corda.v5.crypto.SecureHash
-import java.nio.file.Path
 
 @Suppress("LongParameterList")
 class CpiValidatorImpl constructor(
@@ -36,6 +41,20 @@ class CpiValidatorImpl constructor(
 
         publisher.update(requestId, "Checking signatures")
         validationFunctions.checkSignature(fileInfo)
+
+        // The following bit in only just adds the verifyCpi call site to compile. Having said that:
+        // - The following (cordadevcodesignpublic.pem) is the certificate of "cordadevcodesign.p12" (default)
+        // used in `corda-gradle-plugins.cordapp-cpk` (defaulted CPB developer certificate).
+        // - Normally we would need to load two certificates to verify a CPI, the CPB developer's and the network operator's (?).
+        // - The following CPI verification is de-activated for now because does not work.
+        val cpiVerificationEnabled = System.getProperty("cpiVerificationEnabled", "false").toBoolean()
+        if (cpiVerificationEnabled) {
+            publisher.update(requestId, "Verifying CPI")
+            val certFileName = "cordadevcodesignpublic.pem"
+            // - The certificates are normally going to be loaded from the database.
+            val cert = getCert(certFileName)
+            verifyCpi(fileInfo.name, Files.newInputStream(fileInfo.path), setOf(cert))
+        }
 
         publisher.update(requestId, "Validating CPI")
         val cpi: Cpi = validationFunctions.checkCpi(fileInfo)
@@ -72,5 +91,15 @@ class CpiValidatorImpl constructor(
         cpiInfoWriteService.put(cpiMetadata.cpiId, cpiMetadata)
 
         return fileInfo.checksum
+    }
+
+    @Suppress("warnings")
+    private fun getCert(certFileName: String): X509Certificate {
+        val keyStoreInputStream = this::class.java.classLoader.getResourceAsStream(certFileName)
+            ?: throw FileNotFoundException("Resource file \"$certFileName\" not found")
+
+        val keyStore = KeyStore.getInstance("PKCS12")
+        keyStoreInputStream.use { keyStore.load(it, "cordacadevpass".toCharArray()) }
+        return keyStore.getCertificate("cordacodesign") as X509Certificate
     }
 }
