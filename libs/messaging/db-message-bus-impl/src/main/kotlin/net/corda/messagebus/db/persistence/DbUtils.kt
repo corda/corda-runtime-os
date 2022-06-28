74a5aae3e6b7e02bde75c3dc34f1481f65fc5c01
Ryan Fowler <ryan.fowler@r3.com>
2022-06-28 13:27:02 +0100

    CORE-5331: Use a single entity manager factory across all of DB message bus

--- libs/messaging/db-message-bus-impl/src/main/kotlin/net/corda/messagebus/db/persistence/DbUtils.kt
+++ libs/messaging/db-message-bus-impl/src/main/kotlin/net/corda/messagebus/db/persistence/DbUtils.kt
@@ -3,8 +3,6 @@ package net.corda.messagebus.db.persistence
 import net.corda.db.core.InMemoryDataSourceFactory
 import net.corda.db.core.PostgresDataSourceFactory
 import net.corda.db.schema.DbSchema.DB_MESSAGE_BUS
-import net.corda.messagebus.db.configuration.ResolvedConsumerConfig
-import net.corda.messagebus.db.configuration.ResolvedProducerConfig
 import net.corda.orm.DbEntityManagerConfiguration
 import net.corda.orm.EntityManagerFactoryFactory
 import javax.persistence.EntityManagerFactory
@@ -13,28 +11,12 @@ const val JDBC_URL = "jdbc.url"
 const val USER = "user"
 const val PASS = "pass"
 
-fun EntityManagerFactoryFactory.create(
-    dbConfig: ResolvedConsumerConfig,
+internal fun EntityManagerFactoryFactory.create(
+    jdbcUrl: String?,
+    jdbcUser: String,
+    jdbcPass: String,
     persistenceName: String,
-    entities: List<Class<out Any>>,
-): EntityManagerFactory {
-    return create(dbConfig.jdbcUrl, dbConfig.jdbcUser, dbConfig.jdbcPass, persistenceName, entities)
-}
-
-fun EntityManagerFactoryFactory.create(
-    dbConfig: ResolvedProducerConfig,
-    persistenceName: String,
-    entities: List<Class<out Any>>,
-): EntityManagerFactory {
-    return create(dbConfig.jdbcUrl, dbConfig.jdbcUser, dbConfig.jdbcPass, persistenceName, entities)
-}
-
-
-internal fun EntityManagerFactoryFactory.create(jdbcUrl: String?,
-                   jdbcUser: String,
-                   jdbcPass: String,
-                   persistenceName: String,
-                   entities: List<Class<out Any>>
+    entities: List<Class<out Any>>
 ): EntityManagerFactory {
     val dbSource = if (jdbcUrl == null || jdbcUrl.contains("hsqldb", ignoreCase = true)) {
         InMemoryDataSourceFactory().create(DB_MESSAGE_BUS)
