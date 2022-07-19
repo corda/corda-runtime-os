5d6b01cc0f824e3c8f9cbc0c914c2036acf9efa6
Charlie Crean <charlie.crean@r3.com>
2022-07-19 12:05:34 +0100

    NOTICK Set valid group policy in CPI upload for smoke tests

--- applications/workers/workers-smoketest/src/smokeTest/kotlin/net/corda/applications/workers/smoketest/virtualnode/helpers/CpiLoader.kt
+++ applications/workers/workers-smoketest/src/smokeTest/kotlin/net/corda/applications/workers/smoketest/virtualnode/helpers/CpiLoader.kt
@@ -1,5 +1,6 @@
 package net.corda.applications.workers.smoketest.virtualnode.helpers
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import java.io.ByteArrayOutputStream
 import java.io.FileNotFoundException
 import java.io.InputStream
@@ -8,6 +9,8 @@ import java.util.zip.ZipInputStream
 import java.util.zip.ZipOutputStream
 
 object CpiLoader {
+    private fun createCertificate() = CpiLoader::class.java.classLoader.getResource("certificate.pem")!!.readText()
+
     private fun getInputStream(resourceName: String): InputStream {
         return this::class.java.getResource(resourceName)?.openStream()
             ?: throw FileNotFoundException("No such resource: '$resourceName'")
@@ -39,9 +42,56 @@ object CpiLoader {
         return bytes.inputStream()
     }
 
-    private fun addGroupPolicy(zipOutputStream: ZipOutputStream, groupId: String) {
+    private fun addGroupPolicy(
+        zipOutputStream: ZipOutputStream,
+        groupId: String,
+        staticMemberNames: List<String> = emptyList()
+    ) {
         zipOutputStream.putNextEntry(ZipEntry("META-INF/GroupPolicy.json"))
-        """{"groupId":"$groupId", "fileFormatVersion": 1}\n""".byteInputStream().use { it.copyTo(zipOutputStream) }
+        val groupPolicy = mapOf(
+            "fileFormatVersion" to 1,
+            "groupId" to groupId,
+            "registrationProtocol"
+                    to "net.corda.membership.impl.registration.staticnetwork.StaticMemberRegistrationService",
+            "synchronisationProtocol"
+                    to "net.corda.membership.impl.sync.staticnetwork.StaticMemberSyncService",
+            "protocolParameters" to mapOf(
+                "sessionKeyPolicy" to "Combined",
+                "staticNetwork" to mapOf(
+                    "members" to
+                        staticMemberNames.map {
+                            mapOf(
+                                "name" to it,
+                                "memberStatus" to "ACTIVE",
+                                "endpointUrl-1" to "http://localhost:1080",
+                                "endpointProtocol-1" to 1
+                            )
+                        }
+                )
+            ),
+            "p2pParameters" to mapOf(
+                "sessionTrustRoots" to listOf(
+                    createCertificate(),
+                    createCertificate()
+                ),
+                "tlsTrustRoots" to listOf(
+                    createCertificate()
+                ),
+                "sessionPki" to "Standard",
+                "tlsPki" to "Standard",
+                "tlsVersion" to "1.3",
+                "protocolMode" to "Authentication_Encryption"
+            ),
+            "cipherSuite" to mapOf(
+                "corda.provider" to "default",
+                "corda.signature.provider" to "default",
+                "corda.signature.default" to "ECDSA_SECP256K1_SHA256",
+                "corda.signature.FRESH_KEYS" to "ECDSA_SECP256K1_SHA256",
+                "corda.digest.default" to "SHA256",
+                "corda.cryptoservice.provider" to "default"
+            )
+        )
+        ObjectMapper().writeValueAsString(groupPolicy).byteInputStream().use { it.copyTo(zipOutputStream) }
         zipOutputStream.closeEntry()
     }
 }
