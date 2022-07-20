e9e38b2e5a87b19085df2bc324535c739b545537
William Vigor <58432369+williamvigorr3@users.noreply.github.com>
2022-07-20 16:53:29 +0100

    CORE-5752 Adjust topic dump tool to use TLS (#1688)
    Modified the topic-dump tool to take -m options to work in the same way as the LinkManager, Gateway, AppSimulator and the common worker framework. To set the kafka to connect to a broker on kafka-broker:1000 use -mbootstrap.servers=kafka-broker:1000. To enable TLS add the following options: -msecurity.protocol=SSL, -mssl.truststore.location=/certs/ca.crt and -mssl.truststore.type=PEM. Where /certs/ca.crt is the trust store.

--- applications/tools/p2p-test/dump-topic/src/main/kotlin/net/corda/p2p/app/topic/dump/TopicDumper.kt
+++ applications/tools/p2p-test/dump-topic/src/main/kotlin/net/corda/p2p/app/topic/dump/TopicDumper.kt
@@ -1,16 +1,19 @@
 package net.corda.p2p.app.topic.dump
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.typesafe.config.ConfigFactory
 import com.typesafe.config.ConfigValueFactory
+import net.corda.libs.configuration.SmartConfigFactory
 import net.corda.libs.configuration.SmartConfigImpl
+import net.corda.libs.configuration.merger.ConfigMerger
 import net.corda.messaging.api.processor.DurableProcessor
 import net.corda.messaging.api.records.Record
 import net.corda.messaging.api.subscription.config.SubscriptionConfig
 import net.corda.messaging.api.subscription.factory.SubscriptionFactory
+import net.corda.schema.configuration.BootConfig
 import net.corda.schema.configuration.BootConfig.INSTANCE_ID
 import net.corda.schema.configuration.BootConfig.TOPIC_PREFIX
 import net.corda.schema.configuration.MessagingConfig.Bus.BUS_TYPE
-import net.corda.schema.configuration.MessagingConfig.Bus.KAFKA_BOOTSTRAP_SERVERS
 import org.apache.avro.generic.IndexedRecord
 import org.osgi.framework.FrameworkUtil
 import org.slf4j.LoggerFactory
@@ -31,34 +34,35 @@ import kotlin.random.Random
     showAtFileInUsageHelp = true,
 )
 internal class TopicDumper(
-    private val subscriptionFactory: SubscriptionFactory
+    private val subscriptionFactory: SubscriptionFactory,
+    private val configMerger: ConfigMerger
 ) : Runnable, Closeable {
     companion object {
         private val logger = LoggerFactory.getLogger("Console")
     }
     @Option(
-        names = ["-k", "--kafka-servers"],
-        description = ["A comma-separated list of addresses of Kafka brokers."]
+        names = ["-m", "--messagingParams"],
+        description = ["Messaging parameters for the topic dumper."]
     )
-    private var kafkaServers = System.getenv("KAFKA_SERVERS") ?: "localhost:9092"
+    var messagingParams = emptyMap<String, String>()
 
     @Option(
         names = ["-t", "--topic"],
-        description = ["The topic to dump"],
+        description = ["The name of the topic to dump."],
         required = true,
     )
     private lateinit var topic: String
 
     @Option(
         names = ["-v", "--values"],
-        description = ["The value class"],
+        description = ["The class name of the expected value (full canonical name)."],
         required = true,
     )
     private lateinit var values: String
 
     @Option(
         names = ["-o", "--output"],
-        description = ["The output file"],
+        description = ["The output file."],
         required = true,
     )
     private lateinit var output: File
@@ -136,14 +140,26 @@ internal class TopicDumper(
 
     override fun run() {
         output.parentFile.mkdirs()
-        logger.info("Connecting to $kafkaServers")
         val subscriptionConfig = SubscriptionConfig("topic-dumper-${UUID.randomUUID()}", topic)
-        val kafkaConfig = SmartConfigImpl.empty()
-            .withValue(KAFKA_BOOTSTRAP_SERVERS, ConfigValueFactory.fromAnyRef(kafkaServers))
-            .withValue(BUS_TYPE, ConfigValueFactory.fromAnyRef("KAFKA"))
-            .withValue(TOPIC_PREFIX, ConfigValueFactory.fromAnyRef(""))
-            .withValue(INSTANCE_ID, ConfigValueFactory.fromAnyRef(Random.nextInt()))
-        subscription = subscriptionFactory.createDurableSubscription(subscriptionConfig, createProcessor(), kafkaConfig, null).also {
+        val parsedMessagingParams = messagingParams.mapKeys { (key, _) ->
+            "${BootConfig.BOOT_KAFKA_COMMON}.${key.trim()}"
+        }.toMutableMap()
+        parsedMessagingParams.computeIfAbsent("${BootConfig.BOOT_KAFKA_COMMON}.bootstrap.servers") {
+            "localhost:9092"
+        }
+        logger.info("Connecting to ${parsedMessagingParams["${BootConfig.BOOT_KAFKA_COMMON}.bootstrap.servers"]}")
+        val kafkaConfig = SmartConfigFactory.create(SmartConfigImpl.empty()).create(
+            ConfigFactory.parseMap(parsedMessagingParams)
+                .withValue(BUS_TYPE, ConfigValueFactory.fromAnyRef("KAFKA"))
+                .withValue(TOPIC_PREFIX, ConfigValueFactory.fromAnyRef(""))
+                .withValue(INSTANCE_ID, ConfigValueFactory.fromAnyRef(Random.nextInt()))
+        )
+        subscription = subscriptionFactory.createDurableSubscription(
+            subscriptionConfig,
+            createProcessor(),
+            configMerger.getMessagingConfig(kafkaConfig),
+            null
+        ).also {
             it.start()
         }
         logger.info("Started dumping $topic into $output")
