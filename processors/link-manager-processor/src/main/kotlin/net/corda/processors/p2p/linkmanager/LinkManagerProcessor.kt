0b8cc107a559295479f41b02ad18155b6c60f081
Will Vigor <william.vigor@r3.com>
2022-07-21 11:58:08 +0100

    Fix detekt

--- processors/link-manager-processor/src/main/kotlin/net/corda/processors/p2p/linkmanager/LinkManagerProcessor.kt
+++ processors/link-manager-processor/src/main/kotlin/net/corda/processors/p2p/linkmanager/LinkManagerProcessor.kt
@@ -2,7 +2,6 @@ package net.corda.processors.p2p.linkmanager
 
 import net.corda.libs.configuration.SmartConfig
 import net.corda.p2p.linkmanager.LinkManager
-import net.corda.p2p.linkmanager.ThirdPartyComponentsMode
 
 /**
  * The processor for a [LinkManager].
