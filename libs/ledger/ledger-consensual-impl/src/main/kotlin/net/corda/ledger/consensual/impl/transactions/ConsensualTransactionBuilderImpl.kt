1a04230aacc4f53721b9ec7185ace13106646962
Lajos Veres <lajos.veres@r3.com>
2022-07-21 10:20:34 +0100

    CORE-5001 Consensual Transactioj builder - use ConsensualStates to get RequiredSigners

--- libs/ledger/ledger-consensual-impl/src/main/kotlin/net/corda/ledger/consensual/impl/transactions/ConsensualTransactionBuilderImpl.kt
+++ libs/ledger/ledger-consensual-impl/src/main/kotlin/net/corda/ledger/consensual/impl/transactions/ConsensualTransactionBuilderImpl.kt
@@ -4,6 +4,7 @@ import net.corda.ledger.common.impl.transactions.PrivacySaltImpl
 import net.corda.v5.crypto.DigestService
 import net.corda.v5.crypto.merkle.MerkleTreeFactory
 import net.corda.v5.ledger.consensual.ConsensualState
+import net.corda.v5.ledger.consensual.Party
 import net.corda.v5.ledger.consensual.transaction.ConsensualTransactionBuilder
 import net.corda.v5.ledger.consensual.transaction.ConsensualTransactionMetaData
 import net.corda.v5.ledger.consensual.transaction.ConsensualWireTransaction
@@ -14,21 +15,18 @@ import java.time.Instant
 class ConsensualTransactionBuilderImpl(
     override val metadata: ConsensualTransactionMetaData? = null,
     override val timeStamp: Instant? = null,
-    override val requiredSigners: List<PublicKey> = emptyList(),
     override val consensualStates: List<ConsensualState> = emptyList()
 ) : ConsensualTransactionBuilder {
 
     private fun copy(
         metadata: ConsensualTransactionMetaData? = this.metadata,
         timeStamp: Instant? = this.timeStamp,
-        requiredSigners: List<PublicKey> = this.requiredSigners,
         consensualStates: List<ConsensualState> = this.consensualStates
 
     ): ConsensualTransactionBuilderImpl {
         return ConsensualTransactionBuilderImpl(
             metadata,
             timeStamp,
-            requiredSigners,
             consensualStates,
         )
     }
@@ -38,9 +36,6 @@ class ConsensualTransactionBuilderImpl(
 
     override fun withTimeStamp(timeStamp: Instant): ConsensualTransactionBuilder =
         this.copy(timeStamp = timeStamp)
-
-    override fun withRequiredSigner(requiredSigner: PublicKey): ConsensualTransactionBuilder =
-        this.copy(requiredSigners = requiredSigners + requiredSigner)
 
     override fun withConsensualState(consensualState: ConsensualState): ConsensualTransactionBuilder =
         this.copy(consensualStates = consensualStates + consensualState)
@@ -54,6 +49,7 @@ class ConsensualTransactionBuilderImpl(
         val entropy = ByteArray(32) //TODO(get this const from somewhere)
         secureRandom.nextBytes(entropy)
         val privacySalt = PrivacySaltImpl(entropy)
+        val requiredSigners = consensualStates.map{it.participants}.flatten()
         ConsensualWireTransactionImpl(
             merkleTreeFactory,
             digestService,
