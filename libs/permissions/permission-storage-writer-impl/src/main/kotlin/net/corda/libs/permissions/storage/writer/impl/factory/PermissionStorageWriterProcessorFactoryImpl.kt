5e7ab3a28b24ca87c906afca33ca700a2fa6bfdf
Viktor Kolomeyko <viktor.kolomeyko@r3.com>
2022-07-21 10:06:39 +0100

    CORE-5804: Be flexible about order of config change distribution for RPC Permission reader/writer (#1685)
    The problem is that `PermissionStorageWriterProcessorImpl` may a a reference to the stale `PermissionStorageReader` which might have been already closed after the config update.
    See more details on the Jira.

--- libs/permissions/permission-storage-writer-impl/src/main/kotlin/net/corda/libs/permissions/storage/writer/impl/factory/PermissionStorageWriterProcessorFactoryImpl.kt
+++ libs/permissions/permission-storage-writer-impl/src/main/kotlin/net/corda/libs/permissions/storage/writer/impl/factory/PermissionStorageWriterProcessorFactoryImpl.kt
@@ -9,13 +9,18 @@ import org.osgi.service.component.annotations.Component
 import javax.persistence.EntityManagerFactory
 import net.corda.libs.permissions.storage.writer.impl.role.impl.RoleWriterImpl
 import net.corda.libs.permissions.storage.writer.impl.user.impl.UserWriterImpl
+import java.util.function.Supplier
 
 @Component(service = [PermissionStorageWriterProcessorFactory::class])
+@Suppress("Unused")
 class PermissionStorageWriterProcessorFactoryImpl : PermissionStorageWriterProcessorFactory {
 
-    override fun create(entityManagerFactory: EntityManagerFactory, reader: PermissionStorageReader): PermissionStorageWriterProcessor {
+    override fun create(
+        entityManagerFactory: EntityManagerFactory,
+        readerSupplier: Supplier<PermissionStorageReader?>
+    ): PermissionStorageWriterProcessor {
         return PermissionStorageWriterProcessorImpl(
-            reader,
+            readerSupplier,
             UserWriterImpl(entityManagerFactory),
             RoleWriterImpl(entityManagerFactory),
             PermissionWriterImpl(entityManagerFactory)
