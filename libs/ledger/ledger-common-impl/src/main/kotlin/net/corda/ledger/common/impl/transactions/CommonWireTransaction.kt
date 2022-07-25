c711c2cf4da8e35a1f7c944906cc766a4ff8e66d
Lajos Veres <lajos.veres@r3.com>
2022-07-25 11:25:44 +0100

    CORE-5001 Extract Merkle logic into a generic CommonWireTransaction

---
+++ libs/ledger/ledger-common-impl/src/main/kotlin/net/corda/ledger/common/impl/transactions/CommonWireTransaction.kt
@@ -0,0 +1,78 @@
+package net.corda.ledger.common.impl.transactions
+
+import net.corda.crypto.core.concatByteArrays
+import net.corda.crypto.core.toByteArray
+import net.corda.v5.crypto.DigestAlgorithmName
+import net.corda.v5.crypto.DigestService
+import net.corda.v5.crypto.SecureHash
+import net.corda.v5.crypto.merkle.HASH_DIGEST_PROVIDER_ENTROPY_OPTION
+import net.corda.v5.crypto.merkle.HASH_DIGEST_PROVIDER_LEAF_PREFIX_OPTION
+import net.corda.v5.crypto.merkle.HASH_DIGEST_PROVIDER_NODE_PREFIX_OPTION
+import net.corda.v5.crypto.merkle.HASH_DIGEST_PROVIDER_NONCE_NAME
+import net.corda.v5.crypto.merkle.HASH_DIGEST_PROVIDER_TWEAKABLE_NAME
+import net.corda.v5.crypto.merkle.MerkleTree
+import net.corda.v5.crypto.merkle.MerkleTreeFactory
+import net.corda.v5.crypto.merkle.MerkleTreeHashDigestProvider
+import net.corda.v5.ledger.common.transactions.PrivacySalt
+
+internal const val ROOT_MERKLE_TREE_DIGEST_PROVIDER_NAME = HASH_DIGEST_PROVIDER_TWEAKABLE_NAME
+internal val ROOT_MERKLE_TREE_DIGEST_ALGORITHM_NAME = DigestAlgorithmName.SHA2_256D
+
+internal const val COMPONENT_MERKLE_TREE_DIGEST_PROVIDER_NAME = HASH_DIGEST_PROVIDER_NONCE_NAME
+internal val COMPONENT_MERKLE_TREE_DIGEST_ALGORITHM_NAME = DigestAlgorithmName.SHA2_256D
+internal val COMPONENT_MERKLE_TREE_ENTROPY_ALGORITHM_NAME = DigestAlgorithmName.SHA2_256D
+
+abstract class CommonWireTransaction(
+    private val merkleTreeFactory: MerkleTreeFactory,
+    private val digestService: DigestService,
+    val privacySalt: PrivacySalt,
+    protected open val componentGroupLists: List<List<ByteArray>>
+){
+    val id: SecureHash by lazy(LazyThreadSafetyMode.PUBLICATION) {
+        rootMerkleTree.root
+    }
+
+    private fun getRootMerkleTreeDigestProvider() : MerkleTreeHashDigestProvider = merkleTreeFactory.createHashDigestProvider(
+        ROOT_MERKLE_TREE_DIGEST_PROVIDER_NAME,
+        ROOT_MERKLE_TREE_DIGEST_ALGORITHM_NAME,
+        mapOf(
+            HASH_DIGEST_PROVIDER_LEAF_PREFIX_OPTION to "leaf".toByteArray(Charsets.UTF_8),
+            HASH_DIGEST_PROVIDER_NODE_PREFIX_OPTION to "node".toByteArray(Charsets.UTF_8)
+        )
+    )
+
+    private fun getComponentGroupEntropy(
+        privacySalt: PrivacySalt,
+        componentGroupIndexBytes: ByteArray
+    ): ByteArray =
+        digestService.hash(
+            concatByteArrays(privacySalt.bytes, componentGroupIndexBytes),
+            COMPONENT_MERKLE_TREE_ENTROPY_ALGORITHM_NAME
+        ).bytes
+
+    private fun getComponentGroupMerkleTreeDigestProvider(
+        privacySalt: PrivacySalt,
+        componentGroupIndex: Int
+    ) : MerkleTreeHashDigestProvider =
+        merkleTreeFactory.createHashDigestProvider(
+            COMPONENT_MERKLE_TREE_DIGEST_PROVIDER_NAME,
+            COMPONENT_MERKLE_TREE_DIGEST_ALGORITHM_NAME,
+            mapOf(
+                HASH_DIGEST_PROVIDER_ENTROPY_OPTION to
+                    getComponentGroupEntropy(privacySalt, componentGroupIndex.toByteArray())
+            )
+    )
+
+    private val rootMerkleTree: MerkleTree by lazy(LazyThreadSafetyMode.PUBLICATION) {
+        val componentGroupRoots = mutableListOf<ByteArray>()
+        componentGroupLists.forEachIndexed{ index, leaves: List<ByteArray> ->
+            val componentMerkleTree = merkleTreeFactory.createTree(
+                leaves,
+                getComponentGroupMerkleTreeDigestProvider(privacySalt, index)
+            )
+            componentGroupRoots += componentMerkleTree.root.bytes
+        }
+
+        merkleTreeFactory.createTree(componentGroupRoots, getRootMerkleTreeDigestProvider())
+    }
+}
--- libs/ledger/ledger-consensual-impl/src/main/kotlin/net/corda/ledger/consensual/impl/transactions/ConsensualWireTransactionImpl.kt
+++ libs/ledger/ledger-consensual-impl/src/main/kotlin/net/corda/ledger/consensual/impl/transactions/ConsensualWireTransactionImpl.kt
@@ -1,101 +1,76 @@
 package net.corda.ledger.consensual.impl.transactions
 
-import net.corda.crypto.core.concatByteArrays
-import net.corda.crypto.core.toByteArray
-import net.corda.v5.crypto.DigestAlgorithmName
+import net.corda.ledger.common.impl.transactions.CommonWireTransaction
 import net.corda.v5.crypto.DigestService
-import net.corda.v5.crypto.SecureHash
-import net.corda.v5.crypto.merkle.HASH_DIGEST_PROVIDER_ENTROPY_OPTION
-import net.corda.v5.crypto.merkle.HASH_DIGEST_PROVIDER_LEAF_PREFIX_OPTION
-import net.corda.v5.crypto.merkle.HASH_DIGEST_PROVIDER_NODE_PREFIX_OPTION
-import net.corda.v5.crypto.merkle.HASH_DIGEST_PROVIDER_NONCE_NAME
-import net.corda.v5.crypto.merkle.HASH_DIGEST_PROVIDER_TWEAKABLE_NAME
 import net.corda.v5.crypto.merkle.MerkleTree
 import net.corda.v5.crypto.merkle.MerkleTreeFactory
-import net.corda.v5.crypto.merkle.MerkleTreeHashDigestProvider
 import net.corda.v5.ledger.common.transactions.PrivacySalt
 import net.corda.v5.ledger.consensual.transaction.ConsensualWireTransaction
 import java.util.concurrent.ConcurrentHashMap
 
-internal const val ROOT_MERKLE_TREE_DIGEST_PROVIDER_NAME = HASH_DIGEST_PROVIDER_TWEAKABLE_NAME
-internal val ROOT_MERKLE_TREE_DIGEST_ALGORITHM_NAME = DigestAlgorithmName.SHA2_256D
+internal class ConsensualWireTransactionImpl :ConsensualWireTransaction, CommonWireTransaction
+{
+    constructor(
+        merkleTreeFactory: MerkleTreeFactory,
+        digestService: DigestService,
 
-internal const val COMPONENT_MERKLE_TREE_DIGEST_PROVIDER_NAME = HASH_DIGEST_PROVIDER_NONCE_NAME
-internal val COMPONENT_MERKLE_TREE_DIGEST_ALGORITHM_NAME = DigestAlgorithmName.SHA2_256D
-internal val COMPONENT_MERKLE_TREE_ENTROPY_ALGORITHM_NAME = DigestAlgorithmName.SHA2_256D
+        privacySalt: PrivacySalt,
 
-internal class ConsensualWireTransactionImpl
-    internal constructor(
-        private val merkleTreeFactory: MerkleTreeFactory,
-        private val digestService: DigestService,
-
-        override val privacySalt: PrivacySalt,
-
-        override val metadata: ByteArray,
-        override val timestamp: ByteArray,
-        override val requiredSigners: List<ByteArray>,
-        override val consensualStates: List<ByteArray>,
-        override val consensualStateTypes: List<ByteArray>,
-    ): ConsensualWireTransaction {
-
-    override val id: SecureHash by lazy(LazyThreadSafetyMode.PUBLICATION) {
-        rootMerkleTree.root
-    }
-
-    private val componentMerkleTrees = ConcurrentHashMap<ConsensualComponentGroups, MerkleTree>()
-
-    private fun getRootMerkleTreeDigestProvider() : MerkleTreeHashDigestProvider = merkleTreeFactory.createHashDigestProvider(
-        ROOT_MERKLE_TREE_DIGEST_PROVIDER_NAME,
-        ROOT_MERKLE_TREE_DIGEST_ALGORITHM_NAME,
-        mapOf(
-            HASH_DIGEST_PROVIDER_LEAF_PREFIX_OPTION to "leaf".toByteArray(Charsets.UTF_8),
-            HASH_DIGEST_PROVIDER_NODE_PREFIX_OPTION to "node".toByteArray(Charsets.UTF_8)
+        metadata: ByteArray,
+        timestamp: ByteArray,
+        requiredSigners: List<ByteArray>,
+        consensualStates: List<ByteArray>,
+        consensualStateTypes: List<ByteArray>
+    ) : super(merkleTreeFactory,
+        digestService,
+        privacySalt,
+        calculateComponentGroupLists(
+            metadata,
+            timestamp,
+            requiredSigners,
+            consensualStates,
+            consensualStateTypes
         )
     )
+    override val metadata: ByteArray by lazy(LazyThreadSafetyMode.PUBLICATION) {
+        componentGroupLists[ConsensualComponentGroups.METADATA.ordinal].first()
+    }
+    override val timestamp: ByteArray by lazy(LazyThreadSafetyMode.PUBLICATION) {
+        componentGroupLists[ConsensualComponentGroups.TIMESTAMP.ordinal].first()
+    }
+    override val requiredSigners: List<ByteArray> by lazy(LazyThreadSafetyMode.PUBLICATION) {
+        componentGroupLists[ConsensualComponentGroups.REQUIRED_SIGNERS.ordinal]
+    }
+    override val consensualStates: List<ByteArray> by lazy(LazyThreadSafetyMode.PUBLICATION) {
+        componentGroupLists[ConsensualComponentGroups.OUTPUT_STATES.ordinal]
+    }
+    override val consensualStateTypes: List<ByteArray> by lazy(LazyThreadSafetyMode.PUBLICATION) {
+        componentGroupLists[ConsensualComponentGroups.OUTPUT_STATE_TYPES.ordinal]
+    }
 
-    private fun getComponentGroupEntropy(
-        privacySalt: PrivacySalt,
-        componentGroupIndexBytes: ByteArray
-    ): ByteArray =
-        digestService.hash(
-            concatByteArrays(privacySalt.bytes, componentGroupIndexBytes),
-            COMPONENT_MERKLE_TREE_ENTROPY_ALGORITHM_NAME
-        ).bytes
-
-    private fun getComponentMerkleTreeDigestProvider(
-        privacySalt: PrivacySalt,
-        componentGroupIndex: Int
-    ) : MerkleTreeHashDigestProvider =
-        merkleTreeFactory.createHashDigestProvider(
-            COMPONENT_MERKLE_TREE_DIGEST_PROVIDER_NAME,
-            COMPONENT_MERKLE_TREE_DIGEST_ALGORITHM_NAME,
-            mapOf(
-                HASH_DIGEST_PROVIDER_ENTROPY_OPTION to
-                    getComponentGroupEntropy(privacySalt, componentGroupIndex.toByteArray())
-            )
-    )
-
-    private fun getComponentList(component: ConsensualComponentGroups): List<ByteArray> {
-        return when (component) {
-            ConsensualComponentGroups.METADATA -> listOf(metadata)
-            ConsensualComponentGroups.TIMESTAMP -> listOf(timestamp)
-            ConsensualComponentGroups.REQUIRED_SIGNERS -> requiredSigners
-            ConsensualComponentGroups.OUTPUT_STATES -> consensualStates
-            ConsensualComponentGroups.OUTPUT_STATE_TYPES -> consensualStateTypes
+    companion object {
+        private fun calculateComponentGroupLists(
+            metadata: ByteArray,
+            timestamp: ByteArray,
+            requiredSigners: List<ByteArray>,
+            consensualStates: List<ByteArray>,
+            consensualStateTypes: List<ByteArray>
+        ): List<List<ByteArray>> {
+            // TODO(all the checks from te Tx Builder here too? Maybe remove the calculated arguments?Probably not since this may be called when it comes from DB. I guess. But at least the number of item in output states/state types should be the same)
+            val componentGroupLists = mutableListOf<List<ByteArray>>()
+            for (componentGroupIndex in ConsensualComponentGroups.values()) {
+                componentGroupLists += when (componentGroupIndex) {
+                    ConsensualComponentGroups.METADATA -> listOf(metadata)
+                    ConsensualComponentGroups.TIMESTAMP -> listOf(timestamp)
+                    ConsensualComponentGroups.REQUIRED_SIGNERS -> requiredSigners
+                    ConsensualComponentGroups.OUTPUT_STATES -> consensualStates
+                    ConsensualComponentGroups.OUTPUT_STATE_TYPES -> consensualStateTypes
+                }
+            }
+            return componentGroupLists
         }
     }
 
-    private val rootMerkleTree: MerkleTree by lazy(LazyThreadSafetyMode.PUBLICATION) {
-        val componentsList = mutableListOf<ByteArray>()
-        for (componentGroup in ConsensualComponentGroups.values()) {
-            val leaves = getComponentList(componentGroup)
-            val componentMerkleTree = merkleTreeFactory.createTree(
-                leaves,
-                getComponentMerkleTreeDigestProvider(privacySalt, componentGroup.ordinal)
-            )
-            componentMerkleTrees[componentGroup] = componentMerkleTree
-            componentsList += componentMerkleTree.root.bytes
-        }
-        merkleTreeFactory.createTree(componentsList, getRootMerkleTreeDigestProvider())
-    }
+    private val componentMerkleTrees = ConcurrentHashMap<ConsensualComponentGroups, MerkleTree>() // TODO(will we need this?)
+
 }
