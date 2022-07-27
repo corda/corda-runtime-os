5a371183799ac114c56aa4e94dc8a7d3d04770a8
Christian Sailer <christian.sailer@r3.com>
2022-07-27 18:34:16 +0100

    Move CORDA_SYSTEM constant to sandbox interface package

--- libs/virtual-node/sandbox-group-context/src/main/kotlin/net/corda/sandboxgroupcontext/Constants.kt
+++ libs/virtual-node/sandbox-group-context/src/main/kotlin/net/corda/sandboxgroupcontext/Constants.kt
@@ -1,7 +1,8 @@
 @file:JvmName("Constants")
 package net.corda.sandboxgroupcontext
 
+import net.corda.sandbox.CORDA_SYSTEM
+
 const val CORDA_SANDBOX = "corda.sandbox"
 const val CORDA_SANDBOX_FILTER = "($CORDA_SANDBOX=true)"
-const val CORDA_SYSTEM = "corda.system"
 const val CORDA_SYSTEM_FILTER = "($CORDA_SYSTEM=true)"
