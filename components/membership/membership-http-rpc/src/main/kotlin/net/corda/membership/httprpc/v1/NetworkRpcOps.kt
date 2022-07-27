d8b34256392bfea60f39b69228cfc411d0f16021
Viktor Kolomeyko <Viktor.Kolomeyko@r3.com>
2022-07-27 14:44:37 +0100

    CORE-5913: Changes to `NetworkRpcOps`
    Introducing `HostedIdentitySetupRequest` and using it.

--- components/membership/membership-http-rpc/src/main/kotlin/net/corda/membership/httprpc/v1/NetworkRpcOps.kt
+++ components/membership/membership-http-rpc/src/main/kotlin/net/corda/membership/httprpc/v1/NetworkRpcOps.kt
@@ -5,6 +5,7 @@ import net.corda.httprpc.annotations.HttpRpcPUT
 import net.corda.httprpc.annotations.HttpRpcPathParameter
 import net.corda.httprpc.annotations.HttpRpcRequestBodyParameter
 import net.corda.httprpc.annotations.HttpRpcResource
+import net.corda.membership.httprpc.v1.types.request.HostedIdentitySetupRequest
 
 @HttpRpcResource(
     name = "Network API",
@@ -14,12 +15,10 @@ import net.corda.httprpc.annotations.HttpRpcResource
 interface NetworkRpcOps : RpcOps {
 
     /**
-     * POST endpoint which set up the identity network.
+     * PUT endpoint which set up the identity network.
      *
      * @param holdingIdentityShortHash ID of the holding identity to set up.
-     * @param certificateChainAlias The certificates chain alias.
-     * @param tlsTenantId The TLS tenant ID (either 'p2p' or the holdingIdentityShortHash, default to the holdingIdentityShortHash).
-     * @param sessionKeyAlias The session key alias (will use the first one if null).
+
      */
     @HttpRpcPUT(
         path = "setup/{holdingIdentityShortHash}",
@@ -29,19 +28,8 @@ interface NetworkRpcOps : RpcOps {
         @HttpRpcPathParameter(description = "ID of the holding identity to set up.")
         holdingIdentityShortHash: String,
         @HttpRpcRequestBodyParameter(
-            description = "The certificates chain alias.",
-            required = true,
+            description = "Additional parameters for setting up the hosted identity.",
         )
-        certificateChainAlias: String,
-        @HttpRpcRequestBodyParameter(
-            description = "The TLS tenant ID (either 'p2p' or the holdingIdentityShortHash, default to the holdingIdentityShortHash).",
-            required = false,
-        )
-        tlsTenantId: String?,
-        @HttpRpcRequestBodyParameter(
-            description = "The session key ID (will use the first session one by default).",
-            required = false,
-        )
-        sessionKeyId: String?,
+        request: HostedIdentitySetupRequest
     )
 }
