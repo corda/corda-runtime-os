fca7bc1215ca10c305b32d66462b98f982ad30b7
Charlie Crean <charlie.crean@r3.com>
2022-07-29 18:26:11 +0100

    allow MGM to have pending members in it's member cache and to look them up through the group reader

--- components/membership/membership-group-read-impl/src/test/kotlin/net/corda/membership/impl/read/reader/MembershipGroupReaderImplTest.kt
+++ components/membership/membership-group-read-impl/src/test/kotlin/net/corda/membership/impl/read/reader/MembershipGroupReaderImplTest.kt
@@ -1,13 +1,18 @@
 package net.corda.membership.impl.read.reader
 
-import net.corda.membership.lib.MemberInfoExtension.Companion.LEDGER_KEY_HASHES
-import net.corda.membership.lib.MemberInfoExtension.Companion.SESSION_KEY_HASH
 import net.corda.membership.impl.read.TestProperties
 import net.corda.membership.impl.read.TestProperties.Companion.GROUP_ID_1
 import net.corda.membership.impl.read.cache.MemberListCache
 import net.corda.membership.impl.read.cache.MembershipGroupReadCache
+import net.corda.membership.lib.MemberInfoExtension.Companion.LEDGER_KEY_HASHES
+import net.corda.membership.lib.MemberInfoExtension.Companion.MEMBER_STATUS_ACTIVE
+import net.corda.membership.lib.MemberInfoExtension.Companion.MEMBER_STATUS_PENDING
+import net.corda.membership.lib.MemberInfoExtension.Companion.MEMBER_STATUS_SUSPENDED
+import net.corda.membership.lib.MemberInfoExtension.Companion.SESSION_KEY_HASH
+import net.corda.membership.lib.MemberInfoExtension.Companion.STATUS
 import net.corda.v5.crypto.PublicKeyHash
 import net.corda.v5.crypto.sha256Bytes
+import net.corda.v5.membership.MGMContext
 import net.corda.v5.membership.MemberContext
 import net.corda.v5.membership.MemberInfo
 import net.corda.virtualnode.HoldingIdentity
@@ -41,24 +46,43 @@ class MembershipGroupReaderImplTest {
         on { parseSet(eq(LEDGER_KEY_HASHES), eq(PublicKeyHash::class.java)) } doReturn setOf(mockLedgerKeyHash)
         on { parse(eq(SESSION_KEY_HASH), eq(PublicKeyHash::class.java)) } doReturn mockSessionKeyHash
     }
+    private val mockedSuspendedMgmProvidedContext = mock<MGMContext> {
+        on { parse(eq(STATUS), eq(String::class.java)) } doReturn MEMBER_STATUS_SUSPENDED
+    }
     private val suspendedMemberInfo: MemberInfo = mock {
         on { name } doReturn aliceName
         on { ledgerKeys } doReturn listOf(mockLedgerKey)
         on { sessionInitiationKey } doReturn mockSessionKey
         on { memberProvidedContext } doReturn mockedSuspendedMemberProvidedContext
+        on { mgmProvidedContext } doReturn mockedSuspendedMgmProvidedContext
         on { isActive } doReturn false
     }
 
     private val mockedActiveMemberProvidedContext = mock<MemberContext> {
         on { parseSet(eq(LEDGER_KEY_HASHES), eq(PublicKeyHash::class.java)) } doReturn setOf(mockLedgerKeyHash)
         on { parse(eq(SESSION_KEY_HASH), eq(PublicKeyHash::class.java)) } doReturn mockSessionKeyHash
+        on { parse(eq(STATUS), eq(String::class.java)) } doReturn MEMBER_STATUS_ACTIVE
+    }
+    private val mockedActiveMgmProvidedContext = mock<MGMContext> {
+        on { parse(eq(STATUS), eq(String::class.java)) } doReturn MEMBER_STATUS_ACTIVE
     }
     private val activeMemberInfo: MemberInfo = mock {
         on { name } doReturn aliceName
         on { ledgerKeys } doReturn listOf(mockLedgerKey)
         on { sessionInitiationKey } doReturn mockSessionKey
         on { memberProvidedContext } doReturn mockedActiveMemberProvidedContext
+        on { mgmProvidedContext } doReturn mockedActiveMgmProvidedContext
         on { isActive } doReturn true
+    }
+
+    private val mockedPendingMgmProvidedContext = mock<MGMContext> {
+        on { parse(eq(STATUS), eq(String::class.java)) } doReturn MEMBER_STATUS_PENDING
+    }
+    private val pendingMemberInfo: MemberInfo = mock {
+        on { name } doReturn aliceName
+        on { memberProvidedContext } doReturn mockedActiveMemberProvidedContext
+        on { mgmProvidedContext } doReturn mockedPendingMgmProvidedContext
+        on { isActive } doReturn false
     }
 
     @BeforeEach
@@ -83,6 +107,12 @@ class MembershipGroupReaderImplTest {
     fun `lookup known member with non active status based on name`() {
         mockMemberList(listOf(suspendedMemberInfo))
         assertNull(membershipGroupReaderImpl.lookup(aliceName))
+    }
+
+    @Test
+    fun `lookup known member with pending status based on name`() {
+        mockMemberList(listOf(pendingMemberInfo))
+        assertEquals(pendingMemberInfo, membershipGroupReaderImpl.lookup(aliceName))
     }
 
     @Test
