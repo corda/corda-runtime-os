ae3827a70437453e3bc9217350da79b73faa8234
Lajos Veres <lajos.veres@r3.com>
2022-08-01 15:31:43 +0100

    CORE-5001 ConsensualSignedTransactionImpl.missingSigningKeys()

--- libs/ledger/ledger-consensual-impl/src/main/kotlin/net/corda/ledger/consensual/impl/transactions/ConsensualSignedTransactionImpl.kt
+++ libs/ledger/ledger-consensual-impl/src/main/kotlin/net/corda/ledger/consensual/impl/transactions/ConsensualSignedTransactionImpl.kt
@@ -4,8 +4,10 @@ import net.corda.ledger.common.impl.transactions.WireTransaction
 import net.corda.v5.application.crypto.DigitalSignatureAndMetadata
 import net.corda.v5.application.serialization.SerializationService
 import net.corda.v5.crypto.SecureHash
+import net.corda.v5.crypto.isFulfilledBy
 import net.corda.v5.ledger.consensual.transaction.ConsensualLedgerTransaction
 import net.corda.v5.ledger.consensual.transaction.ConsensualSignedTransaction
+import java.security.PublicKey
 
 class ConsensualSignedTransactionImpl(
     val wireTransaction: WireTransaction,
@@ -32,4 +34,10 @@ class ConsensualSignedTransactionImpl(
     /** Returns the same transaction but with an additional (unchecked) signatures. */
     override fun withAdditionalSignatures(sigList: Iterable<DigitalSignatureAndMetadata>): ConsensualSignedTransaction =
         ConsensualSignedTransactionImpl(wireTransaction, sigs + sigList)
+
+    override fun missingSigningKeys(serializer: SerializationService): Set<PublicKey> {
+        val alreadySigned = sigs.map{it.by}.toSet()
+        val requiredSigningKeys = this.toLedgerTransaction(serializer).requiredSigningKeys
+        return requiredSigningKeys.filter { !it.isFulfilledBy(alreadySigned) }.toSet()
+    }
 }
