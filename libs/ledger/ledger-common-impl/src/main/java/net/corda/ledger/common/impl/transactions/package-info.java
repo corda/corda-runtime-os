4c0e83078fc6e5f237737f97bae0b7de88cba134
Lajos Veres <lajos.veres@r3.com>
2022-08-03 12:04:19 +0100

    CORE-5001 package-info.java is needed since consensual-impl depends directly on common-impl

---
+++ libs/ledger/ledger-common-impl/src/main/java/net/corda/ledger/common/impl/transactions/package-info.java
@@ -0,0 +1,13 @@
+@Export
+package net.corda.ledger.common.impl.transactions;
+
+import org.osgi.annotation.bundle.Export;
+
+/*
+For
+    [<<INITIAL>>]
+      ⇒ osgi.identity: (osgi.identity=net.corda.ledger-consensual-impl)
+          ⇒ [net.corda.ledger-consensual-impl version=5.0.0.0-SNAPSHOT]
+              ⇒ osgi.wiring.package: (osgi.wiring.package=net.corda.ledger.common.impl.transactions)
+
+ */
