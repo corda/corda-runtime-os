6cd8ebccdf73f19df5398c6d4e5c52b04c7c0907
Lajos Veres <lajos.veres@r3.com>
2022-08-03 09:58:37 +0100

    CORE-5001 Consensual Ledger Service integration test - wip

--- libs/ledger/ledger-consensual-impl/src/integrationTest/kotlin/net/corda/ledger/consensual/ConsensualLedgerServiceTest.kt
+++ libs/ledger/ledger-consensual-impl/src/integrationTest/kotlin/net/corda/ledger/consensual/ConsensualLedgerServiceTest.kt
@@ -1,11 +1,12 @@
 package net.corda.ledger.consensual
 
 import net.corda.v5.ledger.consensual.ConsensualLedgerService
-import org.assertj.core.api.Assertions.assertThat
+import net.corda.v5.ledger.consensual.transaction.ConsensualTransactionBuilder
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS
 import org.osgi.test.common.annotation.InjectService
+import kotlin.test.assertIs
 
 @TestInstance(PER_CLASS)
 @Suppress("FunctionName")
@@ -15,7 +16,8 @@ class ConsensualLedgerServiceTest {
     lateinit var consensualLedgerService: ConsensualLedgerService
 
     @Test
-    fun `can inject and use the service`() {
-        assertThat(consensualLedgerService.double(21)).isEqualTo(42)
+    fun `getTransactionBuilder should return a Transaction Builder`() {
+        val transactionBuilder = consensualLedgerService.getTransactionBuilder()
+        assertIs<ConsensualTransactionBuilder>(transactionBuilder)
     }
 }
