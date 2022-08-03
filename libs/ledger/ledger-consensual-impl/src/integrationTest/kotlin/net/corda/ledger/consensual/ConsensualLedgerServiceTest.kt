64831f9b7144c8f381b16d0257fb664aaf72e5d5
Lajos Veres <lajos.veres@r3.com>
2022-08-03 12:03:47 +0100

    CORE-5001 Get rid of kotlin.test

--- libs/ledger/ledger-consensual-impl/src/integrationTest/kotlin/net/corda/ledger/consensual/ConsensualLedgerServiceTest.kt
+++ libs/ledger/ledger-consensual-impl/src/integrationTest/kotlin/net/corda/ledger/consensual/ConsensualLedgerServiceTest.kt
@@ -2,11 +2,11 @@ package net.corda.ledger.consensual
 
 import net.corda.v5.ledger.consensual.ConsensualLedgerService
 import net.corda.v5.ledger.consensual.transaction.ConsensualTransactionBuilder
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS
 import org.osgi.test.common.annotation.InjectService
-import kotlin.test.assertIs
 
 @TestInstance(PER_CLASS)
 @Suppress("FunctionName")
@@ -18,6 +18,6 @@ class ConsensualLedgerServiceTest {
     @Test
     fun `getTransactionBuilder should return a Transaction Builder`() {
         val transactionBuilder = consensualLedgerService.getTransactionBuilder()
-        assertIs<ConsensualTransactionBuilder>(transactionBuilder)
+        assertThat(transactionBuilder).isInstanceOf(ConsensualTransactionBuilder::class.java)
     }
 }
