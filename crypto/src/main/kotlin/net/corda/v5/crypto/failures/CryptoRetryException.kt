5acbc7e493cb3ef0169d961799a7f6736aed7e68
Connel McGovern <100574906+mcgovc@users.noreply.github.com>
2022-08-04 12:37:18 +0100

    Update CryptoRetryException.kt

--- crypto/src/main/kotlin/net/corda/v5/crypto/failures/CryptoRetryException.kt
+++ crypto/src/main/kotlin/net/corda/v5/crypto/failures/CryptoRetryException.kt
@@ -2,6 +2,8 @@ package net.corda.v5.crypto.failures
 
 import net.corda.v5.base.annotations.CordaSerializable
 
+// Modifying to test SNYK Delta - not to be merged
+
 /**
  * Signals that the transient fault handling was attempted but wasn't successful.
  */
