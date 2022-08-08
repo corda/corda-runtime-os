2e252b0ca73ffeef57c7994eedcb3e609a12b9d5
LizR3 <91971483+LizR3@users.noreply.github.com>
2022-08-08 16:22:36 +0100

    CORE-6008: Removed println debugging

--- cordapp-test-utils/test-utils/src/main/kotlin/net/corda/testutils/tools/Utilities.kt
+++ cordapp-test-utils/test-utils/src/main/kotlin/net/corda/testutils/tools/Utilities.kt
@@ -9,7 +9,6 @@ fun <T> Flow.injectIfRequired(
     field: Class<T>,
     value: T
 ) = this.javaClass.declaredFields.firstOrNull {
-    println("Field ${it.name} is ${Modifier.toString(it.modifiers)} and of type ${it.type}")
     it.type.equals(field) && it.canAccess(this) && it.isAnnotationPresent(CordaInject::class.java)
 }?.set(this, value)
 
