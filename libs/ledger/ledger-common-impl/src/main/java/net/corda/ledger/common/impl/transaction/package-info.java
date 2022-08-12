2fa18b4c22cb98026e4ef7b9313ac5a1db2e94a9
Lajos Veres <lajos.veres@r3.com>
2022-08-12 11:27:38 +0100

    CORE-5001 fix typo

--- libs/ledger/ledger-common-impl/src/main/java/net/corda/ledger/common/impl/transaction/package-info.java
+++ libs/ledger/ledger-common-impl/src/main/java/net/corda/ledger/common/impl/transaction/package-info.java
@@ -8,6 +8,6 @@ For
     [<<INITIAL>>]
       ⇒ osgi.identity: (osgi.identity=net.corda.ledger-consensual-impl)
           ⇒ [net.corda.ledger-consensual-impl version=5.0.0.0-SNAPSHOT]
-              ⇒ osgi.wiring.package: (osgi.wiring.package=net.corda.ledger.common.impl.transactions)
+              ⇒ osgi.wiring.package: (osgi.wiring.package=net.corda.ledger.common.impl.transaction)
 
  */
