5bef995c7f681fd5c1ff132fe3bb055ffa4d4a2e
Lajos Veres <lajos.veres@r3.com>
2022-08-15 19:39:00 +0100

    CORE-5001 CORE-5001 ConsensualFlow serialisation issue: PartySerializer v2 - working

--- libs/ledger/ledger-consensual-impl/src/main/kotlin/net/corda/ledger/consensual/impl/transaction/PartySerializer.kt
+++ libs/ledger/ledger-consensual-impl/src/main/kotlin/net/corda/ledger/consensual/impl/transaction/PartySerializer.kt
@@ -3,20 +3,24 @@ package net.corda.ledger.consensual.impl.transaction
 import net.corda.ledger.consensual.impl.PartyImpl
 import net.corda.serialization.BaseProxySerializer
 import net.corda.serialization.InternalCustomSerializer
+import net.corda.v5.base.types.MemberX500Name
 import net.corda.v5.ledger.consensual.Party
 import org.osgi.service.component.annotations.Activate
 import org.osgi.service.component.annotations.Component
+import java.security.PublicKey
 
 @Component(service = [InternalCustomSerializer::class])
-class PartySerializer @Activate constructor() : BaseProxySerializer<Party, PartyImpl>() {
-    override fun toProxy(obj: Party): PartyImpl = PartyImpl(obj.name, obj.owningKey)
+class PartySerializer @Activate constructor() : BaseProxySerializer<Party, PartySerializer.PartyProxy>() {
+    override fun toProxy(obj: Party): PartyProxy = PartyProxy(obj.name.toString(), obj.owningKey)
 
-    override fun fromProxy(proxy: PartyImpl): Party = proxy
+    override fun fromProxy(proxy: PartyProxy): Party = PartyImpl(MemberX500Name.Companion.parse(proxy.name), proxy.owningKey)
 
-    override val proxyType: Class<PartyImpl>
-        get() = PartyImpl::class.java
+    override val proxyType: Class<PartyProxy>
+        get() = PartyProxy::class.java
     override val type: Class<Party>
         get() = Party::class.java
     override val withInheritance: Boolean
         get() = true
+
+    data class PartyProxy(val name: String, val owningKey: PublicKey)
 }
