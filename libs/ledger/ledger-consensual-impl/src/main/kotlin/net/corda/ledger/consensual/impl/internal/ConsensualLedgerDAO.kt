57cf3a9caf20e7c08ac113fe35548e027543edc6
Oisín Mac Fhearaí <oisin.macfhearai@r3.com>
2022-08-16 18:00:04 +0100

    CORE-5088: (ConsensualLedgerDAO) More DB support
    * Add temporary SignedTransaction/MerkleTree data classes to hold dummy data for now.
    * Write something to the consensual_transaction_status table when persisting a
      transaction.

--- libs/ledger/ledger-consensual-impl/src/main/kotlin/net/corda/ledger/consensual/impl/internal/ConsensualLedgerDAO.kt
+++ libs/ledger/ledger-consensual-impl/src/main/kotlin/net/corda/ledger/consensual/impl/internal/ConsensualLedgerDAO.kt
@@ -16,24 +16,57 @@ class ConsensualLedgerDAO(
         private val logger = contextLogger()
     }
 
+    // TODO: rm tmp transaction/leaf structures when real ones are merged
+    data class SignedTransaction(val id: String, val privacySalt: Array<Byte>, val accountId: String, val merkleTree: List<List<MerkleLeaf>>)
+    data class MerkleLeaf(val data: Array<Byte>, val hash: String)
+
     fun persistTransaction(request: PersistTransaction, entityManager: EntityManager): EntityResponse {
         logger.debug("TMP DEBUG 1. request: $request, requestId: $requestId, loadClass: $loadClass")
         val now = clock()
-        // we're already inside a transaction
+        // TODO: deserialise the request data to a real SignedTransaction
+        val tx = SignedTransaction(
+            "0",
+            arrayOf<Byte>(1),
+            "2",
+            listOf(listOf(MerkleLeaf(arrayOf<Byte>(3,4), "5")))
+        )
         // do some native queries to insert multiple rows (and across tables)
-        val query = entityManager.createNativeQuery("""
-            INSERT INTO {h-schema}consensual_transaction(id, privacy_salt, account_id, created)
-            VALUES (:id, :privacySalt, :accountId, :createdAt)""")
-            .setParameter("id", "0")
-            .setParameter("privacySalt", arrayOf<Byte>(1))
-            .setParameter("accountId", "2")
-            .setParameter("createdAt", now)
-
-        // execute query
-        val result = query.executeUpdate()
+        writeTransaction(entityManager, now, tx)
+        writeTransactionStatus(entityManager, now, tx, "Faked")
+        // TODO: when and what do we write to the signatures table?
+        // TODO: when and what do we write to the CPKs table?
 
         // construct response
-        logger.info("TMP DEBUG 2. result of insert: $result")
         return EntityResponse(now, requestId, EntityResponseSuccess())
+    }
+
+    private fun writeTransaction(entityManager: EntityManager, timestamp: Instant, tx: SignedTransaction) {
+        entityManager.createNativeQuery(
+            """
+                INSERT INTO {h-schema}consensual_transaction(id, privacy_salt, account_id, created)
+                VALUES (:id, :privacySalt, :accountId, :createdAt)"""
+        )
+            .setParameter("id", tx.id)
+            .setParameter("privacySalt", tx.privacySalt)
+            .setParameter("accountId", tx.accountId)
+            .setParameter("createdAt", timestamp)
+            .executeUpdate()
+    }
+
+    private fun writeTransactionStatus(
+        entityManager: EntityManager,
+        timestamp: Instant,
+        tx: SignedTransaction,
+        status: String
+    ) {
+        entityManager.createNativeQuery(
+            """
+                INSERT INTO {h-schema}consensual_transaction_status(transaction_id, status, created)
+                VALUES (:txId, :status, :createdAt)"""
+        )
+            .setParameter("txId", tx.id)
+            .setParameter("status", status)
+            .setParameter("createdAt", timestamp)
+            .executeUpdate()
     }
 }
