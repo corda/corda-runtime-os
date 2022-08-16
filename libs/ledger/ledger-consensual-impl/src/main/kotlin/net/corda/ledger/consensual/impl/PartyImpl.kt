0353bae9010be9419d0cd7837128787703ca5577
Lajos Veres <lajos.veres@r3.com>
2022-08-16 17:37:35 +0100

    CORE-5001 Refactor Party.description into PartyImpl.toString()

--- libs/ledger/ledger-consensual-impl/src/main/kotlin/net/corda/ledger/consensual/impl/PartyImpl.kt
+++ libs/ledger/ledger-consensual-impl/src/main/kotlin/net/corda/ledger/consensual/impl/PartyImpl.kt
@@ -1,6 +1,7 @@
 package net.corda.ledger.consensual.impl
 
 import net.corda.v5.base.types.MemberX500Name
+import net.corda.v5.crypto.toStringShort
 import net.corda.v5.ledger.consensual.Party
 import java.security.PublicKey
 
@@ -10,7 +11,7 @@ class PartyImpl(override val name: MemberX500Name, override val owningKey: Publi
             : this(MemberX500Name.build(certificate.subjectX500Principal), certificate.publicKey)
     */
     // todo: review these. Name is not really used. Is this OK?
-    override fun toString() = name.toString()
+    override fun toString() = "$name (owningKey = ${owningKey.toStringShort()})"
     override fun equals(other: Any?): Boolean =
         other === this ||
                 other is Party &&
