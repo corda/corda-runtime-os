3938eb90d53acd55743b452ad5c68c66ac3b9eb3
Dickon Reed <dickon.reed@r3.com>
2022-08-17 16:55:43 +0100

    NOSHARE: change port and logging for combined worked

--- applications/workers/worker-common/src/main/kotlin/net/corda/applications/workers/workercommon/internal/Constants.kt
+++ applications/workers/worker-common/src/main/kotlin/net/corda/applications/workers/workercommon/internal/Constants.kt
@@ -5,5 +5,5 @@ internal const val HTTP_SERVICE_UNAVAILABLE_CODE = 503
 internal const val HTTP_HEALTH_ROUTE = "/isHealthy"
 internal const val HTTP_METRICS_ROUTE = "/metrics"
 internal const val HTTP_STATUS_ROUTE = "/status"
-internal const val WORKER_MONITOR_PORT = 7000
+internal const val HEALTH_MONITOR_PORT = 7002
 internal const val NO_CACHE = "no-cache"
