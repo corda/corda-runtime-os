12e669106174466c8c78e2d615ca2023fa46bdc7
Lajos Veres <lajos.veres@r3.com>
2022-08-19 09:33:12 +0100

    CORE-5001 Remove todo

--- libs/ledger/ledger-consensual-impl/src/main/kotlin/net/corda/ledger/consensual/impl/transaction/ConsensualComponentGroupEnum.kt
+++ libs/ledger/ledger-consensual-impl/src/main/kotlin/net/corda/ledger/consensual/impl/transaction/ConsensualComponentGroupEnum.kt
@@ -3,7 +3,7 @@ package net.corda.ledger.consensual.impl.transaction
 internal enum class ConsensualComponentGroupEnum {
     METADATA, // needs to be in sync with [net.corda.ledger.common.impl.transaction.WireTransactionImplKt.ALL_LEDGER_METADATA_COMPONENT_GROUP_ID]
     TIMESTAMP,
-    REQUIRED_SIGNING_KEYS, //TODO(signers? signing keys? CORE-5936)
+    REQUIRED_SIGNING_KEYS,
     OUTPUT_STATES,
     OUTPUT_STATE_TYPES
 }
