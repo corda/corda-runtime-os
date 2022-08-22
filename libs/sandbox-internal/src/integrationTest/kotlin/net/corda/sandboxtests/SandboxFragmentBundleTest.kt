755974fcb900916ae6eaffa63521493359fbe107
Chris Rankin <chris.rankin@r3.com>
2022-08-22 14:04:31 +0100

    CORE-6330: Fix handling of fragment bundles inside sandboxes. (#1931)

--- libs/sandbox-internal/src/integrationTest/kotlin/net/corda/sandboxtests/SandboxFragmentBundleTest.kt
+++ libs/sandbox-internal/src/integrationTest/kotlin/net/corda/sandboxtests/SandboxFragmentBundleTest.kt
@@ -1,16 +1,14 @@
 package net.corda.sandboxtests
 
 import java.nio.file.Path
-import net.corda.sandbox.SandboxException
 import net.corda.testing.sandboxes.SandboxSetup
 import net.corda.testing.sandboxes.fetchService
 import net.corda.testing.sandboxes.lifecycle.AllTestsLifecycle
-import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS
-import org.junit.jupiter.api.assertThrows
 import org.junit.jupiter.api.extension.ExtendWith
 import org.junit.jupiter.api.extension.RegisterExtension
 import org.junit.jupiter.api.io.TempDir
@@ -20,10 +18,10 @@ import org.osgi.test.common.annotation.InjectService
 import org.osgi.test.junit5.context.BundleContextExtension
 import org.osgi.test.junit5.service.ServiceExtension
 
-/** Tests the inability to resolve against private bundles in a public sandbox. */
+@Suppress("FunctionName")
 @ExtendWith(ServiceExtension::class, BundleContextExtension::class)
 @TestInstance(PER_CLASS)
-class SandboxIrresolvableBundleTest {
+class SandboxFragmentBundleTest {
     @RegisterExtension
     private val lifecycle = AllTestsLifecycle()
 
@@ -45,12 +43,9 @@ class SandboxIrresolvableBundleTest {
     }
 
     @Test
-    fun `bundle cannot resolve against a private bundle in a public sandbox`() {
-        val e = assertThrows<SandboxException> {
-            sandboxFactory.createSandboxGroupFor("META-INF/sandbox-irresolvable-cpk.cpb")
-        }
-        assertThat(e)
-            .hasMessageStartingWith("Failed to resolve bundles: ")
-            .hasMessageContaining("com.example.sandbox.sandbox-irresolvable-cpk")
+    fun `fragment bundle inside sandbox is attached to its host`() {
+        val sandboxGroup = sandboxFactory.createSandboxGroupFor("META-INF/sandbox-fragment-cpk.cpb")
+        val message = runFlow<String>(sandboxGroup, "com.example.fragment.ExampleFlow")
+        assertEquals("Hello, Corda Fragment!", message)
     }
 }
