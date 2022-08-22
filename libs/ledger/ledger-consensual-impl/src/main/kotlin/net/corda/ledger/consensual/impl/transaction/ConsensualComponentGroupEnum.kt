8ff04ab3ad03c35cbdfd3af91dbee944a87914e9
Lajos Veres <lajos.veres@r3.com>
2022-08-22 10:12:28 +0100

    CORE-5001 Document ConsensualComponentGroupEnum

--- libs/ledger/ledger-consensual-impl/src/main/kotlin/net/corda/ledger/consensual/impl/transaction/ConsensualComponentGroupEnum.kt
+++ libs/ledger/ledger-consensual-impl/src/main/kotlin/net/corda/ledger/consensual/impl/transaction/ConsensualComponentGroupEnum.kt
@@ -1,5 +1,15 @@
 package net.corda.ledger.consensual.impl.transaction
 
+/**
+ * Specifies Consensual transaction component groups.
+ *
+ * @property METADATA The metadata parameters component group. Ordinal = 0. (It needs to be in the first position.)
+ * @property TIMESTAMP The timestamp parameter component group. Ordinal = 1.
+ * @property REQUIRED_SIGNING_KEYS The required signing keys component group. Ordinal = 2.
+ * @property OUTPUT_STATES The output states component group. Ordinal = 3.
+ * @property OUTPUT_STATE_TYPES The output state types component group. Ordinal = 4.
+ */
+
 internal enum class ConsensualComponentGroupEnum {
     METADATA, // needs to be in sync with [net.corda.ledger.common.impl.transaction.WireTransactionImplKt.ALL_LEDGER_METADATA_COMPONENT_GROUP_ID]
     TIMESTAMP,
