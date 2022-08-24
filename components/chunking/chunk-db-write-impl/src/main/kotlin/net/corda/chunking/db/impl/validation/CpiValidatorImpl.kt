7b262ea5608e9f9855c7d82be16cae42bcc173bc
Ben McMahon <benmcmahon100@gmail.com>
2022-08-24 17:02:57 +0100

    Fix tests/detekt

--- components/chunking/chunk-db-write-impl/src/main/kotlin/net/corda/chunking/db/impl/validation/CpiValidatorImpl.kt
+++ components/chunking/chunk-db-write-impl/src/main/kotlin/net/corda/chunking/db/impl/validation/CpiValidatorImpl.kt
@@ -45,7 +45,7 @@ class CpiValidatorImpl constructor(
         private const val CERTIFICATE_TYPE = "codesigner"
     }
 
-    @Suppress("ForbiddenComment")
+    @Suppress("ForbiddenComment", "ComplexMethod")
     override fun validate(requestId: RequestId): SecureHash {
         //  Each function may throw a [ValidationException]
         log.debug("Validating $requestId")
