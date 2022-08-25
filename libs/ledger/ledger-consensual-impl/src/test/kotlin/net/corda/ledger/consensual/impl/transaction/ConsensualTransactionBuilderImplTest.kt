5dacf2d78ba387a9061999b93aa74e13dbe3b5dc
Lajos Veres <lajos.veres@r3.com>
2022-08-25 15:21:26 +0100

    CORE-5001 follow net.corda.v5.crypto.DigestService -> net.corda.v5.cipher.suite.DigestService

--- libs/ledger/ledger-consensual-impl/src/test/kotlin/net/corda/ledger/consensual/impl/transaction/ConsensualTransactionBuilderImplTest.kt
+++ libs/ledger/ledger-consensual-impl/src/test/kotlin/net/corda/ledger/consensual/impl/transaction/ConsensualTransactionBuilderImplTest.kt
@@ -11,7 +11,7 @@ import net.corda.v5.application.crypto.SigningService
 import net.corda.v5.application.serialization.SerializationService
 import net.corda.v5.base.types.MemberX500Name
 import net.corda.v5.cipher.suite.CipherSchemeMetadata
-import net.corda.v5.crypto.DigestService
+import net.corda.v5.cipher.suite.DigestService
 import net.corda.v5.crypto.merkle.MerkleTreeFactory
 import net.corda.v5.ledger.consensual.ConsensualState
 import net.corda.v5.ledger.consensual.Party
