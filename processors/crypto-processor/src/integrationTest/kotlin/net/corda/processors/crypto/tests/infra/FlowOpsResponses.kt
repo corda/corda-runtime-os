66671a8b4d55a5845b4751bdb697d7c19393de1a
Dan Newton <dan.newton@r3.com>
2022-08-25 17:29:11 +0100

    CORE-5599 remove some logging

--- processors/crypto-processor/src/integrationTest/kotlin/net/corda/processors/crypto/tests/infra/FlowOpsResponses.kt
+++ processors/crypto-processor/src/integrationTest/kotlin/net/corda/processors/crypto/tests/infra/FlowOpsResponses.kt
@@ -1,31 +1,26 @@
 package net.corda.processors.crypto.tests.infra
 
+import java.time.Duration
+import java.util.concurrent.ConcurrentHashMap
+import net.corda.data.CordaAvroDeserializer
 import net.corda.data.crypto.wire.ops.flow.FlowOpsResponse
 import net.corda.data.flow.event.FlowEvent
+import net.corda.data.flow.event.external.ExternalEventResponse
 import net.corda.libs.configuration.SmartConfig
 import net.corda.messaging.api.processor.DurableProcessor
 import net.corda.messaging.api.records.Record
 import net.corda.messaging.api.subscription.Subscription
 import net.corda.messaging.api.subscription.config.SubscriptionConfig
 import net.corda.messaging.api.subscription.factory.SubscriptionFactory
+import net.corda.schema.Schemas
 import net.corda.test.util.eventually
 import org.junit.jupiter.api.Assertions
-import java.time.Duration
-import java.util.concurrent.ConcurrentHashMap
-import net.corda.data.CordaAvroDeserializer
-import net.corda.data.flow.event.external.ExternalEventResponse
-import net.corda.schema.Schemas
-import net.corda.v5.base.util.contextLogger
 
 class FlowOpsResponses(
     messagingConfig: SmartConfig,
     subscriptionFactory: SubscriptionFactory,
     private val deserializer: CordaAvroDeserializer<FlowOpsResponse>
 ) : DurableProcessor<String, FlowEvent>, AutoCloseable {
-
-    private companion object {
-        val log = contextLogger()
-    }
 
     private val subscription: Subscription<String, FlowEvent> =
         subscriptionFactory.createDurableSubscription(
@@ -46,11 +41,8 @@ class FlowOpsResponses(
 
     override fun onNext(events: List<Record<String, FlowEvent>>): List<Record<*, *>> {
         events.forEach {
-            log.info("Received response of $it")
             val response = ((it.value as FlowEvent).payload as ExternalEventResponse)
-            log.info("Response contains external event response of $response")
             val flowOpsResponse = deserializer.deserialize(response.payload.array())
-            log.info("Response contains flow ops response of $flowOpsResponse")
             receivedEvents[it.key] = flowOpsResponse
         }
         return emptyList()
