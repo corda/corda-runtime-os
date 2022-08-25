345d4604283d6f58ed86866f5254da6c3bafe593
Chris Rankin <chris.rankin@r3.com>
2022-08-25 16:48:46 +0100

    CORE-6345: Tidy up Javadoc for OSGi Bootstrapper. (#1951)

--- osgi-framework-bootstrap/src/main/java/net/corda/osgi/framework/OSGiFrameworkMain.java
+++ osgi-framework-bootstrap/src/main/java/net/corda/osgi/framework/OSGiFrameworkMain.java
@@ -17,8 +17,8 @@ import static java.util.stream.Collectors.toUnmodifiableList;
 
 /**
  * This class provided the main entry point for the applications built with the {@code corda.common-app} plugin.
- *
- * Modules having in {@code bundle.gradle} the blocks
+ * <p/>
+ * Modules having a block like this in their {@code bundle.gradle} script:
  * <p/>
  * <pre>{@code
  * plugins {
@@ -38,11 +38,11 @@ import static java.util.stream.Collectors.toUnmodifiableList;
  * }</pre>
  * in the {@code build/bin} directory.
  * The bootable JAR zips the <a href="https://felix.apache.org/">Apache Felix</a> OSGi framework,
- * the module assembles as an OSGi  bundle and all the OSGi bundles it requires.
- *
- * The bootable JAR is self-sufficient to start with
+ * the module assembles as an OSGi bundle and all the OSGi bundles it requires.
+ * <p/>
+ * The bootable JAR is self-contained, and can be run using
  * <pre>{@code
- *     java -jar corda-<module_name>-<version>.jar
+ *     $ java -jar corda-<module_name>-<version>.jar
  * }</pre>
  * The main entry point of the bootable JAR is the {@link #main} method.
  */
@@ -65,8 +65,10 @@ final class OSGiFrameworkMain {
     /**
      * Location of the list of bundles to install in the {@link OSGiFrameworkWrap} instance.
      * The location is relative to run time class path:
-     * * {@code build/resources/main} in a gradle project;
-     * * the root of the fat executable {@code .jar}.
+     * <ul>
+     * <li>{@code build/resources/main} in a gradle project</li>
+     * <li>the root of the fat executable {@code .jar}</li>
+     * </ul>
      */
     static final String APPLICATION_BUNDLES = "application_bundles";
 
@@ -74,30 +76,36 @@ final class OSGiFrameworkMain {
      * Location of the file listing the extra system packages exposed from the JDK to the framework.
      * See <a href="http://docs.osgi.org/specification/osgi.core/7.0.0/framework.lifecycle.html#framework.lifecycle.launchingproperties">OSGi Core Release 7 - 4.2.2 Launching Properties</a>
      * The location is relative to run time class path:
-     * * {@code build/resources/main} in a gradle project;
-     * * the root of the fat executable {@code .jar}.
+     * <ul>
+     * <li>{@code build/resources/main} in a gradle project</li>
+     * <li>the root of the fat executable {@code .jar}</li>
+     * </ul>
      */
     static final String SYSTEM_PACKAGES_EXTRA = "system_packages_extra";
 
     /**
      * The main entry point for the bootable JAR built with the `corda.common-app` plugin.
-     *
+     * <p/>
      * This method bootstraps the application:
-     * 1. **Start Up**
-     *      1. Start Felix OSGi framework
-     *      2. Install OSGi framework services.
-     * 2.  **Load bundles in bootstrapper**
-     *      1. Install OSGi bundles in the OSGi framework,
-     *      2. Activate OSGi bundles.
-     * 3. **Call application entry-point**
-     *      1. Call the {@link net.corda.osgi.api.Application#startup} method of active application bundles, if any,
-     *      passing {@code args}.
+     * <ol>
+     * <li><b>Start Up</b><ol>
+     *      <li>Start Felix OSGi framework</li>
+     *      <li>Install OSGi framework services.</li></ol>
+     * <li><b>Load bundles in bootstrapper</b><ol>
+     *      <li>Install OSGi bundles in the OSGi framework,</li>
+     *      <li>Activate OSGi bundles.</li></ol>
+     * <li><b>Call application entry-point</b<ol>
+     *      <li>Call the {@link net.corda.osgi.api.Application#startup} method of active application bundles, if any,
+     *      passing {@code args}.</li></ol>
+     * </ol>
      *
      *  Then, the method waits for the JVM receives the signal to terminate to
-     *  1. **Shut Down**
-     *      1. Call the {@link net.corda.osgi.api.Application#shutdown} method of application bundles, if any.
-     *      1. Deactivate OSGi bundles.
-     *      2. Stop the OSGi framework.
+     *  <ol>
+     *  <li><b>Shut Down</b><ol>
+     *      <li>Call the {@link net.corda.osgi.api.Application#shutdown} method of application bundles, if any.</li>
+     *      <li>Deactivate OSGi bundles.</li>
+     *      <li>Stop the OSGi framework.</li></ol>
+     *  </ol>
      *
      * @param args passed by the OS when invoking JVM to run this bootable JAR.
      */
@@ -179,7 +187,7 @@ final class OSGiFrameworkMain {
 
     /**
      * Get the db driver path up front, rather than in the {@link net.corda.osgi.api.Application}
-     *
+     * <p/>
      * We need this cli arg *before* the application parses the args
      * as we need it to find the location of the jdbc drivers so that
      * we can install them here.
