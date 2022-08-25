bfe6e74249916c039474dea74417e5ee5e733d33
Lajos Veres <lajos.veres@r3.com>
2022-08-25 09:22:23 +0100

    CORE-5002 test-serialization module (#1946)
    Extract testSerializationContext into a dedicated test-serialization module and some additional test helpers.

---
+++ testing/test-serialization/src/main/kotlin/net/corda/internal/serialization/amqp/helper/TestSerializationContext.kt
@@ -0,0 +1,33 @@
+@file:JvmName("TestSerializationContext")
+
+package net.corda.internal.serialization.amqp.helper
+
+import net.corda.internal.serialization.SerializationContextImpl
+import net.corda.internal.serialization.amqp.amqpMagic
+import net.corda.libs.packaging.core.CpkMetadata
+import net.corda.sandbox.SandboxGroup
+import net.corda.serialization.SerializationContext
+import org.osgi.framework.Bundle
+
+private class MockSandboxGroup(private val classLoader: ClassLoader = ClassLoader.getSystemClassLoader()) : SandboxGroup {
+    override val metadata: Map<Bundle, CpkMetadata> = emptyMap()
+
+    override fun loadClassFromMainBundles(className: String): Class<*> =
+        Class.forName(className, false, classLoader)
+    override fun <T : Any> loadClassFromMainBundles(className: String, type: Class<T>): Class<out T> =
+        Class.forName(className, false, classLoader).asSubclass(type)
+    override fun getClass(className: String, serialisedClassTag: String): Class<*> = Class.forName(className)
+
+    override fun getStaticTag(klass: Class<*>): String = "S;bundle;sandbox"
+    override fun getEvolvableTag(klass: Class<*>) = "E;bundle;sandbox"
+}
+
+@JvmField
+val testSerializationContext = SerializationContextImpl(
+    preferredSerializationVersion = amqpMagic,
+    properties = mutableMapOf(),
+    objectReferencesEnabled = false,
+    useCase = SerializationContext.UseCase.Testing,
+    encoding = null,
+    sandboxGroup = MockSandboxGroup()
+)
