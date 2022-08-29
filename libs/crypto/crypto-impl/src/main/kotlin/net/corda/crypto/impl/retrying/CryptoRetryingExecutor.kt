5d21fa93c35913f8cc907ce644b844e1ecc47a25
Alexey Kadyrov <alexey.kadyrov@r3.com>
2022-08-29 11:44:01 +0100

    CORE-6420 follow up fixes after changing the API

--- libs/crypto/crypto-impl/src/main/kotlin/net/corda/crypto/impl/retrying/CryptoRetryingExecutor.kt
+++ libs/crypto/crypto-impl/src/main/kotlin/net/corda/crypto/impl/retrying/CryptoRetryingExecutor.kt
@@ -1,7 +1,6 @@
 package net.corda.crypto.impl.retrying
 
 import net.corda.crypto.core.isRecoverable
-import net.corda.v5.base.exceptions.BackoffStrategy
 import net.corda.v5.base.util.debug
 import net.corda.v5.crypto.exceptions.CryptoRetryException
 import org.slf4j.Logger
