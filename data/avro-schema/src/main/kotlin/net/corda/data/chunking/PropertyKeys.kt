6416bca035e70f95630e106eb2900216269eb07c
Ben McMahon <benmcmahon100@gmail.com>
2022-09-01 12:14:39 +0100

    CORE-5375: vnode db reset (#526)
    * Add request for db reset
    * Add reset db as parameter
    * Add actor to request

--- data/avro-schema/src/main/kotlin/net/corda/data/chunking/PropertyKeys.kt
+++ data/avro-schema/src/main/kotlin/net/corda/data/chunking/PropertyKeys.kt
@@ -6,4 +6,12 @@ object PropertyKeys {
      * Boolean property specifying whether same data content can re-upload again
      */
     const val FORCE_UPLOAD = "forceUpload"
+    /**
+     * Boolean property specifying whether the vnode databases should be deleted and recreated
+     */
+    const val RESET_DB = "resetDb"
+    /**
+     * String property specifying the RPC actor that made the request
+     */
+    const val ACTOR = "actor"
 }
