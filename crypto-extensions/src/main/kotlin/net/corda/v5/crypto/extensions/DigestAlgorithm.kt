d3d7d05cb9bf7a8b651d5837b0f88d95aafa3d44
Alexey Kadyrov <67952405+alexey-kadyrov-r3@users.noreply.github.com>
2022-09-03 16:18:45 +0100

    CORE-6475 Updated KDocs (#549)

--- crypto-extensions/src/main/kotlin/net/corda/v5/crypto/extensions/DigestAlgorithm.kt
+++ crypto-extensions/src/main/kotlin/net/corda/v5/crypto/extensions/DigestAlgorithm.kt
@@ -3,11 +3,16 @@ package net.corda.v5.crypto.extensions
 import java.io.InputStream
 
 /**
- * Interface for injecting custom digest implementation bypassing JCA.
+ * Digest extensions: Interface defining a custom digest calculation implementation. The interface should be implemented
+ * if a CPK developer wishes to provide support for digest algorithms beyond supported by the Corda Platform.
+ * The implementation of the interface must be coupled with the implementation of the [DigestAlgorithmFactory] which
+ * will be used to create the instances. For each algorithm there must be matching a pair of
+ * [DigestAlgorithmFactory] and [DigestAlgorithm] implementations.
  */
 interface DigestAlgorithm {
     /**
-     * Algorithm identifier.
+     * Algorithm identifier, e.g. 'QUAD-SHA-256', the unique name (per Corda Platform and given CPK)
+     * of the digest algorithm. The name must match the names used by the corresponding [DigestAlgorithmFactory].
      */
     val algorithm: String
 
@@ -17,16 +22,18 @@ interface DigestAlgorithm {
     val digestLength: Int
 
     /**
-     * Computes the digest of the [ByteArray].
+     * Computes the digest of the [ByteArray]. The computation must be stateless and thread safe.
      *
      * @param bytes The [ByteArray] to hash.
+     * @return the hash value of the [digestLength]
      */
     fun digest(bytes: ByteArray): ByteArray
 
     /**
-     * Computes the digest of the [InputStream] bytes.
+     * Computes the digest of the [InputStream] bytes. The computation must be stateless and thread safe.
      *
      * @param inputStream The [InputStream] to hash.
+     * @return the hash value of the [digestLength]
      */
-    fun digest(inputStream : InputStream): ByteArray
+    fun digest(inputStream: InputStream): ByteArray
 }
--- crypto-extensions/src/main/kotlin/net/corda/v5/crypto/extensions/DigestAlgorithmFactory.kt
+++ crypto-extensions/src/main/kotlin/net/corda/v5/crypto/extensions/DigestAlgorithmFactory.kt
@@ -1,16 +1,20 @@
 package net.corda.v5.crypto.extensions
 
 /**
- * A factory for custom digest implementations bypassing JCA.
+ * Digest extensions: Interface defining a factory creating custom digest implementation. The interface
+ * should be implemented if a CPK developer wishes to provide support for digest algorithms beyond supported
+ * by the Corda Platform.
+ * For each algorithm there must be matching a pair of [DigestAlgorithmFactory] and [DigestAlgorithm] implementations.
  */
 interface DigestAlgorithmFactory {
     /**
-     * The algorithm name.
+     * The algorithm name, e.g. 'QUAD-SHA-256', the unique name (per Corda Platform and given CPK)
+     * of the digest algorithm. The name must match the names used by the created [DigestAlgorithm].
      */
     val algorithm: String
 
     /**
-     * The factory method.
+     * The factory method. Should return a new instance on each call. The method must be thread safe.
      */
     fun getInstance(): DigestAlgorithm
 }
