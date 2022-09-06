3ec790f5df6980136dee38e42278930a4ca92514
Ben McMahon <benmcmahon100@gmail.com>
2022-09-06 15:41:27 +0100

    Fixup error messages and response code. Also add E2E Test to help validate

--- components/chunking/chunk-db-write-impl/src/main/kotlin/net/corda/chunking/db/impl/validation/CpiValidatorImpl.kt
+++ components/chunking/chunk-db-write-impl/src/main/kotlin/net/corda/chunking/db/impl/validation/CpiValidatorImpl.kt
@@ -6,6 +6,7 @@ import net.corda.chunking.db.impl.persistence.ChunkPersistence
 import net.corda.chunking.db.impl.persistence.CpiPersistence
 import net.corda.chunking.db.impl.persistence.StatusPublisher
 import net.corda.cpiinfo.write.CpiInfoWriteService
+import net.corda.libs.cpiupload.ReUsedGroupIdException
 import net.corda.libs.cpiupload.ValidationException
 import net.corda.libs.packaging.Cpi
 import net.corda.libs.packaging.core.CpiMetadata
@@ -114,9 +115,9 @@ class CpiValidatorImpl constructor(
                 cpi.metadata.cpiId.version
             )
         ) {
-            throw ValidationException(
+            throw ReUsedGroupIdException(
                 "Group id ($groupId) in use with another CPI.  " +
-                        "Cannot upload ${cpi.metadata.cpiId.name} ${cpi.metadata.cpiId.version}"
+                    "Cannot upload ${cpi.metadata.cpiId.name} ${cpi.metadata.cpiId.version}"
             )
         }
     }
