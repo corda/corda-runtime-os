163a5603c37413b14598980fb3820e83857ffed3
Dan Newton <dan.newton@r3.com>
2022-09-08 15:34:49 +0100

    CORE-6586 American English APIs
    Includes some HTTP endpoint parameters and a topic name,

--- components/membership/membership-http-rpc/src/main/kotlin/net/corda/membership/httprpc/v1/MemberLookupRpcOps.kt
+++ components/membership/membership-http-rpc/src/main/kotlin/net/corda/membership/httprpc/v1/MemberLookupRpcOps.kt
@@ -18,8 +18,8 @@ interface MemberLookupRpcOps : RpcOps {
      *
      * @param holdingIdentityShortHash ID of the holding identity to be checked.
      * @param commonName Optional. Common Name (CN) attribute of the X.500 name to filter members by.
-     * @param organisation Optional. Organisation (O) attribute of the X.500 name to filter members by.
-     * @param organisationUnit Optional. Organisation Unit (OU) attribute of the X.500 name to filter members by.
+     * @param organization Optional. Organization (O) attribute of the X.500 name to filter members by.
+     * @param organizationUnit Optional. Organization Unit (OU) attribute of the X.500 name to filter members by.
      * @param locality Optional. Locality (L) attribute of the X.500 name to filter members by.
      * @param state Optional. State (ST) attribute of the X.500 name to filter members by.
      * @param country Optional. Country (C) attribute of the X.500 name to filter members by.
@@ -42,16 +42,16 @@ interface MemberLookupRpcOps : RpcOps {
         commonName: String? = null,
         @HttpRpcQueryParameter(
             name = "o",
-            description = "Organisation (O) attribute of the X.500 name to filter members by",
+            description = "Organization (O) attribute of the X.500 name to filter members by",
             required = false
         )
-        organisation: String? = null,
+        organization: String? = null,
         @HttpRpcQueryParameter(
             name = "ou",
-            description = "Organisation Unit (OU) attribute of the X.500 name to filter members by",
+            description = "Organization Unit (OU) attribute of the X.500 name to filter members by",
             required = false
         )
-        organisationUnit: String? = null,
+        organizationUnit: String? = null,
         @HttpRpcQueryParameter(
             name = "l",
             description = "Locality (L) attribute of the X.500 name to filter members by",
