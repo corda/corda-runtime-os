d46fa5d8ef29a30305162f90b64a2d4e33a79ba1
Lajos Veres <lajos.veres@r3.com>
2022-09-09 18:06:23 +0100

    CORE-5114 detekt

--- libs/serialization/serialization-kryo/src/main/kotlin/net/corda/kryoserialization/serializers/PrivacySaltImplSerializer.kt
+++ libs/serialization/serialization-kryo/src/main/kotlin/net/corda/kryoserialization/serializers/PrivacySaltImplSerializer.kt
@@ -8,7 +8,7 @@ import net.corda.kryoserialization.readBytesWithLength
 import net.corda.kryoserialization.writeBytesWithLength
 import net.corda.ledger.common.impl.transaction.PrivacySaltImpl
 
-class PrivacySaltImplSerializer() : Serializer<PrivacySaltImpl>() {
+class PrivacySaltImplSerializer : Serializer<PrivacySaltImpl>() {
     override fun write(kryo: Kryo, output: Output, obj: PrivacySaltImpl) {
         output.writeBytesWithLength(obj.bytes)
     }
