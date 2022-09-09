fd6145616c2629e79b67a6bf2335ac6129ef3fc7
Joseph Zuniga-Daly <joseph.zunigadaly@r3.com>
2022-09-09 17:41:47 +0100

    Use package plugin to build CPI

--- applications/workers/workers-smoketest/src/smokeTest/kotlin/net/corda/applications/workers/smoketest/virtualnode/helpers/CpiLoader.kt
+++ applications/workers/workers-smoketest/src/smokeTest/kotlin/net/corda/applications/workers/smoketest/virtualnode/helpers/CpiLoader.kt
@@ -6,6 +6,9 @@ import net.corda.applications.workers.smoketest.X500_CHARLIE
 import net.corda.applications.workers.smoketest.X500_DAVID
 import net.corda.applications.workers.smoketest.virtualnode.helpers.GroupPolicyUtils.getDefaultStaticNetworkGroupPolicy
 import net.corda.cli.plugins.packaging.CreateCpiV2
+import net.corda.cli.plugins.packaging.signing.SigningOptions
+import net.corda.utilities.deleteRecursively
+import net.corda.utilities.write
 import java.io.ByteArrayOutputStream
 import java.io.FileNotFoundException
 import java.io.InputStream
@@ -30,28 +33,60 @@ object CpiLoader {
      */
     private fun cpbToCpi(inputStream: InputStream, groupId: String): InputStream {
 
-        createTempDirectory()
+        val tempDirectory = createTempDirectory()
+        try {
+            // Save CPB to disk
+            val cpbPath = tempDirectory.resolve("cpb")
+            cpbPath.write { inputStream.copyTo(it) }
 
-        CreateCpiV2().apply {
-            cpbFileName
-        }
-        val bytes = ByteArrayOutputStream().use { byteStream ->
-            ZipOutputStream(byteStream).use { zout ->
-                val zin = ZipInputStream(inputStream)
-                var zipEntry: ZipEntry?
-                while (zin.nextEntry.apply { zipEntry = this } != null) {
-                    zout.apply {
-                        putNextEntry(zipEntry!!)
-                        zin.copyTo(zout)
-                        closeEntry()
-                    }
+            // Save group policy to disk
+            val groupPolicyPath = tempDirectory.resolve("groupPolicy")
+            groupPolicyPath.write { it.bufferedWriter().write(getStaticNetworkPolicy(groupId)) }
+
+            // Save keystore to disk
+            val keyStorePath = tempDirectory.resolve("alice.p12")
+            keyStorePath.write { it.write(getKeyStore()) }
+
+            // Create CPI
+            val cpiPath = tempDirectory.resolve("cpi")
+            CreateCpiV2().apply {
+                cpbFileName = cpbPath.toString()
+                cpiName = "cpi name"
+                cpiVersion = "1.0.0.0-SNAPSHOT"
+                cpiUpgrade = false
+                groupPolicyFileName = groupPolicyPath.toString()
+                outputFileName = cpiPath.toString()
+                signingOptions = SigningOptions().apply {
+                    keyStoreFileName = keyStorePath.toString()
+                    keyStorePass = "cordadevpass"
+                    keyAlias = "alice"
                 }
-                addGroupPolicy(zout, groupId)
+
+            }.run()
+
+            val bytes = ByteArrayOutputStream().use { byteStream ->
+                ZipOutputStream(byteStream).use { zout ->
+                    val zin = ZipInputStream(inputStream)
+                    var zipEntry: ZipEntry?
+                    while (zin.nextEntry.apply { zipEntry = this } != null) {
+                        zout.apply {
+                            putNextEntry(zipEntry!!)
+                            zin.copyTo(zout)
+                            closeEntry()
+                        }
+                    }
+                    addGroupPolicy(zout, groupId)
+                }
+                byteStream.toByteArray()
             }
-            byteStream.toByteArray()
+            return bytes.inputStream()
+        } finally {
+            tempDirectory.deleteRecursively()
         }
-        return bytes.inputStream()
     }
+
+    private fun getKeyStore() = javaClass.classLoader.getResourceAsStream("alice.p12")?.use { it.readAllBytes() }
+        ?: throw Exception("alice.p12 not found")
 
     private fun addGroupPolicy(
         zipOutputStream: ZipOutputStream,
@@ -71,4 +106,15 @@ object CpiLoader {
         groupPolicy.byteInputStream().use { it.copyTo(zipOutputStream) }
         zipOutputStream.closeEntry()
     }
+
+    private fun getStaticNetworkPolicy(groupId: String) = getDefaultStaticNetworkGroupPolicy(
+        groupId,
+        listOf(
+            X500_ALICE,
+            X500_BOB,
+            X500_CHARLIE,
+            X500_DAVID
+        ))
+        .reader().use { it.readText() }
+        .replace(groupIdPlaceholder, groupId)
 }
