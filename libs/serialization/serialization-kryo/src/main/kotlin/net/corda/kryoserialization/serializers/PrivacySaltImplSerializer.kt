8e7cd7ec9b60c0126cc1266f5494a445bc97da31
Lajos Veres <lajos.veres@r3.com>
2022-09-09 16:47:48 +0100

    CORE-5114 PrivacySaltImplSerializer

---
+++ libs/serialization/serialization-kryo/src/main/kotlin/net/corda/kryoserialization/serializers/PrivacySaltImplSerializer.kt
@@ -0,0 +1,20 @@
+package net.corda.kryoserialization.serializers
+
+import com.esotericsoftware.kryo.Kryo
+import com.esotericsoftware.kryo.Serializer
+import com.esotericsoftware.kryo.io.Input
+import com.esotericsoftware.kryo.io.Output
+import net.corda.kryoserialization.readBytesWithLength
+import net.corda.kryoserialization.writeBytesWithLength
+import net.corda.ledger.common.impl.transaction.PrivacySaltImpl
+
+class PrivacySaltImplSerializer() : Serializer<PrivacySaltImpl>() {
+    override fun write(kryo: Kryo, output: Output, obj: PrivacySaltImpl) {
+        output.writeBytesWithLength(obj.bytes)
+    }
+
+    override fun read(kryo: Kryo, input: Input, type: Class<PrivacySaltImpl>): PrivacySaltImpl {
+        return PrivacySaltImpl(input.readBytesWithLength())
+    }
+}
+
---
+++ libs/serialization/serialization-kryo/src/test/kotlin/net/corda/kryoserialization/serializers/PrivacySaltImplSerializerTest.kt
@@ -0,0 +1,24 @@
+package net.corda.kryoserialization.serializers
+
+import com.esotericsoftware.kryo.Kryo
+import com.esotericsoftware.kryo.io.Input
+import com.esotericsoftware.kryo.io.Output
+import net.corda.ledger.common.impl.transaction.PrivacySaltImpl
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class PrivacySaltImplSerializerTest {
+    @Test
+    fun `serialization of a PrivacySalt`() {
+        val privacySalt = PrivacySaltImpl("1".repeat(32).toByteArray())
+
+        val wireTransactionKryoSerializer = PrivacySaltImplSerializer()
+
+        val kryo = Kryo()
+        val output = Output(100)
+        wireTransactionKryoSerializer.write(kryo, output, privacySalt)
+        val deserialized = wireTransactionKryoSerializer.read(kryo, Input(output.buffer), PrivacySaltImpl::class.java)
+
+        assertThat(deserialized).isEqualTo(privacySalt)
+    }
+}
