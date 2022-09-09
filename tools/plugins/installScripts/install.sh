09e5cc6339b04984b38d57aeb7afdd2346e7a281
Chris Barratt <chris.barratt@r3.com>
2022-09-09 01:20:06 +0100

    Copy the files from the correct place, generate the correct corda-cli.sh script and make it executable

--- tools/plugins/installScripts/install.sh
+++ tools/plugins/installScripts/install.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+zipdir=$(dirname $0)
+
 cliHome=~/.corda/cli/
 
 echo "Creating corda-cli dir at cliHome"
 mkdir -p $cliHome
 
 echo "copying files and plugins"
-cp -R . $cliHome
+cp -R $zipdir/* $cliHome
 
 echo "Creating corda-cli Script"
-echo "java -Dpf4j.pluginsDir=$cliHome/plugins -jar corda-cli.jar $@" > $cliHome/corda-cli.sh
+echo "java -Dpf4j.pluginsDir=$cliHome/plugins -jar $cliHome/corda-cli.jar \"\$@\"" > $cliHome/corda-cli.sh
+
+chmod 755 $cliHome/corda-cli.sh
+
 
