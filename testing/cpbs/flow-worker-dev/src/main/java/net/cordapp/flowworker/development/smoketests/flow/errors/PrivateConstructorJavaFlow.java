76c3e7c1541fc79d185a83911f178c62fdd06a3c
Juan José Ramos Cassella <jujoramos@gmail.com>
2022-09-09 11:47:32 +0100

    CORE-5566: Add Explicit Assertions & Java Tests
    - Explicitly assert within tests that a flow execution failed due to
      invalid flow constructor.
    - Add extra unit tests to verify that 'FlowFactory' works as expected
      for flows written in Java.
    - Add new flows, written in Java and mirroring the ones written in
      Kotlin, and exercise them within the  smoke tests.

---
+++ testing/cpbs/flow-worker-dev/src/main/java/net/cordapp/flowworker/development/smoketests/flow/errors/PrivateConstructorJavaFlow.java
@@ -0,0 +1,17 @@
+package net.cordapp.flowworker.development.smoketests.flow.errors;
+
+import net.corda.v5.application.flows.RPCRequestData;
+import net.corda.v5.application.flows.RPCStartableFlow;
+import org.jetbrains.annotations.NotNull;
+
+@SuppressWarnings("unused")
+public class PrivateConstructorJavaFlow implements RPCStartableFlow {
+    private PrivateConstructorJavaFlow() {
+    }
+
+    @NotNull
+    @Override
+    public String call(@NotNull RPCRequestData requestBody) {
+        throw new IllegalStateException("Should not reach this point");
+    }
+}
