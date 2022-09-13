b34498bc0082bd87db0a75604ca6ffaa0dae84ed
Dan Newton <dan.newton@r3.com>
2022-09-13 16:58:04 +0100

    CORE-6586 stuff

--- components/membership/membership-p2p-impl/src/integrationTest/kotlin/net/corda/membership/impl/p2p/MembershipP2PIntegrationTest.kt
+++ components/membership/membership-p2p-impl/src/integrationTest/kotlin/net/corda/membership/impl/p2p/MembershipP2PIntegrationTest.kt
@@ -50,7 +50,7 @@ import net.corda.p2p.app.UnauthenticatedMessage
 import net.corda.p2p.app.UnauthenticatedMessageHeader
 import net.corda.schema.Schemas
 import net.corda.schema.Schemas.Membership.Companion.REGISTRATION_COMMAND_TOPIC
-import net.corda.schema.Schemas.Membership.Companion.SYNCHRONISATION_TOPIC
+import net.corda.schema.Schemas.Membership.Companion.SYNCHRONIZATION_TOPIC
 import net.corda.schema.configuration.BootConfig.INSTANCE_ID
 import net.corda.schema.configuration.ConfigKeys
 import net.corda.schema.configuration.MessagingConfig.Bus.BUS_TYPE
@@ -432,7 +432,7 @@ class MembershipP2PIntegrationTest {
         val completableResult = CompletableFuture<SynchronisationCommand>()
 
         val syncRequestSubscription = subscriptionFactory.createPubSubSubscription(
-            SubscriptionConfig("membership_p2p_sync_test_receiver", SYNCHRONISATION_TOPIC),
+            SubscriptionConfig("membership_p2p_sync_test_receiver", SYNCHRONIZATION_TOPIC),
             getPubSubTestProcessor { v ->
                 completableResult.complete(v as SynchronisationCommand)
             },
