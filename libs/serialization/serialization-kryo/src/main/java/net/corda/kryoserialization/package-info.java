56c1c5f6068a59bde1d7bc885c8c0030bc94a26d
Lajos Veres <lajos.veres@r3.com>
2022-09-14 14:37:02 +0100

    CORE-5114 Since upper level function serialization-kryo is bit less needed here

--- libs/serialization/serialization-kryo/src/main/java/net/corda/kryoserialization/package-info.java
+++ libs/serialization/serialization-kryo/src/main/java/net/corda/kryoserialization/package-info.java
@@ -1,6 +1,4 @@
 @QuasarIgnoreAllPackages
-@Export
 package net.corda.kryoserialization;
 
 import co.paralleluniverse.quasar.annotations.QuasarIgnoreAllPackages;
-import org.osgi.annotation.bundle.Export;
--- libs/serialization/serialization-kryo/src/main/java/net/corda/kryoserialization/resolver/package-info.java
+++
@@ -1,4 +0,0 @@
-@Export
-package net.corda.kryoserialization.resolver;
-
-import org.osgi.annotation.bundle.Export;
