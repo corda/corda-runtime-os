f389cf0bdb8b54b38608f0c9408b04241dcf8baa
Dan Newton <dan.newton@r3.com>
2022-09-14 17:41:38 +0100

    CORE-6418 Do not throw in `ExternalEventFactory`
    Document that we should not throw in any `ExternalEventFactory`s.
    
    Change some signing code to ensure this is true.

--- components/flow/flow-service/src/main/kotlin/net/corda/flow/application/crypto/external/events/SignParameters.kt
+++
@@ -1,6 +0,0 @@
-package net.corda.flow.application.crypto.external.events
-
-import java.security.PublicKey
-import net.corda.v5.crypto.SignatureSpec
-
-data class SignParameters(val bytes: ByteArray, val publicKey: PublicKey, val signatureSpec: SignatureSpec)
