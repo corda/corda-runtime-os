13bc1cca1f1cf0c835328fa1271a4a08e1bc404f
LizR3 <91971483+LizR3@users.noreply.github.com>
2022-09-14 11:41:22 +0100

    CORE-6150: Simulator runs named queries, fixed tests

--- simulator/example-app/src/integrationTest/kotlin/net/cordacon/example/RollCallTest.kt
+++ simulator/example-app/src/integrationTest/kotlin/net/cordacon/example/RollCallTest.kt
@@ -14,12 +14,12 @@ class RollCallTest {
     @Test
     fun `should get roll call from multiple recipients`() {
         // Given a RollCallFlow that's been uploaded to Corda for a teacher
-        val corda = Simulator()
+        val simulator = Simulator()
         val teacher = MemberX500Name.parse(
             "CN=Ben Stein, OU=Economics, O=Glenbrook North High School, L=Chicago, C=US"
         )
         val teacherId = HoldingIdentity.create(teacher)
-        val teacherVNode = corda.createVirtualNode(teacherId, RollCallFlow::class.java)
+        val teacherVNode = simulator.createVirtualNode(teacherId, RollCallFlow::class.java)
 
         // And a key to sign the absence record with
         teacherVNode.generateKey("teacher-key", HsmCategory.LEDGER, "Any scheme will do")
@@ -34,7 +34,7 @@ class RollCallTest {
         val students = listOf("Albers", "Anderson", "Anheiser", "Busch", "Bueller"). map {
             "CN=$it, OU=Economics, O=Glenbrook North High School, L=Chicago, C=US"
         }
-        students.forEach { corda.createVirtualNode(
+        students.forEach { simulator.createVirtualNode(
             HoldingIdentity.create(MemberX500Name.parse(it)),
             RollCallResponderFlow::class.java,
             AbsenceCallResponderFlow::class.java) }
@@ -43,7 +43,7 @@ class RollCallTest {
         val truantingAuth = MemberX500Name.parse(
             "O=TruantAuth, L=Chicago, C=US"
         )
-        val truantAuthVNode = corda.createVirtualNode(
+        val truantAuthVNode = simulator.createVirtualNode(
             HoldingIdentity.create(truantingAuth),
             TruancyResponderFlow::class.java
         )
@@ -79,6 +79,6 @@ class RollCallTest {
         assertThat(absenceResponses.execute().map { it.name },
             `is`(listOf("CN=Bueller, OU=Economics, O=Glenbrook North High School, L=Chicago, C=US")))
 
-        corda.close()
+        simulator.close()
     }
 }
