616d110b71dc7388fd443dc39b838f617e953750
Dimos Raptis <dimos.raptis@r3.com>
2022-09-14 14:48:26 +0100

    Add comment

--- components/link-manager/src/main/kotlin/net/corda/p2p/linkmanager/sessions/SessionManagerImpl.kt
+++ components/link-manager/src/main/kotlin/net/corda/p2p/linkmanager/sessions/SessionManagerImpl.kt
@@ -117,6 +117,8 @@ internal class SessionManagerImpl(
 
     private val sessionNegotiationLock = ReentrantReadWriteLock()
 
+    // This default needs to be removed and the lifecycle dependency graph adjusted to ensure the inbound subscription starts only after
+    // the configuration has been received and the session manager has started (see CORE-6730).
     private val config = AtomicReference(
         SessionManagerConfig(1000000, 4)
     )
