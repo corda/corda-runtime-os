2528463aefc8b30b2f34d9092c3a6f30f8603d7d
Lajos Veres <lajos.veres@r3.com>
2022-09-16 11:10:16 +0100

    CORE-5114 testing/ledger/ledger-consensual-testkit

---
+++ testing/ledger/ledger-consensual-testkit/src/main/kotlin/net/corda/ledger/consensual/testkit/ConsensualSignedTransactionExample.kt
@@ -0,0 +1,34 @@
+package net.corda.ledger.consensual.testkit
+
+import net.corda.ledger.common.testkit.WireTransactionExample.Companion.getWireTransaction
+import net.corda.ledger.consensual.impl.transaction.ConsensualSignedTransactionImpl
+import net.corda.v5.application.crypto.DigitalSignatureAndMetadata
+import net.corda.v5.application.crypto.DigitalSignatureMetadata
+import net.corda.v5.application.serialization.SerializationService
+import net.corda.v5.cipher.suite.DigestService
+import net.corda.v5.crypto.DigitalSignature
+import net.corda.v5.crypto.merkle.MerkleTreeFactory
+import java.security.KeyPairGenerator
+import java.time.Instant
+
+class ConsensualSignedTransactionImplExample {
+    companion object{
+        fun getConsensualSignedTransactionImpl(
+            digestService: DigestService,
+            merkleTreeFactory: MerkleTreeFactory,
+            serializationService: SerializationService,
+        ): ConsensualSignedTransactionImpl{
+            val wireTransaction = getWireTransaction(digestService, merkleTreeFactory)
+
+            val kpg = KeyPairGenerator.getInstance("RSA")
+            kpg.initialize(512)
+            val testPublicKey = kpg.genKeyPair().public
+
+            val signature = DigitalSignature.WithKey(testPublicKey, "0".toByteArray(), mapOf())
+            val digitalSignatureMetadata = DigitalSignatureMetadata(Instant.now(), mapOf()) //CORE-5091 populate this properly...
+            val signatureWithMetaData = DigitalSignatureAndMetadata(signature, digitalSignatureMetadata)
+            return ConsensualSignedTransactionImpl(serializationService, wireTransaction, listOf(signatureWithMetaData))
+        }
+    }
+}
+
