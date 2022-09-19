55648c82265be5df9b683857ae6763f4ee8454ca
LWogan <lorcan.wogan@r3.com>
2022-09-19 15:47:46 +0100

    CORE-6748 config doc update

--- libs/configuration/configuration-endpoints/src/main/kotlin/net/corda/libs/configuration/endpoints/v1/ConfigRPCOps.kt
+++ libs/configuration/configuration-endpoints/src/main/kotlin/net/corda/libs/configuration/endpoints/v1/ConfigRPCOps.kt
@@ -13,20 +13,24 @@ import net.corda.libs.configuration.endpoints.v1.types.UpdateConfigResponse
 /** RPC operations for cluster configuration management. */
 @HttpRpcResource(
     name = "Configuration API",
-    description = "Cluster configuration management endpoints.",
+    description = "The Configuration API consists of a number of endpoints used to manage the configuration of Corda clusters.",
     path = "config"
 )
 interface ConfigRPCOps : RpcOps {
 
     /**
-     * Updates cluster configuration.
+     * Sends a request to update the clusters configuration.
      *
+     * @property section Section of the configuration to be updated.
+     * @property version Version number used for optimistic locking.
+     * @property config Updated configuration in JSON or HOCON format.
+     * @property schemaVersion Schema version of the configuration.
      * @throws `ConfigRPCOpsServiceException` If the updated configuration could not be published.
      * @throws `HttpApiException` If the request returns an exceptional response.
      */
     @HttpRpcPUT(
         title = "Update cluster configuration",
-        description = "Updates a section of the cluster configuration.",
+        description = "This method updates a section of the cluster configuration.",
         responseDescription = "The updated cluster configuration for the specified section."
     )
     fun updateConfig(
@@ -34,10 +38,14 @@ interface ConfigRPCOps : RpcOps {
         request: UpdateConfigParameters
     ): UpdateConfigResponse
 
+    /**
+     * Get the configuration data the cluster is set with for a specific [section].
+     * @param section the top level section of cluster configuration to return.
+     */
     @HttpRpcGET(
         path = "{section}",
         title = "Get Configuration.",
-        description = "Get Configuration. Returns the 'active' configuration for the given section, " +
+        description = "This method returns the 'active' configuration for the given section, " +
                 "in both the 'raw' format and with defaults applied.",
         responseDescription = "The Configuration for the given section."
     )
