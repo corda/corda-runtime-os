9f747eaad827d9a54314a0d618954554137bb8f9
Matthew Layton <matthew.layton@live.co.uk>
2022-09-19 22:11:33 +0100

    Added CPK Constraint Implementations and Tests

---
+++ libs/ledger/ledger-utxo-impl/src/main/kotlin/net/corda/ledger/utxo/impl/AutomaticPlaceholderCpkConstraint.kt
@@ -0,0 +1,22 @@
+package net.corda.ledger.utxo.impl
+
+import net.corda.v5.cipher.suite.DigestService
+import net.corda.v5.ledger.utxo.CpkConstraint
+import net.corda.v5.ledger.utxo.CpkConstraintContext
+
+/**
+ * Represents a CPK constraint that is always accepted.
+ */
+object AutomaticPlaceholderCpkConstraint : CpkConstraint {
+
+    /**
+     * Determines whether the constraint is satisfied by the specified context.
+     *
+     * @param digestService The digest service to use for constraint checking.
+     * @param context The context in which to check the current constraint.
+     * @return Returns true if the constraint is satisfied by the specified context; otherwise, false.
+     */
+    override fun isSatisfiedBy(digestService: DigestService, context: CpkConstraintContext): Boolean {
+        throw UnsupportedOperationException("Contracts cannot be satisfied by AutomaticPlaceholderCpkConstraint.")
+    }
+}
---
+++ libs/ledger/ledger-utxo-impl/src/test/kotlin/net/corda/ledger/utxo/impl/AutomaticPlaceholderCpkConstraintTests.kt
@@ -0,0 +1,22 @@
+package net.corda.ledger.utxo.impl
+
+import net.corda.v5.cipher.suite.DigestService
+import net.corda.v5.ledger.utxo.CpkConstraintContext
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertThrows
+import org.junit.jupiter.api.Test
+import org.mockito.Mockito
+
+class AutomaticPlaceholderCpkConstraintTests {
+    private val digestService = Mockito.mock(DigestService::class.java)
+    private val constraintContext = Mockito.mock(CpkConstraintContext::class.java)
+
+    @Test
+    fun `AutomaticPlaceholderCpkConstraint isSatisfiedBy should throw the expected exception`() {
+        val exception = assertThrows(java.lang.UnsupportedOperationException::class.java) {
+            AutomaticPlaceholderCpkConstraint.isSatisfiedBy(digestService, constraintContext)
+        }
+
+        assertEquals("Contracts cannot be satisfied by AutomaticPlaceholderCpkConstraint.", exception.message)
+    }
+}
