0b955483281d2a80d53358a1f697e96c796b198b
LWogan <lorcan.wogan@r3.com>
2022-09-19 16:09:33 +0100

    CORE-6748 config doc update

--- libs/http-rpc/http-rpc/src/main/kotlin/net/corda/httprpc/RpcOps.kt
+++ libs/http-rpc/http-rpc/src/main/kotlin/net/corda/httprpc/RpcOps.kt
@@ -1,11 +1,17 @@
 package net.corda.httprpc
 
+import net.corda.httprpc.annotations.HttpRpcGET
+
 /**
  * [RpcOps] is a marker interface to indicate a class that contains HTTP endpoints. Any class that provides HTTP endpoints must implement
  * this class; using the annotations alone does not suffice.
  */
+
 interface RpcOps {
 
     /** Returns the RPC protocol version. Exists since version 0 so guaranteed to be present. */
+    @HttpRpcGET(
+        description = "This method returns the version of the endpoint.",
+    )
     val protocolVersion: Int
 }
