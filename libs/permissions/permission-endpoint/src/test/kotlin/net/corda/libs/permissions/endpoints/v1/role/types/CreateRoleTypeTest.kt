86bec1a7af978d62f959ce73b09e3b8ada60248e
Peter McKinney <pmck91@users.noreply.github.com>
2022-09-20 10:11:03 +0100

    Able to create roles without specifying any roleName in the request

---
+++ libs/permissions/permission-endpoint/src/test/kotlin/net/corda/libs/permissions/endpoints/v1/role/types/CreateRoleTypeTest.kt
@@ -0,0 +1,28 @@
+package net.corda.libs.permissions.endpoints.v1.role.types
+
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
+
+class CreateRoleTypeTest {
+
+    @Test
+    fun testHappyPath() {
+        assertDoesNotThrow {
+            CreateRoleType(
+                "cool-role",
+                "awesome-group"
+            )
+        }
+    }
+
+    @Test
+    fun testBlankRoleName() {
+        Assertions.assertThatThrownBy {
+            CreateRoleType(
+                "",
+                "awful-group"
+            )
+        }.hasMessage("Role name must not be null or blank.")
+    }
+}
