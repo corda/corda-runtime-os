f24bfb904f7ac9dca16983745414d17460c7c6bd
LWogan <lorcan.wogan@r3.com>
2022-09-20 10:46:05 +0100

    CORE-6561 test update for signing

--- applications/workers/workers-smoketest/src/smokeTest/kotlin/net/corda/applications/workers/smoketest/ClusterBootstrapTest.kt
+++ applications/workers/workers-smoketest/src/smokeTest/kotlin/net/corda/applications/workers/smoketest/ClusterBootstrapTest.kt
@@ -31,7 +31,6 @@ class ClusterBootstrapTest {
     private val client = HttpClient.newBuilder().build()
 
     @Test
-    @Disabled
     fun checkCluster() {
         runBlocking(Dispatchers.Default) {
             val softly = SoftAssertions()
