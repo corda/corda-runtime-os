e914a8e4cf418efc1d39996f72fd6fc9f14f6c24
Simon Johnson <simon.johnson@r3.com>
2022-09-23 12:44:25 +0100

    unnecessary file

--- libs/flows/session-manager-impl/src/main/kotlin/net/corda/session/manager/impl/processor/helper/ErrorMessages.kt
+++
@@ -1,6 +0,0 @@
-package net.corda.session.manager.impl.processor.helper
-
-const val PROTOCOL_MISMATCH_HINT =
-    "Please check CorDapp protocol expectations match on both sides regarding 'send' and 'receive' statements and that " +
-            "sessions are not closed unexpectedly on either side. Stack traces around this error originating in CorDapp " +
-            "code may provide a hint."
