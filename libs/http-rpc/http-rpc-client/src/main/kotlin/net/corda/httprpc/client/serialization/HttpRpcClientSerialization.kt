a713c4ff8eba687999899fe7ba505c1906f0718f
Peter McKinney <pmck91@users.noreply.github.com>
2022-09-27 14:56:42 +0100

    wild card import removal

--- libs/http-rpc/http-rpc-client/src/main/kotlin/net/corda/httprpc/client/serialization/HttpRpcClientSerialization.kt
+++ libs/http-rpc/http-rpc-client/src/main/kotlin/net/corda/httprpc/client/serialization/HttpRpcClientSerialization.kt
@@ -3,11 +3,14 @@ package net.corda.httprpc.client.serialization
 import com.fasterxml.jackson.core.JsonGenerator
 import com.fasterxml.jackson.core.JsonParseException
 import com.fasterxml.jackson.core.JsonParser
-import com.fasterxml.jackson.core.TreeNode
-import com.fasterxml.jackson.databind.*
+import com.fasterxml.jackson.databind.DeserializationContext
+import com.fasterxml.jackson.databind.JavaType
+import com.fasterxml.jackson.databind.JsonDeserializer
+import com.fasterxml.jackson.databind.SerializerProvider
+import com.fasterxml.jackson.databind.BeanProperty
+import com.fasterxml.jackson.databind.JsonSerializer
 import com.fasterxml.jackson.databind.deser.ContextualDeserializer
 import com.fasterxml.jackson.databind.module.SimpleModule
-import com.fasterxml.jackson.databind.node.ValueNode
 import net.corda.common.json.serialization.jacksonObjectMapper
 import net.corda.httprpc.JsonObject
 import net.corda.httprpc.durablestream.DurableCursorTransferObject
