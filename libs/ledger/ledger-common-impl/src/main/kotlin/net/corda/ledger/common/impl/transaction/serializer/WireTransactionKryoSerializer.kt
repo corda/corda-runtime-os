2aa104bab9c9ac135a66c97166daca6c85c6fa31
Lajos Veres <lajos.veres@r3.com>
2022-09-27 07:55:03 +0100

    CORE-5114 SignedTransaction Serializers and some general Ledger serialization cleanups (#2201)
    * `ledger-consensual-serialization` module with ConsensualSignedTransaction AMQP serializer.
    * ConsensualSignedTransactionImpl Kryo Serializer.
    * `ledger-common-testkit` and `ledger-consensual-testkit` for some shared code.
    * Transition from Jackson to JsonMarshallingService.
    * WireTransaction AMQP serialization integration test.
    * Remove some OSGi exports.

--- libs/ledger/ledger-common-impl/src/main/kotlin/net/corda/ledger/common/impl/transaction/serializer/WireTransactionKryoSerializer.kt
+++ libs/ledger/ledger-common-impl/src/main/kotlin/net/corda/ledger/common/impl/transaction/serializer/WireTransactionKryoSerializer.kt
@@ -4,6 +4,7 @@ import net.corda.ledger.common.impl.transaction.WireTransaction
 import net.corda.serialization.checkpoint.CheckpointInput
 import net.corda.serialization.checkpoint.CheckpointInternalCustomSerializer
 import net.corda.serialization.checkpoint.CheckpointOutput
+import net.corda.v5.application.marshalling.JsonMarshallingService
 import net.corda.v5.base.util.uncheckedCast
 import net.corda.v5.cipher.suite.DigestService
 import net.corda.v5.crypto.merkle.MerkleTreeFactory
@@ -15,7 +16,8 @@ import org.osgi.service.component.annotations.Reference
 @Component(service = [CheckpointInternalCustomSerializer::class])
 class WireTransactionKryoSerializer @Activate constructor(
     @Reference(service = MerkleTreeFactory::class) private val merkleTreeFactory: MerkleTreeFactory,
-    @Reference(service = DigestService::class) private val digestService: DigestService
+    @Reference(service = DigestService::class) private val digestService: DigestService,
+    @Reference(service = JsonMarshallingService::class) private val jsonMarshallingService: JsonMarshallingService
 ) : CheckpointInternalCustomSerializer<WireTransaction> {
     override val type: Class<WireTransaction> get() = WireTransaction::class.java
 
@@ -30,6 +32,7 @@ class WireTransactionKryoSerializer @Activate constructor(
         return WireTransaction(
             merkleTreeFactory,
             digestService,
+            jsonMarshallingService,
             privacySalt,
             componentGroupLists,
         )
