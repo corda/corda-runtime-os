0dff2a5aa85ae66e8bad360fb825f0cb1acae3c9
Peter McKinney <pmck91@users.noreply.github.com>
2022-09-28 10:52:14 +0100

    detekt

--- libs/http-rpc/http-rpc-client/src/main/kotlin/net/corda/httprpc/client/serialization/HttpRpcClientSerialization.kt
+++ libs/http-rpc/http-rpc-client/src/main/kotlin/net/corda/httprpc/client/serialization/HttpRpcClientSerialization.kt
@@ -3,7 +3,6 @@ package net.corda.httprpc.client.serialization
 import com.fasterxml.jackson.core.JsonGenerator
 import com.fasterxml.jackson.core.JsonParseException
 import com.fasterxml.jackson.core.JsonParser
-import com.fasterxml.jackson.core.TreeNode
 import com.fasterxml.jackson.databind.DeserializationContext
 import com.fasterxml.jackson.databind.JavaType
 import com.fasterxml.jackson.databind.JsonDeserializer
@@ -12,7 +11,6 @@ import com.fasterxml.jackson.databind.BeanProperty
 import com.fasterxml.jackson.databind.JsonSerializer
 import com.fasterxml.jackson.databind.deser.ContextualDeserializer
 import com.fasterxml.jackson.databind.module.SimpleModule
-import com.fasterxml.jackson.databind.node.ValueNode
 import net.corda.common.json.serialization.jacksonObjectMapper
 import net.corda.httprpc.JsonObject
 import net.corda.httprpc.durablestream.DurableCursorTransferObject
