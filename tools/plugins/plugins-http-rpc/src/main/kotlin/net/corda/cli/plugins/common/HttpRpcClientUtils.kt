5dbecf6c9dd42e5efa3583254240cf099fb84419
Viktor Kolomeyko <Viktor.Kolomeyko@r3.com>
2022-09-29 10:45:52 +0100

    CORE-6660: Increase wait duration and add additional logging

--- tools/plugins/plugins-http-rpc/src/main/kotlin/net/corda/cli/plugins/common/HttpRpcClientUtils.kt
+++ tools/plugins/plugins-http-rpc/src/main/kotlin/net/corda/cli/plugins/common/HttpRpcClientUtils.kt
@@ -4,9 +4,14 @@ import net.corda.httprpc.RpcOps
 import net.corda.httprpc.client.HttpRpcClient
 import net.corda.httprpc.client.HttpRpcConnection
 import net.corda.httprpc.client.config.HttpRpcClientConfig
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import kotlin.reflect.KClass
 
 object HttpRpcClientUtils {
+
+    private val logger = LoggerFactory.getLogger(this::class.java)
+    private val errOut: Logger = LoggerFactory.getLogger("SystemErr")
 
     fun <I : RpcOps> HttpRpcCommand.createHttpRpcClient(rpcOps: KClass<I>): HttpRpcClient<I> {
         val localTargetUrl = if(targetUrl.endsWith("/")) {
@@ -36,10 +41,12 @@ object HttpRpcClientUtils {
                 return this.start()
             } catch (ex: Exception) {
                 lastException = ex
+                logger.info("Cannot start HttpRpcClient yet", ex)
                 Thread.sleep(1000)
             }
         } while (System.currentTimeMillis() <= endTime)
 
+        errOut.error("Unable to start HttpRpcClient", lastException)
         throw lastException!!
     }
 }
