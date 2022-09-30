ca71d4811f363b2dbe2fb9a65822a59c8e7ef44e
Will Vigor <william.vigor@r3.com>
2022-09-30 16:40:52 +0100

    Respond to review comments

--- applications/tools/p2p-test/app-simulator/scripts/tearDown.sh
+++ applications/tools/p2p-test/app-simulator/scripts/tearDown.sh
@@ -5,8 +5,6 @@ set -e
 
 declare -a namespaces=($A_CLUSTER_NAMESPACE $B_CLUSTER_NAMESPACE $MGM_CLUSTER_NAMESPACE $APP_SIMULATOR_DB_NAMESPACE)
 
-for namespace in ${namespaces[@]}; do
-   kubectl delete ns $namespace
-done
+kubectl delete ns $namespaces
 
 killall kubectl
