8aea6826481d61fa4af56a93fbf6abbbce1c3646
Yiftach Kaplan <yiftach.kaplan@r3.com>
2022-10-03 13:11:26 +0100

    more

--- libs/layered-property-map/src/main/kotlin/net/corda/layeredpropertymap/LayeredPropertyMapUtils.kt
+++ libs/layered-property-map/src/main/kotlin/net/corda/layeredpropertymap/LayeredPropertyMapUtils.kt
@@ -8,8 +8,7 @@ import net.corda.v5.base.types.LayeredPropertyMap
  * Extension function for converting the content of [LayeredPropertyMap] to a [KeyValuePairList].
  */
 fun LayeredPropertyMap.toAvro(): KeyValuePairList = KeyValuePairList(
-    entries
-        .map {
-            KeyValuePair(it.key, it.value)
-        }
+    entries.map {
+        KeyValuePair(it.key, it.value)
+    }
 )
