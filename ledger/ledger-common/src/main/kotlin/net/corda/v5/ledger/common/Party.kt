66b372b27759ee33817c50948297007d2dfe9b73
Matthew Layton <matthew.layton@live.co.uk>
2022-10-10 17:08:20 +0100

    Added Party.toString implementation.

--- ledger/ledger-common/src/main/kotlin/net/corda/v5/ledger/common/Party.kt
+++ ledger/ledger-common/src/main/kotlin/net/corda/v5/ledger/common/Party.kt
@@ -2,6 +2,7 @@ package net.corda.v5.ledger.common
 
 import net.corda.v5.base.annotations.CordaSerializable
 import net.corda.v5.base.types.MemberX500Name
+import net.corda.v5.crypto.toStringShort
 import java.security.PublicKey
 
 /**
@@ -11,4 +12,14 @@ import java.security.PublicKey
  * @property owningKey The [PublicKey] that represents the current identity.
  */
 @CordaSerializable
-data class Party(val name: MemberX500Name, val owningKey: PublicKey)
+data class Party(val name: MemberX500Name, val owningKey: PublicKey) {
+
+    /**
+     * Gets the [String] representation of the current object.
+     *
+     * @return Returns the [String] representation of the current object.
+     */
+    override fun toString(): String {
+        return "$name (owningKey = ${owningKey.toStringShort()})"
+    }
+}
