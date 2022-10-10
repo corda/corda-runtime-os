8b3fa8e200b867c7ae2bb7217bc26168ff375f84
Yiftach Kaplan <yiftach.kaplan@r3.com>
2022-10-10 12:24:30 +0100

    CORE-6987: WIP

--- components/membership/registration-impl/src/main/kotlin/net/corda/membership/impl/registration/MemberRole.kt
+++ components/membership/registration-impl/src/main/kotlin/net/corda/membership/impl/registration/MemberRole.kt
@@ -1,18 +1,26 @@
 package net.corda.membership.impl.registration
 
+import net.corda.crypto.core.CryptoConsts.Categories.NOTARY
+import net.corda.membership.lib.MemberInfoExtension.Companion.NOTARY_KEY_HASH
+import net.corda.membership.lib.MemberInfoExtension.Companion.NOTARY_KEY_PEM
+import net.corda.membership.lib.MemberInfoExtension.Companion.NOTARY_KEY_SPEC
+import net.corda.membership.lib.MemberInfoExtension.Companion.NOTARY_NAME
+import net.corda.membership.lib.MemberInfoExtension.Companion.NOTARY_SERVICE_NAME
+import net.corda.membership.lib.MemberInfoExtension.Companion.NOTARY_SERVICE_PLUGIN
+import net.corda.membership.lib.MemberInfoExtension.Companion.ROLES_PREFIX
 import net.corda.v5.base.types.MemberX500Name
 
 internal sealed class MemberRole {
     companion object {
         fun extractRolesFromContext(context: Map<String, String>): Collection<MemberRole> {
             val roleNames = context.entries.filter {
-                it.key.startsWith("corda.roles.")
+                it.key.startsWith(ROLES_PREFIX)
             }.map {
                 it.value
             }.toSet()
             val roles: Collection<MemberRole> = roleNames.map { roleName ->
                 when (roleName) {
-                    "notary" -> {
+                    NOTARY_NAME -> {
                         readNotary(context)
                     }
 
@@ -32,17 +40,46 @@ internal sealed class MemberRole {
             return roles
         }
 
+        fun Collection<MemberRole>.toMemberInfo(
+            keysFactory: KeysFactory,
+        ): Collection<Pair<String, String>> {
+            return this.flatMapIndexed { index, role ->
+                role.toMemberInfo(keysFactory, index)
+            }
+        }
+
         private fun readNotary(context: Map<String, String>): Notary {
-            val serviceName = context["corda.notary.service.name"] ?: throw IllegalArgumentException("Notary must have a service name")
-            val plugin = context["corda.notary.service.plugin"] ?: throw IllegalArgumentException("Notary must have a service plugin")
+            val serviceName = context[NOTARY_SERVICE_NAME] ?: throw IllegalArgumentException("Notary must have a service name")
+            val plugin = context[NOTARY_SERVICE_PLUGIN] ?: throw IllegalArgumentException("Notary must have a service plugin")
             return Notary(
                 serviceName = MemberX500Name.parse(serviceName),
                 plugin = plugin,
             )
         }
     }
+
+    protected abstract fun toMemberInfo(
+        keysFactory: KeysFactory,
+        index: Int,
+    ): Collection<Pair<String, String>>
+
     data class Notary(
         val serviceName: MemberX500Name,
         val plugin: String,
-    ) : MemberRole()
+    ) : MemberRole() {
+        override fun toMemberInfo(
+            keysFactory: KeysFactory,
+            index: Int,
+        ): Collection<Pair<String, String>> {
+            val key = keysFactory.getOrGenerateKeyPair(NOTARY)
+            return listOf(
+                "$ROLES_PREFIX.$index" to NOTARY_NAME,
+                NOTARY_SERVICE_NAME to serviceName.toString(),
+                NOTARY_SERVICE_PLUGIN to plugin,
+                String.format(NOTARY_KEY_PEM, index) to key.pem,
+                String.format(NOTARY_KEY_HASH, index) to key.hash.toString(),
+                String.format(NOTARY_KEY_SPEC, index) to key.spec.signatureName,
+            )
+        }
+    }
 }
--- libs/membership/membership-common/src/main/kotlin/net/corda/membership/lib/MemberInfoExtension.kt
+++ libs/membership/membership-common/src/main/kotlin/net/corda/membership/lib/MemberInfoExtension.kt
@@ -97,6 +97,25 @@ class MemberInfoExtension {
          */
         const val MEMBER_STATUS_SUSPENDED = "SUSPENDED"
 
+        /**
+         * PREFIX for notary roles name
+         */
+        const val ROLES_PREFIX = "corda.roles."
+
+        /**
+         * Notary role name
+         */
+        const val NOTARY_NAME = "notary"
+
+        /**
+         * Notary role properties
+         */
+        const val NOTARY_SERVICE_NAME = "corda.notary.service.name"
+        const val NOTARY_SERVICE_PLUGIN = "corda.notary.service.plugin"
+        const val NOTARY_KEY_PEM = "corda.notary.keys.%s.pem"
+        const val NOTARY_KEY_HASH = "corda.notary.keys.%s.hash"
+        const val NOTARY_KEY_SPEC = "corda.notary.keys.%s.signature.spec"
+
         /** Identity certificate or null for non-PKI option. Certificate subject and key should match party */
         // TODO we will need a CertPath converter somewhere
         /*@JvmStatic
