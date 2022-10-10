b0b4bf0e3f2ae082bbe19f23b2423c96221cc404
Matthew Layton <matthew.layton@live.co.uk>
2022-10-10 13:07:36 +0100

    fixed failing tests.

--- ledger/ledger-common/src/test/java/net/corda/v5/ledger/common/transaction/PartyJavaApiTest.java
+++ ledger/ledger-common/src/test/java/net/corda/v5/ledger/common/transaction/PartyJavaApiTest.java
@@ -4,36 +4,30 @@ import net.corda.v5.base.types.MemberX500Name;
 import net.corda.v5.ledger.common.Party;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 
 import java.security.PublicKey;
 
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
 
 public class PartyJavaApiTest {
-    private final Party party = mock(Party.class);
+
+    private final MemberX500Name name = new MemberX500Name("Bob Plc", "Rome", "IT");
+    private final PublicKey key = mock(PublicKey.class);
+    private final Party party = new Party(name, key);
 
     @Test
     public void getName() {
-        MemberX500Name testMemberX500Name = new MemberX500Name("Bob Plc", "Rome", "IT");
-        when(party.getName()).thenReturn(testMemberX500Name);
-
         MemberX500Name result = party.getName();
 
         Assertions.assertThat(result).isNotNull();
-        Assertions.assertThat(result).isEqualTo(testMemberX500Name);
-        verify(party, times(1)).getName();
+        Assertions.assertThat(result).isEqualTo(name);
     }
 
     @Test
     void getOwningKey() {
-        final PublicKey publicKey = mock(PublicKey.class);
-        Mockito.when(party.getOwningKey()).thenReturn(publicKey);
-
         PublicKey result = party.getOwningKey();
 
         Assertions.assertThat(result).isNotNull();
-        Assertions.assertThat(result).isEqualTo(publicKey);
-        verify(party, times(1)).getOwningKey();
+        Assertions.assertThat(result).isEqualTo(key);
     }
 }
