217f0bdc9f4c85529d7cfa543430df634908b2d6
driessamyn <dries.samyn@r3.com>
2022-10-11 17:23:39 +0100

    PR feedback.

--- applications/workers/worker-common/src/main/kotlin/net/corda/applications/workers/workercommon/WorkerMonitor.kt
+++ applications/workers/worker-common/src/main/kotlin/net/corda/applications/workers/workercommon/WorkerMonitor.kt
@@ -5,7 +5,7 @@ package net.corda.applications.workers.workercommon
  *
  * A worker indicates its healthiness/readiness by returning a 200 code for HTTP requests to
  * `HTTP_HEALTH_ROUTE`/`HTTP_READINESS_ROUTE`.
- * Worker metrics are reported on `HTTP_STATUS_ROUTE`.
+ * Worker metrics are reported on `HTTP_METRICS_ROUTE`.
  *
  * A worker is considered healthy if no component has a `LifecycleStatus` of `LifecycleStatus.ERROR`. A worker is
  * considered ready if no component has a `LifecycleStatus` of either `LifecycleStatus.DOWN` or `LifecycleStatus.ERROR`.
