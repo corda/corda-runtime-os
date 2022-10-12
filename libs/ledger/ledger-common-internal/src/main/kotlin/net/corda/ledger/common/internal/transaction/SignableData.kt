28244a54bdc22b215d8890f41f752849b8bf127d
Lajos Veres <lajos.veres@r3.com>
2022-10-12 10:03:10 +0100

    CORE-5091 Fix doc

--- libs/ledger/ledger-common-internal/src/main/kotlin/net/corda/ledger/common/internal/transaction/SignableData.kt
+++ libs/ledger/ledger-common-internal/src/main/kotlin/net/corda/ledger/common/internal/transaction/SignableData.kt
@@ -10,8 +10,8 @@ import net.corda.v5.crypto.SecureHash
  * Note that when multi-transaction signing (signing a block of transactions) is used, the root of the Merkle tree
  * (having transaction IDs as leaves) is actually signed and thus [hash] refers to this root and not a specific transaction.
  *
- * @param hash transaction's id or root of multi-transaction Merkle tree in case of multi-transaction signing.
- * @param digitalSignatureMetadata meta data required.
+ * @property hash transaction's id or root of multi-transaction Merkle tree in case of multi-transaction signing.
+ * @property digitalSignatureMetadata meta data required.
  */
 @CordaSerializable
 data class SignableData(
