c202b26ab3402bdf2be2fdfb16f7643de7c853ce
Simon Johnson <106170800+simon-johnson-r3@users.noreply.github.com>
2022-10-12 11:04:27 +0100

    CORE-6876 Json Serializer forwarding (#2281)
    * Adaptors between the Corda Json serializer interfaces and Jackson.
    * These are not yet exposed for CorDapp developer use.

---
+++ libs/application/application-impl/src/main/kotlin/net/corda/application/impl/services/json/JsonSerializerAdaptor.kt
@@ -0,0 +1,23 @@
+package net.corda.application.impl.services.json
+
+import com.fasterxml.jackson.core.JsonGenerator
+import com.fasterxml.jackson.databind.SerializerProvider
+import com.fasterxml.jackson.databind.ser.std.StdSerializer
+import net.corda.v5.application.marshalling.json.JsonSerializer
+
+/**
+ * Adaptor between a Jackson serializer and a Corda Json serializer exposed to the public api. Every Json serializer
+ * written against Corda's public api is adapted to a Jackson serializer and loaded into the Jackson module. This
+ * class creates that simple bridge, wrapping the Corda serializer into a Jackson [StdSerializer] subclass.
+ */
+class JsonSerializerAdaptor<T>(private val jsonSerializer: JsonSerializer<T>, clazz: Class<T>) :
+    StdSerializer<T>(clazz) {
+    override fun serialize(
+        value: T, jgen: JsonGenerator, provider: SerializerProvider
+    ) {
+        jsonSerializer.serialize(value, JsonWriterAdaptor(jgen))
+    }
+}
+
+inline fun <reified T> jsonSerializerAdaptorOf(jsonSerializer: JsonSerializer<T>) =
+    JsonSerializerAdaptor<T>(jsonSerializer, T::class.java)
