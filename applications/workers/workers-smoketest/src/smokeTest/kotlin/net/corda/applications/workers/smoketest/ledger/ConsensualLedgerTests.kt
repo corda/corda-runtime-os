4dd327b52ad950471b61c6cc3419301d6524cc7b
Lajos Veres <lajos.veres@r3.com>
2022-10-12 18:47:43 +0100

    CORE-5091 Make ConsensualLedgerTests more self containing

--- applications/workers/workers-smoketest/src/smokeTest/kotlin/net/corda/applications/workers/smoketest/ledger/ConsensualLedgerTests.kt
+++ applications/workers/workers-smoketest/src/smokeTest/kotlin/net/corda/applications/workers/smoketest/ledger/ConsensualLedgerTests.kt
@@ -2,11 +2,21 @@ package net.corda.applications.workers.smoketest.ledger
 
 import net.corda.applications.workers.smoketest.GROUP_ID
 import net.corda.applications.workers.smoketest.RPC_FLOW_STATUS_SUCCESS
+import net.corda.applications.workers.smoketest.TEST_CPB_LOCATION
+import net.corda.applications.workers.smoketest.TEST_CPI_NAME
+import net.corda.applications.workers.smoketest.X500_ALICE
 import net.corda.applications.workers.smoketest.X500_BOB
+import net.corda.applications.workers.smoketest.X500_CHARLIE
+import net.corda.applications.workers.smoketest.X500_DAVID
 import net.corda.applications.workers.smoketest.awaitRpcFlowFinished
+import net.corda.applications.workers.smoketest.conditionallyUploadCordaPackage
+import net.corda.applications.workers.smoketest.flow.FlowTests
 import net.corda.applications.workers.smoketest.getHoldingIdShortHash
+import net.corda.applications.workers.smoketest.getOrCreateVirtualNodeFor
+import net.corda.applications.workers.smoketest.registerMember
 import net.corda.applications.workers.smoketest.startRpcFlow
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.MethodOrderer
 import org.junit.jupiter.api.Order
 import org.junit.jupiter.api.Test
@@ -15,13 +25,29 @@ import org.junit.jupiter.api.TestInstance.Lifecycle
 import org.junit.jupiter.api.TestMethodOrder
 
 @Suppress("Unused", "FunctionName")
-@Order(25)
+@Order(24)
 @TestMethodOrder(MethodOrderer.OrderAnnotation::class)
 @TestInstance(Lifecycle.PER_CLASS)
 class ConsensualLedgerTests {
 
     companion object {
         val bobHoldingId: String = getHoldingIdShortHash(X500_BOB, GROUP_ID)
+
+        @BeforeAll
+        @JvmStatic
+        internal fun beforeAll() {
+            // Upload test flows if not already uploaded
+            conditionallyUploadCordaPackage(TEST_CPI_NAME, TEST_CPB_LOCATION, GROUP_ID)
+
+            // Make sure Virtual Nodes are created
+            val bobActualHoldingId = getOrCreateVirtualNodeFor(X500_BOB)
+
+            // Just validate the function and actual vnode holding ID hash are in sync
+            // if this fails the X500_BOB formatting could have changed or the hash implementation might have changed
+            assertThat(bobActualHoldingId).isEqualTo(bobHoldingId)
+
+            registerMember(bobHoldingId)
+        }
     }
 
     @Test
