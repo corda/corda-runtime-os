f660bcb78376583e3770cbc800cc38f4bf2ab4db
TomFitzpatrick <tom.fitzpatrick@r3.com>
2022-10-19 17:21:07 +0100

    uncomment test code

--- libs/serialization/serialization-amqp/src/test/kotlin/net/corda/internal/serialization/amqp/ThrowableEvolutionTests.kt
+++ libs/serialization/serialization-amqp/src/test/kotlin/net/corda/internal/serialization/amqp/ThrowableEvolutionTests.kt
@@ -23,69 +23,69 @@ class ThrowableEvolutionTests {
      * AddConstructorParametersException with an extra parameter called "added"
      */
     @CordaSerializable
-    class AddConstructorParametersException(message: String) : CordaRuntimeException(message)
-//    class AddConstructorParametersException(message: String, val added: String?) : CordaRuntimeException(message)
+//    class AddConstructorParametersException(message: String) : CordaRuntimeException(message)
+    class AddConstructorParametersException(message: String, val added: String?) : CordaRuntimeException(message)
 
     /**
      * RemoveConstructorParametersException with the "toBeRemoved" parameter removed
      */
     @CordaSerializable
-    class RemoveConstructorParametersException(message: String, val toBeRemoved: String) : CordaRuntimeException(message)
-//    class RemoveConstructorParametersException(message: String) : CordaRuntimeException(message)
+//    class RemoveConstructorParametersException(message: String, val toBeRemoved: String) : CordaRuntimeException(message)
+    class RemoveConstructorParametersException(message: String) : CordaRuntimeException(message)
 
     /**
      * AddAndRemoveConstructorParametersException with the "toBeRemoved" parameter removed and "added" added
      */
     @CordaSerializable
-    class AddAndRemoveConstructorParametersException(message: String, val toBeRemoved: String) : CordaRuntimeException(message)
-//    class AddAndRemoveConstructorParametersException(message: String, val added: String?) : CordaRuntimeException(message)
+//    class AddAndRemoveConstructorParametersException(message: String, val toBeRemoved: String) : CordaRuntimeException(message)
+    class AddAndRemoveConstructorParametersException(message: String, val added: String?) : CordaRuntimeException(message)
 
     @Test
     fun addConstructorParametersException() {
 
-        val exception = AddConstructorParametersException(message)
-        saveSerializedObject(exception)
+//        val exception = AddConstructorParametersException(message)
+//        saveSerializedObject(exception)
 
-//        val bytes = this::class.java.getResource(testResourceName()).readBytes()
-//
-//        val sf = testDefaultFactory().also { registerCustomSerializers(it) }
-//        val deserializedException = DeserializationInput(sf)
-//            .deserialize(SerializedBytes<AddConstructorParametersException>(bytes))
-//
-//        assertThat(deserializedException.message).isEqualTo(message)
-//        assertThat(deserializedException).isInstanceOf(AddConstructorParametersException::class.java)
+        val bytes = this::class.java.getResource(testResourceName()).readBytes()
+
+        val sf = testDefaultFactory().also { registerCustomSerializers(it) }
+        val deserializedException = DeserializationInput(sf)
+            .deserialize(SerializedBytes<AddConstructorParametersException>(bytes))
+
+        assertThat(deserializedException.message).isEqualTo(message)
+        assertThat(deserializedException).isInstanceOf(AddConstructorParametersException::class.java)
     }
 
     @Test
     fun removeConstructorParametersException() {
 
-        val exception = RemoveConstructorParametersException(message, toBeRemovedValue)
-        saveSerializedObject(exception)
+//        val exception = RemoveConstructorParametersException(message, toBeRemovedValue)
+//        saveSerializedObject(exception)
 
-//        val bytes = this::class.java.getResource(testResourceName()).readBytes()
-//
-//        val sf = testDefaultFactory().also { registerCustomSerializers(it) }
-//        val deserializedException = DeserializationInput(sf)
-//            .deserialize(SerializedBytes<RemoveConstructorParametersException>(bytes))
-//
-//        assertThat(deserializedException.message).isEqualTo(message)
-//        assertThat(deserializedException).isInstanceOf(RemoveConstructorParametersException::class.java)
+        val bytes = this::class.java.getResource(testResourceName()).readBytes()
+
+        val sf = testDefaultFactory().also { registerCustomSerializers(it) }
+        val deserializedException = DeserializationInput(sf)
+            .deserialize(SerializedBytes<RemoveConstructorParametersException>(bytes))
+
+        assertThat(deserializedException.message).isEqualTo(message)
+        assertThat(deserializedException).isInstanceOf(RemoveConstructorParametersException::class.java)
     }
 
     @Test
     fun addAndRemoveConstructorParametersException() {
 
-        val exception = AddAndRemoveConstructorParametersException(message, toBeRemovedValue)
-        saveSerializedObject(exception)
+//        val exception = AddAndRemoveConstructorParametersException(message, toBeRemovedValue)
+//        saveSerializedObject(exception)
 
-//        val bytes = this::class.java.getResource(testResourceName()).readBytes()
-//
-//        val sf = testDefaultFactory().also { registerCustomSerializers(it) }
-//        val deserializedException = DeserializationInput(sf)
-//            .deserialize(SerializedBytes<AddAndRemoveConstructorParametersException>(bytes))
-//
-//        assertThat(deserializedException.message).isEqualTo(message)
-//        assertThat(deserializedException).isInstanceOf(AddAndRemoveConstructorParametersException::class.java)
+        val bytes = this::class.java.getResource(testResourceName()).readBytes()
+
+        val sf = testDefaultFactory().also { registerCustomSerializers(it) }
+        val deserializedException = DeserializationInput(sf)
+            .deserialize(SerializedBytes<AddAndRemoveConstructorParametersException>(bytes))
+
+        assertThat(deserializedException.message).isEqualTo(message)
+        assertThat(deserializedException).isInstanceOf(AddAndRemoveConstructorParametersException::class.java)
     }
 
     /**
