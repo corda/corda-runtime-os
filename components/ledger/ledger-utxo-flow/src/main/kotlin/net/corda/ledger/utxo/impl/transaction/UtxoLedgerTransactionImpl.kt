6a7641f88c65a151ec43bf1cb57512e7f2b23542
Lajos Veres <lajos.veres@r3.com>
2022-10-20 17:51:11 +0100

    CORE-7115 Check if we can get away without changing the API

--- components/ledger/ledger-utxo-flow/src/main/kotlin/net/corda/ledger/utxo/impl/transaction/UtxoLedgerTransactionImpl.kt
+++ components/ledger/ledger-utxo-flow/src/main/kotlin/net/corda/ledger/utxo/impl/transaction/UtxoLedgerTransactionImpl.kt
@@ -21,7 +21,7 @@ data class UtxoLedgerTransactionImpl(
     private val serializationService: SerializationService
 ) : UtxoLedgerTransaction {
 
-    override val id: SecureHash
+    val id: SecureHash
         get() = wireTransaction.id
 
     override val timeWindow: TimeWindow by lazy(LazyThreadSafetyMode.PUBLICATION) {
