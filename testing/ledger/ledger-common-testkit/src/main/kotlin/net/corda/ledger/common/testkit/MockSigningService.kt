f78110d2713d03f37faf50521e9863842eef19d0
Lajos Veres <lajos.veres@r3.com>
2022-10-20 18:06:45 +0100

    CORE-7115 Centralize a bit more in testkits

--- testing/ledger/ledger-common-testkit/src/main/kotlin/net/corda/ledger/common/testkit/MockSigningService.kt
+++ testing/ledger/ledger-common-testkit/src/main/kotlin/net/corda/ledger/common/testkit/MockSigningService.kt
@@ -5,9 +5,9 @@ import net.corda.v5.crypto.DigitalSignature
 import net.corda.v5.crypto.SignatureSpec
 import java.security.PublicKey
 
-private class MockSigningService :SigningService{
+private class MockSigningService: SigningService{
     override fun sign(bytes: ByteArray, publicKey: PublicKey, signatureSpec: SignatureSpec): DigitalSignature.WithKey {
-        return DigitalSignature.WithKey(publicKeyExample, "0".toByteArray(), mapOf())
+        return signatureWithMetaDataExample.signature
     }
 }
 
--- testing/ledger/ledger-consensual-testkit/src/main/kotlin/net/corda/ledger/consensual/testkit/ConsensualSignedTransactionExample.kt
+++ testing/ledger/ledger-consensual-testkit/src/main/kotlin/net/corda/ledger/consensual/testkit/ConsensualSignedTransactionExample.kt
@@ -1,18 +1,14 @@
 package net.corda.ledger.consensual.testkit
 
-import java.security.KeyPairGenerator
-import java.time.Instant
 import net.corda.ledger.common.testkit.getWireTransaction
+import net.corda.ledger.common.testkit.signatureWithMetaDataExample
 import net.corda.ledger.consensual.data.transaction.ConsensualSignedTransactionImpl
-import net.corda.v5.application.crypto.DigitalSignatureAndMetadata
-import net.corda.v5.application.crypto.DigitalSignatureMetadata
 import net.corda.v5.application.crypto.DigitalSignatureVerificationService
 import net.corda.v5.application.crypto.SigningService
 import net.corda.v5.application.marshalling.JsonMarshallingService
 import net.corda.v5.application.serialization.SerializationService
 import net.corda.v5.cipher.suite.DigestService
 import net.corda.v5.cipher.suite.merkle.MerkleTreeProvider
-import net.corda.v5.crypto.DigitalSignature
 import net.corda.v5.ledger.consensual.transaction.ConsensualSignedTransaction
 
 @Suppress("Unused", "LongParameterList")
@@ -30,21 +26,11 @@ fun getConsensualSignedTransaction(
         jsonMarshallingService,
         consensualTransactionMetaDataExample
     )
-
-    val kpg = KeyPairGenerator.getInstance("RSA")
-    kpg.initialize(512)
-    val testPublicKey = kpg.genKeyPair().public
-
-    val signature = DigitalSignature.WithKey(testPublicKey, "0".toByteArray(), mapOf())
-    val digitalSignatureMetadata =
-        DigitalSignatureMetadata(Instant.now(), mapOf())
-    val signatureWithMetaData = DigitalSignatureAndMetadata(signature, digitalSignatureMetadata)
     return ConsensualSignedTransactionImpl(
         serializationService,
         signingService,
         digitalSignatureVerificationService,
         wireTransaction,
-        listOf(signatureWithMetaData)
+        listOf(signatureWithMetaDataExample)
     )
 }
-
