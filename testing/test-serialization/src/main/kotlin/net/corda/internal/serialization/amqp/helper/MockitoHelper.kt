58aa99915a7796ad7d4141669ad5515b1d3b840c
Lajos Veres <lajos.veres@r3.com>
2022-10-25 08:57:52 +0100

    CORE-7115 Add comment

--- testing/test-serialization/src/main/kotlin/net/corda/internal/serialization/amqp/helper/MockitoHelper.kt
+++ testing/test-serialization/src/main/kotlin/net/corda/internal/serialization/amqp/helper/MockitoHelper.kt
@@ -2,6 +2,7 @@ package net.corda.internal.serialization.amqp.helper
 
 import org.mockito.Mockito
 
+/** Helps where `Mockito.any()` can't infer the type correctly and returns `null` instead */
 object MockitoHelper {
     fun <T> anyObject(): T {
         Mockito.any<T>()
