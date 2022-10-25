5b0104a7abce83ae0c916b365a45d6381a58e0ef
Lajos Veres <lajos.veres@r3.com>
2022-10-25 14:53:40 +0100

    CORE-7115 Fix PR review concerns

--- testing/ledger/ledger-common-testkit/src/main/kotlin/net/corda/ledger/common/testkit/PublicKeyExample.kt
+++ testing/ledger/ledger-common-testkit/src/main/kotlin/net/corda/ledger/common/testkit/PublicKeyExample.kt
@@ -3,6 +3,7 @@ package net.corda.ledger.common.testkit
 import java.security.KeyPairGenerator
 import java.security.PublicKey
 
+// TODO change this to something safer. (ECDSA)
 private val kpg: KeyPairGenerator = KeyPairGenerator.getInstance("RSA").also {
     it.initialize(512)
 }
