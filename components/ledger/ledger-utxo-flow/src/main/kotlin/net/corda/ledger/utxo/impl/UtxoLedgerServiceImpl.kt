10541091d5e336b44c3326522d6c440109fe01b1
Lajos Veres <lajos.veres@r3.com>
2022-10-26 16:54:56 +0100

    CORE-7118 UTXO Ledger Service getTransactionBuilder and UtxoTransactionBuilderFactory

--- components/ledger/ledger-utxo-flow/src/main/kotlin/net/corda/ledger/utxo/impl/UtxoLedgerServiceImpl.kt
+++ components/ledger/ledger-utxo-flow/src/main/kotlin/net/corda/ledger/utxo/impl/UtxoLedgerServiceImpl.kt
@@ -1,15 +1,27 @@
 package net.corda.ledger.utxo.impl
 
+import net.corda.ledger.utxo.impl.transaction.factory.UtxoTransactionBuilderFactory
+import net.corda.v5.base.annotations.Suspendable
 import net.corda.v5.ledger.utxo.ContractState
 import net.corda.v5.ledger.utxo.StateAndRef
 import net.corda.v5.ledger.utxo.StateRef
 import net.corda.v5.ledger.utxo.UtxoLedgerService
 import net.corda.v5.ledger.utxo.transaction.UtxoTransactionBuilder
+import net.corda.v5.serialization.SingletonSerializeAsToken
+import org.osgi.service.component.annotations.Activate
+import org.osgi.service.component.annotations.Component
+import org.osgi.service.component.annotations.Reference
+import org.osgi.service.component.annotations.ServiceScope
 
-class UtxoLedgerServiceImpl : UtxoLedgerService {
+@Component(service = [UtxoLedgerService::class, SingletonSerializeAsToken::class], scope = ServiceScope.PROTOTYPE)
+class UtxoLedgerServiceImpl @Activate constructor(
+    @Reference(service = UtxoTransactionBuilderFactory::class)
+    private val utxoTransactionBuilderFactory: UtxoTransactionBuilderFactory
+) : UtxoLedgerService, SingletonSerializeAsToken {
 
+    @Suspendable
     override fun getTransactionBuilder(): UtxoTransactionBuilder {
-        TODO("Not yet implemented")
+        return utxoTransactionBuilderFactory.create()
     }
 
     override fun <T : ContractState> resolve(stateRefs: Iterable<StateRef>): List<StateAndRef<T>> {
