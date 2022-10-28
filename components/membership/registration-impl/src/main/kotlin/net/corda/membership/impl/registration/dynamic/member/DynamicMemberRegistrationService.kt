51b76347b5195ff913371d28cee69d4a2bea9e07
Charlie Crean <charlie.crean@r3.com>
2022-10-28 19:56:43 +0100

    Tidy import

--- components/membership/registration-impl/src/main/kotlin/net/corda/membership/impl/registration/dynamic/member/DynamicMemberRegistrationService.kt
+++ components/membership/registration-impl/src/main/kotlin/net/corda/membership/impl/registration/dynamic/member/DynamicMemberRegistrationService.kt
@@ -87,7 +87,7 @@ import org.osgi.service.component.annotations.Component
 import org.osgi.service.component.annotations.Reference
 import org.slf4j.Logger
 import java.nio.ByteBuffer
-import java.util.*
+import java.util.UUID
 import java.util.concurrent.TimeUnit
 
 @Suppress("LongParameterList")
