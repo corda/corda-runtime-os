faf34062d65f5a32bc6adc6d795fa940c23be707
LWogan <lorcan.wogan@r3.com>
2022-10-28 13:41:05 +0100

    CORE-6962 fix test conditions that are no longer required

--- components/virtual-node/cpi-upload-rpcops-service/src/test/kotlin/net/corda/cpi/upload/endpoints/common/CpiUploadRPCOpsHandlerTest.kt
+++ components/virtual-node/cpi-upload-rpcops-service/src/test/kotlin/net/corda/cpi/upload/endpoints/common/CpiUploadRPCOpsHandlerTest.kt
@@ -4,10 +4,10 @@ import net.corda.cpi.upload.endpoints.service.CpiUploadRPCOpsService
 import net.corda.cpiinfo.read.CpiInfoReadService
 import net.corda.lifecycle.LifecycleCoordinator
 import net.corda.lifecycle.LifecycleCoordinatorName
-import net.corda.lifecycle.RegistrationHandle
-import net.corda.lifecycle.StartEvent
-import net.corda.lifecycle.RegistrationStatusChangeEvent
 import net.corda.lifecycle.LifecycleStatus
+import net.corda.lifecycle.RegistrationHandle
+import net.corda.lifecycle.RegistrationStatusChangeEvent
+import net.corda.lifecycle.StartEvent
 import net.corda.lifecycle.StopEvent
 import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Assertions.assertNull
@@ -51,23 +51,20 @@ class CpiUploadRPCOpsHandlerTest {
     }
 
     @Test
-    fun `on RegistrationStatusChangeEvent ERROR event closes resources and updates coordinator status to ERROR`(){
+    fun `on RegistrationStatusChangeEvent ERROR event closes resources and updates coordinator status to DOWN`(){
         val event = RegistrationStatusChangeEvent(mock(), LifecycleStatus.ERROR)
         val registrationHandle = mock<RegistrationHandle>()
         rpcOpsHandler.cpiUploadRPCOpsServiceRegistrationHandle = registrationHandle
         rpcOpsHandler.processEvent(event, coordinator)
-        verify(registrationHandle).close()
-        assertNull(rpcOpsHandler.cpiUploadRPCOpsServiceRegistrationHandle)
-        verify(coordinator).updateStatus(LifecycleStatus.ERROR)
+        verify(coordinator).updateStatus(LifecycleStatus.DOWN)
     }
 
     @Test
-    fun `on StopEvent event closes resources and sets coordinator status to DOWN`() {
+    fun `on StopEvent event closes resources`() {
         val registrationHandle = mock<RegistrationHandle>()
         rpcOpsHandler.cpiUploadRPCOpsServiceRegistrationHandle = registrationHandle
         rpcOpsHandler.processEvent(StopEvent(), coordinator)
         verify(registrationHandle).close()
         assertNull(rpcOpsHandler.cpiUploadRPCOpsServiceRegistrationHandle)
-        verify(coordinator).updateStatus(LifecycleStatus.DOWN)
     }
 }
