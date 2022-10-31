ae6a941221a5c5acb644bbf48acc2cde6f43f0dc
Matthew Layton <matthew.layton@live.co.uk>
2022-10-31 17:36:51 +0000

    Initial UTXO demo application implementation.

---
+++ testing/cpbs/ledger-utxo-demo-app/src/main/kotlin/net/cordapp/demo/utxo/contract/ObligationContract.kt
@@ -0,0 +1,119 @@
+package net.cordapp.demo.utxo.contract
+
+import net.corda.v5.ledger.utxo.Command
+import net.corda.v5.ledger.utxo.Contract
+import net.corda.v5.ledger.utxo.transaction.UtxoLedgerTransaction
+import java.math.BigDecimal
+
+class ObligationContract : Contract {
+
+    override fun verify(transaction: UtxoLedgerTransaction) {
+        val command = transaction.getCommands(TestUtxoContractCommand::class.java).singleOrNull()
+            ?: throw IllegalArgumentException("Expected a single command of type: ${TestUtxoContractCommand::class.java}.")
+
+        command.verify(transaction)
+    }
+
+    private interface TestUtxoContractCommand : Command {
+        fun verify(transaction: UtxoLedgerTransaction)
+    }
+
+    object Create : TestUtxoContractCommand {
+
+        internal const val CONTRACT_RULE_INPUTS =
+            "On state creating, zero input states must be consumed."
+
+        internal const val CONTRACT_RULE_OUTPUTS =
+            "On state creating, only one output state must be created."
+
+        internal const val CONTRACT_RULE_PARTICIPANTS =
+            "On state creating, the issuer and holder must not be the same participant."
+
+        internal const val CONTRACT_RULE_AMOUNT =
+            "On state creating, the amount must be greater than zero."
+
+        internal const val CONTRACT_RULE_SIGNATORIES =
+            "On state creating, the issuer must sign the transaction."
+
+        override fun verify(transaction: UtxoLedgerTransaction) {
+
+            // TODO : The generic variants of this don't work properly!
+            val inputs = transaction.getInputStates(ObligationState::class.java)
+            val outputs = transaction.getOutputStates(ObligationState::class.java)
+
+            require(inputs.isEmpty()) { CONTRACT_RULE_INPUTS }
+            require(outputs.size == 1) { CONTRACT_RULE_OUTPUTS }
+
+            val output = outputs.single()
+
+            require(output.issuer != output.holder) { CONTRACT_RULE_PARTICIPANTS }
+            require(output.amount > BigDecimal.ZERO) { CONTRACT_RULE_AMOUNT }
+            require(output.issuer in transaction.signatories) { CONTRACT_RULE_SIGNATORIES }
+        }
+    }
+
+    object Update : TestUtxoContractCommand {
+
+        internal const val CONTRACT_RULE_INPUTS =
+            "On state updating, only one input state must be consumed."
+
+        internal const val CONTRACT_RULE_OUTPUTS =
+            "On state updating, only one output state must be created."
+
+        internal const val CONTRACT_RULE_ISSUER =
+            "On state updating, the issuer must not change."
+
+        internal const val CONTRACT_RULE_HOLDER =
+            "On state updating, the holder must not change."
+
+        internal const val CONTRACT_RULE_AMOUNT =
+            "On state updating, the output state amount must be less than the input state amount."
+
+        internal const val CONTRACT_RULE_SIGNATORIES =
+            "On state updating, the holder must sign the transaction."
+
+        override fun verify(transaction: UtxoLedgerTransaction) {
+            val inputs = transaction.getInputStates(ObligationState::class.java)
+            val outputs = transaction.getOutputStates(ObligationState::class.java)
+
+            require(inputs.size == 1) { CONTRACT_RULE_INPUTS }
+            require(outputs.size == 1) { CONTRACT_RULE_OUTPUTS }
+
+            val input = inputs.single()
+            val output = outputs.single()
+
+            require(input.issuer == output.issuer) { CONTRACT_RULE_ISSUER }
+            require(input.holder == output.holder) { CONTRACT_RULE_HOLDER }
+            require(input.amount > output.amount) { CONTRACT_RULE_AMOUNT }
+            require(output.holder in transaction.signatories) { CONTRACT_RULE_SIGNATORIES }
+        }
+    }
+
+    object Delete : TestUtxoContractCommand {
+
+        internal const val CONTRACT_RULE_INPUTS =
+            "On state updating, only one input state must be consumed."
+
+        internal const val CONTRACT_RULE_OUTPUTS =
+            "On state updating, zero output states must be created."
+
+        internal const val CONTRACT_RULE_AMOUNT =
+            "On state updating, the amount must be zero."
+
+        internal const val CONTRACT_RULE_SIGNATORIES =
+            "On state updating, the issuer and the holder must sign the transaction."
+
+        override fun verify(transaction: UtxoLedgerTransaction) {
+            val inputs = transaction.getInputStates(ObligationState::class.java)
+            val outputs = transaction.getOutputStates(ObligationState::class.java)
+
+            require(inputs.size == 1) { CONTRACT_RULE_INPUTS }
+            require(outputs.isEmpty()) { CONTRACT_RULE_OUTPUTS }
+
+            val input = inputs.single()
+
+            require(input.amount == BigDecimal.ZERO) { CONTRACT_RULE_AMOUNT }
+            require(input.participants.all { it in transaction.signatories }) { CONTRACT_RULE_SIGNATORIES }
+        }
+    }
+}
