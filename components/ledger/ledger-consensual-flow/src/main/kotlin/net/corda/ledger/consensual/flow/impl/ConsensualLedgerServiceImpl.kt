c42f2bae6a4d5d8340734e26279c5c3db56cfee1
Christian Sailer <christian.sailer@r3.com>
2022-10-31 17:59:13 +0000

    Remove osgi test dependency

--- components/ledger/ledger-consensual-flow/src/main/kotlin/net/corda/ledger/consensual/flow/impl/ConsensualLedgerServiceImpl.kt
+++ components/ledger/ledger-consensual-flow/src/main/kotlin/net/corda/ledger/consensual/flow/impl/ConsensualLedgerServiceImpl.kt
@@ -2,6 +2,7 @@ package net.corda.ledger.consensual.flow.impl
 
 import net.corda.ledger.consensual.flow.impl.flows.finality.ConsensualFinalityFlow
 import net.corda.ledger.consensual.flow.impl.flows.finality.ConsensualReceiveFinalityFlow
+import net.corda.ledger.consensual.flow.impl.persistence.ConsensualLedgerPersistenceService
 import net.corda.ledger.consensual.flow.impl.transaction.factory.ConsensualTransactionBuilderFactory
 import net.corda.v5.application.flows.FlowEngine
 import net.corda.v5.application.messaging.FlowSession
@@ -25,7 +26,9 @@ class ConsensualLedgerServiceImpl @Activate constructor(
     @Reference(service = ConsensualTransactionBuilderFactory::class)
     private val consensualTransactionBuilderFactory: ConsensualTransactionBuilderFactory,
     @Reference(service = FlowEngine::class)
-    private val flowEngine: FlowEngine
+    private val flowEngine: FlowEngine,
+    @Reference(service = ConsensualLedgerPersistenceService::class)
+    private val persistenceService: ConsensualLedgerPersistenceService
 ) : ConsensualLedgerService, SingletonSerializeAsToken {
 
     @Suspendable
@@ -34,7 +37,7 @@ class ConsensualLedgerServiceImpl @Activate constructor(
     }
 
     override fun fetchTransaction(id: SecureHash): ConsensualSignedTransaction? {
-        TODO("Not yet implemented")
+        return persistenceService.find(id)
     }
 
     @Suspendable
