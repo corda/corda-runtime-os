f3a7ac3337c1efaad2d226f80482d9c1519f90c8
Dan Newton <dan.newton@r3.com>
2022-10-31 14:38:17 +0000

    CORE-7027 Make `ConsensualSignedTransactionVerifier` serializable (#670)
    Extend `Serializable` to make lambdas that implement this interface
    serializable by Kryo. Without this, errors are thrown when deserializing
    lambdas implementing `ConsensualSignedTransactionVerifier`.
    
    This improves the syntax of `receiveFinality` as we can pass a lambda
    into it rather than defining an anonymous class.

--- ledger/ledger-consensual/src/main/kotlin/net/corda/v5/ledger/consensual/transaction/ConsensualSignedTransactionVerifier.kt
+++ ledger/ledger-consensual/src/main/kotlin/net/corda/v5/ledger/consensual/transaction/ConsensualSignedTransactionVerifier.kt
@@ -1,10 +1,34 @@
 package net.corda.v5.ledger.consensual.transaction
 
 import net.corda.v5.base.annotations.Suspendable
+import net.corda.v5.base.exceptions.CordaRuntimeException
+import net.corda.v5.ledger.consensual.ConsensualLedgerService
+import java.io.Serializable
 
-fun interface ConsensualSignedTransactionVerifier {
+/**
+ * [ConsensualSignedTransactionVerifier] verifies a [ConsensualSignedTransaction].
+ *
+ * Implement [ConsensualSignedTransactionVerifier] and pass the implementation into [ConsensualLedgerService.receiveFinality] to perform
+ * custom verification on the [ConsensualSignedTransaction] received from the initiator of finality.
+ *
+ * When verifying a [ConsensualSignedTransaction] throw either an [IllegalArgumentException], [IllegalStateException] or
+ * [CordaRuntimeException] to indicate that the transaction is invalid. This will lead to the termination of finality for the caller of
+ * [ConsensualLedgerService.receiveFinality] and all participants included in finalizing the transaction. Other exceptions will still stop
+ * the progression of finality; however, the reason for the failure will not be communicated to the initiator of finality.
+ *
+ * @see ConsensualLedgerService.receiveFinality
+ */
+fun interface ConsensualSignedTransactionVerifier : Serializable {
 
-    // TODO [CORE-7027] Document that this API should throw to fail verification
+    /**
+     * Verify a [ConsensualSignedTransaction].
+     *
+     * Throw an [IllegalArgumentException], [IllegalStateException] or [CordaRuntimeException] to indicate that the transaction is invalid.
+     *
+     * @param signedTransaction The [ConsensualSignedTransaction] to verify.
+     *
+     * @throws Throwable If the [signedTransaction] fails verification.
+     */
     @Suspendable
     fun verify(signedTransaction: ConsensualSignedTransaction)
 }
