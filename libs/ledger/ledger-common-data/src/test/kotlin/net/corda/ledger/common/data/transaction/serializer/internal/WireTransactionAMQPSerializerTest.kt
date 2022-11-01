fd20179359acd59c3b77ab49b88842c0fef3b4fc
Lajos Veres <lajos.veres@r3.com>
2022-11-01 10:37:26 +0000

    CORE-7291 Simplify WireTransactionAMQPSerializerTest slightly

--- libs/ledger/ledger-common-data/src/test/kotlin/net/corda/ledger/common/data/transaction/serializer/internal/WireTransactionAMQPSerializerTest.kt
+++ libs/ledger/ledger-common-data/src/test/kotlin/net/corda/ledger/common/data/transaction/serializer/internal/WireTransactionAMQPSerializerTest.kt
@@ -4,29 +4,15 @@ import net.corda.application.impl.services.json.JsonMarshallingServiceImpl
 import net.corda.cipher.suite.impl.CipherSchemeMetadataImpl
 import net.corda.cipher.suite.impl.DigestServiceImpl
 import net.corda.crypto.merkle.impl.MerkleTreeProviderImpl
-import net.corda.flow.fiber.FlowFiber
-import net.corda.flow.fiber.FlowFiberService
 import net.corda.internal.serialization.amqp.helper.TestFlowFiberServiceWithSerialization
 import net.corda.internal.serialization.amqp.helper.TestSerializationService
 import net.corda.ledger.common.data.transaction.factory.WireTransactionFactoryImpl
 import net.corda.ledger.common.data.transaction.serializer.amqp.WireTransactionSerializer
 import net.corda.ledger.common.testkit.getWireTransactionExample
 import net.corda.v5.application.serialization.deserialize
-import net.corda.v5.cipher.suite.CipherSchemeMetadata
-import net.corda.v5.serialization.SingletonSerializeAsToken
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
-
-class TestFlowFiberServiceWithSerializationProxy constructor(
-    private val cipherSchemeMetadata: CipherSchemeMetadata
-) : FlowFiberService, SingletonSerializeAsToken {
-    override fun getExecutingFiber(): FlowFiber {
-        val testFlowFiberServiceWithSerialization = TestFlowFiberServiceWithSerialization()
-        testFlowFiberServiceWithSerialization.configureSerializer({}, cipherSchemeMetadata)
-        return testFlowFiberServiceWithSerialization.getExecutingFiber()
-    }
-}
 
 class WireTransactionAMQPSerializerTest {
     companion object {
@@ -36,7 +22,7 @@ class WireTransactionAMQPSerializerTest {
         val jsonMarshallingService = JsonMarshallingServiceImpl()
         private val serializationServiceBasic =
             TestSerializationService.getTestSerializationService({}, cipherSchemeMetadata)
-        private val flowFiberService = TestFlowFiberServiceWithSerializationProxy(cipherSchemeMetadata)
+        private val flowFiberService = TestFlowFiberServiceWithSerialization()
         private val wireTransactionFactory = WireTransactionFactoryImpl(
             merkleTreeProvider, digestService, jsonMarshallingService, cipherSchemeMetadata,
             serializationServiceBasic, flowFiberService
@@ -44,7 +30,6 @@ class WireTransactionAMQPSerializerTest {
         val serializationService = TestSerializationService.getTestSerializationService({
             it.register(WireTransactionSerializer(wireTransactionFactory), it)
         }, cipherSchemeMetadata)
-
     }
 
     @Test
