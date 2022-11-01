986542f2d6d0aaabbc74718b7178c034fa9d9808
Viktor Kolomeyko <Viktor.Kolomeyko@r3.com>
2022-11-01 09:50:57 +0000

    CORE-7229: Minor changes/comments

--- libs/permissions/permission-manager-impl/src/main/kotlin/net/corda/libs/permissions/manager/impl/RepeatedLogonsCache.kt
+++ libs/permissions/permission-manager-impl/src/main/kotlin/net/corda/libs/permissions/manager/impl/RepeatedLogonsCache.kt
@@ -5,8 +5,13 @@ import com.github.benmanes.caffeine.cache.Caffeine
 import net.corda.v5.crypto.sha256Bytes
 import java.time.Duration
 
-class RepeatedLogonsCache {
+/**
+ * Cache which is meant to improve performance of the repeated logon requests with Basic Authentication.
+ * To prevent clear text passwords to be stored in memory a fast hashing function (SHA-256) is applied to them.
+ */
+internal class RepeatedLogonsCache {
 
+    // Potentially those two parameters should be moved to the configuration schema.
     private val maximumSize =
         java.lang.Long.getLong("net.corda.libs.permissions.manager.repeatedLogons.cache.size", 1000)
 
