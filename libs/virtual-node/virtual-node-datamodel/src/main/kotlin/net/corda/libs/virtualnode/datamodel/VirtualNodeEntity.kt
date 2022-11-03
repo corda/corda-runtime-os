1f66cfcc976f47b731e1c992b06bd93e1d5e406a
Ben McMahon <benmcmahon100@gmail.com>
2022-11-03 11:36:34 +0000

    Remove unused dep

--- libs/virtual-node/virtual-node-datamodel/src/main/kotlin/net/corda/libs/virtualnode/datamodel/VirtualNodeEntity.kt
+++ libs/virtual-node/virtual-node-datamodel/src/main/kotlin/net/corda/libs/virtualnode/datamodel/VirtualNodeEntity.kt
@@ -15,7 +15,6 @@ import javax.persistence.Id
 import javax.persistence.IdClass
 import javax.persistence.JoinColumn
 import javax.persistence.ManyToOne
-import javax.persistence.NoResultException
 import javax.persistence.Table
 import javax.persistence.Version
 
