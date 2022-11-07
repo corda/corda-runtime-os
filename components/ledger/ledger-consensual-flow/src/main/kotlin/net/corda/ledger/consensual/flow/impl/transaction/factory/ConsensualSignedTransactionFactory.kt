3c6fbe97f69f1858b62a633733a120d74094fb4f
Lajos Veres <lajos.veres@r3.com>
2022-11-07 14:39:48 +0000

    Revert "CORE-7291 Remove @Suspendable from internal interfaces"
    This reverts commit fd99162a8386c61e4f68b76b08dd34b8c17c3fbf.

--- components/ledger/ledger-consensual-flow/src/main/kotlin/net/corda/ledger/consensual/flow/impl/transaction/factory/ConsensualSignedTransactionFactory.kt
+++ components/ledger/ledger-consensual-flow/src/main/kotlin/net/corda/ledger/consensual/flow/impl/transaction/factory/ConsensualSignedTransactionFactory.kt
@@ -2,11 +2,13 @@ package net.corda.ledger.consensual.flow.impl.transaction.factory
 
 import net.corda.ledger.common.data.transaction.WireTransaction
 import net.corda.v5.application.crypto.DigitalSignatureAndMetadata
+import net.corda.v5.base.annotations.Suspendable
 import net.corda.v5.ledger.consensual.transaction.ConsensualSignedTransaction
 import net.corda.v5.ledger.consensual.transaction.ConsensualTransactionBuilder
 import java.security.PublicKey
 
 interface ConsensualSignedTransactionFactory {
+    @Suspendable
     fun create(
         consensualTransactionBuilder: ConsensualTransactionBuilder,
         signatories: Iterable<PublicKey>
