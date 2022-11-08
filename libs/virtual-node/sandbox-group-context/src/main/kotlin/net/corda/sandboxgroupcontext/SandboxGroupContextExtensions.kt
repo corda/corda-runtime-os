006dbb0cd72635649e2c1f35399d165a41f1b850
Chris Rankin <chris.rankin@r3.com>
2022-11-08 12:19:13 +0000

    CORE-7240: Simplify helper functions for fetching sandbox singletons. (#2468)
    * Simplify helper functions for fetching sandbox singletons.
    * Prevent sandbox singletons from obtaining marker interface references.
    * Add singleton DigestService for use outside sandboxes.

--- libs/virtual-node/sandbox-group-context/src/main/kotlin/net/corda/sandboxgroupcontext/SandboxGroupContextExtensions.kt
+++ libs/virtual-node/sandbox-group-context/src/main/kotlin/net/corda/sandboxgroupcontext/SandboxGroupContextExtensions.kt
@@ -13,9 +13,21 @@ inline fun <reified T : Any> SandboxGroupContext.getObjectByKey(key: String) = g
  * Fetch service instances previously created using [SandboxGroupContextComponent.registerMetadataServices].
  */
 @Suppress("KDocUnresolvedReference")
-inline fun <reified T : Any> SandboxGroupContext.getMetadataServices(): Set<T> = getObjectByKey(T::class.java.name) ?: emptySet()
+fun <T : Any> SandboxGroupContext.getMetadataServices(type: Class<T>): Set<T> = getObjectByKey(type.name) ?: emptySet()
+
+inline fun <reified T : Any> SandboxGroupContext.getMetadataServices(): Set<T> = getMetadataServices(T::class.java)
 
 /**
  * Fetch the set of singleton services created for use by this sandbox.
  */
 fun SandboxGroupContext.getSandboxSingletonServices(): Set<Any> = getObjectByKey(SANDBOX_SINGLETONS) ?: emptySet()
+
+/**
+ * Fetch the single service of the given type from the sandbox's set of singletons.
+ */
+fun <T : Any> SandboxGroupContext.getSandboxSingletonService(type: Class<T>): T {
+    return getSandboxSingletonServices().filterIsInstance(type).singleOrNull()
+        ?: throw IllegalStateException("${type.name} service missing from sandbox")
+}
+
+inline fun <reified T : Any> SandboxGroupContext.getSandboxSingletonService(): T = getSandboxSingletonService(T::class.java)
