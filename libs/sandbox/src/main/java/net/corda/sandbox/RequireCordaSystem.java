6ce9dc2c9f7e3c506349a9cd5fa2c54a16381e86
Chris Rankin <chris.rankin@r3.com>
2022-11-09 16:02:18 +0000

    CORE-7954: Allow discovery of all bundles containing system flows.

---
+++ libs/sandbox/src/main/java/net/corda/sandbox/RequireCordaSystem.java
@@ -0,0 +1,25 @@
+package net.corda.sandbox;
+
+import org.osgi.annotation.bundle.Requirement;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.CLASS;
+import static net.corda.sandbox.SandboxConstants.CORDA_SYSTEM;
+import static org.osgi.annotation.bundle.Requirement.Cardinality.MULTIPLE;
+import static org.osgi.framework.Constants.RESOLUTION_DIRECTIVE;
+import static org.osgi.framework.Constants.RESOLUTION_OPTIONAL;
+
+@Requirement(
+    namespace = CORDA_SYSTEM,
+    name = "*",
+    version = RequireCordaSystem.VERSION,
+    cardinality = MULTIPLE,
+    attribute = { RESOLUTION_DIRECTIVE + ":=" + RESOLUTION_OPTIONAL }
+)
+@Retention(CLASS)
+@Target(TYPE)
+public @interface RequireCordaSystem {
+    String VERSION = "1.0";
+}
