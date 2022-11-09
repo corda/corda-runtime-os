1106cf6e7f4d27ae3249a6170f1bc2d5ad54f767
Joseph Zuniga-Daly <joseph.zunigadaly@r3.com>
2022-11-09 16:55:10 +0000

    Reuse ObjectMapper

--- applications/workers/workers-smoketest/src/smokeTest/kotlin/net/corda/applications/workers/smoketest/virtualnode/helpers/SimpleResponse.kt
+++ applications/workers/workers-smoketest/src/smokeTest/kotlin/net/corda/applications/workers/smoketest/virtualnode/helpers/SimpleResponse.kt
@@ -1,9 +1,10 @@
 package net.corda.applications.workers.smoketest.virtualnode.helpers
 
-import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
+
+private val objectMapper = ObjectMapper()
 
 /** Simplified response in case we switch underlying web clients, again */
 data class SimpleResponse(val code: Int, val body: String, val url: String) {
-    fun toJson(): JsonNode = ObjectMapper().readTree(this.body)!!
+    fun toJson() = objectMapper.readTree(this.body)!!
 }
