97b6d0125f09d76c0857c13ca2c0799164531143
Lajos Veres <lajos.veres@r3.com>
2022-11-12 00:05:05 +0000

    CORE-7943 Improve test classes

--- testing/ledger/ledger-consensual-state-app/src/main/kotlin/net/cordapp/testing/ledger/consensual/TestConsensualState.kt
+++ testing/ledger/ledger-consensual-state-app/src/main/kotlin/net/cordapp/testing/ledger/consensual/TestConsensualState.kt
@@ -3,6 +3,7 @@ package net.cordapp.testing.ledger.consensual
 import net.corda.v5.ledger.consensual.ConsensualState
 import net.corda.v5.ledger.consensual.transaction.ConsensualLedgerTransaction
 import java.security.PublicKey
+import java.util.Objects
 
 @Suppress("Unused")
 class TestConsensualState(
@@ -10,4 +11,12 @@ class TestConsensualState(
     override val participants: List<PublicKey>
 ) : ConsensualState {
     override fun verify(ledgerTransaction: ConsensualLedgerTransaction) {}
+    override fun equals(other: Any?): Boolean =
+        this === other ||
+                other is TestConsensualState &&
+                other.testField == testField &&
+                other.participants.size == participants.size &&
+                other.participants.containsAll(participants)
+
+    override fun hashCode(): Int = Objects.hash(testField, participants)
 }
