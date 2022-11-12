e8f8b8ded4dc41f9d6e3a834098743b35e4b79ed
Lajos Veres <lajos.veres@r3.com>
2022-11-12 00:19:52 +0000

    CORE-7943 update UtxoSignedTransactionKryoSerializationTest

--- components/ledger/ledger-utxo-flow/src/integrationTest/kotlin/net/corda/ledger/utxo/flow/impl/transaction/serializer/tests/UtxoSignedTransactionKryoSerializationTest.kt
+++ components/ledger/ledger-utxo-flow/src/integrationTest/kotlin/net/corda/ledger/utxo/flow/impl/transaction/serializer/tests/UtxoSignedTransactionKryoSerializationTest.kt
@@ -1,20 +1,18 @@
 package net.corda.ledger.utxo.flow.impl.transaction.serializer.tests
 
-import net.corda.internal.serialization.amqp.helper.TestSerializationService
-import net.corda.ledger.common.data.transaction.WireTransaction
-import net.corda.ledger.common.flow.transaction.TransactionSignatureService
-import net.corda.ledger.utxo.testkit.getUtxoSignedTransactionExample
-import net.corda.sandbox.SandboxCreationService
-import net.corda.sandbox.SandboxGroup
-import net.corda.serialization.checkpoint.CheckpointInternalCustomSerializer
-import net.corda.serialization.checkpoint.factory.CheckpointSerializerBuilderFactory
+import net.corda.flow.pipeline.sandbox.FlowSandboxService
+import net.corda.flow.pipeline.sandbox.impl.FlowSandboxGroupContextImpl
+import net.corda.ledger.common.data.transaction.factory.WireTransactionFactory
+import net.corda.ledger.utxo.flow.impl.transaction.factory.UtxoSignedTransactionFactory
+import net.corda.ledger.utxo.testkit.createExample
+import net.corda.sandboxgroupcontext.getObjectByKey
+import net.corda.sandboxgroupcontext.getSandboxSingletonService
+import net.corda.serialization.checkpoint.CheckpointSerializer
 import net.corda.testing.sandboxes.SandboxSetup
 import net.corda.testing.sandboxes.fetchService
 import net.corda.testing.sandboxes.lifecycle.AllTestsLifecycle
+import net.corda.testing.sandboxes.testkit.VirtualNodeService
 import net.corda.v5.application.marshalling.JsonMarshallingService
-import net.corda.v5.cipher.suite.CipherSchemeMetadata
-import net.corda.v5.cipher.suite.DigestService
-import net.corda.v5.cipher.suite.merkle.MerkleTreeProvider
 import net.corda.v5.ledger.utxo.transaction.UtxoSignedTransaction
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Assertions
@@ -24,6 +22,7 @@ import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS
 import org.junit.jupiter.api.extension.ExtendWith
 import org.junit.jupiter.api.extension.RegisterExtension
+import org.junit.jupiter.api.fail
 import org.junit.jupiter.api.io.TempDir
 import org.osgi.framework.BundleContext
 import org.osgi.test.common.annotation.InjectBundleContext
@@ -32,21 +31,21 @@ import org.osgi.test.junit5.context.BundleContextExtension
 import org.osgi.test.junit5.service.ServiceExtension
 import java.nio.file.Path
 
+private const val TESTING_CPB = "/META-INF/ledger-utxo-state-app.cpb"
+private const val TIMEOUT_MILLIS = 10000L
+
 @ExtendWith(ServiceExtension::class, BundleContextExtension::class)
 @TestInstance(PER_CLASS)
+
 class UtxoSignedTransactionKryoSerializationTest {
     @RegisterExtension
     private val lifecycle = AllTestsLifecycle()
 
-    private lateinit var emptySandboxGroup: SandboxGroup
-    private lateinit var digestService: DigestService
-    private lateinit var cipherSchemeMetadata: CipherSchemeMetadata
-    private lateinit var merkleTreeProvider: MerkleTreeProvider
+    private lateinit var flowSandboxService: FlowSandboxService
     private lateinit var jsonMarshallingService: JsonMarshallingService
-    private lateinit var transactionSignatureService: TransactionSignatureService
-    private lateinit var checkpointSerializerBuilderFactory: CheckpointSerializerBuilderFactory
-    private lateinit var wireTransactionKryoSerializer: CheckpointInternalCustomSerializer<WireTransaction>
-    private lateinit var utxoSignedTransactionSerializer: CheckpointInternalCustomSerializer<UtxoSignedTransaction>
+    private lateinit var wireTransactionFactory: WireTransactionFactory
+    private lateinit var utxoSignedTransactionFactory: UtxoSignedTransactionFactory
+    private lateinit var kryoSerializer: CheckpointSerializer
 
     @BeforeAll
     fun setup(
@@ -58,46 +57,30 @@ class UtxoSignedTransactionKryoSerializationTest {
         baseDirectory: Path
     ) {
         sandboxSetup.configure(bundleContext, baseDirectory)
-        Companion.lifecycle.accept(sandboxSetup) { setup ->
-            val sandboxCreationService = setup.fetchService<SandboxCreationService>(timeout = 1500)
-            emptySandboxGroup = sandboxCreationService.createSandboxGroup(emptyList())
-            setup.withCleanup { sandboxCreationService.unloadSandboxGroup(emptySandboxGroup) }
+        lifecycle.accept(sandboxSetup) { setup ->
+            flowSandboxService = setup.fetchService(TIMEOUT_MILLIS)
 
-            digestService = setup.fetchService(1500)
-            cipherSchemeMetadata = setup.fetchService(1500)
-            merkleTreeProvider = setup.fetchService(1500)
-            jsonMarshallingService = setup.fetchService(1500)
-            transactionSignatureService = setup.fetchService(1500)
-            checkpointSerializerBuilderFactory = setup.fetchService(1500)
+            val virtualNode = setup.fetchService<VirtualNodeService>(TIMEOUT_MILLIS)
+            val virtualNodeInfo = virtualNode.loadVirtualNode(TESTING_CPB)
+            val sandboxGroupContext = flowSandboxService.get(virtualNodeInfo.holdingIdentity)
+            setup.withCleanup { virtualNode.unloadSandbox(sandboxGroupContext) }
 
-            wireTransactionKryoSerializer = setup.fetchService(
-                "(component.name=net.corda.ledger.common.flow.impl.transaction.serializer.kryo.WireTransactionKryoSerializer)",
-                1500
-            )
-            utxoSignedTransactionSerializer = setup.fetchService(
-                "(component.name=net.corda.ledger.utxo.flow.impl.transaction.serializer.kryo.UtxoSignedTransactionKryoSerializer)",
-                1500
-            )
+            jsonMarshallingService = sandboxGroupContext.getSandboxSingletonService()
+            wireTransactionFactory = sandboxGroupContext.getSandboxSingletonService()
+            utxoSignedTransactionFactory = sandboxGroupContext.getSandboxSingletonService()
+            kryoSerializer = sandboxGroupContext.getObjectByKey(FlowSandboxGroupContextImpl.CHECKPOINT_SERIALIZER)
+                ?: fail("No CheckpointSerializer in sandbox context")
+
         }
     }
 
     @Test
     @Suppress("FunctionName")
     fun `correct serialization of a utxo Signed Transaction`() {
-        val serializationService = TestSerializationService.getTestSerializationService({ }, cipherSchemeMetadata)
-
-        val builder = checkpointSerializerBuilderFactory.createCheckpointSerializerBuilder(emptySandboxGroup)
-        val kryoSerializer = builder
-            .addSerializer(WireTransaction::class.java, wireTransactionKryoSerializer)
-            .addSerializer(UtxoSignedTransaction::class.java, utxoSignedTransactionSerializer)
-            .build()
-
-        val signedTransaction = getUtxoSignedTransactionExample(
-            digestService,
-            merkleTreeProvider,
-            serializationService,
+        val signedTransaction = utxoSignedTransactionFactory.createExample(
             jsonMarshallingService,
-            transactionSignatureService
+            wireTransactionFactory,
+            utxoSignedTransactionFactory
         )
 
         val bytes = kryoSerializer.serialize(signedTransaction)
--- testing/ledger/ledger-utxo-testkit/src/main/kotlin/net/corda/ledger/utxo/testkit/UtxoSignedTransactionExample.kt
+++ testing/ledger/ledger-utxo-testkit/src/main/kotlin/net/corda/ledger/utxo/testkit/UtxoSignedTransactionExample.kt
@@ -1,14 +1,26 @@
 package net.corda.ledger.utxo.testkit
 
+import net.corda.ledger.common.data.transaction.factory.WireTransactionFactory
 import net.corda.ledger.common.flow.transaction.TransactionSignatureService
+import net.corda.ledger.common.testkit.createExample
 import net.corda.ledger.common.testkit.getWireTransactionExample
 import net.corda.ledger.common.testkit.signatureWithMetadataExample
 import net.corda.ledger.utxo.flow.impl.transaction.UtxoSignedTransactionImpl
+import net.corda.ledger.utxo.flow.impl.transaction.factory.UtxoSignedTransactionFactory
 import net.corda.v5.application.marshalling.JsonMarshallingService
 import net.corda.v5.application.serialization.SerializationService
 import net.corda.v5.cipher.suite.DigestService
 import net.corda.v5.cipher.suite.merkle.MerkleTreeProvider
 import net.corda.v5.ledger.utxo.transaction.UtxoSignedTransaction
+
+fun UtxoSignedTransactionFactory.createExample(
+    jsonMarshallingService: JsonMarshallingService,
+    wireTransactionFactory: WireTransactionFactory,
+    utxoSignedTransactionFactory:UtxoSignedTransactionFactory
+):UtxoSignedTransaction {
+    val wireTransaction = wireTransactionFactory.createExample(jsonMarshallingService)
+    return utxoSignedTransactionFactory.create(wireTransaction, listOf(signatureWithMetadataExample))
+}
 
 @Suppress("LongParameterList")
 fun getUtxoSignedTransactionExample(
