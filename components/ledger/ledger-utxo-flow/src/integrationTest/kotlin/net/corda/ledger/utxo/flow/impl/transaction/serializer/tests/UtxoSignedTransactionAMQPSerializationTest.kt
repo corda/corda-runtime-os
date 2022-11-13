4b01b8cd0323c01e2804b0eec3d3cccaa9f422a6
Lajos Veres <lajos.veres@r3.com>
2022-11-13 17:16:05 +0000

    CORE-7943 Use centralized tests in utxo ledger integration tests

--- components/ledger/ledger-utxo-flow/src/integrationTest/kotlin/net/corda/ledger/utxo/flow/impl/transaction/serializer/tests/UtxoSignedTransactionAMQPSerializationTest.kt
+++ components/ledger/ledger-utxo-flow/src/integrationTest/kotlin/net/corda/ledger/utxo/flow/impl/transaction/serializer/tests/UtxoSignedTransactionAMQPSerializationTest.kt
@@ -1,109 +1,14 @@
 package net.corda.ledger.utxo.flow.impl.transaction.serializer.tests
 
-import net.corda.flow.pipeline.sandbox.FlowSandboxService
-import net.corda.internal.serialization.AMQP_STORAGE_CONTEXT
 import net.corda.internal.serialization.amqp.DeserializationInput
-import net.corda.internal.serialization.amqp.ObjectAndEnvelope
 import net.corda.internal.serialization.amqp.SerializationOutput
-import net.corda.internal.serialization.amqp.SerializerFactory
-import net.corda.internal.serialization.amqp.SerializerFactoryBuilder
-import net.corda.internal.serialization.registerCustomSerializers
-import net.corda.ledger.common.data.transaction.factory.WireTransactionFactory
-import net.corda.ledger.utxo.flow.impl.transaction.factory.UtxoSignedTransactionFactory
-import net.corda.ledger.utxo.testkit.createExample
-import net.corda.sandbox.SandboxGroup
-import net.corda.sandboxgroupcontext.getSandboxSingletonService
-import net.corda.sandboxgroupcontext.getSandboxSingletonServices
-import net.corda.serialization.InternalCustomSerializer
-import net.corda.serialization.SerializationContext
-import net.corda.testing.sandboxes.SandboxSetup
-import net.corda.testing.sandboxes.fetchService
-import net.corda.testing.sandboxes.lifecycle.AllTestsLifecycle
-import net.corda.testing.sandboxes.testkit.VirtualNodeService
-import net.corda.v5.application.marshalling.JsonMarshallingService
+import net.corda.ledger.utxo.testkit.UtxoLedgerIntegrationTest
 import net.corda.v5.ledger.utxo.transaction.UtxoSignedTransaction
-import net.corda.v5.serialization.SerializedBytes
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Assertions.assertDoesNotThrow
-import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.TestInstance
-import org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS
-import org.junit.jupiter.api.Timeout
-import org.junit.jupiter.api.extension.ExtendWith
-import org.junit.jupiter.api.extension.RegisterExtension
-import org.junit.jupiter.api.io.TempDir
-import org.osgi.framework.BundleContext
-import org.osgi.test.common.annotation.InjectBundleContext
-import org.osgi.test.common.annotation.InjectService
-import org.osgi.test.junit5.context.BundleContextExtension
-import org.osgi.test.junit5.service.ServiceExtension
-import java.io.NotSerializableException
-import java.nio.file.Path
-import java.util.concurrent.TimeUnit
 
-private const val TESTING_CPB = "/META-INF/ledger-utxo-state-app.cpb"
-private const val TIMEOUT_MILLIS = 10000L
-
-@Timeout(value = 30, unit = TimeUnit.SECONDS)
-@ExtendWith(ServiceExtension::class, BundleContextExtension::class)
-@TestInstance(PER_CLASS)
-class UtxoSignedTransactionAMQPSerializationTest {
-    private val testSerializationContext = AMQP_STORAGE_CONTEXT
-
-    @RegisterExtension
-    private val lifecycle = AllTestsLifecycle()
-
-    private lateinit var flowSandboxService: FlowSandboxService
-    private lateinit var jsonMarshallingService: JsonMarshallingService
-    private lateinit var wireTransactionFactory: WireTransactionFactory
-    private lateinit var utxoSignedTransactionFactory: UtxoSignedTransactionFactory
-
-    private lateinit var sandboxGroup: SandboxGroup
-    private lateinit var internalCustomSerializers: Set<InternalCustomSerializer<out Any>>
-
-    @BeforeAll
-    fun setUp(
-        @InjectService(timeout = 1000)
-        sandboxSetup: SandboxSetup,
-        @InjectBundleContext
-        bundleContext: BundleContext,
-        @TempDir
-        baseDirectory: Path
-
-    ) {
-        sandboxSetup.configure(bundleContext, baseDirectory)
-        lifecycle.accept(sandboxSetup) { setup ->
-            flowSandboxService = setup.fetchService(TIMEOUT_MILLIS)
-
-            val virtualNode = setup.fetchService<VirtualNodeService>(TIMEOUT_MILLIS)
-            val virtualNodeInfo = virtualNode.loadVirtualNode(TESTING_CPB)
-            val sandboxGroupContext = flowSandboxService.get(virtualNodeInfo.holdingIdentity)
-            setup.withCleanup { virtualNode.unloadSandbox(sandboxGroupContext) }
-            sandboxGroup = sandboxGroupContext.sandboxGroup
-
-            jsonMarshallingService = sandboxGroupContext.getSandboxSingletonService()
-            wireTransactionFactory = sandboxGroupContext.getSandboxSingletonService()
-            utxoSignedTransactionFactory = sandboxGroupContext.getSandboxSingletonService()
-
-            internalCustomSerializers = sandboxGroupContext.getSandboxSingletonServices()
-        }
-    }
-
-    private fun testDefaultFactory(sandboxGroup: SandboxGroup): SerializerFactory =
-        SerializerFactoryBuilder.build(sandboxGroup, allowEvolution = true).also {
-            registerCustomSerializers(it)
-            internalCustomSerializers.map{ customSerializer->
-                it.register(customSerializer, it)
-            }
-        }
-
-    @Throws(NotSerializableException::class)
-    inline fun <reified T : Any> DeserializationInput.deserializeAndReturnEnvelope(
-        bytes: SerializedBytes<T>,
-        serializationContext: SerializationContext
-    ): ObjectAndEnvelope<T> = deserializeAndReturnEnvelope(bytes, T::class.java, serializationContext)
-
+class UtxoSignedTransactionAMQPSerializationTest: UtxoLedgerIntegrationTest() {
     @Test
     @Suppress("FunctionName")
     fun `successfully serialize and deserialize a utxo Signed Transaction`() {
@@ -114,13 +19,8 @@ class UtxoSignedTransactionAMQPSerializationTest {
 
         // Initialise the serialisation context
         val testSerializationContext = testSerializationContext.withSandboxGroup(sandboxGroup)
-
-        val signedTransaction = utxoSignedTransactionFactory.createExample(
-            jsonMarshallingService,
-            wireTransactionFactory,
-            utxoSignedTransactionFactory
-        )
-        val serialised = SerializationOutput(factory1).serialize(signedTransaction, testSerializationContext)
+        
+        val serialised = SerializationOutput(factory1).serialize(utxoSignedTransaction, testSerializationContext)
 
 
         // Perform deserialization and check if the correct class is deserialized
@@ -132,11 +32,11 @@ class UtxoSignedTransactionAMQPSerializationTest {
 
         assertThat(deserialized.obj)
             .isInstanceOf(UtxoSignedTransaction::class.java)
-            .isEqualTo(signedTransaction)
+            .isEqualTo(utxoSignedTransaction)
 
         assertDoesNotThrow {
             deserialized.obj.id
         }
-        assertThat(deserialized.obj.id).isEqualTo(signedTransaction.id)
+        assertThat(deserialized.obj.id).isEqualTo(utxoSignedTransaction.id)
     }
 }
--- components/ledger/ledger-utxo-flow/src/integrationTest/kotlin/net/corda/ledger/utxo/flow/impl/transaction/serializer/tests/UtxoSignedTransactionKryoSerializationTest.kt
+++ components/ledger/ledger-utxo-flow/src/integrationTest/kotlin/net/corda/ledger/utxo/flow/impl/transaction/serializer/tests/UtxoSignedTransactionKryoSerializationTest.kt
@@ -1,99 +1,21 @@
 package net.corda.ledger.utxo.flow.impl.transaction.serializer.tests
 
-import net.corda.flow.pipeline.sandbox.FlowSandboxService
-import net.corda.flow.pipeline.sandbox.impl.FlowSandboxGroupContextImpl
-import net.corda.ledger.common.data.transaction.factory.WireTransactionFactory
-import net.corda.ledger.utxo.flow.impl.transaction.factory.UtxoSignedTransactionFactory
-import net.corda.ledger.utxo.testkit.createExample
-import net.corda.sandboxgroupcontext.getObjectByKey
-import net.corda.sandboxgroupcontext.getSandboxSingletonService
-import net.corda.serialization.checkpoint.CheckpointSerializer
-import net.corda.testing.sandboxes.SandboxSetup
-import net.corda.testing.sandboxes.fetchService
-import net.corda.testing.sandboxes.lifecycle.AllTestsLifecycle
-import net.corda.testing.sandboxes.testkit.VirtualNodeService
-import net.corda.v5.application.marshalling.JsonMarshallingService
+import net.corda.ledger.utxo.testkit.UtxoLedgerIntegrationTest
 import net.corda.v5.ledger.utxo.transaction.UtxoSignedTransaction
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Assertions
-import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.TestInstance
-import org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS
-import org.junit.jupiter.api.extension.ExtendWith
-import org.junit.jupiter.api.extension.RegisterExtension
-import org.junit.jupiter.api.fail
-import org.junit.jupiter.api.io.TempDir
-import org.osgi.framework.BundleContext
-import org.osgi.test.common.annotation.InjectBundleContext
-import org.osgi.test.common.annotation.InjectService
-import org.osgi.test.junit5.context.BundleContextExtension
-import org.osgi.test.junit5.service.ServiceExtension
-import java.nio.file.Path
 
-private const val TESTING_CPB = "/META-INF/ledger-utxo-state-app.cpb"
-private const val TIMEOUT_MILLIS = 10000L
-
-@ExtendWith(ServiceExtension::class, BundleContextExtension::class)
-@TestInstance(PER_CLASS)
-
-class UtxoSignedTransactionKryoSerializationTest {
-    @RegisterExtension
-    private val lifecycle = AllTestsLifecycle()
-
-    private lateinit var flowSandboxService: FlowSandboxService
-    private lateinit var jsonMarshallingService: JsonMarshallingService
-    private lateinit var wireTransactionFactory: WireTransactionFactory
-    private lateinit var utxoSignedTransactionFactory: UtxoSignedTransactionFactory
-    private lateinit var kryoSerializer: CheckpointSerializer
-
-    @BeforeAll
-    fun setup(
-        @InjectService(timeout = 1000)
-        sandboxSetup: SandboxSetup,
-        @InjectBundleContext
-        bundleContext: BundleContext,
-        @TempDir
-        baseDirectory: Path
-    ) {
-        sandboxSetup.configure(bundleContext, baseDirectory)
-        lifecycle.accept(sandboxSetup) { setup ->
-            flowSandboxService = setup.fetchService(TIMEOUT_MILLIS)
-
-            val virtualNode = setup.fetchService<VirtualNodeService>(TIMEOUT_MILLIS)
-            val virtualNodeInfo = virtualNode.loadVirtualNode(TESTING_CPB)
-            val sandboxGroupContext = flowSandboxService.get(virtualNodeInfo.holdingIdentity)
-            setup.withCleanup { virtualNode.unloadSandbox(sandboxGroupContext) }
-
-            jsonMarshallingService = sandboxGroupContext.getSandboxSingletonService()
-            wireTransactionFactory = sandboxGroupContext.getSandboxSingletonService()
-            utxoSignedTransactionFactory = sandboxGroupContext.getSandboxSingletonService()
-            kryoSerializer = sandboxGroupContext.getObjectByKey(FlowSandboxGroupContextImpl.CHECKPOINT_SERIALIZER)
-                ?: fail("No CheckpointSerializer in sandbox context")
-
-        }
-    }
-
+class UtxoSignedTransactionKryoSerializationTest: UtxoLedgerIntegrationTest() {
     @Test
     @Suppress("FunctionName")
     fun `correct serialization of a utxo Signed Transaction`() {
-        val signedTransaction = utxoSignedTransactionFactory.createExample(
-            jsonMarshallingService,
-            wireTransactionFactory,
-            utxoSignedTransactionFactory
-        )
 
-        val bytes = kryoSerializer.serialize(signedTransaction)
+        val bytes = kryoSerializer.serialize(utxoSignedTransaction)
         val deserialized = kryoSerializer.deserialize(bytes, UtxoSignedTransaction::class.java)
 
-        assertThat(deserialized).isEqualTo(signedTransaction)
+        assertThat(deserialized).isEqualTo(utxoSignedTransaction)
         Assertions.assertDoesNotThrow { deserialized.id }
-        Assertions.assertEquals(signedTransaction.id, deserialized.id)
-    }
-
-    companion object {
-        @JvmStatic
-        @RegisterExtension
-        private val lifecycle = AllTestsLifecycle()
+        Assertions.assertEquals(utxoSignedTransaction.id, deserialized.id)
     }
 }
