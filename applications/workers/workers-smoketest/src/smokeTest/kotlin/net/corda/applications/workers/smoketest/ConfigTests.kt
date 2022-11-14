c843a96f438b31651fea8f9190c0b0f2fad7952d
LWogan <lorcan.wogan@r3.com>
2022-11-14 16:22:04 +0000

    CORE-7999 revert change to use membership config as it did not help

--- applications/workers/workers-smoketest/src/smokeTest/kotlin/net/corda/applications/workers/smoketest/ConfigTests.kt
+++ applications/workers/workers-smoketest/src/smokeTest/kotlin/net/corda/applications/workers/smoketest/ConfigTests.kt
@@ -1,8 +1,9 @@
 package net.corda.applications.workers.smoketest
 
 import net.corda.schema.configuration.ConfigKeys
-import net.corda.schema.configuration.ConfigKeys.MEMBERSHIP_CONFIG
-import net.corda.schema.configuration.MembershipConfig.MAX_DURATION_BETWEEN_SYNC_REQUESTS_MINUTES
+import net.corda.schema.configuration.ConfigKeys.RECONCILIATION_CONFIG
+import net.corda.schema.configuration.ReconciliationConfig
+import net.corda.schema.configuration.ReconciliationConfig.RECONCILIATION_CONFIG_INTERVAL_MS
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Order
 import org.junit.jupiter.api.Test
@@ -13,8 +14,8 @@ class ConfigTests {
 
     @Test
     fun `get config includes defaults`() {
-        val defaultedConfigValues = getConfig(MEMBERSHIP_CONFIG).configWithDefaultsNode()
-        MEMBERSHIP_CONFIG::class.java.declaredFields
+        val defaultedConfigValues = getConfig(RECONCILIATION_CONFIG).configWithDefaultsNode()
+        ReconciliationConfig::class.java.declaredFields
             .filter { it.name != "INSTANCE" }
             .map { it.get(ConfigKeys) as String }
             .forEach {
@@ -28,21 +29,21 @@ class ConfigTests {
     fun `can update config`() {
         var currentValue = getCurrentReconConfigValue()
         val newValue = (currentValue * 2)
-        updateConfig(mapOf(MAX_DURATION_BETWEEN_SYNC_REQUESTS_MINUTES to newValue).toJsonString(), MEMBERSHIP_CONFIG)
+        updateConfig(mapOf(RECONCILIATION_CONFIG_INTERVAL_MS to newValue).toJsonString(), RECONCILIATION_CONFIG)
 
         try {
             currentValue = getCurrentReconConfigValue()
             assertThat(currentValue).isEqualTo(newValue)
         } finally {
             // Be a good neighbour and rollback the configuration change back to what it was
-            updateConfig(mapOf(MAX_DURATION_BETWEEN_SYNC_REQUESTS_MINUTES to currentValue).toJsonString(), MEMBERSHIP_CONFIG)
+            updateConfig(mapOf(RECONCILIATION_CONFIG_INTERVAL_MS to currentValue).toJsonString(), RECONCILIATION_CONFIG)
         }
     }
 
     private fun getCurrentReconConfigValue(): Int {
-        val currentConfig = getConfig(MEMBERSHIP_CONFIG)
+        val currentConfig = getConfig(RECONCILIATION_CONFIG)
         val currentConfigJSON = currentConfig.sourceConfigNode()
         println("currentConfig: ${currentConfigJSON.toPrettyString()}")
-        return currentConfigJSON[MAX_DURATION_BETWEEN_SYNC_REQUESTS_MINUTES].asInt()
+        return currentConfigJSON[RECONCILIATION_CONFIG_INTERVAL_MS].asInt()
     }
 }
