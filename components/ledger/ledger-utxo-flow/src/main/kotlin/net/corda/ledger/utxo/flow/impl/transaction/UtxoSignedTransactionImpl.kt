81cc94a8e4a40e3fd7ce4e99d145f98dfafba849
Lajos Veres <lajos.veres@r3.com>
2022-11-15 17:25:23 +0000

    CORE-8112 Update UtxoSignedTransaction

--- components/ledger/ledger-utxo-flow/src/main/kotlin/net/corda/ledger/utxo/flow/impl/transaction/UtxoSignedTransactionImpl.kt
+++ components/ledger/ledger-utxo-flow/src/main/kotlin/net/corda/ledger/utxo/flow/impl/transaction/UtxoSignedTransactionImpl.kt
@@ -5,6 +5,7 @@ import net.corda.ledger.common.flow.transaction.TransactionSignatureService
 import net.corda.ledger.utxo.data.transaction.UtxoLedgerTransactionImpl
 import net.corda.v5.application.crypto.DigitalSignatureAndMetadata
 import net.corda.v5.application.serialization.SerializationService
+import net.corda.v5.base.annotations.Suspendable
 import net.corda.v5.crypto.SecureHash
 import net.corda.v5.crypto.isFulfilledBy
 import net.corda.v5.ledger.utxo.transaction.UtxoLedgerTransaction
@@ -40,12 +41,48 @@ data class UtxoSignedTransactionImpl(
 
     override val id: SecureHash get() = wireTransaction.id
 
-    override fun addSignatures(signatures: Iterable<DigitalSignatureAndMetadata>): UtxoSignedTransaction {
-        return copy(signatures = this.signatures + signatures)
+    /**
+     * Sign the current [UtxoSignedTransactionImpl] with the specified key.
+     *
+     * @param publicKey The private counterpart of the specified public key will be used for signing the
+     *      [ConsensualSignedTransaction].
+     * @return Returns the new [UtxoSignedTransactionImpl] containing the applied signature and the signature itself.
+     */
+    @Suspendable
+    fun sign(publicKey: PublicKey): Pair<UtxoSignedTransaction, DigitalSignatureAndMetadata> {
+        val newSignature = transactionSignatureService.sign(id, publicKey)
+        return Pair(
+            UtxoSignedTransactionImpl(
+                serializationService,
+                transactionSignatureService,
+                wireTransaction,
+                signatures + newSignature
+            ),
+            newSignature
+        )
     }
 
-    override fun addSignatures(vararg signatures: DigitalSignatureAndMetadata): UtxoSignedTransaction {
-        return addSignatures(signatures.toList())
+    /**
+     * Adds a signature to the current [UtxoSignedTransactionImpl].
+     *
+     * @param signature The signature to be added to the [UtxoSignedTransactionImpl].
+     *
+     * @return Returns a new [UtxoSignedTransactionImpl] containing the new signature.
+     */
+    fun addSignature(signature: DigitalSignatureAndMetadata): UtxoSignedTransactionImpl =
+        UtxoSignedTransactionImpl(serializationService, transactionSignatureService,
+            wireTransaction, signatures + signature)
+
+    /**
+     * Crosschecks the missing signatures with the available keys and signs the transaction with their intersection
+     * if there are any. (Disabled until crypto support becomes available.)
+     *
+     * @return Returns the new [UtxoSignedTransaction] containing the applied signature and a
+     *          list of added signatures.
+     */
+    @Suspendable
+    fun addMissingSignatures(): Pair<UtxoSignedTransaction, List<DigitalSignatureAndMetadata>>{
+        TODO("Not implemented yet")
     }
 
     override fun getMissingSignatories(): Set<PublicKey> {
