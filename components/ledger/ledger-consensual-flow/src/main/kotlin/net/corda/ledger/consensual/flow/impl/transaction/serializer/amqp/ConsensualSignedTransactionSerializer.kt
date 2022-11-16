60435bac16c5ec54e75a58fac6db6f38e2dc3431
Lajos Veres <lajos.veres@r3.com>
2022-11-16 14:25:23 +0000

    CORE-8112 Update AMQP serializers to use directly the SignedTransactionInternal classes

--- components/ledger/ledger-consensual-flow/src/main/kotlin/net/corda/ledger/consensual/flow/impl/transaction/serializer/amqp/ConsensualSignedTransactionSerializer.kt
+++ components/ledger/ledger-consensual-flow/src/main/kotlin/net/corda/ledger/consensual/flow/impl/transaction/serializer/amqp/ConsensualSignedTransactionSerializer.kt
@@ -3,6 +3,7 @@ package net.corda.ledger.consensual.flow.impl.transaction.serializer.amqp
 import net.corda.ledger.common.data.transaction.WireTransaction
 import net.corda.ledger.common.flow.transaction.TransactionSignatureService
 import net.corda.ledger.consensual.flow.impl.transaction.ConsensualSignedTransactionImpl
+import net.corda.ledger.consensual.flow.impl.transaction.ConsensualSignedTransactionInternal
 import net.corda.sandbox.type.UsedByFlow
 import net.corda.sandbox.type.UsedByVerification
 import net.corda.serialization.BaseProxySerializer
@@ -11,7 +12,6 @@ import net.corda.v5.application.crypto.DigitalSignatureAndMetadata
 import net.corda.v5.application.serialization.SerializationService
 import net.corda.v5.base.annotations.CordaSerializable
 import net.corda.v5.base.exceptions.CordaRuntimeException
-import net.corda.v5.ledger.consensual.transaction.ConsensualSignedTransaction
 import org.osgi.service.component.annotations.Activate
 import org.osgi.service.component.annotations.Component
 import org.osgi.service.component.annotations.Reference
@@ -26,24 +26,24 @@ class ConsensualSignedTransactionSerializer @Activate constructor(
     private val serializationService: SerializationService,
     @Reference(service = TransactionSignatureService::class)
     private val transactionSignatureService: TransactionSignatureService
-) : BaseProxySerializer<ConsensualSignedTransaction, ConsensualSignedTransactionProxy>(),
+) : BaseProxySerializer<ConsensualSignedTransactionInternal, ConsensualSignedTransactionProxy>(),
     UsedByFlow, UsedByVerification {
 
-    override val type = ConsensualSignedTransaction::class.java
+    override val type = ConsensualSignedTransactionInternal::class.java
 
     override val proxyType = ConsensualSignedTransactionProxy::class.java
 
     override val withInheritance = true
 
-    override fun toProxy(obj: ConsensualSignedTransaction): ConsensualSignedTransactionProxy {
+    override fun toProxy(obj: ConsensualSignedTransactionInternal): ConsensualSignedTransactionProxy {
         return ConsensualSignedTransactionProxy(
             ConsensualSignedTransactionVersion.VERSION_1,
-            (obj as ConsensualSignedTransactionImpl).wireTransaction,
+            obj.wireTransaction,
             obj.signatures
         )
     }
 
-    override fun fromProxy(proxy: ConsensualSignedTransactionProxy): ConsensualSignedTransaction {
+    override fun fromProxy(proxy: ConsensualSignedTransactionProxy): ConsensualSignedTransactionInternal {
         if (proxy.version == ConsensualSignedTransactionVersion.VERSION_1) {
             return ConsensualSignedTransactionImpl(
                 serializationService,
