a23aa386a72ff31f8297ef9674b2c6b0d57c6d0d
Matthew Layton <matthew.layton@live.co.uk>
2022-11-16 11:15:32 +0000

    Refactored and simplified contract verification logic.

--- libs/ledger/ledger-utxo-data/src/main/kotlin/net/corda/ledger/utxo/data/transaction/ContractVerificationFailureReasonImpl.kt
+++ libs/ledger/ledger-utxo-data/src/main/kotlin/net/corda/ledger/utxo/data/transaction/ContractVerificationFailureReasonImpl.kt
@@ -1,18 +1,22 @@
 package net.corda.ledger.utxo.data.transaction
 
-import net.corda.v5.ledger.utxo.Command
-import net.corda.v5.ledger.utxo.ContractVerificationFailureReason
+import net.corda.v5.ledger.utxo.Contract
+import net.corda.v5.ledger.utxo.ContractVerificationFailure
 
 /**
- * Represents a contract verification failure reason.
+ * Represents a contract verification failure.
  *
- * @property command The contract [Command] that caused the verification failure.
- * @property cause The underlying cause of the verification failure.
+ * @property contractClassName The class name of the [Contract] that caused verification failure.
+ * @property contractStateClassNames The class names of the contract states that import the [Contract] that caused verification failure.
+ * @property exceptionClassName The class name of the [Exception] that caused verification failure.
+ * @property exceptionMessage The details of the [Exception] that caused verification failure.
  */
 data class ContractVerificationFailureReasonImpl(
-    override val command: Class<out Command>,
-    override val cause: Exception?,
-) : ContractVerificationFailureReason {
+    override val contractClassName: String,
+    override val contractStateClassNames: List<String>,
+    override val exceptionClassName: String,
+    override val exceptionMessage: String,
+) : ContractVerificationFailure {
 
     /**
      * Returns a string that represents the current object.
@@ -20,7 +24,6 @@ data class ContractVerificationFailureReasonImpl(
      * @return Returns a string that represents the current object.
      */
     override fun toString(): String {
-        val message = if (cause?.message.isNullOrBlank()) "An unknown exception occurred." else cause!!.message
-        return "${command.canonicalName}: $message."
+        return "$contractClassName: $exceptionMessage"
     }
 }
--- libs/ledger/ledger-utxo-data/src/main/kotlin/net/corda/ledger/utxo/data/transaction/UtxoLedgerTransactionVerifier.kt
+++ libs/ledger/ledger-utxo-data/src/main/kotlin/net/corda/ledger/utxo/data/transaction/UtxoLedgerTransactionVerifier.kt
@@ -1,9 +1,7 @@
 package net.corda.ledger.utxo.data.transaction
 
-import net.corda.v5.ledger.utxo.Command
-import net.corda.v5.ledger.utxo.Contract
 import net.corda.v5.ledger.utxo.ContractVerificationException
-import net.corda.v5.ledger.utxo.ContractVerificationFailureReason
+import net.corda.v5.ledger.utxo.ContractVerificationFailure
 import net.corda.v5.ledger.utxo.transaction.UtxoLedgerTransaction
 import kotlin.reflect.full.createInstance
 
@@ -15,93 +13,31 @@ import kotlin.reflect.full.createInstance
  */
 class UtxoLedgerTransactionVerifier(private val transaction: UtxoLedgerTransaction) {
 
-    private val failureReasons = mutableListOf<ContractVerificationFailureReason>()
+    private val failureReasons = mutableListOf<ContractVerificationFailure>()
 
-    /**
-     * Verifies the specified [UtxoLedgerTransaction].
-     *
-     * @throws ContractVerificationException if any of the commands fail to be verified successfully.
-     *
-     * The verification process is as follows:
-     *
-     * 1. Get all contract classes from the transaction input and output state and refs.
-     *
-     * For each command...
-     *
-     * 2. Recursively walk up the class tree until a [Contract] class is found.
-     * If no contract class is found, throw an exception and add it to the list of failure reasons.
-     *
-     * 3. Check that the command's contract class is assignable from one of the transaction contract classes.
-     * If the command's contract class is not assignable, throw an exception and add it to the list of failure reasons.
-     *
-     * 4. Instantiate the command's contract class using the primary parameterless constructor.
-     * If no parameterless constructor is found, an exception is thrown and added to the list of failure reasons.
-     *
-     * 5. Verify the contract using the specified [UtxoLedgerTransaction].
-     * If contract verification fails, an exception is thrown and added to the list of failure reasons.
-     *
-     * 6. If the list of failure reasons is not empty, throw a [ContractVerificationException] containing all failure reasons.
-     */
+
     fun verify() {
         val allTransactionStateAndRefs = transaction.inputStateAndRefs + transaction.outputStateAndRefs
-        val contractClassesForStateAndRefs = allTransactionStateAndRefs.map { it.state.contractType }.distinct()
+        val contractClassMap = allTransactionStateAndRefs.groupBy { it.state.contractType }
 
-        transaction.commands.forEach {
+        contractClassMap.forEach { (contractClass, contractStates) ->
             try {
-
-                val contractClassForCommand = getEnclosingContractClass(it.javaClass)
-
-                contractClassForCommand.checkIsAssignableFromAnyContractClass(contractClassesForStateAndRefs)
-
-                val contract = contractClassForCommand.kotlin.createInstance()
-
+                val contract = contractClass.kotlin.createInstance()
                 contract.verify(transaction)
-
             } catch (ex: Exception) {
-                failureReasons.add(ContractVerificationFailureReasonImpl(it.javaClass, ex))
+                failureReasons.add(
+                    ContractVerificationFailureReasonImpl(
+                        contractClassName = contractClass.canonicalName,
+                        contractStateClassNames = contractStates.map { it.state.contractState.javaClass.canonicalName },
+                        exceptionClassName = ex.javaClass.canonicalName,
+                        exceptionMessage = ex.message ?: "The thrown exception did not provide a failure message."
+                    )
+                )
             }
         }
 
         if (failureReasons.isNotEmpty()) {
             throw ContractVerificationException(transaction.id, failureReasons)
-        }
-    }
-
-    /**
-     * Gets the enclosing [Contract] class by recursively walking up the class tree where the [Command] is implemented.
-     *
-     * @param commandClass The command class from which to obtain a [Contract] class.
-     * @param currentClass The current class in the class tree, starting with the command class.
-     * @throws IllegalStateException if the current class is null.
-     *
-     * 1. If the current class is null, then we've exhausted the class tree and failed to find a [Contract] class.
-     * 2. If the current class is assignable to Contract, then we've found the enclosing contract class.
-     * 3. Recursively walk up the class tree by passing in the current class's enclosing class.
-     */
-    @Suppress("UNCHECKED_CAST")
-    private tailrec fun getEnclosingContractClass(
-        commandClass: Class<out Command>,
-        currentClass: Class<*>? = commandClass
-    ): Class<out Contract> {
-
-        checkNotNull(currentClass) {
-            "Failed to obtain a contract class for the specified command: ${commandClass.canonicalName}."
-        }
-
-        return if (Contract::class.java.isAssignableFrom(currentClass)) currentClass as Class<out Contract>
-        else getEnclosingContractClass(commandClass, currentClass.enclosingClass)
-    }
-
-    /**
-     * Checks that the current [Contract] class is assignable from any of the specified [Contract] classes.
-     *
-     * @receiver The [Contract] class to check whether it is assignable from any of the specified [Contract] classes.
-     * @param contractClasses The contract classes to check whether they are assignable from the current [Contract] class.
-     * @throws IllegalStateException if the current [Contract] class is not assignable from any of the specified [Contract] classes.
-     */
-    private fun Class<out Contract>.checkIsAssignableFromAnyContractClass(contractClasses: Iterable<Class<out Contract>>) {
-        check(contractClasses.any { it.isAssignableFrom(this) }) {
-            "The specified command contract class is not assignable from any of the transaction state contracts: ${this.canonicalName}."
         }
     }
 }
