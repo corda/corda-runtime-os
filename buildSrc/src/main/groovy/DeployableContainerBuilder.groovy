31e1464cd8d07302226cdab9d6d49c4c7a78c16f
Juan José Ramos <jujoramos@users.noreply.github.com>
2022-11-17 09:23:51 +0000

    CORE-7096: Configurable Docker Target Platform (#122)
    Add 'targetPlatform' property to 'DeployableContainerBuilder' so
    consumers can manually choose target platform for images built through
    the 'publishOSGiImage' gradle task.

--- buildSrc/src/main/groovy/DeployableContainerBuilder.groovy
+++ buildSrc/src/main/groovy/DeployableContainerBuilder.groovy
@@ -137,7 +137,13 @@ abstract class DeployableContainerBuilder extends DefaultTask {
 
     @Input
     final Property<Boolean> multiArch =
-            getObjects().property(Boolean).convention(true)           
+            getObjects().property(Boolean).convention(true)
+
+    @Input
+    @Optional
+    // Force Target Platform, using format "operatingSystem/architecture"
+    final Property<String> targetPlatform =
+            getObjects().property(String).convention('')
 
     DeployableContainerBuilder() {
         description = 'Creates a new "corda-dev" image with the file specified in "overrideFilePath".'
@@ -227,7 +233,11 @@ abstract class DeployableContainerBuilder extends DefaultTask {
         builder.addEnvironmentVariable('ENABLE_LOG4J2_DEBUG', 'false')
         builder.addEnvironmentVariable('CONSOLE_LOG_LEVEL', 'info')
 
-        if (System.getenv().containsKey("JENKINS_URL") && multiArch.get()) {
+        if (!targetPlatform.get().empty) {
+            logger.quiet("Forcing Jib to use ${targetPlatform.get()} platform")
+            String[] osArch = targetPlatform.get().split("/")
+            builder.setPlatforms(Set.of(new Platform(osArch[1], osArch[0])))
+        } else if (System.getenv().containsKey("JENKINS_URL") && multiArch.get()) {
             logger.quiet("Running on CI server - producing arm64 and amd64 images, property multiArch is ${multiArch.get()}")
             builder.addPlatform("arm64","linux")
         } else if (System.properties['os.arch'] == "aarch64") { 
