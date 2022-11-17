830467e13ce5f4770bcc9a24abac84b653cda9f6
LWogan <lorcan.wogan@r3.com>
2022-11-17 12:38:08 +0000

    CORE-8010 trigger error

--- applications/workers/workers-smoketest/src/smokeTest/kotlin/net/corda/applications/workers/smoketest/ConfigTestUtils.kt
+++ applications/workers/workers-smoketest/src/smokeTest/kotlin/net/corda/applications/workers/smoketest/ConfigTestUtils.kt
@@ -71,7 +71,7 @@ fun updateConfig(config: String, section: String) {
  * Wait for the REST API on the rpc-worker to respond with an updated config value.
  * If [expectServiceToBeDown] is set to true it is expected the config endpoint will go down before coming back up with the new config.
  */
-@Suppress("UNNECESSARY_SAFE_CALL")
+@Suppress("SENSELESS_COMPARISON")
 fun waitForConfigurationChange(section: String, key: String, value: String, expectServiceToBeDown: Boolean = true, timeout: Duration = Duration
     .ofMinutes(1)) {
     cluster {
