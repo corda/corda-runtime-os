8d8644f74ac41e0174c9de79cf4f4d53233e4af7
Yiftach Kaplan <67583323+yift-r3@users.noreply.github.com>
2022-11-21 12:18:40 +0000

    CORE-2368: Certificates read APIs (#2556)
    * CORE-2368: Certificate read APIs
    
    * Apply suggestions from code review
    
    Co-authored-by: Viktor Kolomeyko <viktor.kolomeyko@r3.com>
    
    * Add responseDescription
    
    * Bump version
    
    Co-authored-by: Viktor Kolomeyko <viktor.kolomeyko@r3.com>

--- components/membership/certificates-service-impl/src/test/kotlin/net/corda/membership/certificate/service/impl/CertificatesProcessorTest.kt
+++ components/membership/certificates-service-impl/src/test/kotlin/net/corda/membership/certificate/service/impl/CertificatesProcessorTest.kt
@@ -3,371 +3,205 @@ package net.corda.membership.certificate.service.impl
 import net.corda.data.certificates.CertificateUsage
 import net.corda.data.certificates.rpc.request.CertificateRpcRequest
 import net.corda.data.certificates.rpc.request.ImportCertificateRpcRequest
+import net.corda.data.certificates.rpc.request.ListCertificateAliasesRpcRequest
 import net.corda.data.certificates.rpc.request.RetrieveCertificateRpcRequest
 import net.corda.data.certificates.rpc.response.CertificateImportedRpcResponse
 import net.corda.data.certificates.rpc.response.CertificateRetrievalRpcResponse
 import net.corda.data.certificates.rpc.response.CertificateRpcResponse
-import net.corda.db.connection.manager.DbConnectionManager
-import net.corda.db.schema.CordaDb
-import net.corda.membership.certificates.CertificateUsageUtils.publicName
-import net.corda.membership.certificates.datamodel.Certificate
-import net.corda.membership.certificates.datamodel.ClusterCertificate
-import net.corda.orm.JpaEntitiesRegistry
-import net.corda.orm.JpaEntitiesSet
+import net.corda.data.certificates.rpc.response.ListCertificateAliasRpcResponse
+import net.corda.membership.certificate.client.DbCertificateClient
+import net.corda.v5.base.exceptions.CordaRuntimeException
 import net.corda.virtualnode.ShortHash
-import net.corda.virtualnode.VirtualNodeInfo
-import net.corda.virtualnode.read.VirtualNodeInfoReadService
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.mockito.kotlin.any
-import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.anyOrNull
 import org.mockito.kotlin.doThrow
-import org.mockito.kotlin.eq
 import org.mockito.kotlin.mock
-import org.mockito.kotlin.never
 import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
-import java.util.UUID
 import java.util.concurrent.CompletableFuture
-import javax.persistence.EntityManager
-import javax.persistence.EntityManagerFactory
-import javax.persistence.EntityTransaction
-import javax.persistence.TypedQuery
-import javax.persistence.criteria.CriteriaBuilder
-import javax.persistence.criteria.CriteriaQuery
-import javax.persistence.criteria.Order
-import javax.persistence.criteria.Path
-import javax.persistence.criteria.Predicate
-import javax.persistence.criteria.Root
 
 class CertificatesProcessorTest {
-    private val mockTransaction = mock<EntityTransaction>()
-    private val clusterEntityManager = mock<EntityManager> {
-        on { transaction } doReturn mockTransaction
-    }
-    private val clusterFactory = mock<EntityManagerFactory> {
-        on { createEntityManager() } doReturn clusterEntityManager
-    }
-    private val nodeEntityManager = mock<EntityManager> {
-        on { transaction } doReturn mockTransaction
-    }
-    private val nodeFactory = mock<EntityManagerFactory> {
-        on { createEntityManager() } doReturn nodeEntityManager
-    }
-    private val registry = mock<JpaEntitiesSet>()
-    private val jpaEntitiesRegistry = mock<JpaEntitiesRegistry> {
-        on { get(CordaDb.Vault.persistenceUnitName) } doReturn registry
-    }
-    private val nodeTenantId = ShortHash.of("1234567890ab")
-    private val dmlConnectionId = UUID(10, 30)
-    private val dbConnectionManager = mock<DbConnectionManager> {
-        on { getClusterEntityManagerFactory() } doReturn clusterFactory
-        on {
-            createEntityManagerFactory(
-                eq(dmlConnectionId),
-                eq(registry)
-            )
-        } doReturn nodeFactory
-    }
-    private val nodeInfo = mock<VirtualNodeInfo> {
-        on { vaultDmlConnectionId } doReturn dmlConnectionId
-    }
-    private val virtualNodeInfoReadService = mock<VirtualNodeInfoReadService> {
-        on { getByHoldingIdentityShortHash(nodeTenantId) } doReturn nodeInfo
-    }
-    private val response = CompletableFuture<CertificateRpcResponse>()
+    private val client = mock<DbCertificateClient>()
+    private val future = CompletableFuture<CertificateRpcResponse>()
 
-    private val processor = CertificatesProcessor(dbConnectionManager, jpaEntitiesRegistry, virtualNodeInfoReadService)
+    private val processor = CertificatesProcessor(client)
 
     @Test
-    fun `onNext merge the certificate into the P2P_TLS tenant`() {
+    fun `onNext will throw an exception for invalid holding identity`() {
+        val request = CertificateRpcRequest(
+            CertificateUsage.P2P_TLS,
+            "nop",
+            ListCertificateAliasesRpcRequest()
+        )
+
+        processor.onNext(request, future)
+
+        assertThat(future).isCompletedExceptionally
+    }
+
+    @Test
+    fun `onNext will call the client importCertificates if needed`() {
         val request = CertificateRpcRequest(
             CertificateUsage.P2P_TLS,
             null,
-            ImportCertificateRpcRequest("alias", "certificate")
-        )
-
-        processor.onNext(request, response)
-
-        verify(clusterEntityManager).merge(
-            ClusterCertificate("alias", CertificateUsage.P2P_TLS.publicName, "certificate")
-        )
-    }
-
-    @Test
-    fun `onNext merge the certificate into the RPC API tenant`() {
-        val request = CertificateRpcRequest(
-            CertificateUsage.RPC_API_TLS,
-            null,
-            ImportCertificateRpcRequest("alias", "certificate")
-        )
-
-        processor.onNext(request, response)
-
-        verify(clusterEntityManager).merge(ClusterCertificate(
-            "alias", CertificateUsage.RPC_API_TLS.publicName, "certificate")
-        )
-    }
-
-    @Test
-    fun `onNext with invalid node throw an exception`() {
-        val request = CertificateRpcRequest(
-            CertificateUsage.RPC_API_TLS,
-            "123456789011",
-            ImportCertificateRpcRequest("alias", "certificate")
-        )
-
-        processor.onNext(request, response)
-
-        assertThat(response).isCompletedExceptionally
-    }
-
-    @Test
-    fun `onNext find the certificate from the RPC API tenant`() {
-        val request = CertificateRpcRequest(
-            CertificateUsage.RPC_API_TLS,
-            null,
-            RetrieveCertificateRpcRequest("alias")
-        )
-
-        processor.onNext(request, response)
-
-        verify(clusterEntityManager).find(
-            ClusterCertificate::class.java,
-            "alias",
-        )
-    }
-
-    @Test
-    fun `onNext find the certificate from the P2P_TLS tenant`() {
-        val request = CertificateRpcRequest(
-            CertificateUsage.P2P_TLS,
-            null,
-            RetrieveCertificateRpcRequest("alias")
-        )
-
-        processor.onNext(request, response)
-
-        verify(clusterEntityManager).find(
-            ClusterCertificate::class.java,
-            "alias",
-        )
-    }
-
-    @Test
-    fun `onNext find the certificate from the node entity`() {
-        val request = CertificateRpcRequest(
-            CertificateUsage.P2P_SESSION,
-            nodeTenantId.value,
-            RetrieveCertificateRpcRequest("alias")
-        )
-
-        processor.onNext(request, response)
-
-        verify(nodeEntityManager).find(
-            Certificate::class.java,
-            "alias"
-        )
-    }
-
-    @Test
-    fun `onNext will close the node factory`() {
-        val request = CertificateRpcRequest(
-            CertificateUsage.P2P_SESSION,
-            nodeTenantId.value,
-            RetrieveCertificateRpcRequest("alias")
-        )
-
-        processor.onNext(request, response)
-
-        verify(nodeFactory).close()
-    }
-
-    @Test
-    fun `onNext will not close the node factory`() {
-        val request = CertificateRpcRequest(
-            CertificateUsage.P2P_TLS,
-            null,
-            RetrieveCertificateRpcRequest("alias")
-        )
-
-        processor.onNext(request, response)
-
-        verify(clusterFactory, never()).close()
-    }
-
-    @Test
-    fun `onNext merge the certificate into the node entity`() {
-        val request = CertificateRpcRequest(
-            CertificateUsage.RPC_API_TLS,
-            nodeTenantId.value,
-            ImportCertificateRpcRequest("alias", "certificate")
-        )
-
-        processor.onNext(request, response)
-
-        verify(nodeEntityManager).merge(
-            Certificate(
+            ImportCertificateRpcRequest(
                 "alias",
-                CertificateUsage.RPC_API_TLS.publicName,
                 "certificate",
             )
         )
+
+        processor.onNext(request, future)
+
+        verify(client).importCertificates(
+            CertificateUsage.P2P_TLS,
+            null,
+            "alias",
+            "certificate"
+        )
     }
 
     @Test
-    fun `onNext returns CertificateRetrievalRpcResponse without value`() {
+    fun `onNext will return CertificateImportedRpcResponse after import`() {
         val request = CertificateRpcRequest(
             CertificateUsage.P2P_TLS,
-            nodeTenantId.value,
-            RetrieveCertificateRpcRequest("alias")
+            null,
+            ImportCertificateRpcRequest(
+                "alias",
+                "certificate",
+            )
         )
 
-        processor.onNext(request, response)
+        processor.onNext(request, future)
 
-        assertThat(response).isCompletedWithValue(CertificateRpcResponse(CertificateRetrievalRpcResponse(null)))
+        assertThat(future).isCompletedWithValue(CertificateRpcResponse(CertificateImportedRpcResponse()))
     }
 
     @Test
-    fun `onNext returns CertificateRetrievalRpcResponse with value`() {
-        whenever(nodeEntityManager.find(Certificate::class.java, "alias"))
-            .doReturn(Certificate("alias", CertificateUsage.P2P_TLS.publicName, "certificate"))
+    fun `onNext will will complete exceptionally if client fails`() {
+        whenever(
+            client.importCertificates(
+                any(),
+                anyOrNull(),
+                any(),
+                any()
+            )
+        ).doThrow(CordaRuntimeException("Ooops"))
         val request = CertificateRpcRequest(
             CertificateUsage.P2P_TLS,
-            nodeTenantId.value,
-            RetrieveCertificateRpcRequest("alias")
+            null,
+            ImportCertificateRpcRequest(
+                "alias",
+                "certificate",
+            )
         )
 
-        processor.onNext(request, response)
+        processor.onNext(request, future)
 
-        assertThat(response).isCompletedWithValue(CertificateRpcResponse(CertificateRetrievalRpcResponse("certificate")))
+        assertThat(future).isCompletedExceptionally
     }
 
     @Test
-    fun `onNext returns null if the usage is wrong with value`() {
-        whenever(nodeEntityManager.find(Certificate::class.java, "alias"))
-            .doReturn(Certificate("alias", CertificateUsage.P2P_TLS.publicName, "certificate"))
+    fun `onNext will call the client retrieveCertificates if needed`() {
+        val request = CertificateRpcRequest(
+            CertificateUsage.P2P_TLS,
+            "123123123213",
+            RetrieveCertificateRpcRequest(
+                "alias"
+            )
+        )
+
+        processor.onNext(request, future)
+
+        verify(client).retrieveCertificates(
+            ShortHash.of("123123123213"),
+            CertificateUsage.P2P_TLS,
+            "alias",
+        )
+    }
+
+    @Test
+    fun `onNext will return CertificateRetrievalRpcResponse after get with the correct value`() {
+        whenever(
+            client.retrieveCertificates(
+                ShortHash.of("123123123213"),
+                CertificateUsage.CODE_SIGNER,
+                "alias",
+            )
+        ).thenReturn("certificate")
+        val request = CertificateRpcRequest(
+            CertificateUsage.CODE_SIGNER,
+            "123123123213",
+            RetrieveCertificateRpcRequest(
+                "alias"
+            )
+        )
+
+        processor.onNext(request, future)
+
+        assertThat(future).isCompletedWithValue(CertificateRpcResponse(CertificateRetrievalRpcResponse("certificate")))
+    }
+
+    @Test
+    fun `onNext will call the client getCertificateAliases if needed`() {
+        val request = CertificateRpcRequest(
+            CertificateUsage.P2P_TLS,
+            "123123123213",
+            ListCertificateAliasesRpcRequest()
+        )
+
+        processor.onNext(request, future)
+
+        verify(client).getCertificateAliases(
+            CertificateUsage.P2P_TLS,
+            ShortHash.of("123123123213"),
+        )
+    }
+
+    @Test
+    fun `onNext will return ListCertificateAliasRpcResponse after get with the correct value`() {
+        whenever(
+            client.getCertificateAliases(
+                CertificateUsage.P2P_SESSION,
+                ShortHash.of("123123123213"),
+            )
+        ).thenReturn(
+            listOf(
+                "one",
+                "two",
+            )
+        )
         val request = CertificateRpcRequest(
             CertificateUsage.P2P_SESSION,
-            nodeTenantId.value,
-            RetrieveCertificateRpcRequest("alias")
+            "123123123213",
+            ListCertificateAliasesRpcRequest()
         )
 
-        processor.onNext(request, response)
+        processor.onNext(request, future)
 
-        assertThat(response).isCompletedWithValue(CertificateRpcResponse(CertificateRetrievalRpcResponse(null)))
-    }
-
-    @Test
-    fun `readAllCertificates creates the correct query for retrieve all by usage`() {
-        val certificates = (1..5).map {
-            ClusterCertificate(
-                alias = "alias:$it",
-                usage = CertificateUsage.P2P_TLS.publicName,
-                rawCertificate = "certificate-$it"
+        assertThat(future).isCompletedWithValue(
+            CertificateRpcResponse(
+                ListCertificateAliasRpcResponse(
+                    listOf(
+                        "one",
+                        "two",
+                    )
+                )
             )
-        }
-        val usagePath = mock<Path<String>>()
-        val aliasPath = mock<Path<String>>()
-        val equalPredicate = mock<Predicate>()
-        val order = mock<Order>()
-        val root = mock<Root<ClusterCertificate>> {
-            on { get<String>("usage") } doReturn usagePath
-            on { get<String>("alias") } doReturn aliasPath
-        }
-        val query = mock<CriteriaQuery<ClusterCertificate>> {
-            on { from(ClusterCertificate::class.java) } doReturn root
-            on { select(root) } doReturn mock
-            on { where(equalPredicate) } doReturn mock
-            on { orderBy(order) } doReturn mock
-        }
-        val criteriaBuilder = mock<CriteriaBuilder> {
-            on { createQuery(ClusterCertificate::class.java) } doReturn query
-            on { equal(usagePath, CertificateUsage.P2P_TLS.publicName) } doReturn equalPredicate
-            on { asc(aliasPath) } doReturn order
-        }
-        val typedQuery = mock<TypedQuery<ClusterCertificate>> {
-            on { resultList } doReturn certificates
-        }
-        whenever(clusterEntityManager.criteriaBuilder).doReturn(criteriaBuilder)
-        whenever(clusterEntityManager.createQuery(query)).doReturn(typedQuery)
-
-        val rawCertificates = processor.useCertificateProcessor(
-            null,
-            CertificateUsage.P2P_TLS
-        ) {
-            it.readAllCertificates()
-        }
-
-        assertThat(rawCertificates).isEqualTo(
-            certificates.map { it.rawCertificate }
         )
     }
 
     @Test
-    fun `onNext will return CertificateImportedRpcResponse`() {
+    fun `onNext will return an error for an unexpected value`() {
         val request = CertificateRpcRequest(
-            CertificateUsage.RPC_API_TLS,
-            null,
-            ImportCertificateRpcRequest("alias", "certificate")
-        )
-
-        processor.onNext(request, response)
-
-        assertThat(response).isCompletedWithValue(CertificateRpcResponse(CertificateImportedRpcResponse()))
-    }
-
-    @Test
-    fun `onNext will throw an error if there are any`() {
-        whenever(clusterEntityManager.merge(any<ClusterCertificate>())).doThrow(RuntimeException("OOPs"))
-        val request = CertificateRpcRequest(
-            CertificateUsage.RPC_API_TLS,
-            null,
-            ImportCertificateRpcRequest("alias", "certificate")
-        )
-
-        processor.onNext(request, response)
-
-        assertThat(response).isCompletedExceptionally
-    }
-
-    @Test
-    fun `onNext will throw an error for unexpected request`() {
-        val request = CertificateRpcRequest(
-            CertificateUsage.RPC_API_TLS,
-            null,
+            CertificateUsage.P2P_SESSION,
+            "123123123213",
             null,
         )
 
-        processor.onNext(request, response)
+        processor.onNext(request, future)
 
-        assertThat(response).isCompletedExceptionally
-    }
-
-    @Test
-    fun `onNext throws exception for unknown node`() {
-        val request = CertificateRpcRequest(
-            CertificateUsage.RPC_API_TLS,
-            "Nop",
-            ImportCertificateRpcRequest("alias", "certificate")
-        )
-
-        processor.onNext(request, response)
-
-        assertThat(response).isCompletedExceptionally
-    }
-
-    @Test
-    fun `onNext throws exception for unknown tenant ID`() {
-        whenever(jpaEntitiesRegistry.get(CordaDb.Vault.persistenceUnitName)) doReturn null
-        val request = CertificateRpcRequest(
-            CertificateUsage.RPC_API_TLS,
-            nodeTenantId.value,
-            ImportCertificateRpcRequest("alias", "certificate")
-        )
-
-        processor.onNext(request, response)
-
-        assertThat(response).isCompletedExceptionally
+        assertThat(future).isCompletedExceptionally
     }
 }
--- components/membership/certificates-service/src/main/kotlin/net/corda/membership/certificate/service/CertificatesService.kt
+++ components/membership/certificates-service/src/main/kotlin/net/corda/membership/certificate/service/CertificatesService.kt
@@ -1,47 +1,8 @@
 package net.corda.membership.certificate.service
 
-import net.corda.data.certificates.CertificateUsage
 import net.corda.lifecycle.Lifecycle
-import net.corda.virtualnode.ShortHash
+import net.corda.membership.certificate.client.DbCertificateClient
 
 interface CertificatesService : Lifecycle {
-
-    /**
-     * Import certificate chain.
-     *
-     * @param usage The certificate usage
-     * @param holdingIdentityId The holding Identity ID. null for a cluster-level certificate.
-     * @param alias Unique alias of the certificate.
-     * @param certificates The certificates in PEM format
-     * @throws Exception in case of network or persistent error.
-     */
-    fun importCertificates(
-        usage: CertificateUsage,
-        holdingIdentityId: ShortHash?,
-        alias: String,
-        certificates: String,
-    )
-
-    /**
-     * Retrieve certificate.
-     *
-     * @param usage The certificate usage
-     * @param holdingIdentityId The holding Identity ID. null for a cluster-level certificate.
-     * @param alias Unique alias of the certificate
-     * @return Certificate in PEM format.
-     */
-    fun retrieveCertificates(
-        usage: CertificateUsage,
-        holdingIdentityId: ShortHash?,
-        alias: String,
-    ): String?
-
-    /**
-     * Retrieve all certificates for given tenant.
-     *
-     * @param usage The certificate usage
-     * @param holdingIdentityId The holding Identity ID. null for a cluster-level certificate.
-     * @return Certificates in PEM format.
-     */
-    fun retrieveAllCertificates(usage: CertificateUsage, holdingIdentityId: ShortHash?): List<String>
+    val client: DbCertificateClient
 }
