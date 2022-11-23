ee99b5f879ebfa1b21caf4af5aaa6552415a8753
Chris Rankin <chris.rankin@r3.com>
2022-11-23 16:17:31 +0000

    NOTICK: Restore SandboxGroupContextComponentImpl to the sandbox testkit. (#2606)

--- testing/sandboxes/src/main/kotlin/net/corda/testing/sandboxes/impl/CpkReadServiceImpl.kt
+++ testing/sandboxes/src/main/kotlin/net/corda/testing/sandboxes/impl/CpkReadServiceImpl.kt
@@ -24,6 +24,7 @@ import java.util.concurrent.ConcurrentHashMap
 
 @Suppress("unused")
 @Component(
+    name = CpiLoader.COMPONENT_NAME,
     service = [ CpkReadService::class, CpiLoader::class ],
     configurationPolicy = REQUIRE
 )
@@ -32,17 +33,12 @@ class CpkReadServiceImpl @Activate constructor(
     bundleContext: BundleContext,
     properties: Map<String, Any?>
 ) : CpkReadService, CpiLoader {
-    companion object {
-        const val BASE_DIRECTORY_KEY = "baseDirectory"
-        const val TEST_BUNDLE_KEY = "testBundle"
-    }
-
     private val logger = loggerFor<CpkReadService>()
 
-    private val cpkDir = (properties[BASE_DIRECTORY_KEY] as? String)?.let { Paths.get(it) }
+    private val cpkDir = (properties[CpiLoader.BASE_DIRECTORY_KEY] as? String)?.let { Paths.get(it) }
         ?: throw IllegalStateException("Base directory not configured")
 
-    private val testBundle = (properties[TEST_BUNDLE_KEY] as? String)?.let(bundleContext::getBundle)
+    private val testBundle = (properties[CpiLoader.TEST_BUNDLE_KEY] as? String)?.let(bundleContext::getBundle)
         ?: throw IllegalStateException("Test bundle not found")
 
     private val cpis = ConcurrentHashMap<CpiIdentifier, Cpi>()
@@ -50,7 +46,7 @@ class CpkReadServiceImpl @Activate constructor(
         get() = cpis.values.flatMap(Cpi::cpks)
 
     private val cpksMeta: Collection<CpkMetadata>
-        get() = cpks.map {it.metadata}
+        get() = cpks.map(Cpk::metadata)
 
     override val isRunning: Boolean get() = true
 
@@ -82,7 +78,7 @@ class CpkReadServiceImpl @Activate constructor(
     }
 
     override fun getAllCpiMetadata(): CompletableFuture<List<CpiMetadata>> {
-        val cpiList = cpis.values.map { it.metadata }
+        val cpiList = cpis.values.map(Cpi::metadata)
         return CompletableFuture.completedFuture(cpiList)
     }
 
@@ -93,9 +89,7 @@ class CpkReadServiceImpl @Activate constructor(
     }
 
     override fun get(cpkFileChecksum: SecureHash): Cpk? {
-        val cpk = cpks.firstOrNull { it.metadata.fileChecksum == cpkFileChecksum }
-
-        return cpk
+        return cpks.firstOrNull { it.metadata.fileChecksum == cpkFileChecksum }
     }
 
     override fun start() {
