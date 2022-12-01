8f500ae28fb9d90814ce2db9152b445ca3d52e72
Christian Sailer <christian.sailer@r3.com>
2022-12-01 16:05:14 +0000

    CORE-8431 Data access on signed transaction (#2636)
    Implemenation of the data accessors on UtxoSignedTransaction

--- components/ledger/ledger-utxo-flow/src/main/kotlin/net/corda/ledger/utxo/flow/impl/transaction/UtxoSignedTransactionImpl.kt
+++ components/ledger/ledger-utxo-flow/src/main/kotlin/net/corda/ledger/utxo/flow/impl/transaction/UtxoSignedTransactionImpl.kt
@@ -3,12 +3,19 @@ package net.corda.ledger.utxo.flow.impl.transaction
 import net.corda.ledger.common.data.transaction.WireTransaction
 import net.corda.ledger.common.flow.transaction.TransactionSignatureService
 import net.corda.ledger.utxo.data.transaction.UtxoLedgerTransactionImpl
+import net.corda.ledger.utxo.data.transaction.WrappedUtxoWireTransaction
 import net.corda.v5.application.crypto.DigitalSignatureAndMetadata
 import net.corda.v5.application.serialization.SerializationService
 import net.corda.v5.base.annotations.Suspendable
 import net.corda.v5.crypto.SecureHash
 import net.corda.v5.crypto.isFulfilledBy
+import net.corda.v5.ledger.common.Party
+import net.corda.v5.ledger.common.transaction.TransactionMetadata
 import net.corda.v5.ledger.common.transaction.TransactionVerificationException
+import net.corda.v5.ledger.utxo.Command
+import net.corda.v5.ledger.utxo.StateAndRef
+import net.corda.v5.ledger.utxo.StateRef
+import net.corda.v5.ledger.utxo.TimeWindow
 import net.corda.v5.ledger.utxo.transaction.UtxoLedgerTransaction
 import java.security.PublicKey
 import java.util.Objects
@@ -26,6 +33,8 @@ data class UtxoSignedTransactionImpl(
         // TODO(CORE-7237 Check WireTx's metadata's ledger type and allow only the matching ones.)
     }
 
+    private val wrappedWireTransaction = WrappedUtxoWireTransaction(wireTransaction, serializationService)
+
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (other !is UtxoSignedTransactionImpl) return false
@@ -40,6 +49,22 @@ data class UtxoSignedTransactionImpl(
     override fun hashCode(): Int = Objects.hash(wireTransaction, signatures)
 
     override val id: SecureHash get() = wireTransaction.id
+    override val inputStateRefs: List<StateRef>
+        get() = wrappedWireTransaction.inputStateRefs
+    override val metadata: TransactionMetadata
+        get() = wireTransaction.metadata
+    override val notary: Party
+        get() = wrappedWireTransaction.notary
+    override val outputStateAndRefs: List<StateAndRef<*>>
+        get() = wrappedWireTransaction.outputStateAndRefs
+    override val referenceStateRefs: List<StateRef>
+        get() = wrappedWireTransaction.referenceInputStateRefs
+    override val timeWindow: TimeWindow
+        get() = wrappedWireTransaction.timeWindow
+    override val signatories: List<PublicKey>
+        get() = wrappedWireTransaction.signatories
+    override val commands: List<Command>
+        get() = wrappedWireTransaction.commands
 
     @Suspendable
     override fun sign(publicKey: PublicKey): Pair<UtxoSignedTransactionInternal, DigitalSignatureAndMetadata> {
