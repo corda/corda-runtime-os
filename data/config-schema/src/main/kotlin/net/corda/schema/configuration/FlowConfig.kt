0dc85ab6b25d771a832892a9b765da9efd187acd
LWogan <lorcan.wogan@r3.com>
2022-12-02 13:55:37 +0000

    CORE-7353 flow mapper cleanup configs

--- data/config-schema/src/main/kotlin/net/corda/schema/configuration/FlowConfig.kt
+++ data/config-schema/src/main/kotlin/net/corda/schema/configuration/FlowConfig.kt
@@ -6,7 +6,9 @@ object FlowConfig {
     const val SESSION_MESSAGE_RESEND_WINDOW = "session.messageResendWindow"
     const val SESSION_HEARTBEAT_TIMEOUT_WINDOW = "session.heartbeatTimeout"
     const val SESSION_P2P_TTL = "session.p2pTTL"
+    const val SESSION_FLOW_CLEANUP_TIME = "session.cleanupTime"
     const val PROCESSING_MAX_RETRY_ATTEMPTS = "processing.maxRetryAttempts"
     const val PROCESSING_MAX_RETRY_DELAY = "processing.maxRetryDelay"
     const val PROCESSING_MAX_FLOW_SLEEP_DURATION = "processing.maxFlowSleepDuration"
+    const val PROCESSING_FLOW_CLEANUP_TIME = "processing.cleanupTime"
 }
