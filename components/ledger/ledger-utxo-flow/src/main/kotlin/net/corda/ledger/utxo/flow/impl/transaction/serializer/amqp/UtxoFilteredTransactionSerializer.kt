f724b7c4ca705cbcd74d15dad3547862e602a4a6
Christian Sailer <christian.sailer@r3.com>
2022-12-02 10:01:15 +0000

    First shot at AMQP custom serializer

---
+++ components/ledger/ledger-utxo-flow/src/main/kotlin/net/corda/ledger/utxo/flow/impl/transaction/serializer/amqp/UtxoFilteredTransactionSerializer.kt
@@ -0,0 +1,41 @@
+package net.corda.ledger.utxo.flow.impl.transaction.serializer.amqp
+
+import net.corda.ledger.common.flow.transaction.filtered.FilteredTransaction
+import net.corda.ledger.utxo.flow.impl.transaction.filtered.UtxoFilteredTransactionImpl
+import net.corda.sandbox.type.UsedByFlow
+import net.corda.sandbox.type.UsedByVerification
+import net.corda.serialization.BaseProxySerializer
+import net.corda.serialization.InternalCustomSerializer
+import net.corda.v5.application.serialization.SerializationService
+import org.osgi.service.component.annotations.Activate
+import org.osgi.service.component.annotations.Component
+import org.osgi.service.component.annotations.Reference
+import org.osgi.service.component.annotations.ServiceScope
+
+@Component(
+    service = [ InternalCustomSerializer::class, UsedByFlow::class, UsedByVerification::class ],
+    scope = ServiceScope.PROTOTYPE
+)
+class UtxoFilteredTransactionSerializer @Activate constructor(
+    @Reference(service = SerializationService::class)
+    private val serializationService: SerializationService,
+) : BaseProxySerializer<UtxoFilteredTransactionImpl, UtxoFilteredTransactionProxy>() {
+    override fun toProxy(obj: UtxoFilteredTransactionImpl): UtxoFilteredTransactionProxy {
+        return UtxoFilteredTransactionProxy(obj.filteredTransaction)
+    }
+
+    override fun fromProxy(proxy: UtxoFilteredTransactionProxy): UtxoFilteredTransactionImpl
+        = UtxoFilteredTransactionImpl(serializationService, proxy.filteredTransaction)
+
+    override val proxyType: Class<UtxoFilteredTransactionProxy>
+        get() = UtxoFilteredTransactionProxy::class.java
+    override val type: Class<UtxoFilteredTransactionImpl>
+        get() = UtxoFilteredTransactionImpl::class.java
+    override val withInheritance: Boolean
+        get() = false
+}
+
+
+data class UtxoFilteredTransactionProxy(
+    val filteredTransaction: FilteredTransaction
+)
