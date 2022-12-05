4aa4d7ec3573c5a4392a7468ccf7a6b897be9bbe
Lajos Veres <lajos.veres@r3.com>
2022-12-05 10:57:53 +0000

    Revert "Revert "Revert "CORE-7769 Disable Consensual smoke test temporarily to check if the timeout is enough without them"""
    This reverts commit 8517f39a90d48badf9548ee260338b8a8b7a2a02.

--- applications/workers/workers-smoketest/src/smokeTest/kotlin/net/corda/applications/workers/smoketest/ledger/ConsensualLedgerTests.kt
+++ applications/workers/workers-smoketest/src/smokeTest/kotlin/net/corda/applications/workers/smoketest/ledger/ConsensualLedgerTests.kt
@@ -14,7 +14,6 @@ import net.corda.applications.workers.smoketest.startRpcFlow
 import net.corda.v5.crypto.SecureHash
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeAll
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS
@@ -22,7 +21,6 @@ import java.util.UUID
 
 @Suppress("Unused", "FunctionName")
 @TestInstance(PER_CLASS)
-@Disabled
 class ConsensualLedgerTests {
 
     private companion object {
