c32934e18672c1f56fe08147b0cafb3f003c5eee
Viktor Kolomeyko <Viktor.Kolomeyko@r3.com>
2022-12-05 17:21:40 +0000

    CORE-8626: Fix query parameters encoding in permission strings
    Also extended E2E test to prove that fix works.

--- applications/workers/release/rpc-worker/src/e2eTest/kotlin/net/corda/applications/workers/rpc/CheckClusterRolesE2eTest.kt
+++ applications/workers/release/rpc-worker/src/e2eTest/kotlin/net/corda/applications/workers/rpc/CheckClusterRolesE2eTest.kt
@@ -3,6 +3,8 @@ package net.corda.applications.workers.rpc
 import net.corda.applications.workers.rpc.http.SkipWhenRpcEndpointUnavailable
 import net.corda.applications.workers.rpc.http.TestToolkitProperty
 import net.corda.libs.permissions.endpoints.v1.permission.PermissionEndpoint
+import net.corda.libs.permissions.endpoints.v1.permission.types.PermissionResponseType
+import net.corda.libs.permissions.endpoints.v1.permission.types.PermissionType
 import net.corda.libs.permissions.endpoints.v1.role.RoleEndpoint
 import net.corda.libs.permissions.endpoints.v1.role.types.RoleResponseType
 import org.assertj.core.api.Assertions.assertThat
@@ -21,6 +23,11 @@ class CheckClusterRolesE2eTest {
 
     @Test
     fun `test UserAdminRole content`() {
+
+        fun wildcardMatch(permissionString: String, permissionRequested: String): Boolean {
+            return permissionRequested.matches(permissionString.toRegex(RegexOption.IGNORE_CASE))
+        }
+
         testToolkit.httpClientFor(RoleEndpoint::class.java).use { roleClient ->
             val roleProxy = roleClient.start().proxy
 
@@ -31,9 +38,16 @@ class CheckClusterRolesE2eTest {
 
             testToolkit.httpClientFor(PermissionEndpoint::class.java).use { permClient ->
                 val permProxy = permClient.start().proxy
-                val permissions = userAdminRole.permissions.map { permProxy.getPermission(it.id) }
+                val permissions: List<PermissionResponseType> =
+                    userAdminRole.permissions.map { permProxy.getPermission(it.id) }
                 assertThat(permissions.size).withFailMessage("Permissions: $permissions").isEqualTo(14)
                 assertThat(permissions.map { it.permissionString }).contains("POST:/api/v1/user")
+                assertThat(permissions.filter { it.permissionType == PermissionType.ALLOW }).anyMatch {
+                    wildcardMatch(
+                        it.permissionString,
+                        "GET:/api/v1/user?loginname=RandomUser"
+                    )
+                }
             }
         }
     }
