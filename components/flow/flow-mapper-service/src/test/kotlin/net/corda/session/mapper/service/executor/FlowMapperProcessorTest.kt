1606556ce021e72719c02e4788d898b7c84efc6b
LWogan <lorcan.wogan@r3.com>
2022-12-06 14:21:08 +0000

    CORE-7353 adjust payload type

---
+++ components/flow/flow-mapper-service/src/test/kotlin/net/corda/session/mapper/service/executor/FlowMapperProcessorTest.kt
@@ -0,0 +1,17 @@
+package net.corda.session.mapper.service.executor
+
+import org.junit.jupiter.api.Test
+
+class FlowMapperProcessorTest {
+
+    @Test
+    fun `when state is closing new session events are not processed`() {
+
+    }
+
+    @Test
+    fun `when state is null new flow events are not processed`() {
+
+    }
+
+}
