1f54169c795348fbb53ef9a3ad24cb27beeac07d
Christian Sailer <christian.sailer@r3.com>
2022-12-06 17:36:30 +0000

    Complete integration test

--- components/ledger/ledger-utxo-flow/src/integrationTest/kotlin/net/corda/ledger/utxo/flow/impl/transaction/serializer/tests/UtxoFilteredTransactionAMQPSerializationTest.kt
+++ components/ledger/ledger-utxo-flow/src/integrationTest/kotlin/net/corda/ledger/utxo/flow/impl/transaction/serializer/tests/UtxoFilteredTransactionAMQPSerializationTest.kt
@@ -10,37 +10,42 @@ import net.corda.ledger.utxo.testkit.UtxoLedgerIntegrationTest
 import net.corda.sandboxgroupcontext.getSandboxSingletonService
 import net.corda.v5.crypto.SecureHash
 import net.corda.v5.ledger.utxo.StateRef
+import net.corda.v5.ledger.utxo.transaction.UtxoFilteredData
+import net.corda.v5.ledger.utxo.transaction.UtxoFilteredTransaction
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
 
-class UtxoFilteredTransactionAMQPSerializationTest: UtxoLedgerIntegrationTest() {
+class UtxoFilteredTransactionAMQPSerializationTest : UtxoLedgerIntegrationTest() {
 
     lateinit var filteredTransactionFactory: FilteredTransactionFactory
     lateinit var utxoFilteredTransactionFactory: UtxoFilteredTransactionFactory
 
-    companion object{
+    companion object {
         private val inputHash = SecureHash.parse("SHA256:1234567890abcdef")
     }
 
     @Test
-    fun `can serialize and deserialize utxo filtered transaction`(){
+    fun `can serialize and deserialize utxo filtered transaction`() {
         filteredTransactionFactory = sandboxGroupContext.getSandboxSingletonService()
 
-        val wireTx = wireTransactionFactory.createExample(jsonMarshallingService, jsonValidator, listOf(
-            emptyList(), // Notary
-            emptyList(), // Signatories
-            emptyList(), // output infos
-            emptyList(), // command infos
-            emptyList(), // attachments
-            listOf(
-                serializationService.serialize(StateRef(inputHash, 0)).bytes,
-                serializationService.serialize(StateRef(inputHash, 1)).bytes
-            ), // inputs
-            emptyList(), // references
-            emptyList(), // outputs
-            emptyList(), // commands
-        ))
-        val filteredTx = filteredTransactionFactory.create(wireTx,
+        val wireTx = wireTransactionFactory.createExample(
+            jsonMarshallingService, jsonValidator, listOf(
+                emptyList(), // Notary
+                emptyList(), // Signatories
+                emptyList(), // output infos
+                emptyList(), // command infos
+                emptyList(), // attachments
+                listOf(
+                    serializationService.serialize(StateRef(inputHash, 0)).bytes,
+                    serializationService.serialize(StateRef(inputHash, 1)).bytes
+                ), // inputs
+                emptyList(), // references
+                emptyList(), // outputs
+                emptyList(), // commands
+            )
+        )
+        val filteredTx = filteredTransactionFactory.create(
+            wireTx,
             listOf(
                 ComponentGroupFilterParameters.AuditProof(
                     UtxoComponentGroup.METADATA.ordinal,
@@ -52,13 +57,20 @@ class UtxoFilteredTransactionAMQPSerializationTest: UtxoLedgerIntegrationTest() 
 
         utxoFilteredTransactionFactory = sandboxGroupContext.getSandboxSingletonService()
         val utxoFilteredTx = utxoFilteredTransactionFactory.create(filteredTx)
-        Assertions.assertThat(utxoFilteredTx.id).isNotNull()
+        Assertions.assertThat(utxoFilteredTx.id).isNotNull
 
         val bytes = serializationService.serialize(utxoFilteredTx)
-        Assertions.assertThat(bytes).isNotNull()
-//        val deserialized = serializationService.deserialize(bytes, UtxoFilteredTransaction::class.java)
-//
-//        Assertions.assertThat(deserialized).isNotNull
-//        Assertions.assertThat(deserialized.id).isEqualTo(wireTx.id)
+        Assertions.assertThat(bytes).isNotNull
+        val deserialized = serializationService.deserialize(bytes, UtxoFilteredTransaction::class.java)
+
+        // check that the deserialized UtxoFilteredTransaction is fully functional
+        Assertions.assertThat(deserialized).isNotNull
+        Assertions.assertThat(deserialized.id).isEqualTo(wireTx.id)
+        Assertions.assertThat(deserialized.commands).isInstanceOf(UtxoFilteredData.Removed::class.java)
+        Assertions.assertThat(deserialized.inputStateRefs).isInstanceOf(UtxoFilteredData.Audit::class.java)
+        val inputs = deserialized.inputStateRefs as UtxoFilteredData.Audit<StateRef>
+        Assertions.assertThat(inputs.size).isEqualTo(2)
+        Assertions.assertThat(inputs.values.size).isEqualTo(2)
+        Assertions.assertThat(inputs.values.get(0)?.transactionHash).isEqualTo(inputHash)
     }
 }
