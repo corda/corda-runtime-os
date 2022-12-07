10ee857f165257afc8512ea1f7885928f9e25ef0
James Higgs <james.higgs@r3.com>
2022-12-07 17:17:09 +0000

    CORE-8691: Formatting changes

--- components/virtual-node/sandbox-group-context-service/src/main/kotlin/net/corda/sandboxgroupcontext/service/impl/SandboxGroupContextCacheImpl.kt
+++ components/virtual-node/sandbox-group-context-service/src/main/kotlin/net/corda/sandboxgroupcontext/service/impl/SandboxGroupContextCacheImpl.kt
@@ -9,27 +9,27 @@ import net.corda.sandboxgroupcontext.VirtualNodeContext
 import net.corda.v5.base.util.loggerFor
 import java.util.concurrent.ConcurrentHashMap
 
-internal class SandboxGroupContextCacheImpl(override val capacity: Long): SandboxGroupContextCache {
+internal class SandboxGroupContextCacheImpl(override val capacity: Long) : SandboxGroupContextCache {
     private companion object {
         private val logger = loggerFor<SandboxGroupContextCache>()
     }
 
     private val contexts = ConcurrentHashMap(SandboxGroupType.values().associate {
-            val cache: Cache<VirtualNodeContext, CloseableSandboxGroupContext> = CacheFactoryImpl().build(
-                "Sandbox-Cache-${it.name}",
-                Caffeine.newBuilder()
-                    .maximumSize(capacity)
-                    .removalListener { key, value, cause ->
-                        logger.info(
-                            "Evicting {} sandbox for: {} [{}]",
-                            key!!.sandboxGroupType,
-                            key.holdingIdentity.x500Name,
-                            cause.name
-                        )
-                        value?.close()
-                    })
-            Pair(it.name, cache)
-        })
+        val cache: Cache<VirtualNodeContext, CloseableSandboxGroupContext> = CacheFactoryImpl().build(
+            "Sandbox-Cache-${it.name}",
+            Caffeine.newBuilder()
+                .maximumSize(capacity)
+                .removalListener { key, value, cause ->
+                    logger.info(
+                        "Evicting {} sandbox for: {} [{}]",
+                        key!!.sandboxGroupType,
+                        key.holdingIdentity.x500Name,
+                        cause.name
+                    )
+                    value?.close()
+                })
+        Pair(it.name, cache)
+    })
 
     override fun remove(virtualNodeContext: VirtualNodeContext) {
         contexts.computeIfPresent(virtualNodeContext.sandboxGroupType.name) { _, cache ->
@@ -42,10 +42,13 @@ internal class SandboxGroupContextCacheImpl(override val capacity: Long): Sandbo
         virtualNodeContext: VirtualNodeContext,
         createFunction: (VirtualNodeContext) -> CloseableSandboxGroupContext
     ): SandboxGroupContext {
-        val cache = contexts[virtualNodeContext.sandboxGroupType.name] ?: throw IllegalArgumentException("Sandbox of type ${virtualNodeContext.sandboxGroupType.name} does not exist.")
+        val cache = contexts[virtualNodeContext.sandboxGroupType.name]
+            ?: throw IllegalArgumentException("Sandbox of type ${virtualNodeContext.sandboxGroupType.name} does not exist.")
         return cache.get(virtualNodeContext) {
-            logger.info("Caching {} sandbox for: {} (cache size: {})",
-                virtualNodeContext.sandboxGroupType, virtualNodeContext.holdingIdentity.x500Name, cache.estimatedSize())
+            logger.info(
+                "Caching {} sandbox for: {} (cache size: {})",
+                virtualNodeContext.sandboxGroupType, virtualNodeContext.holdingIdentity.x500Name, cache.estimatedSize()
+            )
             createFunction(virtualNodeContext)
         }
     }
