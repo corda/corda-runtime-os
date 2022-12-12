dcbac833e6c58894eadeb2ad2291de7839262fd1
Emily Bowe <emily.bowe@r3.com>
2022-12-12 14:12:03 +0000

    rm p2pLayerEndToEndTest, p2p-setup, p2p-layer-deployment

--- applications/tools/p2p-test/p2p-layer-deployment/src/main/kotlin/net/corda/p2p/deployment/GetEnv.kt
+++
@@ -1,5 +0,0 @@
-package net.corda.p2p.deployment
-
-fun getAndCheckEnv(variableName: String) =
-    System.getenv(variableName)
-        ?: throw DeploymentException("Please set the $variableName environment variable")
