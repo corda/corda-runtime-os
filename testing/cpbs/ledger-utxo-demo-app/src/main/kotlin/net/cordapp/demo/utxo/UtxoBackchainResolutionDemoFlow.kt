4ddea8c7b63edccd0de5100f9c413ba340ef4112
Dan Newton <dan.newton@r3.com>
2022-12-13 08:54:56 +0000

    CORE-8644 Integrate backchain resolution into finality

--- testing/cpbs/ledger-utxo-demo-app/src/main/kotlin/net/cordapp/demo/utxo/UtxoBackchainResolutionDemoFlow.kt
+++ testing/cpbs/ledger-utxo-demo-app/src/main/kotlin/net/cordapp/demo/utxo/UtxoBackchainResolutionDemoFlow.kt
@@ -15,14 +15,15 @@ import net.corda.v5.application.messaging.receive
 import net.corda.v5.base.annotations.Suspendable
 import net.corda.v5.base.types.MemberX500Name
 import net.corda.v5.base.util.contextLogger
+import net.corda.v5.crypto.SecureHash
 import net.corda.v5.ledger.common.Party
 import net.corda.v5.ledger.utxo.BelongsToContract
+import net.corda.v5.ledger.utxo.Command
 import net.corda.v5.ledger.utxo.Contract
 import net.corda.v5.ledger.utxo.ContractState
 import net.corda.v5.ledger.utxo.StateRef
 import net.corda.v5.ledger.utxo.UtxoLedgerService
 import net.corda.v5.ledger.utxo.transaction.UtxoLedgerTransaction
-import net.corda.v5.ledger.utxo.transaction.UtxoSignedTransaction
 import java.security.PublicKey
 import java.time.Instant
 import java.time.temporal.ChronoUnit
@@ -43,6 +44,8 @@ class UtxoBackchainResolutionDemoFlow : RPCStartableFlow {
 
         }
     }
+
+    class TestCommand : Command
 
     private companion object {
         val log = contextLogger()
@@ -70,9 +73,14 @@ class UtxoBackchainResolutionDemoFlow : RPCStartableFlow {
             val myInfo = memberLookup.myInfo()
             val members = request.members.map { memberLookup.lookup(MemberX500Name.parse(it))!! }
 
+//            val testState = TestState(
+//                request.input,
+//                members.map { it.ledgerKeys.first() } + myInfo.ledgerKeys.first()
+//            )
+
             val testState = TestState(
                 request.input,
-                members.map { it.ledgerKeys.first() } + myInfo.ledgerKeys.first()
+                members.map { it.ledgerKeys.first() }
             )
 
             val sessions = members.map { flowMessaging.initiateFlow(it.name) }
@@ -81,6 +89,8 @@ class UtxoBackchainResolutionDemoFlow : RPCStartableFlow {
             val signedTransaction = txBuilder
                 .setNotary(Party(members.first().name, members.first().ledgerKeys.first()))
                 .setTimeWindowUntil(Instant.now().plus(10, ChronoUnit.DAYS))
+                .addCommand(TestCommand())
+                .addSignatories(listOf(myInfo.ledgerKeys.first()))
                 .addOutputState(testState)
                 .addOutputState(testState)
                 .addOutputState(testState)
@@ -91,6 +101,8 @@ class UtxoBackchainResolutionDemoFlow : RPCStartableFlow {
             val tx2 = utxoLedgerService.getTransactionBuilder()
                 .setNotary(Party(members.first().name, members.first().ledgerKeys.first()))
                 .setTimeWindowUntil(Instant.now().plus(10, ChronoUnit.DAYS))
+                .addCommand(TestCommand())
+                .addSignatories(listOf(myInfo.ledgerKeys.first()))
                 .addInputState(StateRef(signedTransaction.id, 0))
                 .addInputState(StateRef(signedTransaction.id, 1))
                 .addOutputState(testState)
@@ -99,6 +111,8 @@ class UtxoBackchainResolutionDemoFlow : RPCStartableFlow {
             val tx3 = utxoLedgerService.getTransactionBuilder()
                 .setNotary(Party(members.first().name, members.first().ledgerKeys.first()))
                 .setTimeWindowUntil(Instant.now().plus(10, ChronoUnit.DAYS))
+                .addCommand(TestCommand())
+                .addSignatories(listOf(myInfo.ledgerKeys.first()))
                 .addInputState(StateRef(signedTransaction.id, 2))
                 .addInputState(StateRef(signedTransaction.id, 3))
                 .addOutputState(testState)
@@ -107,6 +121,8 @@ class UtxoBackchainResolutionDemoFlow : RPCStartableFlow {
             val tx4 = utxoLedgerService.getTransactionBuilder()
                 .setNotary(Party(members.first().name, members.first().ledgerKeys.first()))
                 .setTimeWindowUntil(Instant.now().plus(10, ChronoUnit.DAYS))
+                .addCommand(TestCommand())
+                .addSignatories(listOf(myInfo.ledgerKeys.first()))
                 .addInputState(StateRef(signedTransaction.id, 4))
                 .addOutputState(testState)
                 .toSignedTransaction(myInfo.ledgerKeys.first())
@@ -114,6 +130,8 @@ class UtxoBackchainResolutionDemoFlow : RPCStartableFlow {
             val tx5 = utxoLedgerService.getTransactionBuilder()
                 .setNotary(Party(members.first().name, members.first().ledgerKeys.first()))
                 .setTimeWindowUntil(Instant.now().plus(10, ChronoUnit.DAYS))
+                .addCommand(TestCommand())
+                .addSignatories(listOf(myInfo.ledgerKeys.first()))
                 .addInputState(StateRef(tx2.id, 1))
                 .addOutputState(testState)
                 .toSignedTransaction(myInfo.ledgerKeys.first())
@@ -121,6 +139,8 @@ class UtxoBackchainResolutionDemoFlow : RPCStartableFlow {
             val tx6 = utxoLedgerService.getTransactionBuilder()
                 .setNotary(Party(members.first().name, members.first().ledgerKeys.first()))
                 .setTimeWindowUntil(Instant.now().plus(10, ChronoUnit.DAYS))
+                .addCommand(TestCommand())
+                .addSignatories(testState.participants)
                 .addInputState(StateRef(tx4.id, 0))
                 .addInputState(StateRef(tx5.id, 0))
                 .addOutputState(testState)
@@ -129,6 +149,8 @@ class UtxoBackchainResolutionDemoFlow : RPCStartableFlow {
             val tx7 = utxoLedgerService.getTransactionBuilder()
                 .setNotary(Party(members.first().name, members.first().ledgerKeys.first()))
                 .setTimeWindowUntil(Instant.now().plus(10, ChronoUnit.DAYS))
+                .addCommand(TestCommand())
+                .addSignatories(listOf(myInfo.ledgerKeys.first()))
                 .addInputState(StateRef(tx3.id, 0))
                 .addOutputState(testState)
                 .toSignedTransaction(myInfo.ledgerKeys.first())
@@ -136,6 +158,8 @@ class UtxoBackchainResolutionDemoFlow : RPCStartableFlow {
             val tx8 = utxoLedgerService.getTransactionBuilder()
                 .setNotary(Party(members.first().name, members.first().ledgerKeys.first()))
                 .setTimeWindowUntil(Instant.now().plus(10, ChronoUnit.DAYS))
+                .addCommand(TestCommand())
+                .addSignatories(listOf(myInfo.ledgerKeys.first()))
                 .addInputState(StateRef(tx6.id, 0))
                 .addInputState(StateRef(tx7.id, 0))
                 .addOutputState(testState)
@@ -145,6 +169,8 @@ class UtxoBackchainResolutionDemoFlow : RPCStartableFlow {
             val tx9 = utxoLedgerService.getTransactionBuilder()
                 .setNotary(Party(members.first().name, members.first().ledgerKeys.first()))
                 .setTimeWindowUntil(Instant.now().plus(10, ChronoUnit.DAYS))
+                .addCommand(TestCommand())
+                .addSignatories(listOf(myInfo.ledgerKeys.first()))
                 .addInputState(StateRef(tx8.id, 0))
                 .addOutputState(testState)
                 .toSignedTransaction(myInfo.ledgerKeys.first())
@@ -152,6 +178,8 @@ class UtxoBackchainResolutionDemoFlow : RPCStartableFlow {
             val tx10 = utxoLedgerService.getTransactionBuilder()
                 .setNotary(Party(members.first().name, members.first().ledgerKeys.first()))
                 .setTimeWindowUntil(Instant.now().plus(10, ChronoUnit.DAYS))
+                .addCommand(TestCommand())
+                .addSignatories(listOf(myInfo.ledgerKeys.first()))
                 .addInputState(StateRef(tx8.id, 1))
                 .addOutputState(testState)
                 .toSignedTransaction(myInfo.ledgerKeys.first())
@@ -159,6 +187,8 @@ class UtxoBackchainResolutionDemoFlow : RPCStartableFlow {
             val tx11 = utxoLedgerService.getTransactionBuilder()
                 .setNotary(Party(members.first().name, members.first().ledgerKeys.first()))
                 .setTimeWindowUntil(Instant.now().plus(10, ChronoUnit.DAYS))
+                .addCommand(TestCommand())
+                .addSignatories(testState.participants)
                 .addInputState(StateRef(tx9.id, 0))
                 .addInputState(StateRef(tx10.id, 0))
                 .addOutputState(testState)
@@ -178,64 +208,23 @@ class UtxoBackchainResolutionDemoFlow : RPCStartableFlow {
              */
 
             for (session in sessions) {
-                session.send(listOf(signedTransaction, tx2, tx3, tx4, tx5, tx6, tx7, tx8, tx9, tx10, tx11))
+                session.send(listOf(signedTransaction.id, tx2.id, tx3.id, tx4.id, tx5.id, tx6.id, tx7.id, tx8.id, tx9.id, tx10.id, tx11.id))
             }
 
-            log.info("RESOLVING TX BACKCHAIN original_id=${signedTransaction.id} new_tx_id=${tx6.id}")
+            utxoLedgerService.finalize(signedTransaction, emptyList())
+            utxoLedgerService.finalize(tx2, emptyList())
+            utxoLedgerService.finalize(tx3, emptyList())
+            utxoLedgerService.finalize(tx4, emptyList())
+            utxoLedgerService.finalize(tx5, emptyList())
 
-            utxoLedgerService.resolveBackchain(tx6, sessions.first())
+            utxoLedgerService.finalize(tx6, sessions)
 
-            utxoLedgerService.persistTransaction(tx6)
+            utxoLedgerService.finalize(tx7, emptyList())
+            utxoLedgerService.finalize(tx8, emptyList())
+            utxoLedgerService.finalize(tx9, emptyList())
+            utxoLedgerService.finalize(tx10, emptyList())
 
-            log.info("RESOLVED TX6 BACKCHAIN")
-
-            var a = utxoLedgerService.findSignedTransaction(signedTransaction.id)
-            var b = utxoLedgerService.findSignedTransaction(tx2.id)
-            var c = utxoLedgerService.findSignedTransaction(tx3.id)
-            var d = utxoLedgerService.findSignedTransaction(tx4.id)
-            var e = utxoLedgerService.findSignedTransaction(tx5.id)
-            var f = utxoLedgerService.findSignedTransaction(tx6.id)
-            var g = utxoLedgerService.findSignedTransaction(tx7.id)
-            var h = utxoLedgerService.findSignedTransaction(tx8.id)
-            log.info("Existence of tx1 = $a")
-            log.info("Existence of tx2 = $b")
-            log.info("Existence of tx3 = $c")
-            log.info("Existence of tx4 = $d")
-            log.info("Existence of tx5 = $e")
-            log.info("Existence of tx6 = $f")
-            log.info("Existence of tx7 = $g")
-            log.info("Existence of tx8 = $h")
-
-            utxoLedgerService.resolveBackchain(tx11, sessions.first())
-
-            utxoLedgerService.persistTransaction(tx11)
-
-            log.info("RESOLVED TX11 BACKCHAIN")
-
-            a = utxoLedgerService.findSignedTransaction(signedTransaction.id)
-            b = utxoLedgerService.findSignedTransaction(tx2.id)
-            c = utxoLedgerService.findSignedTransaction(tx3.id)
-            d = utxoLedgerService.findSignedTransaction(tx4.id)
-            e = utxoLedgerService.findSignedTransaction(tx5.id)
-            f = utxoLedgerService.findSignedTransaction(tx6.id)
-            g = utxoLedgerService.findSignedTransaction(tx7.id)
-            h = utxoLedgerService.findSignedTransaction(tx8.id)
-            val i = utxoLedgerService.findSignedTransaction(tx9.id)
-            val j = utxoLedgerService.findSignedTransaction(tx10.id)
-            val k = utxoLedgerService.findSignedTransaction(tx11.id)
-
-            log.info("Existence of tx1 (2) = $a")
-            log.info("Existence of tx2 (2) = $b")
-            log.info("Existence of tx3 (2) = $c")
-            log.info("Existence of tx4 (2) = $d")
-            log.info("Existence of tx5 (2) = $e")
-            log.info("Existence of tx6 (2) = $f")
-            log.info("Existence of tx7 (2) = $g")
-            log.info("Existence of tx8 (2) = $h")
-            log.info("Existence of tx9 (2) = $i")
-            log.info("Existence of tx10 (2) = $j")
-            log.info("Existence of tx11 (2) = $k")
-
+            utxoLedgerService.finalize(tx11, sessions)
             return "SUCCESS"
         } catch (e: Exception) {
             log.warn("Failed to process consensual flow for request body '$requestBody' because:'${e.message}'")
@@ -254,20 +243,32 @@ class UtxoBackchainResolutionDemoResponderFlow : ResponderFlow {
     @CordaInject
     lateinit var utxoLedgerService: UtxoLedgerService
 
+
     @Suppress("DEPRECATION")
     @Suspendable
     override fun call(session: FlowSession) {
-        val txs = session.receive<List<UtxoSignedTransaction>>()
-        log.info("RECEIVED TXs ON PEER")
-        for (tx in txs) {
-            utxoLedgerService.persistTransaction(tx)
-        }
-        log.info("RESOLVING ON PEER")
-        utxoLedgerService.sendBackchain(session)
-        log.info("FINISHED SENDING BACKCHAIN")
+        val txs = session.receive<List<SecureHash>>()
+        txs.map { utxoLedgerService.findSignedTransaction(it) }
+            .forEachIndexed { index, tx ->
+                log.info("PEER TX${index+1} = ${tx?.id}")
+            }
 
-        log.info("RESOLVING ON PEER (2)")
-        utxoLedgerService.sendBackchain(session)
-        log.info("FINISHED SENDING BACKCHAIN (2)")
+        utxoLedgerService.receiveFinality(session) {
+            log.info("Received finality - ${it.id}")
+        }
+
+        txs.map { utxoLedgerService.findSignedTransaction(it) }
+            .forEachIndexed { index, tx ->
+                log.info("PEER TX${index+1} = ${tx?.id}")
+            }
+
+        utxoLedgerService.receiveFinality(session) {
+            log.info("Received finality (2) - ${it.id}")
+        }
+
+        txs.map { utxoLedgerService.findSignedTransaction(it) }
+            .forEachIndexed { index, tx ->
+                log.info("PEER TX${index+1} = ${tx?.id}")
+            }
     }
 }
