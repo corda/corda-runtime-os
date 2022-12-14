3f842575ac54b0b0284e472952acc2827fa4ede4
nkovacsx <nandor.kovacs@r3.com>
2022-12-14 17:40:10 +0000

    fix test

--- components/ledger/notary-worker-selection-impl/src/test/kotlin/net/corda/ledger/notary/worker/selection/impl/NotaryVirtualNodeSelectorServiceImplTest.kt
+++ components/ledger/notary-worker-selection-impl/src/test/kotlin/net/corda/ledger/notary/worker/selection/impl/NotaryVirtualNodeSelectorServiceImplTest.kt
@@ -1,11 +1,14 @@
 package net.corda.ledger.notary.worker.selection.impl
 
-import net.corda.membership.read.NotaryVirtualNodeLookup
+import net.corda.membership.lib.MemberInfoExtension
+import net.corda.membership.lib.notary.MemberNotaryDetails
+import net.corda.v5.application.membership.MemberLookup
 import net.corda.v5.base.types.MemberX500Name
 import net.corda.v5.ledger.common.Party
+import net.corda.v5.membership.MGMContext
+import net.corda.v5.membership.MemberContext
 import net.corda.v5.membership.MemberInfo
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.eq
@@ -15,41 +18,48 @@ class NotaryVirtualNodeSelectorServiceImplTest {
 
     private companion object {
         const val SELECTION_COUNT = 100
-        val NOTARY_WORKER_1 = mockNotaryVirtualNode("O=notary1, L=London, C=GB")
-        val NOTARY_WORKER_2 = mockNotaryVirtualNode("O=notary2, L=London, C=GB")
-        val NOTARY_WORKER_3 = mockNotaryVirtualNode("O=notary3, L=London, C=GB")
-        val NOTARY_WORKER_4 = mockNotaryVirtualNode("O=notary4, L=London, C=GB")
-
-        fun mockNotaryVirtualNode(memberName: String): MemberInfo {
-            return mock {
-                on { name } doReturn MemberX500Name.parse(memberName)
-                on { sessionInitiationKey } doReturn mock()
-            }
-        }
-    }
-
-    private lateinit var firstNotaryServiceIdentity: Party
-    private lateinit var secondNotaryServiceIdentity: Party
-    private lateinit var virtualNodeLookup: NotaryVirtualNodeLookup
-
-    @BeforeEach
-    fun setup() {
-        firstNotaryServiceIdentity = Party(
+        private val firstNotaryServiceIdentity = Party(
             MemberX500Name.parse("O=MyNotaryService, L=London, C=GB"),
             mock()
         )
 
-        secondNotaryServiceIdentity = Party(
+        private val secondNotaryServiceIdentity = Party(
             MemberX500Name.Companion.parse("O=MySecondNotaryService, L=London, C=GB"),
             mock()
         )
 
-        virtualNodeLookup = mock {
-            on { getNotaryVirtualNodes(eq(firstNotaryServiceIdentity.name)) } doReturn
-                    listOf(NOTARY_WORKER_1, NOTARY_WORKER_3)
+        val NOTARY_WORKER_1 = mockNotaryVirtualNode("O=notary1, L=London, C=GB", firstNotaryServiceIdentity)
+        val NOTARY_WORKER_2 = mockNotaryVirtualNode("O=notary2, L=London, C=GB", secondNotaryServiceIdentity)
+        val NOTARY_WORKER_3 = mockNotaryVirtualNode("O=notary3, L=London, C=GB", firstNotaryServiceIdentity)
+        val NOTARY_WORKER_4 = mockNotaryVirtualNode("O=notary4, L=London, C=GB", secondNotaryServiceIdentity)
 
-            on { getNotaryVirtualNodes(eq(secondNotaryServiceIdentity.name)) } doReturn
-                    listOf(NOTARY_WORKER_2, NOTARY_WORKER_4)
+        private val memberLookup = mock<MemberLookup> {
+            on { lookup() } doReturn
+                    listOf(NOTARY_WORKER_1, NOTARY_WORKER_2, NOTARY_WORKER_3, NOTARY_WORKER_4)
+        }
+
+        private val selector = NotaryVirtualNodeSelectorServiceImpl(memberLookup)
+
+        fun mockNotaryVirtualNode(memberName: String, notary: Party): MemberInfo {
+            val mockNotaryDetails = MemberNotaryDetails(
+                notary.name,
+                null,
+                emptyList()
+            )
+            val mockMemberContext: MemberContext = mock {
+                on { entries } doReturn mapOf(
+                    "${MemberInfoExtension.ROLES_PREFIX}.0" to MemberInfoExtension.NOTARY_ROLE
+                ).entries
+                on { parse(eq("corda.notary"), eq(MemberNotaryDetails::class.java)) } doReturn mockNotaryDetails
+            }
+            val mgmContext: MGMContext = mock()
+
+            return mock {
+                on { name } doReturn MemberX500Name.parse(memberName)
+                on { sessionInitiationKey } doReturn mock()
+                on { memberProvidedContext } doReturn mockMemberContext
+                on { mgmProvidedContext } doReturn mgmContext
+            }
         }
     }
 
@@ -57,8 +67,6 @@ class NotaryVirtualNodeSelectorServiceImplTest {
     fun `select notary virtual nodes randomly`() {
         // It's hard to test that it is actually random so we'll just check the distribution of selected elements to
         // make sure none of the elements is chosen exclusively
-        val selector = NotaryVirtualNodeSelectorServiceImpl(virtualNodeLookup)
-
         val selectedVirtualNodes = mutableMapOf<Party, Int>()
 
         (1..SELECTION_COUNT).forEach { _ ->
@@ -73,8 +81,6 @@ class NotaryVirtualNodeSelectorServiceImplTest {
 
     @Test
     fun `only select virtual nodes that belong to the given service`() {
-        val selector = NotaryVirtualNodeSelectorServiceImpl(virtualNodeLookup)
-
         val selectedVirtualNodesForFirstService = mutableMapOf<Party, Int>()
         val selectedVirtualNodesForSecondService = mutableMapOf<Party, Int>()
 
