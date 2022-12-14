1cf6ee69408b66c3c20e5876c0e3b2e0a771b2d5
Miljenko Brkic <miljenko.brkic@r3.com>
2022-12-14 10:28:20 +0000

    CORE-8843 Fixed calculation of relevant states

--- components/ledger/ledger-utxo-flow/src/main/kotlin/net/corda/ledger/utxo/flow/impl/flows/finality/FinalityFlowUtils.kt
+++ components/ledger/ledger-utxo-flow/src/main/kotlin/net/corda/ledger/utxo/flow/impl/flows/finality/FinalityFlowUtils.kt
@@ -2,24 +2,16 @@ package net.corda.ledger.utxo.flow.impl.flows.finality
 
 import net.corda.ledger.utxo.flow.impl.transaction.UtxoSignedTransactionInternal
 import net.corda.v5.application.membership.MemberLookup
-import net.corda.v5.ledger.utxo.Contract
 import java.security.PublicKey
 
-internal fun UtxoSignedTransactionInternal.getContracts(): Set<Contract> {
-    val transaction = this.toLedgerTransaction()
-    val allTransactionStateAndRefs = transaction.inputStateAndRefs + transaction.outputStateAndRefs
-    return allTransactionStateAndRefs
-        .mapTo(HashSet()) { it.state.contractType.getConstructor().newInstance() }
-}
-
-internal fun UtxoSignedTransactionInternal.getRelevantStatesIndexes(keys: Set<PublicKey>): List<Int> {
-    val contracts = this.getContracts()
+fun UtxoSignedTransactionInternal.getRelevantStatesIndexes(keys: Set<PublicKey>): List<Int> {
     return this.outputStateAndRefs.withIndex().filter { (_, stateAndRef) ->
-        contracts.any { it.isRelevant(stateAndRef.state.contractState, keys) }
+        val contract = stateAndRef.state.contractType.getConstructor().newInstance()
+        contract.isRelevant(stateAndRef.state.contractState, keys)
     }.map { it.index }
 }
 
-internal fun MemberLookup.getMyLedgerKeys() =
+fun MemberLookup.getMyLedgerKeys() =
     this.myInfo()
         .ledgerKeys
         .toSet()
