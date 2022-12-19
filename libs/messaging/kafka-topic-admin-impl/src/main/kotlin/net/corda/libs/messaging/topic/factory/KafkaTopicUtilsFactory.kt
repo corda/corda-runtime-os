dfe6b4f3f946db16e1e7424ad3930fd93f0bbbba
Will Vigor <william.vigor@r3.com>
2022-12-19 17:22:01 +0000

    CORE-8967 Fix-App-Simulator-After-ACL-Changes
    Change App simulator helm charts to support Simple Authentication
    and Secruity Layer (SASL) in addition to TLS. I also fixed the
    topic creation code so it can find the correct ScramLoginModule
    using the classloader from the bundle. This was previously done for
    kafka producers and consumers in CORE-5056.

--- libs/messaging/kafka-topic-admin-impl/src/main/kotlin/net/corda/libs/messaging/topic/factory/KafkaTopicUtilsFactory.kt
+++ libs/messaging/kafka-topic-admin-impl/src/main/kotlin/net/corda/libs/messaging/topic/factory/KafkaTopicUtilsFactory.kt
@@ -3,7 +3,9 @@ package net.corda.libs.messaging.topic.factory
 import net.corda.libs.messaging.topic.KafkaTopicUtils
 import net.corda.libs.messaging.topic.utils.TopicUtils
 import net.corda.libs.messaging.topic.utils.factory.TopicUtilsFactory
+import net.corda.messagebus.kafka.utils.OsgiDelegatedClassLoader
 import org.apache.kafka.clients.admin.AdminClient
+import org.osgi.framework.FrameworkUtil
 import org.osgi.service.component.annotations.Component
 import java.util.*
 
@@ -16,6 +18,14 @@ import java.util.*
 class KafkaTopicUtilsFactory : TopicUtilsFactory {
 
     override fun createTopicUtils(props: Properties): TopicUtils {
-        return KafkaTopicUtils(AdminClient.create(props))
+        val contextClassLoader = Thread.currentThread().contextClassLoader
+        val currentBundle = FrameworkUtil.getBundle(KafkaTopicUtils::class.java)
+
+        return try {
+            Thread.currentThread().contextClassLoader = OsgiDelegatedClassLoader(currentBundle)
+            KafkaTopicUtils(AdminClient.create(props))
+        } finally {
+            Thread.currentThread().contextClassLoader = contextClassLoader
+        }
     }
 }
