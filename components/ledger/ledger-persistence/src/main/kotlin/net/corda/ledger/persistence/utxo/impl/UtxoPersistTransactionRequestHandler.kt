5794a4e0fb79f8ec11888455bd43b66a451afcc6
Miljenko Brkic <97448832+mbrkic-r3@users.noreply.github.com>
2022-12-20 15:40:06 +0000

    CORE-8998 Cannot load contract class from UtxoFindUnconsumedStatesByTypeRequestHandler (#2783)
    Loading sate class in UtxoFindUnconsumedStatesByTypeRequestHandler via sandbox.
    
    Sending serialized data from DB over the wire and constructing StateAndRef on the flow side.
    
    Fixed check for state type being ContractState.
    
    CORE-5553 Update the UTXO transaction persistence code to ensure it outputs tokens for the token cache only for relevant states and verified transactions.

--- components/ledger/ledger-persistence/src/main/kotlin/net/corda/ledger/persistence/utxo/impl/UtxoPersistTransactionRequestHandler.kt
+++ components/ledger/ledger-persistence/src/main/kotlin/net/corda/ledger/persistence/utxo/impl/UtxoPersistTransactionRequestHandler.kt
@@ -1,6 +1,7 @@
 package net.corda.ledger.persistence.utxo.impl
 
 import net.corda.data.flow.event.external.ExternalEventContext
+import net.corda.ledger.common.data.transaction.TransactionStatus
 import net.corda.ledger.persistence.common.RequestHandler
 import net.corda.ledger.persistence.utxo.UtxoOutputRecordFactory
 import net.corda.ledger.persistence.utxo.UtxoPersistenceService
@@ -11,8 +12,10 @@ import net.corda.v5.base.util.contextLogger
 import net.corda.v5.ledger.utxo.ContractState
 import net.corda.v5.ledger.utxo.StateAndRef
 import net.corda.v5.ledger.utxo.observer.UtxoToken
+import net.corda.virtualnode.HoldingIdentity
 
-class UtxoPersistTransactionRequestHandler(
+class UtxoPersistTransactionRequestHandler @Suppress("LongParameterList") constructor(
+    private val holdingIdentity: HoldingIdentity,
     private val transaction: UtxoTransactionReader,
     private val tokenObservers: UtxoTokenObserverMap,
     private val externalEventContext: ExternalEventContext,
@@ -25,26 +28,41 @@ class UtxoPersistTransactionRequestHandler(
     }
 
     override fun execute(): List<Record<*, *>> {
-        // Feed all produced and consumed states through any token observers defined for them
-        val producedTokens = transaction.getProducedStates().toTokens(tokenObservers)
-        val consumedTokens = transaction.getConsumedStates(persistenceService).toTokens(tokenObservers)
+        val isTransactionVerified = transaction.status == TransactionStatus.VERIFIED
+
+        val outputTokenRecords = if (isTransactionVerified) {
+            utxoOutputRecordFactory.getTokenCacheChangeEventRecords(
+                holdingIdentity,
+                transaction.getProducedStates().toTokens(tokenObservers),
+                transaction.getConsumedStates(persistenceService).toTokens(tokenObservers)
+            )
+        } else {
+            listOf()
+        }
 
         // persist the transaction
         persistenceService.persistTransaction(transaction)
 
         // return output records
-        return utxoOutputRecordFactory.getTokenCacheChangeEventRecords(producedTokens, consumedTokens) +
-                utxoOutputRecordFactory.getPersistTransactionSuccessRecord(externalEventContext)
+        return outputTokenRecords + utxoOutputRecordFactory.getPersistTransactionSuccessRecord(externalEventContext)
     }
 
-    private fun List<StateAndRef<ContractState>>.toTokens(tokenObservers: UtxoTokenObserverMap): List<UtxoToken> {
+    private fun List<StateAndRef<ContractState>>.toTokens(
+        tokenObservers: UtxoTokenObserverMap
+    ): List<Pair<StateAndRef<*>, UtxoToken>> {
         return this.flatMap { stateAndRef ->
-            tokenObservers.getObserversFor(stateAndRef.state.javaClass)
+            tokenObservers.getObserversFor(stateAndRef.state.contractStateType)
                 .mapNotNull { observer ->
                     try {
-                        observer.onCommit(stateAndRef.state.contractState)
+                        val token = observer.onCommit(stateAndRef.state.contractState).let { token ->
+                            token.poolKey.tokenType?.let { token }
+                                ?: token.copy(
+                                    poolKey = token.poolKey.copy(tokenType = stateAndRef.state.contractStateType.name)
+                                )
+                        }
+                        stateAndRef to token
                     } catch (e: Exception) {
-                        log.error("Failed while trying call '${this.javaClass}'.onCommit() with '${stateAndRef.state}'")
+                        log.error("Failed while trying call '${this.javaClass}'.onCommit() with '${stateAndRef.state.contractStateType}'")
                         null
                     }
                 }
