0616dc02293ad21eacede8d18383c0a7170ad048
Juan José Ramos <jujoramos@users.noreply.github.com>
2022-12-21 13:33:45 +0000

    CORE-8691: Delay Closing SandboxGroupContext (#2745)
    Prevent automatically closing SandboxGroupContexts as soon as they are
    evicted from the cache since they might still be in use. Instead, keep
    a WeakReference to an internal wrapper and only close the evicted
    SandboxGroupContext once all strong references to the wrapper are gone.
    
    - Fix adapted from https://github.com/corda/corda/commit/4316838.

--- components/virtual-node/sandbox-group-context-service/src/main/kotlin/net/corda/sandboxgroupcontext/service/impl/SandboxGroupContextCacheImpl.kt
+++ components/virtual-node/sandbox-group-context-service/src/main/kotlin/net/corda/sandboxgroupcontext/service/impl/SandboxGroupContextCacheImpl.kt
@@ -5,23 +5,113 @@ import com.github.benmanes.caffeine.cache.Caffeine
 import net.corda.cache.caffeine.CacheFactoryImpl
 import net.corda.sandboxgroupcontext.SandboxGroupContext
 import net.corda.sandboxgroupcontext.VirtualNodeContext
+import net.corda.v5.base.annotations.VisibleForTesting
 import net.corda.v5.base.util.loggerFor
+import java.lang.ref.ReferenceQueue
+import java.lang.ref.WeakReference
+import java.util.concurrent.ConcurrentHashMap
 
-internal class SandboxGroupContextCacheImpl(override val capacity: Long): SandboxGroupContextCache {
+internal class SandboxGroupContextCacheImpl(override val capacity: Long) : SandboxGroupContextCache {
     private companion object {
         private val logger = loggerFor<SandboxGroupContextCache>()
     }
 
-    private val contexts: Cache<VirtualNodeContext, CloseableSandboxGroupContext> = CacheFactoryImpl().build(
+    private class ToBeClosed(
+        val cacheKey: VirtualNodeContext,
+        val sandboxGroupContextToClose: AutoCloseable,
+        sandboxGroupContext: SandboxGroupContextWrapper,
+        referenceQueue: ReferenceQueue<SandboxGroupContextWrapper>
+    ) : WeakReference<SandboxGroupContextWrapper>(sandboxGroupContext, referenceQueue)
+
+    private val expiryQueue = ReferenceQueue<SandboxGroupContextWrapper>()
+    private val toBeClosed: ConcurrentHashMap.KeySetView<ToBeClosed, Boolean> = ConcurrentHashMap.newKeySet()
+
+    /**
+     * Wrapper around [CloseableSandboxGroupContext], solely used to keep a [WeakReference] to every instance and only
+     * invoke [CloseableSandboxGroupContext.close] on cache eviction when all strong references are gone.
+     */
+    private class SandboxGroupContextWrapper(
+        val wrappedSandboxGroupContext: CloseableSandboxGroupContext
+    ) : SandboxGroupContext by wrappedSandboxGroupContext
+
+    /**
+     * Checks whether there are contexts that were evicted from the cache but haven't been closed yet, either because
+     * there are still strong reference to the [SandboxGroupContextWrapper] or because the garbage collector hasn't
+     * updated the [ReferenceQueue] yet. Used for testing purposes only.
+     *
+     * @return true if there are contexts to be closed, false otherwise.
+     */
+    @VisibleForTesting
+    internal fun evictedContextsToBeClosed() : Boolean = toBeClosed.isNotEmpty()
+
+    @Suppress("TooGenericExceptionCaught")
+    private fun purgeExpiryQueue() {
+        // Close the wrapped [CloseableSandboxGroupContext] for every [SandboxGroupContextWrapper]
+        // that has already been garbage-collected.
+        while (true) {
+            val head = expiryQueue.poll() as? ToBeClosed ?: break
+
+            if (!toBeClosed.remove(head)) {
+                logger.warn("Reaped unexpected sandboxGroup context for {}", head.cacheKey)
+            }
+
+            try {
+                logger.info(
+                    "Closing {} sandbox for {} [UNUSED]",
+                    head.cacheKey.sandboxGroupType,
+                    head.cacheKey.holdingIdentity.x500Name
+                )
+
+                head.sandboxGroupContextToClose.close()
+            } catch (exception: Exception) {
+                logger.warn(
+                    "Error closing {} sandbox for {}",
+                    head.cacheKey.sandboxGroupType,
+                    head.cacheKey.holdingIdentity.x500Name,
+                    exception
+                )
+            }
+        }
+    }
+
+    private val contexts: Cache<VirtualNodeContext, SandboxGroupContextWrapper> = CacheFactoryImpl().build(
         "Sandbox-Cache",
         Caffeine.newBuilder()
             .maximumSize(capacity)
-            .removalListener { key, value, cause ->
-                logger.info("Evicting {} sandbox for: {} [{}]", key!!.sandboxGroupType, key.holdingIdentity.x500Name, cause.name)
-                value?.close()
-            })
+            // If the entry was manually removed from the cache by the user, automatically close the wrapped
+            // [CloseableSandboxGroupContext]. If the entry was automatically removed due to eviction, however, add the
+            // wrapped [CloseableSandboxGroupContext] to the internal [expiryQueue], so it is only closed once it's safe
+            // to do so (wrapping [SandboxGroupContextWrapper] is not referenced anymore).
+            .removalListener { key, context, cause ->
+                if (cause.wasEvicted()) {
+                    (context?.wrappedSandboxGroupContext as? AutoCloseable)?.also { autoCloseable ->
+                        toBeClosed += ToBeClosed(key!!, autoCloseable, context, expiryQueue)
+                    }
+
+                    logger.info(
+                        "Evicting {} sandbox for {} [{}]",
+                        key!!.sandboxGroupType,
+                        key.holdingIdentity.x500Name,
+                        cause.name
+                    )
+
+                    purgeExpiryQueue()
+                } else {
+                    logger.info(
+                        "Closing {} sandbox for {} [{}]",
+                        key!!.sandboxGroupType,
+                        key.holdingIdentity.x500Name,
+                        cause.name
+                    )
+
+                    (context?.wrappedSandboxGroupContext as? AutoCloseable)?.close()
+                }
+            }
+    )
 
     override fun remove(virtualNodeContext: VirtualNodeContext) {
+        purgeExpiryQueue()
+
         contexts.invalidate(virtualNodeContext)
     }
 
@@ -29,10 +119,17 @@ internal class SandboxGroupContextCacheImpl(override val capacity: Long): Sandbo
         virtualNodeContext: VirtualNodeContext,
         createFunction: (VirtualNodeContext) -> CloseableSandboxGroupContext
     ): SandboxGroupContext {
+        purgeExpiryQueue()
+
         return contexts.get(virtualNodeContext) {
-            logger.info("Caching {} sandbox for: {} (cache size: {})",
-                virtualNodeContext.sandboxGroupType, virtualNodeContext.holdingIdentity.x500Name, contexts.estimatedSize())
-            createFunction(virtualNodeContext)
+            logger.info(
+                "Caching {} sandbox for {} (cache size: {})",
+                virtualNodeContext.sandboxGroupType,
+                virtualNodeContext.holdingIdentity.x500Name,
+                contexts.estimatedSize()
+            )
+
+            SandboxGroupContextWrapper(createFunction(virtualNodeContext))
         }
     }
 
