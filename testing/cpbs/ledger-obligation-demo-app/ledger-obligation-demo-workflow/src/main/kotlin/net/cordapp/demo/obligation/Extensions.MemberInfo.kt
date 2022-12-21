4683a7ea83e7aa56b94551abd4340ee4e766f5b0
Lajos Veres <lajos.veres@r3.com>
2022-12-21 21:54:56 +0000

    CORE-8977 Use single instead of first in notary lookup logic

--- testing/cpbs/ledger-obligation-demo-app/ledger-obligation-demo-workflow/src/main/kotlin/net/cordapp/demo/obligation/Extensions.MemberInfo.kt
+++ testing/cpbs/ledger-obligation-demo-app/ledger-obligation-demo-workflow/src/main/kotlin/net/cordapp/demo/obligation/Extensions.MemberInfo.kt
@@ -6,7 +6,7 @@ import net.corda.v5.ledger.common.Party
 import net.corda.v5.membership.MemberInfo
 
 fun MemberInfo.getNotaryParty(memberLookup: MemberLookup, notaryService: MemberX500Name, index: Int = 0): Party {
-    val notaryKey = memberLookup.lookup().first {
+    val notaryKey = memberLookup.lookup().single {
         it.memberProvidedContext["corda.notary.service.name"] == notaryService.toString()
     }.ledgerKeys[index]
     return Party(notaryService, notaryKey)
