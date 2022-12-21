5b2d3def8df52aecbe7779efe3a18f88e67156a1
Lajos Veres <lajos.veres@r3.com>
2022-12-21 21:30:19 +0000

    CORE-9052, CORE-9053 Remove ledger transaction use from Backchain resolutions graph logic.

--- components/ledger/ledger-utxo-flow/src/main/kotlin/net/corda/ledger/utxo/flow/impl/flows/backchain/TransactionBackchainExtensions.kt
+++ components/ledger/ledger-utxo-flow/src/main/kotlin/net/corda/ledger/utxo/flow/impl/flows/backchain/TransactionBackchainExtensions.kt
@@ -1,12 +1,10 @@
 package net.corda.ledger.utxo.flow.impl.flows.backchain
 
-import net.corda.v5.base.annotations.Suspendable
 import net.corda.v5.crypto.SecureHash
 import net.corda.v5.ledger.utxo.transaction.UtxoSignedTransaction
 
-@Suspendable
 val UtxoSignedTransaction.dependencies: Set<SecureHash>
-    get() = toLedgerTransaction()
-        .let { it.inputStateRefs.asSequence() + it.referenceInputStateRefs.asSequence() }
+    get() = this
+        .let { it.inputStateRefs.asSequence() + it.referenceStateRefs.asSequence() }
         .map { it.transactionHash }
         .toSet()
