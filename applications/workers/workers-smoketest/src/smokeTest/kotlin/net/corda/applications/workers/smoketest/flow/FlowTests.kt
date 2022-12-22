6f3c218e1aa73f84df437a665f8d0b4a4b83edc9
nkovacsx <nandor.kovacs@r3.com>
2022-12-22 16:16:00 +0000

    Re-add scenarios for notary smoke tests

--- applications/workers/workers-smoketest/src/smokeTest/kotlin/net/corda/applications/workers/smoketest/flow/FlowTests.kt
+++ applications/workers/workers-smoketest/src/smokeTest/kotlin/net/corda/applications/workers/smoketest/flow/FlowTests.kt
@@ -883,9 +883,90 @@ class FlowTests {
     }
 
     @Test
+    fun `Notary - Non-validating plugin returns error when trying to spend unknown reference state`() {
+        // 1. Issue 1 state
+        val issuedStates = mutableListOf<String>()
+        issueStatesAndValidateResult(1) { issuanceResult ->
+            // 2. Make sure the states were issued
+            assertThat(issuanceResult.flowStatus).isEqualTo(RPC_FLOW_STATUS_SUCCESS)
+            val flowResultMap = issuanceResult.mapFlowJsonResult()
+
+            @Suppress("unchecked_cast")
+            val issuedStateRefs = flowResultMap["issuedStateRefs"] as List<String>
+
+            assertThat(issuedStateRefs).hasSize(1)
+
+            issuedStates.addAll(issuedStateRefs)
+
+            // 3. Make sure no states were consumed
+            assertAll({
+                assertThat(flowResultMap["consumedInputStateRefs"] as List<*>).hasSize(0)
+                assertThat(flowResultMap["consumedReferenceStateRefs"] as List<*>).hasSize(0)
+            })
+        }
+
+        // 4. Spend a valid state and reference an unknown state
+        consumeStatesAndValidateResult(
+            inputStates = listOf(issuedStates.first()),
+            refStates = listOf(
+                // Random unknown state
+                "SHA-256:CDFF8A944383063AB86AFE61488208CCCC84149911F85BE4F0CACCF399CA9903:0"
+            )
+        ) { consumeResult ->
+            assertAll({
+                assertThat(consumeResult.flowStatus).isEqualTo(RPC_FLOW_STATUS_FAILED)
+                assertThat(consumeResult.flowError?.message).contains("Unable to notarise transaction")
+                assertThat(consumeResult.flowError?.message).contains("NotaryErrorReferenceStateUnknown")
+            })
+        }
+    }
+
+    @Test
+    fun `Notary - Non-validating plugin executes successfully when using the same state for input and ref`() {
+        // 1. Issue 1 state
+        val issuedStates = mutableListOf<String>()
+        issueStatesAndValidateResult(1) { issuanceResult ->
+            // 2. Make sure the states were issued
+            assertThat(issuanceResult.flowStatus).isEqualTo(RPC_FLOW_STATUS_SUCCESS)
+            val flowResultMap = issuanceResult.mapFlowJsonResult()
+
+            @Suppress("unchecked_cast")
+            val issuedStateRefs = flowResultMap["issuedStateRefs"] as List<String>
+
+            assertThat(issuedStateRefs).hasSize(1)
+
+            issuedStates.addAll(issuedStateRefs)
+
+            // 3. Make sure no states were consumed
+            assertAll({
+                assertThat(flowResultMap["consumedInputStateRefs"] as List<*>).hasSize(0)
+                assertThat(flowResultMap["consumedReferenceStateRefs"] as List<*>).hasSize(0)
+            })
+        }
+
+        // 4. Make sure we consumed the state and managed to reference it
+        // Since the state we are trying to spend and reference is not spent yet (not persisted) we should be able
+        // to spend it and reference at the same time
+        consumeStatesAndValidateResult(
+            inputStates = listOf(issuedStates.first()),
+            refStates = listOf(issuedStates.first())
+        ) { consumeResult ->
+            val flowResultMap = consumeResult.mapFlowJsonResult()
+            assertAll({
+                assertThat(consumeResult.flowStatus).isEqualTo(RPC_FLOW_STATUS_SUCCESS)
+                assertAll({
+                    assertThat(flowResultMap["consumedInputStateRefs"] as List<*>).hasSize(1)
+                    assertThat(flowResultMap["consumedReferenceStateRefs"] as List<*>).hasSize(1)
+                })
+            })
+        }
+    }
+
+    @Test
     fun `Notary - Non-validating plugin returns error when trying to spend unknown input state`() {
         consumeStatesAndValidateResult(
             inputStates = listOf(
+                // Random unknown state
                 "SHA-256:CDFF8A944383063AB86AFE61488208CCCC84149911F85BE4F0CACCF399CA9903:0"
             ),
             refStates = emptyList()
@@ -932,7 +1013,8 @@ class FlowTests {
         }
 
         // 5. Try to reference the spent state.
-        // Note we MUST spend a state otherwise it is not a valid transaction
+        // Note we MUST spend or issue a state otherwise it is not a valid transaction. In this case we choose to spend
+        // it because it's easier to do with `consumeStatesAndValidateResult`.
         consumeStatesAndValidateResult(
             inputStates = listOf(issuedStates[1]),
             refStates = listOf(issuedStates.first())
