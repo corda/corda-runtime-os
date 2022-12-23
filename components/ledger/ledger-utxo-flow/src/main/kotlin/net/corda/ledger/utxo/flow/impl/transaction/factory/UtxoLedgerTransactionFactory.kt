2d1f078872f2c434a19e63b55daf8d79c6773087
Lajos Veres <lajos.veres@r3.com>
2022-12-23 11:34:15 +0000

    CORE-9052 original PR's review comments

--- components/ledger/ledger-utxo-flow/src/main/kotlin/net/corda/ledger/utxo/flow/impl/transaction/factory/UtxoLedgerTransactionFactory.kt
+++ components/ledger/ledger-utxo-flow/src/main/kotlin/net/corda/ledger/utxo/flow/impl/transaction/factory/UtxoLedgerTransactionFactory.kt
@@ -4,6 +4,10 @@ import net.corda.ledger.common.data.transaction.WireTransaction
 import net.corda.v5.base.annotations.Suspendable
 import net.corda.v5.ledger.utxo.transaction.UtxoLedgerTransaction
 
+/**
+ * Factory to create [UtxoLedgerTransaction]s.
+ * Mainly needed to resolve referenced states.
+ */
 interface UtxoLedgerTransactionFactory {
     @Suspendable
     fun create(
