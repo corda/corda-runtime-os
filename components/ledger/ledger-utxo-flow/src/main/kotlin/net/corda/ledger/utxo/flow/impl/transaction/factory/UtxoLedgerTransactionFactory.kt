b86e5a128d7b47e4ac1a6f1ce49ab93979fd838a
Christian Sailer <christian.sailer@r3.com>
2022-12-23 11:48:01 +0000

    Update UtxoLedgerTransactionFactory.kt
    Flesh out kDocs

--- components/ledger/ledger-utxo-flow/src/main/kotlin/net/corda/ledger/utxo/flow/impl/transaction/factory/UtxoLedgerTransactionFactory.kt
+++ components/ledger/ledger-utxo-flow/src/main/kotlin/net/corda/ledger/utxo/flow/impl/transaction/factory/UtxoLedgerTransactionFactory.kt
@@ -5,10 +5,16 @@ import net.corda.v5.base.annotations.Suspendable
 import net.corda.v5.ledger.utxo.transaction.UtxoLedgerTransaction
 
 /**
- * Factory to create [UtxoLedgerTransaction]s.
- * Mainly needed to resolve referenced states.
+ * Factory to create a [UtxoLedgerTransaction]s.
+ * This is required to resolve input and reference stateRefs to actual
+ * transaction states.
  */
 interface UtxoLedgerTransactionFactory {
+    
+    /**
+     * Resolves the input and reference stateRefs to TransactionState objects
+     * and returns a fully resolved UtxoLedgerTransation
+     */
     @Suspendable
     fun create(
         wireTransaction: WireTransaction
