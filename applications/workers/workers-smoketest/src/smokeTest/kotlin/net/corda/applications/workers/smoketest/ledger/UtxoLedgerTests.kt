01aa170ab7fbdab996d0226d9a9a04ce485932e7
Lajos Veres <lajos.veres@r3.com>
2023-01-04 18:08:49 +0000

    CORE-8977 Disable UtxoLedgerTests

--- applications/workers/workers-smoketest/src/smokeTest/kotlin/net/corda/applications/workers/smoketest/ledger/UtxoLedgerTests.kt
+++ applications/workers/workers-smoketest/src/smokeTest/kotlin/net/corda/applications/workers/smoketest/ledger/UtxoLedgerTests.kt
@@ -16,6 +16,7 @@ import net.corda.applications.workers.smoketest.startRpcFlow
 import net.corda.v5.crypto.SecureHash
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS
@@ -23,6 +24,7 @@ import java.util.UUID
 
 @Suppress("Unused", "FunctionName")
 @TestInstance(PER_CLASS)
+@Disabled
 class UtxoLedgerTests {
 
     private companion object {
