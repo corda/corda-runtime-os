e43884ec53ca1fe4d69da3ad2f23edddeabbd576
Dickon Reed <dickon.reed@r3.com>
2023-01-06 16:11:22 +0000

    CORE-9154: reduce logging on KeyAlreadyExistsException.kt to a trace without a backtrace

--- libs/crypto/crypto-core/src/main/kotlin/net/corda/crypto/core/KeyAlreadyExistsException.kt
+++ libs/crypto/crypto-core/src/main/kotlin/net/corda/crypto/core/KeyAlreadyExistsException.kt
@@ -2,5 +2,5 @@ package net.corda.crypto.core
 
 import net.corda.v5.base.exceptions.CordaRuntimeException
 
-class KeyAlreadyExistsException(message: String) :
+class KeyAlreadyExistsException(message: String, val alias: String, val tenantId: String) :
     CordaRuntimeException(message)
