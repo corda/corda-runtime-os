6c13f024fb3e9c7cd121c1a82222d3cf7197b535
Ryan Fowler <ryan.fowler@r3.com>
2023-01-11 15:46:09 +0000

    CORE-9206: Add localWorkerPlatformVersion to the PlatformInfoProvider by reading it from build properties via the BundleContext

--- libs/platform-info/src/main/kotlin/net/corda/libs/platform/impl/PlatformInfoProviderImpl.kt
+++ libs/platform-info/src/main/kotlin/net/corda/libs/platform/impl/PlatformInfoProviderImpl.kt
@@ -5,6 +5,7 @@ import org.osgi.framework.BundleContext
 import org.osgi.service.component.annotations.Activate
 import org.osgi.service.component.annotations.Component
 
+
 @Component(service = [PlatformInfoProvider::class])
 class PlatformInfoProviderImpl @Activate constructor(
     bundleContext: BundleContext,
@@ -12,12 +13,15 @@ class PlatformInfoProviderImpl @Activate constructor(
 
     internal companion object {
         const val STUB_PLATFORM_VERSION = 5000
+
+        private const val DEFAULT_PLATFORM_VERSION = 5000
+        private const val PLATFORM_VERSION_KEY = "net.corda.platform.version"
     }
 
     /** Temporary stub values **/
     override val activePlatformVersion = STUB_PLATFORM_VERSION
 
-    override val localWorkerPlatformVersion = bundleContext.getProperty("net.corda.platform.version").toInt()
+    override val localWorkerPlatformVersion = bundleContext.getProperty(PLATFORM_VERSION_KEY)?.toInt() ?: DEFAULT_PLATFORM_VERSION
 
     override val localWorkerSoftwareVersion = bundleContext.bundle.version.toString()
 }
