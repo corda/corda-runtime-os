57861e5660514cd49d28d5f4682089887ab0806a
Yiftach Kaplan <yiftach.kaplan@r3.com>
2023-01-13 12:47:29 +0000

    CORE-8873: Add member TLS certificate subject to member context in mutual TLS

--- processors/member-processor/src/test/kotlin/net/corda/processors/member/internal/MemberProcessorImplTest.kt
+++ processors/member-processor/src/test/kotlin/net/corda/processors/member/internal/MemberProcessorImplTest.kt
@@ -44,6 +44,7 @@ class MemberProcessorImplTest {
             mock(),
             mock(),
             mock(),
+            mock(),
         )
 
         verify(lifecycleCoordinatorFactory).createCoordinator(any(), any(), any())
