b29842d27c7d35f1e3f76ffff8f55b72e0367fee
Will Vigor <william.vigor@r3.com>
2023-01-13 16:08:30 +0000

    CORE-9442 Delete Setup Corda Cluster Command
    Instead applications/tools/p2p-test/app-simulator/scripts/deploy.sh
    can be used to deploy a worker cluster

--- applications/tools/p2p-test/app-simulator/scripts/tearDown.sh
+++ applications/tools/p2p-test/app-simulator/scripts/tearDown.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-source settings.sh
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+source $SCRIPT_DIR/settings.sh
 set -e
 
 declare -a namespaces=($A_CLUSTER_NAMESPACE $B_CLUSTER_NAMESPACE $MGM_CLUSTER_NAMESPACE $APP_SIMULATOR_DB_NAMESPACE)
