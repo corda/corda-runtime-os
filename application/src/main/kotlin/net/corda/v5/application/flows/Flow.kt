fc6562c50121f60f0feacd790e8159918d66b73a
Peter McKinney <pmck91@users.noreply.github.com>
2023-01-18 16:59:49 +0000

    Update application/src/main/kotlin/net/corda/v5/application/flows/Flow.kt
    Co-authored-by: Viktor Kolomeyko <viktor.kolomeyko@r3.com>

--- application/src/main/kotlin/net/corda/v5/application/flows/Flow.kt
+++ application/src/main/kotlin/net/corda/v5/application/flows/Flow.kt
@@ -8,7 +8,7 @@ import net.corda.v5.base.annotations.DoNotImplement
  * This should not be implemented directly when writing a flow. Instead, one of the more specialised types of flow
  * should be implemented:
  *
- * - [ClientStartableFlow] for flows that are started via remote client.
+ * - [ClientStartableFlow] for flows that are started via remote client, e.g. REST.
  * - [ResponderFlow] for flows that are started by a peer session.
  * - [SubFlow] for flows that are started from within other flows.
  */
