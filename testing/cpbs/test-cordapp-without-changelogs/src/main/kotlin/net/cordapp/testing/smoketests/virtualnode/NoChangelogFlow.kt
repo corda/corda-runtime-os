11ad18c85bcead8c993762581374f8771e645cee
Conal Smith <conal.smith@r3.com>
2023-01-20 13:31:40 +0000

    CORE-8610 - fix compilation

--- testing/cpbs/test-cordapp-without-changelogs/src/main/kotlin/net/cordapp/testing/smoketests/virtualnode/NoChangelogFlow.kt
+++ testing/cpbs/test-cordapp-without-changelogs/src/main/kotlin/net/cordapp/testing/smoketests/virtualnode/NoChangelogFlow.kt
@@ -1,18 +1,18 @@
 package net.cordapp.testing.smoketests.virtualnode
 
-import net.corda.v5.application.flows.RPCRequestData
-import net.corda.v5.application.flows.RPCStartableFlow
+import net.corda.v5.application.flows.ClientStartableFlow
+import net.corda.v5.application.flows.RestRequestBody
 import net.corda.v5.base.annotations.Suspendable
 import net.corda.v5.base.util.contextLogger
 
-class NoChangelogFlow : RPCStartableFlow {
+class NoChangelogFlow : ClientStartableFlow {
 
     private companion object {
         val log = contextLogger()
     }
 
     @Suspendable
-    override fun call(requestBody: RPCRequestData): String {
+    override fun call(requestBody: RestRequestBody): String {
         log.info("NoChangelogFlow starting...")
         return "NO_CHANGELOG_FLOW_COMPLETE"
     }
