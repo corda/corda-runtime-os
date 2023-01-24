4cead5e14668425fea709013c49df7b92e2af9c6
Viktor Kolomeyko <Viktor.Kolomeyko@r3.com>
2023-01-24 16:25:35 +0000

    NOTICK: Make test pass

--- libs/http-rpc/http-rpc-server-impl/src/integrationTest/kotlin/net/corda/httprpc/server/impl/HttpRpcServerAzureAdTest.kt
+++ libs/http-rpc/http-rpc-server-impl/src/integrationTest/kotlin/net/corda/httprpc/server/impl/HttpRpcServerAzureAdTest.kt
@@ -21,7 +21,6 @@ import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
-import kotlin.test.fail
 
 
 class HttpRpcServerAzureAdTest {
@@ -75,7 +74,6 @@ class HttpRpcServerAzureAdTest {
             log.info("Remote call performed")
             assertEquals(HttpStatus.OK, getPathResponse.responseStatus)
         }
-        fail("Deliberate failure to capture logs")
     }
 
     @Test
