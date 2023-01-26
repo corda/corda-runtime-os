97af99b2a6d9666dd4d806abfc6a8d7728537e14
LWogan <lorcan.wogan@r3.com>
2023-01-26 16:03:50 +0000

    CORE-9337 custom partitioner to ensure chunked records end up on the same partition

---
+++ libs/messaging/kafka-message-bus-impl/src/main/kotlin/net/corda/messagebus/kafka/producer/KafkaProducerPartitioner.kt
@@ -0,0 +1,47 @@
+package net.corda.messagebus.kafka.producer
+
+import net.corda.data.chunking.ChunkKey
+import net.corda.utilities.copyBytes
+import net.corda.v5.base.util.contextLogger
+import net.corda.v5.base.util.debug
+import net.corda.v5.base.util.trace
+import org.apache.kafka.clients.producer.Partitioner
+import org.apache.kafka.clients.producer.internals.BuiltInPartitioner
+import org.apache.kafka.common.Cluster
+
+/**
+ * Custom partitioner to be used with Kafka Producers.
+ */
+class KafkaProducerPartitioner : Partitioner {
+    private companion object {
+        val logger = contextLogger()
+    }
+
+    override fun configure(configs: MutableMap<String, *>?) {}
+    override fun close() {}
+
+    /**
+     * To ensure chunked records have unique keys, the real key is wrapped in a ChunkKey object.
+     * This means kafka compaction will not delete chunks as the keys will be different.
+     * However we still want the records to end up on the same partition so when the key is a ChunkKey we pass the real key bytes to the
+     * kafka built in partitioner logic. This function is what is called by default within the normal kafkaProducers.
+     * @param topic topic
+     * @param key the key of the record. Could be the real key or an object of type ChunkKey
+     * @param keyBytes bytes of the key
+     * @param value value of the record
+     * @param valueBytes serialized value bytes
+     * @param cluster cluster object provided by kafka client with cluster information
+     * @return partition to send record to
+     */
+    override fun partition(topic: String, key: Any, keyBytes: ByteArray, value: Any?, valueBytes: ByteArray?, cluster: Cluster): Int {
+        val keyBytesToPartition = if (key is ChunkKey) {
+            logger.debug { "Found ChunKey with id ${key.requestId}. Using real bytes for partitioning" }
+            key.realKey.copyBytes()
+        } else {
+            keyBytes
+        }
+        val partition = BuiltInPartitioner.partitionForKey(keyBytesToPartition, cluster.partitionsForTopic(topic).size)
+        logger.trace { "Sending record with key $key to partition $partition" }
+        return partition
+    }
+}
