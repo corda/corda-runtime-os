d41c4caf31eb1e3d406ce1b52aba7fb829c0343d
Peter McKinney <pmck91@users.noreply.github.com>
2023-01-26 14:06:44 +0000

    CORE-8027 http-rpc-stubs-impl module is now rest-stubs-impl (#2979)
    * http-rpc-stubs-impl module is now rest-stubs-impl
    
    * files failed to move to new folder in refactor
    
    * openapi test

--- libs/http-rpc/rest-stubs-impl/src/main/kotlin/net/corda/httprpc/endpoints/impl/HelloRestResourceImpl.kt
+++ libs/http-rpc/rest-stubs-impl/src/main/kotlin/net/corda/httprpc/endpoints/impl/HelloRestResourceImpl.kt
@@ -9,10 +9,10 @@ import net.corda.httprpc.security.CURRENT_REST_CONTEXT
 import net.corda.v5.base.util.contextLogger
 import org.osgi.service.component.annotations.Component
 
-@HttpRestResource(name = "Hello RPC API", description = "The Hello RPC API is used to test interactions via the " +
-        "HTTP-RPC API. It verifies that a call to HTTP-RPC can be made, and that the identity of the user making the " +
+@HttpRestResource(name = "Hello Rest API", description = "The Hello Rest API is used to test interactions via the " +
+        "Rest API. It verifies that a call to Rest can be made, and that the identity of the user making the " +
         "call can be recognized. RBAC permissions are checked and the call is successfully processed by " +
-        "the HTTP-RPC worker.",
+        "the HTTP-Rest worker.",
     path = "hello")
 interface HelloRestResource : RestResource {
 
