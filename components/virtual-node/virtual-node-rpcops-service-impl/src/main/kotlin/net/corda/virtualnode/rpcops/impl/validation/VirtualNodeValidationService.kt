9f992167834544458291c7ceb7e8afad289c4cf0
Conal Smith <conal.smith@r3.com>
2023-01-27 16:56:13 +0000

    CORE-8610 apply some code review changes

--- components/virtual-node/virtual-node-rpcops-service-impl/src/main/kotlin/net/corda/virtualnode/rpcops/impl/validation/VirtualNodeValidationService.kt
+++ components/virtual-node/virtual-node-rpcops-service-impl/src/main/kotlin/net/corda/virtualnode/rpcops/impl/validation/VirtualNodeValidationService.kt
@@ -3,7 +3,7 @@ package net.corda.virtualnode.rpcops.impl.validation
 import net.corda.libs.packaging.core.CpiMetadata
 import net.corda.virtualnode.VirtualNodeInfo
 
-interface VirtualNodeValidationService {
+internal interface VirtualNodeValidationService {
     fun validateAndGetVirtualNode(virtualNodeShortId: String): VirtualNodeInfo
     fun validateAndGetCpiByChecksum(cpiFileChecksum: String): CpiMetadata
     fun validateCpiUpgradePrerequisites(currentCpi: CpiMetadata, upgradeCpi: CpiMetadata)
--- components/virtual-node/virtual-node-write-service-impl/src/main/kotlin/net/corda/virtualnode/write/db/impl/writer/asyncoperation/VirtualNodeAsyncOperationHandler.kt
+++ components/virtual-node/virtual-node-write-service-impl/src/main/kotlin/net/corda/virtualnode/write/db/impl/writer/asyncoperation/VirtualNodeAsyncOperationHandler.kt
@@ -3,6 +3,16 @@ package net.corda.virtualnode.write.db.impl.writer.asyncoperation
 import net.corda.messaging.api.records.Record
 import java.time.Instant
 
+/**
+ * Interface for handling asynchronous virtual node operations.
+ */
 interface VirtualNodeAsyncOperationHandler<REQUEST> {
+    /**
+     * Given a request of type [REQUEST], handle the request and return a record.
+     *
+     * @param requestTimestamp the instant the request was received
+     * @param requestId the identifier of the request, used for traceability and logging purposes
+     * @param request the request itself, each implemention should handle one type
+     */
     fun handle(requestTimestamp: Instant, requestId: String, request: REQUEST): Record<*, *>?
 }
