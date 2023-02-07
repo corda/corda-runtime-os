f031d39e600be1336cef258609680afe5ef62ed5
saurabh.sonawane <saurabh.sonawane@r3.com>
2023-02-07 12:56:26 +0000

    CORE:9881 : Added json serialisation de-serialisation config in Facade Response.

--- components/interop/interop-service/src/main/kotlin/net/corda/interop/data/FacadeResponse.kt
+++ components/interop/interop-service/src/main/kotlin/net/corda/interop/data/FacadeResponse.kt
@@ -1,3 +1,8 @@
 package net.corda.interop.data
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize
+import com.fasterxml.jackson.databind.annotation.JsonSerialize
+
+@JsonSerialize(using = FacadeResponseSerializer::class)
+@JsonDeserialize(using = FacadeResponseDeserializer::class)
 data class FacadeResponse(val facadeId: FacadeId, val methodName: String, val outParameters: List<FacadeParameterValue<*>>)
