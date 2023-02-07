59ec56622b5cfbfc663b2769df6a83da2aef24a3
Yiftach Kaplan <yiftach.kaplan@r3.com>
2023-02-07 11:35:32 +0000

    Remove package infos

--- components/gateway/src/main/java/net/corda/p2p/gateway/certificates/package-info.java
+++
@@ -1,4 +0,0 @@
-@Export
-package net.corda.p2p.gateway.certificates;
-
-import org.osgi.annotation.bundle.Export;
--- components/gateway/src/main/java/net/corda/p2p/gateway/messaging/internal/package-info.java
+++
@@ -1,4 +0,0 @@
-@Export
-package net.corda.p2p.gateway.messaging.internal;
-
-import org.osgi.annotation.bundle.Export;
