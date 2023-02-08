a199e79e4e686c9016ca797a5dfb5b81af0eb40d
Kyriakos Tharrouniatis <kyriakos.tharrouniatis@r3.com>
2023-02-08 16:35:31 +0000

    Add TODO

--- components/crypto/crypto-persistence/src/main/kotlin/net/corda/crypto/persistence/SigningCachedKey.kt
+++ components/crypto/crypto-persistence/src/main/kotlin/net/corda/crypto/persistence/SigningCachedKey.kt
@@ -5,6 +5,7 @@ import java.time.Instant
 @Suppress("LongParameterList")
 class SigningCachedKey(
     val id: String,
+    // TODO See if we need to return the full key id
     val tenantId: String,
     val category: String,
     val alias: String?,
