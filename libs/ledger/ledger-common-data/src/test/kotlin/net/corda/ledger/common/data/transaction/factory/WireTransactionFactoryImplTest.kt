3e6db26ce3941f4563fc27d0f28faf2463cfc4ee
Lajos Veres <lajos.veres@r3.com>
2023-02-13 11:59:12 +0000

    CORE-7146 Test some constraints defined in json schema

--- libs/ledger/ledger-common-data/src/test/kotlin/net/corda/ledger/common/data/transaction/factory/WireTransactionFactoryImplTest.kt
+++ libs/ledger/ledger-common-data/src/test/kotlin/net/corda/ledger/common/data/transaction/factory/WireTransactionFactoryImplTest.kt
@@ -8,6 +8,7 @@ import net.corda.ledger.common.testkit.transactionMetadataExample
 import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Test
 
+@Suppress("MaxLineLength")
 class WireTransactionFactoryImplTest : CommonLedgerTest() {
     private val metadata = transactionMetadataExample(numberOfComponentGroups = 1)
     private val metadataJson = jsonMarshallingService.format(metadata)
@@ -68,7 +69,7 @@ class WireTransactionFactoryImplTest : CommonLedgerTest() {
             )
         }
             .isInstanceOf(IllegalStateException::class.java)
-            .hasMessageStartingWith("JSON validation failed due to: \$.ledgerModel: is missing but it is required")
+            .hasMessageMatching("JSON validation failed due to: \\$.*: is missing but it is required")
     }
 
     @Test
@@ -83,7 +84,22 @@ class WireTransactionFactoryImplTest : CommonLedgerTest() {
             )
         }
             .isInstanceOf(IllegalStateException::class.java)
-            .hasMessage("JSON validation failed due to: \$.ledgerVersion: is missing but it is required")
+            .hasMessageStartingWith("JSON validation failed due to: \$.ledgerVersion: is missing but it is required")
+    }
+
+    @Test
+    fun `Creating a WireTransaction with unknown json metadata properties throws`() {
+        val mangledJson = canonicalJson.replaceFirst("{", "{\"aaa\":\"value\",")
+
+        assertThatThrownBy {
+            wireTransactionFactory.create(
+                listOf(
+                    listOf(mangledJson.toByteArray()),
+                ), privacySalt
+            )
+        }
+            .isInstanceOf(IllegalStateException::class.java)
+            .hasMessage("JSON validation failed due to: \$.aaa: is not defined in the schema and the schema does not allow additional properties")
     }
 
     @Test
@@ -100,5 +116,106 @@ class WireTransactionFactoryImplTest : CommonLedgerTest() {
             .isInstanceOf(IllegalStateException::class.java)
             .hasMessageStartingWith("JSON validation failed due to: \$.cpkMetadata: there must be a minimum of 1 items in the array")
     }
+
+    @Test
+    fun `Creating a WireTransaction with Consensual settings`() {
+        val metadata = transactionMetadataExample(numberOfComponentGroups = 1,
+            ledgerModel = "net.corda.ledger.consensual.data.transaction.ConsensualLedgerTransactionImpl",
+            transactionSubType = null)
+        val metadataJson = jsonMarshallingService.format(metadata)
+        val canonicalJson = jsonValidator.canonicalize(metadataJson)
+        wireTransactionFactory.create(
+            listOf(
+                listOf(canonicalJson.toByteArray()),
+            ), privacySalt
+        )
+    }
+
+    @Test
+    fun `Creating a WireTransaction with Consensual settings with transaction subtype throws`() {
+        val metadata = transactionMetadataExample(numberOfComponentGroups = 1,
+            ledgerModel = "net.corda.ledger.consensual.data.transaction.ConsensualLedgerTransactionImpl",
+            transactionSubType = "GENERAL")
+        val metadataJson = jsonMarshallingService.format(metadata)
+        val canonicalJson = jsonValidator.canonicalize(metadataJson)
+        assertThatThrownBy {
+            wireTransactionFactory.create(
+                listOf(
+                    listOf(canonicalJson.toByteArray()),
+                ), privacySalt
+            )
+        }
+            .isInstanceOf(IllegalStateException::class.java)
+            .hasMessageStartingWith("JSON validation failed due to: \$.transactionSubtype: does not have a value in the enumeration [null],\$.transactionSubtype: string found, null expected")
+    }
+
+    @Test
+    fun `Creating a WireTransaction with Utxo settings`() {
+        val metadata = transactionMetadataExample(numberOfComponentGroups = 1,
+            ledgerModel = "net.corda.ledger.utxo.data.transaction.UtxoLedgerTransactionImpl",
+            transactionSubType = "GENERAL")
+        val metadataJson = jsonMarshallingService.format(metadata)
+        val canonicalJson = jsonValidator.canonicalize(metadataJson)
+        wireTransactionFactory.create(
+            listOf(
+                listOf(canonicalJson.toByteArray()),
+            ), privacySalt
+        )
+    }
+
+    @Test
+    fun `Creating a WireTransaction with Consensual settings without transaction subtype throws`() {
+        val metadata = transactionMetadataExample(numberOfComponentGroups = 1,
+            ledgerModel = "net.corda.ledger.utxo.data.transaction.UtxoLedgerTransactionImpl",
+            transactionSubType = null)
+        val metadataJson = jsonMarshallingService.format(metadata)
+        val canonicalJson = jsonValidator.canonicalize(metadataJson)
+        assertThatThrownBy {
+            wireTransactionFactory.create(
+                listOf(
+                    listOf(canonicalJson.toByteArray()),
+                ), privacySalt
+            )
+        }
+            .isInstanceOf(IllegalStateException::class.java)
+            .hasMessage("JSON validation failed due to: \$.transactionSubtype: does not have a value in the enumeration [NOTARY_CHANGE, GENERAL],\$.transactionSubtype: null found, string expected")
+    }
+
+    @Test
+    fun `Creating a WireTransaction with Consensual settings with unknown transaction subtype throws`() {
+        val metadata = transactionMetadataExample(numberOfComponentGroups = 1,
+            ledgerModel = "net.corda.ledger.utxo.data.transaction.UtxoLedgerTransactionImpl",
+            transactionSubType = "UNKNOWN")
+        val metadataJson = jsonMarshallingService.format(metadata)
+        val canonicalJson = jsonValidator.canonicalize(metadataJson)
+        assertThatThrownBy {
+            wireTransactionFactory.create(
+                listOf(
+                    listOf(canonicalJson.toByteArray()),
+                ), privacySalt
+            )
+        }
+            .isInstanceOf(IllegalStateException::class.java)
+            .hasMessage("JSON validation failed due to: \$.transactionSubtype: does not have a value in the enumeration [null, NOTARY_CHANGE, GENERAL],\$.transactionSubtype: does not have a value in the enumeration [NOTARY_CHANGE, GENERAL]")
+    }
+
+    @Test
+    fun `Creating a WireTransaction with unknown ledger model throws`() {
+        val metadata = transactionMetadataExample(numberOfComponentGroups = 1,
+            ledgerModel = "unknown"
+        )
+        val metadataJson = jsonMarshallingService.format(metadata)
+        val canonicalJson = jsonValidator.canonicalize(metadataJson)
+        assertThatThrownBy {
+            wireTransactionFactory.create(
+                listOf(
+                    listOf(canonicalJson.toByteArray()),
+                ), privacySalt
+            )
+        }
+            .isInstanceOf(IllegalStateException::class.java)
+            .hasMessage("JSON validation failed due to: \$.ledgerModel: does not have a value in the enumeration [net.corda.ledger.utxo.data.transaction.UtxoLedgerTransactionImpl, net.corda.ledger.consensual.data.transaction.ConsensualLedgerTransactionImpl]")
+    }
+
 
 }
