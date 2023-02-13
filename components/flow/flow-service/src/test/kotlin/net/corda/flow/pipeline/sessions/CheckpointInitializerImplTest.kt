8ab2c323b9bf3aa563d78263261068822c9411f5
Emily Bowe <emily.bowe@r3.com>
2023-02-13 12:36:57 +0000

    tests for null vnodeInfo and null cpiMetadata

--- components/flow/flow-service/src/test/kotlin/net/corda/flow/pipeline/sessions/CheckpointInitializerImplTest.kt
+++ components/flow/flow-service/src/test/kotlin/net/corda/flow/pipeline/sessions/CheckpointInitializerImplTest.kt
@@ -10,8 +10,6 @@ import net.corda.flow.state.FlowCheckpoint
 import net.corda.libs.packaging.core.CpiIdentifier
 import net.corda.libs.packaging.core.CpiMetadata
 import net.corda.libs.packaging.core.CpkMetadata
-import net.corda.v5.base.types.MemberX500Name
-import net.corda.virtualnode.HoldingIdentity
 import net.corda.virtualnode.VirtualNodeInfo
 import net.corda.virtualnode.read.VirtualNodeInfoReadService
 import net.corda.virtualnode.toCorda
@@ -24,10 +22,6 @@ import org.mockito.Mockito.verify
 import org.mockito.kotlin.whenever
 
 class CheckpointInitializerImplTest {
-    private val aliceX500Name = "C=GB, L=London, O=Alice"
-    private val aliceMemberName = MemberX500Name.parse(aliceX500Name)
-    private val groupId = "ABC123"
-    private val aliceHoldingIdentity = HoldingIdentity(aliceMemberName, groupId)
 
     @Test
     fun `vnode info`() {
@@ -75,26 +69,52 @@ class CheckpointInitializerImplTest {
 
     @Test
     fun `null cpi metadata`() {
-        val startContext = mock<FlowStartContext>()
-        whenever(startContext.identity.toCorda()).thenReturn(aliceHoldingIdentity)
+        val virtualNodeInfoReadService = mock<VirtualNodeInfoReadService>()
+        val cpiInfoReadService = mock<CpiInfoReadService>()
 
-/*        val virtualNodeInfoReadService = mock<VirtualNodeInfoReadService>()
-        val virtualNodeInfo = mock<VirtualNodeInfo>()  //mocks null by default
-        val holdingIdentity = startContext.identity.toCorda()*/
+        val checkpointInitializer = CheckpointInitializerImpl(virtualNodeInfoReadService, cpiInfoReadService)
 
-        assertThrows<IllegalStateException> { "Failed to find the cpiMetadata for identifier $aliceHoldingIdentity" }
+        val checkpoint = mock<FlowCheckpoint>()
+        val key = FlowKey(FLOW_ID_1, BOB_X500_HOLDING_IDENTITY)
+        val holdingIdentity: net.corda.data.identity.HoldingIdentity = BOB_X500_HOLDING_IDENTITY
+
+        val startContext = FlowStartContext().apply {
+            statusKey = key
+            identity = holdingIdentity
+        }
+
+        val waitingFor = mock<WaitingFor>()
+
+        val virtualNodeInfo = mock<VirtualNodeInfo>()
+        whenever(virtualNodeInfoReadService.get(startContext.identity.toCorda())).thenReturn(virtualNodeInfo)
+
+        val nullCpiMetadata = null
+        whenever(cpiInfoReadService.get(virtualNodeInfo.cpiIdentifier)).thenReturn(nullCpiMetadata)
+
+        assertThrows<IllegalStateException>{checkpointInitializer.initialize(checkpoint, startContext, waitingFor)}
     }
 
     @Test
     fun `null vnode`() {
-        val startContext = mock<FlowStartContext>()
-        whenever(startContext.identity.toCorda()).thenReturn(aliceHoldingIdentity)
+        val virtualNodeInfoReadService = mock<VirtualNodeInfoReadService>()
+        val cpiInfoReadService = mock<CpiInfoReadService>()
 
-        /*val virtualNodeInfoReadService = mock<VirtualNodeInfoReadService>()
-        val virtualNodeInfo = mock<VirtualNodeInfo>()  //mocks null by default
-        val holdingIdentity = startContext.identity.toCorda()*/
+        val checkpointInitializer = CheckpointInitializerImpl(virtualNodeInfoReadService, cpiInfoReadService)
 
-        assertThrows<IllegalStateException> { "Failed to find the virtual node info for holder $aliceHoldingIdentity" }
+        val checkpoint = mock<FlowCheckpoint>()
+        val key = FlowKey(FLOW_ID_1, BOB_X500_HOLDING_IDENTITY)
+        val holdingIdentity: net.corda.data.identity.HoldingIdentity = BOB_X500_HOLDING_IDENTITY
+
+        val startContext = FlowStartContext().apply {
+            statusKey = key
+            identity = holdingIdentity
+        }
+
+        val waitingFor = mock<WaitingFor>()
+        val nullVirtualNodeInfo = null
+        whenever(virtualNodeInfoReadService.get(startContext.identity.toCorda())).thenReturn(nullVirtualNodeInfo)
+
+        assertThrows<IllegalStateException>{checkpointInitializer.initialize(checkpoint, startContext, waitingFor)}
     }
 
     @Test
