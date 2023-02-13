7a38ccc66fea5b5b9cd459ef7bba836b0547d2d5
Lorcan Wogan <69468264+LWogan@users.noreply.github.com>
2023-02-13 19:28:42 +0000

    CORE-9338 add chunk key to allow chunks to have unique keys. make optional fields nullable (#814)
    add chunk key to allow chunks to have unique keys. make optional fields nullable. This allows for chunks to have unique keys to avoid compaction on kafka

--- data/config-schema/src/main/kotlin/net/corda/schema/configuration/BootConfig.kt
+++ data/config-schema/src/main/kotlin/net/corda/schema/configuration/BootConfig.kt
@@ -1,17 +1,20 @@
 package net.corda.schema.configuration
 
+import net.corda.schema.configuration.MessagingConfig.MAX_ALLOWED_MSG_SIZE
+
 /**
  * Configuration paths for values used to bootstrap the worker
  */
 object BootConfig {
     const val INSTANCE_ID = "instanceId"
     const val TOPIC_PREFIX = "topicPrefix"
-    const val BOOT_KAFKA = "kafka"
+    const val BOOT_MAX_ALLOWED_MSG_SIZE = MAX_ALLOWED_MSG_SIZE
 
+    const val BOOT_KAFKA = "kafka"
     const val BOOT_KAFKA_COMMON = "$BOOT_KAFKA.common"
     const val BOOT_CRYPTO = "crypto"
-    const val BOOT_DB = "db"
 
+    const val BOOT_DB = "db"
     const val BOOT_DB_PARAMS = "$BOOT_DB.params"
     const val BOOT_JDBC_URL = "$BOOT_DB_PARAMS.database.jdbc.url"
     const val BOOT_JDBC_USER = "$BOOT_DB_PARAMS.database.user"
