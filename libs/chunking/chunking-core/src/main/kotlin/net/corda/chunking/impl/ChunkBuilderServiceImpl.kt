dc76d505f349cf932bebaccb006ed33bb7d644ab
Lorcan Wogan <69468264+LWogan@users.noreply.github.com>
2023-02-14 08:56:54 +0000

    CORE-9340  Message chunking services to handle generation of chunks from data and to reassemble chunks into the original data (#2990)
    Provide chunking services to break up large objects into smaller chunks and to take these chunks and reassemble them back into their original type

---
+++ libs/chunking/chunking-core/src/main/kotlin/net/corda/chunking/impl/ChunkBuilderServiceImpl.kt
@@ -0,0 +1,48 @@
+package net.corda.chunking.impl
+
+import java.nio.ByteBuffer
+import net.corda.chunking.ChunkBuilderService
+import net.corda.chunking.toAvro
+import net.corda.data.KeyValuePairList
+import net.corda.data.chunking.Chunk
+import net.corda.v5.crypto.SecureHash
+import org.osgi.service.component.annotations.Component
+
+@Component(service = [ChunkBuilderService::class])
+class ChunkBuilderServiceImpl : ChunkBuilderService {
+    override fun buildFinalChunk(
+        identifier: String,
+        chunkNumber: Int,
+        checksum: SecureHash,
+        offset: Long,
+        properties: KeyValuePairList?,
+        fileName: String?,
+    ): Chunk = Chunk.newBuilder()
+        .setRequestId(identifier)
+        .setPartNumber(chunkNumber)
+        .setData(ByteBuffer.wrap(ByteArray(0)))
+        .setChecksum(checksum.toAvro())
+        .setProperties(properties)
+        .setOffset(offset)
+        //TODO - remove these CORE-9481
+        .setFileName(fileName)
+        .build()
+
+    override fun buildChunk(
+        identifier: String,
+        chunkNumber: Int,
+        byteBuffer: ByteBuffer,
+        offset: Long,
+        properties: KeyValuePairList?,
+        fileName: String?,
+    ): Chunk = Chunk.newBuilder()
+        .setRequestId(identifier)
+        .setPartNumber(chunkNumber)
+        .setData(byteBuffer)
+        .setProperties(properties)
+        .setChecksum(null)
+        .setOffset(offset)
+        //TODO - remove these CORE-9481
+        .setFileName(fileName)
+        .build()
+}
---
+++ libs/messaging/messaging/src/main/java/net/corda/messaging/api/chunking/package-info.java
@@ -0,0 +1,4 @@
+@Export
+package net.corda.messaging.api.chunking;
+
+import org.osgi.annotation.bundle.Export;
