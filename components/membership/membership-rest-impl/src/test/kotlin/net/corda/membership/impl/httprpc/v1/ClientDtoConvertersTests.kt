892b947b7b63097c2e05d8fe2625c651d17850bb
Ben Millar <44114751+ben-millar@users.noreply.github.com>
2023-02-14 16:40:54 +0000

    CORE-9879 Renaming 'membership/membership-http-rpc-impl' module to 'membership/membership-rest-impl' (#3132)

---
+++ components/membership/membership-rest-impl/src/test/kotlin/net/corda/membership/impl/httprpc/v1/ClientDtoConvertersTests.kt
@@ -0,0 +1,57 @@
+package net.corda.membership.impl.httprpc.v1
+
+import net.corda.membership.client.dto.MemberInfoSubmittedDto
+import net.corda.membership.client.dto.RegistrationRequestStatusDto
+import net.corda.membership.client.dto.RegistrationStatusDto
+import net.corda.membership.httprpc.v1.types.response.MemberInfoSubmitted
+import net.corda.membership.httprpc.v1.types.response.RegistrationRequestStatus
+import net.corda.membership.httprpc.v1.types.response.RegistrationStatus
+import net.corda.membership.impl.rest.v1.fromDto
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.EnumSource
+import java.time.Instant
+
+class ClientDtoConvertersTests {
+    @Test
+    fun `fromDto for RegistrationRequestStatusDto`() {
+        val dto = RegistrationRequestStatusDto(
+            "id",
+            Instant.ofEpochSecond(10),
+            Instant.ofEpochSecond(20),
+            RegistrationStatusDto.PENDING_MANUAL_APPROVAL,
+            MemberInfoSubmittedDto(
+                mapOf(
+                    "key 1" to "value 1",
+                    "key 2" to "value 2",
+                )
+            )
+        )
+
+        val status = dto.fromDto()
+
+        assertThat(status).isEqualTo(
+            RegistrationRequestStatus(
+                "id",
+                Instant.ofEpochSecond(10),
+                Instant.ofEpochSecond(20),
+                RegistrationStatus.PENDING_MANUAL_APPROVAL,
+                MemberInfoSubmitted(
+                    mapOf(
+                        "key 1" to "value 1",
+                        "key 2" to "value 2",
+                    )
+                )
+            )
+        )
+    }
+
+    @ParameterizedTest
+    @EnumSource(RegistrationStatusDto::class)
+    fun `fromDto for RegistrationStatusDto`(type: RegistrationStatusDto) {
+        val status = type.fromDto()
+
+        assertThat(status.name).isEqualTo(type.name)
+    }
+}
