8a827d433d4271e722c46b1d6475b7f779dfa84b
Jacob <jacob.scott@r3.com>
2023-02-15 13:08:28 +0000

    fixed bash scripts and merged changes with recent PRs

--- applications/workers/release/rpc-worker/src/e2eTest/resources/create.multi.clusters.sh
+++ applications/workers/release/rpc-worker/src/e2eTest/resources/create.multi.clusters.sh
@@ -8,22 +8,18 @@ telepresence quit
 create_cluster() {
   BASE_IMAGE="preTest-bf256886b"
   NAMESPACE="$USER-$1"
+  echo "Trying namespace $NAMESPACE"
   kubectl delete ns $NAMESPACE || echo "Name space $NAMESPACE not exists"
   kubectl create ns $NAMESPACE
   kubectl label ns $NAMESPACE namespace-type=corda-e2e --overwrite=true
   kubectl label ns $NAMESPACE branch=$USER-registration-e2e --overwrite=true
 
-  cd ~/corda-dev-helm/
-
-  helm repo add bitnami https://charts.bitnami.com/bitnami
-  helm dependency build charts/corda-dev
   helm upgrade --install prereqs -n corda \
-    charts/corda-dev \
+    oci://corda-os-docker.software.r3.com/helm-charts/corda-dev \
     --set 'kafka.replicaCount=1,kafka.zookeeper.replicaCount=1' -n $NAMESPACE --wait --timeout 600s
 
-
-  cd ~/corda-runtime-os/
-  helm install corda ./charts/corda -f .ci/e2eTests/corda.yaml --set "image.tag=$BASE_IMAGE,bootstrap.kafka.replicas=1,kafka.sasl.enabled=false" -n $NAMESPACE --wait --timeout 600s
+  ./gradlew clean publishOSGiImage -PcompositeBuild=true
+  helm install corda ./charts/corda -f .ci/e2eTests/corda.yaml --set "image.tag=$BASE_IMAGE,bootstrap.kafka.replicas=1,kafka.sasl.enabled=false" -n $NAMESPACE --wait
 
 }
 
