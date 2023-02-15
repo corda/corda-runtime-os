0fd26187881f4584719a98fc5429bc6a7fc93479
saurabh.sonawane <saurabh.sonawane@r3.com>
2023-02-15 12:50:20 +0000

    CORE:9881 : Changes for review

--- components/interop/interop-service/src/main/kotlin/net/corda/interop/service/InteropMemberRegistrationService.kt
+++ components/interop/interop-service/src/main/kotlin/net/corda/interop/service/InteropMemberRegistrationService.kt
@@ -8,6 +8,7 @@ import net.corda.membership.lib.MemberInfoExtension
 import net.corda.membership.lib.MemberInfoExtension.Companion.GROUP_ID
 import net.corda.membership.lib.MemberInfoExtension.Companion.LEDGER_KEYS_KEY
 import net.corda.membership.lib.MemberInfoExtension.Companion.LEDGER_KEY_HASHES_KEY
+import net.corda.membership.lib.MemberInfoExtension.Companion.LEDGER_KEY_SIGNATURE_SPEC
 import net.corda.membership.lib.MemberInfoExtension.Companion.MEMBER_CPI_NAME
 import net.corda.membership.lib.MemberInfoExtension.Companion.MEMBER_CPI_SIGNER_HASH
 import net.corda.membership.lib.MemberInfoExtension.Companion.MEMBER_CPI_VERSION
@@ -45,32 +46,31 @@ class InteropMemberRegistrationService {
      fun createDummyMemberInfo(): List<Record<String, PersistentMemberInfo>> {
         val registeringMember = memberList[0]
         val memberId = registeringMember.shortHash.value
-
         val memberContext = listOf(
-            KeyValuePair(PARTY_NAME, "PARTY_NAME"),
-            KeyValuePair(PARTY_SESSION_KEY, "PARTY_SESSION_KEY"),
-            KeyValuePair(SESSION_KEY_HASH, "SESSION_KEY_HASH"),
-            KeyValuePair(GROUP_ID, "GROUP_ID"),
-            KeyValuePair(LEDGER_KEYS_KEY.format(0), "LEDGER_KEYS_KEY"),
-            KeyValuePair(LEDGER_KEY_HASHES_KEY.format(0), "LEDGER_KEY_HASHES_KEY"),
-            KeyValuePair(LEDGER_KEY_HASHES_KEY.format(0), "LEDGER_KEY_HASHES_KEY"),
-            KeyValuePair(SOFTWARE_VERSION, "SV-5.0"),
-            KeyValuePair(PLATFORM_VERSION, "PV-5.0"),
-            KeyValuePair(MEMBER_CPI_NAME, "MEMBER_CPI_NAME"),
-            KeyValuePair(MEMBER_CPI_VERSION, "CPIV-1.0"),
-            KeyValuePair(MEMBER_CPI_SIGNER_HASH, "CPIV-1.0")
+            KeyValuePair(PARTY_NAME, ALICE_X500),
+            KeyValuePair(PARTY_SESSION_KEY, "-----BEGIN PUBLIC KEY-----\\nMFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEJI8YBSEpJDs+4HLZVeObqHzDq+OR\\nDhhbypT2UUKZGNpG8StV8bQ8YSFHLsyosdULMNI0r68l3k2Q6pKmpu91Jg==\\n-----END PUBLIC KEY-----\\n"),
+            KeyValuePair(SESSION_KEY_HASH, "9DEA9C982267BD142162ADC141C1C11C2F547C3C37B4C693A3EA3A017C2C6563"),
+            KeyValuePair(GROUP_ID, "3dfc0aae-be7c-44c2-aa4f-4d0d7145cf08"),
+            KeyValuePair(LEDGER_KEYS_KEY.format(0), "-----BEGIN PUBLIC KEY-----\\nMFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEWWiM/LoGe1589/22j+IeoggGarAA\\n8ckK4e0gz23IlZNTfBDWHRthwth/I2PKqiHTrRHZh3L6k41Sb2A/1TjX5A==\\n-----END PUBLIC KEY-----\\n"),
+            KeyValuePair(LEDGER_KEY_HASHES_KEY.format(0), "DFE65EAD29C556DF3A9C94C1A0F2C2155FFCC0768A282E18985BB021E8103B9D"),
+            KeyValuePair(LEDGER_KEY_SIGNATURE_SPEC.format(0), "SHA256withECDSA"),
+            KeyValuePair(SOFTWARE_VERSION, "5.0.0.0-Fox10-RC03"),
+            KeyValuePair(PLATFORM_VERSION, "5000"),
+            KeyValuePair(MEMBER_CPI_NAME, "calculator.cpi"),
+            KeyValuePair(MEMBER_CPI_VERSION, "1.0.0.0-SNAPSHOT"),
+            KeyValuePair(MEMBER_CPI_SIGNER_HASH, "SHA-256:367DDC08BB0BFBC8B338E2B8DC17EB1715A542386E6FE2376A9FB9EBC80A3DEC")
         )
         val mgmContext = listOf(
-            KeyValuePair(STATUS, MemberInfoExtension.MEMBER_STATUS_ACTIVE),
+            KeyValuePair(STATUS, "ACTIVE"),
             KeyValuePair(MODIFIED_TIME, Instant.now().toString()),
-            KeyValuePair(PARTY_NAME, "1"),
+            KeyValuePair(MemberInfoExtension.SERIAL, "1"),
         )
         return memberList.map {
             Record(
                 Schemas.Membership.MEMBER_LIST_TOPIC,
                 "${it.shortHash}-$memberId",
                 PersistentMemberInfo(
-                    registeringMember.toAvro(),
+                    it.toAvro(),
                     KeyValuePairList(memberContext),
                     KeyValuePairList(mgmContext)
                 )
