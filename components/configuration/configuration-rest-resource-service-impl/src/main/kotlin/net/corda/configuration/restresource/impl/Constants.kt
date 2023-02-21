365775517eef560be636a0a420d7ba47a6ee7fc5
Ben Millar <44114751+ben-millar@users.noreply.github.com>
2023-02-21 17:05:45 +0000

    CORE-10593 Renamed 'configuration-rpc-ops-impl' to 'configuration-rest-resource-impl' (#3202)
    * Renamed 'configuration-rpc-ops-impl' to 'configuration-rest-resource-impl'
    
    * Renaming 'configuration-rpcops-service-impl' to 'configuration-rest-resource-service-impl' in test.bndrun

--- components/configuration/configuration-rest-resource-service-impl/src/main/kotlin/net/corda/configuration/restresource/impl/Constants.kt
+++ components/configuration/configuration-rest-resource-service-impl/src/main/kotlin/net/corda/configuration/restresource/impl/Constants.kt
@@ -1,4 +1,4 @@
-package net.corda.configuration.rpcops.impl
+package net.corda.configuration.rest.impl
 
 internal const val GROUP_NAME = "config.management"
 internal const val CLIENT_NAME_HTTP = "config.manager.http"
