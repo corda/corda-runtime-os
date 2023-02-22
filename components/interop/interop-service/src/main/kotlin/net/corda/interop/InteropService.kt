5de7df4388eda8e453a281d761af089b99aa0c23
szymonsztuka <szymon.sztuka@r3.com>
2023-02-22 17:00:25 +0000

    More logs, don't subscribe for InteropProcessor

--- components/interop/interop-service/src/main/kotlin/net/corda/interop/InteropService.kt
+++ components/interop/interop-service/src/main/kotlin/net/corda/interop/InteropService.kt
@@ -4,7 +4,7 @@ import net.corda.configuration.read.ConfigChangedEvent
 import net.corda.configuration.read.ConfigurationReadService
 import net.corda.data.CordaAvroSerializationFactory
 import net.corda.data.flow.event.SessionEvent
-import net.corda.libs.configuration.helper.getConfig
+//import net.corda.libs.configuration.helper.getConfig
 import net.corda.lifecycle.Lifecycle
 import net.corda.lifecycle.LifecycleCoordinator
 import net.corda.lifecycle.LifecycleCoordinatorFactory
@@ -16,9 +16,9 @@ import net.corda.lifecycle.StartEvent
 import net.corda.lifecycle.createCoordinator
 import net.corda.messaging.api.publisher.Publisher
 import net.corda.messaging.api.publisher.factory.PublisherFactory
-import net.corda.messaging.api.subscription.config.SubscriptionConfig
+//import net.corda.messaging.api.subscription.config.SubscriptionConfig
 import net.corda.messaging.api.subscription.factory.SubscriptionFactory
-import net.corda.schema.Schemas.P2P.Companion.P2P_IN_TOPIC
+//import net.corda.schema.Schemas.P2P.Companion.P2P_IN_TOPIC
 import net.corda.schema.configuration.ConfigKeys.MESSAGING_CONFIG
 import net.corda.schema.configuration.ConfigKeys.P2P_LINK_MANAGER_CONFIG
 import org.osgi.service.component.annotations.Activate
@@ -57,6 +57,7 @@ class InteropService @Activate constructor(
     private fun eventHandler(event: LifecycleEvent, coordinator: LifecycleCoordinator) {
         when (event) {
             is StartEvent -> {
+                logger.info("StartEvent")
                 configurationReadService.start()
                 coordinator.createManagedResource(REGISTRATION) {
                     coordinator.followStatusChangesByName(
@@ -67,6 +68,7 @@ class InteropService @Activate constructor(
                 }
             }
             is RegistrationStatusChangeEvent -> {
+                logger.info("StartEvent ${event.status}")
                 if (event.status == LifecycleStatus.UP) {
                     coordinator.createManagedResource(CONFIG_HANDLE) {
                         configurationReadService.registerComponentForUpdates(
@@ -82,22 +84,25 @@ class InteropService @Activate constructor(
             is ConfigChangedEvent -> {
                 restartInteropProcessor(event)
             }
+            else -> {
+                logger.info("Other event $event")
+            }
         }
     }
 
     private fun restartInteropProcessor(event: ConfigChangedEvent) {
-        logger.info("restartInteropProcessor $event")
-        val messagingConfig = event.config.getConfig(MESSAGING_CONFIG)
-        coordinator.createManagedResource(SUBSCRIPTION) {
-            subscriptionFactory.createDurableSubscription(
-                SubscriptionConfig(CONSUMER_GROUP, P2P_IN_TOPIC),
-                InteropProcessor(cordaAvroSerializationFactory),
-                messagingConfig,
-                null
-            ).also {
-                it.start()
-            }
-        }
+        logger.info("$event")
+//        val messagingConfig = event.config.getConfig(MESSAGING_CONFIG)
+//        coordinator.createManagedResource(SUBSCRIPTION) {
+//            subscriptionFactory.createDurableSubscription(
+//                SubscriptionConfig(CONSUMER_GROUP, P2P_IN_TOPIC),
+//                InteropProcessor(cordaAvroSerializationFactory),
+//                messagingConfig,
+//                null
+//            ).also {
+//                it.start()
+//            }
+//        }
         //TODO below is temporary tactical code to setup members of interop group,
         // this will be phased out later on by CORE-10446
 //        publisher?.close()
@@ -106,7 +111,7 @@ class InteropService @Activate constructor(
 //            event.config.getConfig(MESSAGING_CONFIG)
 //        )
 //        publisher?.start()
-        logger.info("restartInteropProcessor - end")
+//        logger.info("restartInteropProcessor - end")
     }
 
     override val isRunning: Boolean
