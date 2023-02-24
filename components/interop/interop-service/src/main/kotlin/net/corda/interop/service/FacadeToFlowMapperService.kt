d0a6982cee9d5687eb3d9cff77d8f76a84985812
saurabh.sonawane <saurabh.sonawane@r3.com>
2023-02-24 12:51:57 +0000

    CORE:10425 : Changes to fix detekt issue and adding fallback if virtual node info not available.

--- components/interop/interop-service/src/main/kotlin/net/corda/interop/service/FacadeToFlowMapperService.kt
+++ components/interop/interop-service/src/main/kotlin/net/corda/interop/service/FacadeToFlowMapperService.kt
@@ -22,9 +22,7 @@ class FacadeToFlowMapperService @Activate constructor(
     companion object {
         private val logger = LoggerFactory.getLogger(this::class.java.enclosingClass)
         private val mapper = ObjectMapper(YAMLFactory()).registerModule(KotlinModule())
-
     }
-
     fun getFlowName(
         destinationIdentity: HoldingIdentity,
         facadeId: String,
@@ -32,26 +30,26 @@ class FacadeToFlowMapperService @Activate constructor(
     ): String {
         logger.info("Fetching flow name for holding identity : ${destinationIdentity.x500Name} " +
                 "facade id : $facadeId facade name : $facadeName")
-        //TODO: Now we are using hardcoded facade definition and
+        //Now we are using hardcoded facade definition and
         // the assumption is we will get the required facade to flow mapping configuration as a part of CPIMetaData.
         // The following code snippet suggest that how we can read it when it will be available.
-
-//        val vNodeInfo = virtualNodeInfoReadService.get(destinationIdentity)
-//        checkNotNull(vNodeInfo) {
-//            "Failed to find the virtual node info for holder '${destinationIdentity}' in ${virtualNodeInfoReadService::class.java}"
-//        }
-//
-//        val cpiMetadata = cpiInfoReadService.get(vNodeInfo.cpiIdentifier)
-//        checkNotNull(cpiMetadata) { "Failed to find the CPI meta data for '${vNodeInfo.cpiIdentifier}}'" }
-//        check(cpiMetadata.cpksMetadata.isNotEmpty()) { "No CPKs defined for CPI Meta data id='${cpiMetadata.cpiId}'" }
-//        return cpiMetadata.cpksMetadata.first().cordappManifest.attributes.get("") ?: ""
-
-        return mapper.readValue(
-            this::class.java.getResource("/dummy-facade-to-flow-config.yaml")?.readText(),
-            FacadeFlowMapping::class.java
-        ).facadeFlowMapping
-            .first { it.facadeId == facadeId }.mapping
-            .first { it.facadeMethod == facadeName }
-            .flowName
+        val vNodeInfo = virtualNodeInfoReadService.get(destinationIdentity)
+        if(vNodeInfo != null) {
+            checkNotNull(vNodeInfo) {
+                "Failed to find the virtual node info for holder '${destinationIdentity}' in ${virtualNodeInfoReadService::class.java}"
+            }
+            val cpiMetadata = cpiInfoReadService.get(vNodeInfo.cpiIdentifier)
+            checkNotNull(cpiMetadata) { "Failed to find the CPI meta data for '${vNodeInfo.cpiIdentifier}}'" }
+            check(cpiMetadata.cpksMetadata.isNotEmpty()) { "No CPKs defined for CPI Meta data id='${cpiMetadata.cpiId}'" }
+            return cpiMetadata.cpksMetadata.first().cordappManifest.attributes.get("") ?: ""
+        } else {
+            return mapper.readValue(
+                this::class.java.getResource("/dummy-facade-to-flow-config.yaml")?.readText(),
+                FacadeFlowMapping::class.java
+            ).facadeFlowMapping
+                .first { it.facadeId == facadeId }.mapping
+                .first { it.facadeMethod == facadeName }
+                .flowName
+        }
     }
 }
