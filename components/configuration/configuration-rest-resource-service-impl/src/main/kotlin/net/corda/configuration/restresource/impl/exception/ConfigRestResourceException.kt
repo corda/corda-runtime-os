38e7166f4b71d3d01f4c75bf573755f6ad21855e
ben-millar <ben.millar@r3.com>
2023-02-24 11:39:35 +0000

    Renamed remaining rpcContext properties, and renamed directories from restresource to rest

--- components/configuration/configuration-rest-resource-service-impl/src/main/kotlin/net/corda/configuration/restresource/impl/exception/ConfigRestResourceException.kt
+++
@@ -1,6 +0,0 @@
-package net.corda.configuration.rest.impl.exception
-
-import net.corda.v5.base.exceptions.CordaRuntimeException
-
-/** Exceptions related to the [net.corda.configuration.rpcops.impl.v1.ConfigRestResourceImpl]. */
-class ConfigRestResourceException(message: String, e: Exception? = null) : CordaRuntimeException(message, e)
