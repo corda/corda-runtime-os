a2e4cd4f16844aa1cf91ae974974dcdebddf5b30
saurabh.sonawane <saurabh.sonawane@r3.com>
2023-02-27 15:33:03 +0000

    CORE:10425 : Review changes

--- components/interop/interop-service/src/main/kotlin/net/corda/interop/service/FacadeToFlowMapperService.kt
+++ components/interop/interop-service/src/main/kotlin/net/corda/interop/service/FacadeToFlowMapperService.kt
@@ -20,6 +20,7 @@ class FacadeToFlowMapperService @Activate constructor(
     private val cpiInfoReadService: CpiInfoReadService
 ) {
     companion object {
+        private const val FACADE_TO_FLOW_MAPPING = "FACADE_TO_FLOW_MAPPING"
         private val logger = LoggerFactory.getLogger(this::class.java.enclosingClass)
         private val mapper = ObjectMapper(YAMLFactory()).registerModule(KotlinModule())
     }
@@ -37,6 +38,7 @@ class FacadeToFlowMapperService @Activate constructor(
         // the assumption is we will get the required facade to flow mapping configuration as a part of CPIMetaData.
         // The following code snippet suggests how we can read it and when it will be available.
         val vNodeInfo = virtualNodeInfoReadService.get(destinationIdentity)
+        var content: String
         if (vNodeInfo != null) {
             checkNotNull(vNodeInfo) {
                 "Failed to find the virtual node info for holder '${destinationIdentity}' in ${virtualNodeInfoReadService::class.java}"
@@ -44,10 +46,18 @@ class FacadeToFlowMapperService @Activate constructor(
             val cpiMetadata = cpiInfoReadService.get(vNodeInfo.cpiIdentifier)
             checkNotNull(cpiMetadata) { "Failed to find the CPI meta data for '${vNodeInfo.cpiIdentifier}}'" }
             check(cpiMetadata.cpksMetadata.isNotEmpty()) { "No CPKs defined for CPI Meta data id='${cpiMetadata.cpiId}'" }
-            return cpiMetadata.cpksMetadata.first().cordappManifest.attributes.get("") ?: ""
+            // In below code we are assuming that we will read the content from first CPK Metadata
+            // If the packaging team will change the solution in the future, then we need make appropriate changes.
+            content = cpiMetadata.cpksMetadata.first().cordappManifest.attributes[FACADE_TO_FLOW_MAPPING]?:""
+        } else {
+            content = this::class.java.getResource("/dummy-facade-to-flow-config.yaml")?.readText().toString()
+        }
+
+        if (content.trim().isEmpty()) {
+            throw IllegalStateException("Failed to fetch facade to flow mapping.")
         } else {
             return mapper.readValue(
-                this::class.java.getResource("/dummy-facade-to-flow-config.yaml")?.readText(),
+                content,
                 FacadeFlowMapping::class.java
             ).facadeFlowMapping
                 .first { it.facadeId == facadeId }.facadeMethodMapping
