e22536942f098d9dad9acaa26e2381004ca88429
ben-millar <ben.millar@r3.com>
2023-02-27 14:48:57 +0000

    Fixing input error in Schema model converter

--- libs/rest/rest-server-impl/src/main/kotlin/net/corda/rest/server/impl/apigen/processing/openapi/schema/SchemaModelToOpenApiSchemaConverter.kt
+++ libs/rest/rest-server-impl/src/main/kotlin/net/corda/rest/server/impl/apigen/processing/openapi/schema/SchemaModelToOpenApiSchemaConverter.kt
@@ -1,4 +1,4 @@
-package net.corda.rest.server.impl.apigen.processing.openapi.schema
+package net.corda.httprpc.server.impl.apigen.processing.openapi.schema
 
 import io.swagger.v3.oas.models.media.ArraySchema
 import io.swagger.v3.oas.models.media.BooleanSchema
@@ -8,23 +8,23 @@ import io.swagger.v3.oas.models.media.NumberSchema
 import io.swagger.v3.oas.models.media.ObjectSchema
 import io.swagger.v3.oas.models.media.Schema
 import io.swagger.v3.oas.models.media.StringSchema
-import net.corda.rest.server.impl.apigen.processing.openapi.schema.model.JsonSchemaModel
-import net.corda.rest.server.impl.apigen.processing.openapi.schema.model.SchemaCollectionModel
-import net.corda.rest.server.impl.apigen.processing.openapi.schema.model.SchemaDurableReturnResultModel
-import net.corda.rest.server.impl.apigen.processing.openapi.schema.model.SchemaEnumModel
-import net.corda.rest.server.impl.apigen.processing.openapi.schema.model.SchemaMapModel
-import net.corda.rest.server.impl.apigen.processing.openapi.schema.model.SchemaModel
-import net.corda.rest.server.impl.apigen.processing.openapi.schema.model.SchemaModelFieldsHelper
-import net.corda.rest.server.impl.apigen.processing.openapi.schema.model.SchemaMultiRefObjectModel
-import net.corda.rest.server.impl.apigen.processing.openapi.schema.model.SchemaObjectModel
-import net.corda.rest.server.impl.apigen.processing.openapi.schema.model.SchemaPairModel
-import net.corda.rest.server.impl.apigen.processing.openapi.schema.model.SchemaPositionedValueModel
-import net.corda.rest.server.impl.apigen.processing.openapi.schema.model.SchemaRefObjectModel
+import net.corda.httprpc.server.impl.apigen.processing.openapi.schema.model.JsonSchemaModel
+import net.corda.httprpc.server.impl.apigen.processing.openapi.schema.model.SchemaCollectionModel
+import net.corda.httprpc.server.impl.apigen.processing.openapi.schema.model.SchemaDurableReturnResultModel
+import net.corda.httprpc.server.impl.apigen.processing.openapi.schema.model.SchemaEnumModel
+import net.corda.httprpc.server.impl.apigen.processing.openapi.schema.model.SchemaMapModel
+import net.corda.httprpc.server.impl.apigen.processing.openapi.schema.model.SchemaModel
+import net.corda.httprpc.server.impl.apigen.processing.openapi.schema.model.SchemaModelFieldsHelper
+import net.corda.httprpc.server.impl.apigen.processing.openapi.schema.model.SchemaMultiRefObjectModel
+import net.corda.httprpc.server.impl.apigen.processing.openapi.schema.model.SchemaObjectModel
+import net.corda.httprpc.server.impl.apigen.processing.openapi.schema.model.SchemaPairModel
+import net.corda.httprpc.server.impl.apigen.processing.openapi.schema.model.SchemaPositionedValueModel
+import net.corda.httprpc.server.impl.apigen.processing.openapi.schema.model.SchemaRefObjectModel
 import net.corda.v5.base.util.trace
 import org.slf4j.LoggerFactory
 
 private val log =
-    LoggerFactory.getLogger("net.corda.rest.server.impl.SchemaModelToOpenApiSchemaConverter.kt")
+    LoggerFactory.getLogger("net.corda.httprpc.server.impl.SchemaModelToOpenApiSchemaConverter.kt")
 
 /**
  * [SchemaModelToOpenApiSchemaConverter] is a conversion layer between our [SchemaModel] and swagger's required [Schema].
@@ -32,75 +32,33 @@ private val log =
 object SchemaModelToOpenApiSchemaConverter {
     @Suppress("ComplexMethod")
     fun convert(schemaModel: SchemaModel): Schema<Any> {
-        net.corda.rest.server.impl.apigen.processing.openapi.schema.log.trace { """Convert schemaModel "$schemaModel" to schema.""" }
+        log.trace { """Convert schemaModel "$schemaModel" to schema.""" }
         return when (schemaModel) {
-            is SchemaEnumModel -> net.corda.rest.server.impl.apigen.processing.openapi.schema.SchemaModelToOpenApiSchemaConverter.convertBaseSchemaModel(
-                schemaModel
-            ).apply {
+            is SchemaEnumModel -> convertBaseSchemaModel(schemaModel).apply {
                 schemaModel.enum?.forEach { this.addEnumItemObject(it) }
             }
             is SchemaCollectionModel -> ArraySchema().apply {
                 schemaModel.type?.let { this.type(it.toString().lowercase()) }
-                schemaModel.items?.let { this.items(
-                    net.corda.rest.server.impl.apigen.processing.openapi.schema.SchemaModelToOpenApiSchemaConverter.convert(
-                        it
-                    )
-                ) }
+                schemaModel.items?.let { this.items(convert(it)) }
                 this.uniqueItems(schemaModel.uniqueItems)
             }
-            is SchemaMapModel -> net.corda.rest.server.impl.apigen.processing.openapi.schema.SchemaModelToOpenApiSchemaConverter.convertBaseSchemaModel(
-                schemaModel
-            ).apply {
-                schemaModel.additionalProperties?.let { props -> this.additionalProperties(
-                    net.corda.rest.server.impl.apigen.processing.openapi.schema.SchemaModelToOpenApiSchemaConverter.convert(
-                        props
-                    )
-                ) }
+            is SchemaMapModel -> convertBaseSchemaModel(schemaModel).apply {
+                schemaModel.additionalProperties?.let { props -> this.additionalProperties(convert(props)) }
             }
-            is SchemaObjectModel -> net.corda.rest.server.impl.apigen.processing.openapi.schema.SchemaModelToOpenApiSchemaConverter.convertBaseSchemaModel(
-                schemaModel
-            ).apply {
-                this.properties(schemaModel.properties.mapValues {
-                    net.corda.rest.server.impl.apigen.processing.openapi.schema.SchemaModelToOpenApiSchemaConverter.convert(
-                        it.value
-                    )
-                })
+            is SchemaObjectModel -> convertBaseSchemaModel(schemaModel).apply {
+                this.properties(schemaModel.properties.mapValues { convert(it.value) })
             }
-            is SchemaPairModel -> net.corda.rest.server.impl.apigen.processing.openapi.schema.SchemaModelToOpenApiSchemaConverter.convertBaseSchemaModel(
-                schemaModel
-            ).apply {
-                this.properties(schemaModel.properties.mapValues {
-                    net.corda.rest.server.impl.apigen.processing.openapi.schema.SchemaModelToOpenApiSchemaConverter.convert(
-                        it.value
-                    )
-                })
+            is SchemaPairModel -> convertBaseSchemaModel(schemaModel).apply {
+                this.properties(schemaModel.properties.mapValues { convert(it.value) })
             }
-            is SchemaDurableReturnResultModel -> net.corda.rest.server.impl.apigen.processing.openapi.schema.SchemaModelToOpenApiSchemaConverter.convertBaseSchemaModel(
-                schemaModel
-            ).apply {
-                this.properties(schemaModel.properties.mapValues {
-                    net.corda.rest.server.impl.apigen.processing.openapi.schema.SchemaModelToOpenApiSchemaConverter.convert(
-                        it.value
-                    )
-                })
+            is SchemaDurableReturnResultModel -> convertBaseSchemaModel(schemaModel).apply {
+                this.properties(schemaModel.properties.mapValues { convert(it.value) })
             }
-            is SchemaPositionedValueModel -> net.corda.rest.server.impl.apigen.processing.openapi.schema.SchemaModelToOpenApiSchemaConverter.convertBaseSchemaModel(
-                schemaModel
-            ).apply {
-                this.properties(schemaModel.properties.mapValues {
-                    net.corda.rest.server.impl.apigen.processing.openapi.schema.SchemaModelToOpenApiSchemaConverter.convert(
-                        it.value
-                    )
-                })
+            is SchemaPositionedValueModel -> convertBaseSchemaModel(schemaModel).apply {
+                this.properties(schemaModel.properties.mapValues { convert(it.value) })
             }
-            is SchemaMultiRefObjectModel -> net.corda.rest.server.impl.apigen.processing.openapi.schema.SchemaModelToOpenApiSchemaConverter.convertBaseSchemaModel(
-                schemaModel
-            ).apply {
-                this.properties(schemaModel.properties.mapValues {
-                    net.corda.rest.server.impl.apigen.processing.openapi.schema.SchemaModelToOpenApiSchemaConverter.convert(
-                        it.value
-                    )
-                })
+            is SchemaMultiRefObjectModel -> convertBaseSchemaModel(schemaModel).apply {
+                this.properties(schemaModel.properties.mapValues { convert(it.value) })
             }
             // extraordinary case, where the object's ref is expected to be found in the overall structure
             is SchemaRefObjectModel -> Schema<Any>().apply {
@@ -120,13 +78,11 @@ object SchemaModelToOpenApiSchemaConverter {
                     )
                 )
             }
-            else -> net.corda.rest.server.impl.apigen.processing.openapi.schema.SchemaModelToOpenApiSchemaConverter.convertBaseSchemaModel(
-                schemaModel
-            )
+            else -> convertBaseSchemaModel(schemaModel)
 
         }.also {
             it.setRequiredAndNullable(schemaModel)
-            net.corda.rest.server.impl.apigen.processing.openapi.schema.log.trace { """Convert schemaModel "$schemaModel" to schema, Result: "$it" completed.""" }
+            log.trace { """Convert schemaModel "$schemaModel" to schema, Result: "$it" completed.""" }
         }
     }
 
