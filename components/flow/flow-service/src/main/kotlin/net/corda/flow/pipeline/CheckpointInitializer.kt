9ab32685a1a52eb0779b71cde0a2e61275141ea2
Emily Bowe <emily.bowe@r3.com>
2023-02-28 14:18:15 +0000

    kdoc and format changes

--- components/flow/flow-service/src/main/kotlin/net/corda/flow/pipeline/CheckpointInitializer.kt
+++ components/flow/flow-service/src/main/kotlin/net/corda/flow/pipeline/CheckpointInitializer.kt
@@ -9,20 +9,23 @@ import net.corda.virtualnode.HoldingIdentity
 /**
  * Initializes the Checkpoint
  *
- * This interface initializes the Checkpoint.
- *
- * @param checkpoint a FlowCheckpoint.
- * @param waitingFor a WaitingFor.
- * @param holdingIdentity a HoldingIdentity.
- * @param contextBuilder a lambda which takes a Set<SecureHash> and returns a FlowStartContext.
+ * This interface is used to initialize the Checkpoint.
  */
-
 interface CheckpointInitializer {
+    /**
+     * Initializes the Checkpoint
+     *
+     * This method initializes the Checkpoint.
+     *
+     * @param checkpoint a FlowCheckpoint.
+     * @param waitingFor a WaitingFor.
+     * @param holdingIdentity a HoldingIdentity.
+     * @param contextBuilder a lambda which takes a Set<SecureHash> and returns a FlowStartContext.
+     */
     fun initialize(
         checkpoint: FlowCheckpoint,
         waitingFor: WaitingFor,
         holdingIdentity: HoldingIdentity,
-        contextBuilder
-        : (Set<SecureHash>) -> FlowStartContext
+        contextBuilder: (Set<SecureHash>) -> FlowStartContext
     )
 }
