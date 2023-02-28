537c930e50deb0ebe13d15b2057c10a6459ddf31
Owen Stanford <92725587+owenstanford@users.noreply.github.com>
2023-02-28 11:48:40 +0000

    CORE-11100 Remove unused cpk-service-common module (#3260)

--- components/virtual-node/cpk-service-common/src/main/kotlin/net/corda/cpk/readwrite/CpkServiceConfigKeys.kt
+++
@@ -1,9 +0,0 @@
-package net.corda.cpk.readwrite
-
-object CpkServiceConfigKeys {
-    /**
-     * Config key for the location of the cpk cache directory - bootstrap cfg key only.
-     * TODO - move to corda-api
-     */
-    const val CPK_CACHE_DIR = "corda.cpk.cacheDir"
-}
