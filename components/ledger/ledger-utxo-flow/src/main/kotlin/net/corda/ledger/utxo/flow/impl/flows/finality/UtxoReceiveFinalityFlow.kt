20b7c0c7d58db8a4eede85b8f2bd52022f628cc5
nkovacsx <57627796+nkovacsx@users.noreply.github.com>
2023-02-28 13:18:38 +0000

    CORE-10461 Use Fatal/Unknown notary errors (#3186)
    * CORE-10461 Use Fatal/Unknown notary errors
    
    * vbump
    
    * vbump
    
    * Rework with Java and bump API
    
    * modify kdocs
    
    * remove TODO
    
    * vbump, small cosmetics
    
    * vbump
    
    * vbump
    
    * vbump and log improve
    
    * fix compile
    
    * comment address, vbump
    
    * vbump

--- components/ledger/ledger-utxo-flow/src/main/kotlin/net/corda/ledger/utxo/flow/impl/flows/finality/UtxoReceiveFinalityFlow.kt
+++ components/ledger/ledger-utxo-flow/src/main/kotlin/net/corda/ledger/utxo/flow/impl/flows/finality/UtxoReceiveFinalityFlow.kt
@@ -131,7 +131,7 @@ class UtxoReceiveFinalityFlow(
                     "${transaction.id} with message: ${failure.message}"
             log.warn(message)
             val reason = failure.reason
-            if (reason != null && reason.toFinalityNotarizationFailureType() == FinalityNotarizationFailureType.UNRECOVERABLE) {
+            if (reason != null && reason.toFinalityNotarizationFailureType() == FinalityNotarizationFailureType.FATAL) {
                 persistInvalidTransaction(transaction)
             }
             CordaRuntimeException(message)
