35c4a28ce67a5e2ea9fffb87a744f242b4a1d925
LWogan <lorcan.wogan@r3.com>
2023-03-01 12:37:41 +0000

    CORE-8444 do not log secrets  in the db. make the db password a secret. fix up some old code in the boot config logic.

--- applications/workers/worker-common/src/main/kotlin/net/corda/applications/workers/workercommon/WorkerHelpers.kt
+++ applications/workers/worker-common/src/main/kotlin/net/corda/applications/workers/workercommon/WorkerHelpers.kt
@@ -11,11 +11,13 @@ import net.corda.libs.configuration.validation.ConfigurationValidator
 import net.corda.libs.platform.PlatformInfoProvider
 import net.corda.osgi.api.Shutdown
 import net.corda.schema.configuration.BootConfig.BOOT_DB
+import net.corda.schema.configuration.BootConfig.BOOT_JDBC_PASS
 import net.corda.schema.configuration.BootConfig.BOOT_KAFKA_COMMON
 import net.corda.schema.configuration.BootConfig.INSTANCE_ID
 import net.corda.schema.configuration.BootConfig.TOPIC_PREFIX
 import net.corda.schema.configuration.ConfigKeys
 import net.corda.schema.configuration.ConfigKeys.BOOT_CONFIG
+import net.corda.schema.configuration.MessagingConfig.Bus.BUS_TYPE
 import net.corda.schema.configuration.MessagingConfig.MAX_ALLOWED_MSG_SIZE
 import net.corda.v5.base.util.debug
 import org.osgi.framework.FrameworkUtil
@@ -75,7 +77,6 @@ class WorkerHelpers {
             defaultParams: DefaultWorkerParams,
             validator: ConfigurationValidator,
             extraParams: List<PathAndConfig> = emptyList(),
-            busType: BusType = BusType.KAFKA
         ): SmartConfig {
             val extraParamsMap = extraParams
                 .map { (path, params) -> params.mapKeys { (key, _) -> "$path.$key" } }
@@ -86,11 +87,11 @@ class WorkerHelpers {
                 ConfigKeys.WORKSPACE_DIR to defaultParams.workspaceDir,
                 ConfigKeys.TEMP_DIR to defaultParams.tempDir
             )
-
-            val messagingParams = if (busType == BusType.KAFKA) {
-                defaultParams.messagingParams.mapKeys { (key, _) -> "$BOOT_KAFKA_COMMON.${key.trim()}" }
+            val defaultMessagingParams = defaultParams.messagingParams
+            val messagingParams = if (defaultMessagingParams[BUS_TYPE] == BusType.KAFKA.name) {
+                defaultMessagingParams.mapKeys { (key, _) -> "$BOOT_KAFKA_COMMON.${key.trim()}" }
             } else {
-                defaultParams.messagingParams.mapKeys { (key, _) -> "$BOOT_DB.${key.trim()}" }
+                defaultMessagingParams.mapKeys { (key, _) -> "$BOOT_DB.${key.trim()}" }
             }
 
             val config = ConfigFactory
@@ -104,10 +105,12 @@ class WorkerHelpers {
 
             val smartConfigFactory = SmartConfigFactory
                 .createWith(secretsConfig, secretsServiceFactoryResolver.findAll())
+
             val bootConfig = smartConfigFactory.create(config)
+            val bootConfigWithSecrets = makeSecrets(smartConfigFactory, bootConfig)
             logger.debug { "Worker boot config\n: ${bootConfig.root().render()}" }
 
-            validator.validate(BOOT_CONFIG, bootConfig, loadResource(BOOT_CONFIG_PATH), true)
+            validator.validate(BOOT_CONFIG, bootConfigWithSecrets, loadResource(BOOT_CONFIG_PATH), true)
 
             // we now know bootConfig has:
             //
@@ -123,6 +126,21 @@ class WorkerHelpers {
             //  - have unspecified fields filled in with defaults from the schema. (This part is handled later)
 
             return bootConfig
+        }
+
+        /**
+         * Hide any fields which should be hidden such as passwords.
+         * @param smartConfigFactory used to generate secrets
+         * @param config base config
+         * @return New config object with boot values such as passwords made secret.
+         */
+        private fun makeSecrets(smartConfigFactory: SmartConfigFactory, config: SmartConfig): SmartConfig {
+            return if (config.hasPath(BOOT_JDBC_PASS)) {
+                smartConfigFactory.makeSecret(config.getString(BOOT_JDBC_PASS), "boot-db-pass").atPath(BOOT_JDBC_PASS)
+                    .withFallback(config)
+            } else {
+                config
+            }
         }
 
         private fun loadResource(resource: String): InputStream {
--- libs/messaging/db-message-bus-impl/src/main/kotlin/net/corda/messagebus/db/configuration/DbBusConfigMergerImpl.kt
+++ libs/messaging/db-message-bus-impl/src/main/kotlin/net/corda/messagebus/db/configuration/DbBusConfigMergerImpl.kt
@@ -16,7 +16,7 @@ import org.osgi.service.component.annotations.Component
 class DbBusConfigMergerImpl : BusConfigMerger {
 
     override fun getMessagingConfig(bootConfig: SmartConfig, messagingConfig: SmartConfig?): SmartConfig {
-        val updatedMessagingConfig = messagingConfig?: SmartConfigImpl.empty()
+        val updatedMessagingConfig = messagingConfig?: getBaseDBConfig(bootConfig)
         return updatedMessagingConfig
             .withValue(MessagingConfig.Bus.DB_JDBC_URL,
                 ConfigValueFactory.fromAnyRef(bootConfig.getStringOrNull(BootConfig.BOOT_JDBC_URL + "_messagebus")))
@@ -27,5 +27,14 @@ class DbBusConfigMergerImpl : BusConfigMerger {
             .withValue(MessagingConfig.Bus.BUS_TYPE, ConfigValueFactory.fromAnyRef("DATABASE"))
             .withValue(INSTANCE_ID, ConfigValueFactory.fromAnyRef(bootConfig.getString(INSTANCE_ID)))
             .withValue(TOPIC_PREFIX, ConfigValueFactory.fromAnyRef(bootConfig.getStringOrDefault(TOPIC_PREFIX, "")))
+    }
+
+    /**
+     * Mandatory messaging config generated from the [bootConfig] which is allowed to be overwritten by the message bus.
+     */
+    private fun getBaseDBConfig(bootConfig: SmartConfig): SmartConfig {
+        return SmartConfigImpl.empty()
+            .withValue(MessagingConfig.MAX_ALLOWED_MSG_SIZE,
+                ConfigValueFactory.fromAnyRef(bootConfig.getLong(BootConfig.BOOT_MAX_ALLOWED_MSG_SIZE)))
     }
 }
