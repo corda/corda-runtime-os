decbd188b28747cb084add7a5e4c3ca0d1a091c3
Chris Rankin <chris.rankin@r3.com>
2023-03-03 09:30:56 +0000

    CORE-11243: Configure Quasar to ignore Lifecycle and Security Manager. (#3295)

--- components/security-manager/src/main/java/net/corda/securitymanager/package-info.java
+++ components/security-manager/src/main/java/net/corda/securitymanager/package-info.java
@@ -1,4 +1,6 @@
 @Export
+@QuasarIgnoreAllPackages
 package net.corda.securitymanager;
 
+import co.paralleluniverse.quasar.annotations.QuasarIgnoreAllPackages;
 import org.osgi.annotation.bundle.Export;
