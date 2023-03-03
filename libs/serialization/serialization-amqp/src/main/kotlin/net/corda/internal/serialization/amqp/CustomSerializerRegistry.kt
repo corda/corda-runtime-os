b8c558c0c782c5f2b976f76a54ad3038014fe03c
Joseph Zuniga-Daly <joseph.zunigadaly@r3.com>
2023-03-03 17:01:42 +0000

    CORE-6971: Allow custom serializers against all CordaSerializable annotated classes

--- libs/serialization/serialization-amqp/src/main/kotlin/net/corda/internal/serialization/amqp/CustomSerializerRegistry.kt
+++ libs/serialization/serialization-amqp/src/main/kotlin/net/corda/internal/serialization/amqp/CustomSerializerRegistry.kt
@@ -12,7 +12,6 @@ import net.corda.serialization.InternalProxySerializer
 import net.corda.utilities.debug
 import net.corda.utilities.trace
 import net.corda.v5.base.annotations.CordaSerializable
-import net.corda.v5.base.annotations.DoNotImplement
 import net.corda.v5.base.exceptions.CordaThrowable
 import net.corda.v5.serialization.SerializationCustomSerializer
 import net.corda.v5.serialization.SingletonSerializeAsToken
@@ -265,8 +264,6 @@ class CachingCustomSerializerRegistry(
             AMQPTypeIdentifiers.isPrimitive(this) -> forbidden
             isSubClassOf(CordaThrowable::class.java) -> allowed
             allowedFor.any { it.isAssignableFrom(this) } -> allowed
-            isAnnotationPresent(CordaSerializable::class.java) && isAnnotationPresent(DoNotImplement::class.java) -> allowed
-            isAnnotationPresent(CordaSerializable::class.java) && !isAnnotationPresent(DoNotImplement::class.java) -> forbidden
             else -> allowed
         }
     }
