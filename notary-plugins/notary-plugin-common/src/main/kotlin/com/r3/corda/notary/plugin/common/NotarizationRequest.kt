9cb419f28ff3698e964495361542e4a472f6ca28
Ramzi El-Yafi <ramzi.el-yafi@r3.com>
2023-03-07 09:37:03 +0000

    CORE-11181 Notary code updates to conform to engineering code guidelines (#3300)

--- notary-plugins/notary-plugin-common/src/main/kotlin/com/r3/corda/notary/plugin/common/NotarizationRequest.kt
+++ notary-plugins/notary-plugin-common/src/main/kotlin/com/r3/corda/notary/plugin/common/NotarizationRequest.kt
@@ -5,12 +5,12 @@ import net.corda.v5.crypto.SecureHash
 import net.corda.v5.ledger.utxo.StateRef
 
 /**
- * A notarisation request specifies a list of states to consume and the id of the consuming transaction. Its primary
- * purpose is for notarisation traceability – a signature over the notarisation request, [NotarisationRequestSignature],
+ * A notarization request specifies a list of states to consume and the id of the consuming transaction. Its primary
+ * purpose is for notarization traceability – a signature over the notarization request, [NotarizationRequestSignature],
  * allows a notary to prove that a certain party requested the consumption of a particular state.
  */
 @CordaSerializable
-data class NotarisationRequest(
+data class NotarizationRequest(
     val statesToConsume: List<StateRef>,
     val transactionId: SecureHash
 )
