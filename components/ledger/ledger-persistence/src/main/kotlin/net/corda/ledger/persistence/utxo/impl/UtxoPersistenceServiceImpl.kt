a3ae82f7974426af5c3b49cef4ac7c04fabacd1d
Lajos Veres <lajos.veres@r3.com>
2023-03-10 10:25:47 +0000

    CORE-9980 Avoid nested transactions - single transaction approach

--- components/ledger/ledger-persistence/src/main/kotlin/net/corda/ledger/persistence/utxo/impl/UtxoPersistenceServiceImpl.kt
+++ components/ledger/ledger-persistence/src/main/kotlin/net/corda/ledger/persistence/utxo/impl/UtxoPersistenceServiceImpl.kt
@@ -15,6 +15,7 @@ import net.corda.v5.crypto.DigestAlgorithmName
 import net.corda.v5.ledger.common.transaction.CordaPackageSummary
 import net.corda.v5.ledger.utxo.ContractState
 import net.corda.v5.ledger.utxo.StateRef
+import javax.persistence.EntityManager
 import javax.persistence.EntityManagerFactory
 
 class UtxoPersistenceServiceImpl constructor(
@@ -78,108 +79,112 @@ class UtxoPersistenceServiceImpl constructor(
     }
 
     override fun persistTransaction(transaction: UtxoTransactionReader): List<CordaPackageSummary> {
+        entityManagerFactory.transaction { em ->
+            return persistTransaction(em, transaction)
+        }
+    }
+
+    private fun persistTransaction(em: EntityManager, transaction: UtxoTransactionReader): List<CordaPackageSummary> {
         val nowUtc = utcClock.instant()
         val transactionIdString = transaction.id.toString()
 
-        entityManagerFactory.transaction { em ->
-            // Insert the Transaction
-            repository.persistTransaction(
-                em,
-                transactionIdString,
-                transaction.privacySalt.bytes,
-                transaction.account,
-                nowUtc
-            )
+        // Insert the Transaction
+        repository.persistTransaction(
+            em,
+            transactionIdString,
+            transaction.privacySalt.bytes,
+            transaction.account,
+            nowUtc
+        )
 
-            // Insert the Transactions components
-            transaction.rawGroupLists.mapIndexed { groupIndex, leaves ->
-                leaves.mapIndexed { leafIndex, data ->
-                    repository.persistTransactionComponentLeaf(
-                        em,
-                        transactionIdString,
-                        groupIndex,
-                        leafIndex,
-                        data,
-                        sandboxDigestService.hash(data, DigestAlgorithmName.SHA2_256).toString(),
-                        nowUtc
-                    )
-                }
-            }
-
-            // Insert inputs data
-            val inputs = transaction.getConsumedStateRefs()
-            inputs.forEachIndexed  { index, input ->
-                repository.persistTransactionSource(
+        // Insert the Transactions components
+        transaction.rawGroupLists.mapIndexed { groupIndex, leaves ->
+            leaves.mapIndexed { leafIndex, data ->
+                repository.persistTransactionComponentLeaf(
                     em,
                     transactionIdString,
-                    UtxoComponentGroup.INPUTS.ordinal,
-                    index,
-                    input.transactionId.toString(),
-                    input.index,
-                    false,
+                    groupIndex,
+                    leafIndex,
+                    data,
+                    sandboxDigestService.hash(data, DigestAlgorithmName.SHA2_256).toString(),
                     nowUtc
                 )
             }
-
-            // Insert outputs data
-            transaction.getProducedStates().forEachIndexed { index, stateAndRef ->
-                repository.persistTransactionOutput(
-                    em,
-                    transactionIdString,
-                    UtxoComponentGroup.OUTPUTS.ordinal,
-                    index,
-                    stateAndRef.state.contractState::class.java.canonicalName,
-                    timestamp = nowUtc
-                )
-            }
-
-            // Insert relevancy information for outputs
-            transaction.relevantStatesIndexes.forEach { relevantStateIndex ->
-                repository.persistTransactionRelevantStates(
-                    em,
-                    transactionIdString,
-                    UtxoComponentGroup.OUTPUTS.ordinal,
-                    relevantStateIndex,
-                    consumed = false,
-                    nowUtc
-                )
-            }
-
-            // Mark inputs as consumed
-            if (transaction.status == TransactionStatus.VERIFIED) {
-                val inputStateRefs = transaction.getConsumedStateRefs()
-                if (inputStateRefs.isNotEmpty()) {
-                    repository.markTransactionRelevantStatesConsumed(
-                        em,
-                        inputStateRefs
-                    )
-                }
-            }
-
-            // Insert the Transactions signatures
-            transaction.signatures.forEachIndexed { index, digitalSignatureAndMetadata ->
-                repository.persistTransactionSignature(
-                    em,
-                    transactionIdString,
-                    index,
-                    digitalSignatureAndMetadata,
-                    nowUtc
-                )
-            }
-
-            // Insert the transactions current status
-            repository.persistTransactionStatus(
-                em,
-                transactionIdString,
-                transaction.status,
-                nowUtc
-            )
-
-            // Insert the CPK details liked to this transaction
-            // TODOs: The CPK file meta does not exist yet, this will be implemented by
-            // https://r3-cev.atlassian.net/browse/CORE-7626
-            return emptyList()
         }
+
+        // Insert inputs data
+        val inputs = transaction.getConsumedStateRefs()
+        inputs.forEachIndexed { index, input ->
+            repository.persistTransactionSource(
+                em,
+                transactionIdString,
+                UtxoComponentGroup.INPUTS.ordinal,
+                index,
+                input.transactionId.toString(),
+                input.index,
+                false,
+                nowUtc
+            )
+        }
+
+        // Insert outputs data
+        transaction.getProducedStates().forEachIndexed { index, stateAndRef ->
+            repository.persistTransactionOutput(
+                em,
+                transactionIdString,
+                UtxoComponentGroup.OUTPUTS.ordinal,
+                index,
+                stateAndRef.state.contractState::class.java.canonicalName,
+                timestamp = nowUtc
+            )
+        }
+
+        // Insert relevancy information for outputs
+        transaction.relevantStatesIndexes.forEach { relevantStateIndex ->
+            repository.persistTransactionRelevantStates(
+                em,
+                transactionIdString,
+                UtxoComponentGroup.OUTPUTS.ordinal,
+                relevantStateIndex,
+                consumed = false,
+                nowUtc
+            )
+        }
+
+        // Mark inputs as consumed
+        if (transaction.status == TransactionStatus.VERIFIED) {
+            val inputStateRefs = transaction.getConsumedStateRefs()
+            if (inputStateRefs.isNotEmpty()) {
+                repository.markTransactionRelevantStatesConsumed(
+                    em,
+                    inputStateRefs
+                )
+            }
+        }
+
+        // Insert the Transactions signatures
+        transaction.signatures.forEachIndexed { index, digitalSignatureAndMetadata ->
+            repository.persistTransactionSignature(
+                em,
+                transactionIdString,
+                index,
+                digitalSignatureAndMetadata,
+                nowUtc
+            )
+        }
+
+        // Insert the transactions current status
+        repository.persistTransactionStatus(
+            em,
+            transactionIdString,
+            transaction.status,
+            nowUtc
+        )
+
+        // Insert the CPK details liked to this transaction
+        // TODOs: The CPK file meta does not exist yet, this will be implemented by
+        // https://r3-cev.atlassian.net/browse/CORE-7626
+        return emptyList()
     }
 
     override fun persistTransactionIfDoesNotExist(transaction: UtxoTransactionReader): Pair<String?, List<CordaPackageSummary>> {
@@ -192,7 +197,7 @@ class UtxoPersistenceServiceImpl constructor(
                 return@transaction status to emptyList()
             }
 
-            val cpkDetails = persistTransaction(transaction)
+            val cpkDetails = persistTransaction(em, transaction)
 
             return null to cpkDetails
         }
