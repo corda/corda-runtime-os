ff52f4ff4933e7fda9c7032953d38100a0532f9b
Dickon Reed <dickon.reed@r3.com>
2023-03-13 17:58:01 +0000

    green bar: test we got back the instance type expected

--- components/crypto/crypto-persistence/src/test/kotlin/net/corda/crypto/persistence/TestCryptoRepositoryFactoryImpl.kt
+++ components/crypto/crypto-persistence/src/test/kotlin/net/corda/crypto/persistence/TestCryptoRepositoryFactoryImpl.kt
@@ -1,11 +1,15 @@
 package net.corda.crypto.persistence
 
+import net.corda.crypto.core.CryptoTenants
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.mockito.kotlin.mock
 
 class TestCryptoRepositoryFactoryImpl {
     @Test
     fun `go go go`() {
-        CryptoRepositoryFactoryImpl(mock(), mock(), mock(), mock())
+        val cut = CryptoRepositoryFactoryImpl(mock(), mock(), mock(), mock())
+        val repo = cut.create(CryptoTenants.CRYPTO)
+        assertThat(repo::class.simpleName).isEqualTo("V1CryptoRepositoryImpl")
     }
 }
