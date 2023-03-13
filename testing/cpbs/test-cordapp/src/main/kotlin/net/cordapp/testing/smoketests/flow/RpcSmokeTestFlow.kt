04a598d8ac10120ad2ee8d60acc5d90e21b3086e
LWogan <lorcan.wogan@r3.com>
2023-03-13 11:20:07 +0000

    CORE-6564 updates to return flow info from the flow

--- testing/cpbs/test-cordapp/src/main/kotlin/net/cordapp/testing/smoketests/flow/RpcSmokeTestFlow.kt
+++ testing/cpbs/test-cordapp/src/main/kotlin/net/cordapp/testing/smoketests/flow/RpcSmokeTestFlow.kt
@@ -1,5 +1,7 @@
 package net.cordapp.testing.smoketests.flow
 
+import java.time.Instant
+import java.util.UUID
 import net.corda.v5.application.crypto.DigitalSignatureVerificationService
 import net.corda.v5.application.crypto.SignatureSpecService
 import net.corda.v5.application.crypto.SigningService
@@ -27,11 +29,9 @@ import net.cordapp.testing.smoketests.flow.messages.JsonSerializationOutput
 import net.cordapp.testing.smoketests.flow.messages.RpcSmokeTestInput
 import net.cordapp.testing.smoketests.flow.messages.RpcSmokeTestOutput
 import org.slf4j.LoggerFactory
-import java.time.Instant
-import java.util.UUID
 
 @Suppress("unused", "TooManyFunctions")
-@InitiatingFlow(protocol = "smoke-test-protocol")
+@InitiatingFlow(protocol = "smoke-test-protocol", version = [2,3,4])
 class RpcSmokeTestFlow : ClientStartableFlow {
 
     private companion object {
@@ -96,6 +96,7 @@ class RpcSmokeTestFlow : ClientStartableFlow {
 
     @Suspendable
     override fun call(requestBody: ClientRequestBody): String {
+        flowEngine.flowContextProperties
         val request = requestBody.getRequestBodyAs(jsonMarshallingService, RpcSmokeTestInput::class.java)
         return jsonMarshallingService.format(execute(request))
     }
