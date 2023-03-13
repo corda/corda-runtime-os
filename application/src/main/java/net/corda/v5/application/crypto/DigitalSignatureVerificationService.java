518c84e3bdf25f9e6716fb0a980e2ebebe806a13
Anna Paskova <anna.paskova@r3.com>
2023-03-13 13:37:44 +0000

    CORE-9332: Align `DigitalSignatureVerificationService.verify` overloads (#928)
    * Change the signature order of one verify method to align with the second verify method in `DigitalSignatureVerificationService`
    * Rename `clearData` to `originalData` to align with the rest of the interface

--- application/src/main/java/net/corda/v5/application/crypto/DigitalSignatureVerificationService.java
+++ application/src/main/java/net/corda/v5/application/crypto/DigitalSignatureVerificationService.java
@@ -15,33 +15,30 @@ import java.security.PublicKey;
  */
 @DoNotImplement
 public interface DigitalSignatureVerificationService {
-    // TODO The following `verify` overload should be aligned with the other one as per: https://r3-cev.atlassian.net/browse/CORE-9332
     /**
      * Verifies a digital signature by using {@code signatureSpec}.
      * Always throws an exception if verification fails.
      *
-     * @param publicKey The signer's {@link PublicKey}.
-     * @param signatureSpec The signature spec.
+     * @param originalData  The original data/message that was signed (usually the Merkle root).
      * @param signatureData The signatureData on a message.
-     * @param clearData The clear data/message that was signed (usually the Merkle root).
-     *
+     * @param publicKey     The signer's {@link PublicKey}.
+     * @param signatureSpec The signature spec.
      * @throws CryptoSignatureException If verification of the digital signature fails.
-     * @throws IllegalArgumentException If the signature scheme is not supported or if any of the clear or signature
-     * data is empty.
+     * @throws IllegalArgumentException If the signature scheme is not supported or if any of the original or signature
+     *                                  data is empty.
      */
-    void verify(@NotNull PublicKey publicKey, @NotNull SignatureSpec signatureSpec, @NotNull byte[] signatureData, @NotNull byte[] clearData);
+    void verify(@NotNull byte[] originalData, @NotNull byte[] signatureData, @NotNull PublicKey publicKey, @NotNull SignatureSpec signatureSpec);
 
     /**
      * Verifies a digital signature against data. Throws {@link CryptoSignatureException} if verification fails.
      *
-     * @param originalData The original data on which the signature was applied (usually the Merkle root).
-     * @param signature The digital signature.
-     * @param publicKey The signer's {@link PublicKey}.
+     * @param originalData  The original data on which the signature was applied (usually the Merkle root).
+     * @param signature     The digital signature.
+     * @param publicKey     The signer's {@link PublicKey}.
      * @param signatureSpec The signature spec.
-     *
      * @throws CryptoSignatureException If verification of the digital signature fails.
-     * @throws IllegalArgumentException If the signature scheme is not supported or if any of the clear or signature
-     * data is empty.
+     * @throws IllegalArgumentException If the signature scheme is not supported or if any of the original or signature
+     *                                  data is empty.
      */
     void verify(@NotNull byte[] originalData, @NotNull DigitalSignature signature, @NotNull PublicKey publicKey, @NotNull SignatureSpec signatureSpec);
 }
