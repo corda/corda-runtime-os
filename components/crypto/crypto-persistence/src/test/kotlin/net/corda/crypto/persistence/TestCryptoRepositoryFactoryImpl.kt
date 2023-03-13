90544622a584b5f605f6584bfcf611b9d506573f
Dickon Reed <dickon.reed@r3.com>
2023-03-13 22:28:15 +0000

    green bar: check virtual node creation

--- components/crypto/crypto-persistence/src/test/kotlin/net/corda/crypto/persistence/TestCryptoRepositoryFactoryImpl.kt
+++ components/crypto/crypto-persistence/src/test/kotlin/net/corda/crypto/persistence/TestCryptoRepositoryFactoryImpl.kt
@@ -7,6 +7,9 @@ import net.corda.crypto.core.CryptoTenants
 import net.corda.db.connection.manager.DbConnectionManager
 import net.corda.db.core.DbPrivilege
 import net.corda.db.schema.CordaDb
+import net.corda.orm.JpaEntitiesRegistry
+import net.corda.virtualnode.VirtualNodeInfo
+import net.corda.virtualnode.read.VirtualNodeInfoReadService
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.mockito.kotlin.any
@@ -36,6 +39,33 @@ class TestCryptoRepositoryFactoryImpl {
         verify(dbConnectionManager).getOrCreateEntityManagerFactory(CordaDb.Crypto, DbPrivilege.DML)
         verifyNoMoreInteractions(dbConnectionManager)
         cut.create(CryptoTenants.P2P)
+        verifyNoMoreInteractions(dbConnectionManager)
+    }
+
+    @Test
+    fun `Fresh database connections for virtual node and P2P`() {
+        val virtualNodeInfo = mock<VirtualNodeInfo> {
+            on { cryptoDmlConnectionId } doReturn mock()
+        }
+        val dbConnectionManager = mock<DbConnectionManager> {
+            on { getOrCreateEntityManagerFactory(any(), any()) } doReturn mock()
+            on { createEntityManagerFactory(any(), any()) } doReturn mock()
+        }
+        val virtualNodeInfoReadService = mock<VirtualNodeInfoReadService> {
+            on { getByHoldingIdentityShortHash(any()) } doReturn virtualNodeInfo
+        }
+        val jpaEntitiesRegistry = mock<JpaEntitiesRegistry> {
+            on { get(any()) } doReturn mock()
+        }
+        val cache = makeConnectionCache()
+        val cut =
+            CryptoRepositoryFactoryImpl(dbConnectionManager, jpaEntitiesRegistry, virtualNodeInfoReadService, cache)
+        verifyNoMoreInteractions(dbConnectionManager)
+        cut.create(CryptoTenants.P2P)
+        verify(dbConnectionManager).getOrCreateEntityManagerFactory(CordaDb.Crypto, DbPrivilege.DML)
+        verifyNoMoreInteractions(dbConnectionManager)
+        cut.create("123456789012") // try shorter, ShortHash bombs
+        verify(dbConnectionManager).createEntityManagerFactory(any(), any())
         verifyNoMoreInteractions(dbConnectionManager)
     }
 }
