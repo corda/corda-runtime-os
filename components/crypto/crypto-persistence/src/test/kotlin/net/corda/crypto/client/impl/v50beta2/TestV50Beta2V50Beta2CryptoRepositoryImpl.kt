bed9b4ee5c9360f2677d52fdb5375e4b514e25b8
Dickon Reed <dickon.reed@r3.com>
2023-03-13 11:19:42 +0000

    unit test for saveWrappingKey

--- components/crypto/crypto-persistence/src/test/kotlin/net/corda/crypto/client/impl/v50beta2/TestV50Beta2V50Beta2CryptoRepositoryImpl.kt
+++ components/crypto/crypto-persistence/src/test/kotlin/net/corda/crypto/client/impl/v50beta2/TestV50Beta2V50Beta2CryptoRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package net.corda.crypto.client.impl.v50beta2
 
+import net.corda.crypto.persistence.WrappingKeyInfo
 import net.corda.crypto.persistence.v50beta2.V50Beta2CryptoRepositoryImpl
 import org.junit.jupiter.api.Test
 import org.mockito.kotlin.any
@@ -13,15 +14,14 @@ class TestV50Beta2CryptoRepositoryImpl {
     @Test
     fun `go`() {
         val em: EntityManager = mock {
-            on { persist(any()) } doAnswer {
-
-            }
+            on { persist(any()) } doAnswer {}
+            on { transaction } doReturn mock()
         }
         em.persist(1)
-//        val repo = V50Beta2CryptoRepositoryImpl(mock {
-//            on(mock()) doReturn em
-//        })
-//        repo.saveWrappingKey("a", mock())
+        val repo = V50Beta2CryptoRepositoryImpl(mock {
+            on(mock()) doReturn em
+        })
+        repo.saveWrappingKey("a", WrappingKeyInfo(1, "caesar", byteArrayOf()))
     }
 
 }
