8f5cc8123a0e89c7ee2cf9c454bf89b246f41d41
Dickon Reed <dickon.reed@r3.com>
2023-03-13 17:55:03 +0000

    green bar: instantiate factory

--- components/crypto/crypto-persistence/src/test/kotlin/net/corda/crypto/persistence/TestCryptoRepositoryFactoryImpl.kt
+++ components/crypto/crypto-persistence/src/test/kotlin/net/corda/crypto/persistence/TestCryptoRepositoryFactoryImpl.kt
@@ -1,10 +1,11 @@
 package net.corda.crypto.persistence
 
 import org.junit.jupiter.api.Test
+import org.mockito.kotlin.mock
 
 class TestCryptoRepositoryFactoryImpl {
     @Test
     fun `go go go`() {
-
+        CryptoRepositoryFactoryImpl(mock(), mock(), mock(), mock())
     }
 }
