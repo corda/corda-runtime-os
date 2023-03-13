77dcc206f6b8cdb3a9c457fdb9515b09262a322b
Dickon Reed <dickon.reed@r3.com>
2023-03-13 21:35:20 +0000

    refactor: grab dbConnectionManager

--- components/crypto/crypto-persistence/src/test/kotlin/net/corda/crypto/persistence/TestCryptoRepositoryFactoryImpl.kt
+++ components/crypto/crypto-persistence/src/test/kotlin/net/corda/crypto/persistence/TestCryptoRepositoryFactoryImpl.kt
@@ -1,14 +1,16 @@
 package net.corda.crypto.persistence
 
 import net.corda.crypto.core.CryptoTenants
+import net.corda.db.connection.manager.DbConnectionManager
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.mockito.kotlin.mock
 
 class TestCryptoRepositoryFactoryImpl {
     @Test
-    fun `go go go`() {
-        val cut = CryptoRepositoryFactoryImpl(mock(), mock(), mock(), mock())
+    fun `different virtual nodes should have different crypto repositories`() {
+        val dbConnectionManager = mock<DbConnectionManager>()
+        val cut = CryptoRepositoryFactoryImpl(dbConnectionManager, mock(), mock(), mock())
         val repo = cut.create(CryptoTenants.CRYPTO)
         assertThat(repo::class.simpleName).isEqualTo("V1CryptoRepositoryImpl")
     }
