c18818ccb378f4a3d69697194d58302c5ce277a6
LWogan <lorcan.wogan@r3.com>
2023-03-13 11:20:07 +0000

    CORE-6564 updates to return flow info from the flow

--- components/flow/flow-service/src/main/kotlin/net/corda/flow/pipeline/handlers/waiting/sessions/CounterPartyFlowInfoWaitingForHandler.kt
+++ components/flow/flow-service/src/main/kotlin/net/corda/flow/pipeline/handlers/waiting/sessions/CounterPartyFlowInfoWaitingForHandler.kt
@@ -3,15 +3,10 @@
 import net.corda.data.flow.state.session.SessionState
 import net.corda.data.flow.state.session.SessionStateType
 import net.corda.data.flow.state.waiting.CounterPartyFlowInfo
-import net.corda.flow.application.sessions.FlowInfoImpl
 import net.corda.flow.fiber.FlowContinuation
 import net.corda.flow.pipeline.events.FlowEventContext
 import net.corda.flow.pipeline.exceptions.FlowPlatformException
 import net.corda.flow.pipeline.handlers.waiting.FlowWaitingForHandler
-import net.corda.flow.utils.KeyValueStore
-import net.corda.session.manager.Constants.Companion.FLOW_PROTOCOL
-import net.corda.session.manager.Constants.Companion.FLOW_PROTOCOL_VERSION_USED
-import net.corda.v5.application.messaging.FlowInfo
 import net.corda.v5.base.exceptions.CordaRuntimeException
 import org.osgi.service.component.annotations.Component
 
@@ -40,16 +35,13 @@ class CounterPartyFlowInfoWaitingForHandler : FlowWaitingForHandler<CounterParty
             SessionStateType.ERROR -> {
                 FlowContinuation.Error(
                     CordaRuntimeException(
-                        "Failed to get counterparty info due to sessions with errored status."
+                        "Failed to get counterparty info due to sessions with errorred status."
                     )
                 )
             }
-            else -> FlowContinuation.Run(getFlowEventInfo(sessionState))
+            else -> FlowContinuation.Run(Unit)
         }
     }
 
-     private fun getFlowEventInfo(sessionState: SessionState): FlowInfo {
-         val props = KeyValueStore(sessionState.counterpartySessionProperties)
-         return FlowInfoImpl(props[FLOW_PROTOCOL].toString(), props[FLOW_PROTOCOL_VERSION_USED]!!.toInt())
-     }
+
  }
