177801e0ee36375920ce0f38a9a7448f9c476999
Dickon Reed <dickon.reed@r3.com>
2023-03-13 21:39:36 +0000

    green bar: check all interactions and rename test

--- components/crypto/crypto-persistence/src/test/kotlin/net/corda/crypto/persistence/TestCryptoRepositoryFactoryImpl.kt
+++ components/crypto/crypto-persistence/src/test/kotlin/net/corda/crypto/persistence/TestCryptoRepositoryFactoryImpl.kt
@@ -2,16 +2,22 @@ package net.corda.crypto.persistence
 
 import net.corda.crypto.core.CryptoTenants
 import net.corda.db.connection.manager.DbConnectionManager
+import net.corda.db.core.DbPrivilege
+import net.corda.db.schema.CordaDb
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.mockito.kotlin.mock
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.verifyNoMoreInteractions
 
 class TestCryptoRepositoryFactoryImpl {
     @Test
-    fun `different virtual nodes should have different crypto repositories`() {
+    fun `different DML for Crypto tenant`() {
         val dbConnectionManager = mock<DbConnectionManager>()
         val cut = CryptoRepositoryFactoryImpl(dbConnectionManager, mock(), mock(), mock())
         val repo = cut.create(CryptoTenants.CRYPTO)
         assertThat(repo::class.simpleName).isEqualTo("V1CryptoRepositoryImpl")
+        verify(dbConnectionManager).getOrCreateEntityManagerFactory(CordaDb.Crypto, DbPrivilege.DML)
+        verifyNoMoreInteractions(dbConnectionManager)
     }
 }
