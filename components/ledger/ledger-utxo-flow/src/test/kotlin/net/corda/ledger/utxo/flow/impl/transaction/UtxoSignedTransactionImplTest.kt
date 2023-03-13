632ce02d813765dbd8ac78c16980fec4147a32f6
Lajos Veres <lajos.veres@r3.com>
2023-03-13 15:53:21 +0000

    CORE-9980 Debug to be reverted/fixed

--- components/ledger/ledger-utxo-flow/src/test/kotlin/net/corda/ledger/utxo/flow/impl/transaction/UtxoSignedTransactionImplTest.kt
+++ components/ledger/ledger-utxo-flow/src/test/kotlin/net/corda/ledger/utxo/flow/impl/transaction/UtxoSignedTransactionImplTest.kt
@@ -8,6 +8,7 @@ import net.corda.ledger.utxo.testkit.UtxoCommandExample
 import net.corda.ledger.utxo.testkit.getUtxoStateExample
 import net.corda.ledger.utxo.testkit.utxoTimeWindowExample
 import net.corda.v5.base.types.MemberX500Name
+import net.corda.v5.crypto.KeyUtils
 import net.corda.v5.ledger.common.Party
 import net.corda.v5.ledger.common.transaction.TransactionSignatureException
 import org.assertj.core.api.Assertions
@@ -21,9 +22,10 @@ internal class UtxoSignedTransactionImplTest: UtxoLedgerTest() {
     private lateinit var signedTransaction: UtxoSignedTransactionInternal
 
     private val kpg: KeyPairGenerator = KeyPairGenerator.getInstance("EC").apply { initialize(ECGenParameterSpec("secp256r1")) }
-    private val notaryNode1PublicKey = kpg.generateKeyPair().public
-    private val notaryNode2PublicKey = kpg.generateKeyPair().public
-    private val notaryKey = CompositeKeyProviderImpl().createFromKeys(listOf(notaryNode1PublicKey, notaryNode2PublicKey), 1)
+    private val notaryNode1PublicKey = kpg.generateKeyPair().public.also{println(it)}
+    private val notaryNode2PublicKey = kpg.generateKeyPair().public.also{println(it)}
+    private val notaryKey =
+        CompositeKeyProviderImpl().createFromKeys(listOf(notaryNode1PublicKey, notaryNode2PublicKey), 1).also{println(it)}
     private val notaryX500Name = MemberX500Name.parse("O=ExampleNotaryService, L=London, C=GB")
     private val notary = Party(notaryX500Name, notaryKey)
 
@@ -51,7 +53,41 @@ internal class UtxoSignedTransactionImplTest: UtxoLedgerTest() {
 
     @Test
     fun `verifyNotarySignatureAttached does not throw on notarized transaction`() {
-        signedTransaction = signedTransaction.addSignature(getSignatureWithMetadataExample(notaryNode1PublicKey))
+        val sig = getSignatureWithMetadataExample(notaryNode1PublicKey)
+        println(sig.by)
+        signedTransaction = signedTransaction.addSignature(sig)
+
+        val publicKeys = signedTransaction.signatures.map { it.by }
+
+        println("notary.owningKey: ${notary.owningKey}")
+        println("signedTransaction.notary.owningKey: ${signedTransaction.notary.owningKey}")
+
+        if (KeyUtils.isKeyInSet(notary.owningKey, publicKeys)) {
+            println("KeyUtils.isKeyInSet(notary.owningKey, publicKeys) passed")
+        } else {
+            println("KeyUtils.isKeyInSet(notary.owningKey, publicKeys) not passed")
+        }
+        if (KeyUtils.isKeyFulfilledBy(notary.owningKey, publicKeys)) {
+            println("KeyUtils.isKeyFulfilledBy(notary.owningKey, publicKeys) passed")
+        } else {
+            println("KeyUtils.isKeyFulfilledBy(notary.owningKey, publicKeys) not passed")
+        }
+        if (notary.owningKey == signedTransaction.notary.owningKey) {
+            println("notary.owningKey  == signedTransaction.notary.owningKey passed")
+        } else {
+            println("notary.owningKey  == signedTransaction.notary.owningKey not passed")
+        }
+        if (KeyUtils.isKeyInSet(signedTransaction.notary.owningKey, publicKeys)) {
+            println("KeyUtils.isKeyInSet(signedTransaction.notary.owningKey, publicKeys) passed")
+        } else {
+            println("KeyUtils.isKeyInSet(signedTransaction.notary.owningKey, publicKeys) not passed")
+        }
+        if (KeyUtils.isKeyFulfilledBy(signedTransaction.notary.owningKey, publicKeys)) {
+            println("KeyUtils.isKeyFulfilledBy(signedTransaction.notary.owningKey, publicKeys) passed")
+        } else {
+            println("KeyUtils.isKeyFulfilledBy(signedTransaction.notary.owningKey, publicKeys) not passed")
+        }
+
         assertDoesNotThrow {
             signedTransaction.verifyNotarySignatureAttached()
         }
