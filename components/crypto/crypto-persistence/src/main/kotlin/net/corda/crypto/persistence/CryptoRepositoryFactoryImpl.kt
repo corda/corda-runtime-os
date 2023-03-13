baed53b8a8638f7dea08c01dc7d136b38e294444
Dickon Reed <dickon.reed@r3.com>
2023-03-13 22:17:30 +0000

    green bar: cache cluster connections

--- components/crypto/crypto-persistence/src/main/kotlin/net/corda/crypto/persistence/CryptoRepositoryFactoryImpl.kt
+++ components/crypto/crypto-persistence/src/main/kotlin/net/corda/crypto/persistence/CryptoRepositoryFactoryImpl.kt
@@ -1,10 +1,7 @@
 package net.corda.crypto.persistence
 
 import com.github.benmanes.caffeine.cache.Cache
-import com.github.benmanes.caffeine.cache.Caffeine
-import net.corda.cache.caffeine.CacheFactory
 import net.corda.crypto.core.CryptoTenants
-import net.corda.crypto.core.CryptoTenants.REST
 import net.corda.crypto.core.ShortHash
 import net.corda.crypto.persistence.v1schema.V1CryptoRepositoryImpl
 import net.corda.db.connection.manager.DbConnectionManager
@@ -13,39 +10,25 @@ import net.corda.db.core.DbPrivilege
 import net.corda.db.schema.CordaDb
 import net.corda.orm.JpaEntitiesRegistry
 import net.corda.virtualnode.read.VirtualNodeInfoReadService
-import org.osgi.service.component.annotations.Activate
-import org.osgi.service.component.annotations.Component
-import org.osgi.service.component.annotations.Reference
-import java.util.concurrent.TimeUnit
 import javax.persistence.EntityManagerFactory
 
 
-@Component(service = [CryptoRepositoryFactory::class])
 class CryptoRepositoryFactoryImpl
-@Activate constructor(
-    @Reference(service = DbConnectionManager::class)
+constructor(
     private val dbConnectionManager: DbConnectionManager,
-    @Reference(service = JpaEntitiesRegistry::class)
     private val jpaEntitiesRegistry: JpaEntitiesRegistry,
-    @Reference(service = VirtualNodeInfoReadService::class)
     private val virtualNodeInfoReadService: VirtualNodeInfoReadService,
-    @Reference(service = CacheFactory::class)
-    private val cacheFactory: CacheFactory
+    private val connectionsCache: Cache<String, EntityManagerFactory>
 ) : CryptoRepositoryFactory {
-    private val connectionsCache: Cache<String, EntityManagerFactory> = cacheFactory.build(
-        "Crypto-Db-Connections-Cache",
-        Caffeine.newBuilder()
-            .expireAfterAccess(60 * 24, TimeUnit.MINUTES) // TODO dynamic config
-            .maximumSize(3600) // TODO dynamic config
-            .evictionListener { _, value, _ -> value?.close() })
-
     override fun create(tenantId: String): CryptoRepository {
-        val entityManagerFactory = if (CryptoTenants.isClusterTenant(tenantId)) {
-            // tenantID is crypto, P2P or REST; let's obtain connect to our cluster Crypto database 
-            dbConnectionManager.getOrCreateEntityManagerFactory(CordaDb.Crypto, DbPrivilege.DML)
-        } else {
-            // tenantID is a virtual node; let's connect to one of the virtual node Crypto databases
-            connectionsCache.get(tenantId) {
+        val onCluster = CryptoTenants.isClusterTenant(tenantId)
+        val key = if (onCluster) CordaDb.Crypto.toString() else tenantId
+        val entityManagerFactory = connectionsCache.get(key) {
+            if (onCluster) {
+                // tenantID is crypto, P2P or REST; let's obtain connect to our cluster Crypto database 
+                dbConnectionManager.getOrCreateEntityManagerFactory(CordaDb.Crypto, DbPrivilege.DML)
+            } else {
+                // tenantID is a virtual node; let's connect to one of the virtual node Crypto databases
                 dbConnectionManager.createEntityManagerFactory(
                     connectionId = virtualNodeInfoReadService.getByHoldingIdentityShortHash(
                         ShortHash.of(
@@ -62,6 +45,7 @@ class CryptoRepositoryFactoryImpl
                 )
             }
         }
+
 //        // somehow figure out which version we want, e.g.
 //        when (entityManagerFactory.getSchemaVersion()) {
 //            1 -> V1CryptoRepositoryImpl(entityManagerFactory)
--- components/crypto/crypto-persistence/src/test/kotlin/net/corda/crypto/persistence/TestCryptoRepositoryFactoryImpl.kt
+++ components/crypto/crypto-persistence/src/test/kotlin/net/corda/crypto/persistence/TestCryptoRepositoryFactoryImpl.kt
@@ -1,20 +1,36 @@
 package net.corda.crypto.persistence
 
+import com.github.benmanes.caffeine.cache.Cache
+import com.github.benmanes.caffeine.cache.Caffeine
+import net.corda.cache.caffeine.CacheFactoryImpl
 import net.corda.crypto.core.CryptoTenants
 import net.corda.db.connection.manager.DbConnectionManager
 import net.corda.db.core.DbPrivilege
 import net.corda.db.schema.CordaDb
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.mockito.kotlin.any
+import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.verify
 import org.mockito.kotlin.verifyNoMoreInteractions
+import java.util.concurrent.TimeUnit
+import javax.persistence.EntityManagerFactory
+
+fun makeConnectionCache(): Cache<String, EntityManagerFactory> = CacheFactoryImpl().build(
+    "connection cache", Caffeine.newBuilder()
+        .expireAfterAccess(24 * 60, TimeUnit.MINUTES)
+        .maximumSize(100)
+)
 
 class TestCryptoRepositoryFactoryImpl {
     @Test
-    fun `DML to Corda crypto DB for Crypto tenant`() {
-        val dbConnectionManager = mock<DbConnectionManager>()
-        val cut = CryptoRepositoryFactoryImpl(dbConnectionManager, mock(), mock(), mock())
+    fun `DML to Corda crypto DB for Crypto tenant and P2P`() {
+        val dbConnectionManager = mock<DbConnectionManager> {
+            on { getOrCreateEntityManagerFactory(any(), any()) } doReturn mock()
+        }
+        val cache = makeConnectionCache()
+        val cut = CryptoRepositoryFactoryImpl(dbConnectionManager, mock(), mock(), cache)
         val repo = cut.create(CryptoTenants.CRYPTO)
         assertThat(repo::class.simpleName).isEqualTo("V1CryptoRepositoryImpl")
         verify(dbConnectionManager).getOrCreateEntityManagerFactory(CordaDb.Crypto, DbPrivilege.DML)
