39999f981fe0dce5c6d9d88672ac482cdb4ee4ec
Dickon Reed <dickon.reed@r3.com>
2023-03-13 15:21:54 +0000

    green bar: get working

--- components/crypto/crypto-persistence/src/test/kotlin/net/corda/crypto/client/impl/v50beta2/TestV50Beta2V50Beta2CryptoRepositoryImpl.kt
+++ components/crypto/crypto-persistence/src/test/kotlin/net/corda/crypto/client/impl/v50beta2/TestV50Beta2V50Beta2CryptoRepositoryImpl.kt
@@ -2,6 +2,7 @@ package net.corda.crypto.client.impl.v50beta2
 
 import net.corda.crypto.persistence.WrappingKeyInfo
 import net.corda.crypto.persistence.v50beta2.V50Beta2CryptoRepositoryImpl
+import net.corda.crypto.persistence.v50beta2.V50Beta2WrappingKeyEntity
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.mockito.kotlin.any
@@ -9,23 +10,28 @@ import org.mockito.kotlin.doAnswer
 import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.mock
 import javax.persistence.EntityManager
+import kotlin.test.assertNotNull
 
 
 class TestV50Beta2CryptoRepositoryImpl {
     @Test
     fun `save a wrapping key`() {
-        val em: EntityManager = mock {
-            on { persist(any()) } doAnswer {}
+        val stored = ArrayList<V50Beta2WrappingKeyEntity>()
+        val em = mock<EntityManager> {
+            on { persist(any()) } doAnswer {
+                stored.add(it.getArgument(0))
+                Unit
+            }
+            on { find<V50Beta2WrappingKeyEntity>(any(), any()) } doAnswer { stored.first() }
             on { transaction } doReturn mock()
         }
-        em.persist(1)
         val repo = V50Beta2CryptoRepositoryImpl(mock {
             on(mock()) doReturn em
         })
         val wrappingKeyInfo = WrappingKeyInfo(1, "caesar", byteArrayOf())
         repo.saveWrappingKey("a", wrappingKeyInfo)
         val retrievedWrappingKeyInfo = repo.findWrappingKey("a")
-        assertThat(wrappingKeyInfo).isEqualTo(retrievedWrappingKeyInfo)
+        assertNotNull(retrievedWrappingKeyInfo)
+        assertThat(wrappingKeyInfo.encodingVersion).isEqualTo(retrievedWrappingKeyInfo.encodingVersion)
     }
-    
 }
