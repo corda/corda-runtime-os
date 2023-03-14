4d8a8a72f38d2550589e3e05da6357c136db045b
Alexander Cramb <alex.cramb@r3.com>
2023-03-14 17:47:52 +0000

    Update facade invocation flow

--- testing/cpbs/test-cordapp/src/main/kotlin/net/cordapp/testing/smoketests/flow/FacadeInvocationFlow.kt
+++ testing/cpbs/test-cordapp/src/main/kotlin/net/cordapp/testing/smoketests/flow/FacadeInvocationFlow.kt
@@ -1,15 +1,13 @@
-package net.cordapp.testing.testflows
+package net.cordapp.testing.smoketests.flow
 
-import net.corda.v5.application.flows.ClientStartableFlow
-import net.corda.v5.application.flows.CordaInject
-import net.corda.v5.application.flows.InitiatingFlow
-import net.corda.v5.application.flows.RestRequestBody
+import net.corda.v5.application.flows.*
+import net.corda.v5.application.marshalling.JsonMarshallingService
 import net.corda.v5.application.messaging.FlowMessaging
 import net.corda.v5.base.annotations.Suspendable
 import net.corda.v5.base.types.MemberX500Name
 import org.slf4j.LoggerFactory
 
-@InitiatingFlow(protocol = "facade_invocation_flow")
+@InitiatingFlow(protocol = "invoke_facade_method")
 class FacadeInvocationFlow : ClientStartableFlow {
     private companion object {
         val log = LoggerFactory.getLogger(this::class.java.enclosingClass)
@@ -25,23 +23,30 @@ class FacadeInvocationFlow : ClientStartableFlow {
     @CordaInject
     lateinit var flowMessaging: FlowMessaging
 
+    @CordaInject
+    lateinit var jsonMarshallingService: JsonMarshallingService
+
     @Suspendable
     override fun call(requestBody: RestRequestBody): String {
-        log.info("FacadeInfocationFlow.call() starting...")
+        log.info("FacadeInfocationFlow.call() starting")
 
-        val message = requestBody.getRequestBody()
+        val args = requestBody.getRequestBodyAs<Map<String, String>>(jsonMarshallingService)
 
-        log.info("Calling facade with payload '$message'...")
+        val facadeName = args.getValue("facadeName")
+        val methodName = args.getValue("methodName")
+        val payload = args.getValue("payload")
+
+        log.info("Calling facade method '$methodName@$facadeName' with payload '$payload'")
 
         val response = flowMessaging.callFacade(
             memberName = alterEgoX500Name,
-            facadeName = "None",
-            methodName = "None",
-            payload = message
+            facadeName = facadeName,
+            methodName = methodName,
+            payload = payload
         )
 
         log.info("Facade responded with '$response'")
-        log.info("FacadeInvocationFlow.call() ending...")
+        log.info("FacadeInvocationFlow.call() ending")
 
         return response
     }
