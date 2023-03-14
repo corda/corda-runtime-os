8d3155d8833bb411ea907c388923494aa0bb34e6
Alexander Cramb <alex.cramb@r3.com>
2023-03-14 12:21:24 +0000

    Fix compilation error in test

--- testing/cpbs/interop-facade-flow/src/main/kotlin/org/example/interop/FacadeInvocationFlow.kt
+++ testing/cpbs/interop-facade-flow/src/main/kotlin/org/example/interop/FacadeInvocationFlow.kt
@@ -4,11 +4,11 @@ import net.corda.v5.application.flows.*
 import net.corda.v5.application.messaging.FlowMessaging
 import net.corda.v5.base.annotations.Suspendable
 import net.corda.v5.base.types.MemberX500Name
-import net.corda.v5.base.util.contextLogger
+import org.slf4j.LoggerFactory
 
 class FacadeInvocationFlow : ClientStartableFlow {
     private companion object {
-        val log = contextLogger()
+        val log = LoggerFactory.getLogger(this::class.java.enclosingClass)
     }
 
     private val alterEgoX500Name = MemberX500Name(
