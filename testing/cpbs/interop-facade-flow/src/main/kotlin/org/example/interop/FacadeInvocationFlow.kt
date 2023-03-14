cdf1a9ea2306b11e4b035b0ab5eadb3401a13924
Alexander Cramb <alex.cramb@r3.com>
2023-03-14 14:43:10 +0000

    Add initiating flow annotation to facade test flow

--- testing/cpbs/interop-facade-flow/src/main/kotlin/org/example/interop/FacadeInvocationFlow.kt
+++ testing/cpbs/interop-facade-flow/src/main/kotlin/org/example/interop/FacadeInvocationFlow.kt
@@ -2,12 +2,14 @@ package org.example.interop
 
 import net.corda.v5.application.flows.ClientStartableFlow
 import net.corda.v5.application.flows.CordaInject
+import net.corda.v5.application.flows.InitiatingFlow
 import net.corda.v5.application.flows.RestRequestBody
 import net.corda.v5.application.messaging.FlowMessaging
 import net.corda.v5.base.annotations.Suspendable
 import net.corda.v5.base.types.MemberX500Name
 import org.slf4j.LoggerFactory
 
+@InitiatingFlow("facade-invocation-flow")
 class FacadeInvocationFlow : ClientStartableFlow {
     private companion object {
         val log = LoggerFactory.getLogger(this::class.java.enclosingClass)
