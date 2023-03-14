734f9aaeab6b0a71cf7b234332bf0de85a929c0d
Alexander Cramb <alex.cramb@r3.com>
2023-03-14 14:46:16 +0000

    Add mocked flow messaging object to test

--- testing/cpbs/interop-facade-flow/src/test/kotlin/org/example/interop/FacadeInvocationFlowTest.kt
+++ testing/cpbs/interop-facade-flow/src/test/kotlin/org/example/interop/FacadeInvocationFlowTest.kt
@@ -1,18 +1,22 @@
 package org.example.interop
 
 import net.corda.v5.application.flows.RestRequestBody
+import net.corda.v5.application.messaging.FlowMessaging
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.mockito.kotlin.any
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.whenever
 
 class FacadeInvocationFlowTest {
-    /**
-     * This is a temporary test which checks that the responding flow
-     */
     @Test
-    fun `facade call echos facade invocation payload back to calling flow`() {
+    fun `facade invocation flow invokes the call facade API`() {
         val flow = FacadeInvocationFlow()
+
+        val mockFlowMessaging = mock<FlowMessaging>()
+        whenever(mockFlowMessaging.callFacade(any(), any(), any(), any())).then { it.arguments[3] as String }
+
+        flow.flowMessaging = mockFlowMessaging
 
         val requestBody = "Hello world!"
         val request = mock<RestRequestBody>()
