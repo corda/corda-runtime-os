5d3e0a261486cb6b12977ce22782dd41998f410c
dickon <dickon.reed@r3.com>
2023-03-15 09:15:37 +0000

    CORE-11640: start JPA repository layer, currently inert (#3346)
    We should follow the JPA repository pattern in the Crypto persistence code. In this change we add the new code, not referenced yet from production code. Take care to make the new structure highly unit testable, by developing it using test driven development.
    
    In future releases, we will need to support upgrade of the Crypto schema, which is challenging since tables are shared between cluster and virtual nodes. In production the crypto processor has many database connections, most to per-virtual-node databases and a small number to cluster databases. The timing of virtual node upgrade is up to the owner of the virtual node; we will not require everyone to upgrade at the same time. Therefore, structure the code with a "V1" in the schema name, so that we can later have "V2", "V3" etc, and potentially use any of them on a per database choice.
    
    Stop caching the virtual node database connections. DbConnectionManager is the right place to do that, though it doesn't do so yet.
    
    Take care over closing down the EntityManager and EntityManagerFactory when appropriate, but not for the EntityManagerFactory for the cluste which is owned by DbConnectionManager.

---
+++ components/crypto/crypto-persistence/src/main/kotlin/net/corda/crypto/persistence/v1schema/V1WrappingKeyEntity.kt
@@ -0,0 +1,52 @@
+package net.corda.crypto.persistence.v1schema
+
+import net.corda.db.schema.DbSchema
+import java.time.Instant
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.Id
+import javax.persistence.Table
+
+/**
+ * An entity representing a wrapping key which is used by Soft HSM implementation of the CryptoService
+ * to wrap.
+ *
+ * The records are immutable.
+ */
+@Entity
+@Table(name = DbSchema.CRYPTO_WRAPPING_KEY_TABLE)
+class V1WrappingKeyEntity(
+    /**
+     * Key alias must be unique across all tenants. The key can be reused by different tenants.
+     */
+    @Id
+    @Column(name = "alias", nullable = false, updatable = false, length = 64)
+    val alias: String,
+
+    /**
+     * When the key was generated.
+     */
+    @Column(name = "created", nullable = false, updatable = false)
+    var created: Instant,
+
+    /**
+     * Encoding version of the key.
+     */
+    @Column(name = "encoding_version", nullable = false, updatable = false)
+    var encodingVersion: Int,
+
+    /**
+     * Key's algorithm's.
+     */
+    @Column(name = "algorithm_name", nullable = false, updatable = false, length = 64)
+    var algorithmName: String,
+
+    /**
+     * Key material for the wrapping key. It's encrypted by another key which is obtained through the configuration.
+     */
+    @Column(name = "key_material", nullable = false, updatable = false, columnDefinition = "BLOB")
+    var keyMaterial: ByteArray,
+) {
+    override fun hashCode() = alias.hashCode()
+    override fun equals(other: Any?) = other != null && other is V1WrappingKeyEntity && other.alias.equals(alias)
+}
