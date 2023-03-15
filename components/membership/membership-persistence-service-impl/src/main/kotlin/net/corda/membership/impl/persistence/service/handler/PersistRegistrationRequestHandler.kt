0bc39da417d971320bdc354d29391de84ba20132
Kyriakos Tharrouniatis <kyriakos.tharrouniatis@r3.com>
2023-03-15 11:25:36 +0000

    Remove unused imports

--- components/membership/membership-persistence-service-impl/src/main/kotlin/net/corda/membership/impl/persistence/service/handler/PersistRegistrationRequestHandler.kt
+++ components/membership/membership-persistence-service-impl/src/main/kotlin/net/corda/membership/impl/persistence/service/handler/PersistRegistrationRequestHandler.kt
@@ -1,7 +1,5 @@
 package net.corda.membership.impl.persistence.service.handler
 
-import net.corda.data.CordaAvroSerializer
-import net.corda.data.KeyValuePairList
 import net.corda.data.membership.db.request.MembershipRequestContext
 import net.corda.data.membership.db.request.command.PersistRegistrationRequest
 import net.corda.membership.datamodel.MemberSignatureEntity
