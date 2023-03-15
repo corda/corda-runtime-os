d505214d8c847cc61d018241c99fc47f85d6fa59
Dickon Reed <dickon.reed@r3.com>
2023-03-15 18:16:40 +0000

    fix detekt

--- components/crypto/crypto-service-impl/src/test/kotlin/net/corda/crypto/service/impl/infra/TestCryptoRepository.kt
+++ components/crypto/crypto-service-impl/src/test/kotlin/net/corda/crypto/service/impl/infra/TestCryptoRepository.kt
@@ -2,11 +2,6 @@ package net.corda.crypto.service.impl.infra
 
 import net.corda.crypto.persistence.CryptoRepository
 import net.corda.crypto.persistence.WrappingKeyInfo
-import net.corda.crypto.persistence.WrappingKeyStore
-import net.corda.lifecycle.LifecycleCoordinatorFactory
-import net.corda.lifecycle.LifecycleCoordinatorName
-import net.corda.lifecycle.LifecycleStatus
-import net.corda.lifecycle.StartEvent
 import java.util.concurrent.ConcurrentHashMap
 
 class TestCryptoRepository(
