987667d14d251b1776725942883a3da745321908
Simon Johnson <simon.johnson@r3.com>
2023-03-17 14:54:26 +0000

    detekt

--- testing/cpbs/packaging-verification-contract-v1/src/main/kotlin/net/cordapp/testing/packagingverification.contract/SimpleStateCommon.kt
+++ testing/cpbs/packaging-verification-contract-v1/src/main/kotlin/net/cordapp/testing/packagingverification.contract/SimpleStateCommon.kt
@@ -4,8 +4,6 @@ import net.corda.v5.application.crypto.DigestService
 import net.corda.v5.base.types.MemberX500Name
 import net.corda.v5.crypto.DigestAlgorithmName
 import net.corda.v5.crypto.SecureHash
-import net.corda.v5.crypto.SecureHash.DELIMITER
-import java.security.MessageDigest
 
 val ISSUER = MemberX500Name.parse("C=GB, L=London, O=Bob")
 const val STATE_SYMBOL = "symbol"
