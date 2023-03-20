319581db90d60b48e27369127cf36ec12118c9c0
Simon Johnson <simon.johnson@r3.com>
2023-03-20 09:40:51 +0000

    first test run

--- applications/workers/workers-packagingtests/src/packagingTest/kotlin/net/corda/applications/workers/packagingtest/contractverification/ContractVerificationTests.kt
+++ applications/workers/workers-packagingtests/src/packagingTest/kotlin/net/corda/applications/workers/packagingtest/contractverification/ContractVerificationTests.kt
@@ -34,74 +34,74 @@ class ContractVerificationTests {
 
     @Test
     fun `contract verification across two clusters succeeds`() {
-//        val multiClusterHelper = MultiClusterHelper(false)
-//
-//        // Clusters A and B have the same CPI uploaded, the is the happy flow test
-//
-//        val clusterA = MultiClusterNode(ClusterAInfo.rest.uri)
-//        clusterA.conditionallyUploadCordaPackage(cpiName, TEST_CPB_LOCATION, multiClusterHelper.memberGroupPolicy)
-//        val clusterB = MultiClusterNode(ClusterBInfo.rest.uri)
-//        clusterB.conditionallyUploadCordaPackage(cpiName, TEST_CPB_LOCATION, multiClusterHelper.memberGroupPolicy)
-//
-//        val aliceHoldingId = clusterA.getOrCreateVirtualNodeFor(aliceX500, cpiName)
-//        val bobHoldingId = clusterB.getOrCreateVirtualNodeFor(bobX500, cpiName)
-//        val notaryHoldingId = clusterB.getOrCreateVirtualNodeFor(notaryX500, cpiName)
-//
-//        // Mint states as Bob the issuer, checks that Bob's v1 contract is verified correctly
-//
-//        val mintFlowId = clusterB.startRpcFlow(
-//            bobHoldingId,
-//            mapOf("stateValues" to statesValueList),
-//            "net.cordapp.testing.packagingverification.MintFlow"
-//        )
-//        val mintFlowResult = clusterB.awaitRpcFlowFinished(bobHoldingId, mintFlowId)
-//        assertThat(mintFlowResult.flowStatus).isEqualTo(RPC_FLOW_STATUS_SUCCESS)
-//
-//        // Get states for Bob
-//
-//        val bobInitialStatesFlowId = clusterB.startRpcFlow(
-//            bobHoldingId,
-//            emptyMap(),
-//            "net.cordapp.testing.packagingverification.ReportStatesFlow"
-//        )
-//        val bobInitialStatesResult = clusterB.awaitRpcFlowFinished(bobHoldingId, bobInitialStatesFlowId)
-//        assertThat(bobInitialStatesResult.flowStatus).isEqualTo(RPC_FLOW_STATUS_SUCCESS)
-//
-//        assertThat(bobInitialStatesResult.flowResult).isEqualTo(statesValueList.sum().toString())
-//
-//        // Transaction from Bob to Alice
-//        // Alice has the same CPI as Bob as this is the happy flow, so this should go through ok
-//
-//        val transferStatesFlowId = clusterB.startRpcFlow(
-//            bobHoldingId,
-//            mapOf("recipientX500Name" to aliceX500, "value" to statesValueList[0]),
-//            "net.cordapp.testing.packagingverification.MintFlow"
-//        )
-//        val transferStatesResult = clusterB.awaitRpcFlowFinished(bobHoldingId, transferStatesFlowId)
-//        assertThat(transferStatesResult.flowStatus).isEqualTo(RPC_FLOW_STATUS_SUCCESS)
-//
-//        // Get states for Alice
-//
-//        val aliceGetStatesFlowId = clusterA.startRpcFlow(
-//            bobHoldingId,
-//            emptyMap(),
-//            "net.cordapp.testing.packagingverification.ReportStatesFlow"
-//        )
-//        val aliceStatesResult = clusterA.awaitRpcFlowFinished(aliceHoldingId, aliceGetStatesFlowId)
-//        assertThat(aliceStatesResult.flowStatus).isEqualTo(RPC_FLOW_STATUS_SUCCESS)
-//
-//        assertThat(aliceStatesResult.flowResult).isEqualTo(statesValueList[0].toString())
-//
-//        // Get states for Bob
-//
-//        val bobNewStatesFlowId = clusterB.startRpcFlow(
-//            bobHoldingId,
-//            emptyMap(),
-//            "net.cordapp.testing.packagingverification.ReportStatesFlow"
-//        )
-//        val bobNewStatesResult = clusterB.awaitRpcFlowFinished(bobHoldingId, bobNewStatesFlowId)
-//        assertThat(bobNewStatesResult.flowStatus).isEqualTo(RPC_FLOW_STATUS_SUCCESS)
-//
-//        assertThat(bobNewStatesResult.flowResult).isEqualTo(statesValueList.subList(1, statesValueList.size).sum().toString())
+        val multiClusterHelper = MultiClusterHelper(false)
+
+        // Clusters A and B have the same CPI uploaded, the is the happy flow test
+
+        val clusterA = MultiClusterNode(ClusterAInfo.rest.uri)
+        clusterA.conditionallyUploadCordaPackage(cpiName, TEST_CPB_LOCATION, multiClusterHelper.memberGroupPolicy)
+        val clusterB = MultiClusterNode(ClusterBInfo.rest.uri)
+        clusterB.conditionallyUploadCordaPackage(cpiName, TEST_CPB_LOCATION, multiClusterHelper.memberGroupPolicy)
+
+        val aliceHoldingId = clusterA.getOrCreateVirtualNodeFor(aliceX500, cpiName)
+        val bobHoldingId = clusterB.getOrCreateVirtualNodeFor(bobX500, cpiName)
+        val notaryHoldingId = clusterB.getOrCreateVirtualNodeFor(notaryX500, cpiName)
+
+        // Mint states as Bob the issuer, checks that Bob's v1 contract is verified correctly
+
+        val mintFlowId = clusterB.startRpcFlow(
+            bobHoldingId,
+            mapOf("stateValues" to statesValueList),
+            "net.cordapp.testing.packagingverification.MintFlow"
+        )
+        val mintFlowResult = clusterB.awaitRpcFlowFinished(bobHoldingId, mintFlowId)
+        assertThat(mintFlowResult.flowStatus).isEqualTo(RPC_FLOW_STATUS_SUCCESS)
+
+        // Get states for Bob
+
+        val bobInitialStatesFlowId = clusterB.startRpcFlow(
+            bobHoldingId,
+            emptyMap(),
+            "net.cordapp.testing.packagingverification.ReportStatesFlow"
+        )
+        val bobInitialStatesResult = clusterB.awaitRpcFlowFinished(bobHoldingId, bobInitialStatesFlowId)
+        assertThat(bobInitialStatesResult.flowStatus).isEqualTo(RPC_FLOW_STATUS_SUCCESS)
+
+        assertThat(bobInitialStatesResult.flowResult).isEqualTo(statesValueList.sum().toString())
+
+        // Transaction from Bob to Alice
+        // Alice has the same CPI as Bob as this is the happy flow, so this should go through ok
+
+        val transferStatesFlowId = clusterB.startRpcFlow(
+            bobHoldingId,
+            mapOf("recipientX500Name" to aliceX500, "value" to statesValueList[0]),
+            "net.cordapp.testing.packagingverification.MintFlow"
+        )
+        val transferStatesResult = clusterB.awaitRpcFlowFinished(bobHoldingId, transferStatesFlowId)
+        assertThat(transferStatesResult.flowStatus).isEqualTo(RPC_FLOW_STATUS_SUCCESS)
+
+        // Get states for Alice
+
+        val aliceGetStatesFlowId = clusterA.startRpcFlow(
+            bobHoldingId,
+            emptyMap(),
+            "net.cordapp.testing.packagingverification.ReportStatesFlow"
+        )
+        val aliceStatesResult = clusterA.awaitRpcFlowFinished(aliceHoldingId, aliceGetStatesFlowId)
+        assertThat(aliceStatesResult.flowStatus).isEqualTo(RPC_FLOW_STATUS_SUCCESS)
+
+        assertThat(aliceStatesResult.flowResult).isEqualTo(statesValueList[0].toString())
+
+        // Get states for Bob
+
+        val bobNewStatesFlowId = clusterB.startRpcFlow(
+            bobHoldingId,
+            emptyMap(),
+            "net.cordapp.testing.packagingverification.ReportStatesFlow"
+        )
+        val bobNewStatesResult = clusterB.awaitRpcFlowFinished(bobHoldingId, bobNewStatesFlowId)
+        assertThat(bobNewStatesResult.flowStatus).isEqualTo(RPC_FLOW_STATUS_SUCCESS)
+
+        assertThat(bobNewStatesResult.flowResult).isEqualTo(statesValueList.subList(1, statesValueList.size).sum().toString())
     }
 }
