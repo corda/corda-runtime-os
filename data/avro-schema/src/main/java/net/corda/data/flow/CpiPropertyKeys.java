da5c26ae9a47908ee90857fe267a88cb8ee7b5a8
Peter McKinney <pmck91@users.noreply.github.com>
2023-03-21 14:43:05 +0000

    Added property keys accessible via flow context properties

---
+++ data/avro-schema/src/main/java/net/corda/data/flow/CpiPropertyKeys.java
@@ -0,0 +1,15 @@
+package net.corda.data.flow;
+
+public final class CpiPropertyKeys {
+
+    private CpiPropertyKeys(){}
+
+    //
+    public static final String NAME = "corda.cpiName";
+    //
+    public static final String VERSION = "corda.cpiVersion";
+    //
+    public static final String SIGNER_SUMMARY_HASH = "corda.cpiSignerSummaryHash";
+    //
+    public static final String FILE_CHECKSUM = "corda.cpiFileChecksum";
+}
