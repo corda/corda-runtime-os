9d5ed0ae8b883f4a0846dbee0793d3e261a09977
Peter McKinney <pmck91@users.noreply.github.com>
2023-03-22 15:50:17 +0000

    re-adding private constructor

--- application/src/main/java/net/corda/v5/application/flows/FlowContextPropertyKeys.java
+++ application/src/main/java/net/corda/v5/application/flows/FlowContextPropertyKeys.java
@@ -1,6 +1,8 @@
 package net.corda.v5.application.flows;
 
 public final class FlowContextPropertyKeys {
+
+    private FlowContextPropertyKeys() { }
     public static final String CPI_NAME = "corda.cpiName";
     public static final String CPI_VERSION = "corda.cpiVersion";
     public static final String CPI_SIGNER_SUMMARY_HASH = "corda.cpiSignerSummaryHash";
