9474fc7a0cdbeae4f09f72a61dcb24548b42bbe6
Viktor Kolomeyko <Viktor.Kolomeyko@r3.com>
2023-03-22 15:49:25 +0000

    CORE-11905: Extend `SslCertReadServiceImpl` to process data in PEM format
    Also added a unit test for it.

--- libs/rest/ssl-cert-read-impl/src/main/kotlin/net/corda/rest/ssl/impl/SslCertReadServiceImpl.kt
+++ libs/rest/ssl-cert-read-impl/src/main/kotlin/net/corda/rest/ssl/impl/SslCertReadServiceImpl.kt
@@ -4,20 +4,40 @@ import net.corda.libs.configuration.SmartConfig
 import net.corda.rest.ssl.KeyStoreInfo
 import net.corda.rest.ssl.SslCertReadService
 import net.corda.schema.configuration.BootConfig
+import net.corda.schema.configuration.BootConfig.BOOT_REST_TLS_CA_CRT_PATH
+import net.corda.schema.configuration.BootConfig.BOOT_REST_TLS_CRT_PATH
+import net.corda.schema.configuration.BootConfig.BOOT_REST_TLS_KEYSTORE_FILE_PATH
+import net.corda.schema.configuration.BootConfig.BOOT_REST_TLS_KEYSTORE_PASSWORD
+import net.corda.schema.configuration.BootConfig.BOOT_REST_TLS_KEY_PATH
 import net.corda.v5.base.exceptions.CordaRuntimeException
+import org.bouncycastle.asn1.pkcs.PrivateKeyInfo
+import org.bouncycastle.jcajce.provider.asymmetric.x509.CertificateFactory
+import org.bouncycastle.openssl.PEMParser
+import org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import java.io.File
 import java.nio.file.Files
 import java.nio.file.Path
+import java.security.KeyStore
+import java.security.PrivateKey
+import java.security.cert.Certificate
+import kotlin.io.path.inputStream
+import kotlin.io.path.outputStream
 
 class SslCertReadServiceImpl(private val createDirectory: () -> Path) : SslCertReadService {
 
     constructor() : this(createDirectory = { Files.createTempDirectory("rest-ssl") })
 
     internal companion object {
+
         const val PASSWORD = "httpsPassword"
 
         const val KEYSTORE_NAME = "https.keystore"
+
+        const val TLS_ENTRY = "tls_entry"
+
+        const val KEYSTORE_TYPE = "pkcs12"
 
         private val log: Logger = LoggerFactory.getLogger(this::class.java.enclosingClass)
 
@@ -55,15 +75,21 @@ class SslCertReadServiceImpl(private val createDirectory: () -> Path) : SslCertR
 
         if (localKeyStoreInfo != null) return localKeyStoreInfo
 
-        localKeyStoreInfo = if (config.hasPath(BootConfig.BOOT_REST_TLS_KEYSTORE_FILE_PATH)) {
-            val bootKeyStorePath = config.getString(BootConfig.BOOT_REST_TLS_KEYSTORE_FILE_PATH)
-            val keyStorePassword = config.getString(BootConfig.BOOT_REST_TLS_KEYSTORE_PASSWORD)
+        localKeyStoreInfo = if (config.hasPath(BOOT_REST_TLS_KEYSTORE_FILE_PATH)) {
+            val bootKeyStorePath = config.getString(BOOT_REST_TLS_KEYSTORE_FILE_PATH)
+            val keyStorePassword = config.getString(BOOT_REST_TLS_KEYSTORE_PASSWORD)
             KeyStoreInfo(Path.of(bootKeyStorePath), keyStorePassword)
+        } else if(config.hasPath(BOOT_REST_TLS_CRT_PATH)) {
+            val tlsCertPath = config.getString(BOOT_REST_TLS_CRT_PATH)
+            val tlsKeyPath = config.getString(BOOT_REST_TLS_KEY_PATH)
+            val caChainPath = config.getString(BOOT_REST_TLS_CA_CRT_PATH)
+            createKeyStoreInfoFromCrt(tlsCertPath, tlsKeyPath, caChainPath)
         } else {
             log.warn(
                 "Using default self-signed TLS certificate for REST endpoint. To stop seeing this message, please use bootstrap " +
-                        "parameters: '-r${BootConfig.BOOT_REST_TLS_KEYSTORE_FILE_PATH.withoutPrefix}' and " +
-                        "'-r${BootConfig.BOOT_REST_TLS_KEYSTORE_PASSWORD.withoutPrefix}'."
+                        "parameters: " +
+                        "('-r${BOOT_REST_TLS_KEYSTORE_FILE_PATH.withoutPrefix}' and '-r${BOOT_REST_TLS_KEYSTORE_PASSWORD.withoutPrefix}') or " +
+                        "('-r${BOOT_REST_TLS_CRT_PATH.withoutPrefix}', '-r${BOOT_REST_TLS_KEY_PATH.withoutPrefix}' and '-r${BOOT_REST_TLS_CA_CRT_PATH.withoutPrefix}')."
             )
             val tempDirectoryPath = createDirectory()
             val keyStorePath = Path.of(tempDirectoryPath.toString(), KEYSTORE_NAME)
@@ -72,6 +98,48 @@ class SslCertReadServiceImpl(private val createDirectory: () -> Path) : SslCertR
         }
         keyStoreInfo = localKeyStoreInfo
         return localKeyStoreInfo
+    }
+
+    private fun createKeyStoreInfoFromCrt(tlsCertPath: String, tlsKeyPath: String, caChainPath: String): KeyStoreInfo {
+
+        val ks = KeyStore.getInstance(KEYSTORE_TYPE)
+        ks.load(null, PASSWORD.toCharArray())
+
+        val privateKey = readPrivateKey(tlsKeyPath)
+        val certChain = readCertChain(tlsCertPath, caChainPath)
+
+        ks.setKeyEntry(TLS_ENTRY, privateKey, PASSWORD.toCharArray(), certChain)
+
+        val tempDirectoryPath = createDirectory()
+        val keyStorePath = tempDirectoryPath.resolve(KEYSTORE_NAME)
+        keyStorePath.outputStream().use {
+            ks.store(it, PASSWORD.toCharArray())
+        }
+
+        return KeyStoreInfo(keyStorePath, PASSWORD)
+    }
+
+    private fun readCertChain(tlsKeyPath: String, caChainPath: String): Array<Certificate> {
+        val certificateFactory = CertificateFactory()
+        val leafCertificate = Path.of(tlsKeyPath).inputStream().use {
+            certificateFactory.engineGenerateCertificate(it)
+        }
+
+        @Suppress("UNCHECKED_CAST")
+        val certsChain = Path.of(caChainPath).inputStream().use {
+            certificateFactory.engineGenerateCertificates(it)
+        } as Collection<Certificate>
+
+        return (listOf(leafCertificate) + certsChain).toTypedArray()
+    }
+
+    private fun readPrivateKey(tlsKeyPath: String): PrivateKey {
+
+        val privateKeyInfo = File(tlsKeyPath).reader().use { keyReader ->
+            val pemParser = PEMParser(keyReader)
+            PrivateKeyInfo.getInstance(pemParser.readObject())
+        }
+        return JcaPEMKeyConverter().getPrivateKey(privateKeyInfo)
     }
 
     private fun loadKeystoreFromResources(): ByteArray {
--- libs/rest/ssl-cert-read-impl/src/test/kotlin/net/corda/rest/ssl/impl/SslCertReadServiceImplTest.kt
+++ libs/rest/ssl-cert-read-impl/src/test/kotlin/net/corda/rest/ssl/impl/SslCertReadServiceImplTest.kt
@@ -1,18 +1,35 @@
 package net.corda.rest.ssl.impl
 
 import net.corda.libs.configuration.SmartConfig
-import net.corda.schema.configuration.BootConfig
+import net.corda.rest.ssl.impl.SslCertReadServiceImpl.Companion.KEYSTORE_NAME
+import net.corda.rest.ssl.impl.SslCertReadServiceImpl.Companion.KEYSTORE_TYPE
+import net.corda.rest.ssl.impl.SslCertReadServiceImpl.Companion.PASSWORD
+import net.corda.rest.ssl.impl.SslCertReadServiceImpl.Companion.TLS_ENTRY
+import net.corda.schema.configuration.BootConfig.BOOT_REST_TLS_CA_CRT_PATH
+import net.corda.schema.configuration.BootConfig.BOOT_REST_TLS_CRT_PATH
+import net.corda.schema.configuration.BootConfig.BOOT_REST_TLS_KEYSTORE_FILE_PATH
+import net.corda.schema.configuration.BootConfig.BOOT_REST_TLS_KEYSTORE_PASSWORD
+import net.corda.schema.configuration.BootConfig.BOOT_REST_TLS_KEY_PATH
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.io.TempDir
 import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.whenever
 import java.io.File
 import java.nio.file.Files
 import java.nio.file.Path
+import java.security.KeyStore
+import kotlin.io.path.inputStream
+import kotlin.io.path.outputStream
 
 class SslCertReadServiceImplTest {
+
+    @TempDir
+    lateinit var resourcesDir: Path
 
     private var tempDirectoryPath: Path? = null
 
@@ -54,12 +71,49 @@ class SslCertReadServiceImplTest {
         val configMock = mock<SmartConfig>()
         val keyStorePath = "/foo/bar/keystore.file"
         val password = "myPassword"
-        whenever(configMock.hasPath(BootConfig.BOOT_REST_TLS_KEYSTORE_FILE_PATH)).doReturn(true)
-        whenever(configMock.getString(BootConfig.BOOT_REST_TLS_KEYSTORE_FILE_PATH)).doReturn(keyStorePath)
-        whenever(configMock.getString(BootConfig.BOOT_REST_TLS_KEYSTORE_PASSWORD)).doReturn(password)
+        whenever(configMock.hasPath(BOOT_REST_TLS_KEYSTORE_FILE_PATH)).doReturn(true)
+        whenever(configMock.getString(BOOT_REST_TLS_KEYSTORE_FILE_PATH)).doReturn(keyStorePath)
+        whenever(configMock.getString(BOOT_REST_TLS_KEYSTORE_PASSWORD)).doReturn(password)
 
         val keyStoreInfo = service.getOrCreateKeyStoreInfo(configMock)
         Assertions.assertEquals(Path.of(keyStorePath), keyStoreInfo.path)
         Assertions.assertEquals(password, keyStoreInfo.password)
+    }
+
+    @Test
+    fun `can create Keystore from CRT files`() {
+
+        copyResources()
+
+        val configMock = mock<SmartConfig>()
+        whenever(configMock.hasPath(BOOT_REST_TLS_CRT_PATH)).doReturn(true)
+        whenever(configMock.getString(BOOT_REST_TLS_CRT_PATH)).doReturn(resourcesDir.resolve("server.crt").toString())
+        whenever(configMock.getString(BOOT_REST_TLS_KEY_PATH)).doReturn(resourcesDir.resolve("server.key").toString())
+        whenever(configMock.getString(BOOT_REST_TLS_CA_CRT_PATH)).doReturn(
+            resourcesDir.resolve("ca-chain-bundle.crt").toString()
+        )
+
+        val keyStoreInfo = service.getOrCreateKeyStoreInfo(configMock)
+
+        val keyStoreDir = requireNotNull(tempDirectoryPath)
+        Assertions.assertEquals(keyStoreDir.resolve(KEYSTORE_NAME), keyStoreInfo.path)
+        Assertions.assertEquals(PASSWORD, keyStoreInfo.password)
+
+        // Check Keystore
+        val ks = KeyStore.getInstance(KEYSTORE_TYPE)
+        keyStoreDir.resolve(KEYSTORE_NAME).inputStream().use {
+            ks.load(it, PASSWORD.toCharArray())
+        }
+        assertTrue(ks.containsAlias(TLS_ENTRY))
+        assertThat(ks.getCertificateChain(TLS_ENTRY).asList()).hasSize(3)
+    }
+
+    private fun copyResources() {
+        listOf("ca-chain-bundle.crt", "server.crt", "server.key").forEach { resourceName ->
+            requireNotNull(this::class.java.classLoader.getResourceAsStream(resourceName)).use { inputStream ->
+                val bytes = inputStream.readAllBytes()
+                resourcesDir.resolve(resourceName).outputStream().use { os -> os.write(bytes) }
+            }
+        }
     }
 }
