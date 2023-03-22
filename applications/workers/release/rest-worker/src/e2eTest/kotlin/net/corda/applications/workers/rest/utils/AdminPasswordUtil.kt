916c3e62363bbcf5e9b617dee729f24883d6d75c
David Currie <david.currie@r3.com>
2023-03-22 21:46:36 +0000

    CORE-11846 Change tests back

--- applications/workers/release/rest-worker/src/e2eTest/kotlin/net/corda/applications/workers/rest/utils/AdminPasswordUtil.kt
+++ applications/workers/release/rest-worker/src/e2eTest/kotlin/net/corda/applications/workers/rest/utils/AdminPasswordUtil.kt
@@ -2,5 +2,5 @@ package net.corda.applications.workers.rest.utils
 
 object AdminPasswordUtil {
     const val adminUser = "admin"
-    val adminPassword = System.getenv("REST_API_ADMIN_PASSWORD") ?: "admin"
+    val adminPassword = System.getenv("INITIAL_ADMIN_USER_PASSWORD") ?: "admin"
 }
--- testing/e2e-test-utilities/src/main/kotlin/net/corda/e2etest/utilities/AdminPasswordUtil.kt
+++ testing/e2e-test-utilities/src/main/kotlin/net/corda/e2etest/utilities/AdminPasswordUtil.kt
@@ -2,5 +2,5 @@ package net.corda.e2etest.utilities
 
 object AdminPasswordUtil {
     const val adminUser = "admin"
-    val adminPassword = System.getenv("REST_API_ADMIN") ?: "admin"
+    val adminPassword = System.getenv("INITIAL_ADMIN_USER_PASSWORD") ?: "admin"
 }
