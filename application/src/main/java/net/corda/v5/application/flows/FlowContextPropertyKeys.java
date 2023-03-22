f194858aa8ceb374fa6dd0152bb1b0cb0caef264
Peter McKinney <pmck91@users.noreply.github.com>
2023-03-22 15:46:36 +0000

    adding CPI to titles

--- application/src/main/java/net/corda/v5/application/flows/FlowContextPropertyKeys.java
+++ application/src/main/java/net/corda/v5/application/flows/FlowContextPropertyKeys.java
@@ -1,8 +1,8 @@
 package net.corda.v5.application.flows;
 
 public final class FlowContextPropertyKeys {
-    public static final String NAME = "corda.cpiName";
-    public static final String VERSION = "corda.cpiVersion";
-    public static final String SIGNER_SUMMARY_HASH = "corda.cpiSignerSummaryHash";
-    public static final String FILE_CHECKSUM = "corda.cpiFileChecksum";
+    public static final String CPI_NAME = "corda.cpiName";
+    public static final String CPI_VERSION = "corda.cpiVersion";
+    public static final String CPI_SIGNER_SUMMARY_HASH = "corda.cpiSignerSummaryHash";
+    public static final String CPI_FILE_CHECKSUM = "corda.cpiFileChecksum";
 }
