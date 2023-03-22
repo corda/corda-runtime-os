73ef9bc2ca6d0958d351e89ecd61bd658d2ec1a0
Peter McKinney <pmck91@users.noreply.github.com>
2023-03-22 16:59:01 +0000

    Adding platform version infor keys

--- application/src/main/java/net/corda/v5/application/flows/FlowContextPropertyKeys.java
+++ application/src/main/java/net/corda/v5/application/flows/FlowContextPropertyKeys.java
@@ -2,9 +2,15 @@ package net.corda.v5.application.flows;
 
 public final class FlowContextPropertyKeys {
 
-    private FlowContextPropertyKeys() { }
+    private FlowContextPropertyKeys() {
+    }
+
     public static final String CPI_NAME = "corda.cpiName";
     public static final String CPI_VERSION = "corda.cpiVersion";
     public static final String CPI_SIGNER_SUMMARY_HASH = "corda.cpiSignerSummaryHash";
     public static final String CPI_FILE_CHECKSUM = "corda.cpiFileChecksum";
+    public static final String INITIAL_PLATFORM_VERSION = "corda.initialPlatformVersion";
+    public static final String INITIAL_SOFTWARE_VERSION = "corda.initialSoftwareVersion";
+    public static final String CURRENT_PLATFORM_VERSION = "corda.currentPlatformVersion";
+    public static final String CURRENT_SOFTWARE_VERSION = "corda.currentSoftwareVersion";
 }
