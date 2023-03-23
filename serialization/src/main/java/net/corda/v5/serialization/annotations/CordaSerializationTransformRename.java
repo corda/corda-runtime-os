132ea5ddf30e483685ecc272eddb66d103eb5c29
Anna Kaludzinska <77064094+AnnaK-R3@users.noreply.github.com>
2023-03-23 17:52:44 +0000

    DOC-5029: serialization Javadocs non-technical review (#989)
    JavaDoc fixes

--- serialization/src/main/java/net/corda/v5/serialization/annotations/CordaSerializationTransformRename.java
+++ serialization/src/main/java/net/corda/v5/serialization/annotations/CordaSerializationTransformRename.java
@@ -10,12 +10,12 @@ import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 /**
- * This annotation is used to mark a class has having had a property element. It is used by the
+ * This annotation is used to mark a class as having had a property element. It is used by the
  * AMQP deserializer to allow instances with different versions of the class on their Class Path
  * to successfully deserialize the object.
  *
  * NOTE: Renaming of the class itself isn't done with this annotation or, at present, supported
- * by Corda
+ * by Corda.
  */
 @Target(TYPE)
 @Retention(RUNTIME)
@@ -23,13 +23,13 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 public @interface CordaSerializationTransformRename {
     /**
      * to
-     * @return {@link String} representation of the properties new name
+     * @return {@link String} representation of the properties new name.
      */
     String to();
 
     /**
      * from
-     * @return {@link String} representation of the properties old new
+     * @return {@link String} representation of the properties old name.
      */
     String from();
 }
