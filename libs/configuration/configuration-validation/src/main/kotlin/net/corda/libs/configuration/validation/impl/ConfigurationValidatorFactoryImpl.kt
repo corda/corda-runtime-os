8b7cd21bb6ccaeb5e112917fa62a3455df319384
Filipe Oliveira <filipe.oliveira@r3.com>
2023-03-23 10:13:17 +0000

    Update regarding the configuration and cordapp configuration factories.

--- libs/configuration/configuration-validation/src/main/kotlin/net/corda/libs/configuration/validation/impl/ConfigurationValidatorFactoryImpl.kt
+++ libs/configuration/configuration-validation/src/main/kotlin/net/corda/libs/configuration/validation/impl/ConfigurationValidatorFactoryImpl.kt
@@ -2,7 +2,8 @@ package net.corda.libs.configuration.validation.impl
 
 import net.corda.libs.configuration.validation.ConfigurationValidator
 import net.corda.libs.configuration.validation.ConfigurationValidatorFactory
-import net.corda.schema.common.provider.SchemaProviderFactory
+import net.corda.schema.configuration.provider.SchemaProviderConfigFactory
+import net.corda.schema.cordapp.configuration.provider.SchemaProviderCordappConfigFactory
 import org.osgi.service.component.annotations.Component
 
 /**
@@ -15,7 +16,7 @@ class ConfigurationValidatorFactoryImpl : ConfigurationValidatorFactory {
      * Create a new configuration validator.
      */
     override fun createConfigValidator(): ConfigurationValidator {
-        val schemaProvider = SchemaProviderFactory.getConfigSchemaProvider()
+        val schemaProvider = SchemaProviderConfigFactory.getSchemaProvider()
         return ConfigurationValidatorImpl(schemaProvider)
     }
 
@@ -23,7 +24,7 @@ class ConfigurationValidatorFactoryImpl : ConfigurationValidatorFactory {
      * Create a configuration validator for the external messaging
      */
     override fun createCordappConfigValidator(): ConfigurationValidator {
-        val schemaProvider = SchemaProviderFactory.getCordappConfigSchemaProvider()
+        val schemaProvider = SchemaProviderCordappConfigFactory.getSchemaProvider()
         return ConfigurationValidatorImpl(schemaProvider)
     }
 }
