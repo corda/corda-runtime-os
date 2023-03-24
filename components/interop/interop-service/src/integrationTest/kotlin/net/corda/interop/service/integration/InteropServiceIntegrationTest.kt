78d2cb01cf196a1d2bbb0ebb00fd4dd32d7225d7
Alexander Cramb <alex.cramb@r3.com>
2023-03-24 15:36:15 +0000

    Revert unneeded changes to InteropServiceIntegrationTest

--- components/interop/interop-service/src/integrationTest/kotlin/net/corda/interop/service/integration/InteropServiceIntegrationTest.kt
+++ components/interop/interop-service/src/integrationTest/kotlin/net/corda/interop/service/integration/InteropServiceIntegrationTest.kt
@@ -39,10 +39,13 @@ import net.corda.schema.configuration.BootConfig.TOPIC_PREFIX
 import net.corda.schema.configuration.ConfigKeys.MESSAGING_CONFIG
 import net.corda.schema.configuration.MessagingConfig
 import net.corda.schema.configuration.MessagingConfig.Bus.BUS_TYPE
-import org.junit.jupiter.api.*
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.jupiter.api.fail
 import org.osgi.test.common.annotation.InjectService
 import org.osgi.test.junit5.service.ServiceExtension
 import java.nio.ByteBuffer
@@ -96,7 +99,6 @@ class InteropServiceIntegrationTest {
             setupConfig(publisher)
         }
     }
-
     @Test
     fun `verify messages from p2p-in are send back to p2p-out`() {
         interopService.start()
