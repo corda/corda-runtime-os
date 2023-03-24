d233452c377d5a4da41526883c5ecda8ddb1704f
owenstanford <owen.stanford@r3.com>
2023-03-24 15:55:40 +0000

    CORE-9775 PR comments

--- components/ledger/ledger-utxo-flow/src/main/kotlin/net/corda/ledger/utxo/flow/impl/FinalizationResultImpl.kt
+++ components/ledger/ledger-utxo-flow/src/main/kotlin/net/corda/ledger/utxo/flow/impl/FinalizationResultImpl.kt
@@ -3,7 +3,7 @@ package net.corda.ledger.utxo.flow.impl
 import net.corda.v5.ledger.utxo.FinalizationResult
 import net.corda.v5.ledger.utxo.transaction.UtxoSignedTransaction
 
-class FinalizationResultImpl(private val utxoSignedTransaction:UtxoSignedTransaction):FinalizationResult {
+class FinalizationResultImpl(private val utxoSignedTransaction: UtxoSignedTransaction) : FinalizationResult {
     override fun getTransaction(): UtxoSignedTransaction {
         return utxoSignedTransaction
     }
