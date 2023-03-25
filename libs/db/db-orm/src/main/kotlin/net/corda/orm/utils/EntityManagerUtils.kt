4e96070f198401fdc70fe8845526c6ed77cff22f
Dickon Reed <Dickon.Reed@r3.com>
2023-03-25 09:56:50 +0000

    mark up the confusing extension as deprecated, clarify kdocs
    add a matching consume method to SigningRepository
    
    use the new extension methods in more places

--- libs/db/db-orm/src/main/kotlin/net/corda/orm/utils/EntityManagerUtils.kt
+++ libs/db/db-orm/src/main/kotlin/net/corda/orm/utils/EntityManagerUtils.kt
@@ -4,7 +4,14 @@ import javax.persistence.EntityManager
 import javax.persistence.EntityManagerFactory
 
 /**
+ *
+ * DEPRECATED due to confusing semantics which are not idiomatic in Kotlin.
+ * You probably want consume instead.
+ *
  * Creates an [EntityManager], executes the [block] and closes the [EntityManager].
+ *
+ * NOTE: this does not close the EntityManagerFactory, which is different to the semantics
+ * of use in the Kotlin standard library.
  *
  * Starting and committing the [EntityManager]'s transaction is up to the [block].
  *
@@ -14,6 +21,8 @@ import javax.persistence.EntityManagerFactory
  * @return The result of executing [block].
  *
  * @see transaction
+ * @see transactionStep
+ * @see consume
  */
 inline fun <R> EntityManagerFactory.use(block: (EntityManager) -> R): R {
     return createEntityManager().use(block)
@@ -32,10 +41,36 @@ inline fun <R> EntityManagerFactory.use(block: (EntityManager) -> R): R {
  * @return The result of executing [block].
  *
  * @see transaction
+ * @see transactionStep
+ * @see use
  */
 inline fun <R> EntityManagerFactory.consume(block: EntityManager.() -> R): R {
     try {
         return createEntityManager().use { it.block() }
+    } finally {
+        this.close()
+    }
+}
+
+
+/**
+ * Creates an [EntityManager], executes the [block] with the [EntityManager] as the receiver in a transaction,
+ * and closes the [EntityManager] and the [EntityManagerFactory] itself.
+ **
+ * @param block The code to execute using the [EntityManager].
+ * @param R The type returned by [block].
+ *
+ * @return The result of executing [block].
+ *
+ * @see transaction
+ */
+inline fun <R> EntityManagerFactory.consumeTransaction(block: EntityManager.() -> R): R {
+    try {
+        return createEntityManager().use {
+            transaction {
+                it.block()
+            }
+        }
     } finally {
         this.close()
     }
@@ -79,6 +114,9 @@ inline fun <R> EntityManagerFactory.transaction(block: (EntityManager) -> R): R 
 }
 
 /**
+ *
+ * DEPRECATED - you probably want transactionStep instead, since this closes the parent EntityManager.
+ *
  * Begins a new transaction, commits it and then closes the transaction and the parent entity manager
  * after executing the [block].
  *
