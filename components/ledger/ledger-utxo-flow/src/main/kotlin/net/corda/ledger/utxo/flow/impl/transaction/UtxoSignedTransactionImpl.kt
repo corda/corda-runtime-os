c3dc4c07179f05cc2503d8b80255f1f653e89308
Kyriakos Tharrouniatis <kyriakos.tharrouniatis@r3.com>
2023-03-27 23:36:14 +0100

    Remove redundant check

--- components/ledger/ledger-utxo-flow/src/main/kotlin/net/corda/ledger/utxo/flow/impl/transaction/UtxoSignedTransactionImpl.kt
+++ components/ledger/ledger-utxo-flow/src/main/kotlin/net/corda/ledger/utxo/flow/impl/transaction/UtxoSignedTransactionImpl.kt
@@ -229,14 +229,6 @@ data class UtxoSignedTransactionImpl(
                         "Notary signature key Id: ${signature.by}"
             )
 
-        // TODO The below check is actually redundant. Above check means key id matched notary key or one of notary key leaves.
-        if (!KeyUtils.isKeyInSet(notaryKey, listOf(publicKey))) {
-            throw CordaRuntimeException( // todo transition to TransactionSignatureException
-                "Notary signature has not been created by the notary for this transaction. " +
-                        "Notary public key: $notaryKey " +
-                        "Notary signature key: $publicKey"
-            )
-        }
         try {
             transactionSignatureServiceInternal.verifySignature(this, signature, publicKey)
         } catch (e: Exception) {
