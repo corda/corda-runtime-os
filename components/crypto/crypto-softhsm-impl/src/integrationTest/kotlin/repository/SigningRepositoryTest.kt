3fe5c9951e6bbc86ffcba762598d2dc9e8e11769
Dries Samyn <dries.samyn@r3.com>
2023-03-30 12:35:59 +0100

    lookup tests

--- components/crypto/crypto-softhsm-impl/src/integrationTest/kotlin/repository/SigningRepositoryTest.kt
+++ components/crypto/crypto-softhsm-impl/src/integrationTest/kotlin/repository/SigningRepositoryTest.kt
@@ -42,6 +42,9 @@ class SigningRepositoryTest : CryptoRepositoryTest() {
     private val cipherSchemeMetadata = CipherSchemeMetadataImpl()
     private val digestService = PlatformDigestServiceImpl(cipherSchemeMetadata)
 
+    private val defaultTenantId = "Memento Mori"
+    private val defaultMasterKeyName = "Domination's the name of the game"
+
     private fun createNewPubKeyInfo(): SigningKeyInfo {
         val unique = UUID.randomUUID().toString()
         val key = SecureHashUtils.randomBytes()
@@ -72,7 +75,7 @@ class SigningRepositoryTest : CryptoRepositoryTest() {
             keyMaterial = privKey,
             encodingVersion = 1,
             hsmAlias = null,
-            masterKeyAlias = "Domination's the name of the game"
+            masterKeyAlias = defaultMasterKeyName
         )
     }
 
@@ -101,38 +104,63 @@ class SigningRepositoryTest : CryptoRepositoryTest() {
             capabilities = setOf(KeySchemeCapability.SIGN)
         )
 
-    private fun createLayeredPropertyMapFactory(info: SigningKeyInfo): LayeredPropertyMapFactory {
-        println(info)
+    private fun createLayeredPropertyMapFactory(): LayeredPropertyMapFactory {
         return mock<LayeredPropertyMapFactory>()
     }
 
+    private val wrappingKeys = mutableMapOf<EntityManagerFactory, WrappingKeyInfo>()
     private fun saveWrappingKey(emf: EntityManagerFactory, alias: String): WrappingKeyInfo {
-        return emf.createEntityManager().use { em ->
-            em.transaction {
-                it.merge(
-                    WrappingKeyEntity(
-                        id = UUID.randomUUID(),
-                        generation = 1,
-                        alias = alias,
-                        created = Instant.now(),
-                        rotationDate = LocalDate.parse("9999-12-31").atStartOfDay().toInstant(ZoneOffset.UTC),
-                        encodingVersion = 1,
-                        algorithmName = "foo",
-                        keyMaterial = SecureHashUtils.randomBytes(),
-                        isParentKeyManaged = false,
-                        parentKeyReference = "root",
+        return wrappingKeys.computeIfAbsent(emf) {
+            emf.createEntityManager().use { em ->
+                em.transaction {
+                    it.merge(
+                        WrappingKeyEntity(
+                            id = UUID.randomUUID(),
+                            generation = 1,
+                            alias = alias,
+                            created = Instant.now(),
+                            rotationDate = LocalDate.parse("9999-12-31").atStartOfDay().toInstant(ZoneOffset.UTC),
+                            encodingVersion = 1,
+                            algorithmName = "foo",
+                            keyMaterial = SecureHashUtils.randomBytes(),
+                            isParentKeyManaged = false,
+                            parentKeyReference = "root",
+                        )
                     )
-                )
-            }
-        }.toDto()
+                }
+            }.toDto()
+        }
     }
 
-    @ParameterizedTest
-    @MethodSource("emfs")
-    fun `savePublicKey and find`(emf: EntityManagerFactory) {
-        val info = createNewPubKeyInfo()
+    private val createdKeys = mutableMapOf<EntityManagerFactory, List<SigningKeyInfo>>()
+
+    private fun createKeys(emf: EntityManagerFactory) : List<SigningKeyInfo> {
+        val repo = SigningRepositoryImpl(
+            emf,
+            defaultTenantId,
+            cipherSchemeMetadata,
+            digestService,
+            createLayeredPropertyMapFactory(),
+        )
+
+        saveWrappingKey(emf, defaultMasterKeyName)
+        val privateKeys = (0..2).map { _ ->
+            val info = createPrivateKeyInfo()
+            val ctx = createSigningWrappedKeySaveContext(info)
+            repo.savePrivateKey(ctx)
+        }
+
+        val publicKeys = (0..3).map { _ ->
+            val info = createNewPubKeyInfo()
+            val ctx = createSigningPublicKeySaveContext(info)
+            repo.savePublicKey(ctx)
+        }
+        return privateKeys + publicKeys
+    }
+
+    fun createSigningPublicKeySaveContext(info: SigningKeyInfo) : SigningPublicKeySaveContext {
         val pubKey = createGeneratedPublicKey(info)
-        val ctx = SigningPublicKeySaveContext(
+        return SigningPublicKeySaveContext(
             key = pubKey,
             alias = info.alias,
             category = info.category,
@@ -140,13 +168,34 @@ class SigningRepositoryTest : CryptoRepositoryTest() {
             externalId = info.externalId,
             hsmId = info.hsmId,
         )
+    }
+
+    fun createSigningWrappedKeySaveContext(info: SigningKeyInfo) : SigningWrappedKeySaveContext {
+        val privKey = createGeneratedWrappedKey(info)
+        return SigningWrappedKeySaveContext(
+            key = privKey,
+            masterKeyAlias = info.masterKeyAlias,
+            externalId = info.externalId,
+            alias = info.alias,
+            category = info.category,
+            keyScheme = createKeyScheme(info),
+            hsmId = info.hsmId,
+        )
+    }
+
+    @ParameterizedTest
+    @MethodSource("emfs")
+    fun `savePublicKey and find`(emf: EntityManagerFactory) {
+        val info = createNewPubKeyInfo()
+        val pubKey = createGeneratedPublicKey(info)
+        val ctx = createSigningPublicKeySaveContext(info)
 
         val repo = SigningRepositoryImpl(
             emf,
             info.tenantId,
             cipherSchemeMetadata,
             digestService,
-            createLayeredPropertyMapFactory(info),
+            createLayeredPropertyMapFactory(),
         )
 
         repo.savePublicKey(ctx)
@@ -160,34 +209,23 @@ class SigningRepositoryTest : CryptoRepositoryTest() {
 
     @ParameterizedTest
     @MethodSource("emfs")
-    fun savePrivateKey(emf: EntityManagerFactory) {
+    fun `savePrivateKey and find by alias`(emf: EntityManagerFactory) {
         val info = createPrivateKeyInfo()
-        val pubKey = createGeneratedWrappedKey(info)
-
         saveWrappingKey(emf, info.masterKeyAlias!!)
 
-        val ctx = SigningWrappedKeySaveContext(
-            key = pubKey,
-            masterKeyAlias = info.masterKeyAlias,
-            externalId = info.externalId,
-            alias = info.alias,
-            category = info.category,
-            keyScheme = createKeyScheme(info),
-            hsmId = info.hsmId,
-        )
-
+        val ctx = createSigningWrappedKeySaveContext(info)
 
         val repo = SigningRepositoryImpl(
             emf,
             info.tenantId,
             cipherSchemeMetadata,
             digestService,
-            createLayeredPropertyMapFactory(info),
+            createLayeredPropertyMapFactory(),
         )
 
         repo.savePrivateKey(ctx)
 
-        val found = repo.findKey(pubKey.publicKey)
+        val found = repo.findKey(info.alias!!)
 
         assertThat(found)
             .usingRecursiveComparison().ignoringFields("timestamp")
@@ -196,31 +234,51 @@ class SigningRepositoryTest : CryptoRepositoryTest() {
 
 //    @ParameterizedTest
 //    @MethodSource("emfs")
-//    fun `findKey by alias`(emf: EntityManagerFactory) {
-//
-//    }
-//
-//    @ParameterizedTest
-//    @MethodSource("emfs")
-//    fun `findKey by public key`(emf: EntityManagerFactory) {
-//
-//    }
-//
-//    @ParameterizedTest
-//    @MethodSource("emfs")
 //    fun query(emf: EntityManagerFactory) {
 //
 //    }
-//
-//    @ParameterizedTest
-//    @MethodSource("emfs")
-//    fun lookupByPublicKeyShortHashes(emf: EntityManagerFactory) {
-//
-//    }
-//
-//    @ParameterizedTest
-//    @MethodSource("emfs")
-//    fun lookupByPublicKeyHashes(emf: EntityManagerFactory) {
-//
-//    }
+
+    @ParameterizedTest
+    @MethodSource("emfs")
+    fun lookupByPublicKeyShortHashes(emf: EntityManagerFactory) {
+        if(!createdKeys.containsKey(emf))
+            createdKeys[emf] = createKeys(emf)
+
+        val repo = SigningRepositoryImpl(
+            emf,
+            defaultTenantId,
+            cipherSchemeMetadata,
+            digestService,
+            createLayeredPropertyMapFactory(),
+        )
+        val allKeys = createdKeys[emf]!!
+        val lookFor = allKeys.map {
+            parseSecureHash(fullPublicKeyIdFromBytes(it.publicKey, digestService))
+        }
+        val found = repo.lookupByPublicKeyHashes(lookFor.toSet())
+
+        assertThat(found).containsExactlyInAnyOrderElementsOf(allKeys)
+    }
+
+    @ParameterizedTest
+    @MethodSource("emfs")
+    fun lookupByPublicKeyHashes(emf: EntityManagerFactory) {
+        if(!createdKeys.containsKey(emf))
+            createdKeys[emf] = createKeys(emf)
+
+        val repo = SigningRepositoryImpl(
+            emf,
+            defaultTenantId,
+            cipherSchemeMetadata,
+            digestService,
+            createLayeredPropertyMapFactory(),
+        )
+        val allKeys = createdKeys[emf]!!
+        val lookFor = allKeys.map {
+            ShortHash.of(parseSecureHash(fullPublicKeyIdFromBytes(it.publicKey, digestService)))
+        }
+        val found = repo.lookupByPublicKeyShortHashes(lookFor.toSet())
+
+        assertThat(found).containsExactlyInAnyOrderElementsOf(allKeys)
+    }
 }
