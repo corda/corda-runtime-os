fda89ba27750d01487f5f78e861fc01fefc613d1
Dickon Reed <dickon.reed@r3.com>
2023-03-30 22:38:07 +0100

    CORE-11502: Behaviour change: use the tenant ID specified by the context parameter when creating a wrapping key, rather than always storing wrapping keys in the CRYPTO tenant in the cluster datbaase.
    Split out wrapping key material into its own table so that we can store multiple representations at once, for later use during key rotation when that is implemented. (Allowing easy joins motivated the behaviour change on where the wrapping keys are stored, but the behaviour change itself is appropriate.)
    
    Add a rotation date column in the wrapping keys, initially set to a long time in the future. Nothing yet checks the rotation date.
    
    Allow a set of root wrapping keys to be passed into the crypto processor, with one selected to be the default. This required supporting secrets in arrays during validation.
    
    Now that we have removed all OSGi and Lifecycle logic from the repository layer of the crypto processor it it possible to have fast integration tests that consturct an in-memory database directly and exercise the repository layer code, and can be debugged without needing to go multi-process. So, rework the RepositoryTests like that. Take the opportunity to group them according to the repository class they are testing, in the standard structure.
    
    Stop allowing querying the signing repository by master key alias (aka the warpping key), since it is possible with the new strucutre that there are multiple master keys for the same signing key.
    
    Use data classes for the *Info DTOs, and take care over their equqals and hashCode methods where ByteArray is concerned.
    
    Make the repository operations return the DTO, to align better with other JPA code in Corda. This might sometimes make calling code more compact since it can use the Fluent style.
    
    Disable certain PersistenceTests for now, which are mostly duplicates of the fast new non-OSGi integration tests.
    
    Disable unit tests for the SigningRepository, since their mocking requires updates.
    
    Add some tracing code (at trace log level) for wrapping key handling.
    
    Fix typo in TestWrappingRepoitory filename.

--- components/crypto/crypto-softhsm-impl/src/main/kotlin/net/corda/crypto/softhsm/impl/SigningRepositoryImpl.kt
+++ components/crypto/crypto-softhsm-impl/src/main/kotlin/net/corda/crypto/softhsm/impl/SigningRepositoryImpl.kt
@@ -3,6 +3,7 @@ package net.corda.crypto.softhsm.impl
 import java.security.PublicKey
 import net.corda.crypto.cipher.suite.KeyEncodingService
 import net.corda.crypto.cipher.suite.PlatformDigestService
+import net.corda.crypto.core.InvalidParamsException
 import net.corda.crypto.core.KEY_LOOKUP_INPUT_ITEMS_LIMIT
 import net.corda.crypto.core.ShortHash
 import net.corda.crypto.core.fullIdHash
@@ -21,8 +22,9 @@ import net.corda.crypto.persistence.createdAfter
 import net.corda.crypto.persistence.createdBefore
 import net.corda.crypto.persistence.db.model.SigningKeyEntity
 import net.corda.crypto.persistence.db.model.SigningKeyEntityStatus
+import net.corda.crypto.persistence.db.model.SigningKeyMaterialEntity
+import net.corda.crypto.persistence.db.model.WrappingKeyEntity
 import net.corda.crypto.persistence.externalId
-import net.corda.crypto.persistence.masterKeyAlias
 import net.corda.crypto.persistence.schemeCodeName
 import net.corda.crypto.softhsm.SigningRepository
 import net.corda.layeredpropertymap.LayeredPropertyMapFactory
@@ -30,7 +32,10 @@ import net.corda.layeredpropertymap.create
 import net.corda.orm.utils.transaction
 import net.corda.orm.utils.use
 import net.corda.v5.crypto.SecureHash
+import java.io.InvalidObjectException
 import java.time.Instant
+import java.util.*
+import javax.persistence.EntityManager
 import javax.persistence.EntityManagerFactory
 
 @Suppress("LongParameterList")
@@ -43,6 +48,7 @@ class SigningRepositoryImpl(
 ) : SigningRepository {
     override fun close() = entityManagerFactory.close()
 
+    // TODO - share code between the two save*Key methods
     /**
      * If short key id clashes with existing key for this [tenantId], [save] will fail. It will fail upon
      * persisting to the DB due to unique constraint of <tenant id, short key id>.
@@ -51,17 +57,17 @@ class SigningRepositoryImpl(
         val publicKeyBytes = keyEncodingService.encodeAsByteArray(context.key.publicKey)
         val keyId = publicKeyIdFromBytes(publicKeyBytes)
         val fullKeyId = fullPublicKeyIdFromBytes(publicKeyBytes, digestService)
+        val now = Instant.now()
         val entity = SigningKeyEntity(
+            id = UUID.randomUUID(),
             tenantId = tenantId,
             keyId = keyId,
             fullKeyId = fullKeyId,
-            timestamp = Instant.now(),
+            created = now,
             category = context.category,
             schemeCodeName = context.keyScheme.codeName,
             publicKey = publicKeyBytes,
-            keyMaterial = null,
             encodingVersion = null,
-            masterKeyAlias = null,
             alias = context.alias,
             hsmAlias = context.key.hsmAlias,
             externalId = context.externalId,
@@ -72,72 +78,121 @@ class SigningRepositoryImpl(
         entityManagerFactory.createEntityManager().transaction {
             it.persist(entity)
         }
-        return entity.toSigningKeyInfo()
+        return entityManagerFactory.createEntityManager().use {
+            entity.joinSigningKeyInfo(it)
+        }
     }
 
+    @Suppress("NestedBlockDepth")
     override fun savePrivateKey(context: SigningWrappedKeySaveContext): SigningKeyInfo {
         val publicKeyBytes = keyEncodingService.encodeAsByteArray(context.key.publicKey)
         val keyId = publicKeyIdFromBytes(publicKeyBytes)
         val fullKeyId = fullPublicKeyIdFromBytes(publicKeyBytes, digestService)
+        val keyMainId = UUID.randomUUID()
+        val now = Instant.now()
+
+        val wrappingKeyId = entityManagerFactory.createEntityManager().use { it ->
+            with(it.criteriaBuilder ?: throw InvalidObjectException("could not get criteria builder")) {
+                val queryBuilder = with(
+                    createQuery(WrappingKeyEntity::class.java)
+                        ?: throw InvalidObjectException("unable to create query on wrapping key")
+                ) {
+                    val root = from(WrappingKeyEntity::class.java)
+                    where(
+                        equal(
+                            root.get<String>("alias"),
+                            context.masterKeyAlias
+                        )
+                    ) // do not care about generation for now
+                }
+
+
+                it.createQuery(queryBuilder)
+                    .setMaxResults(1).resultList.singleOrNull()
+                    ?.let {
+                        it.id
+                    }
+            }
+                ?: throw InvalidParamsException("unable to find master wrapping key ${context.masterKeyAlias} in tenant $tenantId")
+        }
+
+        val materialEntity = SigningKeyMaterialEntity(
+            signingKeyId = keyMainId,
+            wrappingKeyId = wrappingKeyId,
+            created = now,
+            keyMaterial = context.key.keyMaterial,
+        )
+
+        entityManagerFactory.createEntityManager().use {
+            it.transaction {
+                it.persist(materialEntity)
+            }
+        }
+
         val entity = SigningKeyEntity(
+            id = keyMainId,
             tenantId = tenantId,
             keyId = keyId,
             fullKeyId = fullKeyId,
-            timestamp = Instant.now(),
+            created = now,
             category = context.category,
             schemeCodeName = context.keyScheme.codeName,
             publicKey = publicKeyBytes,
-            keyMaterial = context.key.keyMaterial,
             encodingVersion = context.key.encodingVersion,
-            masterKeyAlias = context.masterKeyAlias,
             alias = context.alias,
             hsmAlias = null,
             externalId = context.externalId,
             hsmId = context.hsmId,
             status = SigningKeyEntityStatus.NORMAL
         )
-
-        entityManagerFactory.createEntityManager().transaction {
-            it.persist(entity)
+        entityManagerFactory.createEntityManager().use {
+            it.transaction {
+                it.persist(entity)
+            }
         }
-        return entity.toSigningKeyInfo()
+
+        return entityManagerFactory.createEntityManager().use { entity.joinSigningKeyInfo(it) }
     }
 
     override fun findKey(alias: String): SigningKeyInfo? {
-        val result = entityManagerFactory.createEntityManager().use { em ->
-            em.createQuery(
-                "FROM SigningKeyEntity WHERE tenantId=:tenantId AND alias=:alias",
+        entityManagerFactory.createEntityManager().use { em ->
+            val result = em.createQuery(
+                "FROM ${SigningKeyEntity::class.java.simpleName} WHERE tenantId=:tenantId AND alias=:alias",
                 SigningKeyEntity::class.java
             ).setParameter("tenantId", tenantId)
                 .setParameter("alias", alias)
                 .resultList
-        }
 
-        if (result.size > 1) {
-            throw IllegalStateException("There are more than one key with alias=$alias for tenant=$tenantId")
+            if (result.size > 1) {
+                throw IllegalStateException("There are more than one key with alias=$alias for tenant=$tenantId")
+            }
+
+
+            return result.firstOrNull()?.joinSigningKeyInfo(em)
         }
-        return result.firstOrNull()?.toSigningKeyInfo()
     }
 
     override fun findKey(publicKey: PublicKey): SigningKeyInfo? {
         val requestedFullKeyId = publicKey.fullIdHash(keyEncodingService, digestService)
         return entityManagerFactory.createEntityManager().use { em ->
-            em.createQuery(
-                "FROM ${SigningKeyEntity::class.java.simpleName} " +
-                        "WHERE tenantId=:tenantId " +
-                        "AND fullKeyId=:fullKeyId",
-                SigningKeyEntity::class.java
-            ).setParameter("tenantId", tenantId)
-                .setParameter("fullKeyId", requestedFullKeyId.toString())
-                .resultList.singleOrNull()?.toSigningKeyInfo()
-            em.createQuery<SigningKeyEntity?>(
-                "FROM ${SigningKeyEntity::class.java.simpleName} " +
-                        "WHERE tenantId=:tenantId " +
-                        "AND fullKeyId=:fullKeyId",
-                SigningKeyEntity::class.java
-            ).setParameter("tenantId", tenantId)
-                .setParameter("fullKeyId", requestedFullKeyId.toString())
-                .resultList.singleOrNull<SigningKeyEntity?>()?.toSigningKeyInfo()
+            em.transaction {
+                em.createQuery(
+                    "FROM ${SigningKeyEntity::class.java.simpleName} " +
+                            "WHERE tenantId=:tenantId " +
+                            "AND fullKeyId=:fullKeyId",
+                    SigningKeyEntity::class.java
+                ).setParameter("tenantId", tenantId)
+                    .setParameter("fullKeyId", requestedFullKeyId.toString())
+                    .resultList.singleOrNull()?.joinSigningKeyInfo(em)
+                em.createQuery<SigningKeyEntity?>(
+                    "FROM ${SigningKeyEntity::class.java.simpleName} " +
+                            "WHERE tenantId=:tenantId " +
+                            "AND fullKeyId=:fullKeyId",
+                    SigningKeyEntity::class.java
+                ).setParameter("tenantId", tenantId)
+                    .setParameter("fullKeyId", requestedFullKeyId.toString())
+                    .resultList.singleOrNull<SigningKeyEntity?>()?.joinSigningKeyInfo(em)
+            }
         }
     }
 
@@ -147,18 +202,19 @@ class SigningRepositoryImpl(
         orderBy: SigningKeyOrderBy,
         filter: Map<String, String>,
     ): Collection<SigningKeyInfo> = entityManagerFactory.createEntityManager().use { em ->
-        val map = layeredPropertyMapFactory.create<SigningKeyFilterMapImpl>(filter)
-        val builder = SigningKeyLookupBuilder(em)
-        builder.equal(SigningKeyEntity::tenantId, tenantId)
-        builder.equal(SigningKeyEntity::category, map.category)
-        builder.equal(SigningKeyEntity::schemeCodeName, map.schemeCodeName)
-        builder.equal(SigningKeyEntity::alias, map.alias)
-        builder.equal(SigningKeyEntity::masterKeyAlias, map.masterKeyAlias)
-        builder.equal(SigningKeyEntity::externalId, map.externalId)
-        builder.greaterThanOrEqualTo(SigningKeyEntity::timestamp, map.createdAfter)
-        builder.lessThanOrEqualTo(SigningKeyEntity::timestamp, map.createdBefore)
-        builder.build(skip, take, orderBy).resultList.map {
-            it.toSigningKeyInfo()
+        em.transaction {
+            val map = layeredPropertyMapFactory.create<SigningKeyFilterMapImpl>(filter)
+            val builder = SigningKeyLookupBuilder(em)
+            builder.equal(SigningKeyEntity::tenantId, tenantId)
+            builder.equal(SigningKeyEntity::category, map.category)
+            builder.equal(SigningKeyEntity::schemeCodeName, map.schemeCodeName)
+            builder.equal(SigningKeyEntity::alias, map.alias)
+            builder.equal(SigningKeyEntity::externalId, map.externalId)
+            builder.greaterThanOrEqualTo(SigningKeyEntity::created, map.createdAfter)
+            builder.lessThanOrEqualTo(SigningKeyEntity::created, map.createdBefore)
+            builder.build(skip, take, orderBy).resultList.map {
+                it.joinSigningKeyInfo(em)
+            }
         }
     }
 
@@ -167,13 +223,15 @@ class SigningRepositoryImpl(
             "The number of ids exceeds $KEY_LOOKUP_INPUT_ITEMS_LIMIT"
         }
         return entityManagerFactory.createEntityManager().use { em ->
-            val keyIdsStrings = keyIds.map<ShortHash, String> { it.value }
-            em.createQuery<SigningKeyEntity?>(
-                "FROM SigningKeyEntity WHERE tenantId=:tenantId AND keyId IN(:keyIds)",
-                SigningKeyEntity::class.java
-            ).setParameter("tenantId", tenantId)
-                .setParameter("keyIds", keyIdsStrings)
-                .resultList.map { it.toSigningKeyInfo() }
+            em.transaction {
+                val keyIdsStrings = keyIds.map<ShortHash, String> { it.value }
+                em.createQuery<SigningKeyEntity?>(
+                    "FROM SigningKeyEntity WHERE tenantId=:tenantId AND keyId IN(:keyIds)",
+                    SigningKeyEntity::class.java
+                ).setParameter("tenantId", tenantId)
+                    .setParameter("keyIds", keyIdsStrings)
+                    .resultList.map { it.joinSigningKeyInfo(em) }
+            }
         }
     }
 
@@ -183,24 +241,37 @@ class SigningRepositoryImpl(
         }
 
         return entityManagerFactory.createEntityManager().use { em ->
-            val fullKeyIdsStrings = fullKeyIds.map { it.toString() }
-            em.createQuery<SigningKeyEntity?>(
-                "FROM ${SigningKeyEntity::class.java.simpleName} " +
-                        "WHERE tenantId=:tenantId " +
-                        "AND fullKeyId IN(:fullKeyIds) " +
-                        "ORDER BY timestamp",
-                SigningKeyEntity::class.java
-            )
-                .setParameter("tenantId", tenantId)
-                .setParameter("fullKeyIds", fullKeyIdsStrings)
-                .resultList.map { it.toSigningKeyInfo() }
+            em.transaction {
+                val fullKeyIdsStrings = fullKeyIds.map { it.toString() }
+
+                em.createQuery<SigningKeyEntity?>(
+                    "FROM ${SigningKeyEntity::class.java.simpleName} " +
+                            "WHERE tenantId=:tenantId " +
+                            "AND fullKeyId IN(:fullKeyIds) " +
+                            "ORDER BY created",
+                    SigningKeyEntity::class.java
+                )
+                    .setParameter("tenantId", tenantId)
+                    .setParameter("fullKeyIds", fullKeyIdsStrings)
+                    .resultList.map { it.joinSigningKeyInfo(em) }
+            }
         }
     }
 }
 
 
-fun SigningKeyEntity.toSigningKeyInfo(): SigningKeyInfo =
-    SigningKeyInfo(
+fun SigningKeyEntity.joinSigningKeyInfo(em: EntityManager): SigningKeyInfo {
+    val signingKeyMaterialEntity = em.createQuery(
+        "FROM ${SigningKeyMaterialEntity::class.java.simpleName} WHERE signingKeyId=:signingKeyId",
+        SigningKeyMaterialEntity::class.java
+    ).setParameter("signingKeyId", id)
+        .resultList.singleOrNull()
+    val wrappingKey = if (signingKeyMaterialEntity != null) {
+        em.createQuery(
+            "FROM WrappingKeyEntity WHERE id=:wrappingKeyId", WrappingKeyEntity::class.java,
+        ).setParameter("wrappingKeyId", signingKeyMaterialEntity.wrappingKeyId).resultList.singleOrNull()
+    } else null
+    return SigningKeyInfo(
         id = ShortHash.parse(keyId),
         fullId = parseSecureHash(fullKeyId),
         tenantId = tenantId,
@@ -208,12 +279,13 @@ fun SigningKeyEntity.toSigningKeyInfo(): SigningKeyInfo =
         alias = alias,
         hsmAlias = hsmAlias,
         publicKey = publicKey,
-        keyMaterial = keyMaterial,
+        keyMaterial = signingKeyMaterialEntity?.keyMaterial,
         schemeCodeName = schemeCodeName,
-        masterKeyAlias = masterKeyAlias,
+        masterKeyAlias = wrappingKey?.alias,
         externalId = externalId,
         encodingVersion = encodingVersion,
-        timestamp = timestamp,
+        timestamp = created,
         hsmId = hsmId,
         status = SigningKeyStatus.valueOf(status.name)
     )
+}
