fa43ea7a49afb8ed2d1686a6e847c8a02d376f85
Lajos Veres <lajos.veres@r3.com>
2023-03-30 19:01:31 +0100

    CORE-12139 move privacysalt to runtime-os (#3507)

--- libs/ledger/ledger-common-data/src/main/kotlin/net/corda/ledger/common/data/transaction/PrivacySaltImpl.kt
+++ libs/ledger/ledger-common-data/src/main/kotlin/net/corda/ledger/common/data/transaction/PrivacySaltImpl.kt
@@ -3,7 +3,6 @@ package net.corda.ledger.common.data.transaction
 import net.corda.v5.base.annotations.CordaSerializable
 import net.corda.v5.base.types.OpaqueBytes
 import net.corda.v5.crypto.SecureHash
-import net.corda.v5.ledger.common.transaction.PrivacySalt
 
 /**
  * A privacy salt is required to compute nonces per transaction component in order to ensure that an adversary cannot
