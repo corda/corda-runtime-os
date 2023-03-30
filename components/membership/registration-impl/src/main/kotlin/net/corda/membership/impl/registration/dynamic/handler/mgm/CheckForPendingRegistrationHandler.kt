1955899614d8945365b9ce66500c0c07f9d0f0ca
nikinagy <nikolett.nagy@r3.com>
2023-03-30 09:52:06 +0100

    add new handlers

---
+++ components/membership/registration-impl/src/main/kotlin/net/corda/membership/impl/registration/dynamic/handler/mgm/CheckForPendingRegistrationHandler.kt
@@ -0,0 +1,60 @@
+package net.corda.membership.impl.registration.dynamic.handler.mgm
+
+import net.corda.data.membership.command.registration.RegistrationCommand
+import net.corda.data.membership.command.registration.mgm.CheckForPendingRegistration
+import net.corda.data.membership.command.registration.mgm.StartRegistration
+import net.corda.data.membership.common.RegistrationStatus
+import net.corda.data.membership.state.RegistrationState
+import net.corda.membership.impl.registration.dynamic.handler.RegistrationHandler
+import net.corda.membership.impl.registration.dynamic.handler.RegistrationHandlerResult
+import net.corda.membership.persistence.client.MembershipQueryClient
+import net.corda.messaging.api.records.Record
+import net.corda.schema.Schemas
+import net.corda.virtualnode.toCorda
+import org.slf4j.LoggerFactory
+
+class CheckForPendingRegistrationHandler(
+    private val membershipQueryClient: MembershipQueryClient,
+) : RegistrationHandler<CheckForPendingRegistration> {
+    private companion object {
+        private val logger = LoggerFactory.getLogger(this::class.java.enclosingClass)
+    }
+
+    override val commandType = CheckForPendingRegistration::class.java
+
+    override fun invoke(state: RegistrationState?, key: String, command: CheckForPendingRegistration): RegistrationHandlerResult {
+        val outputCommand = try{
+            state?.let {
+                logger.info("Looking for the next request for member ${command.registeringMember.x500Name}.")
+                val nextRequest = membershipQueryClient.queryRegistrationRequests(
+                    command.mgm.toCorda(),
+                    command.registeringMember.toCorda().x500Name,
+                    listOf(RegistrationStatus.RECEIVED_BY_MGM),
+                    1
+                ).getOrThrow().firstOrNull()
+                // need to check if there were any results at all
+                if(nextRequest != null) {
+                    logger.info("Retrieved next request for member ${command.registeringMember.x500Name} " +
+                            "with ID `${nextRequest.registrationId}` from the database. Proceeding with registration.")
+                    StartRegistration()
+                } else {
+                    logger.info("There are no registration requests queued " +
+                            "for member ${command.registeringMember.x500Name}.")
+                }
+            }
+        } catch (ex: Exception) {
+            logger.warn("Exception happened while looking for the next request to process. Will re-try again.", ex)
+            command
+        }
+        return if(outputCommand != null) {
+            RegistrationHandlerResult(
+                state,
+                listOf(
+                    Record(Schemas.Membership.REGISTRATION_COMMAND_TOPIC, key, RegistrationCommand(outputCommand))
+                )
+            )
+        } else {
+            RegistrationHandlerResult(null, emptyList())
+        }
+    }
+}
--- components/membership/registration-impl/src/main/kotlin/net/corda/membership/impl/registration/dynamic/handler/mgm/QueueRegistrationHandler.kt
+++ components/membership/registration-impl/src/main/kotlin/net/corda/membership/impl/registration/dynamic/handler/mgm/QueueRegistrationHandler.kt
@@ -1,4 +1,104 @@
 package net.corda.membership.impl.registration.dynamic.handler.mgm
 
-class QueueRegistrationHandler {
+import net.corda.data.membership.command.registration.RegistrationCommand
+import net.corda.data.membership.command.registration.mgm.CheckForPendingRegistration
+import net.corda.data.membership.command.registration.mgm.QueueRegistration
+import net.corda.data.membership.common.RegistrationStatus
+import net.corda.data.membership.state.RegistrationState
+import net.corda.membership.impl.registration.dynamic.handler.RegistrationHandler
+import net.corda.membership.impl.registration.dynamic.handler.RegistrationHandlerResult
+import net.corda.membership.lib.registration.RegistrationRequest
+import net.corda.membership.persistence.client.MembershipPersistenceClient
+import net.corda.messaging.api.records.Record
+import net.corda.schema.Schemas.Membership.REGISTRATION_COMMAND_TOPIC
+import net.corda.virtualnode.toCorda
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+internal class QueueRegistrationHandler(
+    private val membershipPersistenceClient: MembershipPersistenceClient,
+) : RegistrationHandler<QueueRegistration> {
+    private companion object {
+        val logger: Logger = LoggerFactory.getLogger(this::class.java.enclosingClass)
+        const val MAX_RETRIES = 10
+    }
+
+    override val commandType = QueueRegistration::class.java
+
+    override fun invoke(state: RegistrationState?, key: String, command: QueueRegistration): RegistrationHandlerResult {
+        val registrationId = command.memberRegistrationRequest.registrationId
+        val (outputState, outputCommand) = try {
+            if (command.numberOfRetriesSoFar < MAX_RETRIES) {
+                queueRequest(state, key, command, registrationId)
+            } else {
+                logger.warn(
+                    "Max re-tries exceeded for registration with ID `$registrationId`." +
+                            "Registration is discarded."
+                )
+                Pair(null, emptyList())
+            }
+        } catch (ex: Exception) {
+            logger.warn("Exception happened while queueing the request with ID `$registrationId`. Will re-try again.")
+            increaseNumberOfRetries(state, key, command)
+        }
+
+        return RegistrationHandlerResult(outputState, outputCommand)
+    }
+
+    private fun QueueRegistration.toRegistrationRequest(): RegistrationRequest {
+        return RegistrationRequest(
+            RegistrationStatus.RECEIVED_BY_MGM,
+            memberRegistrationRequest.registrationId,
+            member.toCorda(),
+            memberRegistrationRequest.memberContext,
+            memberRegistrationRequest.memberSignature,
+            memberRegistrationRequest.memberSignatureSpec,
+            memberRegistrationRequest.serial,
+        )
+    }
+
+    private fun queueRequest(
+        state: RegistrationState?, key: String, command: QueueRegistration, registrationId: String
+    ): Pair<RegistrationState?, List<Record<String, RegistrationCommand>>> {
+        logger.info(
+            "MGM queueing registration request for ${command.member.x500Name} " +
+                    "with request ID `$registrationId`."
+        )
+        membershipPersistenceClient.persistRegistrationRequest(
+            command.mgm.toCorda(),
+            command.toRegistrationRequest()
+        ).getOrThrow()
+        logger.info(
+            "MGM put registration request for ${command.member.x500Name} " +
+                    "with request ID `$registrationId` into the queue."
+        )
+        return Pair(
+            state,
+            listOf(
+                Record(
+                    REGISTRATION_COMMAND_TOPIC,
+                    key,
+                    RegistrationCommand((CheckForPendingRegistration(command.member, command.mgm)))
+                )
+            )
+        )
+    }
+
+    private fun increaseNumberOfRetries(state: RegistrationState?, key: String, command: QueueRegistration) = Pair(
+        state,
+        listOf(
+            Record(
+                REGISTRATION_COMMAND_TOPIC,
+                key,
+                RegistrationCommand(
+                    QueueRegistration(
+                        command.mgm,
+                        command.member,
+                        command.memberRegistrationRequest,
+                        command.numberOfRetriesSoFar + 1
+                    )
+                )
+            )
+        )
+    )
 }
