45aaa833c78dd80e5cc3be1936b04505d92f2561
Conal Smith <conal.smith@r3.com>
2023-03-31 16:37:27 +0100

    CORE-12070 - adding flow fiber cache per cluster level, with invalidation on flow completion or failure.

--- components/flow/flow-service/src/main/kotlin/net/corda/flow/fiber/factory/FlowFiberFactoryImpl.kt
+++ components/flow/flow-service/src/main/kotlin/net/corda/flow/fiber/factory/FlowFiberFactoryImpl.kt
@@ -9,16 +9,22 @@ import net.corda.flow.fiber.FlowFiberExecutionContext
 import net.corda.flow.fiber.FlowFiberImpl
 import net.corda.flow.fiber.FlowLogicAndArgs
 import net.corda.flow.fiber.FiberExceptionConstants
+import net.corda.flow.fiber.FlowFiberCache
 import net.corda.flow.pipeline.exceptions.FlowFatalException
 import net.corda.metrics.CordaMetrics
+import org.osgi.service.component.annotations.Activate
 import org.osgi.service.component.annotations.Component
 import org.osgi.service.component.annotations.Deactivate
+import org.osgi.service.component.annotations.Reference
 import java.util.UUID
 import java.util.concurrent.ExecutorService
 
 @Component
 @Suppress("Unused")
-class FlowFiberFactoryImpl : FlowFiberFactory {
+class FlowFiberFactoryImpl @Activate constructor(
+    @Reference(service = FlowFiberCache::class)
+    private val flowFiberCache: FlowFiberCache
+) : FlowFiberFactory {
 
     private val currentScheduler: FiberScheduler = FiberExecutorScheduler(
         "Same thread scheduler",
@@ -52,13 +58,18 @@ class FlowFiberFactoryImpl : FlowFiberFactory {
             .withTag(CordaMetrics.Tag.FlowClass, flowFiberExecutionContext.flowCheckpoint.flowStartContext.flowClassName)
             .build()
             .recordCallable {
-                flowFiberExecutionContext.sandboxGroupContext.checkpointSerializer.deserialize(
-                    flowFiberExecutionContext.flowCheckpoint.serializedFiber.array(),
-                    FlowFiberImpl::class.java
-                )
+                getFromCacheOrDeserialize(flowFiberExecutionContext)
             }!!
 
         return FiberFuture(fiber, fiber.resume(flowFiberExecutionContext, suspensionOutcome, currentScheduler))
+    }
+
+    private fun getFromCacheOrDeserialize(flowFiberExecutionContext: FlowFiberExecutionContext): FlowFiberImpl {
+        return flowFiberCache.get(flowFiberExecutionContext.flowCheckpoint.flowId) as FlowFiberImpl?
+            ?: flowFiberExecutionContext.sandboxGroupContext.checkpointSerializer.deserialize(
+                flowFiberExecutionContext.flowCheckpoint.serializedFiber.array(),
+                FlowFiberImpl::class.java
+            )
     }
 
     @Deactivate
