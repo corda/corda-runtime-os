b71664aeb5dfb7d3cb70fab5248a5f90f0258fa4
Filipe Oliveira <filipe.oliveira@r3.com>
2023-04-03 16:52:43 +0100

    Simplified the ChannelType by updating the enums in the API.

--- libs/external-messaging/src/main/kotlin/net/corda/libs/external/messaging/entities/ChannelType.kt
+++ libs/external-messaging/src/main/kotlin/net/corda/libs/external/messaging/entities/ChannelType.kt
@@ -1,14 +1,6 @@
 package net.corda.libs.external.messaging.entities
 
-import com.fasterxml.jackson.annotation.JsonProperty
-
-enum class ChannelType(val value: String) {
-    @JsonProperty("send")
-    SEND("send"),
-    @JsonProperty("send-receive")
-    SEND_RECEIVE("send-receive");
-
-    override fun toString(): String {
-        return value
-    }
+enum class ChannelType() {
+    SEND,
+    SEND_RECEIVE
 }
