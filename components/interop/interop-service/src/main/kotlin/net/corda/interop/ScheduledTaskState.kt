ad98fdf43a2d8b168effcf265f8ee1661171d012
SaurabhR3 <110393715+SaurabhR3@users.noreply.github.com>
2023-04-03 14:47:02 +0100

    CORE-10424 : Changes to manage session for interop request (#3464)
    Changes for managing session for interop request
    Changes set/retrieve fakeidentity
    Changed corda api version

---
+++ components/interop/interop-service/src/main/kotlin/net/corda/interop/ScheduledTaskState.kt
@@ -0,0 +1,19 @@
+package net.corda.interop
+
+import net.corda.lifecycle.Resource
+import net.corda.messaging.api.publisher.Publisher
+import java.util.concurrent.ScheduledExecutorService
+import java.util.concurrent.ScheduledFuture
+
+data class ScheduledTaskState(
+    val executorService: ScheduledExecutorService,
+    val publisher: Publisher?,
+    val tasks: MutableMap<String, ScheduledFuture<*>>
+) : Resource {
+    override fun close() {
+        tasks.values.forEach { it.cancel(false) }
+        tasks.clear()
+        executorService.shutdown()
+        publisher?.close()
+    }
+}
