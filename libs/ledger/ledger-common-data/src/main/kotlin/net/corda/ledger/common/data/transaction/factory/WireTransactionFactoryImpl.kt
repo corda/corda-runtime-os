7c87be3e3d5bf797c2247a3987a6f4508b143eb8
Christian Sailer <christian.sailer@r3.com>
2023-04-04 11:36:49 +0100

    Revert formatting changes

--- libs/ledger/ledger-common-data/src/main/kotlin/net/corda/ledger/common/data/transaction/factory/WireTransactionFactoryImpl.kt
+++ libs/ledger/ledger-common-data/src/main/kotlin/net/corda/ledger/common/data/transaction/factory/WireTransactionFactoryImpl.kt
@@ -77,12 +77,11 @@ class WireTransactionFactoryImpl @Activate constructor(
         privacySalt: PrivacySalt
     ): WireTransaction {
         checkComponentGroups(componentGroupLists.values)
-        val metadata =
-            parseMetadata(
-                requireNotNull(componentGroupLists[TransactionMetadataImpl.ALL_LEDGER_METADATA_COMPONENT_GROUP_ID]?.first()) {
-                    "There must be a metadata component group at index 0 with a single leaf"
-                }
-            )
+        val metadata = parseMetadata(
+            requireNotNull(componentGroupLists[TransactionMetadataImpl.ALL_LEDGER_METADATA_COMPONENT_GROUP_ID]?.first()) {
+                "There must be a metadata component group at index 0 with a single leaf"
+            }
+        )
 
         val completeComponentGroupLists = (0 until metadata.getNumberOfComponentGroups()).map { index ->
             componentGroupLists.getOrElse(index) { arrayListOf() }
