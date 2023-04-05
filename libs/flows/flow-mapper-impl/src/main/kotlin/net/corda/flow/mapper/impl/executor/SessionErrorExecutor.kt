3c80cf6a135c9c7e999cd7e2a33da31ccfd2490d
Emily Bowe <emily.bowe@r3.com>
2023-04-05 12:23:13 +0100

    more work on SessionErrorExecutor

--- libs/flows/flow-mapper-impl/src/main/kotlin/net/corda/flow/mapper/impl/executor/SessionErrorExecutor.kt
+++ libs/flows/flow-mapper-impl/src/main/kotlin/net/corda/flow/mapper/impl/executor/SessionErrorExecutor.kt
@@ -4,37 +4,47 @@ import net.corda.data.flow.event.SessionEvent
 import net.corda.data.flow.state.mapper.FlowMapperState
 import net.corda.data.flow.state.mapper.FlowMapperStateType
 import net.corda.flow.mapper.FlowMapperResult
+import net.corda.flow.mapper.executor.FlowMapperEventExecutor
 import org.slf4j.LoggerFactory
 
 class SessionErrorExecutor(
     private val eventKey: String,
     private val sessionEvent: SessionEvent,
-) {
+    private val flowMapperState: FlowMapperState?
+) : FlowMapperEventExecutor {
+
     private companion object {
         private val log = LoggerFactory.getLogger(this::class.java.enclosingClass)
     }
 
-    fun processSessionErrorEvents(flowMapperState: FlowMapperState): FlowMapperResult {
-        val state = flowMapperState.status
-        val errorMsg = "Flow mapper received error event from counterparty for session which does not exist. " +
-                "Session may have expired. "
+    private val errorMsg = "Flow mapper received error event from counterparty for session which does not exist. " +
+            "Session may have expired. This error event will be %. Key: $eventKey, Event: $sessionEvent"
 
-        when (state) {
+    override fun execute(): FlowMapperResult {
+        return if (flowMapperState == null) {
+            log.warn(errorMsg.format("Ignored"))
+            FlowMapperResult(null, listOf())
+        } else {
+            processSessionErrorEvents(flowMapperState)
+        }
+    }
+
+    private fun processSessionErrorEvents(flowMapperState: FlowMapperState): FlowMapperResult {
+        when (flowMapperState.status) {
             FlowMapperStateType.ERROR -> {
-                //we get the error and are already in error - log and ignore
-                log.warn(errorMsg + "Ignoring event. Key: $eventKey, Event: $sessionEvent")
+                log.warn(errorMsg.format("Ignored"))
                 FlowMapperResult(null, listOf())
             }
             FlowMapperStateType.OPEN -> {
-                //we get the error but aren't in error - log and forward error to flow
-                log.warn(errorMsg + "Forwarding event. Key: $eventKey, Event: $sessionEvent")
+                log.warn(errorMsg.format("Forwarded"))
                 FlowMapperResult(null, listOf())
             }
             FlowMapperStateType.CLOSING -> {
-                //we get the error but aren't in error - log and ignore
-                log.warn(errorMsg + "Ignoring event. Key: $eventKey, Event: $sessionEvent")
+                log.warn(errorMsg.format("Ignored"))
                 FlowMapperResult(null, listOf())
             }
         }
+        log.warn(errorMsg.format("Ignored"))
+        return FlowMapperResult(null, listOf())
     }
 }
