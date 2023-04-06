38be2b141c33a772ce1364704d966ece12a7506f
Filipe Oliveira <filipe.oliveira@r3.com>
2023-04-06 15:11:02 +0100

    Improved the code in `ExternalMessagingConfigProviderTest`.

--- libs/external-messaging/src/test/kotlin/net/corda/libs/external/messaging/test/ExternalMessagingConfigProviderTest.kt
+++ libs/external-messaging/src/test/kotlin/net/corda/libs/external/messaging/test/ExternalMessagingConfigProviderTest.kt
@@ -5,6 +5,9 @@ import net.corda.libs.configuration.SmartConfigFactory
 import net.corda.libs.external.messaging.ExternalMessagingConfigDefaults
 import net.corda.libs.external.messaging.ExternalMessagingConfigProviderImpl
 import net.corda.libs.external.messaging.entities.InactiveResponseType
+import net.corda.schema.configuration.ExternalMessagingConfig.EXTERNAL_MESSAGING_ACTIVE
+import net.corda.schema.configuration.ExternalMessagingConfig.EXTERNAL_MESSAGING_INTERACTIVE_RESPONSE_TYPE
+import net.corda.schema.configuration.ExternalMessagingConfig.EXTERNAL_MESSAGING_RECEIVE_TOPIC_PATTERN
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
@@ -15,24 +18,22 @@ class ExternalMessagingConfigProviderTest {
         val expectedConfigDefaults =
             ExternalMessagingConfigDefaults("user.123.a.b.c.receive", false, InactiveResponseType.IGNORE)
 
-        val defaultConfig = genRouteDefaultConfig(expectedConfigDefaults)
+        val defaultConfig = expectedConfigDefaults.toSmartConfig()
 
         val configProvider = ExternalMessagingConfigProviderImpl(defaultConfig)
         assertThat(configProvider.getDefaults()).isEqualTo(expectedConfigDefaults)
     }
 
-    private fun genRouteDefaultConfig(expectedConfigDefaults: ExternalMessagingConfigDefaults) =
+    private fun ExternalMessagingConfigDefaults.toSmartConfig() =
         SmartConfigFactory.createWithoutSecurityServices()
-            .create(ConfigFactory.parseString(genRouteDefaultConfigJsonStr(expectedConfigDefaults)))
+            .create(toConfig())
 
-    private fun genRouteDefaultConfigJsonStr(expectedConfigDefaults: ExternalMessagingConfigDefaults) =
-        """
-            {
-                "routeDefaults": {
-                    "receiveTopicPattern": "${expectedConfigDefaults.receiveTopicPattern}",
-                    "active": "${expectedConfigDefaults.isActive}",
-                    "inactiveResponseType": "${expectedConfigDefaults.inactiveResponseType}"
-                }
-            }
-        """.trimIndent()
+    private fun ExternalMessagingConfigDefaults.toConfig() =
+        ConfigFactory.parseMap(
+            mapOf<String, Any>(
+                EXTERNAL_MESSAGING_RECEIVE_TOPIC_PATTERN to receiveTopicPattern,
+                EXTERNAL_MESSAGING_ACTIVE to isActive,
+                EXTERNAL_MESSAGING_INTERACTIVE_RESPONSE_TYPE to inactiveResponseType.toString()
+            )
+        )
 }
