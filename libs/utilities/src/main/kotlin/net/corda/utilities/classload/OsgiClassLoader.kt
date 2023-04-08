9002e6b5a86a260fce0c9e3206fbc094c6cf6729
Chris Rankin <chris.rankin@r3.com>
2023-04-08 14:28:43 +0100

    CORE-12407: Improve detection of OSGi fragment bundles.

--- libs/utilities/src/main/kotlin/net/corda/utilities/classload/OsgiClassLoader.kt
+++ libs/utilities/src/main/kotlin/net/corda/utilities/classload/OsgiClassLoader.kt
@@ -1,9 +1,11 @@
 package net.corda.utilities.classload
 
-import net.corda.utilities.EmptyEnumeration
 import org.osgi.framework.Bundle
+import org.osgi.framework.wiring.BundleRevision
+import org.osgi.framework.wiring.BundleRevision.TYPE_FRAGMENT
 import java.net.URL
-import java.util.*
+import java.util.Collections.emptyEnumeration
+import java.util.Enumeration
 
 /**
  * Allows combining multiple OSGi bundles under the same classloader. This might be useful in places where
@@ -11,22 +13,37 @@ import java.util.*
  *
  * Inspired by `org.hibernate.osgi.OsgiClassLoader`, but is simpler and immutable.
  */
-class OsgiClassLoader(private val bundles: List<Bundle>) : ClassLoader(null) {
+class OsgiClassLoader(bundles: List<Bundle>) : ClassLoader(null) {
 
-    companion object {
+    private companion object {
         init {
             registerAsParallelCapable()
         }
-    }
 
-    override fun findClass(name: String): Class<*> {
-        bundles.forEach { bundle ->
-            bundle.loadClass(name)?.let { return it }
+        private fun isFragment(bundle: Bundle): Boolean {
+            return (bundle.adapt(BundleRevision::class.java).types and TYPE_FRAGMENT) != 0
         }
-        throw ClassNotFoundException("Could not load requested class : $name")
     }
 
-    override fun findResource(name: String): URL? {
+    // Remove all fragment bundles from consideration.
+    private val bundles = bundles.filterNot(::isFragment)
+
+    override fun findClass(name: String?): Class<*> {
+        val suppressed = mutableListOf<Throwable>()
+        bundles.forEach { bundle ->
+            try {
+                return bundle.loadClass(name)
+            } catch (e: Exception) {
+                // ClassNotFoundException, IllegalStateException
+                suppressed.add(e)
+            }
+        }
+        throw ClassNotFoundException("Could not load requested class : $name").also { ex ->
+            suppressed.forEach(ex::addSuppressed)
+        }
+    }
+
+    override fun findResource(name: String?): URL? {
         bundles.forEach { bundle ->
             bundle.getResource(name)?.let { return it }
         }
@@ -34,11 +51,39 @@ class OsgiClassLoader(private val bundles: List<Bundle>) : ClassLoader(null) {
         return null
     }
 
-    override fun findResources(name: String): Enumeration<URL> {
-        bundles.forEach { bundle ->
-            bundle.getResources(name)?.let { return it }
+    override fun findResources(name: String?): Enumeration<URL> {
+        val enumerations = bundles.mapNotNull { bundle ->
+            bundle.getResources(name)
+        }
+        return CompoundEnumeration(enumerations)
+    }
+
+    private class CompoundEnumeration<E>(enumerations: Iterable<Enumeration<E>>) : Enumeration<E> {
+        private val iterator = enumerations.iterator()
+        private var current = if (iterator.hasNext()) {
+            iterator.next()
+        } else {
+            emptyEnumeration()
         }
 
-        return EmptyEnumeration()
+        init {
+            nextValidPosition()
+        }
+
+        override fun hasMoreElements(): Boolean {
+            return current.hasMoreElements()
+        }
+
+        override fun nextElement(): E {
+            return current.nextElement().also {
+                nextValidPosition()
+            }
+        }
+
+        private fun nextValidPosition() {
+            while (!current.hasMoreElements() && iterator.hasNext()) {
+                current = iterator.next()
+            }
+        }
     }
 }
