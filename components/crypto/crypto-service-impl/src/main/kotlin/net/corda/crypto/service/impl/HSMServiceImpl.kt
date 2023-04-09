c71a27239625eaa22402d16a92121db9ae0d38d8
Kyriakos Tharrouniatis <kyriakos.tharrouniatis@r3.com>
2023-04-09 22:51:31 +0100

    Add JIRA ticket reference for removing multi hsm concept

--- components/crypto/crypto-service-impl/src/main/kotlin/net/corda/crypto/service/impl/HSMServiceImpl.kt
+++ components/crypto/crypto-service-impl/src/main/kotlin/net/corda/crypto/service/impl/HSMServiceImpl.kt
@@ -72,7 +72,8 @@ class HSMServiceImpl @Activate constructor(
 
         private val hsmConfig = event.config.toCryptoConfig().hsm()
 
-        // TODO the below API needs to be removed
+        // TODO the below API needs to be removed as part of removing multi hsm concept
+        //  as per https://r3-cev.atlassian.net/browse/CORE-10562
         @Suppress("unused_parameter")
         fun assignHSM(tenantId: String, category: String, context: Map<String, String>): HSMAssociationInfo {
             logger.info("assignHSM(tenant={}, category={})", tenantId, category)
