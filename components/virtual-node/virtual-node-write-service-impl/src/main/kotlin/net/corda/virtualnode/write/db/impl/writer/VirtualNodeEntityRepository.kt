b943c34a2c918dca472595996c3b846f13513ac3
Filipe Oliveira <filipe.oliveira@r3.com>
2023-04-12 10:27:57 +0100

    Removed the class `CpiMetadataLite` and replaced it with `CpiMetadata`. (#3587)
    The ticket CORE-12177 added the `cpks` field to the class `CpiMetadataLite` rendering the class no longer light. To simplify the code, the class `CpiMetadataLite` was replaced by the existing `CpiMetadata` and deleted.

--- components/virtual-node/virtual-node-write-service-impl/src/main/kotlin/net/corda/virtualnode/write/db/impl/writer/VirtualNodeEntityRepository.kt
+++ components/virtual-node/virtual-node-write-service-impl/src/main/kotlin/net/corda/virtualnode/write/db/impl/writer/VirtualNodeEntityRepository.kt
@@ -6,6 +6,7 @@ import net.corda.crypto.core.parseSecureHash
 import net.corda.libs.cpi.datamodel.entities.CpiMetadataEntity
 import net.corda.libs.cpi.datamodel.entities.CpiMetadataEntityKey
 import net.corda.libs.packaging.core.CpiIdentifier
+import net.corda.libs.packaging.core.CpiMetadata
 import net.corda.libs.packaging.core.CpkMetadata
 import net.corda.orm.utils.transaction
 import net.corda.orm.utils.use
@@ -24,7 +25,7 @@ internal class VirtualNodeEntityRepository(
     }
 
     /** Reads CPI metadata from the database. */
-    override fun getCpiMetadataByChecksum(cpiFileChecksum: String): CpiMetadataLite? {
+    override fun getCpiMetadataByChecksum(cpiFileChecksum: String): CpiMetadata? {
         if (cpiFileChecksum.isBlank()) {
             log.warn("CPI file checksum cannot be empty")
             return null
@@ -46,11 +47,11 @@ internal class VirtualNodeEntityRepository(
             if (foundCpi.isNotEmpty()) foundCpi[0] else null
         } ?: return null
 
-        return cpiMetadataEntity.toLite()
+        return cpiMetadataEntity.toDto()
     }
 
     /** Reads CPI metadata from the database. */
-    override fun getCPIMetadataByNameAndVersion(name: String, version: String): CpiMetadataLite? {
+    override fun getCPIMetadataByNameAndVersion(name: String, version: String): CpiMetadata? {
         val cpiMetadataEntity = entityManagerFactory.createEntityManager().use {
             it.transaction {
                 it.createQuery(
@@ -65,20 +66,20 @@ internal class VirtualNodeEntityRepository(
             }
         }
 
-        return cpiMetadataEntity.toLite()
+        return cpiMetadataEntity.toDto()
     }
 
-    override fun getCPIMetadataById(em: EntityManager, id: CpiIdentifier): CpiMetadataLite? {
-        return em.find(CpiMetadataEntity::class.java, id.toEntity())?.toLite()
+    override fun getCPIMetadataById(em: EntityManager, id: CpiIdentifier): CpiMetadata? {
+        return em.find(CpiMetadataEntity::class.java, id.toEntity())?.toDto()
     }
 
     private fun CpiIdentifier.toEntity(): CpiMetadataEntityKey =
         CpiMetadataEntityKey(name, version, signerSummaryHash.toString())
 
-    private fun CpiMetadataEntity.toLite(): CpiMetadataLite {
+    private fun CpiMetadataEntity.toDto(): CpiMetadata {
         val cpiId = CpiIdentifier(name, version, parseSecureHash(signerSummaryHash))
         val fileChecksum = parseSecureHash(fileChecksum)
         val cpks = cpks.map { CpkMetadata.fromJsonAvro(it.metadata.serializedMetadata) }.toSet()
-        return CpiMetadataLite(cpiId, fileChecksum, groupId, groupPolicy, cpks)
+        return CpiMetadata(cpiId, fileChecksum, cpks, groupPolicy, entityVersion, insertTimestamp!!)
     }
 }
