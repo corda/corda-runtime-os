4cf3697b49f484dbcca1c5ebdbbe494d02af239d
Dan Newton <dan.newton@r3.com>
2023-04-20 16:19:50 +0100

    tests

--- components/ledger/ledger-persistence/src/main/kotlin/net/corda/ledger/persistence/json/impl/DefaultContractStateVaultJsonFactoryImpl.kt
+++ components/ledger/ledger-persistence/src/main/kotlin/net/corda/ledger/persistence/json/impl/DefaultContractStateVaultJsonFactoryImpl.kt
@@ -1,14 +1,8 @@
 package net.corda.ledger.persistence.json.impl
 
 import net.corda.ledger.persistence.json.DefaultContractStateVaultJsonFactory
-import net.corda.sandbox.type.UsedByPersistence
 import net.corda.v5.application.marshalling.JsonMarshallingService
-import net.corda.v5.ledger.utxo.ContractState
 import net.corda.v5.ledger.utxo.StateAndRef
-import net.corda.v5.ledger.utxo.query.json.ContractStateVaultJsonFactory
-import org.osgi.service.component.annotations.Activate
-import org.osgi.service.component.annotations.Component
-import org.osgi.service.component.annotations.ServiceScope
 
 class DefaultContractStateVaultJsonFactoryImpl : DefaultContractStateVaultJsonFactory {
 
