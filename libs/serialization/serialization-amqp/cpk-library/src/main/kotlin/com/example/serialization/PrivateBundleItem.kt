e15bfc66a30ab8d3873d25e14c88856943517854
Kyriakos Tharrouniatis <kyriakos.tharrouniatis@r3.com>
2023-04-20 17:17:17 +0100

    CORE-7184 AMQP custom serialization asserts for private CPK types (#3670)
    * Adds tests to assert the below cases:
      * custom serializer targeting private CPK type fails
      * custom serializer targeting public CPK type but whose proxy type references private CPK type fails
      * custom serializer targeting public CPK type and uses a public CPK proxy type for the private CPK type succeeds

--- libs/serialization/serialization-amqp/cpk-library/src/main/kotlin/com/example/serialization/PrivateBundleItem.kt
+++ libs/serialization/serialization-amqp/cpk-library/src/main/kotlin/com/example/serialization/PrivateBundleItem.kt
@@ -3,4 +3,4 @@ package com.example.serialization
 import net.corda.v5.base.annotations.CordaSerializable
 
 @CordaSerializable
-data class PrivateBundleItem (val i: Int)
+data class PrivateBundleItem(val i: Int)
