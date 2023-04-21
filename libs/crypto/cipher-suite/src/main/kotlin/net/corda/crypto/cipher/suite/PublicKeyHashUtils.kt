058e294377e6ea2c9618c26c3660f2484ae94fda
Kyriakos Tharrouniatis <kyriakos.tharrouniatis@r3.com>
2023-04-21 14:55:17 +0100

    Minor text update

--- libs/crypto/cipher-suite/src/main/kotlin/net/corda/crypto/cipher/suite/PublicKeyHashUtils.kt
+++ libs/crypto/cipher-suite/src/main/kotlin/net/corda/crypto/cipher/suite/PublicKeyHashUtils.kt
@@ -15,7 +15,7 @@ fun PublicKey.sha256Bytes(): ByteArray = messageDigestSha256().digest(encoded)
 
 fun PublicKey.toStringShort(): String = "DL" + EncodingUtils.toBase58(sha256Bytes())
 
-// TODO The below needs to reference with `ShortHash` but can't do now because cipher-suite module can't depend
+// TODO The below needs to reference `ShortHash` but can't do now because cipher-suite module can't depend
 //  on crypto-core due to circular dependency issue
 const val SHORT_KEY_ID_LENGTH = 12
 
