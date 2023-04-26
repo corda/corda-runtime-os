3250f92c8e6d3267c4cb9a804d6916c52868f8e2
nkovacsx <57627796+nkovacsx@users.noreply.github.com>
2023-04-26 14:42:54 +0100

    CORE-12891 Add ordering to custom query and multiply limit/offset (#3713)
    * CORE-12891 Add ordering and multiply offset/limit
    
    * rework smoke tet for named query

--- applications/workers/workers-smoketest/src/smokeTest/kotlin/net/corda/applications/workers/smoketest/ledger/UtxoLedgerTests.kt
+++ applications/workers/workers-smoketest/src/smokeTest/kotlin/net/corda/applications/workers/smoketest/ledger/UtxoLedgerTests.kt
@@ -115,7 +115,8 @@ class UtxoLedgerTests {
             aliceHoldingId,
             mapOf(
                 "offset" to 0,
-                "limit" to 100
+                "limit" to 100,
+                "testField" to input
             ),
             "com.r3.corda.demo.utxo.UtxoCustomQueryDemoFlow"
         )
