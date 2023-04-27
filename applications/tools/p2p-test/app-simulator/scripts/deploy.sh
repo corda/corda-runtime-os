8487668d260db0d1a9a9c9e8d3bd45505b23dc71
Connel McGovern <100574906+mcgovc@users.noreply.github.com>
2023-04-27 13:06:27 +0100

    [Build] Update Helm chart location in corda-runtime-os deploy.sh script (#3732)

--- applications/tools/p2p-test/app-simulator/scripts/deploy.sh
+++ applications/tools/p2p-test/app-simulator/scripts/deploy.sh
@@ -23,7 +23,7 @@ deploy() {
      --wait
 
    echo Installing corda image $DOCKER_IMAGE_VERSION into $namespace
-   helm upgrade --install corda -n $namespace oci://corda-os-docker-unstable.software.r3.com/helm-charts/corda \
+   helm upgrade --install corda -n $namespace oci://corda-os-docker.software.r3.com/helm-charts/release/os/5.0/corda \
      --set "imagePullSecrets={docker-registry-cred}" --set image.tag=$DOCKER_IMAGE_VERSION \
      --set image.registry="corda-os-docker.software.r3.com" --values $REPO_TOP_LEVEL_DIR/values.yaml \
      --values $REPO_TOP_LEVEL_DIR/debug.yaml --wait --version $CORDA_CHART_VERSION
