7dffaa307d5f24fa94d7b5517b42356db0f3d0af
Emily Bowe <emily.bowe@r3.com>
2023-04-28 14:17:59 +0100

    PR comments

--- components/flow/flow-service/src/main/kotlin/net/corda/flow/pipeline/handlers/requests/helper/FlowRecords.kt
+++ components/flow/flow-service/src/main/kotlin/net/corda/flow/pipeline/handlers/requests/helper/FlowRecords.kt
@@ -7,33 +7,32 @@ import net.corda.flow.pipeline.events.FlowEventContext
 import net.corda.flow.pipeline.factory.FlowRecordFactory
 import net.corda.messaging.api.records.Record
 import net.corda.schema.configuration.FlowConfig
-import org.apache.avro.specific.SpecificRecordBase
 import java.time.Instant
 
-    /**
-     * When a flow is started by the REST api, a FlowKey is sent to the Flow mapper, so we send a cleanup event for this key.
-     *
-     * Flows triggered by SessionInit don't have this FlowKey, so we do not send a cleanup event
-     */
-    fun getRecords(
-        flowRecordFactory: FlowRecordFactory,
-        context: FlowEventContext<Any>,
-        status: FlowStatus
-    ): List<Record<out Any, out SpecificRecordBase>> {
-        val checkpoint = context.checkpoint
-        val records = if (checkpoint.flowStartContext.initiatorType == FlowInitiatorType.RPC) {
-            val flowCleanupTime = context.config.getLong(FlowConfig.PROCESSING_FLOW_CLEANUP_TIME)
-            val expiryTime = Instant.now().plusMillis(flowCleanupTime).toEpochMilli()
-            listOf(
-                flowRecordFactory.createFlowStatusRecord(status), flowRecordFactory.createFlowMapperEventRecord(
-                    checkpoint.flowKey.toString(), ScheduleCleanup(expiryTime)
-                )
+/**
+ * When a flow is started by the REST api, a FlowKey is sent to the Flow mapper, so we send a cleanup event for this key.
+ *
+ * Flows triggered by SessionInit don't have this FlowKey, so we do not send a cleanup event
+ */
+fun getRecords(
+    flowRecordFactory: FlowRecordFactory,
+    context: FlowEventContext<Any>,
+    status: FlowStatus
+): List<Record<*,*>> {
+    val checkpoint = context.checkpoint
+    val records = if (checkpoint.flowStartContext.initiatorType == FlowInitiatorType.RPC) {
+        val flowCleanupTime = context.config.getLong(FlowConfig.PROCESSING_FLOW_CLEANUP_TIME)
+        val expiryTime = Instant.now().plusMillis(flowCleanupTime).toEpochMilli()
+        listOf(
+            flowRecordFactory.createFlowStatusRecord(status), flowRecordFactory.createFlowMapperEventRecord(
+                checkpoint.flowKey.toString(), ScheduleCleanup(expiryTime)
             )
-        } else {
-            listOf(
-                flowRecordFactory.createFlowStatusRecord(status)
-            )
-        }
-        return records
+        )
+    } else {
+        listOf(
+            flowRecordFactory.createFlowStatusRecord(status)
+        )
     }
+    return records
+}
 
