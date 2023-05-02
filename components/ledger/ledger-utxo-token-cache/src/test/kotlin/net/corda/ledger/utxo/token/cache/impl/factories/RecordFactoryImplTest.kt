1c7caf33271d40ef93dde176269dcd07ae7c0367
Filipe Oliveira <filipe.oliveira@r3.com>
2023-05-02 12:14:08 +0100

    Added a new test case to `RecordFactoryImplTest` that covers the creation of the token balance response.

--- components/ledger/ledger-utxo-token-cache/src/test/kotlin/net/corda/ledger/utxo/token/cache/impl/factories/RecordFactoryImplTest.kt
+++ components/ledger/ledger-utxo-token-cache/src/test/kotlin/net/corda/ledger/utxo/token/cache/impl/factories/RecordFactoryImplTest.kt
@@ -1,14 +1,19 @@
 package net.corda.ledger.utxo.token.cache.impl.factories
 
+import java.math.BigDecimal
+import java.nio.ByteBuffer
 import net.corda.data.flow.event.FlowEvent
 import net.corda.data.ledger.utxo.token.selection.data.Token
+import net.corda.data.ledger.utxo.token.selection.data.TokenAmount
+import net.corda.data.ledger.utxo.token.selection.data.TokenBalanceQueryResult
 import net.corda.data.ledger.utxo.token.selection.data.TokenClaimQueryResult
 import net.corda.data.ledger.utxo.token.selection.data.TokenClaimResultStatus
 import net.corda.flow.external.events.responses.factory.ExternalEventResponseFactory
-import net.corda.messaging.api.records.Record
+import net.corda.ledger.utxo.impl.token.selection.impl.TokenBalanceImpl
 import net.corda.ledger.utxo.token.cache.entities.CachedToken
 import net.corda.ledger.utxo.token.cache.factories.RecordFactoryImpl
 import net.corda.ledger.utxo.token.cache.impl.POOL_CACHE_KEY
+import net.corda.messaging.api.records.Record
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.mockito.kotlin.any
@@ -68,4 +73,34 @@ class RecordFactoryImplTest {
         assertThat(result).isSameAs(response)
         verify(externalEventResponseFactory).success(externalEventRequestId, flowId, expectedResponse)
     }
+
+    @Test
+    fun `create a token balance response`() {
+
+        val expectedResponse = TokenBalanceQueryResult().apply {
+            this.poolKey = POOL_CACHE_KEY
+            this.balance = BigDecimal(1.0).toTokenAmount()
+            this.balanceIncludingClaimedTokens = BigDecimal(2.0).toTokenAmount()
+
+        }
+        val response = Record<String, FlowEvent>("", "", null)
+
+        whenever(externalEventResponseFactory.success(any(), any(), any())).thenReturn(response)
+
+        val target = RecordFactoryImpl(externalEventResponseFactory)
+        val result = target.getBalanceResponse(
+            flowId,
+            externalEventRequestId,
+            POOL_CACHE_KEY,
+            TokenBalanceImpl(BigDecimal(1.0), BigDecimal(2.0))
+        )
+
+        assertThat(result).isSameAs(response)
+        verify(externalEventResponseFactory).success(externalEventRequestId, flowId, expectedResponse)
+    }
+
+    private fun BigDecimal.toTokenAmount() = TokenAmount.newBuilder()
+        .setScale(scale())
+        .setUnscaledValue(ByteBuffer.wrap(unscaledValue().toByteArray()))
+        .build()
 }
