a179593f3b029c5416881418c7eb72613349a80a
Szymon Sztuka <szymon.sztuka@r3.com>
2023-05-03 18:07:46 +0100

    Weft as Corda Service

---
+++ testing/cpbs/test-responder-cordapp/src/main/kotlin/com/r3/corda/testing/interop/SampleTokensFacade.kt
@@ -0,0 +1,14 @@
+package com.r3.corda.testing.interop
+
+import net.corda.v5.application.interop.binding.*
+
+
+@BindsFacade("/com/r3/tokens/sample")
+@FacadeVersions("v1.0")
+interface SampleTokensFacade {
+    @BindsFacadeMethod("say-hello")
+    fun processHello(greeting: String): @QualifiedWith("greeting") InteropAction<String>
+
+    @BindsFacadeMethod("get-balance")
+    fun getBalance(greeting: String): @QualifiedWith("greeting") InteropAction<String>
+}
