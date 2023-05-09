9b31dd8e1935d199917dc2d9d037efc38e301024
Alexander Cramb <39802235+ac101m@users.noreply.github.com>
2023-05-09 18:02:56 +0100

    CORE-12208 followup (#3832)
    # Changes
    - Adjust TODO comments in interop processor.
    - Fix error caused by recursive addition of Alias suffix during synthetic alias creation.
    - Improve interop integration tests.

--- components/interop/interop-service/src/integrationTest/kotlin/net/corda/interop/service/integration/InteropServiceIntegrationTest.kt
+++ components/interop/interop-service/src/integrationTest/kotlin/net/corda/interop/service/integration/InteropServiceIntegrationTest.kt
@@ -100,8 +100,9 @@ class InteropServiceIntegrationTest {
     }
 
     val groupId = "3dfc0aae-be7c-44c2-aa4f-4d0d7145cf08"
-    val sourceIdentity = HoldingIdentity("CN=Alice, O=Alice Corp, L=LDN, C=GB", groupId)
-    val destinationIdentity = HoldingIdentity("CN=Alice Alias, O=Alice Corp, L=LDN, C=GB", groupId)
+    val sourceIdentity = HoldingIdentity("CN=Bob, O=Bob Corp, L=LDN, C=GB", groupId)
+    val destinationIdentity = HoldingIdentity("CN=Alice, O=Alice Corp, L=LDN, C=GB", groupId)
+    val destinationAliasIdentity = HoldingIdentity("CN=Alice Alias, O=Alice Corp, L=LDN, C=GB", groupId)
 
     private fun messagesToPublish(session: String) : List<Record<*,*>> {
         val interopMessage: ByteBuffer = ByteBuffer.wrap(
@@ -132,11 +133,11 @@ class InteropServiceIntegrationTest {
         ))
 
         val inboundSessionEvent = SessionEvent(
-            MessageDirection.INBOUND, Instant.now(), session, 1, destinationIdentity, sourceIdentity, 0, listOf(),
+            MessageDirection.INBOUND, Instant.now(), session, 1, destinationAliasIdentity, sourceIdentity, 0, listOf(),
             SessionInit(sourceIdentity.toString(), null, contextUserProperties, emptyKeyValuePairList(), emptyKeyValuePairList(), interopMessage))
 
         val outboundSessionEvent = SessionEvent(
-            MessageDirection.OUTBOUND, Instant.now(), session, 2, destinationIdentity, sourceIdentity, 0, listOf(),
+            MessageDirection.OUTBOUND, Instant.now(), session, 2, destinationAliasIdentity, sourceIdentity, 0, listOf(),
             SessionInit(sourceIdentity.toString(), null, contextUserProperties, emptyKeyValuePairList(), emptyKeyValuePairList(), interopMessage))
 
         val inboundMsg = Record(Schemas.Flow.FLOW_INTEROP_EVENT_TOPIC, session, FlowMapperEvent(inboundSessionEvent))
@@ -150,7 +151,9 @@ class InteropServiceIntegrationTest {
         val publisher = publisherFactory.createPublisher(PublisherConfig("client1"), bootConfig)
         // Test config updates don't break Interop Service
         republishConfig(publisher)
+        // Need to publish at least 2 identities in order to trigger InteropAliasProcessor to create synthetic aliases
         publisher.publish(listOf(createHostedAliasIdentity(sourceIdentity.toCorda())))
+        publisher.publish(listOf(createHostedAliasIdentity(destinationIdentity.toCorda())))
         val session = "session1"
         //TODO revisit sleep in CORE-12134
         Thread.sleep(30000)
