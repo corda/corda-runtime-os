cc36da34bee7ddb47021883fef53cf1ce01ef547
Filipe Oliveira <filipe.oliveira@r3.com>
2023-05-10 12:29:21 +0100

    Update.

--- applications/workers/workers-smoketest/src/smokeTest/kotlin/net/corda/applications/workers/smoketest/ledger/TokenBalanceQueryTests.kt
+++ applications/workers/workers-smoketest/src/smokeTest/kotlin/net/corda/applications/workers/smoketest/ledger/TokenBalanceQueryTests.kt
@@ -4,30 +4,27 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.KotlinModule
 import java.math.BigDecimal
 import java.util.UUID
-import net.corda.e2etest.utilities.CLUSTER_URI
-import net.corda.e2etest.utilities.CODE_SIGNER_CERT
-import net.corda.e2etest.utilities.PASSWORD
 import net.corda.e2etest.utilities.RPC_FLOW_STATUS_SUCCESS
 import net.corda.e2etest.utilities.TEST_NOTARY_CPB_LOCATION
 import net.corda.e2etest.utilities.TEST_NOTARY_CPI_NAME
-import net.corda.e2etest.utilities.USERNAME
-import net.corda.e2etest.utilities.assertWithRetry
 import net.corda.e2etest.utilities.awaitRpcFlowFinished
-import net.corda.e2etest.utilities.cluster
 import net.corda.e2etest.utilities.conditionallyUploadCordaPackage
 import net.corda.e2etest.utilities.getHoldingIdShortHash
 import net.corda.e2etest.utilities.getOrCreateVirtualNodeFor
 import net.corda.e2etest.utilities.registerStaticMember
 import net.corda.e2etest.utilities.startRpcFlow
-import net.corda.utilities.seconds
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.MethodOrderer
 import org.junit.jupiter.api.Order
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS
+import org.junit.jupiter.api.TestMethodOrder
 
 @Suppress("Unused", "FunctionName")
 @TestInstance(PER_CLASS)
+@TestMethodOrder(MethodOrderer.OrderAnnotation::class)
 class TokenBalanceQueryTests {
 
     private companion object {
@@ -91,31 +88,8 @@ class TokenBalanceQueryTests {
         return convertToTokenBalanceQueryResponseMsg(flowResult.flowResult!!)
     }
 
-    @Test
-    @Order(1)
-    fun `import codesigner certificate`() {
-        val retryTimeout = 120.seconds
-        val retryInterval = 1.seconds
-
-        cluster {
-            endpoint(
-                CLUSTER_URI,
-                USERNAME,
-                PASSWORD
-            )
-            assertWithRetry {
-                // Certificate upload can be slow in the combined worker, especially after it has just started up.
-                timeout(retryTimeout)
-                interval(retryInterval)
-                command { importCertificate(CODE_SIGNER_CERT, "code-signer", "cordadev") }
-                condition { it.code == 204 }
-            }
-        }
-    }
-
-    @Test
-    @Order(2)
-    fun `setup`() {
+    @BeforeAll
+    fun beforeAll() {
         conditionallyUploadCordaPackage(
             cpiName,
             TEST_CPB_LOCATION,
@@ -144,7 +118,7 @@ class TokenBalanceQueryTests {
     }
 
     @Test
-    @Order(3)
+    @Order(1)
     fun `Token Query Balance - Ensure balance is zero`() {
 
         val tokenBalanceQuery = runTokenBalanceQueryFlow()
@@ -154,7 +128,7 @@ class TokenBalanceQueryTests {
     }
 
     @Test
-    @Order(10)
+    @Order(2)
     fun `Token Query Balance - Create 10 coins`() {
         val rpcStartArgs = mapOf(
             "issuerBankX500" to bobX500,
@@ -175,7 +149,7 @@ class TokenBalanceQueryTests {
     }
 
     @Test
-    @Order(11)
+    @Order(3)
     fun `Token Query Balance - Ensure balance is equal to 10`() {
         val tokenBalanceQuery = runTokenBalanceQueryFlow()
 
@@ -184,7 +158,7 @@ class TokenBalanceQueryTests {
     }
 
     @Test
-    @Order(12)
+    @Order(4)
     fun `Token Query Balance - Claim token`() {
         val rpcStartArgs = mapOf(
             "tokenType" to tokenType,
@@ -207,7 +181,7 @@ class TokenBalanceQueryTests {
     }
 
     @Test
-    @Order(13)
+    @Order(5)
     fun `Token Query Balance - Ensure balance is equal to 5 but the balance including claimed tokens is 10`() {
         val tokenBalanceQuery = runTokenBalanceQueryFlow()
 
