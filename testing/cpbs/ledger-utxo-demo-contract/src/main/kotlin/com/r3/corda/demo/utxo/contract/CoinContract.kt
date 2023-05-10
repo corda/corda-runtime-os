4622578ceb00fbe1417a8b7df738a07be1205921
Filipe Oliveira <filipe.oliveira@r3.com>
2023-05-10 14:05:56 +0100

    Update.

--- testing/cpbs/ledger-utxo-demo-contract/src/main/kotlin/com/r3/corda/demo/utxo/contract/CoinContract.kt
+++ testing/cpbs/ledger-utxo-demo-contract/src/main/kotlin/com/r3/corda/demo/utxo/contract/CoinContract.kt
@@ -5,9 +5,7 @@ import net.corda.v5.ledger.utxo.transaction.UtxoLedgerTransaction
 
 class CoinContract : Contract {
     override fun verify(transaction: UtxoLedgerTransaction) {
-
+        // This coin contract is for test only purposes and so far
+        // there has been no need to add any logic to this function
     }
-
 }
-
-
