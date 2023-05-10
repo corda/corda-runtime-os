8618d4465b9790ce89c99dcdb88a7128ee3ffa17
Filipe Oliveira <filipe.oliveira@r3.com>
2023-05-10 09:25:02 +0100

    Reverted the changes made to `UtxoBackchainResolutionDemoFlow`.

--- testing/cpbs/ledger-utxo-demo-app/src/main/kotlin/com/r3/corda/demo/utxo/UtxoBackchainResolutionDemoFlow.kt
+++ testing/cpbs/ledger-utxo-demo-app/src/main/kotlin/com/r3/corda/demo/utxo/UtxoBackchainResolutionDemoFlow.kt
@@ -14,8 +14,8 @@ import net.corda.v5.base.annotations.Suspendable
 import net.corda.v5.base.types.MemberX500Name
 import net.corda.v5.crypto.SecureHash
 import net.corda.v5.ledger.common.NotaryLookup
-import net.corda.v5.ledger.utxo.UtxoLedgerService
 import net.corda.v5.ledger.utxo.StateRef
+import net.corda.v5.ledger.utxo.UtxoLedgerService
 import com.r3.corda.demo.utxo.contract.TestCommand
 import com.r3.corda.demo.utxo.contract.TestUtxoState
 import org.slf4j.Logger
