c30b70c6c556be6fd0e1ea4b39334f9789aec6ed
Filipe Oliveira <filipe.oliveira@r3.com>
2023-05-10 14:29:39 +0100

    Remove messages that were not being used.

--- testing/cpbs/ledger-utxo-demo-app/src/main/kotlin/com/r3/corda/demo/utxo/token/CoinSupport.kt
+++ testing/cpbs/ledger-utxo-demo-app/src/main/kotlin/com/r3/corda/demo/utxo/token/CoinSupport.kt
@@ -1,8 +1,5 @@
 package com.r3.corda.demo.utxo.token
 
-import net.corda.v5.ledger.utxo.Command
-import net.corda.v5.ledger.utxo.token.selection.ClaimedToken
-
 data class CreateCoinMessage(
     val issuerBankX500: String,
     val currency: String,
@@ -11,20 +8,3 @@ data class CreateCoinMessage(
     val tag: String?,
     val ownerHash: String?
 )
-
-data class SpendCoinMessage(
-    val issuerBankX500: String,
-    val currency: String,
-    val maxCoinsToUse: Int,
-    val targetAmount: Int,
-    val tag: String?,
-    val ownerHash: String?,
-)
-
-data class SpendCoinResponseMessage(
-    val foundCoins: List<ClaimedToken>,
-    val spentCoins: List<String>,
-    val releasedCoins: List<String>,
-)
-
-class NullCoinCommand : Command
