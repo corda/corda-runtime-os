2f902faf34df2feaa4aed514ed52a5f048046ef4
Ronan Browne <ronan.browne@R3.com>
2023-05-11 09:42:07 +0100

    ES-379 Investigate use of unstable tags to skip certain tests on the main CI using Junit tags for e2e tests (#3837)

--- applications/workers/release/rest-worker/src/e2eTest/kotlin/net/corda/applications/workers/rest/SessionCertificateTest.kt
+++ applications/workers/release/rest-worker/src/e2eTest/kotlin/net/corda/applications/workers/rest/SessionCertificateTest.kt
@@ -17,11 +17,12 @@ import net.corda.applications.workers.rest.utils.setSslConfiguration
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.io.TempDir
 import java.nio.file.Path
 
-@Disabled("CORE-13288: Tests are disabled until there is a solution in place")
+
 class SessionCertificateTest {
     @TempDir
     lateinit var tempDir: Path
@@ -56,6 +57,7 @@ class SessionCertificateTest {
     }
 
     @Test
+    @Tag("Unstable")
     fun `Create mgm and allow members to join the group`() {
         onboardMultiClusterGroup()
     }
