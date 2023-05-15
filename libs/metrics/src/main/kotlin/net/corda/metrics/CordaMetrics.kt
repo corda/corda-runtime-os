27b6045f595a0b756e03d2636d8d0c6c671cfad2
Miljenko Brkic <miljenko.brkic@r3.com>
2023-05-15 16:16:55 +0100

    CORE-13705 Add metric indicating UTXO ledger transaction verification time

--- libs/metrics/src/main/kotlin/net/corda/metrics/CordaMetrics.kt
+++ libs/metrics/src/main/kotlin/net/corda/metrics/CordaMetrics.kt
@@ -186,6 +186,11 @@ object CordaMetrics {
          * The time taken by the backing store to perform a single read operation from the database.
          */
         object UniquenessBackingStoreDbReadTime: Metric<Timer>("uniqueness.backingstore.db.read.time", CordaMetrics::timer)
+
+        /**
+         * The time taken by verification processor to verify an UTXO ledger transaction.
+         */
+        object UtxoLedgerTransactionVerificationTime: Metric<Timer>("ledger.utxo.transaction.verification.time", CordaMetrics::timer)
     }
 
     /**
