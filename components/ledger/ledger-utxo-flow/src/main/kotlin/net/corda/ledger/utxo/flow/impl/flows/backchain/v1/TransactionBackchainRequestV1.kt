4da8470bded6958d18ec00b56db310e8f5d4e9a6
Chris Rankin <chris.rankin@r3.com>
2023-05-16 12:06:44 +0100

    CORE-13725: Ensure Payload and TransactionBackchainRequest are @CordaSerializable.

--- components/ledger/ledger-utxo-flow/src/main/kotlin/net/corda/ledger/utxo/flow/impl/flows/backchain/v1/TransactionBackchainRequestV1.kt
+++ components/ledger/ledger-utxo-flow/src/main/kotlin/net/corda/ledger/utxo/flow/impl/flows/backchain/v1/TransactionBackchainRequestV1.kt
@@ -4,7 +4,7 @@ import net.corda.v5.base.annotations.CordaSerializable
 import net.corda.v5.crypto.SecureHash
 
 @CordaSerializable
-sealed interface TransactionBackchainRequestV1 {
-    data class Get(val transactionIds: Set<SecureHash>): TransactionBackchainRequestV1
-    object Stop: TransactionBackchainRequestV1
+sealed class TransactionBackchainRequestV1 {
+    data class Get(val transactionIds: Set<SecureHash>): TransactionBackchainRequestV1()
+    object Stop: TransactionBackchainRequestV1()
 }
