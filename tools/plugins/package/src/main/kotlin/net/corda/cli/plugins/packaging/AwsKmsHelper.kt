c5e96d4cb426bac851a9767a6e11b68662e03d18
Anna Paskova <anna.paskova@r3.com>
2023-05-16 12:33:57 +0100

    Remove unneeded listing of AWS KMS keys

--- tools/plugins/package/src/main/kotlin/net/corda/cli/plugins/packaging/AwsKmsHelper.kt
+++ tools/plugins/package/src/main/kotlin/net/corda/cli/plugins/packaging/AwsKmsHelper.kt
@@ -6,12 +6,7 @@ import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider
 import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.services.kms.KmsClient
 import software.amazon.awssdk.services.kms.model.DescribeKeyRequest
-import software.amazon.awssdk.services.kms.model.KeyListEntry
-import software.amazon.awssdk.services.kms.model.KmsException
-import software.amazon.awssdk.services.kms.model.ListKeysRequest
-import software.amazon.awssdk.services.kms.model.ListKeysResponse
 import java.security.PrivateKey
-import kotlin.system.exitProcess
 
 
 fun getKmsClient(): KmsClient {
@@ -28,23 +23,6 @@ fun getRSAPrivateKey(keyId: String): PrivateKey {
 
 fun getECPrivateKey(keyId: String): PrivateKey {
     return KmsECKeyFactory.getPrivateKey(keyId)
-}
-
-fun listAllKeys(kmsClient: KmsClient) {
-    try {
-        val listKeysRequest: ListKeysRequest = ListKeysRequest.builder()
-            .limit(15)
-            .build()
-        val keysResponse: ListKeysResponse = kmsClient.listKeys(listKeysRequest)
-        val keyListEntries: List<KeyListEntry> = keysResponse.keys()
-        for (key in keyListEntries) {
-            println("The key ARN is: " + key.keyArn())
-            println("The key Id is: " + key.keyId())
-        }
-    } catch (e: KmsException) {
-        System.err.println(e.message)
-        exitProcess(1)
-    }
 }
 
 fun closeKmsClient(kmsClient: KmsClient) {
