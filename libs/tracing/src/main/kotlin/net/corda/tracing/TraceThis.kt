9f4974b4c05caed379af777fb9492e62646a3b9d
Joseph Zuniga-Daly <joseph.zunigadaly@r3.com>
2023-05-18 16:03:13 +0100

    CORE-13796: Remove unused baggage code, add linebreaks for readability

--- libs/tracing/src/main/kotlin/net/corda/tracing/TraceThis.kt
+++ libs/tracing/src/main/kotlin/net/corda/tracing/TraceThis.kt
@@ -1,12 +1,7 @@
 package net.corda.tracing
 
 import brave.Tracing
-import brave.baggage.BaggageField
-import brave.baggage.BaggagePropagation
-import brave.baggage.BaggagePropagationConfig
 import brave.context.slf4j.MDCScopeDecorator
-import brave.handler.SpanHandler
-import brave.propagation.B3Propagation
 import brave.propagation.ThreadLocalCurrentTraceContext
 import brave.sampler.Sampler
 import brave.servlet.TracingFilter
@@ -18,10 +13,11 @@ import zipkin2.reporter.urlconnection.URLConnectionSender
 import java.util.EnumSet
 import javax.servlet.DispatcherType
 
-private val spanHandler: SpanHandler = ZipkinSpanHandler
-    .create(AsyncReporter.create(URLConnectionSender.create("http://localhost:9411/api/v2/spans")))
+private val spanAsyncReporter = AsyncReporter.create(URLConnectionSender.create("http://localhost:9411/api/v2/spans"))
+private val spanHandler = ZipkinSpanHandler
+    .create(spanAsyncReporter)
 
-private val braveCurrentTraceContext: ThreadLocalCurrentTraceContext = ThreadLocalCurrentTraceContext.newBuilder()
+private val braveCurrentTraceContext = ThreadLocalCurrentTraceContext.newBuilder()
     .addScopeDecorator(MDCScopeDecorator.get())
     .build()
 
@@ -31,16 +27,14 @@ fun setTracingServiceName(name: String) {
 }
 
 private val tracing: Tracing by lazy {
-    Tracing.newBuilder().currentTraceContext(braveCurrentTraceContext).supportsJoin(false)
+    Tracing.newBuilder()
+        .currentTraceContext(braveCurrentTraceContext)
+        .supportsJoin(false)
         .localServiceName(serviceName)
         .traceId128Bit(true)
-        .propagationFactory(
-            BaggagePropagation.newFactoryBuilder(B3Propagation.FACTORY)
-                .add(BaggagePropagationConfig.SingleBaggageField.remote(BaggageField.create("baggage_1")))
-                .add(BaggagePropagationConfig.SingleBaggageField.remote(BaggageField.create("baggage_2")))
-                .build()
-        )
-        .sampler(Sampler.ALWAYS_SAMPLE).addSpanHandler(spanHandler).build()
+        .sampler(Sampler.ALWAYS_SAMPLE)
+        .addSpanHandler(spanHandler)
+        .build()
 }
 
 fun configureJavalinForTracing(config: JavalinConfig) {
