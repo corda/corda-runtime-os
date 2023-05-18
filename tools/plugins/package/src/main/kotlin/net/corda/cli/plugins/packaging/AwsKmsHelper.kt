7b0060321863f23e457bc14bc150c5c36d0ae622
Anna Paskova <anna.paskova@r3.com>
2023-05-18 09:07:24 +0100

    Retrieve AWS region and login credentials based on ENV variables and AWS config file

--- tools/plugins/package/src/main/kotlin/net/corda/cli/plugins/packaging/AwsKmsHelper.kt
+++ tools/plugins/package/src/main/kotlin/net/corda/cli/plugins/packaging/AwsKmsHelper.kt
@@ -1,18 +1,9 @@
 package net.corda.cli.plugins.packaging
 
-import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider
-import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.services.kms.KmsClient
 import software.amazon.awssdk.services.kms.model.DescribeKeyRequest
 
-
-fun getKmsClient(): KmsClient {
-    val region: Region = Region.EU_WEST_2
-    return KmsClient.builder()
-        .region(region)
-        .credentialsProvider(ProfileCredentialsProvider.create())
-        .build()
-}
+fun getKmsClient(): KmsClient = KmsClient.builder().build()
 
 fun isRsaKeyType(kmsClient: KmsClient, keyId: String): Boolean {
     val describeKeyResponse = kmsClient.describeKey { builder: DescribeKeyRequest.Builder -> builder.keyId(keyId) }
