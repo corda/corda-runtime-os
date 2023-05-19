b04edaac0e0b5a0dd595c6537c42a5d7ba04494d
Joseph Zuniga-Daly <joseph.zunigadaly@r3.com>
2023-05-19 15:35:31 +0100

    CORE-13796: Move zipkin URL to environment variable

--- libs/tracing/src/main/kotlin/net/corda/tracing/impl/TracingState.kt
+++ libs/tracing/src/main/kotlin/net/corda/tracing/impl/TracingState.kt
@@ -7,6 +7,8 @@ import brave.sampler.Sampler
 import zipkin2.reporter.AsyncReporter
 import zipkin2.reporter.brave.ZipkinSpanHandler
 import zipkin2.reporter.urlconnection.URLConnectionSender
+import java.net.URL
+import java.util.Stack
 
 /**
  * Tracing objects that will exist for the lifetime of the application.
@@ -14,29 +16,41 @@ import zipkin2.reporter.urlconnection.URLConnectionSender
  * Close before shutdown to wait for trace spans to be sent to external systems.
  */
 object TracingState: AutoCloseable {
-    private val spanAsyncReporter = AsyncReporter.create(URLConnectionSender.create("http://localhost:9411/api/v2/spans"))
-    private val spanHandler = ZipkinSpanHandler
-        .create(spanAsyncReporter)
 
-    private val braveCurrentTraceContext = ThreadLocalCurrentTraceContext.newBuilder()
-        .addScopeDecorator(MDCScopeDecorator.get())
-        .build()
+    private val resourcesToClose = Stack<AutoCloseable>()
 
     var serviceName = "unknown"
 
     val tracing: Tracing by lazy {
-        Tracing.newBuilder()
+
+        val braveCurrentTraceContext = ThreadLocalCurrentTraceContext.newBuilder()
+            .addScopeDecorator(MDCScopeDecorator.get())
+            .build()
+
+        val tracingBuilder = Tracing.newBuilder()
             .currentTraceContext(braveCurrentTraceContext)
             .supportsJoin(false)
             .localServiceName(serviceName)
             .traceId128Bit(true)
             .sampler(Sampler.ALWAYS_SAMPLE)
-            .addSpanHandler(spanHandler)
-            .build()
+
+        val zipkinHost = System.getenv("CORDA_TRACING_SERVER_ZIPKIN_PROTOCOL")
+        if (zipkinHost != null) {
+            val zipkinUrl = "$zipkinHost/api/v2/spans"
+            val spanAsyncReporter =
+                AsyncReporter.create(URLConnectionSender.create(zipkinUrl))
+                    .also(resourcesToClose::push)
+            val spanHandler = ZipkinSpanHandler.create(spanAsyncReporter)
+
+            tracingBuilder.addSpanHandler(spanHandler)
+        }
+
+        tracingBuilder.build().also(resourcesToClose::push)
     }
 
     override fun close() {
-        tracing.close()
-        spanAsyncReporter.close()
+        while (resourcesToClose.any()) {
+            resourcesToClose.pop().close()
+        }
     }
 }
