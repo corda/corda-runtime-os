9b3e607d4ae67c132d9a4d996c15236134cfd5ad
Charlie Crean <charlie.crean@r3.com>
2023-05-19 11:36:34 +0100

    revert accidentally committed script changes

--- applications/tools/p2p-test/app-simulator/scripts/deploy.sh
+++ applications/tools/p2p-test/app-simulator/scripts/deploy.sh
@@ -21,7 +21,7 @@ deploy() {
      --wait
 
    echo Installing corda image $DOCKER_IMAGE_VERSION into $namespace
-   helm upgrade --install corda -n $namespace oci://corda-os-docker-dev.software.r3.com/helm-charts/pr-3906/corda \
+   helm upgrade --install corda -n $namespace oci://corda-os-docker.software.r3.com/helm-charts/release/os/5.0/corda \
      --set "imagePullSecrets={docker-registry-cred}" --set image.tag=$DOCKER_IMAGE_VERSION \
      --set image.registry="corda-os-docker.software.r3.com" --values $REPO_TOP_LEVEL_DIR/values.yaml \
      --values $REPO_TOP_LEVEL_DIR/debug.yaml --wait --version $CORDA_CHART_VERSION
