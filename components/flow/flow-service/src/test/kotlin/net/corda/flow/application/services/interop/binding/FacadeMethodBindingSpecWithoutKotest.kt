4e978d59401710c8f26ef758ec28e82109f0d3fd
JuliaFilipczak <julia.filipczak@r3.com>
2023-05-22 15:50:01 +0100

    more refactoring

--- components/flow/flow-service/src/test/kotlin/net/corda/flow/application/services/interop/binding/FacadeMethodBindingSpecWithoutKotest.kt
+++ components/flow/flow-service/src/test/kotlin/net/corda/flow/application/services/interop/binding/FacadeMethodBindingSpecWithoutKotest.kt
@@ -1,11 +1,17 @@
 package net.corda.flow.application.services.interop.binding
 
+import net.corda.flow.application.services.impl.interop.binding.FacadeOutParameterBindings
 import net.corda.flow.application.services.impl.interop.binding.creation.bindTo
 import net.corda.flow.application.services.impl.interop.facade.FacadeReaders
 import net.corda.flow.application.services.interop.example.TokensFacade
 import net.corda.v5.application.interop.binding.BindsFacade
 import net.corda.v5.application.interop.binding.BindsFacadeMethod
 import net.corda.v5.application.interop.binding.InteropAction
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertNotNull
+import org.junit.jupiter.api.Assertions.assertNull
+import org.junit.jupiter.api.Test
+import java.math.BigDecimal
 import java.util.*
 import kotlin.reflect.KClass
 
@@ -51,4 +57,39 @@ class FacadeMethodBindingSpecWithoutKotest {
     infix fun <T : Any> KClass<T>.shouldFailToBindWith(expectedMessage: String) =
         facadeV2.assertBindingFails2(java, expectedMessage)
 
+    @Test
+    fun `should bind methods to all declared facade versions`() {
+        assertNotNull(bindingV1.bindingFor(TokensFacade::getBalance))
+        assertEquals(facadeV1.method("get-balance"), bindingV1.bindingFor(TokensFacade::getBalance)!!.facadeMethod)
+
+        assertNotNull(bindingV2.bindingFor(TokensFacade::getBalance))
+        assertEquals(facadeV2.method("get-balance"), bindingV2.bindingFor(TokensFacade::getBalance)!!.facadeMethod)
+
+        assertNotNull(bindingV1.bindingFor(TokensFacade::reserveTokensV1))
+        assertEquals(facadeV1.method("reserve-tokens"), bindingV1.bindingFor(TokensFacade::reserveTokensV1)!!.facadeMethod)
+
+        assertNotNull(bindingV2.bindingFor(TokensFacade::reserveTokensV2))
+        assertEquals(facadeV2.method("reserve-tokens"), bindingV2.bindingFor(TokensFacade::reserveTokensV2)!!.facadeMethod)
+    }
+
+    @Test
+    fun `should not bind methods to any undeclared facade versions`() {
+        assertNull(bindingV1.bindingFor(TokensFacade::reserveTokensV2))
+        assertNull(bindingV2.bindingFor(TokensFacade::reserveTokensV1))
+    }
+
+    @Test
+    fun `should bind methods with no out parameters`() {
+        assertNotNull(bindingV2.bindingFor(TokensFacade::releaseReservedTokens))
+        assertEquals(FacadeOutParameterBindings.NoOutParameters, bindingV2.bindingFor(TokensFacade::releaseReservedTokens)!!.outParameterBindings)
+    }
+
+    @Test
+    fun `should bind methods with a single out parameter`() {
+        val getBalance = facadeV2.method("get-balance")
+        assertNotNull(bindingV2.bindingFor(TokensFacade::getBalance))
+        assertNotNull(bindingV2.bindingFor(TokensFacade::getBalance)!!.outParameterBindings as? FacadeOutParameterBindings.SingletonOutParameterBinding)
+        assertEquals(getBalance.outParameter("balance", BigDecimal::class.java), (bindingV2.bindingFor(TokensFacade::getBalance)!!.outParameterBindings as? FacadeOutParameterBindings.SingletonOutParameterBinding)!!.outParameter)
+        assertEquals(Double::class.javaObjectType, (bindingV2.bindingFor(TokensFacade::getBalance)!!.outParameterBindings as? FacadeOutParameterBindings.SingletonOutParameterBinding)!!.returnType)
+    }
 }
