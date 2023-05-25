3f6d1b80edd3901b6c99e5a860f3e7f549bc3c7e
Chris Rankin <chris.rankin@r3.com>
2023-05-25 17:07:09 +0100

    CORE-13987: Simplify CurrentSandboxGroupContext component.

--- components/virtual-node/sandbox-group-context-service/src/main/kotlin/net/corda/sandboxgroupcontext/service/impl/CurrentSandboxGroupContextImpl.kt
+++ components/virtual-node/sandbox-group-context-service/src/main/kotlin/net/corda/sandboxgroupcontext/service/impl/CurrentSandboxGroupContextImpl.kt
@@ -1,35 +1,25 @@
 package net.corda.sandboxgroupcontext.service.impl
 
-import net.corda.sandbox.type.UsedByFlow
-import net.corda.sandbox.type.UsedByPersistence
-import net.corda.sandbox.type.UsedByVerification
 import net.corda.sandboxgroupcontext.CurrentSandboxGroupContext
 import net.corda.sandboxgroupcontext.SandboxGroupContext
 import net.corda.utilities.trace
 import net.corda.v5.serialization.SingletonSerializeAsToken
 import org.osgi.service.component.annotations.Component
-import org.slf4j.Logger
+import org.osgi.service.component.annotations.ServiceScope.SINGLETON
 import org.slf4j.LoggerFactory
 
-@Component(
-    service = [
-        CurrentSandboxGroupContext::class,
-        SingletonSerializeAsToken::class,
-        UsedByFlow::class,
-        UsedByPersistence::class,
-        UsedByVerification::class
-    ]
-)
-class CurrentSandboxGroupContextImpl
-    : CurrentSandboxGroupContext, SingletonSerializeAsToken, UsedByFlow, UsedByPersistence, UsedByVerification {
+/**
+ * A singleton service wrapping a [ThreadLocal] that contains the current [SandboxGroupContext].
+ * Declare this service _explicitly_ as a [SINGLETON], just to make the point.
+ *
+ * FLOW sandboxes need this component to declare [SingletonSerializeAsToken] as one of its OSGi
+ * services so that it can be registered with the Checkpoint serializer.
+ */
+@Component(service = [ CurrentSandboxGroupContext::class, SingletonSerializeAsToken::class ], scope = SINGLETON)
+class CurrentSandboxGroupContextImpl : CurrentSandboxGroupContext, SingletonSerializeAsToken {
+    private val log = LoggerFactory.getLogger(CurrentSandboxGroupContextImpl::class.java)
 
-    private companion object {
-        @JvmField
-        val log: Logger = LoggerFactory.getLogger(CurrentSandboxGroupContextImpl::class.java)
-
-        @JvmField
-        val currentSandboxGroupContext = ThreadLocal<SandboxGroupContext?>()
-    }
+    private val currentSandboxGroupContext = ThreadLocal<SandboxGroupContext?>()
 
     override fun set(sandboxGroupContext: SandboxGroupContext) {
         log.trace {
