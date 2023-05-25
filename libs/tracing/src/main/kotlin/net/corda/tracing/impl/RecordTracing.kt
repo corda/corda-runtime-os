96d355198732344d9a67f10f296b2f0c43ba0e1b
Joseph Zuniga-Daly <joseph.zunigadaly@r3.com>
2023-05-25 11:50:04 +0100

    CORE-13796: Keep useful changes from "CORE-13796: Remove KafkaTracing" commit

--- libs/tracing/src/main/kotlin/net/corda/tracing/impl/RecordTracing.kt
+++ libs/tracing/src/main/kotlin/net/corda/tracing/impl/RecordTracing.kt
@@ -2,18 +2,16 @@ package net.corda.tracing.impl
 
 import brave.Span
 import brave.Tracing
-import brave.messaging.MessagingTracing
 import brave.propagation.Propagation
 import net.corda.messaging.api.records.Record
 
 class RecordTracing(tracing: Tracing) {
     private val tracer = tracing.tracer()
-    private val messagingTracing = MessagingTracing.create(tracing)
     private val recordHeaderGetter: Propagation.Getter<List<Pair<String, String>>, String> =
         Propagation.Getter<List<Pair<String, String>>, String> { request, key ->
             request.reversed().firstOrNull { it.first==key }?.second
         }
-    private val tracingContextExtractor = messagingTracing.propagation().extractor(recordHeaderGetter)
+    private val tracingContextExtractor = tracing.propagation().extractor(recordHeaderGetter)
 
     fun nextSpan(record: Record<*, *>): Span {
         val extracted = tracingContextExtractor.extract(record.headers)
