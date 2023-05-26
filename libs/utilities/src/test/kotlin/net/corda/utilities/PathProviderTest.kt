def946d82cb8ed6b36ce87f135714cec500edee7
Chris Rankin <chris.rankin@r3.com>
2023-05-26 11:51:28 +0100

    CORE-14127: Detect whether filesystem supports POSIX.

--- libs/utilities/src/test/kotlin/net/corda/utilities/PathProviderTest.kt
+++ libs/utilities/src/test/kotlin/net/corda/utilities/PathProviderTest.kt
@@ -7,6 +7,8 @@ import net.corda.schema.configuration.ConfigKeys
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.verify
@@ -26,58 +28,116 @@ class PathProviderTest {
     private lateinit var fs: FileSystem
     private lateinit var config: SmartConfig
 
-    @BeforeEach
-    fun setUp() {
-        val posix = Configuration.unix().toBuilder()
-            .setAttributeViews("basic", "posix")
-            .build()
-        fs = Jimfs.newFileSystem(posix)
-
-        config = mock()
-    }
-
     @AfterEach
     fun clear() {
         fs.close()
     }
 
-    @Test
-    fun `getOrCreate creates workspace dir if not existing`() {
-        whenever(config.hasPath(ConfigKeys.WORKSPACE_DIR)).thenReturn(true)
-        whenever(config.getString(ConfigKeys.WORKSPACE_DIR)).thenReturn("/workspace")
-        val workspacePathProvider = WorkspacePathProvider { first, more ->
-            fs.getPath(first, *more)
+    @Nested
+    @DisplayName("PathProvider with UNIX filesystem")
+    inner class Unix {
+        @BeforeEach
+        fun setUp() {
+            val posix = Configuration.unix().toBuilder()
+                .setAttributeViews("basic", "posix")
+                .build()
+            fs = Jimfs.newFileSystem(posix)
+
+            config = mock()
         }
 
-        val dir = workspacePathProvider.getOrCreate(config, "subdir")
-        assertThat(dir).hasToString("/workspace/subdir")
-            .isDirectory
-            .isReadable
-            .isWritable
-            .isExecutable
-        assertThat(Files.getPosixFilePermissions(dir))
-            .containsExactlyInAnyOrder(OWNER_WRITE, OWNER_READ, OWNER_EXECUTE, GROUP_READ, GROUP_EXECUTE, OTHERS_EXECUTE, OTHERS_READ)
-        verify(config).hasPath(ConfigKeys.WORKSPACE_DIR)
-        verify(config).getString(ConfigKeys.WORKSPACE_DIR)
+        @Test
+        fun `getOrCreate creates workspace dir if not existing`() {
+            whenever(config.hasPath(ConfigKeys.WORKSPACE_DIR)).thenReturn(true)
+            whenever(config.getString(ConfigKeys.WORKSPACE_DIR)).thenReturn("/workspace")
+            val workspacePathProvider = WorkspacePathProvider { first, more ->
+                fs.getPath(first, *more)
+            }
+
+            val dir = workspacePathProvider.getOrCreate(config, "subdir")
+            assertThat(dir).hasToString("/workspace/subdir")
+                .isDirectory
+                .isReadable
+                .isWritable
+                .isExecutable
+            assertThat(Files.getPosixFilePermissions(dir))
+                .containsExactlyInAnyOrder(
+                    OWNER_WRITE,
+                    OWNER_READ,
+                    OWNER_EXECUTE,
+                    GROUP_READ,
+                    GROUP_EXECUTE,
+                    OTHERS_EXECUTE,
+                    OTHERS_READ
+                )
+            verify(config).hasPath(ConfigKeys.WORKSPACE_DIR)
+            verify(config).getString(ConfigKeys.WORKSPACE_DIR)
+        }
+
+        @Test
+        fun `getOrCreate creates temporary dir if not existing`() {
+            whenever(config.hasPath(ConfigKeys.TEMP_DIR)).thenReturn(true)
+            whenever(config.getString(ConfigKeys.TEMP_DIR)).thenReturn("/tmp")
+            val temporaryPathProvider = TempPathProvider { first, more ->
+                fs.getPath(first, *more)
+            }
+
+            val dir = temporaryPathProvider.getOrCreate(config, "subdir")
+            assertThat(dir).hasToString("/tmp/subdir")
+                .isDirectory
+                .isReadable
+                .isWritable
+                .isExecutable
+            assertThat(Files.getPosixFilePermissions(dir))
+                .containsExactlyInAnyOrder(OWNER_WRITE, OWNER_READ, OWNER_EXECUTE)
+            verify(config).hasPath(ConfigKeys.TEMP_DIR)
+            verify(config).getString(ConfigKeys.TEMP_DIR)
+        }
     }
 
-    @Test
-    fun `getOrCreate creates temporary dir if not existing`() {
-        whenever(config.hasPath(ConfigKeys.TEMP_DIR)).thenReturn(true)
-        whenever(config.getString(ConfigKeys.TEMP_DIR)).thenReturn("/tmp")
-        val temporaryPathProvider = TempPathProvider { first, more ->
-            fs.getPath(first, *more)
+    @Nested
+    @DisplayName("PathProvider with Windows filesystem")
+    inner class Windows {
+        @BeforeEach
+        fun setUp() {
+            fs = Jimfs.newFileSystem(Configuration.windows())
+            config = mock()
         }
 
-        val dir = temporaryPathProvider.getOrCreate(config, "subdir")
-        assertThat(dir).hasToString("/tmp/subdir")
-            .isDirectory
-            .isReadable
-            .isWritable
-            .isExecutable
-        assertThat(Files.getPosixFilePermissions(dir))
-            .containsExactlyInAnyOrder(OWNER_WRITE, OWNER_READ, OWNER_EXECUTE)
-        verify(config).hasPath(ConfigKeys.TEMP_DIR)
-        verify(config).getString(ConfigKeys.TEMP_DIR)
+        @Test
+        fun `getOrCreate creates workspace dir if not existing`() {
+            whenever(config.hasPath(ConfigKeys.WORKSPACE_DIR)).thenReturn(true)
+            whenever(config.getString(ConfigKeys.WORKSPACE_DIR)).thenReturn("c:\\workspace")
+            val workspacePathProvider = WorkspacePathProvider { first, more ->
+                fs.getPath(first, *more)
+            }
+
+            val dir = workspacePathProvider.getOrCreate(config, "subdir")
+            assertThat(dir).hasToString("c:\\workspace\\subdir")
+                .isDirectory
+                .isReadable
+                .isWritable
+                .isExecutable
+            verify(config).hasPath(ConfigKeys.WORKSPACE_DIR)
+            verify(config).getString(ConfigKeys.WORKSPACE_DIR)
+        }
+
+        @Test
+        fun `getOrCreate creates temporary dir if not existing`() {
+            whenever(config.hasPath(ConfigKeys.TEMP_DIR)).thenReturn(true)
+            whenever(config.getString(ConfigKeys.TEMP_DIR)).thenReturn("c:\\tmp")
+            val temporaryPathProvider = TempPathProvider { first, more ->
+                fs.getPath(first, *more)
+            }
+
+            val dir = temporaryPathProvider.getOrCreate(config, "subdir")
+            assertThat(dir).hasToString("c:\\tmp\\subdir")
+                .isDirectory
+                .isReadable
+                .isWritable
+                .isExecutable
+            verify(config).hasPath(ConfigKeys.TEMP_DIR)
+            verify(config).getString(ConfigKeys.TEMP_DIR)
+        }
     }
 }
