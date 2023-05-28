f094ba26d174b0ab24ec4c0ce3969957cdf5d346
Dan Newton <dan.newton@r3.com>
2023-05-28 09:14:39 +0100

    CORE-13926 tidy

--- components/flow/flow-service/src/main/kotlin/net/corda/flow/application/crypto/MySigningKeysCacheImpl.kt
+++ components/flow/flow-service/src/main/kotlin/net/corda/flow/application/crypto/MySigningKeysCacheImpl.kt
@@ -20,7 +20,7 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import java.security.PublicKey
 
-@Component(service = [MySigningKeysCache::class, SandboxedCache::class])
+@Component(service = [MySigningKeysCache::class])
 class MySigningKeysCacheImpl @Activate constructor(
     @Reference(service = CurrentSandboxGroupContext::class)
     private val currentSandboxGroupContext: CurrentSandboxGroupContext,
