584345b056481c08377740535b26d46d1ea155a4
nkovacsx <nandor.kovacs@r3.com>
2023-05-30 11:35:17 +0100

    CORE-13934 bypass backchain resolution if there are no dependencies

--- components/ledger/ledger-utxo-flow/src/main/kotlin/net/corda/ledger/utxo/flow/impl/flows/finality/v1/UtxoReceiveFinalityFlowV1.kt
+++ components/ledger/ledger-utxo-flow/src/main/kotlin/net/corda/ledger/utxo/flow/impl/flows/finality/v1/UtxoReceiveFinalityFlowV1.kt
@@ -8,6 +8,7 @@ import net.corda.ledger.utxo.flow.impl.flows.finality.getVisibleStateIndexes
 import net.corda.ledger.utxo.flow.impl.flows.finality.v1.FinalityNotarizationFailureType.Companion.toFinalityNotarizationFailureType
 import net.corda.ledger.utxo.flow.impl.transaction.UtxoSignedTransactionInternal
 import net.corda.sandbox.CordaSystemFlow
+import net.corda.utilities.trace
 import net.corda.v5.application.crypto.DigitalSignatureAndMetadata
 import net.corda.v5.application.messaging.FlowSession
 import net.corda.v5.base.annotations.Suspendable
@@ -69,7 +70,14 @@ class UtxoReceiveFinalityFlowV1(
         if (log.isDebugEnabled) {
             log.debug( "Beginning receive finality for transaction: ${initialTransaction.id}")
         }
-        flowEngine.subFlow(TransactionBackchainResolutionFlow(initialTransaction.dependencies, session))
+        val transactionDependencies = initialTransaction.dependencies
+        if (transactionDependencies.isNotEmpty()) {
+            flowEngine.subFlow(TransactionBackchainResolutionFlow(transactionDependencies, session))
+        } else {
+            log.trace {
+                "Transaction with id ${initialTransaction.id} has no dependencies so no backchain resolution will be performed."
+            }
+        }
         return initialTransaction
     }
 
