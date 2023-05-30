aa9ab95dff8926fa77d98949e9f67163aec8a131
jmacmahonr3 <james.macmahon@r3.com>
2023-05-30 14:37:24 +0100

    making Lookup nullable

--- components/interop/interop-service/src/main/kotlin/net/corda/interop/service/impl/InteropIdentityLookUpImpl.kt
+++ components/interop/interop-service/src/main/kotlin/net/corda/interop/service/impl/InteropIdentityLookUpImpl.kt
@@ -4,6 +4,7 @@ import net.corda.interop.helper.HardcodedAliasIdentityHelper
 import net.corda.sandbox.type.UsedByFlow
 import net.corda.v5.application.interop.InteropIdentityLookUp
 import net.corda.v5.application.interop.facade.FacadeId
+import net.corda.v5.base.annotations.Suspendable
 import net.corda.v5.interop.InterOpIdentityInfo
 import net.corda.v5.serialization.SingletonSerializeAsToken
 import org.osgi.service.component.annotations.Activate
@@ -14,16 +15,17 @@ import org.osgi.service.component.annotations.ServiceScope.PROTOTYPE
 class InteropIdentityLookUpImpl @Activate constructor() :
     InteropIdentityLookUp, UsedByFlow, SingletonSerializeAsToken {
 
+    @Suspendable
     override fun lookup(applicationName: String?): InterOpIdentityInfo? {
         return getAliasMemberData().firstOrNull(){
             it.applicationName() == applicationName
         }
     }
-
+    @Suspendable
     override fun lookup(facadeId: FacadeId?): List<InterOpIdentityInfo> {
         return getAliasMemberData().filter { it.facadeIds.contains(facadeId.toString()) }
     }
-
+    @Suspendable
     private fun getAliasMemberData(): List<InterOpIdentityInfo> {
         return HardcodedAliasIdentityHelper.getAliasIdentityData()
     }
