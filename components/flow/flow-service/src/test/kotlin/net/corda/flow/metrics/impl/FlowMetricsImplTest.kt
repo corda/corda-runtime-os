cd59ebbdf3fc9c920b60cf8bdd264a605dc8785b
Emily Bowe <emily.bowe@r3.com>
2023-05-31 17:35:02 +0100

    add to tests

--- components/flow/flow-service/src/test/kotlin/net/corda/flow/metrics/impl/FlowMetricsImplTest.kt
+++ components/flow/flow-service/src/test/kotlin/net/corda/flow/metrics/impl/FlowMetricsImplTest.kt
@@ -18,7 +18,7 @@ internal class FlowMetricsImplTest {
     }
 
     @Test
-    fun flowSessionMessageSent() {
+    fun `handles replay`() {
         val sessionId = "sessionId"
         val flowEventType = "SessionData"
         val sequenceNumber = 1L
@@ -42,6 +42,62 @@ internal class FlowMetricsImplTest {
         flowMetricsImpl.flowSessionMessageSent(flowEventType, sessionId, sequenceNumber)
 
         verify(flowMetricsRecorder, times(1)).recordFlowSessionMessagesReplayed(flowEventType)
+        verify(flowMetricsRecorder, times(1)).recordFlowSessionMessagesSent(flowEventType)
+    }
+
+    @Test
+    fun `handles out-of-order`() {
+        val sessionId = "sessionId"
+        val flowEventType = "SessionData"
+        val sequenceNumber = 3L
+        val HSSN = 1L
+        val recordTimestamp = 100L
+
+        val flowMetricsStateString: String
+        val checkpoint = mock<FlowCheckpoint>()
+        val flowMetricStateObject = FlowMetricsImpl.FlowMetricState()
+        val sessionMetricStateObject = FlowMetricsImpl.SessionMetricState()
+
+        sessionMetricStateObject.highestSeenSequenceNumber = HSSN
+        flowMetricStateObject.sessionMetricStateBySessionId[sessionId] = sessionMetricStateObject
+
+        val flowMetricsRecorder = mock<FlowMetricsRecorder>()
+        flowMetricsStateString = objectMapper.writeValueAsString(flowMetricStateObject)
+
+        whenever(checkpoint.flowMetricsState).thenReturn(flowMetricsStateString)
+
+        val flowMetricsImpl = FlowMetricsImpl(clock, flowMetricsRecorder, checkpoint, recordTimestamp)
+        flowMetricsImpl.flowSessionMessageSent(flowEventType, sessionId, sequenceNumber)
+
+        verify(flowMetricsRecorder, times(0)).recordFlowSessionMessagesReplayed(flowEventType)
+        verify(flowMetricsRecorder, times(1)).recordFlowSessionMessagesSent(flowEventType)
+    }
+
+    @Test
+    fun `handles in-order non-replay`() {
+        val sessionId = "sessionId"
+        val flowEventType = "SessionData"
+        var sequenceNumber = 1L
+        val HSSN = 0L
+        val recordTimestamp = 100L
+
+        val flowMetricsStateString: String
+        val checkpoint = mock<FlowCheckpoint>()
+        val flowMetricStateObject = FlowMetricsImpl.FlowMetricState()
+        val sessionMetricStateObject = FlowMetricsImpl.SessionMetricState()
+
+        sessionMetricStateObject.highestSeenSequenceNumber = HSSN
+        flowMetricStateObject.sessionMetricStateBySessionId[sessionId] = sessionMetricStateObject
+
+        val flowMetricsRecorder = mock<FlowMetricsRecorder>()
+        flowMetricsStateString = objectMapper.writeValueAsString(flowMetricStateObject)
+
+        whenever(checkpoint.flowMetricsState).thenReturn(flowMetricsStateString)
+
+        val flowMetricsImpl = FlowMetricsImpl(clock, flowMetricsRecorder, checkpoint, recordTimestamp)
+        flowMetricsImpl.flowSessionMessageSent(flowEventType, sessionId, sequenceNumber)
+
+        verify(flowMetricsRecorder, times(0)).recordFlowSessionMessagesReplayed(flowEventType)
         verify(flowMetricsRecorder, times(1)).recordFlowSessionMessagesSent(flowEventType)
     }
 }
