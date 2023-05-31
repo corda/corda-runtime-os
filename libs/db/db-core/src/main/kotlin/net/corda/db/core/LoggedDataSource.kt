d8e2c5b38581c5e7e07cc599787258da351e0c64
Yiftach Kaplan <yiftach.kaplan@r3.com>
2023-05-31 11:21:42 +0100

    Report evry minute

--- libs/db/db-core/src/main/kotlin/net/corda/db/core/LoggedDataSource.kt
+++ libs/db/db-core/src/main/kotlin/net/corda/db/core/LoggedDataSource.kt
@@ -4,6 +4,9 @@ import com.zaxxer.hikari.HikariDataSource
 import org.slf4j.LoggerFactory
 import java.io.Closeable
 import java.sql.Connection
+import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
 import javax.sql.DataSource
 
 internal class LoggedConnection(
@@ -11,10 +14,26 @@ internal class LoggedConnection(
 ): Connection by connection {
     private companion object {
         private val logger = LoggerFactory.getLogger("QQQ")
+        private val activeConnection = ConcurrentHashMap.newKeySet<LoggedConnection>()
+        private val thread = Executors.newScheduledThreadPool(1).also {
+            it.scheduleAtFixedRate(::report, 1, 1, TimeUnit.MINUTES)
+        }
+        private fun report() {
+            logger.info("In $thread report, number of open connections so far is: ${activeConnection.size}")
+
+            activeConnection.forEach {
+                logger.info("$it is still open", it.created)
+            }
+        }
     }
     private val created = Exception("QQQ - ${connection.schema}:${connection.catalog}")
+    init {
+        activeConnection.add(this)
+    }
+
 
     override fun close() {
+        activeConnection.remove(this)
         logger.info("Closing connection $created", Exception("Closed", created))
     }
 }
