16e1018a91e810ef0542ef764cbaa9e4ff0f6009
Yiftach Kaplan <yiftach.kaplan@r3.com>
2023-06-01 10:52:37 +0100

    More logs

--- libs/db/db-core/src/main/kotlin/net/corda/db/core/LoggedDataSource.kt
+++ libs/db/db-core/src/main/kotlin/net/corda/db/core/LoggedDataSource.kt
@@ -4,14 +4,18 @@ import org.slf4j.LoggerFactory
 import java.io.PrintWriter
 import java.sql.Connection
 import java.sql.DriverManager
+import java.time.Duration
+import java.time.Instant
 import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.Executors
 import java.util.concurrent.TimeUnit
+import java.util.concurrent.atomic.AtomicLong
 import java.util.logging.Logger
 import javax.sql.DataSource
 
 internal class LoggedConnection(
-    val connection: Connection
+    val connection: Connection,
+    dsCreated: Throwable,
 ): Connection by connection {
     private companion object {
         private val logger = LoggerFactory.getLogger("QQQ")
@@ -19,6 +23,7 @@ internal class LoggedConnection(
         private val thread = Executors.newScheduledThreadPool(1).also {
             it.scheduleAtFixedRate(::report, 1, 1, TimeUnit.MINUTES)
         }
+        private val indexFactory = AtomicLong()
         private fun report() {
             logger.info("In $thread report, number of open connections so far is: ${activeConnection.size}")
 
@@ -27,7 +32,9 @@ internal class LoggedConnection(
             }
         }
     }
-    private val created = Exception("QQQ - ${connection.schema}:${connection.catalog}")
+    private val createdAt = Instant.now()
+    private val index = indexFactory.incrementAndGet()
+    private val created = Exception(toString(), dsCreated)
     init {
         activeConnection.add(this)
     }
@@ -36,8 +43,15 @@ internal class LoggedConnection(
     override fun close() {
         connection.close()
         activeConnection.remove(this)
-        logger.info("Closing connection $created", Exception("Closed", created))
+        logger.info(
+            "Closing connection $this",
+            Exception("Closed", created),
+        )
     }
+
+    override fun toString() =
+        "Connection schema: ${connection.schema}, catalog: ${connection.catalog}," +
+            " index: $index, aliveFor: ${Duration.between(Instant.now(), createdAt)}"
 }
 internal class SimpleDataSource(
     driverClass: String,
@@ -90,12 +104,13 @@ internal class SimpleDataSource(
 internal class LoggedDataSource(
     private val datasource: DataSource,
 ):DataSource by datasource {
+    private val created = Exception("TTT - created datasource")
 
     override fun getConnection(): Connection {
-        return LoggedConnection(datasource.connection)
+        return LoggedConnection(datasource.connection, created)
     }
 
     override fun getConnection(username: String?, password: String?): Connection {
-        return LoggedConnection(datasource.getConnection(username, password))
+        return LoggedConnection(datasource.getConnection(username, password), created)
     }
 }
