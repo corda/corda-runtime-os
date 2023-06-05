1f842254025ae293eebe2eecb94b217e0c0469bd
Joseph Zuniga-Daly <joseph.zunigadaly@r3.com>
2023-06-05 15:33:59 +0100

    CORE-13796: Remove types from constant declarations
    Co-authored-by: Viktor Kolomeyko <viktor.kolomeyko@r3.com>

--- libs/tracing/src/main/kotlin/net/corda/tracing/TraceTag.kt
+++ libs/tracing/src/main/kotlin/net/corda/tracing/TraceTag.kt
@@ -6,7 +6,7 @@ package net.corda.tracing
  * Close before shutdown to wait for trace spans to be sent to external systems.
  */
 object TraceTag {
-    const val FLOW_CLASS:String = "flow.class"
-    const val FLOW_REQUEST_ID:String = "flow.request.id"
-    const val FLOW_REQUEST_VNODE_ID:String = "flow.request.vnode.id"
+    const val FLOW_CLASS = "flow.class"
+    const val FLOW_REQUEST_ID = "flow.request.id"
+    const val FLOW_REQUEST_VNODE_ID = "flow.request.vnode.id"
 }
