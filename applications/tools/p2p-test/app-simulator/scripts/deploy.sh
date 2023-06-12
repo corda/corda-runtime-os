2908678b41e75c80ff574f075e2ad12cedafd1fe
Yiftach Kaplan <yiftach.kaplan@r3.com>
2023-06-12 16:49:57 +0100

    Use two files with metrics, one file without

--- applications/tools/p2p-test/app-simulator/scripts/deploy.sh
+++ applications/tools/p2p-test/app-simulator/scripts/deploy.sh
@@ -10,30 +10,29 @@ deploy() {
    echo Creating $namespace
    kubectl delete ns $namespace || echo ''
    kubectl create ns $namespace
+   prereqs_args="--install prereqs -n $namespace  \
+                oci://corda-os-docker.software.r3.com/helm-charts/corda-dev  \
+                --set image.registry=\"corda-os-docker.software.r3.com\"  \
+                --set kafka.replicaCount=$KAFKA_REPLICAS,kafka.zookeeper.replicaCount=$KAFKA_ZOOKEEPER_REPLICAS  \
+                -f \"$SCRIPT_DIR/prereqs-eks.yaml\"  \
+                --render-subchart-notes  \
+                --timeout 10m  \
+                --wait"
+   corda_args="--install corda -n $namespace oci://corda-os-docker.software.r3.com/helm-charts/release/os/5.0/corda \
+              --set imagePullSecrets={docker-registry-cred} --set image.tag=$DOCKER_IMAGE_VERSION \
+              --set image.registry=corda-os-docker.software.r3.com --values $REPO_TOP_LEVEL_DIR/values.yaml \
+              -f \"$SCRIPT_DIR/corda-eks.yaml\" \
+              --values $REPO_TOP_LEVEL_DIR/debug.yaml --wait --version $CORDA_CHART_VERSION"
    if kubectl get ns metrics-server > /dev/null 2>/dev/null ; then
-       prereqs_eks="$SCRIPT_DIR/prereqs-eks.metrics.yaml"
-       corda_eks="$SCRIPT_DIR/corda-eks.metrics.yaml"
-   else
-       prereqs_eks="$SCRIPT_DIR/prereqs-eks.yaml"
-       corda_eks="$SCRIPT_DIR/corda-eks.yaml"
+     prereqs_args+=" -f \"$SCRIPT_DIR/prereqs-eks.metrics.yaml\""
+    corda_args+=" -f \"$SCRIPT_DIR/corda-eks.metrics.yaml\""
    fi
 
    echo Installing prereqs into $namespace
-   helm upgrade --install prereqs -n $namespace \
-     oci://corda-os-docker.software.r3.com/helm-charts/corda-dev \
-     --set image.registry="corda-os-docker.software.r3.com" \
-     --set kafka.replicaCount=$KAFKA_REPLICAS,kafka.zookeeper.replicaCount=$KAFKA_ZOOKEEPER_REPLICAS \
-     -f "$prereqs_eks" \
-     --render-subchart-notes \
-     --timeout 10m \
-     --wait
+   helm upgrade ${prereqs_args}
 
    echo Installing corda image $DOCKER_IMAGE_VERSION into $namespace
-   helm upgrade --install corda -n $namespace oci://corda-os-docker.software.r3.com/helm-charts/release/os/5.0/corda \
-     --set "imagePullSecrets={docker-registry-cred}" --set image.tag=$DOCKER_IMAGE_VERSION \
-     --set image.registry="corda-os-docker.software.r3.com" --values $REPO_TOP_LEVEL_DIR/values.yaml \
-     -f "$corda_eks" \
-     --values $REPO_TOP_LEVEL_DIR/debug.yaml --wait --version $CORDA_CHART_VERSION
+   helm upgrade ${corda_args}
 }
 
 if [ $# -eq 0 ]
