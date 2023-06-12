eb3b229a673884aa634e8aa0f878def7e244d902
Dimos Raptis <dimos.raptis@r3.com>
2023-06-12 16:42:10 +0100

    CORE-11411 - Refresh certificates using openssl and longer expiration window (#4056)
    Refresh certificates used for integration tests that were due to expire next month using openssl, so that they have a longer expiration window (50 years). I also updated the README file with some extra options I had to use to create the certificates in the required way (e.g. specifying key usage, a valid country, missing mandatory x500 attributes such as organisation etc.).
    
    As part of this change, I didn't refresh:
    * the C4 certificates as they expire in ~4 years and there's no step-by-step documentation on how they had been generated so it will require some reverse engineering and familiarisation with C4 that's probably not worth the effort (also given the fact that support of C4 certificates from C5 was part of the original design, but it's not going to be a feature from what we've seen so it's very likely the tests will get disabled at some point).
    * the Bob certificate, which is a revoked certificate from Tinycert. The tests exercising revocation have been disabled and they will be enabled back again after resolving CORE-11411. However, I split the truststore out into a separate file, since the other certificates will now have a truststore coming from openssl.

--- testing/p2p/certificates/src/main/kotlin/net/corda/testing/p2p/certificates/Certificates.kt
+++ testing/p2p/certificates/src/main/kotlin/net/corda/testing/p2p/certificates/Certificates.kt
@@ -3,18 +3,26 @@ package net.corda.testing.p2p.certificates
 class Certificates {
     companion object {
         val truststoreCertificatePem = this::class.java.classLoader.getResource("truststore/certificate.pem")!!
-        val c4TruststoreCertificatePem = this::class.java.classLoader.getResource("truststore_c4/cordarootca.pem")!!
         val aliceKeyStoreFile = this::class.java.classLoader.getResource("sslkeystore_alice.jks")!!
-        val bobKeyStoreFile = this::class.java.classLoader.getResource("sslkeystore_bob.jks")!!
+        val aliceKeyStorePem = this::class.java.classLoader.getResource("certificate_alice.pem")!!
         val chipKeyStoreFile = this::class.java.classLoader.getResource("sslkeystore_chip.jks")!!
         val daleKeyStoreFile = this::class.java.classLoader.getResource("sslkeystore_dale.jks")!!
+        val ipKeyStore = this::class.java.classLoader.getResource("sslkeystore_127.0.0.1.jks")!!
+
+        val truststoreCertificateWithRevocationPem = this::class.java.classLoader
+                                                            .getResource("truststore/ca_with_revocation_certificate.pem")!!
+        // A revoked certificate from TinyCert
+        val bobKeyStoreFile = this::class.java.classLoader.getResource("sslkeystore_bob.jks")!!
+        val bobKeyStorePem = this::class.java.classLoader.getResource("certificate_bob.pem")!!
+
+        val c4TruststoreCertificatePem = this::class.java.classLoader.getResource("truststore_c4/cordarootca.pem")!!
         val c4KeyStoreFile = this::class.java.classLoader.getResource("sslkeystore_c4.jks")!!
+
         val c5KeyStoreFile = this::class.java.classLoader.getResource("sslkeystore_c5.jks")!!
+
         val ecTrustStorePem = this::class.java.classLoader.getResource("ec_truststore.pem")!!
         val receiverKeyStoreFile = this::class.java.classLoader.getResource("receiver.jks")!!
         val senderKeyStoreFile = this::class.java.classLoader.getResource("sender.jks")!!
-        val aliceKeyStorePem = this::class.java.classLoader.getResource("certificate_alice.pem")!!
-        val bobKeyStorePem = this::class.java.classLoader.getResource("certificate_bob.pem")!!
-        val ipKeyStore = this::class.java.classLoader.getResource("sslkeystore_127.0.0.1.jks")!!
+
     }
 }
