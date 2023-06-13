6d39b53993cd0f6a56d64b77ff12e0b56fa909c2
Anna Paskova <anna.paskova@r3.com>
2023-06-13 15:15:38 +0100

    Delete SignCpxWithKmsKey to prove only one command can be used.

--- tools/plugins/package/src/main/kotlin/net/corda/cli/plugins/packaging/PackagePluginWrapper.kt
+++ tools/plugins/package/src/main/kotlin/net/corda/cli/plugins/packaging/PackagePluginWrapper.kt
@@ -11,7 +11,7 @@ class PackagePluginWrapper : Plugin() {
     @CommandLine.Command(
         name = "package",
         subcommands = [CreateCpiV2::class, Verify::class, CreateCpb::class, CreateCpbSignWithKms::class, SignCpx::class,
-            SignCpxWithKmsKey::class, CreateCsrForKmsKey::class],
+            CreateCsrForKmsKey::class],
         description = ["Plugin for CPB, CPI operations."]
     )
     class PackagePlugin : CordaCliPlugin
