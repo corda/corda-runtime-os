419b790b549615dfd8a1242cd5fa189d82907571
Viktor Kolomeyko <Viktor.Kolomeyko@r3.com>
2023-06-19 11:27:09 +0100

    CORE-14709: Minor changes

--- libs/rest/rest-server/src/main/kotlin/net/corda/rest/server/config/models/RestContext.kt
+++ libs/rest/rest-server/src/main/kotlin/net/corda/rest/server/config/models/RestContext.kt
@@ -5,11 +5,4 @@ data class RestContext(
     val basePath: String,
     val title: String,
     val description: String
-) {
-    object Defaults {
-        const val version = "1"
-        const val basePath = "/api"
-        const val title = ""
-        const val description = ""
-    }
-}
+)
