500ed80c22cc4993bb4d8a159a9f31fa71651934
Yiftach Kaplan <yiftach.kaplan@r3.com>
2023-06-19 17:34:32 +0100

    more

--- components/membership/registration-impl/src/main/kotlin/net/corda/membership/impl/registration/staticnetwork/StaticMemberRegistrationService.kt
+++ components/membership/registration-impl/src/main/kotlin/net/corda/membership/impl/registration/staticnetwork/StaticMemberRegistrationService.kt
@@ -236,6 +236,14 @@ class StaticMemberRegistrationService(
                 ex,
             )
         }
+        val membershipGroupReader = membershipGroupReaderProvider.getGroupReader(member)
+        val lookup = membershipGroupReader.lookup(member.x500Name)
+        if (lookup?.isActive == true) {
+            throw InvalidMembershipRegistrationException(
+                "The member ${member.x500Name} had been registered successfully in the group ${member.groupId}. " +
+                        "Can not re-register."
+            )
+        }
         val latestStatuses = membershipQueryClient.queryRegistrationRequests(
             member,
             member.x500Name,
@@ -258,7 +266,6 @@ class StaticMemberRegistrationService(
                 requireNotNull(this) { "Could not find static member list in group policy file." }
                 map { StaticMember(it, endpointInfoFactory::create) }
             }
-            val membershipGroupReader = membershipGroupReaderProvider.getGroupReader(member)
             val (memberInfo, records) = parseMemberTemplate(
                 member,
                 groupPolicy,
