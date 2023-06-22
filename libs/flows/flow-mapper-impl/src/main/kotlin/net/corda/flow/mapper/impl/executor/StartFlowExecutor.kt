5c21b80c2fb5e86ef463364b30696a7b3bb966f5
rick.parker <rick.parker@r3cev.com>
2023-06-22 14:18:32 +0100

    Fixed partitioning of flow.status and flow.mapper.event(.state) for start flows

--- libs/flows/flow-mapper-impl/src/main/kotlin/net/corda/flow/mapper/impl/executor/StartFlowExecutor.kt
+++ libs/flows/flow-mapper-impl/src/main/kotlin/net/corda/flow/mapper/impl/executor/StartFlowExecutor.kt
@@ -1,5 +1,6 @@
 package net.corda.flow.mapper.impl.executor
 
+import net.corda.data.crypto.SecureHash
 import net.corda.data.flow.event.FlowEvent
 import net.corda.data.flow.event.StartFlow
 import net.corda.data.flow.state.mapper.FlowMapperState
@@ -9,7 +10,10 @@ import net.corda.flow.mapper.executor.FlowMapperEventExecutor
 import net.corda.messaging.api.records.Record
 import net.corda.metrics.CordaMetrics
 import net.corda.utilities.debug
+import net.corda.v5.base.util.ByteArrays.toHexString
 import org.slf4j.LoggerFactory
+import java.nio.ByteBuffer
+import java.util.*
 
 class StartFlowExecutor(
     private val eventKey: String,
@@ -27,7 +31,10 @@ class StartFlowExecutor(
             CordaMetrics.Metric.FlowMapperCreationCount.builder()
                 .withTag(CordaMetrics.Tag.FlowEvent, startRPCFlow::class.java.name)
                 .build().increment()
-            val flowId = generateFlowId()
+            val clientIDSecureHash =
+                SecureHash("SHA256", ByteBuffer.wrap(startRPCFlow.startContext.requestId.encodeToByteArray()))
+            val flowId = toHexString(clientIDSecureHash.bytes.array()).substring(0, 8) + UUID.randomUUID().toString()
+                .substring(8, 36)
             val newState = FlowMapperState(flowId, null, FlowMapperStateType.OPEN)
             val flowEvent = FlowEvent(flowId, startRPCFlow)
             FlowMapperResult(
--- libs/messaging/kafka-message-bus-impl/src/main/kotlin/net/corda/messagebus/kafka/producer/KafkaProducerPartitioner.kt
+++ libs/messaging/kafka-message-bus-impl/src/main/kotlin/net/corda/messagebus/kafka/producer/KafkaProducerPartitioner.kt
@@ -1,12 +1,16 @@
 package net.corda.messagebus.kafka.producer
 
 import net.corda.data.chunking.ChunkKey
+import net.corda.data.crypto.SecureHash
+import net.corda.data.flow.FlowKey
 import net.corda.utilities.trace
+import net.corda.v5.base.util.ByteArrays.toHexString
 import org.apache.kafka.clients.producer.Partitioner
 import org.apache.kafka.clients.producer.internals.BuiltInPartitioner
 import org.apache.kafka.common.Cluster
 import org.apache.kafka.common.utils.Utils
 import org.slf4j.LoggerFactory
+import java.nio.ByteBuffer
 import java.nio.charset.StandardCharsets
 import java.util.*
 
@@ -18,7 +22,10 @@ class KafkaProducerPartitioner : Partitioner {
         private val logger = LoggerFactory.getLogger(this::class.java.enclosingClass)
     }
 
-    override fun configure(configs: MutableMap<String, *>?) {}
+    override fun configure(configs: MutableMap<String, *>?) {
+        logger.info("Config = $configs")
+    }
+
     override fun close() {}
 
     /**
@@ -41,8 +48,8 @@ class KafkaProducerPartitioner : Partitioner {
             logger.info("Sending record on topic $topic to partition $partition (of $partitionCount) with key $key")
         }
 
-        fun partitionFor(hex: String): Int {
-            return (Utils.toPositive(hex.toUInt(16).toInt()) % partitionCount).also { logPartition(it) }
+        fun partitionFor(hex: String, partitionCountToUse: Int = partitionCount): Int {
+            return (Utils.toPositive(hex.toUInt(16).toInt()) % partitionCountToUse).also { logPartition(it) }
         }
 
         fun deserializeString(bytes: ByteArray): String? {
@@ -50,6 +57,31 @@ class KafkaProducerPartitioner : Partitioner {
                 String(bytes, StandardCharsets.UTF_8)
             } catch (e: Exception) {
                 null
+            }
+        }
+
+        fun partitionFromHash(
+            withHash: String,
+            invert: Boolean = false,
+            partitionCountToUse: Int = partitionCount
+        ): Int {
+            return if (invert) {
+                // Invert for p2p.in
+                if (withHash.contains("-INITIATED")) {
+                    // Just immediately mod the hash of the flow ID
+                    partitionFor(withHash.substring(1, 9), partitionCountToUse)
+                } else {
+                    // Just immediately mod the hash of the receiver flow ID
+                    partitionFor(withHash.substring(9, 17), partitionCountToUse)
+                }
+            } else {
+                if (withHash.contains("-INITIATED")) {
+                    // Just immediately mod the hash of the receiver flow ID
+                    partitionFor(withHash.substring(9, 17), partitionCountToUse)
+                } else {
+                    // Just immediately mod the hash of the flow ID
+                    partitionFor(withHash.substring(1, 9), partitionCountToUse)
+                }
             }
         }
 
@@ -61,32 +93,25 @@ class KafkaProducerPartitioner : Partitioner {
             keyBytes to ((key as? String) ?: deserializeString(keyBytes))
         }
 
+        if (key is FlowKey && topic.endsWith("flow.status")) {
+            val keyId = key.id
+            if (keyId.startsWith("#") && keyId.indexOf('/') == 17) return partitionFromHash(keyId)
+            val clientIDSecureHash = SecureHash("SHA256", ByteBuffer.wrap(keyId.encodeToByteArray()))
+            return partitionFromHash("#" + toHexString(clientIDSecureHash.bytes.array()).substring(0, 8))
+        }
         if (keyToPartition is String) {
             if (keyToPartition.startsWith("#") && keyToPartition.indexOf('/') == 17) {
-                return if (topic.endsWith("p2p.in")) {
-                    // Invert for p2p.in
-                    if (keyToPartition.contains("-INITIATED")) {
-                        // Just immediately mod the hash of the flow ID
-                        partitionFor(keyToPartition.substring(1, 9))
-                    } else {
-                        // Just immediately mod the hash of the receiver flow ID
-                        partitionFor(keyToPartition.substring(9, 17))
-                    }
-                } else {
-                    if (keyToPartition.contains("-INITIATED")) {
-                        // Just immediately mod the hash of the receiver flow ID
-                        partitionFor(keyToPartition.substring(9, 17))
-                    } else {
-                        // Just immediately mod the hash of the flow ID
-                        partitionFor(keyToPartition.substring(1, 9))
-                    }
-                }
+                return partitionFromHash(keyToPartition, topic.endsWith("p2p.in"))
             } else if (keyToPartition.length == 36) {
                 try {
                     UUID.fromString(keyToPartition)
                     return partitionFor(keyToPartition.substring(0, 8))
                 } catch (e: Exception) {
                 }
+            } else if (topic.contains("flow.mapper.event") && keyToPartition.startsWith("{\"id\": \"")) {
+                val keyId = keyToPartition.substring(8, 44)
+                val clientIDSecureHash = SecureHash("SHA256", ByteBuffer.wrap(keyId.encodeToByteArray()))
+                return partitionFromHash("#" + toHexString(clientIDSecureHash.bytes.array()).substring(0, 8))
             }
         }
         val partition = BuiltInPartitioner.partitionForKey(keyBytesToPartition, partitionCount)
