e18e3442d6fa15906c913a491eaadadcffd33c47
eunjee.yang <eunjee.yang@r3.com>
2023-06-26 17:50:43 +0100

    CORE-5995 remove imports from testContract

--- testing/cpbs/ledger-utxo-demo-contract/src/main/kotlin/com/r3/corda/demo/utxo/contract/TestContract.kt
+++ testing/cpbs/ledger-utxo-demo-contract/src/main/kotlin/com/r3/corda/demo/utxo/contract/TestContract.kt
@@ -1,16 +1,11 @@
 package com.r3.corda.demo.utxo.contract
 
-import net.corda.v5.application.crypto.DigestService
-import net.corda.v5.application.crypto.DigitalSignatureVerificationService
-import net.corda.v5.application.flows.CordaInject
-import net.corda.v5.application.serialization.SerializationService
 import net.corda.v5.ledger.utxo.Contract
 import net.corda.v5.ledger.utxo.ContractState
 import net.corda.v5.ledger.utxo.VisibilityChecker
 import net.corda.v5.ledger.utxo.transaction.UtxoLedgerTransaction
 
 class TestContract : Contract {
-
     override fun verify(transaction: UtxoLedgerTransaction) {
     }
 
