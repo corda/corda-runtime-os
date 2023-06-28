48f013b81ff6197491bc20c3c6e9b71610d5f21c
Alexander Cramb <alex.cramb@r3.com>
2023-06-28 20:39:11 +0100

    Delete unused write service modules

--- components/interop/interop-write-service/src/main/java/net/corda/interop/write/service/impl/package-info.java
+++
@@ -1,4 +0,0 @@
-@Export
-package net.corda.interop.write.service.impl;
-
-import org.osgi.annotation.bundle.Export;
--- components/interop/interop-write-service/src/main/java/net/corda/interop/write/service/package-info.java
+++
@@ -1,4 +0,0 @@
-@Export
-package net.corda.interop.write.service;
-
-import org.osgi.annotation.bundle.Export;
