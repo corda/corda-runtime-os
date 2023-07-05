7f9fd2d5d865c7b12ea61d0face3b0e163fe7705
James Mac <58435815+jmacmahonr3@users.noreply.github.com>
2023-07-05 20:26:28 +0100

    CORE-15177 Fix for Facade unit tests checking 'shouldFailToBindWith' give false positives (#4225)
    CORE-15177 Fix for Facade unit tests checking 'shouldFailToBindWith' give false positives (#4225)

--- components/flow/flow-service/src/test/kotlin/net/corda/flow/application/services/interop/binding/BindingAssertions.kt
+++ components/flow/flow-service/src/test/kotlin/net/corda/flow/application/services/interop/binding/BindingAssertions.kt
@@ -3,11 +3,13 @@ package net.corda.flow.application.services.interop.binding
 import net.corda.flow.application.services.impl.interop.binding.creation.FacadeInterfaceBindings
 import net.corda.flow.application.services.impl.interop.binding.internal.FacadeInterfaceBindingException
 import net.corda.v5.application.interop.facade.Facade
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.assertThrows
 
 
 fun Facade.assertBindingFails(interfaceClass: Class<*>, expectedMessage: String) {
-    assertThrows<FacadeInterfaceBindingException> {
+    val error = assertThrows<FacadeInterfaceBindingException> {
         FacadeInterfaceBindings.bind(this, interfaceClass)
-    }.message.equals(expectedMessage)
+    }
+    assertTrue(error.message!!.contains(expectedMessage),"Expected Message was not present in : ${error.message}")
 }
