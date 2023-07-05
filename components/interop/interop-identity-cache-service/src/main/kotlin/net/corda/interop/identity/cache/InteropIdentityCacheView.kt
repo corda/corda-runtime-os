593275a984c2d84ee35cce75a09b40de00c0c3bc
Alexander Cramb <alex.cramb@r3.com>
2023-07-05 14:27:05 +0100

    Simplify cache view class

--- components/interop/interop-identity-cache-service/src/main/kotlin/net/corda/interop/identity/cache/InteropIdentityCacheView.kt
+++ components/interop/interop-identity-cache-service/src/main/kotlin/net/corda/interop/identity/cache/InteropIdentityCacheView.kt
@@ -1,44 +1,51 @@
 package net.corda.interop.identity.cache
 
-import net.corda.data.interop.InteropIdentity
+import java.util.*
+import kotlin.collections.HashMap
+import kotlin.collections.HashSet
 
 
 /**
- * This class represents the virtual-node local state of interop identities known to a specific holding identity.
+ * This class represents the state of the interop configuration visible to a specific virtual node.
  */
 class InteropIdentityCacheView(private val holdingIdentityShortHash: String) {
     private val interopIdentities = HashSet<InteropIdentityCacheEntry>()
-    private var myInteropIdentity: InteropIdentityCacheEntry? = null
 
     private val byGroupId = HashMap<String, HashSet<InteropIdentityCacheEntry>>()
+    private val byHoldingIdentity = HashMap<String, HashSet<InteropIdentityCacheEntry>>()
 
-
-    private fun getGroupIdentities(groupId: String): HashSet<InteropIdentityCacheEntry> {
-        if (!byGroupId.containsKey(groupId)) {
-            byGroupId[groupId] = HashSet()
+    private fun identitiesByGroupId(groupId: String): HashSet<InteropIdentityCacheEntry> {
+        return byGroupId.computeIfAbsent(groupId) {
+            HashSet()
         }
+    }
 
-        return byGroupId[groupId]!!
+
+    private fun identitiesByHoldingIdentity(shortHash: String): HashSet<InteropIdentityCacheEntry> {
+        return byHoldingIdentity.computeIfAbsent(shortHash) {
+            HashSet()
+        }
     }
 
 
     fun addIdentity(identity: InteropIdentityCacheEntry) {
         interopIdentities.add(identity)
-
-        if (identity.holdingIdentityShortHash == holdingIdentityShortHash) {
-            myInteropIdentity = identity
-        }
-
-        getGroupIdentities(identity.groupId).add(identity)
+        identitiesByGroupId(identity.groupId).add(identity)
+        identitiesByHoldingIdentity(identity.holdingIdentityShortHash).add(identity)
     }
 
 
     fun removeIdentity(identity: InteropIdentityCacheEntry) {
         interopIdentities.remove(identity)
-        getGroupIdentities(identity.groupId).remove(identity)
+        identitiesByGroupId(identity.groupId).remove(identity)
+        identitiesByHoldingIdentity(identity.holdingIdentityShortHash).remove(identity)
     }
 
 
-    fun getIdentities() = interopIdentities
-    fun getIdentities(groupId: String) = getGroupIdentities(groupId)
+    /** Separate public methods to return immutable containers. */
+    fun getIdentities(): Set<InteropIdentityCacheEntry> = interopIdentities
+    fun getIdentitiesByGroupId(): Map<String, Set<InteropIdentityCacheEntry>> =
+        Collections.unmodifiableMap(byGroupId)
+    fun getIdentitiesByHoldingIdentity(): Map<String, Set<InteropIdentityCacheEntry>> =
+        Collections.unmodifiableMap(byHoldingIdentity)
 }
