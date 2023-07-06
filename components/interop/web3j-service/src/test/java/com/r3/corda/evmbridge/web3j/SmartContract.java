cc2bfa6b7bfd0f7eaedcaa484ad52d661057337c
Ryan Fowler <ryan.fowler@r3.com>
2023-07-06 15:55:41 +0100

    Just a stab/spike for testing smart contracts at the flow level

--- components/interop/web3j-service/src/test/java/com/r3/corda/evmbridge/web3j/SmartContract.java
+++ components/interop/web3j-service/src/test/java/com/r3/corda/evmbridge/web3j/SmartContract.java
@@ -1,8 +1,56 @@
 package com.r3.corda.evmbridge.web3j;
 
-import java.util.Map;
+import java.util.List;
 
 public interface SmartContract {
+    class Input {
+        public static class Builder {
+            public String name;
+            public String type;
+            public Object value;
+
+            public Builder name(String name) {
+                this.name = name;
+                return this;
+            }
+
+            public Builder type(String type) {
+                this.type = type;
+                return this;
+            }
+
+            public Builder value(Object value) {
+                this.value = value;
+                return this;
+            }
+
+            public Input build() {
+                if (name == null || type == null || value == null) {
+                    throw new IllegalArgumentException("Not all values for Input specified");
+                }
+                return new Input(name, type, value);
+            }
+        }
+
+        private Input(String name, String type, Object value) {
+            this.name = name;
+            this.type = type;
+            this.value = value;
+        }
+        public String name;
+        public String type;
+        public Object value;
+
+        @Override
+        public String toString() {
+            return "Input{" +
+                    "name='" + name + '\'' +
+                    ", type='" + type + '\'' +
+                    ", value=" + value +
+                    '}';
+        }
+    }
+
     void execute(String functionName);
-    void execute(String functionName, Map<String, Object> parameters);
+    void execute(String functionName, List<Input> parameters);
 }
--- components/interop/web3j-service/src/test/kotlin/com/r3/corda/evmbridge/web3j/ContractFunction.kt
+++ components/interop/web3j-service/src/test/kotlin/com/r3/corda/evmbridge/web3j/ContractFunction.kt
@@ -1,22 +1,26 @@
 package com.r3.corda.evmbridge.web3j
 
-import org.web3j.abi.TypeReference
-import org.web3j.abi.datatypes.*
-import org.web3j.abi.datatypes.Function
-import org.web3j.abi.datatypes.generated.Uint256
 import java.math.BigInteger
+import org.web3j.abi.TypeReference
+import org.web3j.abi.datatypes.Address
+import org.web3j.abi.datatypes.Bool
+import org.web3j.abi.datatypes.DynamicStruct
+import org.web3j.abi.datatypes.Function
+import org.web3j.abi.datatypes.Type
+import org.web3j.abi.datatypes.Utf8String
+import org.web3j.abi.datatypes.generated.Uint256
 
 data class ContractFunction(
     val methodName: String,
     val transaction: Boolean,
-    val inputs: List<ContractFunctionInput>,
+    val inputs: List<SmartContract.Input>,
     val outputs: List<ContractFunctionOutput>
 )
 
 data class ContractFunctionInput(
     val name: String,
     val type: String,
-    val value: String
+    val value: Any?
 )
 
 data class ContractFunctionOutput(
@@ -30,14 +34,18 @@ fun ContractFunction.generateWeb3JFunction(): Function {
     return Function(functionName, inputs, outputs)
 }
 
-fun generateInputs(inputs: List<ContractFunctionInput>): List<Type<*>> {
+@Suppress("UNCHECKED_CAST")
+fun generateInputs(inputs: List<SmartContract.Input>): List<Type<*>> {
     return inputs.map {
         when(it.type) {
-            "uint256" -> Uint256(it.value.toBigInteger())
-            "bool" -> Bool(it.value.toBoolean())
-            "address" -> Address(it.value)
-            "string" -> Utf8String(it.value)
-            else -> throw IllegalArgumentException("Invalid or unsupported EVM data type ${it.type}")
+            "uint256" -> Uint256(it.value as Long)
+            "bool" -> Bool(it.value as Boolean)
+            "address" -> Address(it.value as String)
+            "string" -> Utf8String(it.value as String)
+            "tuple" -> {
+                DynamicStruct(generateInputs(it.value as List<SmartContract.Input>))
+            }
+            else -> throw IllegalArgumentException("Invalid or unsupported EVM data type ${it.type}: $it")
         }
     }
 }
@@ -49,6 +57,9 @@ fun generateOutputs(outputs: List<ContractFunctionOutput>): List<TypeReference<o
             "bool" -> object : TypeReference<Bool>() {}
             "address" -> object: TypeReference<Address>() {}
             "string" -> object : TypeReference<Utf8String>() {}
+            "tuple" -> {
+                object : TypeReference<DynamicStruct>() {} //(generateInputs(it.value as List<SmartContract.Input>))
+            }
             else -> throw IllegalArgumentException("Invalid or unsupported EVM data type ${it.type}")
         }
     }
