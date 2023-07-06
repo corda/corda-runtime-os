3463d1403edf85481b10202874875d2024c11fa1
nkovacsx <nandor.kovacs@r3.com>
2023-07-06 13:35:37 +0100

    add kdocs

--- libs/flows/flow-api/src/main/kotlin/net/corda/flow/application/services/FlowConfigService.kt
+++ libs/flows/flow-api/src/main/kotlin/net/corda/flow/application/services/FlowConfigService.kt
@@ -5,6 +5,14 @@ import net.corda.v5.base.annotations.DoNotImplement
 
 @DoNotImplement
 interface FlowConfigService {
+
+    /**
+     * Retrieves all the available configs from the flow context.
+     */
     fun getFlowConfigs(): Map<String, SmartConfig>
+
+    /**
+     * Retrieves the ledger config from the flow context if available.
+     */
     fun getLedgerConfig(): SmartConfig?
 }
