55cc3b4fb27bb9302af3d48359fec30fbdf69337
r3-arish <arish.tripathi@r3.com>
2023-07-08 23:59:17 +0100

    changes have addressed the comments on the PR

--- tools/plugins/network/src/main/kotlin/net/corda/cli/plugins/network/MemberList.kt
+++ tools/plugins/network/src/main/kotlin/net/corda/cli/plugins/network/MemberList.kt
@@ -6,81 +6,103 @@ import net.corda.membership.rest.v1.MemberLookupRestResource
 import net.corda.membership.rest.v1.types.response.RestMemberInfo
 import picocli.CommandLine
 
-@CommandLine.Command(name = "members", description = ["List members"])
+data class MembersListOptions(
+    val holdingIdentityShortHash: String?,
+    val commonName: String?,
+    val organizationUnit: String?,
+    val organization: String?,
+    val locality: String?,
+    val state: String?,
+    val country: String?
+)
+
+@CommandLine.Command(name = "members-list", description = ["Shows the list of members on the network."])
 class MemberList : RestCommand(), Runnable {
     override fun run() {
-        // Call the `getMembersList` function with default or empty values for the options
-        getMembersList(
-            holdingIdentityShortHash = null,
-            commonName = null,
-            organizationUnit = null,
-            organization = null,
-            locality = null,
-            state = null,
-            country = null
+        val options = MembersListOptions(
+            holdingIdentityShortHash = holdingIdentityShortHash,
+            commonName = commonName,
+            organizationUnit = organizationUnit,
+            organization = organization,
+            locality = locality,
+            state = state,
+            country = country
         )
+        val result = performMembersLookup(options)
+        println(result)
     }
 
-    private fun getMembersList(
-        @CommandLine.Option(
-            names = ["-h", "--holding-identity-short-hash"],
-            arity = "1",
-            description = ["Short hash of the holding identity to be checked."]
-        ) holdingIdentityShortHash: String?,
-        @CommandLine.Option(
-            names = ["-cn"],
-            arity = "0..1",
-            description = ["Optional. Common Name (CN) attribute of the X.500 name to filter members by."]
-        ) commonName: String?,
-        @CommandLine.Option(
-            names = ["-ou"],
-            arity = "0..1",
-            description = ["Optional. Organization Unit (OU) attribute of the X.500 name to filter members by."]
-        ) organizationUnit: String?,
-        @CommandLine.Option(
-            names = ["-o"],
-            arity = "0..1",
-            description = ["Optional. Organization (O) attribute of the X.500 name to filter members by."]
-        ) organization: String?,
-        @CommandLine.Option(
-            names = ["-l"],
-            arity = "0..1",
-            description = ["Optional. Locality (L) attribute of the X.500 name to filter members by."]
-        ) locality: String?,
-        @CommandLine.Option(
-            names = ["-st"],
-            arity = "0..1",
-            description = ["Optional. State (ST) attribute of the X.500 name to filter members by."]
-        ) state: String?,
-        @CommandLine.Option(
-            names = ["-c"],
-            arity = "0..1",
-            description = ["Optional. Country (C) attribute of the X.500 name to filter members by."]
-        ) country: String?
-    ) {
-        require(holdingIdentityShortHash != null) { "Holding identity short hash was not provided." }
+    private fun performMembersLookup(options: MembersListOptions): List<RestMemberInfo> {
+        require(options.holdingIdentityShortHash != null) { "Holding identity short hash was not provided." }
 
-        var result: List<RestMemberInfo>
-        createRestClient(MemberLookupRestResource::class).use {
-            val connection = it.start()
+        createRestClient(MemberLookupRestResource::class).use { restClient ->
+            val connection = restClient.start()
             with(connection.proxy) {
                 try {
-                    result = lookup(
-                        holdingIdentityShortHash,
-                        commonName,
-                        organization,
-                        organizationUnit,
-                        locality,
-                        state,
-                        country
+                    return lookup(
+                        options.holdingIdentityShortHash,
+                        options.commonName,
+                        options.organization,
+                        options.organizationUnit,
+                        options.locality,
+                        options.state,
+                        options.country
                     ).members
                 } catch (e: Exception) {
                     println(e.message)
                     NetworkPluginWrapper.logger.error(e.stackTrace.toString())
-                    return
                 }
             }
         }
-        println(result)
+        return emptyList()
     }
+
+    @CommandLine.Option(
+        names = ["-h", "--holding-identity-short-hash"],
+        arity = "1",
+        description = ["Short hash of the holding identity to be checked."]
+    )
+    private var holdingIdentityShortHash: String? = null
+
+    @CommandLine.Option(
+        names = ["-cn"],
+        arity = "0..1",
+        description = ["Optional. Common Name (CN) attribute of the X.500 name to filter members by."]
+    )
+    private var commonName: String? = null
+
+    @CommandLine.Option(
+        names = ["-ou"],
+        arity = "0..1",
+        description = ["Optional. Organization Unit (OU) attribute of the X.500 name to filter members by."]
+    )
+    private var organizationUnit: String? = null
+
+    @CommandLine.Option(
+        names = ["-o"],
+        arity = "0..1",
+        description = ["Optional. Organization (O) attribute of the X.500 name to filter members by."]
+    )
+    private var organization: String? = null
+
+    @CommandLine.Option(
+        names = ["-l"],
+        arity = "0..1",
+        description = ["Optional. Locality (L) attribute of the X.500 name to filter members by."]
+    )
+    private var locality: String? = null
+
+    @CommandLine.Option(
+        names = ["-st"],
+        arity = "0..1",
+        description = ["Optional. State (ST) attribute of the X.500 name to filter members by."]
+    )
+    private var state: String? = null
+
+    @CommandLine.Option(
+        names = ["-c"],
+        arity = "0..1",
+        description = ["Optional. Country (C) attribute of the X.500 name to filter members by."]
+    )
+    private var country: String? = null
 }
--- tools/plugins/network/src/main/kotlin/net/corda/cli/plugins/network/README.md
+++ tools/plugins/network/src/main/kotlin/net/corda/cli/plugins/network/README.md
@@ -151,5 +151,5 @@ This is a sub-command under the `network` plugin to view the member list via HTT
 For example,
 
 ```shell
-./corda-cli.sh network members --user=admin --password=admin --target=https://localhost:8888 --insecure members-list -h=<holding-identity-short-hash>
+./corda-cli.sh network members-list --user=admin --password=admin --target=https://localhost:8888 --insecure members-list -h=<holding-identity-short-hash>
 ```
