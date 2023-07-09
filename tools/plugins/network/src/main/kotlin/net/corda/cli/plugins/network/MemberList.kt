ce0ab9459a2209919f85b4a4dc82fb3bf985a6e1
r3-arish <arish.tripathi@r3.com>
2023-07-09 16:02:02 +0100

    commit addressed comments in the PR

--- tools/plugins/network/src/main/kotlin/net/corda/cli/plugins/network/MemberList.kt
+++ tools/plugins/network/src/main/kotlin/net/corda/cli/plugins/network/MemberList.kt
@@ -6,55 +6,35 @@ import net.corda.membership.rest.v1.MemberLookupRestResource
 import net.corda.membership.rest.v1.types.response.RestMemberInfo
 import picocli.CommandLine
 
-data class MembersListOptions(
-    val holdingIdentityShortHash: String?,
-    val commonName: String?,
-    val organizationUnit: String?,
-    val organization: String?,
-    val locality: String?,
-    val state: String?,
-    val country: String?
-)
-
 @CommandLine.Command(name = "members-list", description = ["Shows the list of members on the network."])
 class MemberList : RestCommand(), Runnable {
     override fun run() {
-        val options = MembersListOptions(
-            holdingIdentityShortHash = holdingIdentityShortHash,
-            commonName = commonName,
-            organizationUnit = organizationUnit,
-            organization = organization,
-            locality = locality,
-            state = state,
-            country = country
-        )
-        val result = performMembersLookup(options)
-        println(result)
+        performMembersLookup()
     }
-
-    private fun performMembersLookup(options: MembersListOptions): List<RestMemberInfo> {
-        require(options.holdingIdentityShortHash != null) { "Holding identity short hash was not provided." }
-
+    private fun performMembersLookup() {
+        require(holdingIdentityShortHash != null) { "Holding identity short hash was not provided." }
+        var result: List<RestMemberInfo>
         createRestClient(MemberLookupRestResource::class).use { restClient ->
             val connection = restClient.start()
             with(connection.proxy) {
                 try {
-                    return lookup(
-                        options.holdingIdentityShortHash,
-                        options.commonName,
-                        options.organization,
-                        options.organizationUnit,
-                        options.locality,
-                        options.state,
-                        options.country
+                    result = lookup(
+                        holdingIdentityShortHash.toString(),
+                        commonName,
+                        organization,
+                        organizationUnit,
+                        locality,
+                        state,
+                        country
                     ).members
                 } catch (e: Exception) {
                     println(e.message)
                     NetworkPluginWrapper.logger.error(e.stackTrace.toString())
+                    return
                 }
             }
         }
-        return emptyList()
+        println(result)
     }
 
     @CommandLine.Option(
@@ -62,47 +42,47 @@ class MemberList : RestCommand(), Runnable {
         arity = "1",
         description = ["Short hash of the holding identity to be checked."]
     )
-    private var holdingIdentityShortHash: String? = null
+    var holdingIdentityShortHash: String? = null
 
     @CommandLine.Option(
         names = ["-cn"],
         arity = "0..1",
         description = ["Optional. Common Name (CN) attribute of the X.500 name to filter members by."]
     )
-    private var commonName: String? = null
+    var commonName: String? = null
 
     @CommandLine.Option(
         names = ["-ou"],
         arity = "0..1",
         description = ["Optional. Organization Unit (OU) attribute of the X.500 name to filter members by."]
     )
-    private var organizationUnit: String? = null
+    var organizationUnit: String? = null
 
     @CommandLine.Option(
         names = ["-o"],
         arity = "0..1",
         description = ["Optional. Organization (O) attribute of the X.500 name to filter members by."]
     )
-    private var organization: String? = null
+    var organization: String? = null
 
     @CommandLine.Option(
         names = ["-l"],
         arity = "0..1",
         description = ["Optional. Locality (L) attribute of the X.500 name to filter members by."]
     )
-    private var locality: String? = null
+    var locality: String? = null
 
     @CommandLine.Option(
         names = ["-st"],
         arity = "0..1",
         description = ["Optional. State (ST) attribute of the X.500 name to filter members by."]
     )
-    private var state: String? = null
+    var state: String? = null
 
     @CommandLine.Option(
         names = ["-c"],
         arity = "0..1",
         description = ["Optional. Country (C) attribute of the X.500 name to filter members by."]
     )
-    private var country: String? = null
+    var country: String? = null
 }
