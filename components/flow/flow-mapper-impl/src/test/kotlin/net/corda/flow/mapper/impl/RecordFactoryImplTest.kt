ce5bb877cf8ffcf01861d3e49dec14bd45efefee
Emily Bowe <emily.bowe@r3.com>
2023-07-12 14:00:05 +0100

    adding new test class and testing OUTBOUND and LOCAL

---
+++ components/flow/flow-mapper-impl/src/test/kotlin/net/corda/flow/mapper/impl/RecordFactoryImplTest.kt
@@ -0,0 +1,137 @@
+package net.corda.flow.mapper.impl
+
+import com.typesafe.config.ConfigValueFactory
+import net.corda.avro.serialization.CordaAvroSerializationFactory
+import net.corda.data.ExceptionEnvelope
+import net.corda.data.flow.event.MessageDirection
+import net.corda.data.flow.event.SessionEvent
+import net.corda.data.flow.event.mapper.FlowMapperEvent
+import net.corda.data.flow.event.session.SessionData
+import net.corda.data.flow.event.session.SessionError
+import net.corda.data.identity.HoldingIdentity
+import net.corda.libs.configuration.SmartConfigImpl
+import net.corda.membership.locally.hosted.identities.LocallyHostedIdentitiesService
+import net.corda.schema.configuration.FlowConfig
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.mockito.kotlin.any
+import org.mockito.kotlin.anyOrNull
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.whenever
+import java.time.Instant
+
+internal class RecordFactoryImplTest {
+
+    private lateinit var recordFactoryImpl: RecordFactoryImpl
+
+    @BeforeEach
+    fun setup() {
+        val cordaAvroSerializationFactory: CordaAvroSerializationFactory = mock()
+        whenever(cordaAvroSerializationFactory.createAvroSerializer<SessionEvent>(anyOrNull())).thenReturn(mock())
+        val locallyHostedIdentitiesService: LocallyHostedIdentitiesService = mock()
+        whenever(locallyHostedIdentitiesService.getIdentityInfo(any())).thenReturn(mock())
+        recordFactoryImpl = RecordFactoryImpl(cordaAvroSerializationFactory, locallyHostedIdentitiesService)
+    }
+    private val flowConfig = SmartConfigImpl.empty().withValue(FlowConfig.SESSION_P2P_TTL, ConfigValueFactory.fromAnyRef(10000))
+
+    @Test
+    fun `forwardError returns record`() {
+        val sessionEvent = SessionEvent(
+            MessageDirection.OUTBOUND,
+            Instant.now(), "", 1,
+            HoldingIdentity("CN=Alice, O=Alice Corp, L=LDN, C=GB", "1"),
+            HoldingIdentity("CN=Bob, O=Bob Corp, L=LDN, C=GB", "1"),
+            0,
+            listOf(),
+            SessionError(
+                ExceptionEnvelope(
+                    "FlowMapper-SessionError",
+                    "Received SessionError with sessionId 1"
+                )
+            )
+        )
+
+        val record = recordFactoryImpl.forwardError(
+            sessionEvent,
+            ExceptionEnvelope(
+            "FlowMapper-SessionError",
+            "Received SessionError with sessionId 1"),
+            Instant.now(),
+            flowConfig,
+            sessionEvent.messageDirection
+        )
+        Assertions.assertThat(record).isNotNull
+        Assertions.assertThat(record.topic).isEqualTo("flow.mapper.event")
+        Assertions.assertThat(record.key).isEqualTo("-INITIATED")
+        Assertions.assertThat(record.value!!::class).isEqualTo(FlowMapperEvent::class)
+    }
+
+    @Test
+    fun `forwardEvent returns record`() {
+        val sessionEvent = SessionEvent(
+            MessageDirection.OUTBOUND,
+            Instant.now(), "", 1,
+            HoldingIdentity("CN=Alice, O=Alice Corp, L=LDN, C=GB", "1"),
+            HoldingIdentity("CN=Bob, O=Bob Corp, L=LDN, C=GB", "1"),
+            0,
+            listOf(),
+            SessionData()
+        )
+
+        val record = recordFactoryImpl.forwardEvent(
+            sessionEvent,
+            Instant.now(),
+            flowConfig,
+            sessionEvent.messageDirection
+        )
+        Assertions.assertThat(record).isNotNull
+        Assertions.assertThat(record.topic).isEqualTo("flow.mapper.event")
+        Assertions.assertThat(record.key).isEqualTo("-INITIATED")
+        Assertions.assertThat(record.value!!::class).isEqualTo(FlowMapperEvent::class)
+    }
+
+    @Test
+    fun `forwardAck returns record`() {
+        val sessionEvent = SessionEvent(
+            MessageDirection.OUTBOUND,
+            Instant.now(), "", 1,
+            HoldingIdentity("CN=Alice, O=Alice Corp, L=LDN, C=GB", "1"),
+            HoldingIdentity("CN=Bob, O=Bob Corp, L=LDN, C=GB", "1"),
+            0,
+            listOf(),
+            SessionData()
+        )
+
+        val record = recordFactoryImpl.forwardAck(
+            sessionEvent,
+            Instant.now(),
+            flowConfig,
+            sessionEvent.messageDirection
+        )
+        Assertions.assertThat(record).isNotNull
+        Assertions.assertThat(record.topic).isEqualTo("flow.mapper.event")
+        Assertions.assertThat(record.key).isEqualTo("-INITIATED")
+        Assertions.assertThat(record.value!!::class).isEqualTo(FlowMapperEvent::class)
+    }
+
+    @Test
+    fun `getSessionEventOutputTopic returns topic`() {
+        val sessionEvent = SessionEvent(
+            MessageDirection.OUTBOUND,
+            Instant.now(), "", 1,
+            HoldingIdentity("CN=Alice, O=Alice Corp, L=LDN, C=GB", "1"),
+            HoldingIdentity("CN=Bob, O=Bob Corp, L=LDN, C=GB", "1"),
+            0,
+            listOf(),
+            SessionData()
+        )
+
+        val topic = recordFactoryImpl.getSessionEventOutputTopic(
+            sessionEvent,
+            sessionEvent.messageDirection
+        )
+        Assertions.assertThat(topic).isNotNull
+        Assertions.assertThat(topic).isEqualTo("flow.mapper.event")
+    }
+}
