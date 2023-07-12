38cf52528c5ddba01548ebab8cb4513c018c4ca7
Peter McKinney <pmck91@users.noreply.github.com>
2023-07-12 10:43:10 +0100

    code review

--- components/flow/flow-service/src/main/kotlin/net/corda/flow/pipeline/impl/FlowEventExceptionProcessorImpl.kt
+++ components/flow/flow-service/src/main/kotlin/net/corda/flow/pipeline/impl/FlowEventExceptionProcessorImpl.kt
@@ -136,7 +136,7 @@ class FlowEventExceptionProcessorImpl @Activate constructor(
             )
         }
 
-        val errorEvents = flowSessionManager.getSessionErrorEventRecords(context.checkpoint, context.config, exceptionHandlingStartTime)
+        val errorEvents = flowSessionManager.getSessionErrorEventRecords(checkpoint, context.config, exceptionHandlingStartTime)
         val cleanupEvents = createCleanupEventsForSessions(
             getScheduledCleanupExpiryTime(context, exceptionHandlingStartTime),
             checkpoint.sessions.filterNot { it.hasScheduledCleanup }
