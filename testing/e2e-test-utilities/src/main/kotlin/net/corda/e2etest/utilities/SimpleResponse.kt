2d60c00fa2cc2fb5cb64263acc5b41777113e963
Viktor Kolomeyko <viktor.kolomeyko@r3.com>
2023-07-14 13:36:27 +0100

    CORE-13377: Add headers to `SimpleResponse` for E2Es (#4303)

--- testing/e2e-test-utilities/src/main/kotlin/net/corda/e2etest/utilities/SimpleResponse.kt
+++ testing/e2e-test-utilities/src/main/kotlin/net/corda/e2etest/utilities/SimpleResponse.kt
@@ -3,7 +3,10 @@ package net.corda.e2etest.utilities
 import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
 
+/** Needs to handle duplicates in keys, hence cannot use `Map` */
+typealias Headers = List<Pair<String, String>>
+
 /** Simplified response in case we switch underlying web clients, again */
-data class SimpleResponse(val code: Int, val body: String, val url: String) {
+data class SimpleResponse(val code: Int, val body: String, val url: String, val headers: Headers) {
     fun toJson(): JsonNode = ObjectMapper().readTree(this.body)!!
 }
