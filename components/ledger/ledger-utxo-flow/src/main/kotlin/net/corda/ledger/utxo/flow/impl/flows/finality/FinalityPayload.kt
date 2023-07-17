286dc7f5e89e048df433b7c4d817a456993b9bb4
eunjee.yang <eunjee.yang@r3.com>
2023-07-17 17:40:55 +0100

    CORE-13937: fix wrong error message

--- components/ledger/ledger-utxo-flow/src/main/kotlin/net/corda/ledger/utxo/flow/impl/flows/finality/FinalityPayload.kt
+++ components/ledger/ledger-utxo-flow/src/main/kotlin/net/corda/ledger/utxo/flow/impl/flows/finality/FinalityPayload.kt
@@ -7,6 +7,17 @@ import net.corda.v5.base.annotations.ConstructorForDeserialization
 import net.corda.v5.base.annotations.CordaSerializable
 import net.corda.v5.base.exceptions.CordaRuntimeException
 
+/**
+ * Payload for V2 for [UtxoFinalityFlowV1] and [UtxoReceiveFinalityFlowV1] on the first interaction.
+ * It is to
+ * - serialize and deserialize on the interaction
+ * - safely add more field into the payload in the future
+ * - deserialize only required field
+ *
+ * @constructor Creates a new instance of [FinalityPayload] data class
+ * @property initialTransaction [UtxoSignedTransactionInternal]
+ * @property transferAdditionalSignatures [Boolean]
+*/
 @CordaSerializable
 data class FinalityPayload @ConstructorForDeserialization constructor(
     val map: Map<String, ByteArray>,
@@ -41,7 +52,7 @@ data class FinalityPayload @ConstructorForDeserialization constructor(
             serializationService.deserialize<Boolean>(map[TRANSFER_ADDITIONAL_SIGNATURES]!!)
         } catch (e: Exception) {
             throw CordaRuntimeException(
-                "Deserialization of ${map[INITIAL_TRANSACTION]} into UtxoSignedTransactionInternal failed.", e)
+                "Deserialization of ${map[TRANSFER_ADDITIONAL_SIGNATURES]} into Boolean failed.", e)
         }
     }
 }
