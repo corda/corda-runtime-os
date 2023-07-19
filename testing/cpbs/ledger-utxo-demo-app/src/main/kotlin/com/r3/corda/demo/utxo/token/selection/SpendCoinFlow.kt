9becab23a8d784052aebe0330f3d6ef040c199ac
Filipe Oliveira <filipe.oliveira@r3.com>
2023-07-19 14:59:49 +0100

    Updated the code to actually spend a coin instead of just pretending.

--- testing/cpbs/ledger-utxo-demo-app/src/main/kotlin/com/r3/corda/demo/utxo/token/selection/SpendCoinFlow.kt
+++ testing/cpbs/ledger-utxo-demo-app/src/main/kotlin/com/r3/corda/demo/utxo/token/selection/SpendCoinFlow.kt
@@ -1,6 +1,7 @@
 package com.r3.corda.demo.utxo.token.selection
 
 import com.r3.corda.demo.utxo.contract.CoinState
+import com.r3.corda.demo.utxo.contract.TestCommand
 import net.corda.v5.application.flows.CordaInject
 import net.corda.v5.application.flows.InitiatingFlow
 import net.corda.v5.application.marshalling.JsonMarshallingService
@@ -9,10 +10,16 @@ import net.corda.v5.ledger.common.NotaryLookup
 import net.corda.v5.ledger.utxo.token.selection.TokenClaimCriteria
 import net.corda.v5.ledger.utxo.token.selection.TokenSelection
 import java.math.BigDecimal
+import java.time.Instant
+import java.util.concurrent.TimeUnit
 import net.corda.v5.application.crypto.DigestService
 import net.corda.v5.application.flows.ClientRequestBody
 import net.corda.v5.application.flows.ClientStartableFlow
+import net.corda.v5.application.membership.MemberLookup
+import net.corda.v5.application.messaging.FlowMessaging
+import net.corda.v5.base.types.MemberX500Name
 import net.corda.v5.crypto.DigestAlgorithmName
+import net.corda.v5.ledger.utxo.UtxoLedgerService
 import org.slf4j.LoggerFactory
 
 @InitiatingFlow(protocol = "utxo-spend-coin-protocol")
@@ -21,6 +28,10 @@ class SpendCoinFlow : ClientStartableFlow {
     private companion object {
         val log = LoggerFactory.getLogger(this::class.java.enclosingClass)
     }
+
+
+    @CordaInject
+    lateinit var flowMessaging: FlowMessaging
 
     @CordaInject
     lateinit var jsonMarshallingService: JsonMarshallingService
@@ -33,6 +44,12 @@ class SpendCoinFlow : ClientStartableFlow {
 
     @CordaInject
     lateinit var digestService: DigestService
+
+    @CordaInject
+    lateinit var utxoLedgerService: UtxoLedgerService
+
+    @CordaInject
+    lateinit var memberLookup: MemberLookup
 
     @Suspendable
     override fun call(requestBody: ClientRequestBody): String {
@@ -66,6 +83,50 @@ class SpendCoinFlow : ClientStartableFlow {
             )
 
             val spentCoins = tokenClaim.claimedTokens.take(spendRequest.maxCoinsToUse).map { it.stateRef }
+
+//            val notaryKey = memberLookup.lookup().single {
+//                it.memberProvidedContext["corda.notary.service.name"] == notary.commonName
+//            }.ledgerKeys.first()
+
+            val me = memberLookup.myInfo()
+            val spendCoinMessage = requestBody.getRequestBodyAs(jsonMarshallingService, SpendCoinMessage::class.java)
+            val bankX500 = MemberX500Name.parse(spendCoinMessage.issuerBankX500)
+
+            val bank = requireNotNull(memberLookup.lookup(bankX500)) {
+                "Member $bankX500 does not exist in the membership group"
+            }
+
+            val participants = listOf(me.ledgerKeys.first(), bank.ledgerKeys.first())
+
+            log.info("Creating transaction...")
+            val txBuilder = utxoLedgerService.createTransactionBuilder()
+
+            @Suppress("DEPRECATION")
+            val signedTransaction = txBuilder
+                .setNotary(notary.name)
+                .setTimeWindowBetween(Instant.now(), Instant.now().plusMillis(TimeUnit.DAYS.toMillis(1)))
+                .addInputStates(spentCoins)
+                .addCommand(TestCommand())
+                .addSignatories(participants)
+                .toSignedTransaction()
+
+            val bankSession = flowMessaging.initiateFlow(bank.name)
+
+            val ret = try {
+                val finalizedSignedTransaction = utxoLedgerService.finalize(
+                    signedTransaction,
+                    listOf(bankSession)
+                )
+
+                log.info("Created and finalised transaction with id='${finalizedSignedTransaction.transaction.id}'")
+                finalizedSignedTransaction.transaction.id.toString()
+            } catch (e: Exception) {
+                log.warn("Finality failed", e)
+                "Finality failed, ${e.message}"
+            }
+
+            log.info("Return value: $ret")
+
             val coinsToRelease =
                 tokenClaim.claimedTokens.drop(spendRequest.maxCoinsToUse).map { it.stateRef.toString() }
 
