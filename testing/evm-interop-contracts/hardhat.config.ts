bf000b03b14f4589956469d5272c85752018f29f
Ignace Loomans <ignace.loomans@gmail.com>
2023-07-25 09:13:44 +0100

    removed readme + changed rpc address

--- testing/evm-interop-contracts/hardhat.config.ts
+++ testing/evm-interop-contracts/hardhat.config.ts
@@ -5,7 +5,7 @@ const config: HardhatUserConfig = {
   solidity: "0.8.18",
   networks: {
     besu: {
-      url: "http://127.0.0.1/rpc",
+      url: "http://127.0.0.1:8545",
       accounts: [
         "0x8f2a55949038a9610f50fb23b5883af3b4ecb3c3bb792cbcefbd1542c692be63",
         "0xc87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3",
