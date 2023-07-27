b9451c52bfd3bba18c289cf7c308522f6ee85e74
Miljenko Brkic <miljenko.brkic@r3.com>
2023-07-27 10:50:21 +0200

    CORE-15584 Customizing Kryo with DefaultKryoCustomizer in unit test

--- libs/serialization/serialization-kryo/src/test/kotlin/net/corda/kryoserialization/serializers/InputStreamSerializerTest.kt
+++ libs/serialization/serialization-kryo/src/test/kotlin/net/corda/kryoserialization/serializers/InputStreamSerializerTest.kt
@@ -3,21 +3,28 @@ package net.corda.kryoserialization.serializers
 import com.esotericsoftware.kryo.Kryo
 import com.esotericsoftware.kryo.io.Input
 import com.esotericsoftware.kryo.io.Output
-import org.assertj.core.api.Assertions
+import net.corda.kryoserialization.DefaultKryoCustomizer
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.mockito.kotlin.mock
 import java.io.InputStream
 
-internal class InputStreamSerializerTest {
+class InputStreamSerializerTest {
     @Test
     fun `InputStreamSerializer serializer returns the correct object back`() {
         val kryo = Kryo()
+        DefaultKryoCustomizer.customize(
+            kryo,
+            emptyMap(),
+            ClassSerializer(mock())
+        )
         val output = Output(100)
         val data = "test"
-        InputStreamSerializer.write(kryo, output, data.byteInputStream())
+        kryo.writeClassAndObject(output, data.byteInputStream())
 
-        val deserialized = InputStreamSerializer.read(kryo, Input(output.buffer), InputStream::class.java)
+        val deserialized = kryo.readClassAndObject(Input(output.buffer))
 
-        Assertions.assertThat(deserialized).isInstanceOf(InputStream::class.java)
-        Assertions.assertThat(deserialized).hasContent(data)
+        assertThat(deserialized).isInstanceOf(InputStream::class.java)
+        assertThat(deserialized as InputStream).hasContent(data)
     }
 }
