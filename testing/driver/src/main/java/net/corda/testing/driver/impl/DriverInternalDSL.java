5e83eef84285b1af3eec455246693c64644540b3
Chris Rankin <chris.rankin@r3.com>
2023-08-01 17:03:38 +0100

    CORE-14685: Fix driver bundle resolution as a standalone utility. (#4394)

--- testing/driver/src/main/java/net/corda/testing/driver/impl/DriverInternalDSL.java
+++ testing/driver/src/main/java/net/corda/testing/driver/impl/DriverInternalDSL.java
@@ -2,7 +2,6 @@ package net.corda.testing.driver.impl;
 
 import net.corda.testing.driver.DriverDSL;
 import net.corda.testing.driver.Framework;
-import net.corda.v5.base.types.MemberX500Name;
 import org.jetbrains.annotations.NotNull;
 
 /**
