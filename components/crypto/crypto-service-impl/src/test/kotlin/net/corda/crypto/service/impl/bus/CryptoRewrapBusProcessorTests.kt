7306d4a35be20cc769b617b19d03780e821c6692
Dickon Reed <dickon.reed@r3.com>
2023-08-04 09:25:44 +0100

    do a rewrap

--- components/crypto/crypto-service-impl/src/test/kotlin/net/corda/crypto/service/impl/bus/CryptoRewrapBusProcessorTests.kt
+++ components/crypto/crypto-service-impl/src/test/kotlin/net/corda/crypto/service/impl/bus/CryptoRewrapBusProcessorTests.kt
@@ -1,24 +1,29 @@
 package net.corda.crypto.service.impl.bus
 
 import net.corda.crypto.core.CryptoService
+import net.corda.data.crypto.wire.ops.rewrap.CryptoRewrapRequest
+import net.corda.messaging.api.records.Record
 import org.junit.jupiter.api.Test
 import org.mockito.kotlin.mock
 import java.util.UUID
 
-class CryptoRewrapBusProcessorTests{
+class CryptoRewrapBusProcessorTests {
     companion object {
         private val cryptoService: CryptoService = mock<CryptoService> { }
         private val cryptoRewrapBusProcessor = CryptoRewrapBusProcessor(cryptoService)
         private val tenantId = UUID.randomUUID().toString()
-        private val targetAlias = "alpha"
-        private val parentKeyAlias = "beta"
     }
 
     @Test
-    fun `do nothing`(){
-        //cryptoRewrapBusProcessor.onNext()
-
+    fun `do a mocked rewrap`() {
+        cryptoRewrapBusProcessor.onNext(
+            listOf(
+                Record(
+                    "TBC",
+                    UUID.randomUUID().toString(),
+                    CryptoRewrapRequest(tenantId, "alias1", "root2")
+                )
+            )
+        )
     }
-
-
 }
