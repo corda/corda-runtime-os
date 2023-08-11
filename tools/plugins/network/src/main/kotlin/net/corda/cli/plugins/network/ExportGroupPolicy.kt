6949257efa962cc311255fe764ea9ee944fc1d41
r3-arish <arish.tripathi@r3.com>
2023-08-11 14:13:42 +0100

    Add operate command

---
+++ tools/plugins/network/src/main/kotlin/net/corda/cli/plugins/network/ExportGroupPolicy.kt
@@ -0,0 +1,65 @@
+package net.corda.cli.plugins.network
+
+import net.corda.cli.plugins.common.RestClientUtils.createRestClient
+import net.corda.cli.plugins.common.RestCommand
+import net.corda.cli.plugins.network.utils.InvariantUtils.checkInvariant
+import net.corda.membership.rest.v1.MGMRestResource
+import picocli.CommandLine
+import java.io.File
+import com.fasterxml.jackson.databind.ObjectMapper
+
+@CommandLine.Command(
+    name = "export-group-policy",
+    description = ["Export the group policy from the MGM"]
+)
+class ExportGroupPolicy : Runnable, RestCommand() {
+    @CommandLine.Option(
+        names = ["--save"],
+        description = ["Save the exported group policy to a specific location"]
+    )
+    var saveLocation: String? = null
+
+    @CommandLine.Option(
+        names = ["-h", "--holding-identity-short-hash"],
+        arity = "1",
+        description = ["Short hash of the holding identity to be checked."]
+    )
+    var holdingIdentityShortHash: String? = null
+
+    override fun run() {
+        exportGroupPolicy()
+    }
+
+    private fun exportGroupPolicy() {
+        val objectMapper = ObjectMapper()
+        val groupPolicyResponse = createRestClient(MGMRestResource::class).use { client ->
+            checkInvariant(
+                maxAttempts = MAX_ATTEMPTS,
+                waitInterval = WAIT_INTERVAL,
+                errorMessage = "Failed to export group policy after $MAX_ATTEMPTS attempts."
+            ) {
+                try {
+                    val resource = client.start().proxy
+                    resource.generateGroupPolicy(holdingIdentityShortHash!!)
+                } catch (e: Exception) {
+                    null
+                }
+            }
+        }
+
+        val groupPolicyFile = saveLocation?.let { File(it) } ?: getDefaultGroupPolicyFile()
+        groupPolicyFile.parentFile.mkdirs()
+        objectMapper.writerWithDefaultPrettyPrinter()
+            .writeValue(
+                groupPolicyFile,
+                objectMapper.readTree(groupPolicyResponse)
+            )
+        println("Group policy exported and saved to: ${groupPolicyFile.absolutePath}")
+    }
+
+    private fun getDefaultGroupPolicyFile(): File {
+        val defaultLocation = File(System.getProperty("user.home"), ".corda/groupPolicy.json")
+        defaultLocation.parentFile.mkdirs()
+        return defaultLocation
+    }
+}
---
+++ tools/plugins/network/src/main/kotlin/net/corda/cli/plugins/network/Operate.kt
@@ -0,0 +1,20 @@
+package net.corda.cli.plugins.network
+
+import picocli.CommandLine.Command
+
+@Command(
+    name = "operate",
+    description = [
+        "Operate on the network",
+        "This sub command should only be used for internal development"
+    ],
+    subcommands = [
+        AllowClientCertificate::class,
+        ExportGroupPolicy::class
+    ]
+)
+class Operate : Runnable {
+    override fun run() {
+        // Implementation for the operate command
+    }
+}
