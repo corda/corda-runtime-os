ff7adc0842d60e0843d80ffaa88b62eaa5966106
Miljenko Brkic <miljenko.brkic@r3.com>
2023-08-15 13:25:22 +0100

    CORE-16230 Fixed runtime errors

--- applications/workers/release/uniqueness-worker/src/main/kotlin/net/corda/applications/workers/uniqueness/UniquenessWorker.kt
+++ applications/workers/release/uniqueness-worker/src/main/kotlin/net/corda/applications/workers/uniqueness/UniquenessWorker.kt
@@ -3,21 +3,27 @@ package net.corda.applications.workers.uniqueness
 import net.corda.applications.workers.workercommon.ApplicationBanner
 import net.corda.applications.workers.workercommon.DefaultWorkerParams
 import net.corda.applications.workers.workercommon.JavaSerialisationFilter
+import net.corda.applications.workers.workercommon.PathAndConfig
+import net.corda.applications.workers.workercommon.WorkerHelpers
 import net.corda.applications.workers.workercommon.WorkerHelpers.Companion.getParams
 import net.corda.applications.workers.workercommon.WorkerHelpers.Companion.loggerStartupInfo
 import net.corda.applications.workers.workercommon.WorkerHelpers.Companion.printHelpOrVersion
 import net.corda.applications.workers.workercommon.WorkerHelpers.Companion.setupMonitor
 import net.corda.applications.workers.workercommon.WorkerMonitor
+import net.corda.libs.configuration.secret.SecretsServiceFactoryResolver
+import net.corda.libs.configuration.validation.ConfigurationValidatorFactory
 import net.corda.libs.platform.PlatformInfoProvider
 import net.corda.osgi.api.Application
 import net.corda.osgi.api.Shutdown
 import net.corda.processors.uniqueness.UniquenessProcessor
+import net.corda.schema.configuration.BootConfig
 import net.corda.tracing.configureTracing
 import net.corda.tracing.shutdownTracing
 import org.osgi.service.component.annotations.Activate
 import org.osgi.service.component.annotations.Component
 import org.osgi.service.component.annotations.Reference
 import org.slf4j.LoggerFactory
+import picocli.CommandLine
 import picocli.CommandLine.Mixin
 
 /** The worker for uniqueness checks. */
@@ -34,6 +40,10 @@ class UniquenessWorker @Activate constructor(
     val platformInfoProvider: PlatformInfoProvider,
     @Reference(service = ApplicationBanner::class)
     val applicationBanner: ApplicationBanner,
+    @Reference(service = ConfigurationValidatorFactory::class)
+    private val configurationValidatorFactory: ConfigurationValidatorFactory,
+    @Reference(service = SecretsServiceFactoryResolver::class)
+    val secretsServiceFactoryResolver: SecretsServiceFactoryResolver,
 ) : Application {
 
     private companion object {
@@ -55,7 +65,15 @@ class UniquenessWorker @Activate constructor(
 
         configureTracing("Uniqueness Worker", params.defaultParams.zipkinTraceUrl, params.defaultParams.traceSamplesPerSecond)
 
-        uniquenessProcessor.start()
+        val databaseConfig = PathAndConfig(BootConfig.BOOT_DB, params.databaseParams)
+        val config = WorkerHelpers.getBootstrapConfig(
+            secretsServiceFactoryResolver,
+            params.defaultParams,
+            configurationValidatorFactory.createConfigValidator(),
+            listOf(databaseConfig)
+        )
+
+        uniquenessProcessor.start(config)
     }
 
     override fun shutdown() {
@@ -69,4 +87,7 @@ class UniquenessWorker @Activate constructor(
 private class UniquenessWorkerParams {
     @Mixin
     var defaultParams = DefaultWorkerParams()
+
+    @CommandLine.Option(names = ["-d", "--${BootConfig.BOOT_DB}"], description = ["Database parameters for the worker."])
+    var databaseParams = emptyMap<String, String>()
 }
--- processors/uniqueness-processor/src/main/kotlin/net/corda/processors/uniqueness/internal/UniquenessProcessorImpl.kt
+++ processors/uniqueness-processor/src/main/kotlin/net/corda/processors/uniqueness/internal/UniquenessProcessorImpl.kt
@@ -1,5 +1,10 @@
 package net.corda.processors.uniqueness.internal
 
+import net.corda.configuration.read.ConfigurationReadService
+import net.corda.db.connection.manager.DbConnectionManager
+import net.corda.db.schema.CordaDb
+import net.corda.libs.configuration.SmartConfig
+import net.corda.libs.configuration.datamodel.ConfigurationEntities
 import net.corda.lifecycle.DependentComponents
 import net.corda.lifecycle.LifecycleCoordinator
 import net.corda.lifecycle.LifecycleCoordinatorFactory
@@ -8,7 +13,9 @@ import net.corda.lifecycle.RegistrationStatusChangeEvent
 import net.corda.lifecycle.StartEvent
 import net.corda.lifecycle.StopEvent
 import net.corda.lifecycle.createCoordinator
+import net.corda.orm.JpaEntitiesRegistry
 import net.corda.processors.uniqueness.UniquenessProcessor
+import net.corda.schema.configuration.BootConfig.BOOT_DB
 import net.corda.uniqueness.checker.UniquenessCheckerLifecycle
 import org.osgi.service.component.annotations.Activate
 import org.osgi.service.component.annotations.Component
@@ -22,24 +29,37 @@ import org.slf4j.LoggerFactory
 class UniquenessProcessorImpl @Activate constructor(
     @Reference(service = LifecycleCoordinatorFactory::class)
     private val coordinatorFactory: LifecycleCoordinatorFactory,
+    @Reference(service = ConfigurationReadService::class)
+    private val configurationReadService: ConfigurationReadService,
+    @Reference(service = DbConnectionManager::class)
+    private val dbConnectionManager: DbConnectionManager,
+    @Reference(service = JpaEntitiesRegistry::class)
+    private val jpaEntitiesRegistry: JpaEntitiesRegistry,
     @Reference(service = UniquenessCheckerLifecycle::class)
     private val uniquenessChecker: UniquenessCheckerLifecycle
 ) : UniquenessProcessor {
+
+    init {
+        jpaEntitiesRegistry.register(CordaDb.CordaCluster.persistenceUnitName, ConfigurationEntities.classes)
+    }
 
     companion object {
         private val log = LoggerFactory.getLogger(this::class.java.enclosingClass)
     }
 
     private val dependentComponents = DependentComponents.of(
+        ::configurationReadService,
+        ::dbConnectionManager,
         ::uniquenessChecker
     )
 
     private val lifecycleCoordinator =
         coordinatorFactory.createCoordinator<UniquenessProcessorImpl>(dependentComponents, ::eventHandler)
 
-    override fun start() {
+    override fun start(bootConfig: SmartConfig) {
         log.info("Uniqueness processor starting.")
         lifecycleCoordinator.start()
+        lifecycleCoordinator.postEvent(BootConfigEvent(bootConfig))
     }
 
     override fun stop() {
@@ -56,6 +76,15 @@ class UniquenessProcessorImpl @Activate constructor(
             is StopEvent -> {
                 // Nothing to do
             }
+            is BootConfigEvent -> {
+                val bootstrapConfig = event.config
+
+                log.trace("Bootstrapping {}", configurationReadService::class.simpleName)
+                configurationReadService.bootstrapConfig(bootstrapConfig)
+
+                log.trace("Bootstrapping {}", dbConnectionManager::class.simpleName)
+                dbConnectionManager.bootstrap(bootstrapConfig.getConfig(BOOT_DB))
+            }
             is RegistrationStatusChangeEvent -> {
                 log.info("Uniqueness processor is ${event.status}")
                 coordinator.updateStatus(event.status)
@@ -65,4 +94,6 @@ class UniquenessProcessorImpl @Activate constructor(
             }
         }
     }
+
+    data class BootConfigEvent(val config: SmartConfig) : LifecycleEvent
 }
