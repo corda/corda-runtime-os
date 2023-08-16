8cbf93c926e2230c5d3edb95fb61f053ccf4297a
Emily Bowe <emily.bowe@r3.com>
2023-08-16 20:13:45 +0100

    kdocs for new RecordFactory api

--- components/flow/flow-mapper/src/main/kotlin/net/corda/flow/mapper/factory/RecordFactory.kt
+++ components/flow/flow-mapper/src/main/kotlin/net/corda/flow/mapper/factory/RecordFactory.kt
@@ -7,12 +7,19 @@ import net.corda.libs.configuration.SmartConfig
 import net.corda.messaging.api.records.Record
 import java.time.Instant
 
+/**
+ * Create [Record]
+ * Topic for [Record] is returned based on:
+ * - The message direction
+ * - whether the counterparty is on the same cluster (local)
+ * @return A record for p2p.out or local
+ */
 interface RecordFactory {
-    /**
-     * Create [Record] after checking if the cluster is local.
-     * @return A record for p2p.out or local
-     */
 
+    /**
+     * Forward [Record] of [SessionEvent] using:
+     * @return A record of SessionEvent
+     */
     fun forwardEvent(
         sessionEvent: SessionEvent,
         instant: Instant,
@@ -20,6 +27,10 @@ interface RecordFactory {
         messageDirection: MessageDirection
         ): Record<*, *>
 
+    /**
+     * Forward [Record] of [SessionError]
+     * @return A record of SessionError
+     */
     fun forwardError(
         sessionEvent: SessionEvent,
         exceptionEnvelope: ExceptionEnvelope,
@@ -28,6 +39,10 @@ interface RecordFactory {
         messageDirection: MessageDirection
     ): Record<*, *>
 
+    /**
+     * Forward [Record] of [SessionAck]
+     * @return A record of SessionAck
+     */
     fun forwardAck(
         sessionEvent: SessionEvent,
         instant: Instant,
@@ -35,6 +50,10 @@ interface RecordFactory {
         messageDirection: MessageDirection
     ): Record<*, *>
 
+    /**
+     * Gets the topic for a record
+     * @return topic
+     */
     fun getSessionEventOutputTopic(
         sessionEvent: SessionEvent,
         messageDirection: MessageDirection
