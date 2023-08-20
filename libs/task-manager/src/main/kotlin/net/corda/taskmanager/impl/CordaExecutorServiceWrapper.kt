fd73b4d05e7daae0fa1dedd821756b3b30de8520
Conal Smith <conal.smith@r3.com>
2023-08-20 21:34:40 +0100

    CORE-15450 - move taskManager to its own lib, use in StateAndEventSubscriptionImpl to dispatch coroutines

--- libs/task-manager/src/main/kotlin/net/corda/taskmanager/impl/CordaExecutorServiceWrapper.kt
+++ libs/task-manager/src/main/kotlin/net/corda/taskmanager/impl/CordaExecutorServiceWrapper.kt
@@ -1,10 +1,10 @@
-package net.corda.flow.taskmanager.impl
+package net.corda.taskmanager.impl
 
 import io.micrometer.core.instrument.MeterRegistry
 import io.micrometer.core.instrument.internal.TimedExecutorService
 import java.util.concurrent.ExecutorService
 
-class CordaExecutorServiceWrapper(
+internal class CordaExecutorServiceWrapper(
     private val name: String,
     private val metricPrefix: String,
     private val delegate: ExecutorService,
