b41f3fddc13916c6eb1b0e43a1c6e8690321b63a
Filipe Oliveira <filipe.oliveira@r3.com>
2023-08-22 11:56:20 +0100

    Removed unnecessary import

--- components/ledger/ledger-utxo-token-cache/src/main/kotlin/net/corda/ledger/utxo/token/cache/services/ServiceConfiguration.kt
+++ components/ledger/ledger-utxo-token-cache/src/main/kotlin/net/corda/ledger/utxo/token/cache/services/ServiceConfiguration.kt
@@ -1,7 +1,6 @@
 package net.corda.ledger.utxo.token.cache.services
 
 import net.corda.libs.configuration.SmartConfig
-import net.corda.schema.configuration.LedgerConfig.UTXO_TOKEN_CACHED_TOKEN_PAGE_SIZE
 
 interface ServiceConfiguration {
     fun init(config: SmartConfig)
