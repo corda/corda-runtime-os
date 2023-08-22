858840b28801b0a4ed717da326835999ef93c466
eunjee.yang <eunjee.yang@r3.com>
2023-08-22 11:30:41 +0100

    CORE-15909: update tx verification service test

--- components/ledger/ledger-utxo-flow/src/test/kotlin/net/corda/ledger/utxo/flow/impl/transaction/verifier/UtxoLedgerTransactionVerificationServiceImplTest.kt
+++ components/ledger/ledger-utxo-flow/src/test/kotlin/net/corda/ledger/utxo/flow/impl/transaction/verifier/UtxoLedgerTransactionVerificationServiceImplTest.kt
@@ -12,7 +12,6 @@ import net.corda.ledger.utxo.data.transaction.UtxoLedgerTransactionInternal
 import net.corda.ledger.utxo.flow.impl.persistence.UtxoLedgerGroupParametersPersistenceService
 import net.corda.ledger.utxo.flow.impl.persistence.external.events.ALICE_X500_HOLDING_IDENTITY
 import net.corda.ledger.utxo.flow.impl.transaction.verifier.external.events.TransactionVerificationExternalEventFactory
-import net.corda.ledger.utxo.test.mockGroupParametersService
 import net.corda.ledger.utxo.testkit.notaryX500Name
 import net.corda.membership.lib.SignedGroupParameters
 import net.corda.sandboxgroupcontext.CurrentSandboxGroupContext
@@ -58,8 +57,6 @@ class UtxoLedgerTransactionVerificationServiceImplTest {
         verificationService = UtxoLedgerTransactionVerificationServiceImpl(
             externalEventExecutor,
             serializationService,
-            mockUtxoLedgerGroupParametersPersistenceService,
-            mockGroupParametersService(),
             currentSandboxGroupContext,
             mock()
         )
