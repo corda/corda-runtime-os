489fba4bad113159cd6b3da57bd9273cf1c8b51a
Filipe Oliveira <filipe.oliveira@r3.com>
2023-08-25 10:30:34 +0100

    Added the `replace a token` and `iterating` tests back in.

--- components/ledger/ledger-utxo-token-cache/src/test/kotlin/net/corda/ledger/utxo/token/cache/impl/entities/TokenCacheImplTest.kt
+++ components/ledger/ledger-utxo-token-cache/src/test/kotlin/net/corda/ledger/utxo/token/cache/impl/entities/TokenCacheImplTest.kt
@@ -1,10 +1,7 @@
 package net.corda.ledger.utxo.token.cache.impl.entities
 
-import net.corda.data.ledger.utxo.token.selection.state.TokenPoolCacheState
-import net.corda.ledger.utxo.token.cache.converters.EntityConverter
 import net.corda.ledger.utxo.token.cache.entities.CachedToken
 import net.corda.ledger.utxo.token.cache.entities.internal.TokenCacheImpl
-import net.corda.ledger.utxo.token.cache.impl.POOL_CACHE_KEY
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.mockito.kotlin.mock
@@ -12,23 +9,31 @@ import org.mockito.kotlin.whenever
 
 class TokenCacheImplTest {
 
-    private val entityConverter = mock<EntityConverter>()
-    private val tokenPoolCacheState = TokenPoolCacheState().apply {
-        this.poolKey = POOL_CACHE_KEY
-        this.availableTokens = listOf()
-        this.tokenClaims = listOf()
-    }
-    private val target = TokenCacheImpl()
-
     @Test
     fun `adding a token`() {
+        val target = TokenCacheImpl()
         val cachedToken = mock<CachedToken>()
         target.add(listOf(cachedToken))
         assertThat(target.toList()).containsOnly(cachedToken)
     }
 
     @Test
+    fun `replace a token`() {
+        val target = TokenCacheImpl()
+        val cachedToken1 = mock<CachedToken>().apply { whenever(stateRef).thenReturn("s1") }
+        val cachedToken2 = mock<CachedToken>().apply { whenever(stateRef).thenReturn("s1") }
+        target.add(listOf(cachedToken1))
+
+        assertThat(target.toList()).containsOnly(cachedToken1)
+
+        target.add(listOf(cachedToken2))
+
+        assertThat(target.toList()).containsOnly(cachedToken2)
+    }
+
+    @Test
     fun `removing a token`() {
+        val target = TokenCacheImpl()
         val cachedToken1 = mock<CachedToken>().apply { whenever(stateRef).thenReturn("s1") }
         val cachedToken2 = mock<CachedToken>().apply { whenever(stateRef).thenReturn("s2") }
         val cachedToken3 = mock<CachedToken>().apply { whenever(stateRef).thenReturn("s3") }
@@ -37,5 +42,16 @@ class TokenCacheImplTest {
         assertThat(target.toList()).containsOnly(cachedToken1, cachedToken2, cachedToken3)
         target.removeAll(setOf("s1", "s3"))
         assertThat(target.toList()).containsOnly(cachedToken2)
+    }
+
+    @Test
+    fun `iterating the cache`() {
+        val target = TokenCacheImpl()
+        val cachedToken1 = mock<CachedToken>().apply { whenever(stateRef).thenReturn("s1") }
+        val cachedToken2 = mock<CachedToken>().apply { whenever(stateRef).thenReturn("s2") }
+        val cachedToken3 = mock<CachedToken>().apply { whenever(stateRef).thenReturn("s3") }
+        target.add(listOf(cachedToken1, cachedToken2, cachedToken3))
+
+        assertThat(target.toList()).containsOnly(cachedToken1, cachedToken2, cachedToken3)
     }
 }
