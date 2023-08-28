793902f68502357ad731b37bad25b7b1fd33e207
LWogan <lorcan.wogan@r3.com>
2023-08-28 14:58:42 +0100

    CORE-16195 configure javalin for tracing

--- components/webserver/src/main/kotlin/net/corda/web/server/JavalinFactory.kt
+++ components/webserver/src/main/kotlin/net/corda/web/server/JavalinFactory.kt
@@ -1,10 +1,12 @@
 package net.corda.web.server
 
 import io.javalin.Javalin
+import net.corda.tracing.configureJavalinForTracing
 import org.osgi.service.component.annotations.Component
 
 @Component(service = [JavalinFactory::class])
 class JavalinFactory {
-    fun create(): Javalin = Javalin.create()
-
+    fun create(): Javalin = Javalin.create{
+        configureJavalinForTracing(it)
+    }
 }
