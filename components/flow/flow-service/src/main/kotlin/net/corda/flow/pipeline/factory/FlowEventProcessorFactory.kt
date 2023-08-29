296d6970e0dc3914c76ff8e714e8de554fadd490
nkovacsx <nandor.kovacs@r3.com>
2023-08-29 11:11:19 +0100

    detekt

--- components/flow/flow-service/src/main/kotlin/net/corda/flow/pipeline/factory/FlowEventProcessorFactory.kt
+++ components/flow/flow-service/src/main/kotlin/net/corda/flow/pipeline/factory/FlowEventProcessorFactory.kt
@@ -4,7 +4,6 @@ import net.corda.data.flow.event.FlowEvent
 import net.corda.data.flow.state.checkpoint.Checkpoint
 import net.corda.libs.configuration.SmartConfig
 import net.corda.messaging.api.processor.StateAndEventProcessor
-import net.corda.schema.configuration.ConfigKeys.FLOW_CONFIG
 
 /**
  * [FlowEventProcessorFactory] creates instances of [FlowEventProcessor].
