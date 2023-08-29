7a20cc47cb602dce361cfd48694882f87c979c75
nkovacsx <nandor.kovacs@r3.com>
2023-08-29 09:26:22 +0100

    Remove annotation

--- libs/flows/flow-api/src/main/kotlin/net/corda/flow/application/services/FlowConfigService.kt
+++ libs/flows/flow-api/src/main/kotlin/net/corda/flow/application/services/FlowConfigService.kt
@@ -3,7 +3,6 @@ package net.corda.flow.application.services
 import net.corda.libs.configuration.SmartConfig
 import net.corda.v5.base.annotations.DoNotImplement
 
-@DoNotImplement
 interface FlowConfigService {
 
     /**
