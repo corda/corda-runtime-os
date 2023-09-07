a9f0936af7de83845a669d20c3d284ab14a10e8b
ben-millar <ben.millar@r3.com>
2023-09-07 15:45:54 +0100

    CORE-16177 Adding kdocs to CordaMessage

--- libs/messaging/message-bus/src/main/kotlin/net/corda/messagebus/api/producer/CordaMessage.kt
+++ libs/messaging/message-bus/src/main/kotlin/net/corda/messagebus/api/producer/CordaMessage.kt
@@ -1,26 +1,65 @@
 package net.corda.messagebus.api.producer
 
+/**
+ * Object to encapsulate a generic producer message.
+ * @property payload the payload of the message.
+ * @property props an additional properties map.
+ */
 @Suppress("UNCHECKED_CAST")
 data class CordaMessage<T: Any>(
     val payload: T?,
     val props: MutableMap<String, Any> = mutableMapOf()
 ) {
+    /**
+     * Adds a new property to the internal storage.
+     *
+     * @param property A key value [Pair] to insert into our additional properties map.
+     */
     fun addProperty(property: Pair<String, Any>) {
         props[property.first] = property.second
     }
-    fun getProperty(id: String) : Any {
-        return getPropertyOrNull(id) ?: throw NoSuchElementException("")
+
+    /**
+     * Retrieves a property from the additional properties map without casting.
+     *
+     * @param key The key of the property to retrieve.
+     * @return The property associated with the given key.
+     * @throws NoSuchElementException if no property with the given key exists.
+     */
+    fun getProperty(key: String) : Any {
+        return getPropertyOrNull(key) ?: throw NoSuchElementException("")
     }
 
-    fun <T> getProperty(id: String) : T {
-        return getPropertyOrNull<T>(id) ?: throw NoSuchElementException("")
+    /**
+     * Retrieves a property of a specific type from the additional properties map.
+     *
+     * @param key The key of the property to retrieve.
+     * @return The property associated with the given key, cast to the specified type.
+     * @throws NoSuchElementException if no property with the given key exists.
+     * @throws ClassCastException if the property cannot be cast to the specified type.
+     */
+    fun <T> getProperty(key: String) : T {
+        return getPropertyOrNull<T>(key) ?: throw NoSuchElementException("")
     }
 
-    fun getPropertyOrNull(id: String) : Any? {
-        return props[id]
+    /**
+     * Retrieves a property from the additional properties map without casting, returning null if not found.
+     *
+     * @param key The key of the property to retrieve.
+     * @return The property associated with the given key, or null if not found.
+     */
+    fun getPropertyOrNull(key: String) : Any? {
+        return props[key]
     }
 
-    fun <T> getPropertyOrNull(id: String) : T? {
-        return props[id] as? T ?: throw IllegalArgumentException("")
+    /**
+     * Retrieves a property of a specific type from the additional properties map, returning null if not found or if the cast fails.
+     *
+     * @param key The identifier of the property to retrieve.
+     * @return The property associated with the given key cast to the specified type, or null if not found or casting fails.
+     * @throws ClassCastException if the property cannot be cast to the specified type.
+     */
+    fun <T> getPropertyOrNull(key: String) : T? {
+        return props[key] as? T ?: throw ClassCastException("")
     }
 }
