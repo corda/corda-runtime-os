155596b81ee2c73a45f7fa0546f6230d79ccb22f
owenstanford <owen.stanford@r3.com>
2023-09-12 17:06:52 +0100

    NOTICK- Claim debugging

--- components/ledger/ledger-utxo-token-cache/src/main/kotlin/net/corda/ledger/utxo/token/cache/handlers/TokenClaimQueryEventHandler.kt
+++ components/ledger/ledger-utxo-token-cache/src/main/kotlin/net/corda/ledger/utxo/token/cache/handlers/TokenClaimQueryEventHandler.kt
@@ -9,6 +9,7 @@ import net.corda.ledger.utxo.token.cache.factories.RecordFactory
 import net.corda.ledger.utxo.token.cache.services.AvailableTokenService
 import net.corda.ledger.utxo.token.cache.services.TokenFilterStrategy
 import net.corda.messaging.api.records.Record
+import org.slf4j.LoggerFactory
 import java.math.BigDecimal
 
 class TokenClaimQueryEventHandler(
@@ -17,26 +18,35 @@ class TokenClaimQueryEventHandler(
     private val availableTokenService: AvailableTokenService
 ) : TokenEventHandler<ClaimQuery> {
 
+    private companion object {
+        val log = LoggerFactory.getLogger(this::class.java.enclosingClass)
+    }
+
     override fun handle(
         tokenCache: TokenCache,
         state: PoolCacheState,
         event: ClaimQuery
     ): Record<String, FlowEvent> {
 
+        val sb = StringBuffer("Token Query for $event cache size = ${tokenCache.count()}")
         // Attempt to select the tokens from the current cache
         var selectionResult = selectTokens(tokenCache, state, event)
 
         // if we didn't reach the target amount, reload the cache to ensure it's full and retry
         if (selectionResult.first < event.targetAmount) {
+            sb.appendLine("Cahce miss, found only ${selectionResult}")
             val findResult = availableTokenService.findAvailTokens(event.poolKey, event.ownerHash, event.tagRegex)
+            sb.appendLine("Loaded ${findResult.tokens.size} tokens from DB")
             tokenCache.add(findResult.tokens)
+            sb.appendLine("Cache size ${tokenCache.count()}")
             selectionResult = selectTokens(tokenCache, state, event)
         }
 
         val selectedAmount = selectionResult.first
         val selectedTokens = selectionResult.second
 
-        return if (selectedAmount >= event.targetAmount) {
+        val result = if (selectedAmount >= event.targetAmount) {
+            sb.appendLine("Claim successful $selectionResult")
             state.addNewClaim(event.externalEventRequestId, selectedTokens)
             recordFactory.getSuccessfulClaimResponse(
                 event.flowId,
@@ -45,8 +55,13 @@ class TokenClaimQueryEventHandler(
                 selectedTokens
             )
         } else {
+            sb.appendLine("Claim failed $selectionResult")
             recordFactory.getFailedClaimResponse(event.flowId, event.externalEventRequestId, event.poolKey)
         }
+
+        log.info(sb.toString())
+
+        return result
     }
 
     private fun selectTokens(
