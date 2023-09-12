23f9e47810262317632f47c360a77ab95df51f73
malachy <malachy.byrne@r3.com>
2023-09-12 14:06:41 +0100

    Fix detekt error

--- components/flow/flow-service/src/main/kotlin/net/corda/flow/application/crypto/SigningServiceImpl.kt
+++ components/flow/flow-service/src/main/kotlin/net/corda/flow/application/crypto/SigningServiceImpl.kt
@@ -46,12 +46,19 @@ class SigningServiceImpl @Activate constructor(
     }
 
     @Suspendable
-    override fun sign(bytes: ByteArray, publicKey: PublicKey, signatureSpec: SignatureSpec): DigitalSignature.WithKeyId {
+    override fun sign(bytes: ByteArray,
+                      publicKey: PublicKey,
+                      signatureSpec: SignatureSpec
+    ): DigitalSignature.WithKeyId {
         return sign(bytes, publicKey, signatureSpec, emptyMap())
     }
 
     @Suspendable
-    override fun sign(bytes: ByteArray, publicKey: PublicKey, signatureSpec: SignatureSpec, context: Map<String, String>): DigitalSignature.WithKeyId {
+    override fun sign(bytes: ByteArray,
+                      publicKey: PublicKey,
+                      signatureSpec: SignatureSpec,
+                      context: Map<String, String>
+    ): DigitalSignature.WithKeyId {
         return recordSuspendable({ cryptoFlowTimer("sign") }) @Suspendable {
             val digitalSignatureWithKey = externalEventExecutor.execute(
                 CreateSignatureExternalEventFactory::class.java,
