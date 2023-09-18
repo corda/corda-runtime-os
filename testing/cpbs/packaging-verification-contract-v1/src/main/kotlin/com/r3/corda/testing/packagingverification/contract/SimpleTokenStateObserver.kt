5077edda0636f93251ea1d4c1a3ef6bcea63f72d
Filipe Oliveira <filipe.oliveira@r3.com>
2023-09-18 15:30:17 +0100

    Updated the observer `SimpleTokenStateObserver` to use the new API.

--- testing/cpbs/packaging-verification-contract-v1/src/main/kotlin/com/r3/corda/testing/packagingverification/contract/SimpleTokenStateObserver.kt
+++ testing/cpbs/packaging-verification-contract-v1/src/main/kotlin/com/r3/corda/testing/packagingverification/contract/SimpleTokenStateObserver.kt
@@ -1,17 +1,19 @@
 package com.r3.corda.testing.packagingverification.contract
 
 import net.corda.v5.application.crypto.DigestService
+import net.corda.v5.ledger.utxo.StateAndRef
 import net.corda.v5.ledger.utxo.observer.UtxoToken
 import net.corda.v5.ledger.utxo.observer.UtxoTokenFilterFields
 import net.corda.v5.ledger.utxo.observer.UtxoTokenPoolKey
+import net.corda.v5.ledger.utxo.observer.UtxoTokenTransactionStateObserver
+import net.corda.v5.ledger.utxo.transaction.UtxoLedgerTransaction
 
-@Suppress("DEPRECATION")
-class SimpleTokenStateObserver : net.corda.v5.ledger.utxo.observer.UtxoLedgerTokenStateObserver<SimpleState> {
+class SimpleTokenStateObserver : UtxoTokenTransactionStateObserver<SimpleState> {
     override fun getStateType() = SimpleState::class.java
 
-    override fun onCommit(state: SimpleState, digestService: DigestService) = UtxoToken(
-        UtxoTokenPoolKey(STATE_NAME, state.issuer.toSecureHash(digestService), STATE_SYMBOL),
-        state.value.toBigDecimal(),
+    override fun onCommit(stateAndRef: StateAndRef<SimpleState>, transaction: UtxoLedgerTransaction, digestService: DigestService) = UtxoToken(
+        UtxoTokenPoolKey(STATE_NAME, stateAndRef.state.contractState.issuer.toSecureHash(digestService), STATE_SYMBOL),
+        stateAndRef.state.contractState.value.toBigDecimal(),
         UtxoTokenFilterFields()
     )
 }
