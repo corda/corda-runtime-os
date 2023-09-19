b734f694e1fb04c611ab93a390be637b22691b7d
Dimos Raptis <dimos.raptis@r3.com>
2023-09-19 15:29:52 +0100

    CORE-10087 - Migrate remaining network e2e tests to smoke test suite

---
+++ applications/workers/workers-smoketest/src/smokeTest/kotlin/net/corda/applications/workers/smoketest/network/SingleClusterDynamicNetworkTest.kt
@@ -0,0 +1,45 @@
+package net.corda.applications.workers.smoketest.network
+
+import net.corda.applications.workers.smoketest.utils.TEST_CPB_LOCATION
+import net.corda.e2etest.utilities.DEFAULT_CLUSTER
+import net.corda.e2etest.utilities.containsExactlyInAnyOrderActiveMembers
+import net.corda.e2etest.utilities.exportGroupPolicy
+import net.corda.e2etest.utilities.getHoldingIdShortHash
+import net.corda.e2etest.utilities.onboardMember
+import net.corda.e2etest.utilities.onboardMgm
+import net.corda.e2etest.utilities.onboardNotaryMember
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+import java.util.UUID
+
+class SingleClusterDynamicNetworkTest {
+    private val testUniqueId = UUID.randomUUID()
+    private val groupId = UUID.randomUUID().toString()
+
+    private val mgmX500 = "O=Mgm, L=London, C=GB, OU=${testUniqueId}"
+    private val aliceX500 = "CN=Alice-${testUniqueId}, OU=Application, O=R3, L=London, C=GB"
+    private val bobX500 = "CN=Bob-${testUniqueId}, OU=Application, O=R3, L=London, C=GB"
+    private val notaryX500 = "CN=Notary-${testUniqueId}, OU=Application, O=R3, L=London, C=GB"
+
+    private val aliceHoldingId: String = getHoldingIdShortHash(aliceX500, groupId)
+    private val bobHoldingId: String = getHoldingIdShortHash(bobX500, groupId)
+    private val notaryHoldingId: String = getHoldingIdShortHash(notaryX500, groupId)
+
+    @Test
+    fun `Create mgm and allow members to join the group`() {
+        val mgmInfo = DEFAULT_CLUSTER.onboardMgm(mgmX500)
+        val groupPolicy = DEFAULT_CLUSTER.exportGroupPolicy(mgmInfo.holdingId).also {
+            Assertions.assertThat(it).isNotEmpty.isNotBlank
+        }
+
+        val aliceInfo = DEFAULT_CLUSTER.onboardMember(TEST_CPB_LOCATION, testUniqueId.toString(), groupPolicy, aliceX500)
+        val bobInfo = DEFAULT_CLUSTER.onboardMember(TEST_CPB_LOCATION, testUniqueId.toString(), groupPolicy, bobX500)
+        val notaryInfo = DEFAULT_CLUSTER.onboardNotaryMember(TEST_CPB_LOCATION, testUniqueId.toString(), groupPolicy, notaryX500)
+
+        val allMembers = listOf(mgmInfo, aliceInfo, bobInfo, notaryInfo)
+        allMembers.forEach { member ->
+            DEFAULT_CLUSTER.containsExactlyInAnyOrderActiveMembers(member.toString(), allMembers.map { it.toString() })
+        }
+    }
+
+}
