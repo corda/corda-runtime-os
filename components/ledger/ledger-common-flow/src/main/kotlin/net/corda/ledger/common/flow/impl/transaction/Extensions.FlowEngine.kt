c6c57ff48e1788ec612c8d2c7fb5eb394dfc5f3c
Lajos Veres <lajos.veres@r3.com>
2023-09-20 16:43:29 +0100

    CORE-17166 Remove unneded with

--- components/ledger/ledger-common-flow/src/main/kotlin/net/corda/ledger/common/flow/impl/transaction/Extensions.FlowEngine.kt
+++ components/ledger/ledger-common-flow/src/main/kotlin/net/corda/ledger/common/flow/impl/transaction/Extensions.FlowEngine.kt
@@ -7,15 +7,13 @@ import net.corda.v5.base.exceptions.CordaRuntimeException
 import net.corda.v5.ledger.common.transaction.CordaPackageSummary
 
 fun FlowEngine.getCpiSummary(): CordaPackageSummary =
-    with(this) {
-        CordaPackageSummaryImpl(
-            name = flowContextProperties[FlowContextPropertyKeys.CPI_NAME]
-                ?: throw CordaRuntimeException("CPI name is not accessible"),
-            version = flowContextProperties[FlowContextPropertyKeys.CPI_VERSION]
-                ?: throw CordaRuntimeException("CPI version is not accessible"),
-            signerSummaryHash = flowContextProperties[FlowContextPropertyKeys.CPI_SIGNER_SUMMARY_HASH]
-                ?: throw CordaRuntimeException("CPI signer summary hash is not accessible"),
-            fileChecksum = flowContextProperties[FlowContextPropertyKeys.CPI_FILE_CHECKSUM]
-                ?: throw CordaRuntimeException("CPI file checksum is not accessible"),
-        )
-    }
+    CordaPackageSummaryImpl(
+        name = flowContextProperties[FlowContextPropertyKeys.CPI_NAME]
+            ?: throw CordaRuntimeException("CPI name is not accessible"),
+        version = flowContextProperties[FlowContextPropertyKeys.CPI_VERSION]
+            ?: throw CordaRuntimeException("CPI version is not accessible"),
+        signerSummaryHash = flowContextProperties[FlowContextPropertyKeys.CPI_SIGNER_SUMMARY_HASH]
+            ?: throw CordaRuntimeException("CPI signer summary hash is not accessible"),
+        fileChecksum = flowContextProperties[FlowContextPropertyKeys.CPI_FILE_CHECKSUM]
+            ?: throw CordaRuntimeException("CPI file checksum is not accessible"),
+    )
