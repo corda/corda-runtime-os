5f58e4a1dc34fc841d5addb6097dfecaae6db73f
Kyriakos Tharrouniatis <kyriakos.tharrouniatis@r3.com>
2023-09-22 09:42:21 +0100

    Remove duplicate kDoc

--- libs/flows/flow-api/src/main/kotlin/net/corda/flow/external/events/executor/ExternalEventExecutor.kt
+++ libs/flows/flow-api/src/main/kotlin/net/corda/flow/external/events/executor/ExternalEventExecutor.kt
@@ -32,22 +32,6 @@ interface ExternalEventExecutor {
         parameters: PARAMETERS
     ): RESUME
 
-    /**
-     * Sends an event to an external processor and awaits its response.
-     *
-     * [execute] resumes with either a:
-     * - Response of type [RESUME].
-     * - An exception.
-     *
-     * @param factoryClass The [ExternalEventFactory] that is called to create the event to send and convert the
-     * received response into an acceptable object to resume with.
-     * @param parameters The [PARAMETERS] object.
-     *
-     * @param PARAMETERS The type to pass to the factory when suspending/creating the event.
-     * @param RESPONSE The type received as a response from the external processor.
-     * @param RESUME The type the flow resumes with after calling [execute].
-     * @return The object that the flow will resume with.
-     */
     @Suspendable
     fun <PARAMETERS : Any, RESPONSE : Any, RESUME> execute(
         factoryClass: Class<out ExternalEventFactory<PARAMETERS, RESPONSE, RESUME>>,
