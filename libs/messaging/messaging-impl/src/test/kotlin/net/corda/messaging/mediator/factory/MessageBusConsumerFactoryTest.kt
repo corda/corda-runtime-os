e69ebb33933c5d3b35e0057554db850ddf1cd6bf
Miljenko Brkic <miljenko.brkic@r3.com>
2023-09-22 21:04:34 +0100

    CORE-17300 Multi-Source Event Mediator configurations and factories for consumers and messaging clients.

---
+++ libs/messaging/messaging-impl/src/test/kotlin/net/corda/messaging/mediator/factory/MessageBusConsumerFactoryTest.kt
@@ -0,0 +1,45 @@
+package net.corda.messaging.mediator.factory
+
+import net.corda.libs.configuration.SmartConfig
+import net.corda.messagebus.api.consumer.CordaConsumer
+import net.corda.messagebus.api.consumer.builder.CordaConsumerBuilder
+import net.corda.messaging.api.mediator.config.MediatorConsumerConfig
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.mockito.kotlin.any
+import org.mockito.kotlin.anyOrNull
+import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.mock
+
+class MessageBusConsumerFactoryTest {
+    private lateinit var messageBusConsumerFactory: MessageBusConsumerFactory
+    private val cordaConsumerBuilder = mock<CordaConsumerBuilder>()
+    private val cordaConsumer = mock<CordaConsumer<Any, Any>>()
+    private val messageBusConfig = mock<SmartConfig>()
+
+    @BeforeEach
+    fun beforeEach() {
+        doReturn(cordaConsumer).`when`(cordaConsumerBuilder).createConsumer(
+            any(), any(), any<Class<Any>>(), any<Class<Any>>(), any(), anyOrNull()
+        )
+        messageBusConsumerFactory = MessageBusConsumerFactory(
+            "topic",
+            "group",
+            messageBusConfig,
+            cordaConsumerBuilder,
+        )
+    }
+
+    @Test
+    fun testCreateMessageBusConsumer() {
+        val config = MediatorConsumerConfig(
+            Any::class.java,
+            Any::class.java,
+        ) {}
+
+        val messageBusConsumer = messageBusConsumerFactory.create(config)
+
+        Assertions.assertNotNull(messageBusConsumer)
+    }
+}
