89d175864eeb30558b7c40b3cdc2b3682076067d
James Higgs <james.higgs@r3.com>
2023-09-27 16:54:35 +0100

    CORE-17357: Finish the scheduled task handler and add a unit test

--- components/flow/flow-mapper-service/src/main/kotlin/net/corda/session/mapper/service/executor/ScheduledTaskHandler.kt
+++ components/flow/flow-mapper-service/src/main/kotlin/net/corda/session/mapper/service/executor/ScheduledTaskHandler.kt
@@ -2,9 +2,7 @@ package net.corda.session.mapper.service.executor
 
 import net.corda.data.flow.event.mapper.ExecuteCleanup
 import net.corda.data.flow.state.mapper.FlowMapperStateType
-import net.corda.data.scheduler.ScheduledTaskTrigger
 import net.corda.libs.statemanager.api.StateManager
-import net.corda.messaging.api.records.Record
 import net.corda.session.mapper.service.state.StateMetadataKeys.FLOW_MAPPER_STATUS
 import net.corda.utilities.debug
 import org.slf4j.LoggerFactory
@@ -12,23 +10,25 @@ import java.time.Clock
 import java.time.Duration
 import java.time.Instant
 
-class ScheduledTaskMessageProcessor(
+class ScheduledTaskHandler(
     private val stateManager: StateManager,
     private val clock: Clock,
-    private val cleanupWindow: Long
+    private val cleanupWindow: Long,
+    private val batchSize: Int = ID_BATCH_SIZE
 ) {
 
     private companion object {
         private val logger = LoggerFactory.getLogger(this::class.java.enclosingClass)
+        private const val ID_BATCH_SIZE = 200
     }
 
-    fun process(event: ScheduledTaskTrigger) : List<Record<String, ExecuteCleanup>> {
+    fun process() : List<ExecuteCleanup> {
         logger.debug { "Received a scheduled task trigger. Scheduling cleanup events for the flow mapper." }
-        // Lookup expired states
         val keys = getExpiredStateIds()
-        // Break up into sensible sized batches.
-
-        // Convert to records to send
+        val batches = batchIds(keys)
+        return batches.map {
+            ExecuteCleanup(it)
+        }
     }
 
     private fun getExpiredStateIds() : List<String> {
@@ -41,6 +41,12 @@ class ScheduledTaskMessageProcessor(
             state.metadata[FLOW_MAPPER_STATUS] == FlowMapperStateType.CLOSING.toString()
         }.map {
             it.key
+        }.also {
+            logger.debug { "Found ${states.size} states eligible for cleanup" }
         }
+    }
+
+    private fun batchIds(ids: List<String>) : List<List<String>> {
+        return ids.chunked(batchSize)
     }
 }
---
+++ components/flow/flow-mapper-service/src/test/kotlin/net/corda/session/mapper/service/executor/ScheduledTaskHandlerTest.kt
@@ -0,0 +1,111 @@
+package net.corda.session.mapper.service.executor
+
+import net.corda.data.flow.state.mapper.FlowMapperStateType
+import net.corda.libs.statemanager.api.Operation
+import net.corda.libs.statemanager.api.State
+import net.corda.libs.statemanager.api.StateManager
+import net.corda.libs.statemanager.api.metadata
+import net.corda.session.mapper.service.state.StateMetadataKeys.FLOW_MAPPER_STATUS
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import java.time.Clock
+import java.time.Instant
+import java.time.ZoneId
+
+class ScheduledTaskHandlerTest {
+
+    private val clock = Clock.fixed(Instant.now(), ZoneId.systemDefault())
+    private val window = 1000L
+    private val states = listOf(
+        createStateEntry("key1", clock.instant().minusMillis(window * 2), FlowMapperStateType.CLOSING),
+        createStateEntry("key2", clock.instant(), FlowMapperStateType.CLOSING),
+        createStateEntry("key3", clock.instant().minusMillis(window * 2), FlowMapperStateType.OPEN),
+        createStateEntry("key4", clock.instant().minusMillis(window * 3), FlowMapperStateType.CLOSING),
+        createStateEntry("key5", clock.instant().minusMillis(window * 2), null)
+    ).toMap()
+
+    @Test
+    fun `when scheduled task handler generates new records, ID of each retrieved state is present in output events`() {
+        val scheduledTaskHandler = ScheduledTaskHandler(
+            StateManagerImpl(),
+            clock,
+            window
+        )
+        val output = scheduledTaskHandler.process()
+        val ids = output.flatMap { it.ids }
+        assertThat(ids).contains("key1", "key4")
+    }
+
+    @Test
+    fun `when batch size is set to one, a record per id is present in output events`() {
+        val scheduledTaskHandler = ScheduledTaskHandler(
+            StateManagerImpl(),
+            clock,
+            window,
+            1
+        )
+        val output = scheduledTaskHandler.process()
+        assertThat(output.size).isEqualTo(2)
+    }
+
+    @Test
+    fun `when the last updated time is far enough in the past, no records are returned`() {
+        val scheduledTaskHandler = ScheduledTaskHandler(
+            StateManagerImpl(),
+            clock,
+            window * 5
+        )
+        val output = scheduledTaskHandler.process()
+        assertThat(output).isEmpty()
+    }
+
+    private inner class StateManagerImpl : StateManager {
+        override fun create(states: Collection<State>): Map<String, Exception> {
+            TODO("Not yet implemented")
+        }
+
+        override fun get(keys: Collection<String>): Map<String, State> {
+            TODO("Not yet implemented")
+        }
+
+        override fun update(states: Collection<State>): Map<String, State> {
+            TODO("Not yet implemented")
+        }
+
+        override fun delete(states: Collection<State>): Map<String, State> {
+            TODO("Not yet implemented")
+        }
+
+        override fun getUpdatedBetween(start: Instant, finish: Instant): Map<String, State> {
+            return states.filter {
+                it.value.modifiedTime.isBefore(finish) && it.value.modifiedTime.isAfter(start)
+            }
+        }
+
+        override fun find(key: String, operation: Operation, value: Any): Map<String, State> {
+            TODO("Not yet implemented")
+        }
+
+        override fun close() {
+            TODO("Not yet implemented")
+        }
+    }
+
+    private fun createStateEntry(
+        key: String,
+        lastUpdated: Instant,
+        mapperState: FlowMapperStateType?
+    ): Pair<String, State> {
+        val metadata = metadata()
+        mapperState?.let {
+            metadata.put(FLOW_MAPPER_STATUS, it.toString())
+        }
+        val state = State(
+            key,
+            byteArrayOf(),
+            metadata = metadata,
+            modifiedTime = lastUpdated
+        )
+        return Pair(key, state)
+    }
+}
