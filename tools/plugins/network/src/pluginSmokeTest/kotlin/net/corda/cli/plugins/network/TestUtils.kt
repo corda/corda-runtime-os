026bb473e90da5359a5870d94dd13f5389549731
Yash Nabar <yash.nabar@r3.com>
2023-09-28 15:58:05 +0100

    Add onboard-mgm tests

---
+++ tools/plugins/network/src/pluginSmokeTest/kotlin/net/corda/cli/plugins/network/TestUtils.kt
@@ -0,0 +1,19 @@
+package net.corda.cli.plugins.network
+
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.ObjectMapper
+import net.corda.cli.plugins.network.output.Output
+import net.corda.membership.lib.MemberInfoExtension
+
+internal class OutputStub : Output {
+    private val objectMapper = ObjectMapper()
+    var printedOutput: JsonNode? = null
+
+    override fun generateOutput(content: String) {
+        printedOutput = objectMapper.readTree(content)
+    }
+
+    fun getFirstPartyName(): String? {
+        return printedOutput?.get(0)?.get("memberContext")?.get(MemberInfoExtension.PARTY_NAME)?.asText()
+    }
+}
