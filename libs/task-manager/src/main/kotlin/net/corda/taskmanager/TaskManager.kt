d859c3c97f1d870ea3693bc2913485dc23f04e58
Dan Newton <dan.newton@r3.com>
2023-09-28 16:20:29 +0100

    CORE-16768 `TaskManager` updates

--- libs/task-manager/src/main/kotlin/net/corda/taskmanager/TaskManager.kt
+++ libs/task-manager/src/main/kotlin/net/corda/taskmanager/TaskManager.kt
@@ -4,9 +4,9 @@ import java.util.concurrent.CompletableFuture
 import java.util.concurrent.Executor
 
 interface TaskManager : Executor {
-    enum class CommandType {
-        FLOW, LONG_RUNNING, SCHEDULED
+    enum class TaskType {
+        SHORT_RUNNING, LONG_RUNNING, SCHEDULED
     }
     fun shutdown(): CompletableFuture<Void>
-    fun <T> execute(type: CommandType, command: () -> T): CompletableFuture<T>
+    fun <T> execute(type: TaskType, command: () -> T): CompletableFuture<T>
 }
