332d9b0242303bd298441516f0db1dbdfdab8164
Juan José Ramos Cassella <jujoramos@gmail.com>
2023-09-28 16:26:35 +0100

    Improve KDocs

--- libs/state-manager/state-manager-api/src/main/kotlin/net/corda/libs/statemanager/api/SearchOptions.kt
+++ libs/state-manager/state-manager-api/src/main/kotlin/net/corda/libs/statemanager/api/SearchOptions.kt
@@ -3,7 +3,7 @@ package net.corda.libs.statemanager.api
 import java.time.Instant
 
 /**
- * Supported comparison operations on metadata values.
+ * Supported comparison operations on [State.metadata] values.
  */
 enum class Operation {
     Equals,
@@ -12,6 +12,13 @@ enum class Operation {
     GreaterThan,
 }
 
+/**
+ * Interval of time to use when filtering by [State.modifiedTime] within the underlying persistent storage.
+ */
 data class IntervalFilter(val start: Instant, val finish: Instant)
 
+/**
+ * Parameters to use when filtering by [State.metadata] keys and values within the underlying persistent storage.
+ * As with the [State.metadata] itself, [key] can only be a [String] and [value] can only be of a primitive type.
+ */
 data class SingleKeyFilter(val key: String, val operation: Operation, val value: Any)
