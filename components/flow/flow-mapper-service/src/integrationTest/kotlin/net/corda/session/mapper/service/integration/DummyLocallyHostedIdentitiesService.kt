bbc910fade89a338ef63d9f222314b00b7bc4460
Will Vigor <william.vigor@r3.com>
2023-09-29 11:30:29 +0100

    Fix DummyLocallyHostedIdentitiesService

--- components/flow/flow-mapper-service/src/integrationTest/kotlin/net/corda/session/mapper/service/integration/DummyLocallyHostedIdentitiesService.kt
+++ components/flow/flow-mapper-service/src/integrationTest/kotlin/net/corda/session/mapper/service/integration/DummyLocallyHostedIdentitiesService.kt
@@ -1,13 +1,28 @@
 package net.corda.session.mapper.service.integration
 
+import net.corda.lifecycle.LifecycleCoordinatorFactory
+import net.corda.lifecycle.LifecycleCoordinatorName
+import net.corda.lifecycle.LifecycleStatus
+import net.corda.lifecycle.StartEvent
 import net.corda.membership.locally.hosted.identities.IdentityInfo
 import net.corda.membership.locally.hosted.identities.LocallyHostedIdentitiesService
 import net.corda.virtualnode.HoldingIdentity
 import org.osgi.service.component.annotations.Activate
 import org.osgi.service.component.annotations.Component
+import org.osgi.service.component.annotations.Reference
 
 @Component(service = [LocallyHostedIdentitiesService::class])
-class DummyLocallyHostedIdentitiesService @Activate constructor() : LocallyHostedIdentitiesService {
+class DummyLocallyHostedIdentitiesService @Activate constructor(@Reference(service = LifecycleCoordinatorFactory::class)
+                                                                    private val coordinatorFactory: LifecycleCoordinatorFactory,
+) : LocallyHostedIdentitiesService {
+    private val coordinator =
+        coordinatorFactory.createCoordinator(
+            LifecycleCoordinatorName.forComponent<LocallyHostedIdentitiesService>()
+        ) { event, coordinator ->
+            if (event is StartEvent) {
+                coordinator.updateStatus(LifecycleStatus.UP)
+            }
+        }
 
     private val identityMap = mutableMapOf<HoldingIdentity, IdentityInfo>()
 
@@ -24,11 +39,13 @@ class DummyLocallyHostedIdentitiesService @Activate constructor() : LocallyHoste
     }
 
     override val isRunning: Boolean
-        get() = true
+        get() = coordinator.isRunning
 
     override fun start() {
+        coordinator.start()
     }
 
     override fun stop() {
+        coordinator.stop()
     }
 }
