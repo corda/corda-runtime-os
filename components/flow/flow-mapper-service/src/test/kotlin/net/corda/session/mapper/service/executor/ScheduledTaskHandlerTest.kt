304bbc8018bfd515f2a6fb9f51c11f4f3d79d118
James Higgs <james.higgs@r3.com>
2023-10-02 12:22:24 +0100

    CORE-17357: Update unit tests to better use a mock

--- components/flow/flow-mapper-service/src/test/kotlin/net/corda/session/mapper/service/executor/ScheduledTaskHandlerTest.kt
+++ components/flow/flow-mapper-service/src/test/kotlin/net/corda/session/mapper/service/executor/ScheduledTaskHandlerTest.kt
@@ -10,7 +10,12 @@ import net.corda.libs.statemanager.api.metadata
 import net.corda.session.mapper.service.state.StateMetadataKeys.FLOW_MAPPER_STATUS
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.mockito.kotlin.any
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
 import java.time.Clock
+import java.time.Duration
 import java.time.Instant
 import java.time.ZoneId
 
@@ -20,28 +25,33 @@ class ScheduledTaskHandlerTest {
     private val window = 1000L
     private val states = listOf(
         createStateEntry("key1", clock.instant().minusMillis(window * 2), FlowMapperStateType.CLOSING),
-        createStateEntry("key2", clock.instant(), FlowMapperStateType.CLOSING),
-        createStateEntry("key3", clock.instant().minusMillis(window * 2), FlowMapperStateType.OPEN),
-        createStateEntry("key4", clock.instant().minusMillis(window * 3), FlowMapperStateType.CLOSING),
-        createStateEntry("key5", clock.instant().minusMillis(window * 2), null)
+        createStateEntry("key4", clock.instant().minusMillis(window * 3), FlowMapperStateType.CLOSING)
     ).toMap()
 
     @Test
     fun `when scheduled task handler generates new records, ID of each retrieved state is present in output events`() {
+        val stateManager = mock<StateManager>()
+        whenever(stateManager.findUpdatedBetweenWithMetadataFilter(any(), any())).thenReturn(states)
         val scheduledTaskHandler = ScheduledTaskHandler(
-            StateManagerImpl(),
+            stateManager,
             clock,
             window
         )
         val output = scheduledTaskHandler.process()
         val ids = output.flatMap { it.ids }
         assertThat(ids).contains("key1", "key4")
+        verify(stateManager).findUpdatedBetweenWithMetadataFilter(
+            IntervalFilter(Instant.MIN, clock.instant() - Duration.ofMillis(window)),
+            SingleKeyFilter(FLOW_MAPPER_STATUS, Operation.Equals, FlowMapperStateType.CLOSING.toString())
+        )
     }
 
     @Test
     fun `when batch size is set to one, a record per id is present in output events`() {
+        val stateManager = mock<StateManager>()
+        whenever(stateManager.findUpdatedBetweenWithMetadataFilter(any(), any())).thenReturn(states)
         val scheduledTaskHandler = ScheduledTaskHandler(
-            StateManagerImpl(),
+            stateManager,
             clock,
             window,
             1
@@ -52,60 +62,19 @@ class ScheduledTaskHandlerTest {
 
     @Test
     fun `when the last updated time is far enough in the past, no records are returned`() {
+        val stateManager = mock<StateManager>()
+        whenever(stateManager.findUpdatedBetweenWithMetadataFilter(any(), any())).thenReturn(mapOf())
         val scheduledTaskHandler = ScheduledTaskHandler(
-            StateManagerImpl(),
+            stateManager,
             clock,
             window * 5
         )
         val output = scheduledTaskHandler.process()
         assertThat(output).isEmpty()
-    }
-
-    private inner class StateManagerImpl : StateManager {
-        override fun create(states: Collection<State>): Map<String, Exception> {
-            TODO("Not yet implemented")
-        }
-
-        override fun get(keys: Collection<String>): Map<String, State> {
-            TODO("Not yet implemented")
-        }
-
-        override fun update(states: Collection<State>): Map<String, State> {
-            TODO("Not yet implemented")
-        }
-
-        override fun delete(states: Collection<State>): Map<String, State> {
-            TODO("Not yet implemented")
-        }
-
-        override fun updatedBetween(intervalFilter: IntervalFilter): Map<String, State> {
-            TODO("Not yet implemented")
-        }
-
-        override fun find(singleKeyFilter: SingleKeyFilter): Map<String, State> {
-            TODO("Not yet implemented")
-        }
-
-        override fun findUpdatedBetweenWithMetadataFilter(
-            intervalFilter: IntervalFilter,
-            singleKeyFilter: SingleKeyFilter
-        ): Map<String, State> {
-            val inInterval = { state: State ->
-                state.modifiedTime.isAfter(intervalFilter.start) && state.modifiedTime.isBefore(intervalFilter.finish)
-            }
-            // For the metadata filter assume it uses equals.
-            if (singleKeyFilter.operation != Operation.Equals) {
-                throw IllegalArgumentException("Must use equals")
-            }
-            val keyFilterMatches = { state: State ->
-                state.metadata[singleKeyFilter.key] == singleKeyFilter.value
-            }
-            return states.filter { inInterval(it.value) && keyFilterMatches(it.value) }
-        }
-
-        override fun close() {
-            TODO("Not yet implemented")
-        }
+        verify(stateManager).findUpdatedBetweenWithMetadataFilter(
+            IntervalFilter(Instant.MIN, clock.instant() - Duration.ofMillis(window * 5)),
+            SingleKeyFilter(FLOW_MAPPER_STATUS, Operation.Equals, FlowMapperStateType.CLOSING.toString())
+        )
     }
 
     private fun createStateEntry(
