082c2f262140631bdb6f83a51ef66c6b3a1a1bfd
benyip123 <ben.yip@r3.com>
2023-10-02 15:30:45 +0100

    default to null connection string to cover when uniqueness is "none"

--- testing/e2e-test-utilities/src/main/kotlin/net/corda/e2etest/utilities/ClusterBuilder.kt
+++ testing/e2e-test-utilities/src/main/kotlin/net/corda/e2etest/utilities/ClusterBuilder.kt
@@ -50,12 +50,12 @@ class ClusterBuilder {
     )
 
     data class ExternalDBConnectionParams(
-        val cryptoDdlConnection: String?,
-        val cryptoDmlConnection: String?,
-        val uniquenessDdlConnection: String?,
-        val uniquenessDmlConnection: String?,
-        val vaultDdlConnection: String?,
-        val vaultDmlConnection: String?
+        val cryptoDdlConnection: String? = null,
+        val cryptoDmlConnection: String? = null,
+        val uniquenessDdlConnection: String? = null,
+        val uniquenessDmlConnection: String? = null,
+        val vaultDdlConnection: String? = null,
+        val vaultDmlConnection: String? = null
     )
 
 
