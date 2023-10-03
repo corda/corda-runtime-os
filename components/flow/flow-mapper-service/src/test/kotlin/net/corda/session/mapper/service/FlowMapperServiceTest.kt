a587022f472f8c89631f2fd0af65bd3fdfaef1d1
James Higgs <james.higgs@r3.com>
2023-10-03 16:54:06 +0100

    CORE-17447: Fix broken unit tests

--- components/flow/flow-mapper-service/src/test/kotlin/net/corda/session/mapper/service/FlowMapperServiceTest.kt
+++ components/flow/flow-mapper-service/src/test/kotlin/net/corda/session/mapper/service/FlowMapperServiceTest.kt
@@ -3,20 +3,24 @@ package net.corda.session.mapper.service
 import com.typesafe.config.ConfigFactory
 import com.typesafe.config.ConfigValueFactory
 import net.corda.configuration.read.ConfigurationReadService
+import net.corda.data.flow.event.mapper.ExecuteCleanup
 import net.corda.data.flow.event.mapper.FlowMapperEvent
 import net.corda.data.flow.state.mapper.FlowMapperState
+import net.corda.data.scheduler.ScheduledTaskTrigger
 import net.corda.libs.configuration.SmartConfigFactory
 import net.corda.libs.configuration.SmartConfigImpl
 import net.corda.lifecycle.LifecycleCoordinatorName
 import net.corda.lifecycle.test.impl.LifecycleTest
 import net.corda.messaging.api.exception.CordaMessageAPIConfigException
 import net.corda.messaging.api.subscription.StateAndEventSubscription
+import net.corda.messaging.api.subscription.Subscription
 import net.corda.messaging.api.subscription.factory.SubscriptionFactory
 import net.corda.schema.configuration.ConfigKeys.FLOW_CONFIG
 import net.corda.schema.configuration.ConfigKeys.MESSAGING_CONFIG
 import net.corda.schema.configuration.FlowConfig
 import org.junit.jupiter.api.Test
 import org.mockito.kotlin.any
+import org.mockito.kotlin.anyOrNull
 import org.mockito.kotlin.doAnswer
 import org.mockito.kotlin.eq
 import org.mockito.kotlin.mock
@@ -28,6 +32,7 @@ internal class FlowMapperServiceTest {
 
     private val configFactory = SmartConfigFactory.createWithoutSecurityServices()
     private val flowConfig = SmartConfigImpl.empty().withValue(FlowConfig.SESSION_P2P_TTL, ConfigValueFactory.fromAnyRef(10000))
+        .withValue(FlowConfig.PROCESSING_FLOW_CLEANUP_TIME, ConfigValueFactory.fromAnyRef(1000L))
 
     private val messagingConfig = configFactory.create(
         ConfigFactory.parseString(
@@ -40,6 +45,8 @@ internal class FlowMapperServiceTest {
         val subscriptionFactory = mock<SubscriptionFactory>().also {
             doAnswer { mock<StateAndEventSubscription<String, String, String>>() }
                 .whenever(it).createStateAndEventSubscription<String, String, String>(any(), any(), any(), any())
+            doAnswer { mock<Subscription<String, String>>() }
+                .whenever(it).createDurableSubscription<String, String>(any(), any(), any(), anyOrNull())
         }
         LifecycleTest {
             addDependency<ConfigurationReadService>()
@@ -84,9 +91,21 @@ internal class FlowMapperServiceTest {
         val subscription = mock<StateAndEventSubscription<String, FlowMapperState, FlowMapperEvent>>().apply {
             whenever(subscriptionName).thenReturn(subName)
         }
+        val scheduledTaskSubName = LifecycleCoordinatorName("scheduledSub")
+        val scheduledTaskSubscription = mock<Subscription<String, ScheduledTaskTrigger>>().apply {
+            whenever(subscriptionName).thenReturn(scheduledTaskSubName)
+        }
+        val cleanupTaskSubName = LifecycleCoordinatorName("cleanup")
+        val cleanupSubscription = mock<Subscription<String, ExecuteCleanup>>().apply {
+            whenever(subscriptionName).thenReturn(cleanupTaskSubName)
+        }
         val subscriptionFactory = mock<SubscriptionFactory>().apply {
             whenever(createStateAndEventSubscription<String, FlowMapperState, FlowMapperEvent>(any(), any(), any(), any()))
                 .thenReturn(subscription)
+            whenever(createDurableSubscription<String, ScheduledTaskTrigger>(any(), any(), any(), anyOrNull()))
+                .thenReturn(scheduledTaskSubscription)
+            whenever(createDurableSubscription<String, ExecuteCleanup>(any(), any(), any(), anyOrNull()))
+                .thenReturn(cleanupSubscription)
         }
 
         LifecycleTest {
