83007bf6e4fbd08dd71c0f1aee72bb021c2e2fe1
ben-millar <ben.millar@r3.com>
2023-10-04 18:24:08 +0100

    CORE-16181 Adding RPCClient along with associated factories and unit tests

--- libs/messaging/messaging-impl/src/test/kotlin/net/corda/messaging/mediator/factory/MessagingClientFactoryFactoryTest.kt
+++ libs/messaging/messaging-impl/src/test/kotlin/net/corda/messaging/mediator/factory/MessagingClientFactoryFactoryTest.kt
@@ -1,5 +1,6 @@
 package net.corda.messaging.mediator.factory
 
+import net.corda.avro.serialization.CordaAvroSerializationFactory
 import net.corda.libs.configuration.SmartConfig
 import net.corda.messagebus.api.producer.builder.CordaProducerBuilder
 import org.junit.jupiter.api.Assertions
@@ -10,12 +11,14 @@ import org.mockito.kotlin.mock
 class MessagingClientFactoryFactoryTest {
     private lateinit var messagingClientFactoryFactory: MessagingClientFactoryFactoryImpl
     private val cordaProducerBuilder = mock<CordaProducerBuilder>()
+    private val cordaAvroSerializationFactory = mock<CordaAvroSerializationFactory>()
     private val messageBusConfig = mock<SmartConfig>()
 
     @BeforeEach
     fun beforeEach() {
         messagingClientFactoryFactory = MessagingClientFactoryFactoryImpl(
             cordaProducerBuilder,
+            cordaAvroSerializationFactory
         )
     }
 
@@ -27,5 +30,14 @@ class MessagingClientFactoryFactoryTest {
         )
 
         Assertions.assertNotNull(messageBusClientFactory)
+    }
+
+    @Test
+    fun testCreateRPCClientFactory() {
+        val rpcClientFactory = messagingClientFactoryFactory.createRPCClientFactory(
+            "rpcClient1"
+        )
+
+        Assertions.assertNotNull(rpcClientFactory)
     }
 }
