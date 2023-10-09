7c9f64d882745c427927802f2b945e0de16f84df
Christian Sailer <christian.sailer@r3.com>
2023-10-09 08:45:16 +0100

    Remove the simulator

--- simulator/api/src/main/kotlin/net/corda/simulator/exceptions/NoSuchMemberException.kt
+++
@@ -1,8 +0,0 @@
-package net.corda.simulator.exceptions
-
-import net.corda.v5.base.exceptions.CordaRuntimeException
-import net.corda.v5.base.types.MemberX500Name
-
-class NoSuchMemberException(name: MemberX500Name) : CordaRuntimeException(
-    "No member with name \"${name}\" registered"
-)
