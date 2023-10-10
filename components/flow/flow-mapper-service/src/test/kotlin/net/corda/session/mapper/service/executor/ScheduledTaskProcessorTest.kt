d88b20ad11c4e0a540e84afb673fe5143e887b6c
James Higgs <45565019+JamesHR3@users.noreply.github.com>
2023-10-10 18:09:16 +0100

    CORE-17447: Add subscriptions for cleanup processors in flow mapper (#4776)
    Adds subscriptions for the cleanup tasks in the flow mapper, and integrates the state manager into the flow mapper worker.
    
    Two new durable subscriptions are added to the flow mapper service. One of these processes scheduled task triggers and identifies states to clean up, which are forwarded to the other subscription (to fan out this work). The other then performs the clean up. The state manager is required for these processors to function, so this is also integrated into the flow mapper worker.
    
    Additionally, some integration tests have had timeouts increased to avoid test flakiness.

--- components/flow/flow-mapper-service/src/test/kotlin/net/corda/session/mapper/service/executor/ScheduledTaskProcessorTest.kt
+++ components/flow/flow-mapper-service/src/test/kotlin/net/corda/session/mapper/service/executor/ScheduledTaskProcessorTest.kt
@@ -1,17 +1,22 @@
 package net.corda.session.mapper.service.executor
 
+import net.corda.data.flow.event.mapper.ExecuteCleanup
 import net.corda.data.flow.state.mapper.FlowMapperStateType
+import net.corda.data.scheduler.ScheduledTaskTrigger
 import net.corda.libs.statemanager.api.IntervalFilter
 import net.corda.libs.statemanager.api.Operation
 import net.corda.libs.statemanager.api.MetadataFilter
 import net.corda.libs.statemanager.api.State
 import net.corda.libs.statemanager.api.StateManager
 import net.corda.libs.statemanager.api.metadata
+import net.corda.messaging.api.records.Record
+import net.corda.schema.Schemas
 import net.corda.session.mapper.service.state.StateMetadataKeys.FLOW_MAPPER_STATUS
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.mockito.kotlin.any
 import org.mockito.kotlin.mock
+import org.mockito.kotlin.never
 import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
 import java.time.Clock
@@ -19,29 +24,34 @@ import java.time.Duration
 import java.time.Instant
 import java.time.ZoneId
 
-class ScheduledTaskHandlerTest {
+class ScheduledTaskProcessorTest {
 
     private val clock = Clock.fixed(Instant.now(), ZoneId.systemDefault())
     private val window = 1000L
     private val states = listOf(
-        createStateEntry("key1", clock.instant().minusMillis(window * 2), FlowMapperStateType.CLOSING),
-        createStateEntry("key4", clock.instant().minusMillis(window * 3), FlowMapperStateType.CLOSING)
+        createStateEntry("key1", clock.instant().minusMillis(window * 2)),
+        createStateEntry("key4", clock.instant().minusMillis(window * 3))
     ).toMap()
+    private val inputEvent = Record(
+        Schemas.ScheduledTask.SCHEDULED_TASK_TOPIC_MAPPER_PROCESSOR,
+        "foo",
+        ScheduledTaskTrigger(Schemas.ScheduledTask.SCHEDULED_TASK_NAME_MAPPER_CLEANUP, clock.instant())
+    )
 
     @Test
     fun `when scheduled task handler generates new records, ID of each retrieved state is present in output events`() {
         val stateManager = mock<StateManager>()
         whenever(stateManager.findUpdatedBetweenWithMetadataFilter(any(), any())).thenReturn(states)
-        val scheduledTaskHandler = ScheduledTaskHandler(
+        val scheduledTaskProcessor = ScheduledTaskProcessor(
             stateManager,
             clock,
             window
         )
-        val output = scheduledTaskHandler.process()
-        val ids = output.flatMap { it.ids }
+        val output = scheduledTaskProcessor.onNext(listOf(inputEvent))
+        val ids = output.flatMap { (it.value as ExecuteCleanup).ids }
         assertThat(ids).contains("key1", "key4")
         verify(stateManager).findUpdatedBetweenWithMetadataFilter(
-            IntervalFilter(Instant.MIN, clock.instant() - Duration.ofMillis(window)),
+            IntervalFilter(Instant.EPOCH, clock.instant() - Duration.ofMillis(window)),
             MetadataFilter(FLOW_MAPPER_STATUS, Operation.Equals, FlowMapperStateType.CLOSING.toString())
         )
     }
@@ -50,13 +60,13 @@ class ScheduledTaskHandlerTest {
     fun `when batch size is set to one, a record per id is present in output events`() {
         val stateManager = mock<StateManager>()
         whenever(stateManager.findUpdatedBetweenWithMetadataFilter(any(), any())).thenReturn(states)
-        val scheduledTaskHandler = ScheduledTaskHandler(
+        val scheduledTaskProcessor = ScheduledTaskProcessor(
             stateManager,
             clock,
             window,
             1
         )
-        val output = scheduledTaskHandler.process()
+        val output = scheduledTaskProcessor.onNext(listOf(inputEvent))
         assertThat(output.size).isEqualTo(2)
     }
 
@@ -64,28 +74,45 @@ class ScheduledTaskHandlerTest {
     fun `when the last updated time is far enough in the past, no records are returned`() {
         val stateManager = mock<StateManager>()
         whenever(stateManager.findUpdatedBetweenWithMetadataFilter(any(), any())).thenReturn(mapOf())
-        val scheduledTaskHandler = ScheduledTaskHandler(
+        val scheduledTaskProcessor = ScheduledTaskProcessor(
             stateManager,
             clock,
             window * 5
         )
-        val output = scheduledTaskHandler.process()
+        val output = scheduledTaskProcessor.onNext(listOf(inputEvent))
         assertThat(output).isEmpty()
         verify(stateManager).findUpdatedBetweenWithMetadataFilter(
-            IntervalFilter(Instant.MIN, clock.instant() - Duration.ofMillis(window * 5)),
+            IntervalFilter(Instant.EPOCH, clock.instant() - Duration.ofMillis(window * 5)),
             MetadataFilter(FLOW_MAPPER_STATUS, Operation.Equals, FlowMapperStateType.CLOSING.toString())
         )
     }
 
+    @Test
+    fun `when the input record does not have the correct task name, no processing is attempted`() {
+        val stateManager = mock<StateManager>()
+        val scheduledTaskProcessor = ScheduledTaskProcessor(
+            stateManager,
+            clock,
+            window
+        )
+        val input = Record(
+            Schemas.ScheduledTask.SCHEDULED_TASK_TOPIC_MAPPER_PROCESSOR,
+            "foo",
+            ScheduledTaskTrigger("wrong-name", clock.instant())
+        )
+        val output = scheduledTaskProcessor.onNext(listOf(input))
+        assertThat(output).isEmpty()
+        verify(stateManager, never()).findUpdatedBetweenWithMetadataFilter(any(), any())
+    }
+
     private fun createStateEntry(
         key: String,
-        lastUpdated: Instant,
-        mapperState: FlowMapperStateType
+        lastUpdated: Instant
     ): Pair<String, State> {
         val state = State(
             key,
             byteArrayOf(),
-            metadata = metadata(FLOW_MAPPER_STATUS to mapperState.toString()),
+            metadata = metadata(FLOW_MAPPER_STATUS to FlowMapperStateType.CLOSING.toString()),
             modifiedTime = lastUpdated
         )
         return Pair(key, state)
