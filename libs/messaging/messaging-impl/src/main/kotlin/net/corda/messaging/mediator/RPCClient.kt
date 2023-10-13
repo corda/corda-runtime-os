d5dc203c08b96aed6462a8bf891c2a93bf9c276d
Ben Millar <44114751+ben-millar@users.noreply.github.com>
2023-10-13 16:45:21 +0100

    CORE-16181 Implement RPC Producer that implements RPC client (#4709)
    This PR adds a `RPCClient` which will, in the future, be used by the Multi-source Event Mediator to make calls to Corda workers via RPC. Also included is a simple `HTTPRetryExecutor` which will retry HTTP calls a configurable number of times with incremental backoff and allows for a user-specified set of retryable exceptions.

---
+++ libs/messaging/messaging-impl/src/main/kotlin/net/corda/messaging/mediator/RPCClient.kt
@@ -0,0 +1,130 @@
+package net.corda.messaging.mediator
+
+import java.io.IOException
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
+import java.util.concurrent.TimeoutException
+import net.corda.avro.serialization.CordaAvroSerializationFactory
+import net.corda.messaging.api.exception.CordaHTTPClientErrorException
+import net.corda.messaging.api.exception.CordaHTTPServerErrorException
+import net.corda.messaging.api.mediator.MediatorMessage
+import net.corda.messaging.api.mediator.MessagingClient
+import net.corda.messaging.api.mediator.MessagingClient.Companion.MSG_PROP_ENDPOINT
+import net.corda.messaging.api.records.Record
+import net.corda.messaging.utils.HTTPRetryConfig
+import net.corda.messaging.utils.HTTPRetryExecutor
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+class RPCClient(
+    override val id: String,
+    cordaAvroSerializerFactory: CordaAvroSerializationFactory,
+    private val onSerializationError: ((ByteArray) -> Unit)?,
+    httpClientFactory: () -> HttpClient = { HttpClient.newBuilder().build() },
+    private val retryConfig: HTTPRetryConfig =
+        HTTPRetryConfig.Builder()
+            .retryOn(IOException::class, TimeoutException::class)
+            .build()
+) : MessagingClient {
+    private val httpClient: HttpClient = httpClientFactory()
+    private val serializer = cordaAvroSerializerFactory.createAvroSerializer<Any> {}
+    private val deserializer = cordaAvroSerializerFactory.createAvroDeserializer({}, Record::class.java)
+
+
+    private companion object {
+        private val log: Logger = LoggerFactory.getLogger(this::class.java.enclosingClass)
+    }
+
+    override fun send(message: MediatorMessage<*>): MediatorMessage<*>? {
+        return try {
+            processMessage(message)
+        } catch (e: Exception) {
+            handleExceptions(e)
+            null
+        }
+    }
+
+    private fun processMessage(message: MediatorMessage<*>): MediatorMessage<*> {
+        val payload = serializePayload(message)
+        val request = buildHttpRequest(payload, message.endpoint())
+        val response = sendWithRetry(request)
+
+        checkResponseStatus(response.statusCode())
+
+        val deserializedResponse = deserializePayload(response.body())
+        return MediatorMessage(deserializedResponse, mutableMapOf("statusCode" to response.statusCode()))
+    }
+
+    private fun serializePayload(message: MediatorMessage<*>): ByteArray {
+        val payload = message.payload
+
+        if (payload is ByteArray) return payload
+
+         try {
+             return serializer.serialize(message.payload as Record<*, *>)!!
+        } catch (e: Exception) {
+            val errorMsg = "Failed to serialize instance of class type ${
+                message.payload?.let { it::class.java.name } ?: "null"
+            }."
+            log.error(errorMsg)
+            onSerializationError?.invoke(errorMsg.toByteArray())
+            throw(e)
+        }
+    }
+
+    private fun deserializePayload(payload: ByteArray): Record<*,*> {
+        return try {
+            deserializer.deserialize(payload)!!
+        } catch (e: Exception) {
+            val errorMsg = "Failed to deserialize payload of size ${payload.size} bytes due to: ${e.message}"
+            log.error(errorMsg)
+            onSerializationError?.invoke(errorMsg.toByteArray())
+            throw(e)
+        }
+    }
+
+    private fun buildHttpRequest(payload: ByteArray, endpoint: String): HttpRequest {
+        return HttpRequest.newBuilder()
+            .uri(URI("http://$endpoint"))
+            .PUT(HttpRequest.BodyPublishers.ofByteArray(payload))
+            .build()
+    }
+
+    private fun sendWithRetry(request: HttpRequest): HttpResponse<ByteArray> {
+        return HTTPRetryExecutor.withConfig(retryConfig) {
+            httpClient.send(request, HttpResponse.BodyHandlers.ofByteArray())
+        }
+    }
+
+    private fun checkResponseStatus(statusCode: Int) {
+        when (statusCode) {
+            in 400..499 -> throw CordaHTTPClientErrorException(statusCode, "Server returned status code $statusCode.")
+            in 500..599 -> throw CordaHTTPServerErrorException(statusCode, "Server returned status code $statusCode.")
+        }
+    }
+
+    private fun handleExceptions(e: Exception) {
+        when (e) {
+            is IOException -> log.error("Network or IO operation error in RPCClient: ", e)
+            is InterruptedException -> log.error("Operation was interrupted in RPCClient: ", e)
+            is IllegalArgumentException -> log.error("Invalid argument provided in RPCClient call: ", e)
+            is SecurityException -> log.error("Security violation detected in RPCClient: ", e)
+            is IllegalStateException -> log.error("Coroutine state error in RPCClient: ", e)
+            is CordaHTTPClientErrorException -> log.error("Client-side HTTP error in RPCClient: ", e)
+            is CordaHTTPServerErrorException -> log.error("Server-side HTTP error in RPCClient: ", e)
+            else -> log.error("Unhandled exception in RPCClient: ", e)
+        }
+
+        throw e
+    }
+
+    override fun close() {
+        // Nothing to do here
+    }
+
+    private fun MediatorMessage<*>.endpoint(): String {
+        return getProperty<String>(MSG_PROP_ENDPOINT)
+    }
+}
