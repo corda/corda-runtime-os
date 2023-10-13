6a11aa927b0415bafbe088e85aa2ad4250bd8baa
Miljenko Brkic <miljenko.brkic@r3.com>
2023-10-13 03:36:40 +0100

    CORE-16203 Removed coroutine usage from Multi-Source Event Mediator.

--- libs/messaging/messaging/src/main/kotlin/net/corda/messaging/api/mediator/MessagingClient.kt
+++ libs/messaging/messaging/src/main/kotlin/net/corda/messaging/api/mediator/MessagingClient.kt
@@ -1,7 +1,5 @@
 package net.corda.messaging.api.mediator
 
-import kotlinx.coroutines.Deferred
-
 /**
  * Multi-source event mediator messaging client.
  */
@@ -20,11 +18,10 @@ interface MessagingClient : AutoCloseable {
     val id: String
 
     /**
-     * Asynchronously sends a generic [MediatorMessage], and returns any result/error through a [Deferred] response.
+     * Sends a generic [MediatorMessage] and returns any result/error through a response.
      *
      * @param message The [MediatorMessage] to send.
-     * @return [Deferred] instance representing the asynchronous computation result, or null if the destination doesn't
-     * provide a response.
+     * @return Computation result, or null if the destination doesn't provide a response.
      * */
-    fun send(message: MediatorMessage<*>): Deferred<MediatorMessage<*>?>
+    fun send(message: MediatorMessage<*>): MediatorMessage<*>?
 }
