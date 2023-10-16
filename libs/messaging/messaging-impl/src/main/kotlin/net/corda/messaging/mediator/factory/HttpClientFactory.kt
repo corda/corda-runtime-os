7a91a8fe18b1f149065307cf1210ec01160570e2
ben-millar <ben.millar@r3.com>
2023-10-16 16:18:21 +0100

    CORE-16181 Adding timeout to HTTPClient

--- libs/messaging/messaging-impl/src/main/kotlin/net/corda/messaging/mediator/factory/HttpClientFactory.kt
+++ libs/messaging/messaging-impl/src/main/kotlin/net/corda/messaging/mediator/factory/HttpClientFactory.kt
@@ -1,10 +1,13 @@
 package net.corda.messaging.mediator.factory
 
 import java.net.http.HttpClient
+import java.time.Duration
 
 object HttpClientFactory {
     private val httpClient: HttpClient by lazy {
-        HttpClient.newBuilder().build()
+        HttpClient.newBuilder()
+            .connectTimeout(Duration.ofSeconds(30))
+            .build()
     }
 
     fun getClient(): HttpClient = httpClient
