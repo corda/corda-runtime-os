3b2aeed18a3ca01ff567f7bc4c3d09821ba555d4
Thiago Viana <3837906+thiagoviana@users.noreply.github.com>
2023-10-16 09:30:09 +0100

    feat (tests) create smoke baseline test class for Crypto RPC
    Based on UniquenessCheckerRPCSmokeTests

---
+++ applications/workers/workers-smoketest/src/smokeTest/kotlin/net/corda/applications/workers/smoketest/services/CryptoRPCSmokeTests.kt
@@ -0,0 +1,205 @@
+package net.corda.applications.workers.smoketest.services
+
+import net.corda.applications.workers.smoketest.utils.PLATFORM_VERSION
+import net.corda.data.KeyValuePair
+import net.corda.data.KeyValuePairList
+import net.corda.data.crypto.wire.CryptoRequestContext
+import net.corda.data.crypto.wire.CryptoResponseContext
+import net.corda.data.crypto.wire.ops.flow.FlowOpsRequest
+import net.corda.data.crypto.wire.ops.flow.FlowOpsResponse
+import net.corda.data.flow.event.FlowEvent
+import net.corda.data.flow.event.external.ExternalEventResponse
+import net.corda.e2etest.utilities.DEFAULT_CLUSTER
+import net.corda.e2etest.utilities.TEST_NOTARY_CPB_LOCATION
+import net.corda.e2etest.utilities.TEST_NOTARY_CPI_NAME
+import net.corda.e2etest.utilities.conditionallyUploadCordaPackage
+import net.corda.e2etest.utilities.conditionallyUploadCpiSigningCertificate
+import net.corda.e2etest.utilities.getHoldingIdShortHash
+import net.corda.e2etest.utilities.getOrCreateVirtualNodeFor
+import net.corda.e2etest.utilities.registerStaticMember
+import net.corda.messagebus.kafka.serialization.CordaAvroSerializationFactoryImpl
+import net.corda.schema.registry.impl.AvroSchemaRegistryImpl
+import net.corda.test.util.time.AutoTickTestClock
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.slf4j.LoggerFactory
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
+import java.time.Duration
+import java.time.Instant
+import java.util.UUID
+
+/**
+ * Tests for the Crypto RPC service
+ */
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class CryptoRPCSmokeTests {
+    private val httpClient: HttpClient = HttpClient.newBuilder()
+        .connectTimeout(Duration.ofSeconds(30))
+        .build()
+    private val serializationFactory = CordaAvroSerializationFactoryImpl(
+        AvroSchemaRegistryImpl()
+    )
+
+    private val avroSerializer = serializationFactory.createAvroSerializer<FlowOpsRequest> { }
+    private val avroFlowEventDeserializer = serializationFactory.createAvroDeserializer({}, FlowEvent::class.java)
+    private val avroCryptoDeserializer = serializationFactory.createAvroDeserializer({}, FlowOpsResponse::class.java)
+
+    companion object {
+        const val TEST_CPI_NAME = "ledger-utxo-demo-app"
+        const val TEST_CPB_LOCATION = "/META-INF/ledger-utxo-demo-app.cpb"
+        const val NOTARY_SERVICE_X500 = "O=MyNotaryService, L=London, C=GB"
+
+        val logger = LoggerFactory.getLogger(this::class.java.enclosingClass)
+    }
+
+    private val testRunUniqueId = UUID.randomUUID()
+    private val groupId = UUID.randomUUID().toString()
+    private val cpiName = "${TEST_CPI_NAME}_$testRunUniqueId"
+    private val notaryCpiName = "${TEST_NOTARY_CPI_NAME}_$testRunUniqueId"
+
+    private val aliceX500 = "CN=Alice-${testRunUniqueId}, OU=Application, O=R3, L=London, C=GB"
+    private val bobX500 = "CN=Bob-${testRunUniqueId}, OU=Application, O=R3, L=London, C=GB"
+    private val charlieX500 = "CN=Charlie-${testRunUniqueId}, OU=Application, O=R3, L=London, C=GB"
+    private val notaryX500 = "CN=Notary-${testRunUniqueId}, OU=Application, O=R3, L=London, C=GB"
+
+    private val aliceHoldingId: String = getHoldingIdShortHash(aliceX500, groupId)
+    private val bobHoldingId: String = getHoldingIdShortHash(bobX500, groupId)
+    private val charlieHoldingId: String = getHoldingIdShortHash(charlieX500, groupId)
+    private val notaryHoldingId: String = getHoldingIdShortHash(notaryX500, groupId)
+
+    private var cryptoRequestContext: CryptoRequestContext = createRequestContext()
+    private var tenantId: String = UUID.randomUUID().toString()
+
+    private val staticMemberList = listOf(
+        aliceX500,
+        bobX500,
+        charlieX500,
+        notaryX500
+    )
+
+    @BeforeAll
+    fun beforeAll() {
+        DEFAULT_CLUSTER.conditionallyUploadCpiSigningCertificate()
+
+        conditionallyUploadCordaPackage(
+            cpiName,
+            TEST_CPB_LOCATION,
+            groupId,
+            staticMemberList
+        )
+        conditionallyUploadCordaPackage(
+            notaryCpiName,
+            TEST_NOTARY_CPB_LOCATION,
+            groupId,
+            staticMemberList
+        )
+
+        val aliceActualHoldingId = getOrCreateVirtualNodeFor(aliceX500, cpiName)
+        val bobActualHoldingId = getOrCreateVirtualNodeFor(bobX500, cpiName)
+        val charlieActualHoldingId = getOrCreateVirtualNodeFor(charlieX500, cpiName)
+        val notaryActualHoldingId = getOrCreateVirtualNodeFor(notaryX500, notaryCpiName)
+
+        assertThat(aliceActualHoldingId).isEqualTo(aliceHoldingId)
+        assertThat(bobActualHoldingId).isEqualTo(bobHoldingId)
+        assertThat(charlieActualHoldingId).isEqualTo(charlieHoldingId)
+        assertThat(notaryActualHoldingId).isEqualTo(notaryHoldingId)
+
+        registerStaticMember(aliceHoldingId)
+        registerStaticMember(bobHoldingId)
+        registerStaticMember(charlieHoldingId)
+        registerStaticMember(notaryHoldingId, NOTARY_SERVICE_X500)
+    }
+
+    @Test
+    fun `RPC endpoint accepts a request and returns back a response`() {
+        val url = "${System.getProperty("cryptoWorkerUrl")}api/$PLATFORM_VERSION/crypto"
+
+        logger.info("crypto url: $url")
+        val serializedPayload = avroSerializer.serialize(payloadBuilder().build())
+
+        val request = HttpRequest.newBuilder()
+            .uri(URI.create(url))
+            .headers("Content-Type", "application/octet-stream")
+            .POST(HttpRequest.BodyPublishers.ofByteArray(serializedPayload))
+            .build()
+        val response = httpClient.send(request, HttpResponse.BodyHandlers.ofByteArray())
+
+        assertThat(response.statusCode()).isEqualTo(200).withFailMessage("status code on response: ${response.statusCode()} url: $url")
+
+        val responseBody: ByteArray = response.body()
+        val responseEvent = avroFlowEventDeserializer.deserialize(responseBody)
+
+        assertThat(responseEvent).isNotNull
+
+        val deserializedExternalEventResponse = avroCryptoDeserializer.deserialize((responseEvent?.payload as ExternalEventResponse).payload.array())
+
+        assertThat(deserializedExternalEventResponse).isNotNull
+        assertStandardSuccessResponse(deserializedExternalEventResponse!!, testClock)
+        assertResponseContext(cryptoRequestContext, deserializedExternalEventResponse.context)
+    }
+
+    private val testClock = AutoTickTestClock(Instant.MAX, Duration.ofSeconds(1))
+
+    // TODO: Create a proper builder
+    private fun payloadBuilder() : FlowOpsRequest.Builder =
+        FlowOpsRequest.newBuilder(
+            FlowOpsRequest()
+        )
+
+    private fun createRequestContext(): CryptoRequestContext = CryptoRequestContext(
+        "test-component",
+        Instant.now(),
+        UUID.randomUUID().toString(),
+        tenantId,
+        KeyValuePairList(
+            listOf(
+                KeyValuePair("key1", "value1"),
+                KeyValuePair("key2", "value2")
+            )
+        )
+    )
+
+    // TODO: Move all methods below to a CryptoAssertions utility class
+    private fun assertResponseContext(expected: CryptoRequestContext, actual: CryptoResponseContext) {
+        val now = Instant.now()
+        assertEquals(expected.tenantId, actual.tenantId)
+        assertEquals(expected.requestId, actual.requestId)
+        assertEquals(expected.requestingComponent, actual.requestingComponent)
+        assertEquals(expected.requestTimestamp, actual.requestTimestamp)
+        assertThat(actual.responseTimestamp.epochSecond)
+            .isGreaterThanOrEqualTo(expected.requestTimestamp.epochSecond)
+            .isLessThanOrEqualTo(now.epochSecond)
+        assertTrue(
+            actual.other.items.size == expected.other.items.size &&
+                    actual.other.items.containsAll(expected.other.items) &&
+                    expected.other.items.containsAll(actual.other.items)
+        )
+    }
+
+    private fun assertStandardSuccessResponse(
+        response: FlowOpsResponse,
+        clock: AutoTickTestClock? = null
+    ) = getResultOfType<FlowOpsResponse>(response)
+        .run { assertValidTimestamp(response.context.requestTimestamp, clock) }
+
+    private inline fun <reified T> getResultOfType(response: FlowOpsResponse): T {
+        Assertions.assertInstanceOf(T::class.java, response)
+        @Suppress("UNCHECKED_CAST")
+        return response as T
+    }
+
+    private fun assertValidTimestamp(timestamp: Instant, clock: AutoTickTestClock? = null) {
+        assertThat(timestamp).isAfter(Instant.MIN)
+        if (clock != null) {
+            assertThat(timestamp).isBeforeOrEqualTo(clock.peekTime())
+        }
+    }
+}
