03aee14e085e11c28db7ded48e70fd9804f4e1b9
ben-millar <ben.millar@r3.com>
2023-10-16 12:46:18 +0100

    CORE-16181 Removing ser/des from RPCClient as its handled at subscription level

--- libs/messaging/messaging-impl/src/main/kotlin/net/corda/messaging/mediator/factory/MessagingClientFactoryFactoryImpl.kt
+++ libs/messaging/messaging-impl/src/main/kotlin/net/corda/messaging/mediator/factory/MessagingClientFactoryFactoryImpl.kt
@@ -1,6 +1,5 @@
 package net.corda.messaging.mediator.factory
 
-import net.corda.avro.serialization.CordaAvroSerializationFactory
 import net.corda.libs.configuration.SmartConfig
 import net.corda.messagebus.api.producer.builder.CordaProducerBuilder
 import net.corda.messaging.api.mediator.factory.MessagingClientFactoryFactory
@@ -15,8 +14,6 @@ import org.osgi.service.component.annotations.Reference
 class MessagingClientFactoryFactoryImpl @Activate constructor(
     @Reference(service = CordaProducerBuilder::class)
     private val cordaProducerBuilder: CordaProducerBuilder,
-    @Reference(service = CordaAvroSerializationFactory::class)
-    private val cordaSerializationFactory: CordaAvroSerializationFactory
 ): MessagingClientFactoryFactory {
     override fun createMessageBusClientFactory(
         id: String,
@@ -30,7 +27,6 @@ class MessagingClientFactoryFactoryImpl @Activate constructor(
     override fun createRPCClientFactory(
         id: String
     ) = RPCClientFactory(
-        id,
-        cordaSerializationFactory
+        id
     )
 }
--- libs/messaging/messaging-impl/src/test/kotlin/net/corda/messaging/mediator/factory/RPCClientFactoryTest.kt
+++ libs/messaging/messaging-impl/src/test/kotlin/net/corda/messaging/mediator/factory/RPCClientFactoryTest.kt
@@ -1,22 +1,17 @@
 package net.corda.messaging.mediator.factory
 
-import net.corda.avro.serialization.CordaAvroSerializationFactory
 import net.corda.messaging.api.mediator.config.MessagingClientConfig
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.mockito.Mockito.mock
 
 class RPCClientFactoryTest {
-    private lateinit var cordaSerializationFactory: CordaAvroSerializationFactory
     private lateinit var rpcClientFactory: RPCClientFactory
 
     @BeforeEach
     fun beforeEach() {
-        cordaSerializationFactory = mock(CordaAvroSerializationFactory::class.java)
         rpcClientFactory = RPCClientFactory(
-            "RPCClient1",
-            mock(CordaAvroSerializationFactory::class.java)
+            "RPCClient1"
         )
     }
 
