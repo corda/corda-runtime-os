f674a178ed030a8d85e40231f4ec2e78d5e6aaea
James Higgs <45565019+JamesHR3@users.noreply.github.com>
2023-10-18 11:06:51 +0100

    CORE-17882: Add a route for flow events (#4915)
    Adds a route for flow events to the flow worker event mediator.

--- components/flow/flow-service/src/main/kotlin/net/corda/flow/service/NewConfigurationReceived.kt
+++
@@ -1,6 +0,0 @@
-package net.corda.flow.service
-
-import net.corda.libs.configuration.SmartConfig
-import net.corda.lifecycle.LifecycleEvent
-
-data class NewConfigurationReceived(val config: SmartConfig) : LifecycleEvent
