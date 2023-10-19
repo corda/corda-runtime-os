d78533b57233f9e65fda109df33d88166e7887a4
Miljenko Brkic <miljenko.brkic@r3.com>
2023-10-19 18:46:24 +0100

    CORE-17768 Added logging for debugging purposes

--- libs/messaging/messaging-impl/src/main/kotlin/net/corda/messaging/mediator/ClientTask.kt
+++ libs/messaging/messaging-impl/src/main/kotlin/net/corda/messaging/mediator/ClientTask.kt
@@ -1,9 +1,13 @@
 package net.corda.messaging.mediator
 
+import net.corda.data.flow.event.FlowEvent
+import net.corda.data.flow.event.mapper.FlowMapperEvent
 import net.corda.messaging.api.mediator.MediatorMessage
 import net.corda.messaging.api.mediator.MessageRouter
 import net.corda.messaging.api.mediator.MessagingClient
 import net.corda.messaging.api.mediator.MessagingClient.Companion.MSG_PROP_ENDPOINT
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import java.util.concurrent.Callable
 
 /**
@@ -14,6 +18,9 @@ data class ClientTask<K : Any, S : Any, E : Any>(
     private val messageRouter: MessageRouter,
     val processorTaskResult: ProcessorTask.Result<K, S, E>,
 ) : Callable<ClientTask.Result<K, S, E>> {
+    private companion object {
+        private val log: Logger = LoggerFactory.getLogger(this::class.java.enclosingClass)
+    }
 
     class Result<K : Any, S : Any, E : Any>(
         val clientTask: ClientTask<K, S, E>,
@@ -27,6 +34,28 @@ data class ClientTask<K : Any, S : Any, E : Any>(
 
     override fun call(): Result<K, S, E> {
         val destination = messageRouter.getDestination(message)
+
+        // TODO remove logging that was added for debug purposes
+        val key = processorTaskResult.key
+        when (val eventValue = message.payload) {
+            is FlowEvent -> {
+                val eventType = eventValue.payload::class.java.simpleName
+                log.info("Sending event: FlowEvent:$eventType [${key}] to [${destination.endpoint}]")
+            }
+
+            is FlowMapperEvent -> {
+                val eventType = eventValue.payload
+                val eventTypeName = eventType::class.java.simpleName
+                val eventSubtypeName = if (eventType is FlowEvent) ":${eventType::class.java.simpleName}" else ""
+                log.info("Sending event: FlowMapperEvent:$eventTypeName$eventSubtypeName [${key}] " +
+                        "to [${destination.endpoint}]")
+            }
+
+            else -> {
+                val eventType = eventValue?.let { it::class.java.simpleName }
+                log.info("Sending event: $eventType [${key}] to [${destination.endpoint}]")
+            }
+        }
 
         @Suppress("UNCHECKED_CAST")
         val reply = with(destination) {
--- libs/messaging/messaging-impl/src/main/kotlin/net/corda/messaging/mediator/MultiSourceEventMediatorImpl.kt
+++ libs/messaging/messaging-impl/src/main/kotlin/net/corda/messaging/mediator/MultiSourceEventMediatorImpl.kt
@@ -115,8 +115,9 @@ class MultiSourceEventMediatorImpl<K : Any, S : Any, E : Any>(
                         log.error(
                             "${exception.message} Attempts: $attempts. Closing Multi-Source Event Mediator.", exception
                         )
-                        lifecycleCoordinator.updateStatus(LifecycleStatus.ERROR, "Error: ${exception.message}")
-                        stop()
+                        // TODO Temporarily disabled for testing purposes
+//                        lifecycleCoordinator.updateStatus(LifecycleStatus.ERROR, "Error: ${exception.message}")
+//                        stop()
                     }
                 }
             } finally {
