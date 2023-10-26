a22c9cb27f0b61b59c75d9339e63106a3a694a48
Owen Stanford <92725587+owenstanford@users.noreply.github.com>
2023-10-26 17:19:14 +0100

    CORE-18014 When a token selection process request fails, due to a state concurrency check, we need to clear the available token cache as it could be invalid. (#4987)

--- components/ledger/ledger-utxo-token-cache/src/main/kotlin/net/corda/ledger/utxo/token/cache/services/ClaimStateStoreFactoryImpl.kt
+++ components/ledger/ledger-utxo-token-cache/src/main/kotlin/net/corda/ledger/utxo/token/cache/services/ClaimStateStoreFactoryImpl.kt
@@ -1,5 +1,6 @@
 package net.corda.ledger.utxo.token.cache.services
 
+import net.corda.ledger.utxo.token.cache.entities.TokenPoolCache
 import net.corda.ledger.utxo.token.cache.entities.TokenPoolKey
 import net.corda.libs.statemanager.api.StateManager
 import net.corda.utilities.time.Clock
@@ -7,10 +8,18 @@ import net.corda.utilities.time.Clock
 class ClaimStateStoreFactoryImpl(
     private val stateManager: StateManager,
     private val serialization: TokenPoolCacheStateSerialization,
+    private val tokenPoolCache: TokenPoolCache,
     private val clock: Clock
 ) : ClaimStateStoreFactory {
 
     override fun create(key: TokenPoolKey, storedPoolClaimState: StoredPoolClaimState): ClaimStateStore {
-        return PerformanceClaimStateStoreImpl(key, storedPoolClaimState, serialization, stateManager, clock)
+        return PerformanceClaimStateStoreImpl(
+            key,
+            storedPoolClaimState,
+            serialization,
+            stateManager,
+            tokenPoolCache,
+            clock
+        )
     }
 }
