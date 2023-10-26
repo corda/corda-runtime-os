9d0bff7a16ab01bd5004d5a2f9d2b6d025af2db7
LWogan <lorcan.wogan@r3.com>
2023-10-26 17:08:18 +0100

    CORE-17958 update integration tests

--- components/flow/flow-service/src/integrationTest/kotlin/net/corda/flow/testing/context/FlowIoRequestBuilders.kt
+++ components/flow/flow-service/src/integrationTest/kotlin/net/corda/flow/testing/context/FlowIoRequestBuilders.kt
@@ -1,12 +1,16 @@
 package net.corda.flow.testing.context
 
-import net.corda.data.flow.FlowKey
 import net.corda.flow.testing.tests.ALICE_HOLDING_IDENTITY
+import net.corda.flow.testing.tests.BOB_HOLDING_IDENTITY
+import net.corda.flow.testing.tests.CHARLIE_HOLDING_IDENTITY
 import net.corda.flow.testing.tests.CPI1
 import net.corda.flow.testing.tests.FLOW_ID1
 import net.corda.flow.testing.tests.REQUEST_ID1
+import net.corda.virtualnode.toCorda
 
-val ALICE_FLOW_KEY = FlowKey(REQUEST_ID1, ALICE_HOLDING_IDENTITY).toString()
+val ALICE_FLOW_KEY_MAPPER = "$REQUEST_ID1-${ALICE_HOLDING_IDENTITY.toCorda().shortHash}"
+val BOB_FLOW_KEY_MAPPER = "$REQUEST_ID1-${BOB_HOLDING_IDENTITY.toCorda().shortHash}"
+val CHARLIE_FLOW_KEY_MAPPER = "$REQUEST_ID1-${CHARLIE_HOLDING_IDENTITY.toCorda().shortHash}"
 
 /**
  * Start a flow for [ALICE_HOLDING_IDENTITY]
