a6234f26d6150d5d66153244618d00f24fc177a6
owenstanford <owen.stanford@r3.com>
2023-10-26 15:13:06 +0100

    CORE-18015 Fixed issue where byte array message keys were not added to the outgoing HTTP RPC request header correctly.

--- libs/messaging/messaging-impl/src/main/kotlin/net/corda/messaging/mediator/factory/RPCClientFactory.kt
+++ libs/messaging/messaging-impl/src/main/kotlin/net/corda/messaging/mediator/factory/RPCClientFactory.kt
@@ -3,6 +3,7 @@ package net.corda.messaging.mediator.factory
 import java.net.http.HttpClient
 import java.time.Duration
 import net.corda.avro.serialization.CordaAvroSerializationFactory
+import net.corda.crypto.cipher.suite.PlatformDigestService
 import net.corda.messaging.api.mediator.MessagingClient
 import net.corda.messaging.api.mediator.config.MessagingClientConfig
 import net.corda.messaging.api.mediator.factory.MessagingClientFactory
@@ -10,7 +11,8 @@ import net.corda.messaging.mediator.RPCClient
 
 class RPCClientFactory(
     private val id: String,
-    private val cordaSerializationFactory: CordaAvroSerializationFactory
+    private val cordaSerializationFactory: CordaAvroSerializationFactory,
+    private val platformDigestService: PlatformDigestService
 ): MessagingClientFactory {
     private val httpClient: HttpClient by lazy {
         HttpClient.newBuilder()
@@ -22,6 +24,7 @@ class RPCClientFactory(
         return RPCClient(
             id,
             cordaSerializationFactory,
+            platformDigestService,
             config.onSerializationError,
             httpClient
         )
