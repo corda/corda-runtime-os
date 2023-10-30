dc946de86ff32390f39898e61523a2255b5faee0
bpaunescu <bogdan.paunescu@r3.com>
2023-10-30 14:42:11 +0000

    [CORE-17540] - remove additional logging

--- libs/messaging/messaging-impl/src/main/kotlin/net/corda/messaging/publisher/factory/CordaPublisherFactory.kt
+++ libs/messaging/messaging-impl/src/main/kotlin/net/corda/messaging/publisher/factory/CordaPublisherFactory.kt
@@ -20,8 +20,6 @@ import net.corda.messaging.publisher.CordaRPCSenderImpl
 import org.osgi.service.component.annotations.Activate
 import org.osgi.service.component.annotations.Component
 import org.osgi.service.component.annotations.Reference
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
 import java.util.UUID
 
 /**
