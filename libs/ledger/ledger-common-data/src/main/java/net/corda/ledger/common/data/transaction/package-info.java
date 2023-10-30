d729471dd76257f16d113d03968d388d3e6744ed
nkovacsx <nandor.kovacs@r3.com>
2023-10-30 13:52:48 +0000

    CORE-18071: Quasar Ignore Package
    Add quasar ignore for pacakge net.corda.ledger.common.data.transaction.

--- libs/ledger/ledger-common-data/src/main/java/net/corda/ledger/common/data/transaction/package-info.java
+++ libs/ledger/ledger-common-data/src/main/java/net/corda/ledger/common/data/transaction/package-info.java
@@ -1,4 +1,6 @@
 @Export
+@QuasarIgnoreAllPackages
 package net.corda.ledger.common.data.transaction;
 
+import co.paralleluniverse.quasar.annotations.QuasarIgnoreAllPackages;
 import org.osgi.annotation.bundle.Export;
