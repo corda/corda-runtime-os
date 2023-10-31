3d3c86ebbb993227ed26df4679ce62344ae7a43a
Dries Samyn <dries.samyn@r3.com>
2023-10-31 14:04:30 +0000

    Undo metrics so class compiles.

--- libs/messaging/messaging-impl/src/main/kotlin/net/corda/messaging/mediator/RPCClient.kt
+++ libs/messaging/messaging-impl/src/main/kotlin/net/corda/messaging/mediator/RPCClient.kt
@@ -12,7 +12,6 @@ import net.corda.messaging.api.mediator.MessagingClient.Companion.MSG_PROP_ENDPO
 import net.corda.messaging.api.mediator.MessagingClient.Companion.MSG_PROP_KEY
 import net.corda.messaging.utils.HTTPRetryConfig
 import net.corda.messaging.utils.HTTPRetryExecutor
-import net.corda.metrics.CordaMetrics
 import net.corda.utilities.debug
 import net.corda.utilities.trace
 import net.corda.v5.crypto.DigestAlgorithmName
@@ -23,7 +22,6 @@ import java.net.URI
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
-import java.time.Duration
 import java.util.concurrent.TimeoutException
 
 const val CORDA_REQUEST_KEY_HEADER = "corda-request-key"
@@ -49,8 +47,8 @@ class RPCClient(
 
     private companion object {
         private val log: Logger = LoggerFactory.getLogger(this::class.java.enclosingClass)
-        private const val SUCCESS: String = "SUCCESS"
-        private const val FAILED: String = "FAILED"
+//        private const val SUCCESS: String = "SUCCESS"
+//        private const val FAILED: String = "FAILED"
     }
 
     override fun send(message: MediatorMessage<*>): MediatorMessage<*>? {
@@ -109,42 +107,42 @@ class RPCClient(
     }
 
     private fun sendWithRetry(request: HttpRequest): HttpResponse<ByteArray> {
-        val startTime = System.nanoTime()
+//        val startTime = System.nanoTime()
         return try {
             val response = HTTPRetryExecutor.withConfig(retryConfig) {
                 httpClient.send(request, HttpResponse.BodyHandlers.ofByteArray())
             }
-            buildMetricForResponse(startTime, SUCCESS, request, response)
+//            buildMetricForResponse(startTime, SUCCESS, request, response)
             response
         } catch (ex: Exception) {
             log.debug { "Catching exception in HttpClient sendWithRetry in order to log metrics, $ex" }
-            buildMetricForResponse(startTime, FAILED, request)
+//            buildMetricForResponse(startTime, FAILED, request)
             throw ex
         }
     }
 
-    private fun buildMetricForResponse(
-        startTime: Long,
-        operationStatus: String,
-        request: HttpRequest,
-        response: HttpResponse<ByteArray>? = null
-    ) {
-        val endTime = System.nanoTime()
-        val uri = request.method() + request.uri().toString()
-        CordaMetrics.Metric.Messaging.HTTPRPCResponseTime.builder()
-            .withTag(CordaMetrics.Tag.OperationStatus, operationStatus)
-            .withTag(CordaMetrics.Tag.HttpRequestUri, uri)
-            .withTag(CordaMetrics.Tag.HttpResponseCode, response?.statusCode().toString())
-            .build()
-            .record(Duration.ofNanos(endTime - startTime))
-
-        if (response != null) {
-            CordaMetrics.Metric.Messaging.HTTPRPCResponseSize.builder()
-                .withTag(CordaMetrics.Tag.HttpRequestUri, uri)
-                .build()
-                .record(response.body().size.toDouble())
-        }
-    }
+//    private fun buildMetricForResponse(
+//        startTime: Long,
+//        operationStatus: String,
+//        request: HttpRequest,
+//        response: HttpResponse<ByteArray>? = null
+//    ) {
+//        val endTime = System.nanoTime()
+//        val uri = request.method() + request.uri().toString()
+//        CordaMetrics.Metric.Messaging.HTTPRPCResponseTime.builder()
+//            .withTag(CordaMetrics.Tag.OperationStatus, operationStatus)
+//            .withTag(CordaMetrics.Tag.HttpRequestUri, uri)
+//            .withTag(CordaMetrics.Tag.HttpResponseCode, response?.statusCode().toString())
+//            .build()
+//            .record(Duration.ofNanos(endTime - startTime))
+//
+//        if (response != null) {
+//            CordaMetrics.Metric.Messaging.HTTPRPCResponseSize.builder()
+//                .withTag(CordaMetrics.Tag.HttpRequestUri, uri)
+//                .build()
+//                .record(response.body().size.toDouble())
+//        }
+//    }
 
     private fun handleExceptions(e: Exception): Nothing {
         val exceptionToThrow = when (e) {
