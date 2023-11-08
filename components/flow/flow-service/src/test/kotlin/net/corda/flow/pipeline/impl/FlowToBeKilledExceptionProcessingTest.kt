d50201786d8a4ed8a1e6c51b19b2c423dc519c3f
Emily Bowe <emily.bowe@r3.com>
2023-11-08 15:05:53 +0000

    fix test: error processing FlowMarkedForKillException falls back to null state record, empty response events and marked for DLQ

--- components/flow/flow-service/src/test/kotlin/net/corda/flow/pipeline/impl/FlowToBeKilledExceptionProcessingTest.kt
+++ components/flow/flow-service/src/test/kotlin/net/corda/flow/pipeline/impl/FlowToBeKilledExceptionProcessingTest.kt
@@ -8,8 +8,6 @@ import net.corda.data.flow.event.StartFlow
 import net.corda.data.flow.event.mapper.FlowMapperEvent
 import net.corda.data.flow.output.FlowStates
 import net.corda.data.flow.output.FlowStatus
-import net.corda.data.flow.state.session.SessionState
-import net.corda.data.flow.state.session.SessionStateType
 import net.corda.data.identity.HoldingIdentity
 import net.corda.flow.fiber.cache.FlowFiberCache
 import net.corda.flow.maintenance.CheckpointCleanupHandler
@@ -27,19 +25,11 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.mockito.kotlin.any
-import org.mockito.kotlin.eq
 import org.mockito.kotlin.mock
-import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
 
 class FlowToBeKilledExceptionProcessingTest {
-
     private companion object {
-        const val SESSION_ID_1 = "s1"
-        const val SESSION_ID_2 = "s2"
-        const val SESSION_ID_3 = "s3"
-        const val SESSION_ID_4 = "s4"
-        const val SESSION_ID_5 = "s5"
         const val FLOW_ID = "flowId"
     }
 
@@ -50,32 +40,6 @@ class FlowToBeKilledExceptionProcessingTest {
         FlowConfig.PROCESSING_MAX_RETRY_WINDOW_DURATION, ConfigValueFactory.fromAnyRef(20000L)
     )
     private val smartFlowConfig = SmartConfigFactory.createWithoutSecurityServices().create(flowConfig)
-
-    // starting session states
-    private val sessionState1 = createSessionState(SESSION_ID_1, true, SessionStateType.CLOSING)
-    private val sessionState2 = createSessionState(SESSION_ID_2, true, SessionStateType.ERROR)
-    private val sessionState3 = createSessionState(SESSION_ID_3, true, SessionStateType.CLOSED)
-    private val sessionState4 = createSessionState(SESSION_ID_4, false, SessionStateType.CONFIRMED)
-    private val sessionState5 = createSessionState(SESSION_ID_5, false, SessionStateType.CREATED)
-
-    // session states which have had their state updated to ERROR
-    private val erroredSessionState1 = createSessionState(SESSION_ID_1, false, SessionStateType.ERROR)
-    private val erroredSessionState4 = createSessionState(SESSION_ID_4, false, SessionStateType.ERROR)
-    private val erroredSessionState5 = createSessionState(SESSION_ID_5, false, SessionStateType.ERROR)
-
-    private val allFlowSessions = listOf(sessionState1, sessionState2, sessionState3, sessionState4, sessionState5)
-    private val activeFlowSessionIds = listOf(SESSION_ID_1, SESSION_ID_4, SESSION_ID_5)
-    private val allFlowSessionsAfterErrorsSent = listOf(
-        erroredSessionState1, sessionState2, sessionState3, erroredSessionState4, erroredSessionState5
-    )
-
-    private fun createSessionState(sessionId: String, hasScheduledCleanup: Boolean, status: SessionStateType): SessionState =
-        SessionState().apply {
-            this.sessionId = sessionId
-            this.hasScheduledCleanup = hasScheduledCleanup
-            this.status = status
-        }
-
     private val flowKey = FlowKey("id", HoldingIdentity("x500", "grp1"))
     private val checkpoint = mock<FlowCheckpoint> {
         whenever(it.flowKey).thenReturn(flowKey)
@@ -89,13 +53,8 @@ class FlowToBeKilledExceptionProcessingTest {
     private val flowKilledStatusRecord = Record("s", flowKey, flowKilledStatus)
     private val flowFiberCache = mock<FlowFiberCache>()
     private val checkpointCleanupHandler = mock<CheckpointCleanupHandler>()
-
     private val target = FlowEventExceptionProcessorImpl(
-        flowMessageFactory,
-        flowRecordFactory,
-        flowSessionManager,
-        flowFiberCache,
-        checkpointCleanupHandler
+        flowMessageFactory, flowRecordFactory, flowSessionManager, flowFiberCache, checkpointCleanupHandler
     )
 
     @BeforeEach
@@ -108,58 +67,46 @@ class FlowToBeKilledExceptionProcessingTest {
     fun `processing FlowMarkedForKillException calls checkpoint cleanup handler and copies cleanup records to context`() {
         val testContext = buildFlowEventContext(checkpoint, Any())
         val exception = FlowMarkedForKillException("reasoning")
-
         whenever(checkpoint.doesExist).thenReturn(true)
-
         val flowMapperEvent = mock<FlowMapperEvent>()
         val cleanupRecord = Record(Schemas.Flow.FLOW_MAPPER_SESSION_OUT, "key", flowMapperEvent)
-        val cleanupRecords = listOf (cleanupRecord)
+        val cleanupRecords = listOf(cleanupRecord)
         whenever(checkpointCleanupHandler.cleanupCheckpoint(any(), any(), any())).thenReturn(cleanupRecords)
 
         val response = target.process(exception, testContext)
 
-        assertThat(response.outputRecords)
-            .contains(cleanupRecord)
+        assertThat(response.outputRecords).contains(cleanupRecord)
     }
 
     @Test
     fun `processing FlowMarkedForKillException when checkpoint does not exist only outputs flow killed status record`() {
         whenever(checkpoint.doesExist).thenReturn(false)
-
-        val inputEventPayload = StartFlow(FlowStartContext().apply {statusKey = flowKey}, "")
-
+        val inputEventPayload = StartFlow(FlowStartContext().apply { statusKey = flowKey }, "")
         val testContext = buildFlowEventContext(checkpoint, inputEventPayload)
         val exception = FlowMarkedForKillException("reasoning")
-
         whenever(flowRecordFactory.createFlowStatusRecord(any())).thenReturn(flowKilledStatusRecord)
 
         val response = target.process(exception, testContext)
 
-        assertThat(response.outputRecords)
-            .hasSize(1)
-            .contains(flowKilledStatusRecord)
+        assertThat(response.outputRecords).hasSize(1).contains(flowKilledStatusRecord)
     }
 
     @Test
     fun `error processing FlowMarkedForKillException falls back to null state record, empty response events and marked for DLQ`() {
         val testContext = buildFlowEventContext(checkpoint, Any())
         val exception = FlowMarkedForKillException("reasoning")
-
-
         whenever(checkpoint.doesExist).thenReturn(true)
-
-        // The first call returns all sessions.
-        whenever(checkpoint.sessions)
-            .thenReturn(allFlowSessions)
-            .thenReturn(allFlowSessionsAfterErrorsSent)
-
-        // simulating exception thrown during processing of the flow session
-        whenever(flowSessionManager.sendErrorMessages(eq(checkpoint), eq(activeFlowSessionIds), eq(exception), any()))
-            .thenThrow(IllegalArgumentException("some error message while sending errors to peers"))
+        // simulating exception thrown during processing
+        whenever(
+            checkpointCleanupHandler.cleanupCheckpoint(
+                any(),
+                any(),
+                any()
+            )
+        ).thenThrow(IllegalArgumentException("some error message while sending errors to peers"))
 
         val response = target.process(exception, testContext)
 
-        verify(response.checkpoint).markDeleted()
         assertThat(response.outputRecords).isEmpty()
         assertThat(response.sendToDlq).isTrue
     }
