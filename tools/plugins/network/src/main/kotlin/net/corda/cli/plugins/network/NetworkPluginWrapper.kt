bec9f789dcd905dd17db18a2720e3f7a6f333114
Joseph Zuniga-Daly <joseph.zunigadaly@r3.com>
2023-11-13 15:26:08 +0000

    CORE-17369: Enable display of version information in CLI plugins
    Example output:
    
    ```text
    generatedScripts $ ./corda-cli.sh database --version
    db-config 5.2.0.0-SNAPSHOT
    
    Provider: R3 Ltd
    
    Commit ID: edeca54251cc7c7e088d123c1aa913c31e7a8145
    
    generatedScripts $ ./corda-cli.sh initial-config --version
    initial-config 5.2.0.0-SNAPSHOT
    
    Provider: R3 Ltd
    
    Commit ID: edeca54251cc7c7e088d123c1aa913c31e7a8145
    
    generatedScripts $ ./corda-cli.sh initial-rbac --version
    initial-rbac 5.2.0.0-SNAPSHOT
    
    Provider: R3 Ltd
    
    Commit ID: edeca54251cc7c7e088d123c1aa913c31e7a8145
    
    generatedScripts $ ./corda-cli.sh network --version
    network 5.2.0.0-SNAPSHOT
    
    Provider: R3 Ltd
    
    Commit ID: edeca54251cc7c7e088d123c1aa913c31e7a8145
    
    generatedScripts $ ./corda-cli.sh mgm --version
    network 5.2.0.0-SNAPSHOT
    
    Provider: R3 Ltd
    
    Commit ID: edeca54251cc7c7e088d123c1aa913c31e7a8145
    
    generatedScripts $ ./corda-cli.sh package --version
    package 5.2.0.0-SNAPSHOT
    
    Provider: R3 Ltd
    
    Commit ID: edeca54251cc7c7e088d123c1aa913c31e7a8145
    
    generatedScripts $ ./corda-cli.sh secret-config --version
    secret-config 5.2.0.0-SNAPSHOT
    
    Provider: R3 Ltd
    
    Commit ID: edeca54251cc7c7e088d123c1aa913c31e7a8145
    
    generatedScripts $ ./corda-cli.sh topic --version
    topic-config 5.2.0.0-SNAPSHOT
    
    Provider: R3 Ltd
    
    Commit ID: edeca54251cc7c7e088d123c1aa913c31e7a8145
    
    generatedScripts $ ./corda-cli.sh vnode --version
    virtual-node 5.2.0.0-SNAPSHOT
    
    Provider: R3 Ltd
    
    Commit ID: edeca54251cc7c7e088d123c1aa913c31e7a8145
    
    generatedScripts $ ./corda-cli.sh preinstall --version
    preinstall 5.2.0.0-SNAPSHOT
    
    Provider: R3 Ltd
    
    Commit ID: edeca54251cc7c7e088d123c1aa913c31e7a8145
    
    ```

--- tools/plugins/network/src/main/kotlin/net/corda/cli/plugins/network/NetworkPluginWrapper.kt
+++ tools/plugins/network/src/main/kotlin/net/corda/cli/plugins/network/NetworkPluginWrapper.kt
@@ -1,11 +1,14 @@
 package net.corda.cli.plugins.network
 
+import net.corda.cli.api.AbstractCordaCliVersionProvider
 import net.corda.cli.api.CordaCliPlugin
 import org.pf4j.Extension
 import org.pf4j.Plugin
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import picocli.CommandLine
+
+class VersionProvider : AbstractCordaCliVersionProvider()
 
 @Suppress("unused")
 class NetworkPluginWrapper : Plugin() {
@@ -33,7 +36,8 @@ class NetworkPluginWrapper : Plugin() {
         ],
         hidden = true,
         mixinStandardHelpOptions = true,
-        description = ["Plugin for interacting with a network."]
+        description = ["Plugin for interacting with a network."],
+        versionProvider = VersionProvider::class
     )
     class NetworkPlugin : CordaCliPlugin
 
@@ -44,7 +48,8 @@ class NetworkPluginWrapper : Plugin() {
             GenerateGroupPolicy::class,
         ],
         mixinStandardHelpOptions = true,
-        description = ["Plugin for membership operations."]
+        description = ["Plugin for membership operations."],
+        versionProvider = VersionProvider::class
     )
     class MgmPlugin : CordaCliPlugin
 }
