827228d491ddbb4c586cb12e08b57dcff6964c96
omar.awad <omar.awad@r3.com>
2023-11-14 10:40:04 +0000

    CORE-0000 Increase Sandbox Cache Size

--- components/virtual-node/sandbox-group-context-service/src/main/kotlin/net/corda/sandboxgroupcontext/service/impl/SandboxGroupContextComponentImpl.kt
+++ components/virtual-node/sandbox-group-context-service/src/main/kotlin/net/corda/sandboxgroupcontext/service/impl/SandboxGroupContextComponentImpl.kt
@@ -118,14 +118,14 @@ class SandboxGroupContextComponentImpl @Activate constructor(
         val config = event.config.getConfig(ConfigKeys.SANDBOX_CONFIG)
 
         SandboxGroupType.values().forEach {
-            val cacheSize = try {
-                config.getConfig(it.name.lowercase()).getLong(ConfigKeys.SANDBOX_CACHE_SIZE)
-            } catch (e: ConfigException.Missing) {
-                SANDBOX_CACHE_SIZE_DEFAULT
-            }
+//            val cacheSize = try {
+//                config.getConfig(it.name.lowercase()).getLong(ConfigKeys.SANDBOX_CACHE_SIZE)
+//            } catch (e: ConfigException.Missing) {
+//                SANDBOX_CACHE_SIZE_DEFAULT
+//            }
 
-            logger.info("Re-creating Sandbox ${it.name} cache with size: {}", cacheSize)
-            resizeCache(it, cacheSize)
+//            logger.info("Re-creating Sandbox ${it.name} cache with size: {}", cacheSize)
+            resizeCache(it, 40)
         }
 
         coordinator.updateStatus(LifecycleStatus.UP)
