d65e8d185c13f580fba1dbfedc51c5c8caf6d3e7
Filipe Oliveira <filipe.oliveira@r3.com>
2023-11-15 09:42:54 +0000

    Update

--- components/ledger/ledger-utxo-token-cache/src/main/kotlin/net/corda/ledger/utxo/token/cache/entities/TokenPoolCache.kt
+++ components/ledger/ledger-utxo-token-cache/src/main/kotlin/net/corda/ledger/utxo/token/cache/entities/TokenPoolCache.kt
@@ -1,8 +1,8 @@
 package net.corda.ledger.utxo.token.cache.entities
 
 /**
- * In order to improve performance, tokens are cached in memory. This allows the code to avoid
- * always going to the database. The `TokenPoolCache` class contains a cache for
+ * In order to improve performance, tokens are cached in memory. This avoids the need of always going to
+ * the database to fetch new tokens. The `TokenPoolCache` class contains a cache for
  * each group of tokens which are identified by their key.
  */
 interface TokenPoolCache {
