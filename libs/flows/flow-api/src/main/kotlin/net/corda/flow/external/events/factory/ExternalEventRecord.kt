eb7ecc02427891d3bf145cefcc70883f5e6ba56f
Emily Bowe <emily.bowe@r3.com>
2023-11-28 15:07:03 +0000

    update ExternalEventRecord kdoc

--- libs/flows/flow-api/src/main/kotlin/net/corda/flow/external/events/factory/ExternalEventRecord.kt
+++ libs/flows/flow-api/src/main/kotlin/net/corda/flow/external/events/factory/ExternalEventRecord.kt
@@ -3,7 +3,7 @@ package net.corda.flow.external.events.factory
 /**
  * [ExternalEventRecord] contains the [topic], [key] and [payload] to send to an external processor.
  *
- * @param topic The topic to send the event to.
+ * @param topic The topic to send the event to. Can be null if this ExternalEvent is not sent over kafka.
  * @param key The key of the event. If left as `null`, the calling flow's flow id is used.
  * @param payload The payload to send.
  */
