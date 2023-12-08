63a3556fe367f2c0b2276fe790487b0eb1e0aae8
Dickon Reed <dickon.reed@r3.com>
2023-12-08 13:14:41 +0000

    check a few things about the proof

--- libs/crypto/merkle-impl/src/main/kotlin/net/corda/crypto/merkle/impl/CalculateLeveledHashes.kt
+++ libs/crypto/merkle-impl/src/main/kotlin/net/corda/crypto/merkle/impl/CalculateLeveledHashes.kt
@@ -4,7 +4,9 @@ import net.corda.v5.crypto.merkle.MerkleProof
 
 
 @Suppress("unused")
-fun calculateLeveledHashes(proof: MerkleProof ): List<LeveledHash> {
-    require(proof.leaves.isNotEmpty())
+fun calculateLeveledHashes(proof: MerkleProof): List<LeveledHash> {
+    require(proof.leaves.isNotEmpty(), {"MerkleProof must have leaves"})
+    require(proof.leaves.all { it.index >= 0 && it.index < proof.treeSize }, {"MerkleProof leaves cannot point outside of the original tree."})
+    require(proof.leaves.map { it.index }.toSet().size == proof.leaves.size, {"MerkleProof leaves cannot have duplications."})
     return emptyList()
 }
