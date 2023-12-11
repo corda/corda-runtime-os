875429f1dd41fc80e24beb25d6628c76476e0915
Yash Nabar <yash.nabar@r3.com>
2023-12-11 12:27:01 +0000

    Revert "CORE-18634 Add HSM category for session encryption (#5220)"
    This reverts commit 409fb87d0434d49903cd43d175221759db9da8dd.

--- tools/plugins/network/src/main/kotlin/net/corda/cli/plugins/network/BaseOnboard.kt
+++ tools/plugins/network/src/main/kotlin/net/corda/cli/plugins/network/BaseOnboard.kt
@@ -236,7 +236,7 @@ abstract class BaseOnboard : Runnable, RestCommand() {
         shortHashId
     }
 
-    private fun assignSoftHsm(tenantId: String, category: String) {
+    protected fun assignSoftHsmAndGenerateKey(category: String): String {
         createRestClient(HsmRestResource::class).use { client ->
             checkInvariant(
                 maxAttempts = MAX_ATTEMPTS,
@@ -244,17 +244,13 @@ abstract class BaseOnboard : Runnable, RestCommand() {
                 errorMessage = "Assign Soft HSM operation for $category: failed after the maximum number of attempts ($MAX_ATTEMPTS)."
             ) {
                 try {
-                    client.start().proxy.assignSoftHsm(tenantId, category)
+                    client.start().proxy.assignSoftHsm(holdingId, category)
                 } catch (e: MissingRequestedResourceException) {
                     //This exception can be thrown while the assigning Hsm Key is being processed, so we catch it and re-try.
                     null
                 }
             }
         }
-    }
-
-    protected fun assignSoftHsmAndGenerateKey(category: String): String {
-        assignSoftHsm(holdingId, category)
 
         val response = createRestClient(KeysRestResource::class).use { keyClient ->
             keyClient.start().proxy.generateKeyPair(
@@ -353,8 +349,6 @@ abstract class BaseOnboard : Runnable, RestCommand() {
     }
 
     protected fun setupNetwork() {
-        assignSoftHsm("p2p", "ENCRYPTION_SECRET")
-
         val request = HostedIdentitySetupRequest(
             p2pTlsCertificateChainAlias = P2P_TLS_CERTIFICATE_ALIAS,
             useClusterLevelTlsCertificateAndKey = true,
