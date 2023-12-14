89db8fe3592c4812cdacf797259726c8c419f400
Lorcan Wogan <69468264+LWogan@users.noreply.github.com>
2023-12-14 16:39:33 +0000

    CORE-18935 handle completion exceptions thrown within the mediator properly (#5270)
    Can be thrown by CompletableFuture join calls. this will wrap the thrown CordaMessageAPIIntermittentExceptions in CompletionExceptions. This can erroneously treat them as fatal

--- libs/messaging/messaging-impl/src/main/kotlin/net/corda/messaging/mediator/MultiSourceEventMediatorImpl.kt
+++ libs/messaging/messaging-impl/src/main/kotlin/net/corda/messaging/mediator/MultiSourceEventMediatorImpl.kt
@@ -27,6 +27,7 @@ import org.slf4j.LoggerFactory
 import java.lang.Thread.sleep
 import java.time.Duration
 import java.util.UUID
+import java.util.concurrent.CompletionException
 import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.TimeUnit
 import java.util.concurrent.atomic.AtomicBoolean
@@ -114,7 +115,8 @@ class MultiSourceEventMediatorImpl<K : Any, S : Any, E : Any>(
                         pollAndProcessEvents(consumer)
                         attempts = 0
                     } catch (exception: Exception) {
-                        when (exception) {
+                        val cause = if (exception is CompletionException) exception.cause else exception
+                        when (cause) {
                             is CordaMessageAPIIntermittentException -> {
                                 log.warn(
                                     "Multi-source event mediator ${config.name} failed to process records, " +
