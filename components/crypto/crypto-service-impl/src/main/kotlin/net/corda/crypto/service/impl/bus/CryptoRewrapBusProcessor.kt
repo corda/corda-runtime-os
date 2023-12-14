6fcfb9d4e6b954fe03a114966e67a90cb2f9bffc
malachy <malachy.byrne@r3.com>
2023-12-14 17:05:28 +0000

    CORE-13504: Add CryptoRewrapBusProcessor metrics to Grafana

--- components/crypto/crypto-service-impl/src/main/kotlin/net/corda/crypto/service/impl/bus/CryptoRewrapBusProcessor.kt
+++ components/crypto/crypto-service-impl/src/main/kotlin/net/corda/crypto/service/impl/bus/CryptoRewrapBusProcessor.kt
@@ -4,6 +4,9 @@ import net.corda.crypto.core.CryptoService
 import net.corda.data.crypto.wire.ops.key.rotation.IndividualKeyRotationRequest
 import net.corda.messaging.api.processor.DurableProcessor
 import net.corda.messaging.api.records.Record
+import net.corda.metrics.CordaMetrics
+
+private const val REWRAP_KEYS_OPERATION_NAME = "rewrapKeys"
 
 /**
  * This processor does actual re-wrapping of the keys.
@@ -15,10 +18,15 @@ class CryptoRewrapBusProcessor(
 ) : DurableProcessor<String, IndividualKeyRotationRequest> {
     override val keyClass: Class<String> = String::class.java
     override val valueClass = IndividualKeyRotationRequest::class.java
+    private val rewrapTimer = CordaMetrics.Metric.Crypto.RewrapKeysTimer.builder()
+        .withTag(CordaMetrics.Tag.OperationName, REWRAP_KEYS_OPERATION_NAME)
+        .build()
 
     override fun onNext(events: List<Record<String, IndividualKeyRotationRequest>>): List<Record<*, *>> {
         events.mapNotNull { it.value }.map { request ->
-            cryptoService.rewrapWrappingKey(request.tenantId, request.targetKeyAlias, request.newParentKeyAlias)
+            rewrapTimer.recordCallable {
+                cryptoService.rewrapWrappingKey(request.tenantId, request.targetKeyAlias, request.newParentKeyAlias)
+            }
         }
         return emptyList()
     }
