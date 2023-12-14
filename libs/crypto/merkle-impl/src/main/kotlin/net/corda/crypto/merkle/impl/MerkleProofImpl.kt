5cd688a55df164d097556f139d7850f13e4d92d5
Dickon Reed <dickon.reed@r3.com>
2023-12-14 09:21:18 +0000

    turn trace into comments

--- libs/crypto/merkle-impl/src/main/kotlin/net/corda/crypto/merkle/impl/MerkleProofImpl.kt
+++ libs/crypto/merkle-impl/src/main/kotlin/net/corda/crypto/merkle/impl/MerkleProofImpl.kt
@@ -61,8 +61,8 @@ class MerkleProofImpl(
             )
         }
 
-        // we do support and test with leaves.size == treeSize, which may not
-        // be very useful but needed not be a special case
+        // We do support and test with leaves.size == treeSize, which may not
+        // be very useful but needed not be a special case.
         if (leaves.size > treeSize) {
             throw MerkleProofRebuildFailureException("MerkleProof has too many specified keys ${leaves.size} tree size ${treeSize}")
         }
@@ -86,50 +86,66 @@ class MerkleProofImpl(
         var hashIndex = 0
         val sortedLeaves = leaves.sortedBy { it.index }
         // work out nodeHashes, which is a list of node information for the current level we operate at
-        var nodeHashes = sortedLeaves.map { Pair(it.index, digest.leafHash(it.index, it.nonce, it.leafData)) }
+        var nodeHashes: List<Pair<Int, SecureHash>> = sortedLeaves.map { Pair(it.index, digest.leafHash(it.index, it.nonce, it.leafData)) }
         var treeDepth = MerkleTreeImpl.treeDepth(treeSize)         // initialised to the depth of tree we should
                                                                    // need for the number of elements
         var currentSize = treeSize                                 // outer loop variable; the number of
                                                                    // leaves left as we roll up the tree
-        logger.trace( "calculateRoot initial tree size ${treeSize} depth $treeDepth leaves indices ${leaves.map { it.index }} |hashes|=${hashes.size}")
 
         // loop over each level of the tree, starting at the deepest level (i.e. furthest from root)
         while (currentSize > 1) {
-            logger.trace ("\touter loop currentSize=${currentSize} treeDepth=${treeDepth} |nodeHashes|=${nodeHashes.size}" )
+            // Process a level of the tree which means generating the hashes for the level above (i.e. closer
+            // to the root).
+
+            // There'd be nothing to do if the tree size $currentSize is 1, hence the loop condition
             if (nodeHashes.isEmpty()) {
                 throw MerkleProofRebuildFailureException(
                     "MerkleProof does not have enough nodeHashes to calculate root hash."
                 )
             }
-            --treeDepth                                             // huh?
+            --treeDepth
+            // We could check here that size of nodeHashes is as expected for treeDepth; there should be a closed form.
+
+            // $currentSize is the number of leafs+nodes at this level of the tree
+            // $treeDepth is the level of the tree, counting from the root of the tree where $currentSize==1
+            // $nodeHashes has a list of pairs of the index and hash of the node. We checked we have some content.
+
             val newItems = mutableListOf<Pair<Int, SecureHash>>()   // this will become nodeHashes at the end of this
                                                                     // out iteration
 
-            // walk over our current set of nodeHashes, striding over 1 or 2 at a time
+            // Now walk over the hashes at this tree level, striding over 1 or 2 at a time
             var index = 0
             while (index < nodeHashes.size) {
                 val item = nodeHashes[index]
-                logger.trace("\t\tinner loop at index $index first node index ${item.first} of $currentSize tree depth $treeDepth nodehash.first=${item.first}")
+                // Now we are at level $treeDepth from the top of the tree (counting from 1), and at $index nodes from the left (counting from 0)
+                // $item is a pair of the index and the hash at the index. Since index == item.index we don't really need to use item.index
 
                 if (item.first < currentSize and 0x7FFFFFFE) {      // If the level has odd elements, we'll process
                                                                     // the last element later.
                     if (index < nodeHashes.size - 1) {              // If there is a next element...
                         val next = nodeHashes[index + 1]
-                        logger.trace("\t\t\tconsidering pair elements at index ${item.first} and ${next.first}")
+                        // Decide if we can consume the next two elements since they are adjancent in the Merkle tree
                         if (item.first xor next.first == 1) {       // ... and they are a pair with the current
-                            logger.trace("\t\t\t\twe can combined node in the original tree to make a node with index ${item.first/2}!")
+                            // We now know that the indices ${item.first} and ${next.first} only differ on the bottom bit,
+                            // i.e. they are adjacent. Therefore we can combine them.
 
-                            // make a single new item, computing a new hash
+                            // So, make a single new item, computing a new hash
                             // (Pair is the Kotlin type, nothing to do with pairing nodes)
                             newItems += Pair(                       // in the original tree, we create their parent.
                                 item.first / 2,
                                 digest.nodeHash(treeDepth, item.second, next.second)
                             )
+                            // and record that we consumed two values from our working set, and skip on to the
+                            // start of the next loop
                             index += 2                              // we've consumed two
                             continue                                // continue the inner level scanning loop
                         }
                     }
-                    logger.trace("\t\t\tat $hashIndex on level $index; cannot pair; have ${hashes.size} hashes")
+                    // We've already skipped past here if chose to make a new node by combining two known hashes.
+
+                    // At this point we know we do not know enough to simply take two known hashes at $index and ${index+1} and roll
+                    // them up, so we are going to have to consume a hash.
+
                     if (hashIndex >= hashes.size) {                 // We'll need one more hash to continue. So if
                         throw MerkleProofRebuildFailureException(   // we do not have more, the proof is incorrect.
                             "MerkleProof root calculation requires more hashes than the proof has."
@@ -138,20 +154,27 @@ class MerkleProofImpl(
                                                                     // We pair the current element with a
                                                                     // hash from the proof
                     newItems += if ((item.first and 1) == 0) {      // Even index means, that the item is on the left
-                        logger.trace("\t\t\t\tcombined left of current element ${item.first} with right proof hash $hashIndex")
+                        // Make new node with
+                        //   - left being current element, index $item.first, hash $item.second
+                        //   - right being proof of hash at $hashIndex
+                        //
+                        // Also remember we used hashIndex by bumping the counter
                         Pair(
                             item.first / 2,
                             digest.nodeHash(treeDepth, item.second, hashes[hashIndex++])
                         )
-                    } else {                                        // Odd index means, that the item is on the right
-                        logger.trace("\t\t\t\tcombined left of proof hash $hashIndex with right current element ${item.first}")
+                    } else {
+                        // Make new node with:
+                        //   - left being proof of hash at $hashIndex
+                        //   - right being current element, index $item.first, hash $item.second
+                        //
+                        // Also remember we used hashIndex by bumping the counter.
                         Pair(
                             item.first / 2,
                             digest.nodeHash(treeDepth, hashes[hashIndex++], item.second)
                         )
                     }
                 } else {                                            // The last odd element, just gets lifted.
-                    logger.trace("\t\t\t\tlifting last odd element")
                     newItems += Pair((item.first + 1) / 2, item.second)
                 }
                 ++index // whatever of the last 3 cases we took, we consumed one element
