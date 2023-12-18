9bcefb1aa0377dbe31b329c7ebd8cf74be4d6c11
Yiftach Kaplan <yiftach.kaplan@r3.com>
2023-12-18 11:17:43 +0000

    Simpler stub

--- components/link-manager/src/main/kotlin/net/corda/p2p/linkmanager/stubs/Encryption.kt
+++ components/link-manager/src/main/kotlin/net/corda/p2p/linkmanager/stubs/Encryption.kt
@@ -1,29 +1,15 @@
 package net.corda.p2p.linkmanager.stubs
 
-import javax.crypto.Cipher
-import javax.crypto.KeyGenerator
+import org.bouncycastle.util.encoders.Base64
 
 /**
  * This is an unsafe encryption stub. It should be replaced.
  */
 internal class Encryption {
-    private companion object {
-        const val ALGORITHM = "AES"
-    }
-    private val key by lazy {
-        val key = KeyGenerator.getInstance(ALGORITHM)
-        key.init(128)
-        key.generateKey()
-    }
-
     fun encrypt(data: ByteArray): ByteArray {
-        val cipher = Cipher.getInstance(ALGORITHM)
-        cipher.init(Cipher.ENCRYPT_MODE, key)
-        return cipher.doFinal(data)
+        return Base64.encode(data)
     }
     fun decrypt(data: ByteArray): ByteArray {
-        val cipher = Cipher.getInstance(ALGORITHM)
-        cipher.init(Cipher.DECRYPT_MODE, key)
-        return cipher.doFinal(data)
+        return Base64.decode(data)
     }
 }
