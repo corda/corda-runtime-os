9a8b705f4da6873ea18d27119e8d07dabec9e8ae
William Vigor <58432369+williamvigorr3@users.noreply.github.com>
2023-12-27 17:42:15 +0000

    CORE-18628 Add StatefulSessionManager to LinkManager (#5256)
    * Add StatefulSessionManager as a new component
    
    * Up test timeout to 25 seconds
    
    * Actually add the authenticated messages
    
    * Tidy OutboundMessageProcessor
    
    * Change the Inbound side to do the same.
    
    * Add feature toggle and fix detekt
    
    * Fix metrics
    
    * Move feature flag to Features
    
    * Respond to major review comments
    
    * Fix detekt
    
    * Specify logger type
    
    * Respond to more review comments
    
    * Fix detekt
    
    * Respond to more review comments
    
    * Respond to more review comments
    
    * Make SessionManagerTests work with new API
    
    * Add back unit tests
    
    * More progres fixing tests
    
    * Uncomment tests, apply review comments
    
    * Apply review comments
    
    * Apply review comment
    
    * Apply suggestions from code review
    
    Co-authored-by: Dimos Raptis <dimos.raptis@r3.com>
    
    ---------
    
    Co-authored-by: Yiftach Kaplan <yiftach.kaplan@r3.com>
    Co-authored-by: Yiftach Kaplan <67583323+yift-r3@users.noreply.github.com>
    Co-authored-by: Dimos Raptis <dimos.raptis@r3.com>

--- components/link-manager/src/main/kotlin/net/corda/p2p/linkmanager/common/LinkManagerInternalTypes.kt
+++ components/link-manager/src/main/kotlin/net/corda/p2p/linkmanager/common/LinkManagerInternalTypes.kt
@@ -1,3 +1,13 @@
 package net.corda.p2p.linkmanager
 
+import net.corda.messaging.api.records.EventLogRecord
+
 typealias EndPoint = String
+
+/**
+ * Class which wraps a message, so we can do event tracing by passing the original event log record to a lower layer.
+ */
+internal data class TraceableItem<T, E: Any>(
+    val item: T,
+    val originalRecord: EventLogRecord<String, E>?
+)
