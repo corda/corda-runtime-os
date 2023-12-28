4322f69c8f0d65ad55e7ccd179190e4679533f9d
Filipe Oliveira <filipe.oliveira@r3.com>
2023-12-28 16:36:16 +0000

    Update

--- libs/tracing-impl/src/main/kotlin/net/corda/tracing/brave/BraveRecordExtractor.kt
+++ libs/tracing-impl/src/main/kotlin/net/corda/tracing/brave/BraveRecordExtractor.kt
@@ -4,14 +4,19 @@ import brave.Tracing
 import brave.propagation.Propagation
 import brave.propagation.TraceContextOrSamplingFlags
 
+// This class is responsible for extracting a trace context based on a set of trace headers.
 class BraveRecordExtractor(tracing: Tracing) {
 
+    // Headers can be stored in any format. This instance contains the logic to extract the trace headers from a
+    // list
     private val recordExtractorListFormat = tracing.propagation().extractor(
             Propagation.Getter<List<Pair<String, String>>, String> { headers, key ->
                 headers.reversed().firstOrNull { it.first == key }?.second
             }
     )
 
+    // Headers can be stored in any format. This instance contains the logic to extract the trace headers from a
+    // mutable map
     private val recordExtractorMutableMapFormat = tracing.propagation().extractor(
         Propagation.Getter<MutableMap<String, Any>, String> { headers, key -> headers[key] as String? }
     )
