b6483e98759c7e01e6973a2acdf314cc1ccf8b70
Filipe Oliveira <filipe.oliveira@r3.com>
2023-12-28 16:34:10 +0000

    Update

--- libs/tracing-impl/src/main/kotlin/net/corda/tracing/brave/BraveRecordInjector.kt
+++ libs/tracing-impl/src/main/kotlin/net/corda/tracing/brave/BraveRecordInjector.kt
@@ -3,14 +3,19 @@ package net.corda.tracing.brave
 import brave.Tracing
 import brave.propagation.TraceContext
 
+// This class is responsible for adding the trace headers based on an existing trace context.
 class BraveRecordInjector(tracing: Tracing) {
 
+    // Headers can be stored in any format. This instance contains the logic to insert the trace headers to a
+    // mutable list
     private val recordInjectorListFormat =
         tracing.propagation().injector { param: MutableList<Pair<String, String>>, key: String, value: String ->
             param.removeAll { it.first == key }
             param.add(key to value)
         }
 
+    // Headers can be stored in any format. This instance contains the logic to insert the trace headers to a
+    // mutable map
     private val recordInjectorMutableMapFormat =
         tracing.propagation().injector { param: MutableMap<String, Any>, key: String, value: Any ->
             param[key] = value
