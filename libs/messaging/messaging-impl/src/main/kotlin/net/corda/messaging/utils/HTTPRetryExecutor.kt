97b3e022fa05c46ff3c125e74383ca4918334da8
ben-millar <ben.millar@r3.com>
2024-01-02 16:14:08 +0000

    CORE-19142 Adding additional metrics to HTTPRetryExecutor, removing unused from RPCClient

--- libs/messaging/messaging-impl/src/main/kotlin/net/corda/messaging/utils/HTTPRetryExecutor.kt
+++ libs/messaging/messaging-impl/src/main/kotlin/net/corda/messaging/utils/HTTPRetryExecutor.kt
@@ -1,18 +1,22 @@
 package net.corda.messaging.utils
 
-import java.net.http.HttpResponse
 import net.corda.messaging.api.exception.CordaHTTPClientErrorException
 import net.corda.messaging.api.exception.CordaHTTPServerErrorException
+import net.corda.metrics.CordaMetrics
 import net.corda.utilities.trace
 import net.corda.v5.base.exceptions.CordaRuntimeException
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import java.net.http.HttpResponse
+import java.time.Duration
 
 class HTTPRetryExecutor {
     companion object {
         private val log: Logger = LoggerFactory.getLogger(this::class.java.enclosingClass)
+        private const val SUCCESS: String = "SUCCESS"
+        private const val FAILED: String = "FAILED"
 
-        fun <T> withConfig(config: HTTPRetryConfig, block: () -> HttpResponse<T>): HttpResponse<T> {
+        fun withConfig(config: HTTPRetryConfig, block: () -> HttpResponse<ByteArray>): HttpResponse<ByteArray> {
             var currentDelay = config.initialDelay
             for (i in 0 until config.times) {
                 val result = tryAttempt(i, config, block)
@@ -28,14 +32,22 @@ class HTTPRetryExecutor {
             throw CordaRuntimeException(errorMsg)
         }
 
-        private fun <T> tryAttempt(i: Int, config: HTTPRetryConfig, block: () -> HttpResponse<T>): HttpResponse<T>? {
+        private fun tryAttempt(
+            i: Int,
+            config: HTTPRetryConfig,
+            block: () -> HttpResponse<ByteArray>
+        ): HttpResponse<ByteArray>? {
+            val startTime = System.nanoTime()
+
             return try {
                 log.trace { "HTTPRetryExecutor making attempt #${i + 1}." }
                 val result = block()
                 checkResponseStatus(result.statusCode())
+                buildMetricForResponse(config, startTime, SUCCESS, result)
                 log.trace { "Operation successful after #${i + 1} attempt/s." }
                 result
             } catch (e: Exception) {
+                buildMetricForResponse(config, startTime, FAILED)
                 handleException(i, config, e)
                 null
             }
@@ -60,6 +72,42 @@ class HTTPRetryExecutor {
             when (statusCode) {
                 in 400..499 -> throw CordaHTTPClientErrorException(statusCode, "Server returned status code $statusCode.")
                 in 500..599 -> throw CordaHTTPServerErrorException(statusCode, "Server returned status code $statusCode.")
+            }
+        }
+
+        private fun buildMetricForResponse(
+            config: HTTPRetryConfig,
+            startTime: Long,
+            operationStatus: String,
+            response: HttpResponse<ByteArray>? = null
+        ) {
+            val endTime = System.nanoTime()
+            recordResponseTimeMetric(config, startTime, endTime, operationStatus, response)
+            response?.let { recordResponseSizeMetric(config, response) }
+        }
+
+        private fun recordResponseTimeMetric(
+            config: HTTPRetryConfig,
+            startTime: Long,
+            endTime: Long,
+            operationStatus: String,
+            response: HttpResponse<ByteArray>?
+        ) {
+            CordaMetrics.Metric.Messaging.HTTPRPCResponseTime.builder().apply {
+                withTag(CordaMetrics.Tag.OperationStatus, operationStatus)
+                withTag(CordaMetrics.Tag.HttpResponseCode, response?.statusCode()?.toString() ?: "null")
+                config.additionalMetrics.forEach { (tag, value) -> withTag(tag, value) }
+                build().record(Duration.ofNanos(endTime - startTime))
+            }
+        }
+
+        private fun recordResponseSizeMetric(
+            config: HTTPRetryConfig,
+            response: HttpResponse<ByteArray>
+        ) {
+            CordaMetrics.Metric.Messaging.HTTPRPCResponseSize.builder().apply {
+                config.additionalMetrics.forEach { (tag, value) -> withTag(tag, value) }
+                build().record(response.body().size.toDouble())
             }
         }
     }
