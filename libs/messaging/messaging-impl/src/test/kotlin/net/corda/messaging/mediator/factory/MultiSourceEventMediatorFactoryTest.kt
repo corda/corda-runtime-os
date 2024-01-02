1f1741caa1f6fae6cae36e09824cf0762f7069b3
LWogan <lorcan.wogan@r3.com>
2024-01-02 16:30:02 +0000

    CORE-18963 enable and fix unit test

--- libs/messaging/messaging-impl/src/test/kotlin/net/corda/messaging/mediator/factory/MultiSourceEventMediatorFactoryTest.kt
+++ libs/messaging/messaging-impl/src/test/kotlin/net/corda/messaging/mediator/factory/MultiSourceEventMediatorFactoryTest.kt
@@ -4,6 +4,7 @@ import net.corda.avro.serialization.CordaAvroDeserializer
 import net.corda.avro.serialization.CordaAvroSerializationFactory
 import net.corda.avro.serialization.CordaAvroSerializer
 import net.corda.libs.statemanager.api.StateManager
+import net.corda.lifecycle.LifecycleCoordinator
 import net.corda.lifecycle.LifecycleCoordinatorFactory
 import net.corda.messaging.api.mediator.config.EventMediatorConfig
 import net.corda.messaging.api.mediator.factory.MessageRouterFactory
@@ -12,7 +13,6 @@ import net.corda.taskmanager.TaskManager
 import net.corda.taskmanager.TaskManagerFactory
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeEach
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.mockito.kotlin.any
 import org.mockito.kotlin.anyOrNull
@@ -24,6 +24,7 @@ class MultiSourceEventMediatorFactoryTest {
     private val cordaAvroSerializationFactory = mock<CordaAvroSerializationFactory>()
     private val serializer = mock<CordaAvroSerializer<Any>>()
     private val stateDeserializer = mock<CordaAvroDeserializer<Any>>()
+    private val lifecycleCoordinatorFactory = mock<LifecycleCoordinatorFactory>()
     private val taskManagerFactory = mock<TaskManagerFactory>()
 
     @BeforeEach
@@ -31,15 +32,15 @@ class MultiSourceEventMediatorFactoryTest {
         doReturn(serializer).`when`(cordaAvroSerializationFactory).createAvroSerializer<Any>(anyOrNull())
         doReturn(stateDeserializer).`when`(cordaAvroSerializationFactory).createAvroDeserializer(any(), any<Class<Any>>())
         doReturn(mock<TaskManager>()).`when`(taskManagerFactory).createThreadPoolTaskManager(any(), any(), any())
+        doReturn(mock<LifecycleCoordinator>()).`when`(lifecycleCoordinatorFactory).createCoordinator(any(), any())
         multiSourceEventMediatorFactory = MultiSourceEventMediatorFactoryImpl(
             cordaAvroSerializationFactory,
-            mock<LifecycleCoordinatorFactory>(),
+            lifecycleCoordinatorFactory,
             taskManagerFactory,
         )
     }
 
     @Test
-    @Disabled
     fun testCreateMultiSourceEventMediator() {
         val messageProcessor = mock<StateAndEventProcessor<Any, Any, Any>>()
         doReturn(Any::class.java).`when`(messageProcessor).stateValueClass
@@ -47,6 +48,9 @@ class MultiSourceEventMediatorFactoryTest {
         val stateManager = mock<StateManager>()
         val config = mock<EventMediatorConfig<Any, Any, Any>>()
         doReturn(messageProcessor).`when`(config).messageProcessor
+        doReturn(String::class.java).`when`(messageProcessor).keyClass
+        doReturn(String::class.java).`when`(messageProcessor).eventValueClass
+        doReturn(String::class.java).`when`(messageProcessor).stateValueClass
         doReturn(messageRouterFactory).`when`(config).messageRouterFactory
         doReturn("name").`when`(config).name
         doReturn(1).`when`(config).threads
