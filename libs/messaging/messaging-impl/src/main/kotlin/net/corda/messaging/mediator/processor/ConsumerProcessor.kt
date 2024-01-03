c34130d82822d08adea445883976a19728633df3
LWogan <lorcan.wogan@r3.com>
2024-01-03 09:56:09 +0000

    CORE-18984 use config value to set the poll timeout used in the event mediator

--- libs/messaging/messaging-impl/src/main/kotlin/net/corda/messaging/mediator/processor/ConsumerProcessor.kt
+++ libs/messaging/messaging-impl/src/main/kotlin/net/corda/messaging/mediator/processor/ConsumerProcessor.kt
@@ -18,7 +18,6 @@ import net.corda.messaging.utils.toRecord
 import net.corda.taskmanager.TaskManager
 import net.corda.utilities.debug
 import org.slf4j.LoggerFactory
-import java.time.Duration
 import java.util.concurrent.CompletionException
 import java.util.concurrent.TimeUnit
 
@@ -47,7 +46,7 @@ class ConsumerProcessor<K : Any, S : Any, E : Any>(
 
     private val metrics = EventMediatorMetrics(config.name)
 
-    private val pollTimeout = Duration.ofMillis(50)
+    private val pollTimeout = config.pollTimeout
 
     private val stateManager = config.stateManager
 
