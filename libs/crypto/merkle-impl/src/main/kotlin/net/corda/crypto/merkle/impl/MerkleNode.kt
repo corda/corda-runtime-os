15e959df8a5541143ce4a5011e05ce3959a72541
dickon <dickon.reed@r3.com>
2024-01-11 13:16:53 +0000

    Update libs/crypto/merkle-impl/src/main/kotlin/net/corda/crypto/merkle/impl/MerkleNode.kt
    Co-authored-by: Anna Paskova <anna.paskova@r3.com>

--- libs/crypto/merkle-impl/src/main/kotlin/net/corda/crypto/merkle/impl/MerkleNode.kt
+++ libs/crypto/merkle-impl/src/main/kotlin/net/corda/crypto/merkle/impl/MerkleNode.kt
@@ -6,8 +6,8 @@ import net.corda.v5.crypto.SecureHash
  * A MerkleNode represents a node on a tree, and is used as the parameter for callbacks when we walk
  * a proof of Merkle tree - see MerkleProofImpl.calculateRootInstrumented.
  *
- * @param indexWithinLevel the position of the node at its level, counting from the left (which is zero)
- * @param hash the hash of the node, which is a hash of its children
+ * @param indexWithinLevel The position of the node at its level, counting from the left (which is zero)
+ * @param hash The hash of the node, which is a hash of its children
  */
 
 internal data class MerkleNode(val indexWithinLevel: Int, val hash: SecureHash)
