70e0a12b62d7e77461c9930f27c92e50cee0aa83
Anna Paskova <anna.paskova@r3.com>
2024-01-12 09:23:58 +0000

    Fix compiling issue

--- components/crypto/crypto-service-impl/src/test/kotlin/net/corda/crypto/service/impl/bus/CryptoRewrapBusProcessorTests.kt
+++ components/crypto/crypto-service-impl/src/test/kotlin/net/corda/crypto/service/impl/bus/CryptoRewrapBusProcessorTests.kt
@@ -1,21 +1,44 @@
 package net.corda.crypto.service.impl.bus
 
+import net.corda.avro.serialization.CordaAvroSerializationFactory
+import net.corda.avro.serialization.CordaAvroSerializer
 import net.corda.crypto.core.CryptoService
 import net.corda.data.crypto.wire.ops.key.rotation.IndividualKeyRotationRequest
 import net.corda.data.crypto.wire.ops.key.rotation.KeyType
+import net.corda.data.crypto.wire.ops.key.status.UnmanagedKeyStatus
 import net.corda.messaging.api.records.Record
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.mockito.kotlin.any
+import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.times
 import org.mockito.kotlin.verify
 import java.util.UUID
 
 class CryptoRewrapBusProcessorTests {
+    private lateinit var cryptoRewrapBusProcessor: CryptoRewrapBusProcessor
+    private lateinit var cordaAvroSerializationFactory: CordaAvroSerializationFactory
+
     companion object {
         private val cryptoService: CryptoService = mock<CryptoService> { }
-        private val cryptoRewrapBusProcessor = CryptoRewrapBusProcessor(cryptoService)
         private val tenantId = UUID.randomUUID().toString()
+    }
+
+    @BeforeEach
+    fun setup() {
+        val serializer = mock<CordaAvroSerializer<UnmanagedKeyStatus>> {
+            on { serialize(any()) } doReturn byteArrayOf(42)
+        }
+        cordaAvroSerializationFactory = mock<CordaAvroSerializationFactory> {
+            on { createAvroSerializer<UnmanagedKeyStatus>() } doReturn serializer
+        }
+
+        cryptoRewrapBusProcessor = CryptoRewrapBusProcessor(
+            cryptoService,
+            mock(),
+            cordaAvroSerializationFactory
+        )
     }
 
     @Test
@@ -25,7 +48,14 @@ class CryptoRewrapBusProcessorTests {
                 Record(
                     "TBC",
                     UUID.randomUUID().toString(),
-                    IndividualKeyRotationRequest(UUID.randomUUID().toString(), tenantId, "alias1", "root2", "alias1", KeyType.UNMANAGED)
+                    IndividualKeyRotationRequest(
+                        UUID.randomUUID().toString(),
+                        tenantId,
+                        "alias1",
+                        "root2",
+                        "alias1",
+                        KeyType.UNMANAGED
+                    )
                 )
             )
         )
