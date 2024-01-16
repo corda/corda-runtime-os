f8843682469cddf2c130951dbcb2fd0da3cfdb2a
Emily Bowe <emily.bowe@r3.com>
2024-01-16 10:13:49 +0000

    change to functional interface as only one member to remove code smell

--- components/ledger/ledger-common-flow-api/src/main/kotlin/net/corda/ledger/common/flow/transaction/PrivacySaltProviderService.kt
+++ components/ledger/ledger-common-flow-api/src/main/kotlin/net/corda/ledger/common/flow/transaction/PrivacySaltProviderService.kt
@@ -2,7 +2,7 @@ package net.corda.ledger.common.flow.transaction
 
 import net.corda.ledger.common.data.transaction.PrivacySalt
 
-interface PrivacySaltProviderService {
+fun interface PrivacySaltProviderService {
 
     /**
      * Returns a privacy salt
