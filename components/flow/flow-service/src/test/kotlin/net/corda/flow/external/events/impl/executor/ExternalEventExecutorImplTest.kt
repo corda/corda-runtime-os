958c9d9514125d4d269180f7298d95dd9a1ad3b4
ben-millar <ben.millar@r3.com>
2024-01-16 14:24:39 +0000

    CORE-18929 Finished unit tests

--- components/flow/flow-service/src/test/kotlin/net/corda/flow/external/events/impl/executor/ExternalEventExecutorImplTest.kt
+++ components/flow/flow-service/src/test/kotlin/net/corda/flow/external/events/impl/executor/ExternalEventExecutorImplTest.kt
@@ -4,8 +4,6 @@ import net.corda.data.flow.event.external.ExternalEventResponse
 import net.corda.flow.application.services.MockFlowFiberService
 import net.corda.flow.external.events.factory.ExternalEventFactory
 import net.corda.flow.fiber.FlowIORequest
-import net.corda.flow.state.FlowCheckpoint
-import net.corda.flow.state.impl.FlowCheckpointImpl
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -22,6 +20,11 @@ class ExternalEventExecutorImplTest {
 
     private val capturedArguments = mutableListOf<FlowIORequest.ExternalEvent>()
     private val mockFactoryClass = mock<ExternalEventFactory<Any,Any,Any>>()::class.java
+    private val mockParams = mutableMapOf("test" to "parameters")
+
+    companion object {
+        const val testFlowId: String = "static_flow_id"
+    }
 
     @BeforeEach
     fun setup() {
@@ -29,11 +32,12 @@ class ExternalEventExecutorImplTest {
     }
 
     private fun initializeMocks(
-        flowCheckpoint: FlowCheckpoint = mock()
+        flowId: String = testFlowId,
+        suspendCount: Int = 1
     ) {
-        mockFlowFiberService = MockFlowFiberService().apply {
-            this.flowCheckpoint = flowCheckpoint
-        }
+        mockFlowFiberService = MockFlowFiberService()
+        whenever(mockFlowFiberService.flowCheckpoint.flowId).thenReturn(flowId)
+        whenever(mockFlowFiberService.flowCheckpoint.suspendCount).thenReturn(suspendCount)
 
         // Capture arguments (ArgumentCaptor doesn't play nice with suspending functions)
         doAnswer { invocation ->
@@ -47,43 +51,27 @@ class ExternalEventExecutorImplTest {
 
     @Test
     fun `execute suspends with expected FlowIORequest`() {
-        val flowId = UUID.randomUUID().toString()
-        val suspendCount = 3
+        val contextProperties = mapOf(
+            "platformKey" to "platformValue",
+            "userKey" to "userValue"
+        )
 
         val expectedRequest = FlowIORequest.ExternalEvent(
             // This is hardcoded, but should be deterministic!
-            "cd7ddb81-17d5-3735-941c-dc4be70d2cee",
-            mockFactoryClass, listOf("TEST"),
-            mapOf("platformKey" to "platformValue", "userKey" to "userValue")
+            "26ec4282-eebc-358d-9ac3-6e32ca1b103b",
+            mockFactoryClass, mockParams, contextProperties
         )
 
-        val flowCheckpoint: FlowCheckpoint = mock()
-
-        whenever(flowCheckpoint.flowId).thenReturn(flowId)
-        whenever(flowCheckpoint.suspendCount).thenReturn(suspendCount)
-
-        initializeMocks(flowCheckpoint)
-
-        externalEventExecutorImpl.execute(mockFactoryClass, listOf("TEST"))
+        externalEventExecutorImpl.execute(mockFactoryClass, mockParams)
 
         assertEquals(1, capturedArguments.size)
-        assertEquals(capturedArguments[0], expectedRequest)
+        assertEquals(expectedRequest, capturedArguments[0])
     }
 
     @Test
     fun `Suspending with the same parameters, flowId, and suspensionCount produces the same requestID`() {
-        val flowId = UUID.randomUUID().toString()
-        val suspendCount = 3
-
-        val flowCheckpoint: FlowCheckpoint = mock()
-
-        whenever(flowCheckpoint.flowId).thenReturn(flowId)
-        whenever(flowCheckpoint.suspendCount).thenReturn(suspendCount)
-
-        initializeMocks(flowCheckpoint)
-
-        externalEventExecutorImpl.execute(mockFactoryClass, listOf("TEST"))
-        externalEventExecutorImpl.execute(mockFactoryClass, listOf("TEST"))
+        externalEventExecutorImpl.execute(mockFactoryClass, mockParams)
+        externalEventExecutorImpl.execute(mockFactoryClass, mockParams)
 
         assertEquals(2, capturedArguments.size)
         assertEquals(capturedArguments[0].requestId, capturedArguments[1].requestId)
@@ -91,17 +79,8 @@ class ExternalEventExecutorImplTest {
 
     @Test
     fun `Suspending with different parameters produces a different requestID`() {
-        val flowId = UUID.randomUUID().toString()
-        val suspendCount = 3
-
-        val flowCheckpoint: FlowCheckpoint = mock()
-        whenever(flowCheckpoint.flowId).thenReturn(flowId)
-        whenever(flowCheckpoint.suspendCount).thenReturn(suspendCount)
-
-        initializeMocks(flowCheckpoint)
-
-        externalEventExecutorImpl.execute(mockFactoryClass, listOf("TEST-1"))
-        externalEventExecutorImpl.execute(mockFactoryClass, listOf("TEST-2"))
+        externalEventExecutorImpl.execute(mockFactoryClass, mockParams)
+        externalEventExecutorImpl.execute(mockFactoryClass, mockParams + mapOf("additional" to "data"))
 
         assertEquals(2, capturedArguments.size)
         assertNotEquals(capturedArguments[0].requestId, capturedArguments[1].requestId)
@@ -110,22 +89,14 @@ class ExternalEventExecutorImplTest {
 
     @Test
     fun `Suspending with a different flowId produces a different requestID`() {
-        val flowId = UUID.randomUUID().toString()
-        val suspendCount = 3
+        val flowId1 = UUID.randomUUID().toString()
+        val flowId2 = UUID.randomUUID().toString()
 
-        val flowCheckpoint: FlowCheckpointImpl = mock()
+        initializeMocks(flowId = flowId1)
+        externalEventExecutorImpl.execute(mockFactoryClass, mockParams)
 
-        whenever(flowCheckpoint.flowId).thenReturn(flowId)
-        whenever(flowCheckpoint.suspendCount).thenReturn(suspendCount)
-
-        initializeMocks(flowCheckpoint)
-
-        externalEventExecutorImpl.execute(mockFactoryClass, listOf("TEST"))
-
-        whenever(flowCheckpoint.flowId).thenReturn(UUID.randomUUID().toString())
-        initializeMocks(flowCheckpoint)
-
-        externalEventExecutorImpl.execute(mockFactoryClass, listOf("TEST"))
+        initializeMocks(flowId = flowId2)
+        externalEventExecutorImpl.execute(mockFactoryClass, mockParams)
 
         assertEquals(2, capturedArguments.size)
         assertNotEquals(capturedArguments[0].requestId, capturedArguments[1].requestId)
@@ -133,22 +104,11 @@ class ExternalEventExecutorImplTest {
 
     @Test
     fun `Suspending with a different suspensionCount produces a different requestID`() {
-        val flowId = UUID.randomUUID().toString()
-        val suspendCount = 3
+        initializeMocks(suspendCount = 2)
+        externalEventExecutorImpl.execute(mockFactoryClass, mockParams)
 
-        val flowCheckpoint: FlowCheckpointImpl = mock()
-
-        whenever(flowCheckpoint.flowId).thenReturn(flowId)
-        whenever(flowCheckpoint.suspendCount).thenReturn(suspendCount)
-
-        initializeMocks(flowCheckpoint)
-
-        externalEventExecutorImpl.execute(mockFactoryClass, listOf("TEST"))
-
-        whenever(flowCheckpoint.suspendCount).thenReturn(suspendCount + 1)
-        initializeMocks(flowCheckpoint)
-
-        externalEventExecutorImpl.execute(mockFactoryClass, listOf("TEST"))
+        initializeMocks(suspendCount = 3)
+        externalEventExecutorImpl.execute(mockFactoryClass, mockParams)
 
         assertEquals(2, capturedArguments.size)
         assertNotEquals(capturedArguments[0].requestId, capturedArguments[1].requestId)
