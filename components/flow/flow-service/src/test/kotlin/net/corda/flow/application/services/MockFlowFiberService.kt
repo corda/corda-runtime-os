d7ac9abd36946b5563664d837e8d7533a3eede1d
ben-millar <ben.millar@r3.com>
2024-01-16 14:28:15 +0000

    CORE-18929 Removing change to MockFlowFiberService

--- components/flow/flow-service/src/test/kotlin/net/corda/flow/application/services/MockFlowFiberService.kt
+++ components/flow/flow-service/src/test/kotlin/net/corda/flow/application/services/MockFlowFiberService.kt
@@ -20,7 +20,7 @@ import org.mockito.kotlin.whenever
 class MockFlowFiberService : FlowFiberService {
     val flowFiber = mock<FlowFiber>()
     private val sandboxDependencyInjector = mock<FlowSandboxDependencyInjectorImpl>()
-    var flowCheckpoint = mock<FlowCheckpoint>()
+    val flowCheckpoint = mock<FlowCheckpoint>()
     val flowStack = mock<FlowStack>()
     private val checkpointSerializer = mock<CheckpointSerializer>()
     private val currentSandboxGroupContext = mock<CurrentSandboxGroupContext>()
