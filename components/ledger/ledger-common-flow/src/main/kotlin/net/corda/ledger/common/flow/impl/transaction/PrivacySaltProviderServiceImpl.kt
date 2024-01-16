4b5ad3f04d250d7d633ce1998a7f2a78aee38b3d
Emily Bowe <emily.bowe@r3.com>
2024-01-16 10:05:14 +0000

    code style changes

--- components/ledger/ledger-common-flow/src/main/kotlin/net/corda/ledger/common/flow/impl/transaction/PrivacySaltProviderServiceImpl.kt
+++ components/ledger/ledger-common-flow/src/main/kotlin/net/corda/ledger/common/flow/impl/transaction/PrivacySaltProviderServiceImpl.kt
@@ -9,7 +9,6 @@ import org.osgi.service.component.annotations.Component
 import org.osgi.service.component.annotations.Reference
 
 @Component(service = [PrivacySaltProviderService::class])
-
 class PrivacySaltProviderServiceImpl @Activate constructor(
     @Reference(service = FlowFiberService::class)
     private val flowFiberService: FlowFiberService
