ec44b5f9f70d61b8c2032a10b801b7802b8eb183
Emily Bowe <emily.bowe@r3.com>
2024-01-17 18:16:39 +0000

    edit kdoc

--- components/ledger/ledger-common-flow-api/src/main/kotlin/net/corda/ledger/common/flow/transaction/PrivacySaltProviderService.kt
+++ components/ledger/ledger-common-flow-api/src/main/kotlin/net/corda/ledger/common/flow/transaction/PrivacySaltProviderService.kt
@@ -5,7 +5,7 @@ import net.corda.ledger.common.data.transaction.PrivacySalt
 fun interface PrivacySaltProviderService {
 
     /**
-     * Returns a privacy salt
+     * Returns a deterministic unique salt based on a flows information.
      */
     fun generatePrivacySalt(): PrivacySalt
 }
