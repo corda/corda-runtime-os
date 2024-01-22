d33dda35728da1db105eea2141bb4c7fb553ebe9
Anna Paskova <anna.paskova@r3.com>
2024-01-22 12:08:05 +0000

    Update kdocs

--- testing/e2e-test-utilities/src/main/kotlin/net/corda/e2etest/utilities/ClusterUtils.kt
+++ testing/e2e-test-utilities/src/main/kotlin/net/corda/e2etest/utilities/ClusterUtils.kt
@@ -225,10 +225,10 @@ fun ClusterInfo.whenNoKeyExists(
 }
 
 /**
- * This method triggers rotation of keys for crypto unmanaged Wrapping keys.
- * It takes input 3 parameters, the old and new KeyAlias (in string type) and the status code (Int type)
- *   @param  oldKeyAlias  old unmanaged root key alias that need to be rotated
- *   @param  newKeyAlias  new unmanaged root key alias
+ * This method triggers rotation of keys for crypto unmanaged wrapping keys.
+ * It takes 3 input parameters, the old and new KeyAlias (in string type) and the status code (Int type)
+ *   @param oldKeyAlias Old unmanaged root key alias that needs to be rotated.
+ *   @param newKeyAlias New unmanaged root key alias.
  *   @param expectedHttpStatusCode Status code that should be displayed when the API is hit,
  *   helps to validate both positive or negative scenarios.
  */
@@ -244,8 +244,9 @@ fun ClusterInfo.rotateCryptoUnmanagedWrappingKeys(
 }
 
 /**
- * This method fetch the status of keys for unmanaged Wrapping key Rotation.
- * It takes input 2 parameter, the KeyAlias (in String type) and the status code (in Int type)
+ * This method fetch the status of keys for unmanaged wrapping key rotation.
+ * It takes 2 input parameters, the keyAlias (in String type) and the status code (in Int type)
+ *  @param keyAlias The root key alias of which the status of the last key rotation will be shown.
  *  @param expectedHttpStatusCode Status code that should be displayed when the API is hit,
  *  helps to validate both positive or negative scenarios.
  */
@@ -260,7 +261,7 @@ fun ClusterInfo.getStatusForUnmanagedWrappingKeysRotation(
 }
 
 /**
- * This method fetch the protocol version for unmanaged key Rotation
+ * This method fetch the protocol version for unmanaged key Rotation.
  */
 fun ClusterInfo.getProtocolVersionForUnmanagedKeyRotation(
 ) = cluster {
