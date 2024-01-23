8f819b2ee971b9d7459aef6758bcd1aab2fa1a03
Christian Sailer <christian.sailer@r3.com>
2024-01-23 11:49:25 +0000

    Add ability to parse/output a simple SQL expression

--- components/ledger/ledger-persistence/src/main/kotlin/net/corda/ledger/persistence/query/parsing/VaultNamedQueryParser.kt
+++ components/ledger/ledger-persistence/src/main/kotlin/net/corda/ledger/persistence/query/parsing/VaultNamedQueryParser.kt
@@ -3,4 +3,6 @@ package net.corda.ledger.persistence.query.parsing
 interface VaultNamedQueryParser {
 
     fun parseWhereJson(query: String): String
+
+    fun parseSimpleExpression(input: String): String
 }
