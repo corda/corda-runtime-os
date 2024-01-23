a6f4a1584c3a21bbbe11b78305c233c0687ad01e
Viktor Kolomeyko <Viktor.Kolomeyko@r3.com>
2024-01-23 11:47:02 +0000

    CORE-19201: Make code changes to fix the issue

--- libs/rest/rest-test-common/src/main/kotlin/net/corda/rest/test/ResponseEntityRestResource.kt
+++ libs/rest/rest-test-common/src/main/kotlin/net/corda/rest/test/ResponseEntityRestResource.kt
@@ -1,6 +1,7 @@
 package net.corda.rest.test
 
 import net.corda.rest.RestResource
+import net.corda.rest.SC_CREATED
 import net.corda.rest.annotations.HttpDELETE
 import net.corda.rest.annotations.HttpPOST
 import net.corda.rest.annotations.HttpPUT
@@ -19,7 +20,11 @@ interface ResponseEntityRestResource : RestResource {
     @HttpPOST(path = "post-returns-ok-string-json")
     fun postReturnsOkWithEscapedJson(): ResponseEntity<String>
 
-    @HttpPOST(path = "post-returns-created-string-json", responseDescription = "Description of postReturnsCreatedWithEscapedJson")
+    @HttpPOST(
+        path = "post-returns-created-string-json",
+        responseDescription = "Description of postReturnsCreatedWithEscapedJson",
+        successCode = SC_CREATED
+    )
     fun postReturnsCreatedWithEscapedJson(): ResponseEntity<String>
 
     @HttpPOST(path = "post-returns-raw-entity")
