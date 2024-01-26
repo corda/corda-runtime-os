0a02bf98a8a1f441ef75d0ae0fea167231d69816
Dickon Reed <dickon.reed@r3.com>
2024-01-26 09:23:32 +0000

    CORE-18698: move FilteredTransaction interface and its data classes to ledger-common-flow (#5526)
    In prepration for using FilteredTransaciton from UtxoLedgerService move the interface and add a stub.
    
    Refactoring broken out from #5485.

--- components/ledger/ledger-common-flow-api/src/main/kotlin/net/corda/ledger/common/flow/transaction/filtered/factory/FilteredTransactionFactory.kt
+++ components/ledger/ledger-common-flow-api/src/main/kotlin/net/corda/ledger/common/flow/transaction/filtered/factory/FilteredTransactionFactory.kt
@@ -1,7 +1,7 @@
 package net.corda.ledger.common.flow.transaction.filtered.factory
 
 import net.corda.ledger.common.data.transaction.WireTransaction
-import net.corda.ledger.common.flow.transaction.filtered.FilteredTransaction
+import net.corda.ledger.common.data.transaction.filtered.FilteredTransaction
 import net.corda.v5.base.annotations.Suspendable
 import java.util.function.Predicate
 
