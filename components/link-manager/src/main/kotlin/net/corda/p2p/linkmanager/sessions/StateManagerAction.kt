075bfdf7c6d5c146c41098f7e48591b940b833c5
Yiftach Kaplan <67583323+yift-r3@users.noreply.github.com>
2024-01-26 10:07:25 +0000

    CORE-19413: Fix issues with the statefull session manager. (#5477)
    * CORE-19413: Fix issues with the statefull session manager.
    
    * Apply review comment

---
+++ components/link-manager/src/main/kotlin/net/corda/p2p/linkmanager/sessions/StateManagerAction.kt
@@ -0,0 +1,15 @@
+package net.corda.p2p.linkmanager.sessions
+
+import net.corda.libs.statemanager.api.State
+
+internal interface StateManagerAction {
+    val state: State
+}
+
+internal data class CreateAction(
+    override val state: State,
+) : StateManagerAction
+
+internal data class UpdateAction(
+    override val state: State,
+) : StateManagerAction
