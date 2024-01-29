f7d60390f2a44a855c659b0b161d9c114fb10783
Dan Newton <dan.newton@r3.com>
2024-01-29 10:42:39 +0000

    CORE-19363 Remove 2nd persist in finality flow
    missed code

--- components/ledger/ledger-utxo-flow/src/main/kotlin/net/corda/ledger/utxo/flow/impl/persistence/LedgerPersistenceMetricOperationName.kt
+++ components/ledger/ledger-utxo-flow/src/main/kotlin/net/corda/ledger/utxo/flow/impl/persistence/LedgerPersistenceMetricOperationName.kt
@@ -12,6 +12,7 @@ enum class LedgerPersistenceMetricOperationName {
     PersistSignedGroupParametersIfDoNotExist,
     PersistTransaction,
     PersistTransactionIfDoesNotExist,
+    PersistTransactionSignatures,
     ResolveStateRefs,
     UpdateTransactionStatus,
     PersistMerkleProofIfDoesNotExist
