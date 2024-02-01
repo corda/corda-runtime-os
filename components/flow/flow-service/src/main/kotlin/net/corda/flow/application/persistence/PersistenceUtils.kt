c3a5ac3981ba1d65d9db11f74f4d7f62d0c66a49
LWogan <lorcan.wogan@r3.com>
2024-02-01 15:41:07 +0000

    CORE-18930 kdoc

--- components/flow/flow-service/src/main/kotlin/net/corda/flow/application/persistence/PersistenceUtils.kt
+++ components/flow/flow-service/src/main/kotlin/net/corda/flow/application/persistence/PersistenceUtils.kt
@@ -25,10 +25,11 @@ inline fun <T> wrapWithPersistenceException(function: () -> T): T {
     }
 }
 
-
+/**
+ * Extension functions to assist in translating ByteBuffers to ByteArrays and vice versa.
+ * ByteBuffers are not @CordaSerializable.
+ */
 fun List<ByteArray>.toByteBuffers() = this.map { ByteBuffer.wrap(it) }
 fun List<ByteBuffer>.toByteArrays() = this.map { it.array()}
-
 fun Map<String, ByteArray?>.toByteBuffers() = this.mapValues { value -> value.value?.let { ByteBuffer.wrap(value.value) }  }
-
 fun Map<String, ByteBuffer?>.toByteArrays() = this.mapValues { value -> value.value?.array() }
