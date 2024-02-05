e7e6b5b0c63e7c671027b7c1040515330773581a
Dickon Reed <dickon.reed@r3.com>
2024-02-05 11:24:30 +0000

    remove cluster readiness check since it assumes workers on local ports

--- applications/workers/workers-smoketest/src/smokeTest/kotlin/net/corda/applications/workers/smoketest/ledger/UtxoLedgerTests.kt
+++ applications/workers/workers-smoketest/src/smokeTest/kotlin/net/corda/applications/workers/smoketest/ledger/UtxoLedgerTests.kt
@@ -84,7 +84,7 @@ class UtxoLedgerTests : ClusterReadiness by ClusterReadinessChecker() {
     @BeforeAll
     fun beforeAll() {
         // check cluster is ready
-        assertIsReady(Duration.ofMinutes(2), Duration.ofMillis(100))
+        //assertIsReady(Duration.ofMinutes(1), Duration.ofMillis(100))
 
         DEFAULT_CLUSTER.conditionallyUploadCpiSigningCertificate()
 
