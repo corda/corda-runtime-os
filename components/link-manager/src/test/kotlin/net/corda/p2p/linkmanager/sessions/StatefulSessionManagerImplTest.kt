77fca4f004cdde036ce56dd596f1deea142639e9
Will Vigor <william.vigor@r3.com>
2024-02-06 08:51:50 +0000

    Fix detekt

--- components/link-manager/src/test/kotlin/net/corda/p2p/linkmanager/sessions/StatefulSessionManagerImplTest.kt
+++ components/link-manager/src/test/kotlin/net/corda/p2p/linkmanager/sessions/StatefulSessionManagerImplTest.kt
@@ -33,7 +33,6 @@ import org.mockito.kotlin.whenever
 import java.time.Instant
 import net.corda.lifecycle.domino.logic.util.PublisherWithDominoLogic
 import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.Disabled
 import org.mockito.Mockito
 
 class StatefulSessionManagerImplTest {
