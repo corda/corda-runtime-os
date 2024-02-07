35f6a6871e3a1f7cc2a1b1fcb47c1e9b1704cec3
Dickon Reed <dickon.reed@r3.com>
2024-02-07 14:06:30 +0000

    Revert "CORE-17933 Implement fetching filtered transactions and signatures (#5599)" (#5644)
    This reverts commit 757da2f0892db056641db5bffdd415dcdf017742
    This requires a TODO implementation on a new Utxo Ledger operation.
    Hopefully this is temporary pending a fix to the underlying resolution problem.

--- components/ledger/ledger-utxo-flow/src/main/kotlin/net/corda/ledger/utxo/flow/impl/transaction/filtered/factory/UtxoFilteredTransactionFactoryImpl.kt
+++ components/ledger/ledger-utxo-flow/src/main/kotlin/net/corda/ledger/utxo/flow/impl/transaction/filtered/factory/UtxoFilteredTransactionFactoryImpl.kt
@@ -2,7 +2,6 @@ package net.corda.ledger.utxo.flow.impl.transaction.filtered.factory
 
 import net.corda.ledger.common.data.transaction.filtered.ComponentGroupFilterParameters
 import net.corda.ledger.common.data.transaction.filtered.ComponentGroupFilterParameters.AuditProof.AuditProofPredicate
-import net.corda.ledger.common.data.transaction.filtered.FilteredTransaction
 import net.corda.ledger.common.data.transaction.filtered.factory.FilteredTransactionFactory
 import net.corda.ledger.utxo.data.transaction.UtxoComponentGroup
 import net.corda.ledger.utxo.data.transaction.UtxoComponentGroup.METADATA
@@ -86,14 +85,6 @@ class UtxoFilteredTransactionFactoryImpl @Activate constructor(
                     filteredTransactionBuilder.commands
                 )
             )
-        )
-    }
-
-    @Suspendable
-    override fun create(filteredTransaction: FilteredTransaction): UtxoFilteredTransaction {
-        return UtxoFilteredTransactionImpl(
-            serializationService,
-            filteredTransaction
         )
     }
 }
