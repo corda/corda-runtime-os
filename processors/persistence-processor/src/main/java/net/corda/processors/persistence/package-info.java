15b99246b4a78678456b27b4b1a0bffa703639e1
Dickon Reed <dickon.reed@r3.com>
2024-02-07 17:02:25 +0000

    remote osgi ignore

--- processors/persistence-processor/src/main/java/net/corda/processors/persistence/package-info.java
+++ processors/persistence-processor/src/main/java/net/corda/processors/persistence/package-info.java
@@ -1,6 +1,4 @@
 @Export
-@QuasarIgnoreAllPackages
 package net.corda.processors.persistence;
 
-import co.paralleluniverse.quasar.annotations.QuasarIgnoreAllPackages;
 import org.osgi.annotation.bundle.Export;
