b288b99288b3c63cd84679bbd0c3dfc253c5d36f
ben-millar <ben.millar@r3.com>
2024-02-08 12:19:21 +0000

    CORE-19493 Replaced MD5 with SHA-256

--- components/flow/flow-rest-resource-service-impl/src/main/kotlin/net/corda/flow/rest/impl/utils/FlowStatusLookupUtils.kt
+++ components/flow/flow-rest-resource-service-impl/src/main/kotlin/net/corda/flow/rest/impl/utils/FlowStatusLookupUtils.kt
@@ -1,17 +1,17 @@
 package net.corda.flow.rest.impl.utils
 
+import net.corda.crypto.cipher.suite.sha256Bytes
 import net.corda.data.flow.FlowKey
-import net.corda.v5.base.util.EncodingUtils
+import net.corda.v5.base.util.EncodingUtils.toBase64
 import net.corda.virtualnode.toCorda
-import java.security.MessageDigest
 
 /**
  * The primary key in the database has a maximum size of [255] chars.
  * Given that we're only using the key to uniquely identify each FlowStatus, a hash is sufficient for achieving that.
- * We concatenate an MD5 hash of the [FlowKey.id] with the holding identity short hash, making the odds of collision low.
+ * We concatenate a SHA-256 hash of the [FlowKey.id] with the holding identity short hash, resulting in an 88 character ID.
  */
 fun FlowKey.hash() : String {
-    val idHash = EncodingUtils.toBase64(MessageDigest.getInstance("MD5").digest(this.id.toByteArray()))
+    val idHash = toBase64(this.id.toByteArray().sha256Bytes())
     val holdingIdentityShortHash = this.identity.toCorda().shortHash
     return "$idHash$holdingIdentityShortHash"
 }
