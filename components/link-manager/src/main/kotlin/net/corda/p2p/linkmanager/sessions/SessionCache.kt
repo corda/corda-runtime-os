aba7e7df05933b86996e5954eb902822fa6f3818
Yiftach Kaplan <67583323+yift-r3@users.noreply.github.com>
2024-02-08 15:10:41 +0000

    CORE-19598: Delete the correct version for the state manager (#5649)
    * CORE-19598: Delete the correct version for the state manager
    
    * Post merge changes
    
    * Apply suggestions from code review
    
    Co-authored-by: William Vigor <58432369+williamvigorr3@users.noreply.github.com>
    
    * Add missing test
    
    * Post merge fix issues
    
    ---------
    
    Co-authored-by: William Vigor <58432369+williamvigorr3@users.noreply.github.com>

--- components/link-manager/src/main/kotlin/net/corda/p2p/linkmanager/sessions/SessionCache.kt
+++ components/link-manager/src/main/kotlin/net/corda/p2p/linkmanager/sessions/SessionCache.kt
@@ -2,10 +2,13 @@ package net.corda.p2p.linkmanager.sessions
 
 import com.github.benmanes.caffeine.cache.Cache
 import com.github.benmanes.caffeine.cache.Caffeine
+import net.corda.cache.caffeine.CacheFactoryImpl
 import net.corda.libs.statemanager.api.State
 import net.corda.libs.statemanager.api.StateManager
+import net.corda.p2p.linkmanager.sessions.events.StatefulSessionEventPublisher
 import net.corda.p2p.linkmanager.sessions.metadata.CommonMetadata.Companion.toCommonMetadata
 import net.corda.utilities.time.Clock
+import org.slf4j.LoggerFactory
 import java.time.Duration
 import java.time.Instant
 import java.util.Random
@@ -14,8 +17,6 @@ import java.util.concurrent.Executors
 import java.util.concurrent.Future
 import java.util.concurrent.ScheduledExecutorService
 import java.util.concurrent.TimeUnit
-import net.corda.cache.caffeine.CacheFactoryImpl
-import net.corda.p2p.linkmanager.sessions.events.StatefulSessionEventPublisher
 
 internal class SessionCache(
     private val stateManager: StateManager,
@@ -26,11 +27,13 @@ internal class SessionCache(
 ) {
     private companion object {
         const val CACHE_SIZE = 10_000L
+        val logger = LoggerFactory.getLogger(SessionCache::class.java)
     }
     private val tasks = ConcurrentHashMap<String, SavedState>()
 
-    data class SavedState(
+    private data class SavedState(
         val expiry: Instant,
+        val version: Int,
         val future: Future<*>,
     )
 
@@ -91,26 +94,43 @@ internal class SessionCache(
         cachedOutboundSessions.invalidate(key)
     }
 
-    fun validateStateAndScheduleExpiry(state: State): State? {
+    fun validateStateAndScheduleExpiry(
+        state: State,
+        beforeUpdate: Boolean = false,
+    ): State? {
         val now = clock.instant()
         val expiry = state.metadata.toCommonMetadata().expiry
         val noise = Duration.of(
             noiseFactory.nextLong(20 * 60),
             TimeUnit.SECONDS.toChronoUnit(),
         )
+        val stateToForget = if (beforeUpdate) {
+            state.copy(
+                version = state.version + 1,
+            )
+        } else {
+            state
+        }
         val duration = Duration.between(now, expiry) - noise
         return if (duration.isNegative) {
-            forgetState(state)
+            forgetState(stateToForget)
             null
         } else {
             tasks.compute(state.key) { _, currentValue ->
-                if (currentValue?.expiry == expiry) {
+                if ((currentValue?.expiry == expiry) && (currentValue.version == stateToForget.version)) {
                     currentValue
                 } else {
                     currentValue?.future?.cancel(false)
                     SavedState(
                         expiry = expiry,
-                        future = scheduler.schedule({ forgetState(state) }, duration.toMillis(), TimeUnit.MILLISECONDS),
+                        version = stateToForget.version,
+                        future = scheduler.schedule(
+                            {
+                                forgetState(stateToForget)
+                            },
+                            duration.toMillis(),
+                            TimeUnit.MILLISECONDS,
+                        ),
                     )
                 }
             }
@@ -134,7 +154,11 @@ internal class SessionCache(
 
     private fun forgetState(state: State) {
         val key = state.key
-        stateManager.delete(listOf(state))
+        val failedDeleted = stateManager.delete(listOf(state))
+        if (failedDeleted.isNotEmpty()) {
+            logger.info("Failed to delete state: ${failedDeleted.keys.first()}, trying to delete the latest version")
+            stateManager.delete(failedDeleted.values)
+        }
         invalidate(key)
         eventPublisher.sessionDeleted(key)
         tasks.remove(key)
