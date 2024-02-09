486c2100ab6a38081ca54c60637013e5b62a4383
Dan Newton <dan.newton@r3.com>
2024-02-09 13:01:18 +0000

    CORE-19467 remove dependencies

--- libs/serialization/serialization-amqp-api/src/main/java/net/corda/internal/serialization/amqp/api/package-info.java
+++ libs/serialization/serialization-amqp-api/src/main/java/net/corda/internal/serialization/amqp/api/package-info.java
@@ -1,6 +1,4 @@
 @Export
-@QuasarIgnoreAllPackages
 package net.corda.internal.serialization.amqp.api;
 
 import org.osgi.annotation.bundle.Export;
-import co.paralleluniverse.quasar.annotations.QuasarIgnoreAllPackages;
