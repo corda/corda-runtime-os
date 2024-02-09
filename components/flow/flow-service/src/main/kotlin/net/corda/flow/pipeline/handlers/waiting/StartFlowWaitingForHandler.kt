5454dfe27ff68d468d9614a6d576a2516243dd5b
LWogan <lorcan.wogan@r3.com>
2024-02-09 15:26:29 +0000

    CORE-19630 fix missing avro type

--- components/flow/flow-service/src/main/kotlin/net/corda/flow/pipeline/handlers/waiting/StartFlowWaitingForHandler.kt
+++ components/flow/flow-service/src/main/kotlin/net/corda/flow/pipeline/handlers/waiting/StartFlowWaitingForHandler.kt
@@ -3,6 +3,7 @@ package net.corda.flow.pipeline.handlers.waiting
 import net.corda.flow.fiber.FlowContinuation
 import net.corda.flow.pipeline.events.FlowEventContext
 import org.osgi.service.component.annotations.Component
+import net.corda.data.flow.state.waiting.WaitingForStartFlow
 
 @Component(service = [FlowWaitingForHandler::class])
 class StartFlowWaitingForHandler : FlowWaitingForHandler<WaitingForStartFlow> {
