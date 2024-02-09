ec11494331d33dd83e38c48976b3bd68f4561aaa
LWogan <lorcan.wogan@r3.com>
2024-02-09 17:20:55 +0000

    CORE-19630 fix build

--- components/flow/flow-service/src/test/kotlin/net/corda/flow/external/events/impl/executor/ExternalEventExecutorImplTest.kt
+++ components/flow/flow-service/src/test/kotlin/net/corda/flow/external/events/impl/executor/ExternalEventExecutorImplTest.kt
@@ -1,11 +1,11 @@
 package net.corda.flow.external.events.impl.executor
 
 import net.corda.data.flow.event.external.ExternalEventResponse
-import net.corda.flow.application.serialization.SerializationServiceInternal
 import net.corda.flow.application.services.MockFlowFiberService
 import net.corda.flow.external.events.factory.ExternalEventFactory
 import net.corda.flow.fiber.FlowIORequest
 import net.corda.internal.serialization.SerializedBytesImpl
+import net.corda.internal.serialization.amqp.api.SerializationServiceInternal
 import net.corda.v5.base.annotations.CordaSerializable
 import net.corda.v5.crypto.SecureHash
 import org.junit.jupiter.api.Assertions.assertEquals
