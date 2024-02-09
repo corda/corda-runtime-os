03d8244059a99196e708346d7dbc0ba35e80ad30
Dickon Reed <dickon.reed@r3.com>
2024-02-09 16:36:59 +0000

    only show long mediator runs

--- libs/messaging/messaging-impl/src/main/kotlin/net/corda/messaging/mediator/processor/ConsumerProcessor.kt
+++ libs/messaging/messaging-impl/src/main/kotlin/net/corda/messaging/mediator/processor/ConsumerProcessor.kt
@@ -110,7 +110,6 @@ class ConsumerProcessor<K : Any, S : Any, E : Any>(
             val inputs = generateInputs(states.values, polledRecords)
             var groups = groupAllocator.allocateGroups(inputs, config)
 
-            val start = System.nanoTime()
             while (groups.isNotEmpty()) {
                 // Process each group on a thread
                 val outputs = groups.filter {
@@ -122,12 +121,11 @@ class ConsumerProcessor<K : Any, S : Any, E : Any>(
                     Pair(future, group)
                 }.map { (future, group) ->
                     try {
-                        val start = System.nanoTime()
-                        future.getOrThrow(Duration.ofMillis(config.processorTimeout)).also {
-                            val end = System.nanoTime()
-                            val rt = end-start
-                            log.info("mediator ${Thread.currentThread().id} complete took ${rt/1.0e6}ms")
-                        }
+                        future.getOrThrow(config.processorTimeout)//.also {
+                            //val end = System.nanoTime()
+                            //val rt = end-start
+                            //log.info("mediator ${Thread.currentThread().id} complete took ${rt/1.0e6}ms")
+                        //}
                     } catch (e: TimeoutException) {
                         log.error("timeout in mediator $group")
                         metrics.consumerProcessorFailureCounter.increment(group.keys.size.toDouble())
@@ -167,7 +165,9 @@ class ConsumerProcessor<K : Any, S : Any, E : Any>(
             // logged in SM and recorded by a metric
             stateManager.delete(statesToDelete)
         }
-        log.info("mediator ${Thread.currentThread().id} overall ${(System.nanoTime() - startTimestamp)/1e6}")
+        val rt = System.nanoTime() - startTimestamp
+        if (rt > 100*1000)
+            log.info("mediator ${Thread.currentThread().id} overall ${rt/1e6}ms")
         metrics.processorTimer.record(System.nanoTime() - startTimestamp, TimeUnit.NANOSECONDS)
     }
 
