0e6f046f9b87d94198b836b98006617d5f21b73e
TomFitzpatrick <tom.fitzpatrick@r3.com>
2024-02-12 14:02:01 +0000

    remove unneeded changes
    Signed-off-by: TomFitzpatrick <tom.fitzpatrick@r3.com>

--- components/virtual-node/sandbox-group-context-service/src/main/kotlin/net/corda/sandboxgroupcontext/service/impl/SandboxGroupContextCacheImpl.kt
+++ components/virtual-node/sandbox-group-context-service/src/main/kotlin/net/corda/sandboxgroupcontext/service/impl/SandboxGroupContextCacheImpl.kt
@@ -18,13 +18,13 @@ import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.ConcurrentMap
 
 internal class SandboxGroupContextCacheImpl private constructor(
-    override val capacities: MutableMap<SandboxGroupType, Long>,
+    override val capacities: Map<SandboxGroupType, Long>,
     private val evictionListeners: Map<SandboxGroupType, MutableSet<EvictionListener>>,
     private val expiryQueue: ReferenceQueue<SandboxGroupContextWrapper>,
     private val toBeClosed: MutableSet<ToBeClosed>
 ) : SandboxGroupContextCache {
     constructor(capacities: Long) : this(
-        capacities = SandboxGroupType.values().associateWith { capacities }.toMutableMap(),
+        capacities = SandboxGroupType.values().associateWith { capacities },
         evictionListeners = SandboxGroupType.values().associateWith { linkedSetOf<EvictionListener>() },
         expiryQueue = ReferenceQueue<SandboxGroupContextWrapper>(),
         toBeClosed = ConcurrentHashMap.newKeySet()
@@ -85,58 +85,46 @@ internal class SandboxGroupContextCacheImpl private constructor(
      * thread safe. This also ensures if purgeExpiryQueue closes any sandboxes they are closed in the same thread
      * as would be the case with all other calls to the same method.
      */
-    @Suppress("UNUSED_PARAMETER")
     private fun buildSandboxGroupTypeCache(
         type: SandboxGroupType,
-        capacityIn: Long
-    ): Cache<VirtualNodeContext, SandboxGroupContextWrapper>  {
-        val capacity = 1000L
-        logger.info("Set sandbox cache capacity to $capacity")
+        capacity: Long
+    ): Cache<VirtualNodeContext, SandboxGroupContextWrapper> = CacheFactoryImpl().buildNonAsync(
+        "sandbox-cache-${type}",
+        Caffeine.newBuilder()
+            .maximumSize(capacity)
+            // Add the wrapped [CloseableSandboxGroupContext] to the internal [expiryQueue],
+            // so it is only closed once it's safe to do so (i.e. wrapping [SandboxGroupContextWrapper]
+            // is not referenced anymore).
+            .removalListener { key, context, cause ->
+                purgeExpiryQueue()
+                key ?: return@removalListener
+                (context?.wrappedSandboxGroupContext as? AutoCloseable)?.also { autoCloseable ->
+                    toBeClosed += ToBeClosed(key, context.completion, autoCloseable, context, expiryQueue)
+                    onEviction(key)
+                }
 
-        return CacheFactoryImpl().buildNonAsync(
-            "sandbox-cache-${type}",
-            Caffeine.newBuilder()
-                .maximumSize(capacity)
-                // Add the wrapped [CloseableSandboxGroupContext] to the internal [expiryQueue],
-                // so it is only closed once it's safe to do so (i.e. wrapping [SandboxGroupContextWrapper]
-                // is not referenced anymore).
-                .removalListener { key, context, cause ->
-                    purgeExpiryQueue()
-                    key ?: return@removalListener
-                    (context?.wrappedSandboxGroupContext as? AutoCloseable)?.also { autoCloseable ->
-                        toBeClosed += ToBeClosed(key, context.completion, autoCloseable, context, expiryQueue)
-                        onEviction(key)
-                    }
-
-                    logger.info(
-                        "Evicting {} sandbox for {} holdingId {} [{}]",
-                        key.sandboxGroupType,
-                        key.holdingIdentity.x500Name,
-                        key.holdingIdentity.shortHash,
-                        cause.name
-                    )
-                })
-
-    }
-
-
+                logger.info(
+                    "Evicting {} sandbox for {} holdingId {} [{}]",
+                    key.sandboxGroupType,
+                    key.holdingIdentity.x500Name,
+                    key.holdingIdentity.shortHash,
+                    cause.name
+                )
+            }
+    )
 
     /**
      * Creates the cache for the given [sandboxGroupType] with [newCapacity] maximum size, if not created yet.
      * Changes the maximum size for the [sandboxGroupType]'s cache to [newCapacity] if the cache already exists.
      */
-    @Suppress("UNUSED_PARAMETER")
-
     override fun resize(sandboxGroupType: SandboxGroupType, newCapacity: Long) {
-        logger.info("resize sandbox cache to $newCapacity IGNORED")
-//        val sandboxCache = caches.computeIfAbsent(sandboxGroupType) { type ->
-//            buildSandboxGroupTypeCache(type, newCapacity)
-//        }
-//
-//        sandboxCache.policy().eviction().ifPresent {
-//            it.maximum = newCapacity
-//        }
-//        capacities[sandboxGroupType] = newCapacity
+        val sandboxCache = caches.computeIfAbsent(sandboxGroupType) { type ->
+            buildSandboxGroupTypeCache(type, newCapacity)
+        }
+
+        sandboxCache.policy().eviction().ifPresent {
+            it.maximum = newCapacity
+        }
     }
 
     /**
@@ -157,15 +145,7 @@ internal class SandboxGroupContextCacheImpl private constructor(
     private fun purgeExpiryQueue() {
         // Close the wrapped [CloseableSandboxGroupContext] for every [SandboxGroupContextWrapper]
         // that has already been garbage-collected.
-        val start = System.nanoTime()
-        logger.info("purgeExpiryQuery ${Thread.currentThread().id} starting toBeClosed=${toBeClosed.size}")
-        var count = 0
-        var closes = 0
-        var errors = 0
         while (true) {
-            count++
-
-            logger.info("purgeExpiryQuery  ${Thread.currentThread().id} loop iteration $count queue size is ${toBeClosed.size}")
             val head = expiryQueue.poll() as? ToBeClosed ?: break
             val vnc = head.cacheKey
 
@@ -178,18 +158,14 @@ internal class SandboxGroupContextCacheImpl private constructor(
 
                 head.sandboxGroupContextToClose.close()
                 head.completion.complete(true)
-                closes++
             } catch (exception: Exception) {
                 logger.warn(
                     "Error closing ${vnc.sandboxGroupType} sandbox for ${vnc.holdingIdentity.x500Name}",
                     exception
                 )
                 head.completion.completeExceptionally(exception)
-                errors++
             }
         }
-        logger.info("purgeExpiryQuery ${Thread.currentThread().id} finished after iterations=$count closed=$closes errors=$errors in ${(System.nanoTime()-start)/1.0e6}ms")
-
     }
 
     private val caches: ConcurrentMap<SandboxGroupType, Cache<VirtualNodeContext, SandboxGroupContextWrapper>> =
