ec5b28847f76cbc75e6059252dcfe99a1c79eaec
nkovacsx <nandor.kovacs@r3.com>
2024-02-12 15:36:50 +0000

    Init

--- components/ledger/ledger-utxo-flow/src/main/kotlin/net/corda/ledger/utxo/flow/impl/flows/transactiontransmission/ReceiveTransactionFlow.kt
+++ components/ledger/ledger-utxo-flow/src/main/kotlin/net/corda/ledger/utxo/flow/impl/flows/transactiontransmission/ReceiveTransactionFlow.kt
@@ -1,83 +1,42 @@
 package net.corda.ledger.utxo.flow.impl.flows.transactiontransmission
 
-import net.corda.ledger.common.data.transaction.TransactionStatus.VERIFIED
-import net.corda.ledger.common.flow.flows.Payload
-import net.corda.ledger.utxo.flow.impl.flows.backchain.InvalidBackchainException
-import net.corda.ledger.utxo.flow.impl.flows.backchain.TransactionBackchainResolutionFlow
-import net.corda.ledger.utxo.flow.impl.flows.backchain.dependencies
-import net.corda.ledger.utxo.flow.impl.flows.finality.getVisibleStateIndexes
-import net.corda.ledger.utxo.flow.impl.persistence.UtxoLedgerPersistenceService
-import net.corda.ledger.utxo.flow.impl.transaction.UtxoSignedTransactionInternal
-import net.corda.ledger.utxo.flow.impl.transaction.verifier.UtxoLedgerTransactionVerificationService
+import net.corda.flow.application.services.VersioningService
+import net.corda.flow.application.versioning.VersionedReceiveFlowFactory
+import net.corda.ledger.utxo.flow.impl.flows.transactiontransmission.v1.ReceiveTransactionFlowV1
+import net.corda.libs.platform.PlatformVersion
 import net.corda.sandbox.CordaSystemFlow
-import net.corda.utilities.trace
-import net.corda.v5.application.crypto.DigitalSignatureAndMetadata
 import net.corda.v5.application.flows.CordaInject
-import net.corda.v5.application.flows.FlowEngine
 import net.corda.v5.application.flows.SubFlow
 import net.corda.v5.application.messaging.FlowSession
 import net.corda.v5.base.annotations.Suspendable
-import net.corda.v5.base.exceptions.CordaRuntimeException
-import net.corda.v5.ledger.utxo.VisibilityChecker
 import net.corda.v5.ledger.utxo.transaction.UtxoSignedTransaction
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
+import java.lang.IllegalArgumentException
 
 @CordaSystemFlow
 class ReceiveTransactionFlow(
     private val session: FlowSession
 ) : SubFlow<UtxoSignedTransaction> {
 
-    private companion object {
-        private val log: Logger = LoggerFactory.getLogger(ReceiveTransactionFlow::class.java)
-    }
-
     @CordaInject
-    lateinit var transactionVerificationService: UtxoLedgerTransactionVerificationService
-
-    @CordaInject
-    lateinit var flowEngine: FlowEngine
-
-    @CordaInject
-    lateinit var ledgerPersistenceService: UtxoLedgerPersistenceService
-
-    @CordaInject
-    lateinit var visibilityChecker: VisibilityChecker
+    lateinit var versioningService: VersioningService
 
     @Suspendable
     override fun call(): UtxoSignedTransaction {
-        val transaction = session.receive(UtxoSignedTransactionInternal::class.java)
+        return versioningService.versionedSubFlow(
+            ReceiveTransactionFlowVersionedFlowFactory(),
+            session
+        )
+    }
+}
 
-        val transactionDependencies = transaction.dependencies
-        if (transactionDependencies.isNotEmpty()) {
-            try {
-                flowEngine.subFlow(TransactionBackchainResolutionFlow(transactionDependencies, session))
-            } catch (e: InvalidBackchainException) {
-                val message = "Invalid transaction: ${transaction.id} found during back-chain resolution."
-                log.warn(message, e)
-                session.send(Payload.Failure<List<DigitalSignatureAndMetadata>>(message))
-                throw e
-            }
-        } else {
-            log.trace {
-                "Transaction with id ${transaction.id} has no dependencies so backchain resolution will not be performed."
-            }
+class ReceiveTransactionFlowVersionedFlowFactory : VersionedReceiveFlowFactory<UtxoSignedTransaction> {
+
+    override val versionedInstanceOf: Class<ReceiveTransactionFlow> = ReceiveTransactionFlow::class.java
+
+    override fun create(version: Int, session: FlowSession): SubFlow<UtxoSignedTransaction> {
+        return when {
+            version >= PlatformVersion.CORDA_5_2.value -> ReceiveTransactionFlowV1(session)
+            else -> throw IllegalArgumentException("Unsupported version: $version for SendTransactionFlow")
         }
-
-        try {
-            transaction.verifySignatorySignatures()
-            transaction.verifyAttachedNotarySignature()
-            transactionVerificationService.verify(transaction.toLedgerTransaction())
-        } catch (e: Exception) {
-            val message = "Failed to verify transaction and signatures of transaction: ${transaction.id}"
-            log.warn(message, e)
-            session.send(Payload.Failure<List<DigitalSignatureAndMetadata>>(message))
-            throw CordaRuntimeException(message, e)
-        }
-
-        ledgerPersistenceService.persist(transaction, VERIFIED, transaction.getVisibleStateIndexes(visibilityChecker))
-        session.send(Payload.Success("Successfully received transaction."))
-
-        return transaction
     }
 }
--- components/ledger/ledger-utxo-flow/src/main/kotlin/net/corda/ledger/utxo/flow/impl/flows/transactiontransmission/SendTransactionFlow.kt
+++ components/ledger/ledger-utxo-flow/src/main/kotlin/net/corda/ledger/utxo/flow/impl/flows/transactiontransmission/SendTransactionFlow.kt
@@ -1,9 +1,19 @@
 package net.corda.ledger.utxo.flow.impl.flows.transactiontransmission
 
+import net.corda.flow.application.services.VersioningService
+import net.corda.flow.application.versioning.VersionedSendFlowFactory
 import net.corda.ledger.common.flow.flows.Payload
 import net.corda.ledger.utxo.flow.impl.flows.backchain.TransactionBackchainSenderFlow
 import net.corda.ledger.utxo.flow.impl.flows.backchain.dependencies
+import net.corda.ledger.utxo.flow.impl.flows.transactionbuilder.v1.SendTransactionBuilderDiffFlowV1
+import net.corda.ledger.utxo.flow.impl.flows.transactionbuilder.v2.SendTransactionBuilderDiffFlowV2
+import net.corda.ledger.utxo.flow.impl.flows.transactiontransmission.v1.SendTransactionFlowV1
+import net.corda.ledger.utxo.flow.impl.transaction.UtxoBaselinedTransactionBuilder
 import net.corda.ledger.utxo.flow.impl.transaction.UtxoSignedTransactionInternal
+import net.corda.ledger.utxo.flow.impl.transaction.UtxoTransactionBuilderContainer
+import net.corda.ledger.utxo.flow.impl.transaction.UtxoTransactionBuilderInternal
+import net.corda.libs.platform.PlatformVersion
+import net.corda.libs.platform.PlatformVersion.CORDA_5_2
 import net.corda.sandbox.CordaSystemFlow
 import net.corda.utilities.trace
 import net.corda.v5.application.flows.CordaInject
@@ -15,6 +25,7 @@ import net.corda.v5.base.annotations.Suspendable
 import net.corda.v5.base.exceptions.CordaRuntimeException
 import net.corda.v5.ledger.utxo.transaction.UtxoSignedTransaction
 import org.slf4j.LoggerFactory
+import java.lang.IllegalArgumentException
 
 @CordaSystemFlow
 class SendTransactionFlow(
@@ -22,33 +33,33 @@ class SendTransactionFlow(
     private val sessions: List<FlowSession>
 ) : SubFlow<Unit> {
 
-    private companion object { val log = LoggerFactory.getLogger(this::class.java) }
-
     @CordaInject
-    lateinit var flowMessaging: FlowMessaging
-
-    @CordaInject
-    lateinit var flowEngine: FlowEngine
+    lateinit var versioningService: VersioningService
 
     @Suspendable
     override fun call() {
-        flowMessaging.sendAll(transaction as UtxoSignedTransactionInternal, sessions.toSet())
+        return versioningService.versionedSubFlow(
+            SendTransactionFlowVersionedFlowFactory(
+                transaction,
+            ),
+            sessions
+        )
+    }
+}
 
-        sessions.forEach {
-            if (transaction.dependencies.isNotEmpty()) {
-                flowEngine.subFlow(TransactionBackchainSenderFlow(transaction.id, it))
-            } else {
-                log.trace {
-                    "Transaction with id ${transaction.id} has no dependencies so backchain resolution will not be performed."
-                }
-            }
+class SendTransactionFlowVersionedFlowFactory(
+    private val transaction: UtxoSignedTransaction
+) : VersionedSendFlowFactory<Unit> {
 
-            val sendingTransactionResult = it.receive(Payload::class.java)
-            if (sendingTransactionResult is Payload.Failure) {
-                throw CordaRuntimeException(
-                    sendingTransactionResult.message
-                )
-            }
+    override val versionedInstanceOf: Class<SendTransactionFlow> = SendTransactionFlow::class.java
+
+    override fun create(version: Int, sessions: List<FlowSession>): SubFlow<Unit> {
+        return when {
+            version >= CORDA_5_2.value -> SendTransactionFlowV1(
+                transaction,
+                sessions
+            )
+            else -> throw IllegalArgumentException("Unsupported version: $version for SendTransactionFlow")
         }
     }
 }
