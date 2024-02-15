76eca325ab48ba0c290c1c7af32e7ed0ba7d8281
Yiftach Kaplan <yiftach.kaplan@r3.com>
2024-02-15 16:59:56 +0000

    zip

--- libs/utilities/src/main/kotlin/net/corda/utilities/Context.kt
+++ libs/utilities/src/main/kotlin/net/corda/utilities/Context.kt
@@ -3,19 +3,51 @@ package net.corda.utilities
 import org.slf4j.LoggerFactory
 import java.io.File
 import java.time.Clock
+import java.util.concurrent.atomic.AtomicLong
+import java.util.concurrent.locks.ReentrantLock
+import java.util.zip.ZipEntry
+import java.util.zip.ZipOutputStream
+import kotlin.concurrent.thread
+import kotlin.concurrent.withLock
 
 object Context {
     private val logger = LoggerFactory.getLogger("QQQ")
     private val clock = Clock.systemUTC()
+    private val index = AtomicLong(0)
+    private val lines = AtomicLong(0)
+    private val lock = ReentrantLock()
+
     val context = ThreadLocal<String>();
 
     fun myLog(str: String) {
         try {
-            val file = File("/tmp/logs/log.txt")
-            file.parentFile.mkdirs()
-            file.appendText("${clock.instant()} $str\n")
+            lock.withLock {
+                val file = File("/tmp/logs/log.$index.txt")
+                file.parentFile.mkdirs()
+                file.appendText("${clock.instant()} $str\n")
+                if (lines.incrementAndGet() > 10000) {
+                    lines.set(0)
+                    index.incrementAndGet()
+                    zip(file)
+                }
+            }
         } catch (e: Exception) {
             logger.warn("OOPS: $e - $str", e)
         }
+    }
+
+    private fun zip(input: File) {
+        thread {
+            File(input.parentFile, "${input.name}.zip").outputStream().use { fileOutputStream->
+                ZipOutputStream(fileOutputStream).use { zipOutputStream ->
+                    val entry = ZipEntry(input.name)
+                    zipOutputStream.putNextEntry(entry)
+                    zipOutputStream.write(input.readBytes())
+                    zipOutputStream.closeEntry()
+                }
+            }
+            input.delete()
+        }
+
     }
 }
