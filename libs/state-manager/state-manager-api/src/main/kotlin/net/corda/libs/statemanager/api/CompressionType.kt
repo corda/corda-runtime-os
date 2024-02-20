9d77173954f84dbedd68b866dcc61c2516adf06a
LWogan <lorcan.wogan@r3.com>
2024-02-20 12:35:00 +0000

    CORE-19737 pr comments

--- libs/state-manager/state-manager-api/src/main/kotlin/net/corda/libs/statemanager/api/CompressionType.kt
+++ libs/state-manager/state-manager-api/src/main/kotlin/net/corda/libs/statemanager/api/CompressionType.kt
@@ -3,11 +3,22 @@ package net.corda.libs.statemanager.api
 import java.nio.charset.StandardCharsets
 
 enum class CompressionType(val header: String) {
-    SNAPPY("snap"),
+    SNAPPY("snappy"),
     NONE("none");
 
     fun getHeader(): ByteArray {
         val headerBytes = this.header.toByteArray(StandardCharsets.UTF_8)
-        return headerBytes.copyInto(ByteArray(4))
+        return headerBytes.copyInto(ByteArray(HEADER_SIZE))
+    }
+
+    companion object {
+
+        const val HEADER_SIZE = 8
+
+        fun fromHeader(headerBytes: ByteArray): CompressionType? {
+            val headerBytesTrimmed = headerBytes.filterNot { it.toInt() == 0 }.toByteArray()
+            val headerString = headerBytesTrimmed.toString(StandardCharsets.UTF_8)
+            return values().find { it.header == headerString }
+        }
     }
 }
--- libs/state-manager/state-manager-db-impl/src/main/kotlin/net/corda/libs/statemanager/impl/compression/impl/CompressionServiceImpl.kt
+++ libs/state-manager/state-manager-db-impl/src/main/kotlin/net/corda/libs/statemanager/impl/compression/impl/CompressionServiceImpl.kt
@@ -1,13 +1,13 @@
 package net.corda.libs.statemanager.impl.compression.impl
 
 import net.corda.libs.statemanager.api.CompressionType
+import net.corda.libs.statemanager.api.CompressionType.Companion.HEADER_SIZE
 import net.corda.libs.statemanager.impl.compression.CompressionService
 import net.corda.utilities.compressSnappy
 import net.corda.utilities.debug
 import net.corda.utilities.decompressSnappy
 import org.osgi.service.component.annotations.Component
 import org.slf4j.LoggerFactory
-import java.nio.charset.StandardCharsets
 
 @Component(service = [CompressionService::class])
 class CompressionServiceImpl : CompressionService {
@@ -25,20 +25,21 @@ class CompressionServiceImpl : CompressionService {
     }
 
     override fun readBytes(bytes: ByteArray): ByteArray {
-        if (bytes.size < 4) {
+        if (bytes.size < HEADER_SIZE) {
             log.debug { "Read ByteArray from state manager whose size was less than 4" }
             return bytes
         }
 
-        val headerBytes = bytes.copyOfRange(0, 4)
-        val header = headerBytes.toString(StandardCharsets.UTF_8)
+        val headerBytes = bytes.copyOfRange(0, HEADER_SIZE)
         val compressionType = try {
-            CompressionType.valueOf(header)
-        } catch (ex: Exception) { CompressionType.NONE }
+            CompressionType.fromHeader(headerBytes)
+        } catch (ex: Exception) {
+            CompressionType.NONE
+        }
 
         return when (compressionType) {
             CompressionType.NONE -> bytes
-            CompressionType.SNAPPY -> bytes.copyOfRange(4, bytes.size).decompressSnappy()
+            CompressionType.SNAPPY -> bytes.copyOfRange(HEADER_SIZE, bytes.size).decompressSnappy()
             else -> bytes
         }
     }
