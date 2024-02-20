2e3aab2c58ab5d7b22ba496a0d6068c995250e90
Joseph Zuniga-Daly <joseph.zunigadaly@r3.com>
2024-02-20 09:20:11 +0000

    CORE-19733: Improve readability of default config

--- libs/permissions/permission-manager-impl/src/main/kotlin/net/corda/libs/permissions/manager/impl/SmartConfigUtil.kt
+++ libs/permissions/permission-manager-impl/src/main/kotlin/net/corda/libs/permissions/manager/impl/SmartConfigUtil.kt
@@ -2,10 +2,11 @@ package net.corda.libs.permissions.manager.impl
 
 import net.corda.libs.configuration.SmartConfig
 import net.corda.schema.configuration.ConfigKeys.REST_ENDPOINT_TIMEOUT_MILLIS
+import net.corda.utilities.seconds
 import java.time.Duration
 
 internal object SmartConfigUtil {
-    private const val DEFAULT_ENDPOINT_TIMEOUT_MS = 30000L
+    private val DEFAULT_ENDPOINT_TIMEOUT_MS = 30.seconds.toMillis()
 
     fun SmartConfig.getEndpointTimeout(): Duration {
         return if (hasPath(REST_ENDPOINT_TIMEOUT_MILLIS)) {
