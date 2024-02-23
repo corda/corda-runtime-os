6cf3903835210c3ca917ef96de566099bf310da0
Dickon Reed <dickon.reed@r3.com>
2024-02-23 17:40:27 +0000

    explain what this stuff does

--- processors/persistence-processor/src/main/kotlin/net/corda/processors/persistence/PersistenceProcessor.kt
+++ processors/persistence-processor/src/main/kotlin/net/corda/processors/persistence/PersistenceProcessor.kt
@@ -2,7 +2,16 @@ package net.corda.processors.persistence
 
 import net.corda.libs.configuration.SmartConfig
 
-/** The processor for a `PersistenceWorker`. */
+/** The processor for a `PersistenceWorker`. All this processor will actually do is set up the
+ * database connection manager once Corda configuration is available.
+ *
+ * The entry point to the persistence processor is actually []LedgerPersistenceRequestProcessor]
+ *
+ * This interface is an OSGi dependency of the [PersistenceWorker] and [CombinedWorker], which
+ * call start and stop as the worker process that contains an PersistenceProcessor object starts
+ * and stops.
+ **/
+
 interface PersistenceProcessor {
     /**
      * Starts performing the work of the Persistence worker.
