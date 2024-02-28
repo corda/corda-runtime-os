e5fcc7e88591bc479a971a58d7227296e273f9b9
Filipe Oliveira <filipe.oliveira@r3.com>
2024-02-28 11:45:09 +0000

    CORE-19325 - Excluded the path /metrics from tracing

--- libs/tracing-impl/src/main/kotlin/net/corda/tracing/brave/BraveTracingService.kt
+++ libs/tracing-impl/src/main/kotlin/net/corda/tracing/brave/BraveTracingService.kt
@@ -113,7 +113,9 @@ internal class BraveTracingService(serviceName: String, zipkinHost: String?, sam
     private val serverSampler: SamplerFunction<HttpRequest> = HttpRuleSampler.newBuilder()
         .putRule(and(methodEquals("POST"), pathStartsWith("/api/v5_1/flow")), sampler(samplesPerSecond))
         .putRule(and(methodEquals("POST"), pathStartsWith("/api/v1/flow")), sampler(samplesPerSecond))
-        .putRule(pathStartsWith("/"), sampler(samplesPerSecond)).build()
+        .putRule(and(methodEquals("GET"), pathStartsWith("/metrics")), sampler(PerSecond(0))) // Disable tracing for the specified path
+        .putRule(pathStartsWith("/"), sampler(samplesPerSecond))
+        .build()
 
     private val httpTracing by lazy {
         HttpTracing.newBuilder(tracing)
