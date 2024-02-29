6097d7a53d5b4128dca71e03d5c11ebf177ccd22
Bogdan Paunescu <bogdan.paunescu@r3.com>
2024-02-29 12:01:00 +0000

    core-19140 ensure cpk metadata is retrieved using same fake service used to load cpi (#5767)

--- testing/sandboxes/src/main/kotlin/net/corda/testing/sandboxes/VirtualNodeLoader.kt
+++ testing/sandboxes/src/main/kotlin/net/corda/testing/sandboxes/VirtualNodeLoader.kt
@@ -1,11 +1,14 @@
 package net.corda.testing.sandboxes
 
 import net.corda.libs.packaging.core.CpiIdentifier
+import net.corda.libs.packaging.core.CpiMetadata
 import net.corda.virtualnode.HoldingIdentity
 import net.corda.virtualnode.VirtualNodeInfo
+import java.util.concurrent.CompletableFuture
 
 interface VirtualNodeLoader {
     fun loadVirtualNode(resourceName: String, holdingIdentity: HoldingIdentity): VirtualNodeInfo
     fun unloadVirtualNode(virtualNodeInfo: VirtualNodeInfo)
     fun forgetCPI(id: CpiIdentifier)
+    fun getCpiMetadata(id: CpiIdentifier): CompletableFuture<CpiMetadata?>
 }
