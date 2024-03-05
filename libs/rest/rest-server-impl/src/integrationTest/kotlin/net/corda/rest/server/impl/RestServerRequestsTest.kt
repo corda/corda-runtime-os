f01c6eca49b1834168199e61599089c7d2fe9afe
benyip123 <ben.yip@r3.com>
2024-03-05 16:56:50 +0000

    Fix integration test

--- libs/rest/rest-server-impl/src/integrationTest/kotlin/net/corda/rest/server/impl/RestServerRequestsTest.kt
+++ libs/rest/rest-server-impl/src/integrationTest/kotlin/net/corda/rest/server/impl/RestServerRequestsTest.kt
@@ -93,10 +93,8 @@ class RestServerRequestsTest : RestServerTestBase() {
             password
         )
         assertEquals(HttpStatus.SC_NOT_FOUND, invalidPathResponse.responseStatus)
-        assertEquals(
-            "/api/${apiVersion.versionPath}/invalidPath",
-            (invalidPathResponse.body!!.asMapFromJson()["details"] as Map<*, *>)["url"]
-        )
+        assertThat((invalidPathResponse.body!!.asMapFromJson()["details"] as Map<*, *>)["url"].toString())
+            .contains("/api/${apiVersion.versionPath}/invalidPath")
     }
 
     @Test
