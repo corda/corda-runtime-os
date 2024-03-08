55c60e00053feba574b1c1d56ed879ee4439c2f2
Filipe Oliveira <filipe.oliveira@r3.com>
2024-03-08 17:15:14 +0000

    CORE-14483 - Log entries generated inside Flow code now have a trace ID in the MDC (#5844)
    Updated the code so the `traceId` is preserved when logs are generated by the flow. This helps fetching the relevant logs when troubleshooting bugs and reduces the risk of overwhelming Grafana.
    Added the `traceContext` to `FlowFiberExecutionContext`.
    Replaced the underscore by a dot so the mdc names are consistent in the logs.
    The function `LogUtils.clearMDC()` should only clear the properties that are set explicitly by Corda. The MDC properties set by third-party dependencies should not be cleared out.
    Renamed the MDC variables in `AuthorizationUtils` to follow the same naming pattern found in `LogUtils`
    Moved the MDC variables in `AuthorizationUtils` to `LogUtils` to make it easier to find all MDC related variables.
    Added the prefix `corda.` to the MDC related strings so we can tell which ones are set by Corda explicitly.

--- components/flow/flow-service/src/main/kotlin/net/corda/flow/fiber/FlowFiberExecutionContext.kt
+++ components/flow/flow-service/src/main/kotlin/net/corda/flow/fiber/FlowFiberExecutionContext.kt
@@ -8,6 +8,7 @@ import net.corda.libs.configuration.SmartConfig
 import net.corda.membership.read.MembershipGroupReader
 import net.corda.sandboxgroupcontext.CurrentSandboxGroupContext
 import net.corda.serialization.checkpoint.NonSerializable
+import net.corda.tracing.TraceContext
 import net.corda.v5.base.types.MemberX500Name
 import net.corda.virtualnode.HoldingIdentity
 
@@ -20,7 +21,8 @@ class FlowFiberExecutionContext(
     val currentSandboxGroupContext: CurrentSandboxGroupContext,
     val mdcLoggingData: Map<String, String>,
     val flowMetrics: FlowMetrics,
-    val configs: Map<String, SmartConfig>
+    val configs: Map<String, SmartConfig>,
+    val traceContext: TraceContext
 ) : NonSerializable {
     val memberX500Name: MemberX500Name get() = holdingIdentity.x500Name
     val flowStackService: FlowStack get() = flowCheckpoint.flowStack
