f6497ca2e269898a61896ac89c62d4654cbb2380
nikinagy <nikolett.nagy@r3.com>
2024-03-08 13:57:26 +0000

    disable forget for testing

--- components/link-manager/src/main/kotlin/net/corda/p2p/linkmanager/sessions/SessionCache.kt
+++ components/link-manager/src/main/kotlin/net/corda/p2p/linkmanager/sessions/SessionCache.kt
@@ -139,7 +139,7 @@ internal class SessionCache(
         }
         val duration = Duration.between(now, expiry) - noise
         return if (duration.isNegative) {
-            forgetState(stateToForget)
+            //forgetState(stateToForget)
             null
         } else {
             tasks.compute(state.key) { _, currentValue ->
@@ -152,7 +152,7 @@ internal class SessionCache(
                         version = stateToForget.version,
                         future = scheduler.schedule(
                             {
-                                forgetState(stateToForget)
+                                //forgetState(stateToForget)
                             },
                             duration.toMillis(),
                             TimeUnit.MILLISECONDS,
@@ -212,7 +212,7 @@ internal class SessionCache(
                 logger.warn("Failed to delete session state for '$key', state does not exist")
                 return
             }
-            forgetState(state)
+            //forgetState(state)
         } catch (e: Exception) {
             logger.error("Unexpected error while trying to fetch session state for '$key'.", e)
         }
