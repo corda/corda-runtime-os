833c2fe91aae6fa0f6ab24a57267dd6329a074ec
Anton Subbotin <anton.subbotin@r3.com>
2024-03-08 10:59:22 +0000

    Remove file helpers from sdk - file existence verification should be done elsewhere

--- libs/corda-sdk/src/main/kotlin/net/corda/sdk/packaging/signing/CertificateLoader.kt
+++ libs/corda-sdk/src/main/kotlin/net/corda/sdk/packaging/signing/CertificateLoader.kt
@@ -1,6 +1,5 @@
 package net.corda.sdk.packaging.signing
 
-import net.corda.sdk.packaging.FileHelpers
 import java.io.File
 import java.security.KeyStore
 import java.security.cert.X509Certificate
@@ -10,7 +9,6 @@ object CertificateLoader {
      * Reads trusted certificates from keystore [keyStoreFileName]
      */
     fun readCertificates(keyStoreFileName: String, keyStorePass: String): Collection<X509Certificate> {
-        FileHelpers.requireFileExists(keyStoreFileName)
         val keyStore = KeyStore.getInstance(File(keyStoreFileName), keyStorePass.toCharArray())
         return keyStore.aliases().asSequence()
             .filter(keyStore::isCertificateEntry)
