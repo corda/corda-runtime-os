ef6a509dfbc0ebcffeec16936391bb028a0ee718
Dan Newton <dan.newton@r3.com>
2024-03-12 19:04:18 +0000

    add max duration for recovery flow

--- notary-plugins/notary-plugin-common/src/main/kotlin/com/r3/corda/notary/plugin/common/recovery/NotarizedTransactionRecoveryFlow.kt
+++ notary-plugins/notary-plugin-common/src/main/kotlin/com/r3/corda/notary/plugin/common/recovery/NotarizedTransactionRecoveryFlow.kt
@@ -6,6 +6,7 @@ import net.corda.v5.application.flows.CordaInject
 import net.corda.v5.application.marshalling.JsonMarshallingService
 import net.corda.v5.base.annotations.Suspendable
 import net.corda.v5.ledger.utxo.UtxoLedgerService
+import java.time.Duration
 import java.time.Instant
 
 class NotarizedTransactionRecoveryFlow : ClientStartableFlow {
@@ -19,9 +20,13 @@ class NotarizedTransactionRecoveryFlow : ClientStartableFlow {
     @Suspendable
     override fun call(requestBody: ClientRequestBody): String {
         val parameters = requestBody.getRequestBodyAs(jsonMarshallingService, Parameters::class.java)
-        utxoLedgerService.recoverMissedNotarisedTransactions(Instant.ofEpochMilli(parameters.from), Instant.ofEpochMilli(parameters.until))
+        utxoLedgerService.recoverMissedNotarisedTransactions(
+            Instant.ofEpochMilli(parameters.from),
+            Instant.ofEpochMilli(parameters.until),
+            Duration.ofSeconds(parameters.duration)
+        )
         return ""
     }
 
-    data class Parameters(val from: Long, val until: Long)
+    data class Parameters(val from: Long, val until: Long, val duration: Long)
 }
