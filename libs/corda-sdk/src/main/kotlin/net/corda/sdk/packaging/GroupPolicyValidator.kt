c726ff42622717bda5e1d33e61e9e5d47ba9d3d2
Anton Subbotin <anton.subbotin@r3.com>
2024-03-13 09:49:10 +0000

    ES-1832: Move CPI generation from plugins to corda-sdk (#5841)
    - Move the logic from creating a CPI file from the Corda CLI package plugin into Corda SDK library.
    - The functionality of the Corda CLI plugin remains unchanged but uses the CPI file logic from the library:
      - Test coverage added for the SDK logic largely overlaps with the existing test coverage of the CLI plugin; however it's better to keep having both
    - The dependency from the Network plugin has been updated to use SDK code
    - All logic remained unchanged, including internals of CPI building and signing, and CLI plugin interface
    - Most of the tests were copied and refactored to use the library; certain new/missing coverage was added as well

--- libs/corda-sdk/src/main/kotlin/net/corda/sdk/packaging/GroupPolicyValidator.kt
+++ libs/corda-sdk/src/main/kotlin/net/corda/sdk/packaging/GroupPolicyValidator.kt
@@ -1,16 +1,18 @@
-package net.corda.cli.plugins.packaging
+package net.corda.sdk.packaging
 
 import net.corda.membership.lib.grouppolicy.GroupPolicyParser
+import net.corda.membership.lib.schema.validation.MembershipSchemaValidationException
 import net.corda.membership.lib.schema.validation.impl.MembershipSchemaValidatorImpl
 import net.corda.schema.membership.MembershipSchema
 import net.corda.schema.membership.provider.MembershipSchemaProviderFactory
 import net.corda.v5.base.versioning.Version
 
-internal object GroupPolicyValidator {
+object GroupPolicyValidator {
     /**
      * Validates group policy against schema.
+     * @throws MembershipSchemaValidationException
      */
-    fun validateGroupPolicy(groupPolicyString: String)  {
+    fun validateGroupPolicy(groupPolicyString: String) {
         val fileFormatVersion = GroupPolicyParser.getFileFormatVersion(groupPolicyString)
 
         val membershipSchemaValidator = MembershipSchemaValidatorImpl(MembershipSchemaProviderFactory.getSchemaProvider())
