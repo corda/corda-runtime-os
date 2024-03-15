8a9cc192153b94c081abb0b7af264fedd5ff086d
rick.parker <rick.parker@r3cev.com>
2024-03-15 09:15:29 +0000

    Adding primary key support to request ID

--- components/flow/flow-service/src/main/kotlin/net/corda/flow/external/events/impl/executor/ExternalEventExecutorImpl.kt
+++ components/flow/flow-service/src/main/kotlin/net/corda/flow/external/events/impl/executor/ExternalEventExecutorImpl.kt
@@ -7,9 +7,11 @@ import net.corda.flow.external.events.factory.ExternalEventFactory
 import net.corda.flow.fiber.FlowFiber
 import net.corda.flow.fiber.FlowFiberService
 import net.corda.flow.fiber.FlowIORequest
+import net.corda.v5.application.serialization.SerializationService
 import net.corda.v5.base.annotations.Suspendable
 import net.corda.v5.base.util.EncodingUtils.toBase64
 import net.corda.v5.serialization.SingletonSerializeAsToken
+import org.checkerframework.dataflow.qual.Deterministic
 import org.osgi.service.component.annotations.Activate
 import org.osgi.service.component.annotations.Component
 import org.osgi.service.component.annotations.Reference
@@ -27,15 +29,14 @@ class ExternalEventExecutorImpl @Activate constructor(
         factoryClass: Class<out ExternalEventFactory<PARAMETERS, RESPONSE, RESUME>>,
         parameters: PARAMETERS
     ): RESUME {
-        // `requestId` is a deterministic ID per event which allows us to achieve idempotency by de-duplicating events processing;
-        //  A deterministic ID is required so that events replayed from the flow engine won't be reprocessed on the consumer-side.
-        val uuid = deterministicBytesID(parameters)
 
         @Suppress("unchecked_cast")
         return with(flowFiberService.getExecutingFiber()) {
             suspend(
+                // `requestId` is a deterministic ID per event which allows us to achieve idempotency by de-duplicating events processing;
+                //  A deterministic ID is required so that events replayed from the flow engine won't be reprocessed on the consumer-side.
                 FlowIORequest.ExternalEvent(
-                    generateRequestId(uuid, this),
+                    RequestID.generateRequestId(parameters, this, serializationService),
                     factoryClass,
                     parameters,
                     externalContext(this)
@@ -51,14 +52,11 @@ class ExternalEventExecutorImpl @Activate constructor(
                 platformContextProperties = this.flattenPlatformProperties()
             )
         }
+}
 
-    private fun <PARAMETERS : Any> deterministicBytesID(parameters: PARAMETERS): String {
-        return hash(serializationService.serialize(parameters).bytes)
-    }
-
-    private fun hash(bytes: ByteArray) = toBase64(bytes.sha256Bytes())
-
-    private fun generateRequestId(hashedInput: String, flowFiber: FlowFiber): String {
+object RequestID {
+    fun generateRequestId(deterministic: Any, flowFiber: FlowFiber, serializationService: SerializationService): String {
+        val hashedInput = deterministicBytesID(serializationService, deterministic)
         val flowCheckpoint = flowFiber
             .getExecutionContext()
             .flowCheckpoint
@@ -66,6 +64,18 @@ class ExternalEventExecutorImpl @Activate constructor(
         val flowId = flowCheckpoint.flowId
         val suspendCount = flowCheckpoint.suspendCount
 
-        return "$flowId-$hashedInput-$suspendCount"
+        return "$flowId-$suspendCount-$hashedInput"
+    }
+
+    fun replaceHash(requestId: String, deterministic: Any, serializationService: SerializationService): String {
+        val hashedInput = deterministicBytesID(serializationService, deterministic)
+        // - is not a Base64 character
+        return requestId.substringBeforeLast('-')+"-"+hashedInput
+    }
+
+    private fun hash(bytes: ByteArray) = toBase64(bytes.sha256Bytes())
+
+    private fun deterministicBytesID(serializationService: SerializationService, parameters: Any): String {
+        return hash(serializationService.serialize(parameters).bytes)
     }
 }
--- components/persistence/entity-processor-service-impl/src/main/kotlin/net/corda/entityprocessor/impl/internal/ProcessorService.kt
+++ components/persistence/entity-processor-service-impl/src/main/kotlin/net/corda/entityprocessor/impl/internal/ProcessorService.kt
@@ -103,7 +103,11 @@ class ProcessorService {
 
         return when (val entityRequest = request.request) {
             is PersistEntities -> {
-                val requestId = request.flowExternalEventContext.requestId
+                val existingRequestId = request.flowExternalEventContext.requestId
+                val entities = entityRequest.entities.map { entityManager.persist(serializationService.deserialize(it.array(), Any::class.java)) }
+                // Should warn or something if PK is null
+                val entityPKs = entities.map { entityManagerFactory.persistenceUnitUtil.getIdentifier(it) ?: it }
+                val requestId = RequestID.replaceHash(existingRequestId,entityPKs, serializationService)
                 val entityResponse = withDeduplicationCheck(requestId, entityManager, onDuplication = {
                     EntityResponse(emptyList(), KeyValuePairList(emptyList()), null)
                 }, requestsIdsRepository) {
