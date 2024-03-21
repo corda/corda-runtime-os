6cef2dd882010d66850511192607311e138ffeca
malachy <malachy.byrne@r3.com>
2024-03-21 12:11:54 +0000

    Fix detekt error

--- libs/db/db-admin/src/main/kotlin/net/corda/db/admin/LiquibaseManager.kt
+++ libs/db/db-admin/src/main/kotlin/net/corda/db/admin/LiquibaseManager.kt
@@ -11,7 +11,9 @@ interface LiquibaseManager {
      * Update Liquibase database with optional output file and tag
      *
      * @param lb The liquibase object to run the update on.
-     * @param sql An optional writer to output the sql into. Note: that if non-null value is passed the Liquibase transform is not actually performed, but intended DB changes are sent to the writer.
+     * @param sql An optional writer to output the sql into.
+     * Note: that if non-null value is passed the Liquibase transform is not actually performed,
+     * but intended DB changes are sent to the writer.
      * @param tag An optional tag to apply to the change.
      */
     fun update(lb: Liquibase, sql: Writer? = null, tag: String? = null)
