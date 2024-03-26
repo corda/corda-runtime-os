9ea4ed636e99c18a8609a6d43e04db811bf58e41
Yiftach Kaplan <yiftach.kaplan@r3.com>
2024-03-26 16:48:57 +0000

    more

--- components/membership/membership-persistence-service-impl/src/main/kotlin/net/corda/membership/impl/persistence/service/handler/PersistRegistrationRequestHandler.kt
+++ components/membership/membership-persistence-service-impl/src/main/kotlin/net/corda/membership/impl/persistence/service/handler/PersistRegistrationRequestHandler.kt
@@ -14,68 +14,53 @@ internal class PersistRegistrationRequestHandler(
     persistenceHandlerServices: PersistenceHandlerServices
 ) : BasePersistenceHandler<PersistRegistrationRequest, Unit>(persistenceHandlerServices) {
     override val operation = PersistRegistrationRequest::class.java
-    private val id = Exception(UUID.randomUUID().toString())
+    //private val id = Exception(UUID.randomUUID().toString())
     override fun invoke(context: MembershipRequestContext, request: PersistRegistrationRequest) {
         val registrationId = request.registrationRequest.registrationId
-        val run = Exception("${Thread.currentThread().id} - ${UUID.randomUUID()} for $registrationId", id)
+        //val run = Exception("${Thread.currentThread().id} - ${UUID.randomUUID()} for $registrationId", id)
         logger.info("Persisting registration request with ID [$registrationId] to status ${request.status}.")
-        logger.info("QQQ Persisting registration request with ID [$registrationId] to status ${request.status}.", run)
-        transaction(context.holdingIdentity.toCorda().shortHash) { em ->
-            val currentRegistrationRequest = em.find(
-                RegistrationRequestEntity::class.java,
-                registrationId,
-                LockModeType.PESSIMISTIC_WRITE,
-            )
-            val status = currentRegistrationRequest?.status?.toStatus()
-            if (status == null) {
-                val e = Exception("Gooing to merge ${Thread.currentThread().id}", run)
-                logger.info("QQQ for [$registrationId] going 2.", e)
-                em.persist(createEntityBasedOnRequest(request))
-            } else {
-                if (status == request.status) {
-                    logger.info(
-                        "Registration request [$registrationId] with status: ${currentRegistrationRequest.status}" +
-                                " is already persisted. Persistence request was discarded."
-                    )
-                    return@transaction
-                }
-                if (!status.canMoveToStatus(request.status)) {
-                    logger.info(
-                        "Registration request [$registrationId] has status: ${currentRegistrationRequest.status}" +
-                                " can not move it to status ${request.status}"
-                    )
-                    // In case of processing persistence requests in an unordered manner we need to make sure the serial
-                    // gets persisted. All other existing data of the request will remain the same.
-                    if (request.status == RegistrationStatus.SENT_TO_MGM && currentRegistrationRequest.serial == null) {
-                        logger.info("Updating request [$registrationId] serial to ${currentRegistrationRequest.serial}")
-                        logger.info("QQQ for [$registrationId] going 1.")
-                        em.merge(createEntityBasedOnPreviousEntity(currentRegistrationRequest, request.registrationRequest.serial))
-                        return@transaction
+        logger.info("QQQ Persisting registration request with ID [$registrationId] to status ${request.status}.")
+        try {
+            transaction(context.holdingIdentity.toCorda().shortHash) { em ->
+                val currentRegistrationRequest = em.find(
+                    RegistrationRequestEntity::class.java,
+                    registrationId,
+                    LockModeType.PESSIMISTIC_WRITE,
+                )
+                val status = currentRegistrationRequest?.status?.toStatus()
+                if (status == null) {
+                    //val e = Exception("Gooing to merge ${Thread.currentThread().id}", run)
+                    logger.info("QQQ for [$registrationId] going 2.")
+                    em.persist(createEntityBasedOnRequest(request))
+                } else {
+                    if (status == request.status) {
+                        logger.info(
+                            "Registration request [$registrationId] with status: ${currentRegistrationRequest.status}" +
+                                    " is already persisted. Persistence request was discarded."
+                        )
+                    }
+                    if (!status.canMoveToStatus(request.status)) {
+                        logger.info(
+                            "Registration request [$registrationId] has status: ${currentRegistrationRequest.status}" +
+                                    " can not move it to status ${request.status}"
+                        )
+                        // In case of processing persistence requests in an unordered manner we need to make sure the serial
+                        // gets persisted. All other existing data of the request will remain the same.
+                        if (request.status == RegistrationStatus.SENT_TO_MGM && currentRegistrationRequest.serial == null) {
+                            logger.info("Updating request [$registrationId] serial to ${request.registrationRequest.serial}")
+                            logger.info("QQQ for [$registrationId] going 1.")
+                            //em.merge(createEntityBasedOnPreviousEntity(currentRegistrationRequest, request.registrationRequest.serial))
+                            val now = clock.instant()
+                            currentRegistrationRequest.serial = request.registrationRequest.serial
+                            currentRegistrationRequest.lastModified = now
+                        }
                     }
                 }
-            }
-        }
-    }
 
-    private fun createEntityBasedOnPreviousEntity(previousEntity: RegistrationRequestEntity, newSerial: Long): RegistrationRequestEntity {
-        val now = clock.instant()
-        with(previousEntity) {
-            return RegistrationRequestEntity(
-                registrationId = registrationId,
-                holdingIdentityShortHash = holdingIdentityShortHash,
-                status = status,
-                created = created,
-                lastModified = now,
-                memberContext = memberContext,
-                memberContextSignatureKey = memberContextSignatureKey,
-                memberContextSignatureContent = memberContextSignatureContent,
-                memberContextSignatureSpec = memberContextSignatureSpec,
-                registrationContext = registrationContext,
-                registrationContextSignatureKey = registrationContextSignatureKey,
-                registrationContextSignatureContent = registrationContextSignatureContent,
-                registrationContextSignatureSpec = registrationContextSignatureSpec,
-                serial = newSerial,
-            )
+            }
+        } catch (e: Exception) {
+            logger.info("QQQ Got exception for registrationId: $e", e)
+            throw e
         }
     }
 
