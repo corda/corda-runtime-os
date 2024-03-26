156f38ea3252211384b93f384e58a9644aac9549
Yiftach Kaplan <yiftach.kaplan@r3.com>
2024-03-26 15:34:37 +0000

    more

--- components/membership/membership-persistence-service-impl/src/main/kotlin/net/corda/membership/impl/persistence/service/handler/PersistRegistrationRequestHandler.kt
+++ components/membership/membership-persistence-service-impl/src/main/kotlin/net/corda/membership/impl/persistence/service/handler/PersistRegistrationRequestHandler.kt
@@ -17,7 +17,7 @@ internal class PersistRegistrationRequestHandler(
     private val id = Exception(UUID.randomUUID().toString())
     override fun invoke(context: MembershipRequestContext, request: PersistRegistrationRequest) {
         val registrationId = request.registrationRequest.registrationId
-        val run = Exception("${Thread.currentThread().id} - ${UUID.randomUUID()}", id)
+        val run = Exception("${Thread.currentThread().id} - ${UUID.randomUUID()} for $registrationId", id)
         logger.info("Persisting registration request with ID [$registrationId] to status ${request.status}.")
         logger.info("QQQ Persisting registration request with ID [$registrationId] to status ${request.status}.", run)
         transaction(context.holdingIdentity.toCorda().shortHash) { em ->
