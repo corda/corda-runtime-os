94624b4b2e35c495e872cdc39af7821b6acf2dcf
Filipe Oliveira <filipe.oliveira@r3.com>
2024-03-27 12:05:28 +0000

    Removed unnecessary import.

--- components/ledger/ledger-utxo-token-cache/src/main/kotlin/net/corda/ledger/utxo/token/cache/queries/SqlQueryProvider.kt
+++ components/ledger/ledger-utxo-token-cache/src/main/kotlin/net/corda/ledger/utxo/token/cache/queries/SqlQueryProvider.kt
@@ -1,6 +1,5 @@
 package net.corda.ledger.utxo.token.cache.queries
 
-import net.corda.v5.ledger.utxo.token.selection.Strategy
 
 interface SqlQueryProvider {
     fun getBalanceQuery(includeTagFilter: Boolean, includeOwnerFilter: Boolean): String
