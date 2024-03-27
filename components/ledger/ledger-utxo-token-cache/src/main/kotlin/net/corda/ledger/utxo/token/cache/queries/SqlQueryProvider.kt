10b0aa85e43167b594b7fdbba37c20d2dd96e039
Filipe Oliveira <filipe.oliveira@r3.com>
2024-03-27 12:15:40 +0000

    Undo cahnge

--- components/ledger/ledger-utxo-token-cache/src/main/kotlin/net/corda/ledger/utxo/token/cache/queries/SqlQueryProvider.kt
+++ components/ledger/ledger-utxo-token-cache/src/main/kotlin/net/corda/ledger/utxo/token/cache/queries/SqlQueryProvider.kt
@@ -1,6 +1,5 @@
 package net.corda.ledger.utxo.token.cache.queries
 
-
 interface SqlQueryProvider {
     fun getBalanceQuery(includeTagFilter: Boolean, includeOwnerFilter: Boolean): String
     fun getPagedSelectQuery(limit: Int, includeTagFilter: Boolean, includeOwnerFilter: Boolean): String
