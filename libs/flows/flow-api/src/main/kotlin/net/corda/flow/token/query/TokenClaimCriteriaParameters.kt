e724fa9d07fbf6b69bb478a982682da2220856fd
LWogan <lorcan.wogan@r3.com>
2024-03-28 16:08:53 +0000

    CORE-20225 try out dependency fix

--- libs/flows/flow-api/src/main/kotlin/net/corda/flow/token/query/TokenClaimCriteriaParameters.kt
+++ libs/flows/flow-api/src/main/kotlin/net/corda/flow/token/query/TokenClaimCriteriaParameters.kt
@@ -1,7 +1,20 @@
 package net.corda.flow.token.query
 
 import net.corda.v5.base.annotations.CordaSerializable
-import net.corda.v5.ledger.utxo.token.selection.TokenClaimCriteria
+import net.corda.v5.base.types.MemberX500Name
+import net.corda.v5.crypto.SecureHash
+import java.math.BigDecimal
 
 @CordaSerializable
-data class TokenClaimCriteriaParameters(val deduplicationId: String, val tokenClaimCriteria: TokenClaimCriteria)
+data class TokenClaimCriteriaParameters(val deduplicationId: String, val tokenClaimCriteria: TokenClaimCriteriaRequest)
+
+
+data class TokenClaimCriteriaRequest(
+    val tokenType: String,
+    val issuerHash: SecureHash,
+    val notaryX500Name: MemberX500Name,
+    val symbol: String,
+    val targetAmount: BigDecimal,
+    val tagRegex: String?,
+    val ownerHash: SecureHash?,
+)
