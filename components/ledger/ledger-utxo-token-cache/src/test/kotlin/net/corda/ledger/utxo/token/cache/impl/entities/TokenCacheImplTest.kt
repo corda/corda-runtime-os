d0069dd1df4876e8947a88919092c35ddacb43f5
Filipe Oliveira <filipe.oliveira@r3.com>
2024-03-28 14:23:07 +0000

    CORE-19290 - Replaced the UTCClock by AutoTickTestClock in the tests and removed the sleep calls (#5950)

--- components/ledger/ledger-utxo-token-cache/src/test/kotlin/net/corda/ledger/utxo/token/cache/impl/entities/TokenCacheImplTest.kt
+++ components/ledger/ledger-utxo-token-cache/src/test/kotlin/net/corda/ledger/utxo/token/cache/impl/entities/TokenCacheImplTest.kt
@@ -3,7 +3,9 @@ package net.corda.ledger.utxo.token.cache.impl.entities
 import net.corda.ledger.utxo.token.cache.entities.CachedToken
 import net.corda.ledger.utxo.token.cache.entities.TokenCache
 import net.corda.ledger.utxo.token.cache.entities.internal.TokenCacheImpl
-import net.corda.utilities.time.UTCClock
+import net.corda.test.util.time.AutoTickTestClock
+import net.corda.utilities.millis
+import net.corda.utilities.seconds
 import net.corda.v5.ledger.utxo.token.selection.Strategy
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
@@ -12,8 +14,7 @@ import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.EnumSource
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.whenever
-import java.lang.Thread.sleep
-import java.time.Duration
+import java.time.Instant
 
 class TokenCacheImplTest {
 
@@ -24,7 +25,7 @@ class TokenCacheImplTest {
 
     @BeforeEach
     fun setup() {
-        target = TokenCacheImpl(Duration.ofSeconds(10), UTCClock())
+        target = TokenCacheImpl(10.seconds, AutoTickTestClock(Instant.EPOCH, 1.seconds))
     }
 
     @ParameterizedTest
@@ -85,42 +86,32 @@ class TokenCacheImplTest {
 
     @Test
     fun `ensure the expiry period is respected`() {
-        val expiryPeriodInMillis = 1000L
         val strategy = Strategy.PRIORITY
 
-        val target = TokenCacheImpl(Duration.ofMillis(expiryPeriodInMillis), UTCClock())
+        val target = TokenCacheImpl(2.seconds, AutoTickTestClock(Instant.EPOCH, 1500.millis))
         target.add(listOf(cachedToken1, cachedToken2, cachedToken3), strategy)
         assertThat(target.get(strategy).toList()).containsOnly(cachedToken1, cachedToken2, cachedToken3)
-
-        sleep(expiryPeriodInMillis)
         assertThat(target.get(strategy).toList()).isEmpty()
     }
 
     @Test
     fun `ensure the expiry period is not applied if the random strategy is used`() {
-        val expiryPeriodInMillis = 5L
         val strategy = Strategy.RANDOM
 
-        val target = TokenCacheImpl(Duration.ofMillis(expiryPeriodInMillis), UTCClock())
+        val target = TokenCacheImpl(1.seconds, AutoTickTestClock(Instant.EPOCH, 2.seconds))
         target.add(listOf(cachedToken1, cachedToken2, cachedToken3), strategy)
-        assertThat(target.get(strategy).toList()).containsOnly(cachedToken1, cachedToken2, cachedToken3)
-        sleep(expiryPeriodInMillis)
         assertThat(target.get(strategy).toList()).containsOnly(cachedToken1, cachedToken2, cachedToken3)
     }
 
     @Test
     fun `ensure the expiry time gets reset when the cache is updated`() {
-        val expiryPeriodInMillis = 1000L
         val strategy = Strategy.PRIORITY
 
-        val target = TokenCacheImpl(Duration.ofMillis(expiryPeriodInMillis), UTCClock())
+        val target = TokenCacheImpl(1.seconds, AutoTickTestClock(Instant.EPOCH, 800.millis))
         target.add(listOf(cachedToken1, cachedToken2, cachedToken3), strategy)
         assertThat(target.get(strategy).toList()).containsOnly(cachedToken1, cachedToken2, cachedToken3)
-        sleep(expiryPeriodInMillis / 2)
         target.add(listOf(cachedToken1, cachedToken2, cachedToken3), strategy)
-        sleep(expiryPeriodInMillis / 2)
         assertThat(target.get(strategy).toList()).containsOnly(cachedToken1, cachedToken2, cachedToken3)
-        sleep(expiryPeriodInMillis)
         assertThat(target.get(strategy).toList()).isEmpty()
     }
 }
