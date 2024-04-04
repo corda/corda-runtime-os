f5747e56d78415734684e5f1ebed30ddaab1ae43
LWogan <lorcan.wogan@r3.com>
2024-04-04 09:59:21 +0100

    Revert "CORE-20185 Token Claim idempotency API changes (#5921)"
    This reverts commit 58ae743e7a2d992f9d152b029f6a5fc42ce6c0df.

--- libs/flows/flow-api/src/main/kotlin/net/corda/flow/token/query/TokenClaimCriteriaParameters.kt
+++
@@ -1,7 +0,0 @@
-package net.corda.flow.token.query
-
-import net.corda.v5.base.annotations.CordaSerializable
-import net.corda.v5.ledger.utxo.token.selection.TokenClaimCriteria
-
-@CordaSerializable
-data class TokenClaimCriteriaParameters(val deduplicationId: String, val tokenClaimCriteria: TokenClaimCriteria)
