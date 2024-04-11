afbd5ccdb2172e561e41e36f58ac27e242110173
malachy <56348274+malachyb@users.noreply.github.com>
2024-04-11 10:23:27 +0100

    CORE-20139: Refactorings and LB 4.27.0 upgrade (#5992)

---
+++ libs/db/db-admin-impl/src/main/kotlin/net/corda/db/admin/impl/LiquibaseSchemaUpdaterImpl.kt
@@ -0,0 +1,76 @@
+package net.corda.db.admin.impl
+
+import liquibase.Contexts
+import liquibase.GlobalConfiguration
+import liquibase.LabelExpression
+import liquibase.Liquibase
+import liquibase.Scope
+import liquibase.UpdateSummaryEnum
+import liquibase.UpdateSummaryOutputEnum
+import liquibase.command.CommandScope
+import liquibase.command.core.TagCommandStep
+import liquibase.command.core.UpdateCommandStep
+import liquibase.command.core.UpdateSqlCommandStep
+import liquibase.command.core.helpers.ChangeExecListenerCommandStep
+import liquibase.command.core.helpers.DatabaseChangelogCommandStep
+import liquibase.command.core.helpers.DbUrlConnectionArgumentsCommandStep
+import liquibase.command.core.helpers.ShowSummaryArgument
+import liquibase.io.WriterOutputStream
+import net.corda.db.admin.LiquibaseSchemaUpdater
+import java.io.Writer
+
+class LiquibaseSchemaUpdaterImpl(
+    private val commandScopeFactory: (commandNames: Array<String>) -> CommandScope = { commandNames ->
+        @Suppress("SpreadOperator")
+        CommandScope(*commandNames)
+    }
+): LiquibaseSchemaUpdater {
+    override fun update(
+        lb: Liquibase,
+        sql: Writer?,
+        tag: String?
+    ) {
+        val scopeObjects = mapOf(
+            Scope.Attr.resourceAccessor.name to lb.resourceAccessor
+        )
+        Scope.child(scopeObjects) {
+            if (sql != null) {
+                commandScopeFactory(UpdateSqlCommandStep.COMMAND_NAME).configure(lb, tag).also {
+                    it.setOutput(
+                        WriterOutputStream(
+                            sql,
+                            GlobalConfiguration.OUTPUT_FILE_ENCODING.currentValue
+                        )
+                    )
+                    it.execute()
+                }
+            } else {
+                commandScopeFactory(UpdateCommandStep.COMMAND_NAME).configure(lb, tag).also {
+                    it.execute()
+                }
+            }
+        }
+    }
+}
+
+private fun CommandScope.configure(lb: Liquibase, tag: String?): CommandScope {
+    val command = this.addArgumentValue(DbUrlConnectionArgumentsCommandStep.DATABASE_ARG, lb.database)
+        .addArgumentValue(UpdateCommandStep.CHANGELOG_ARG, lb.databaseChangeLog)
+        .addArgumentValue(UpdateCommandStep.CHANGELOG_FILE_ARG, lb.changeLogFile)
+        .addArgumentValue(UpdateCommandStep.CONTEXTS_ARG, Contexts().toString())
+        .addArgumentValue(UpdateCommandStep.LABEL_FILTER_ARG, LabelExpression().originalString)
+        .addArgumentValue(
+            ChangeExecListenerCommandStep.CHANGE_EXEC_LISTENER_ARG,
+            lb.defaultChangeExecListener
+        )
+        .addArgumentValue(ShowSummaryArgument.SHOW_SUMMARY_OUTPUT, UpdateSummaryOutputEnum.LOG)
+        .addArgumentValue(
+            DatabaseChangelogCommandStep.CHANGELOG_PARAMETERS,
+            lb.changeLogParameters
+        )
+        .addArgumentValue(ShowSummaryArgument.SHOW_SUMMARY, UpdateSummaryEnum.SUMMARY)
+    tag?.let {
+        command.addArgumentValue(TagCommandStep.TAG_ARG, tag)
+    }
+    return command
+}
---
+++ libs/db/db-admin/src/main/kotlin/net/corda/db/admin/LiquibaseSchemaUpdater.kt
@@ -0,0 +1,20 @@
+package net.corda.db.admin
+
+import liquibase.Liquibase
+import java.io.Writer
+
+/**
+ * Class to handle updating of liquibase schemas
+ */
+interface LiquibaseSchemaUpdater {
+    /**
+     * Update Liquibase database with optional output file and tag
+     *
+     * @param lb The liquibase object to run the update on.
+     * @param sql An optional writer to output the sql into.
+     * Note: that if non-null value is passed the Liquibase transform is not actually performed,
+     * but intended DB changes are sent to the writer.
+     * @param tag An optional tag to apply to the change.
+     */
+    fun update(lb: Liquibase, sql: Writer? = null, tag: String? = null)
+}
