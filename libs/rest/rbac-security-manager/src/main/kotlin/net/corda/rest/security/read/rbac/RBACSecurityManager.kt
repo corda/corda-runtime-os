e27fa0e7f2596e7b424ffb0fc0d031915af0379e
Ben Yip <ben.yip@r3.com>
2024-04-16 11:15:49 +0200

    CORE-20348: Propagate password expiry to RestServerInternal (#6016)
    Add checks for password expiry in the RbacBasicAuthenticationService.
    Add states for password expiry, EXPIRED, ACTIVE and CLOSE_TO_EXPIRY
    Add tests to check expiry works

--- libs/rest/rbac-security-manager/src/main/kotlin/net/corda/rest/security/read/rbac/RBACSecurityManager.kt
+++ libs/rest/rbac-security-manager/src/main/kotlin/net/corda/rest/security/read/rbac/RBACSecurityManager.kt
@@ -17,7 +17,7 @@ class RBACSecurityManager(
     override val id = AuthServiceId(RBACSecurityManager::class.java.name)
 
     override fun authenticate(principal: String, password: Password): AuthorizingSubject {
-        if (!basicAuthenticationService.authenticateUser(principal.lowercase(), password.value)) {
+        if (!basicAuthenticationService.authenticateUser(principal.lowercase(), password.value).authenticationSuccess) {
             throw FailedLoginException("User not authenticated.")
         }
 
