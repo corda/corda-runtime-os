481d3dc3bfc6d663a3fddb2bfeda4f6e7d201d0d
Filipe Oliveira <filipe.oliveira@r3.com>
2024-04-18 11:07:40 +0100

    CORE-19288 - Updated a Token Selection test to ensure the tokens are claimed in the correct order when PRIORITY selection is used (#6028)

--- applications/workers/workers-smoketest/src/smokeTest/kotlin/net/corda/applications/workers/smoketest/token/selection/TokenSelectionTests.kt
+++ applications/workers/workers-smoketest/src/smokeTest/kotlin/net/corda/applications/workers/smoketest/token/selection/TokenSelectionTests.kt
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.module.kotlin.KotlinModule
 import net.corda.e2etest.utilities.ClusterReadiness
 import net.corda.e2etest.utilities.ClusterReadinessChecker
 import net.corda.e2etest.utilities.DEFAULT_CLUSTER
+import net.corda.e2etest.utilities.FlowStatus
 import net.corda.e2etest.utilities.REST_FLOW_STATUS_SUCCESS
 import net.corda.e2etest.utilities.TEST_NOTARY_CPB_LOCATION
 import net.corda.e2etest.utilities.TEST_NOTARY_CPI_NAME
@@ -121,7 +122,7 @@ class TokenSelectionTests : ClusterReadiness by ClusterReadinessChecker() {
     }
 
     @Test
-    fun `ensure it is possible to send a balance query request and receive a response`(testInfo: TestInfo) {
+    fun `Ensure it is possible to send a balance query request and receive a response`(testInfo: TestInfo) {
         val idGenerator = TestRequestIdGenerator(testInfo)
 
         // Start the flow that will send the request and receive the response
@@ -176,50 +177,46 @@ class TokenSelectionTests : ClusterReadiness by ClusterReadinessChecker() {
     @Test
     fun `Test priority selection strategy`(testInfo: TestInfo) {
         val idGenerator = TestRequestIdGenerator(testInfo)
-        // Create 3 simple UTXO transactions
-        val input = "token test input"
+
+        val prioritiesList: List<Long?> = listOf(1, 2, 2, 2, 3, 4, 5, 5, 7, 8, 9, 9, 10, 10, 10, null, null, null)
 
         // A large number of tokens must be created to minimise the change to the random selection
-        // match the result for a priority selection. Bear in mind that repeating the test several times in the same
-        // run won't work because the claimed tokens that are released won't be readily available
-        issueTokenWithPriority(input, idGenerator, 2)
-        issueTokenWithPriority(input, idGenerator, 1)
-        issueTokenWithPriority(input, idGenerator, 2)
-        issueTokenWithPriority(input, idGenerator, null)
-        issueTokenWithPriority(input, idGenerator, null)
-        issueTokenWithPriority(input, idGenerator, 3)
-        issueTokenWithPriority(input, idGenerator, 5)
-        issueTokenWithPriority(input, idGenerator, 4)
-        issueTokenWithPriority(input, idGenerator, 9)
-        issueTokenWithPriority(input, idGenerator, 9)
-        issueTokenWithPriority(input, idGenerator, 10)
-        issueTokenWithPriority(input, idGenerator, 10)
-        issueTokenWithPriority(input, idGenerator, null)
-        issueTokenWithPriority(input, idGenerator, 10)
-        issueTokenWithPriority(input, idGenerator, 2)
-        issueTokenWithPriority(input, idGenerator, 5)
-        issueTokenWithPriority(input, idGenerator, 7)
-        issueTokenWithPriority(input, idGenerator, 8)
+        // match the result for a priority selection.
+        prioritiesList.shuffled().forEach {
+            issueTokenWithPriority(idGenerator, it)
+        }
 
-        // Attempt to select the highest priority token created by the transaction
-        val tokenSelectionFlowId = startRestFlow(
-            aliceHoldingId,
-            mapOf(),
-            "com.r3.corda.demo.utxo.token.selection.PriorityTokenSelectionFlow",
-            requestId = idGenerator.nextId
-        )
-        val tokenSelectionResult = awaitRestFlowFinished(aliceHoldingId, tokenSelectionFlowId)
-        assertAll(
-            { assertThat(tokenSelectionResult.flowError).isNull() },
-            { assertThat(tokenSelectionResult.flowStatus).isEqualTo(REST_FLOW_STATUS_SUCCESS) },
-            { assertThat(tokenSelectionResult.flowResult).isEqualTo("[1]") },
-        )
+        // Claim some tokens
+        // Ensure the tokens are claimed in the correct order.
+        // The priority is from the smallest values to the highest ones. Any null value should be placed at the end
+        runPriorityTokenSelectionFlow(5, idGenerator.nextId).let { tokenSelectionResult ->
+            assertAll(
+                { assertThat(tokenSelectionResult.flowError).isNull() },
+                { assertThat(tokenSelectionResult.flowStatus).isEqualTo(REST_FLOW_STATUS_SUCCESS) },
+                { assertThat(tokenSelectionResult.flowResult).isEqualTo(prioritiesList.take(5).toString()) },
+            )
+        }
+
+        // Claim the remaining tokens
+        // Ensure the tokens are claimed in the correct order.
+        // The priority is from the smallest values to the highest ones. Any null value should be placed at the end
+        runPriorityTokenSelectionFlow(13, idGenerator.nextId).let { tokenSelectionResult ->
+            assertAll(
+                { assertThat(tokenSelectionResult.flowError).isNull() },
+                { assertThat(tokenSelectionResult.flowStatus).isEqualTo(REST_FLOW_STATUS_SUCCESS) },
+                {
+                    assertThat(tokenSelectionResult.flowResult).isEqualTo(
+                        prioritiesList.subList(5, prioritiesList.size).toString()
+                    )
+                },
+            )
+        }
     }
 
-    private fun issueTokenWithPriority(input: String, idGenerator: TestRequestIdGenerator, priority: Long?) {
+    private fun issueTokenWithPriority(idGenerator: TestRequestIdGenerator, priority: Long?) {
         val utxoFlowRequestId = startRestFlow(
             bobHoldingId,
-            mapOf("input" to input, "members" to listOf(aliceX500), "notary" to NOTARY_SERVICE_X500, "priority" to priority),
+            mapOf("input" to "token test input", "members" to listOf(aliceX500), "notary" to NOTARY_SERVICE_X500, "priority" to priority),
             "com.r3.corda.demo.utxo.UtxoDemoFlow",
             requestId = idGenerator.nextId
         )
@@ -228,6 +225,19 @@ class TokenSelectionTests : ClusterReadiness by ClusterReadinessChecker() {
             { assertThat(utxoFlowResult.flowStatus).isEqualTo(REST_FLOW_STATUS_SUCCESS) },
             { assertThat(utxoFlowResult.flowError).isNull() },
         )
+    }
+
+    private fun runPriorityTokenSelectionFlow(noTokensToClaim: Int, requestId: String): FlowStatus {
+        val tokenSelectionFlowId = startRestFlow(
+            bobHoldingId,
+            mapOf(
+                "noTokensToClaim" to noTokensToClaim,
+                "memberX500" to aliceX500
+            ),
+            "com.r3.corda.demo.utxo.token.selection.PriorityTokenSelectionFlow",
+            requestId = requestId
+        )
+        return awaitRestFlowFinished(bobHoldingId, tokenSelectionFlowId)
     }
 }
 
--- testing/cpbs/ledger-utxo-demo-app/src/main/kotlin/com/r3/corda/demo/utxo/token/selection/PriorityTokenSelectionFlow.kt
+++ testing/cpbs/ledger-utxo-demo-app/src/main/kotlin/com/r3/corda/demo/utxo/token/selection/PriorityTokenSelectionFlow.kt
@@ -4,22 +4,38 @@ import com.r3.corda.demo.utxo.contract.TOKEN_AMOUNT
 import com.r3.corda.demo.utxo.contract.TOKEN_ISSUER_HASH
 import com.r3.corda.demo.utxo.contract.TOKEN_SYMBOL
 import com.r3.corda.demo.utxo.contract.TOKEN_TYPE
+import com.r3.corda.demo.utxo.contract.TestCommand
 import com.r3.corda.demo.utxo.contract.TestUtxoState
 import net.corda.v5.application.crypto.DigestService
 import net.corda.v5.application.flows.ClientRequestBody
 import net.corda.v5.application.flows.ClientStartableFlow
 import net.corda.v5.application.flows.CordaInject
+import net.corda.v5.application.flows.InitiatedBy
+import net.corda.v5.application.flows.InitiatingFlow
+import net.corda.v5.application.flows.ResponderFlow
 import net.corda.v5.application.marshalling.JsonMarshallingService
+import net.corda.v5.application.membership.MemberLookup
+import net.corda.v5.application.messaging.FlowMessaging
+import net.corda.v5.application.messaging.FlowSession
 import net.corda.v5.base.annotations.Suspendable
+import net.corda.v5.base.types.MemberX500Name
 import net.corda.v5.ledger.common.NotaryLookup
 import net.corda.v5.ledger.utxo.UtxoLedgerService
+import net.corda.v5.ledger.utxo.token.selection.ClaimedToken
 import net.corda.v5.ledger.utxo.token.selection.Strategy
 import net.corda.v5.ledger.utxo.token.selection.TokenClaimCriteria
 import net.corda.v5.ledger.utxo.token.selection.TokenSelection
+import net.corda.v5.membership.MemberInfo
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import java.time.Instant
+import java.time.temporal.ChronoUnit
+import java.util.UUID
+
+private const val PRIORITY_TOKEN_SELECTION_PROTOCOL = "token-selection-priority-flow-protocol"
 
 @Suppress("unused")
+@InitiatingFlow(protocol = PRIORITY_TOKEN_SELECTION_PROTOCOL)
 class PriorityTokenSelectionFlow : ClientStartableFlow {
 
     private companion object {
@@ -30,7 +46,13 @@ class PriorityTokenSelectionFlow : ClientStartableFlow {
     lateinit var tokenSelection: TokenSelection
 
     @CordaInject
+    lateinit var flowMessaging: FlowMessaging
+
+    @CordaInject
     lateinit var digestService: DigestService
+
+    @CordaInject
+    lateinit var memberLookup: MemberLookup
 
     @CordaInject
     lateinit var notaryLookup: NotaryLookup
@@ -45,31 +67,121 @@ class PriorityTokenSelectionFlow : ClientStartableFlow {
     override fun call(requestBody: ClientRequestBody): String {
         log.info("PriorityTokenSelectionFlow starting...")
         try {
-            val queryCriteria = TokenClaimCriteria(
-                TOKEN_TYPE,
-                digestService.parseSecureHash(TOKEN_ISSUER_HASH),
-                notaryLookup.notaryServices.single().name,
-                TOKEN_SYMBOL,
-                TOKEN_AMOUNT,
-                Strategy.PRIORITY
-            )
+            val request =
+                requestBody.getRequestBodyAs(jsonMarshallingService, PriorityTokenSelectionMsg::class.java)
 
-            val tokenClaim = requireNotNull(tokenSelection.tryClaim("claim1", queryCriteria))
+            val claimedTokenList = claimTokens(request.noTokensToClaim)
 
-            // Lookup the states that match the returned tokens
-            val stateRefList = tokenClaim.claimedTokens.map { it.stateRef }
-            val priorities = utxoLedgerService
-                .resolve<TestUtxoState>(stateRefList)
-                .map { it.state.contractState.priority }
+            if (claimedTokenList.size != request.noTokensToClaim) {
+                log.warn("Failed to claim enough tokens. Claimed tokens: $${claimedTokenList.size}" +
+                        "No. Tokens to claim: ${request.noTokensToClaim}")
+                return "Failed to claim enough tokens"
+            }
 
-            // Now we just exit and let the postprocessing handler clean up for us
-            // If we run this flow again we expect to get the same results as we never used
-            // the claimed token and the flow completing should have freed the claim.
-            return jsonMarshallingService.format(priorities)
+            val memberInfo = requireNotNull(memberLookup.lookup(MemberX500Name.parse(request.memberX500))) {
+                "Member ${request.memberX500} does not exist in the membership group"
+            }
 
+            // Spend the tokens so they cannot be selected again
+            spendTokens(claimedTokenList, memberInfo)
+
+            // Create a list with the priority of each claimed token
+            val tokenPriorityList = claimedTokenList.map {
+                utxoLedgerService.resolve<TestUtxoState>(it.stateRef).state.contractState.priority
+            }
+
+            return tokenPriorityList.toString()
         } catch (e: Exception) {
             log.error("Unexpected error while processing the flow", e)
             throw e
+        }
+    }
+
+    @Suspendable
+    private fun claimTokens(noTokensToClaim: Int): List<ClaimedToken> {
+
+        val queryCriteria = TokenClaimCriteria(
+            TOKEN_TYPE,
+            digestService.parseSecureHash(TOKEN_ISSUER_HASH),
+            notaryLookup.notaryServices.single().name,
+            TOKEN_SYMBOL,
+            TOKEN_AMOUNT,
+            Strategy.PRIORITY
+        )
+
+        val claimedTokenList = mutableListOf<ClaimedToken>()
+        do {
+            val tokenClaim = tokenSelection.tryClaim("claim-${UUID.randomUUID()}", queryCriteria)
+            if (tokenClaim != null) {
+                // Lookup the states that match the returned tokens
+                claimedTokenList.add(tokenClaim.claimedTokens.single())
+            }
+        } while (tokenClaim != null && claimedTokenList.size < noTokensToClaim)
+
+        return claimedTokenList
+    }
+
+    @Suspendable
+    private fun spendTokens(claimedTokenList: List<ClaimedToken>, memberInfo: MemberInfo) {
+        val tokensToBeSpent = claimedTokenList.map { it.stateRef }
+
+        val me = memberLookup.myInfo()
+
+        val participants = listOf(me.ledgerKeys.first(), memberInfo.ledgerKeys.first())
+
+        log.info("Creating transaction...")
+        val txBuilder = utxoLedgerService.createTransactionBuilder()
+
+        val now = Instant.now()
+
+        val signedTransaction = txBuilder
+            .setNotary(notaryLookup.notaryServices.single().name)
+            .setTimeWindowBetween(now, now.plus(1, ChronoUnit.DAYS))
+            .addInputStates(tokensToBeSpent)
+            .addCommand(TestCommand())
+            .addSignatories(participants)
+            .toSignedTransaction()
+
+        val session = flowMessaging.initiateFlow(memberInfo.name)
+
+        try {
+            val finalizedSignedTransaction = utxoLedgerService.finalize(
+                signedTransaction,
+                listOf(session)
+            )
+            log.info("Created and finalised transaction with id='${finalizedSignedTransaction.transaction.id}'")
+        } catch (e: Exception) {
+            log.warn("Finality failed", e)
+            "Finality failed, ${e.message}"
+        }
+    }
+
+    private data class PriorityTokenSelectionMsg(
+        val noTokensToClaim: Int,
+        val memberX500: String
+    )
+}
+
+@Suppress("unused")
+@InitiatedBy(protocol = PRIORITY_TOKEN_SELECTION_PROTOCOL)
+class SpendTokenResponder : ResponderFlow {
+
+    private companion object {
+        val log: Logger = LoggerFactory.getLogger(this::class.java.enclosingClass)
+    }
+
+    @CordaInject
+    lateinit var utxoLedgerService: UtxoLedgerService
+
+    @Suspendable
+    override fun call(session: FlowSession) {
+        try {
+            val finalizedSignedTransaction = utxoLedgerService.receiveFinality(session) {
+                log.info("Verified the transaction- ${it.id}")
+            }
+            log.info("Finished responder flow - ${finalizedSignedTransaction.transaction.id}")
+        } catch (e: Exception) {
+            log.warn("Exceptionally finished spend token responder flow", e)
         }
     }
 }
