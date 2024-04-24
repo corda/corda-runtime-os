77f1d6f9815046aa32e488f48fb9736bea443efb
Anna Paskova <anna.paskova@r3.com>
2024-04-24 15:38:37 +0100

    Some tests require longer byte array

--- libs/utilities/src/main/kotlin/net/corda/utilities/UuidUtils.kt
+++ libs/utilities/src/main/kotlin/net/corda/utilities/UuidUtils.kt
@@ -8,7 +8,7 @@ import java.util.UUID
  * which dilutes the randomness if only part of the uuid is taken later on.
  */
 fun UUID.toByteArray(): ByteArray {
-    val bb = ByteBuffer.wrap(ByteArray(16))
+    val bb = ByteBuffer.wrap(ByteArray(32))
     bb.putLong(this.mostSignificantBits)
     bb.putLong(this.leastSignificantBits)
     return bb.array()
