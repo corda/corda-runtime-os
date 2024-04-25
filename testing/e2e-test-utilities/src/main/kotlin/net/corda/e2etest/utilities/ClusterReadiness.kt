46bd166f088ac9c6062b32bca2a5267779fc8d9e
malachy <malachy.byrne@r3.com>
2024-04-25 13:10:10 +0100

    CORE-19952: Add more logging in ClusterReadinessChecker to catch future occurrences of error.

--- testing/e2e-test-utilities/src/main/kotlin/net/corda/e2etest/utilities/ClusterReadiness.kt
+++ testing/e2e-test-utilities/src/main/kotlin/net/corda/e2etest/utilities/ClusterReadiness.kt
@@ -79,7 +79,7 @@ class ClusterReadinessChecker: ClusterReadiness {
                 val response = function()
                 val statusCode = response.statusCode()
                 if (statusCode in 200..299) {
-                    logger.info("Status successful. Exiting tryUntil.")
+                    logger.info("Status successful with code $statusCode. Exiting tryUntil.")
                     return true
                 }
                 else {
@@ -99,8 +99,8 @@ class ClusterReadinessChecker: ClusterReadiness {
         val request = HttpRequest.newBuilder()
             .uri(URI.create(url))
             .build()
-        val response = client.send(request, HttpResponse.BodyHandlers.ofString())
-        logger.info("Returning status ${response.statusCode()} $name on $url.")
-        return response
+        return client.send(request, HttpResponse.BodyHandlers.ofString()).also {
+            logger.info("Returning status ${it.statusCode()} $name on $url.")
+        }
     }
 }
