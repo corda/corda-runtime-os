8d6f61e0d4a43ef54b6c2a4bf5ac3512821d33e4
eunjee.yang <eunjee.yang@r3.com>
2024-05-07 15:11:57 +0100

    CORE-20430 add test that runs command in a separate JVM

--- applications/tools/universal-blob-inspector/src/test/kotlin/net/corda/blobinspector/AppTest.kt
+++ applications/tools/universal-blob-inspector/src/test/kotlin/net/corda/blobinspector/AppTest.kt
@@ -1,6 +1,11 @@
 package net.corda.blobinspector
 
 import org.junit.jupiter.api.Test
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.util.concurrent.Executors
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
 class AppTest {
     @Test
@@ -39,8 +44,17 @@ class AppTest {
     }
 
     @Test
-    fun parseWithoutOriginalBytes() {
-        run("C5WireTx.bin", false)
+    fun runWithoutBytes() {
+        val(exitCode, output) = runInSeparateJVM("C5WireTx.bin", listOf("-b"))
+        assertTrue(exitCode == 0)
+        assertFalse(output.contains("_bytes"))
+    }
+
+    @Test
+    fun runWithBytes() {
+        val(exitCode, output) = runInSeparateJVM("C5WireTx.bin")
+        assertTrue(exitCode == 0)
+        assertTrue(output.contains("_bytes"))
     }
 
     private fun run(resourceName: String, includeOriginalBytes: Boolean = true) {
@@ -50,5 +64,38 @@ class AppTest {
         }
         val decoded = Encoding.decodedBytes(bytes, includeOriginalBytes)
         println(decoded.result.prettyPrint())
+    }
+
+    private fun runInSeparateJVM(binaryFileName: String, flags: List<String> = emptyList()): Pair<Int, String> {
+        val currentPath = System.getProperty("user.dir")
+        val pathToBinary = "$currentPath/src/test/resources/net/corda/blobinspector/$binaryFileName"
+
+        // Create a StringBuilder to capture the printed output
+        val output = StringBuilder()
+
+        // Create a ProcessBuilder to execute the JAR file
+        val pathToBlobInspectorJar = "$currentPath/build/bin/corda-universal-blob-inspector-5.3.0.0-SNAPSHOT.jar"
+        val args = listOf("java", "-jar", pathToBlobInspectorJar, pathToBinary) + flags
+        val processBuilder = ProcessBuilder(args)
+
+        val process = processBuilder.start()
+
+        // Create a thread to read the output of the process
+        val executor = Executors.newSingleThreadExecutor()
+        executor.submit {
+            val reader = BufferedReader(InputStreamReader(process.inputStream))
+            var line: String?
+            while (reader.readLine().also { line = it } != null) {
+                output.append(line).append("\n")
+            }
+        }
+
+        // Wait for the process to complete
+        val exitCode = process.waitFor()
+        executor.shutdown()
+
+        println("Captured output: $output")
+
+        return exitCode to output.toString()
     }
 }
