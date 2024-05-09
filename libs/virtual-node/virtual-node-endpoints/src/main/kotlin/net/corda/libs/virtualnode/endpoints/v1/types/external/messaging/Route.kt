d9ea8a60a4e7dcc63d34216d3d300c3ea693f18c
Filipe Oliveira <filipe.oliveira@r3.com>
2024-05-09 16:38:35 +0100

    Update

--- libs/virtual-node/virtual-node-endpoints/src/main/kotlin/net/corda/libs/virtualnode/endpoints/v1/types/external/messaging/Route.kt
+++ libs/virtual-node/virtual-node-endpoints/src/main/kotlin/net/corda/libs/virtualnode/endpoints/v1/types/external/messaging/Route.kt
@@ -1,14 +1,8 @@
 package net.corda.libs.virtualnode.endpoints.v1.types.external.messaging
 
-import com.fasterxml.jackson.annotation.JsonProperty
-
 data class Route(
-    @JsonProperty("channelName")
     val channelName: String,
-    @JsonProperty("externalReceiveTopicName")
     val externalReceiveTopicName: String,
-    @JsonProperty("active")
     val active: Boolean,
-    @JsonProperty("inactiveResponseType")
     val inactiveResponseType: InactiveResponseType
 )
--- libs/virtual-node/virtual-node-endpoints/src/main/kotlin/net/corda/libs/virtualnode/endpoints/v1/types/external/messaging/RouteConfiguration.kt
+++ libs/virtual-node/virtual-node-endpoints/src/main/kotlin/net/corda/libs/virtualnode/endpoints/v1/types/external/messaging/RouteConfiguration.kt
@@ -1,10 +1,6 @@
 package net.corda.libs.virtualnode.endpoints.v1.types.external.messaging
 
-import com.fasterxml.jackson.annotation.JsonProperty
-
 data class RouteConfiguration(
-    @JsonProperty("currentRoutes")
     val currentRoutes: Routes,
-    @JsonProperty("previousVersionRoutes")
     val previousVersionRoutes: List<Routes>
 )
