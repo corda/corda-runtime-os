dae2812349e54a66a0a936f8a41635529af9fb70
Will Vigor <william.vigor@r3.com>
2024-05-10 16:33:19 +0100

    Fix detekt

--- tools/plugins/upgrade/src/main/kotlin/net/corda/cli/plugins/upgrade/MigrateHostedIdentities.kt
+++ tools/plugins/upgrade/src/main/kotlin/net/corda/cli/plugins/upgrade/MigrateHostedIdentities.kt
@@ -26,7 +26,6 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import picocli.CommandLine
 import java.io.FileInputStream
-import java.sql.DriverManager
 import java.time.Duration
 import java.util.Properties
 import java.util.UUID
