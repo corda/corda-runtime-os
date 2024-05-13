70a23b2e656a5f2e23b07e10bc6baddff0051321
Anna Paskova <anna.paskova@r3.com>
2024-05-13 17:39:51 +0100

    Replace crypto cluster level tenant with P2P in rekey processor

--- components/crypto/crypto-service-impl/src/main/kotlin/net/corda/crypto/service/impl/bus/CryptoRekeyBusProcessor.kt
+++ components/crypto/crypto-service-impl/src/main/kotlin/net/corda/crypto/service/impl/bus/CryptoRekeyBusProcessor.kt
@@ -98,9 +98,9 @@ class CryptoRekeyBusProcessor(
 
                 // We do not need to use separate wrapping repositories for the different cluster level tenants,
                 // since they share the cluster crypto database. So we scan over the virtual node tenants and an arbitrary
-                // choice of cluster level tenant. We pick CryptoTenants.CRYPTO as the arbitrary cluster level tenant,
-                // and we should not also check CryptoTenants.P2P and CryptoTenants.REST since if we do we'll get duplicate.
-                val allTenantIds = virtualNodeTenantIds + listOf(CryptoTenants.CRYPTO)
+                // choice of cluster level tenant. We pick CryptoTenants.P2P as the arbitrary cluster level tenant,
+                // and we should not also check other cluster level tenants since if we do we'll get duplicate.
+                val allTenantIds = virtualNodeTenantIds + listOf(CryptoTenants.P2P)
                 logger.debug("Found ${allTenantIds.size} tenants; first few are: ${allTenantIds.take(10)}")
                 val targetWrappingKeys = allTenantIds.map { tenantId ->
                     try {
