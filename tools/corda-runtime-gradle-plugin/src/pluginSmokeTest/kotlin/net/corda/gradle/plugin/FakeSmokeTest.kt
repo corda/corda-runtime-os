8b4f55c6988ae678f301784bf15ac1b685e992fa
Anton Subbotin <anton.subbotin@r3.com>
2024-05-17 16:36:20 +0100

    Add fake smoke test to check in the dir structure

---
+++ tools/corda-runtime-gradle-plugin/src/pluginSmokeTest/kotlin/net/corda/gradle/plugin/FakeSmokeTest.kt
@@ -0,0 +1,10 @@
+package net.corda.gradle.plugin
+
+import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.Test
+
+class FakeSmokeTest {
+    @Test
+    @Disabled("Tests to be added by https://r3-cev.atlassian.net/browse/ES-2344")
+    fun `fake test`() {}
+}
