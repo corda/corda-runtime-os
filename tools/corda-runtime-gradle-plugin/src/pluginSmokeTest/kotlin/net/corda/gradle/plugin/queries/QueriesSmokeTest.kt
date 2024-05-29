55f5a0966c8f520ef5022c741fdfabbc8c11d227
Anton Subbotin <anton.subbotin@r3.com>
2024-05-29 11:33:12 +0100

    Rename test to QueriesSmokeTest in queries package

--- tools/corda-runtime-gradle-plugin/src/pluginSmokeTest/kotlin/net/corda/gradle/plugin/queries/QueriesSmokeTest.kt
+++ tools/corda-runtime-gradle-plugin/src/pluginSmokeTest/kotlin/net/corda/gradle/plugin/queries/QueriesSmokeTest.kt
@@ -1,9 +1,9 @@
-package net.corda.gradle.plugin
+package net.corda.gradle.plugin.queries
 
 import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 
-class FakeSmokeTest {
+class QueriesSmokeTest {
     @Test
     @Disabled("Tests to be added by https://r3-cev.atlassian.net/browse/ES-2344")
     fun `fake test`() {}
