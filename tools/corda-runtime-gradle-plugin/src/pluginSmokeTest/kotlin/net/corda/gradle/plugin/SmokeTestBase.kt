26845787ad28188a40d16dd88a53d6f9be82d2cb
Anton Subbotin <anton.subbotin@r3.com>
2024-05-29 13:34:48 +0100

    Add base test using gradle test kit, queries test, and resources

---
+++ tools/corda-runtime-gradle-plugin/src/pluginSmokeTest/kotlin/net/corda/gradle/plugin/SmokeTestBase.kt
@@ -0,0 +1,130 @@
+package net.corda.gradle.plugin
+
+import net.corda.e2etest.utilities.DEFAULT_CLUSTER
+import org.gradle.testkit.runner.BuildResult
+import org.gradle.testkit.runner.BuildTask
+import org.gradle.testkit.runner.GradleRunner
+import org.gradle.testkit.runner.TaskOutcome
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.io.TempDir
+import java.io.File
+
+// https://docs.gradle.org/current/userguide/test_kit.html
+abstract class SmokeTestBase {
+
+    @field:TempDir
+    lateinit var projectDir: File
+    protected lateinit var buildFile: File
+    private lateinit var networkPath: String
+
+    private val targetUrl = DEFAULT_CLUSTER.rest.uri
+    private val user = DEFAULT_CLUSTER.rest.user
+    private val password = DEFAULT_CLUSTER.rest.password
+
+    @BeforeEach
+    fun setup() {
+        buildFile = projectDir.resolve("build.gradle")
+        buildFile.writeText(
+            """
+            plugins {
+                id 'net.corda.gradle.plugin'
+            }
+
+            """.trimIndent()
+        )
+        val sourceConfigFolder = File("src/pluginSmokeTest/resources/")
+        val targetConfigFolder = File("$projectDir/")
+        sourceConfigFolder.absoluteFile.copyRecursively(targetConfigFolder)
+    }
+
+    fun appendCordaRuntimeGradlePluginExtension(
+        appendArtifactoryCredentials: Boolean = false,
+        isStaticNetwork: Boolean = true
+    ) {
+
+        networkPath = if (isStaticNetwork) {
+            "config/static-network-config.json"
+        } else {
+            "config/dynamic-network-config.json"
+        }
+
+        buildFile.appendText(
+            """
+            cordaRuntimeGradlePlugin {
+                cordaClusterURL = "$targetUrl"
+                cordaRestUser = "$user"
+                cordaRestPasswd ="$password"
+                notaryVersion = "5.2.0.0-beta-1711530608468"
+                runtimeVersion = "5.2.0.0-beta-1711530608468"
+                composeFilePath = "config/combined-worker-compose.yml"
+                networkConfigFile = "$networkPath"
+                r3RootCertFile = "config/r3-ca-key.pem"
+                corDappCpiName = "MyCorDapp"
+                notaryCpiName = "NotaryServer"
+                workflowsModuleName = "workflowsModule"
+                cordaRuntimePluginWorkspaceDir = "workspace"
+                cordaProcessorTimeout = "-1"
+                vnodeRegistrationTimeout = "30000"
+            }
+            repositories {
+                mavenCentral()
+                mavenLocal()
+            }
+            """.trimIndent()
+        )
+        if (appendArtifactoryCredentials)
+            appendArtifactoryCredentialsToTheCordaRuntimeGradlePluginExtension()
+    }
+
+    private fun appendArtifactoryCredentialsToTheCordaRuntimeGradlePluginExtension() {
+        val existingContent = buildFile.readText()
+        val newContent = if (existingContent.contains("cordaRuntimeGradlePlugin")) {
+            existingContent.replace(
+                "cordaRuntimeGradlePlugin {",
+                """
+                     cordaRuntimeGradlePlugin {
+                        artifactoryUsername = findProperty('cordaArtifactoryUsername') ?: System.getenv('CORDA_ARTIFACTORY_USERNAME')
+                        artifactoryPassword = findProperty('cordaArtifactoryPassword') ?: System.getenv('CORDA_ARTIFACTORY_PASSWORD')
+                """.trimIndent()
+            )
+        } else {
+            existingContent + """
+                cordaRuntimeGradlePlugin {
+                    artifactoryUsername = findProperty('cordaArtifactoryUsername') ?: System.getenv('CORDA_ARTIFACTORY_USERNAME')
+                    artifactoryPassword = findProperty('cordaArtifactoryPassword') ?: System.getenv('CORDA_ARTIFACTORY_PASSWORD')
+                }
+            """.trimIndent()
+        }
+        buildFile.writeText(newContent)
+    }
+
+    /**
+     * Allow tests to edit the network config file
+     */
+    fun getNetworkConfigFile() : File {
+        return File("$projectDir/$networkPath")
+    }
+
+    fun executeWithRunner(vararg args: String): BuildResult {
+        return GradleRunner
+            .create()
+            .withPluginClasspath()
+            .withProjectDir(projectDir)
+            .withArguments(*args)
+            .build()
+    }
+
+    fun executeAndFailWithRunner(vararg args: String): BuildResult {
+        return GradleRunner
+            .create()
+            .withPluginClasspath()
+            .withProjectDir(projectDir)
+            .withArguments(*args)
+            .buildAndFail()
+    }
+
+    fun BuildTask.assertTaskSucceeded() {
+        assertTrue(outcome == TaskOutcome.SUCCESS, "The task '$path' is expected to be successful")
+    }
+}
