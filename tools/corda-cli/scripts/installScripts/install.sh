3b8b253b7e2badfcc57be3696a99b967782a929d
Pavel Nenov <pavel.nenov@r3.com>
2024-06-04 13:45:56 +0300

    Removed unnecessary copying into corda cli run script

--- tools/corda-cli/scripts/installScripts/install.sh
+++ tools/corda-cli/scripts/installScripts/install.sh
@@ -13,9 +13,6 @@ mkdir -p $cliHome
 echo "Copying files and plugins"
 cp -R $zipdir/* $cliHome
 
-echo "Creating corda-cli Script"
-echo "java -jar $cliHome/corda-cli.jar \"\$@\"" > corda-cli.sh
-
 chmod 755 $cliHome/corda-cli.sh
 
 
