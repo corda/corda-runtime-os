90c4c8c3232188bfc4973d34f8e2211c5ba84f1e
Anton Subbotin <anton.subbotin@r3.com>
2024-06-04 11:02:49 +0100

    Add tests for PEM, greet and SQL output

--- libs/rest/generated-rest-client/src/test/kotlin/net/corda/restclient/generated/TestKnownIssues.kt
+++ libs/rest/generated-rest-client/src/test/kotlin/net/corda/restclient/generated/TestKnownIssues.kt
@@ -55,7 +55,7 @@ class TestKnownIssues {
     @Test
     fun testGetGroupPolicyFromMgm() {
         val inputStream = File("./src/test/resources/groupPolicy.json").inputStream()
-        app.get("api/v5_3/mgm/1234/info") {ctx ->
+        app.get("api/v5_3/mgm/1234/info") { ctx ->
             ctx.header("Content-Type", "application/json")
             ctx.result(inputStream)
         }
@@ -122,7 +122,7 @@ class TestKnownIssues {
                 "oR+yXgQLjd7Gnt/LRo6eAiEAvGQxrZgM85IgngWBO033RYWVyxICmj/yepwDDNj+\n" +
                 "SDc=\n" +
                 "-----END CERTIFICATE REQUEST-----\n"
-        app.post("api/v5_3/certificate/p2p/123") {ctx ->
+        app.post("api/v5_3/certificate/p2p/123") { ctx ->
             ctx.header("Content-Type", "application/json")
             ctx.result(csrResponse)
         }
@@ -141,6 +141,86 @@ class TestKnownIssues {
     }
 
     /**
+     * See comment for workaround details
+     * https://r3-cev.atlassian.net/browse/ES-2378?focusedCommentId=306841
+     */
+    @Test
+    fun testGetKeyInPemFormatHandlesString() {
+        val keyResponse = "-----BEGIN PUBLIC KEY-----\n" +
+                "MIIBojANBgkqhkiG9w0BAQEFAAOCAY8AMIIBigKCAYEArARp009wi/O/lwKFPe26\n" +
+                "B46cx3iILv/i6eNAzIHQK2QCelXzFyFQhJKgGe4HWtvxvwHRXv6hYtuHe4whfaue\n" +
+                "T1URYKYcr5r095BCW8olEbOnfQnRq5evprQRLHFMEKQ0lWTawi9Y5jcbVL5jbvTG\n" +
+                "8/FjL66AuG/hpUS9woft+e3143uLPtbM6AmV1aGEn1hdpWPH8Js7J4rhpZ56lX1h\n" +
+                "KJBe2TwRsb6cp3RxcWPISCi2x39VLHOmI+9z8wBPWCqjUzX2qn4qBQwxqDPZc/eq\n" +
+                "GCCzSXrSUtH1uJs3498CQKBsvRzSIicbPBvDHF6XvFk6skgR6qWVPTPjD6HmTQx7\n" +
+                "bCk8s/Ai4KSAV3tPJ+x3KyDj6DnMi/aXOpwf2CIpX73I535OQk6giowdHjnsnhNM\n" +
+                "mWlglNL7sMCOjhUurLdppiUafd2vpF2vd0ujFO+v377jYae/redJCRnvdRtWQ5oD\n" +
+                "IDfirpEtpwLrVFejsxYh0uPrbacm30edQ2x3g5TUGozvAgMBAAE=\n" +
+                "-----END PUBLIC KEY-----"
+        app.get("api/v5_3/key/p2p/123") { ctx ->
+            ctx.header("Content-Type", "application/json")
+            ctx.result(keyResponse)
+        }
+
+        val client = CordaRestClient.createHttpClient(baseUrl = localhost)
+        assertThatCodeDoesNotThrowAnyException {
+            val response: String = client.keyManagementClient.getKeyTenantidKeyid("p2p", "123")
+            assertThat(response).contains("-----BEGIN PUBLIC KEY-----")
+        }
+    }
+
+    /**
+     * See comment for workaround details
+     * https://r3-cev.atlassian.net/browse/ES-2378?focusedCommentId=306841
+     */
+    @Test
+    fun testGetSqlResponseHandlesString() {
+        val sqlResponse = "CREATE TABLE IF NOT EXISTS databasechangelog (ID VARCHAR(255) NOT NULL, AUTHOR VARCHAR(255) NOT NULL, FILENAME VARCHAR(255) NOT NULL, DATEEXECUTED TIMESTAMP WITHOUT TIME ZONE NOT NULL, ORDEREXECUTED INTEGER NOT NULL, EXECTYPE VARCHAR(10) NOT NULL, MD5SUM VARCHAR(35), DESCRIPTION VARCHAR(255), COMMENTS VARCHAR(255), TAG VARCHAR(255), LIQUIBASE VARCHAR(20), CONTEXTS VARCHAR(255), LABELS VARCHAR(255), DEPLOYMENT_ID VARCHAR(10));\n" +
+                "\n" +
+                "-- *********************************************************************\n" +
+                "-- Update Database Script\n" +
+                "-- *********************************************************************\n" +
+                "-- Change Log: /tmp/corda/tmp/offline-db/changelog-a0d1eff0-d055-437b-99ad-82601c4362a5.xml\n" +
+                "-- Ran at: 6/3/24, 10:25 AM\n" +
+                "-- Against: null@offline:postgresql?changeLogFile=/tmp/corda/tmp/offline-db/changelog-a0d1eff0-d055-437b-99ad-82601c4362a5.xml&outputLiquibaseSql=all\n" +
+                "-- Liquibase version: 4.24.0\n" +
+                "-- *********************************************************************\n" +
+                "\n" +
+                "-- Changeset net/corda/db/schema/vnode-vault/migration/vnode-vault-creation-v1.0.xml::vnode-vault-creation-v1.0::R3.Corda\n" +
+                "CREATE TABLE vnode_vault (holding_identity_id VARCHAR(12) NOT NULL);\n"
+        app.get("api/v5_3/virtualnode/create/db/vault/123") { ctx ->
+            ctx.header("Content-Type", "application/json")
+            ctx.result(sqlResponse)
+        }
+
+        val client = CordaRestClient.createHttpClient(baseUrl = localhost)
+        assertThatCodeDoesNotThrowAnyException {
+            val response: String = client.virtualNodeClient.getVirtualnodeCreateDbVaultCpichecksum("123")
+            assertThat(response).contains("-- Update Database Script\n")
+        }
+    }
+
+    /**
+     * See comment for workaround details
+     * https://r3-cev.atlassian.net/browse/ES-2378?focusedCommentId=306841
+     */
+    @Test
+    fun testHelloEndpointHandlesString() {
+        val username = "TestUsername"
+        val greeting = "\"Hello, $username! (from admin)\""
+        app.post("api/v5_3/hello") { ctx ->
+            ctx.header("Content-Type", "application/json")
+            ctx.result(greeting)
+        }
+
+        val client = CordaRestClient.createHttpClient(baseUrl = localhost)
+        assertThatCodeDoesNotThrowAnyException {
+            val response: String = client.helloRestClient.postHello(username)
+            assertThat(response).isEqualTo(greeting)
+        }
+    }
+
+    /**
      * The generated client can't deserialize the Instant type natively
      * The suggested fix is to add the dependency on libs.jackson.datatype.jsr310
      * After adding the dependency this now works locally, but still fails on Jenkins
@@ -150,7 +230,7 @@ class TestKnownIssues {
         val response = """
             [{"id":"62badb8b-1836-4a89-901a-fd6b65906a67","version":0,"updateTimestamp":"2024-05-17T08:33:07.602Z","roleName":"Default System Admin","groupVisibility":null,"permissions":[{"id":"4b3477d9-b812-4ae5-bcb1-5537c7a373d5","createdTimestamp":"2024-05-17T08:33:07.692Z"}]}]
         """.trimIndent()
-        app.get("api/v5_3/role") {ctx ->
+        app.get("api/v5_3/role") { ctx ->
             ctx.header("Content-Type", "application/json")
             ctx.result(response)
         }
