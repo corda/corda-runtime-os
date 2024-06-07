a7e784112b482942f79edaa29a1e012746aa674b
malachy <malachy.byrne@r3.com>
2024-06-07 17:12:32 +0100

    Detekt

--- components/membership/certificates-client-impl/src/main/kotlin/net/corda/membership/certificate/client/impl/CertificatesClientImpl.kt
+++ components/membership/certificates-client-impl/src/main/kotlin/net/corda/membership/certificate/client/impl/CertificatesClientImpl.kt
@@ -178,7 +178,7 @@ class CertificatesClientImpl @Activate constructor(
             ).getOrThrow()?.response.also {
                 logger.info(
                     "End send request for certificate chain $payload at ${Instant.now()}. " +
-                            "Duration: ${Instant.now().toEpochMilli() - startTime.toEpochMilli()} ms"
+                        "Duration: ${Instant.now().toEpochMilli() - startTime.toEpochMilli()} ms"
                 )
             } as? R
         }
