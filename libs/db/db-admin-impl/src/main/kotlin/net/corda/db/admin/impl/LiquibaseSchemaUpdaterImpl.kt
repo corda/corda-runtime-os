ab10e2958f141c281ca443e5e3375eeac62d7a22
malachy <malachy.byrne@r3.com>
2024-06-11 15:20:45 +0100

    Use FilenameUtils classloader

--- libs/db/db-admin-impl/src/main/kotlin/net/corda/db/admin/impl/LiquibaseSchemaUpdaterImpl.kt
+++ libs/db/db-admin-impl/src/main/kotlin/net/corda/db/admin/impl/LiquibaseSchemaUpdaterImpl.kt
@@ -7,7 +7,6 @@ import liquibase.Liquibase
 import liquibase.Scope
 import liquibase.UpdateSummaryEnum
 import liquibase.UpdateSummaryOutputEnum
-import liquibase.changelog.DatabaseChangeLog
 import liquibase.command.CommandScope
 import liquibase.command.core.TagCommandStep
 import liquibase.command.core.UpdateCommandStep
@@ -18,6 +17,7 @@ import liquibase.command.core.helpers.DbUrlConnectionArgumentsCommandStep
 import liquibase.command.core.helpers.ShowSummaryArgument
 import liquibase.io.WriterOutputStream
 import net.corda.db.admin.LiquibaseSchemaUpdater
+import org.apache.commons.io.FilenameUtils
 import java.io.Writer
 
 class LiquibaseSchemaUpdaterImpl(
@@ -33,7 +33,7 @@ class LiquibaseSchemaUpdaterImpl(
     ) {
         val scopeObjects = mapOf(
             Scope.Attr.resourceAccessor.name to lb.resourceAccessor,
-            Scope.Attr.classLoader.name to DatabaseChangeLog::class.java.classLoader
+            Scope.Attr.classLoader.name to FilenameUtils::class.java.classLoader
         )
         Scope.child(scopeObjects) {
             val classLoaderScopeObjects = mapOf(Scope.Attr.classLoader.name to Liquibase::class.java.classLoader)
