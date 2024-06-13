77ed27c922e53398b22a784de8029e3b6546053d
Ben Yip <ben.yip@r3.com>
2024-06-13 09:10:01 +0100

    CORE-20702: Add REST API version check to E2E utilities (#6204)
    * check for version when creating vNodeCreatorClient

--- testing/e2e-test-utilities/src/main/kotlin/net/corda/e2etest/utilities/ClusterBuilder.kt
+++ testing/e2e-test-utilities/src/main/kotlin/net/corda/e2etest/utilities/ClusterBuilder.kt
@@ -44,10 +44,14 @@ class ClusterBuilder(clusterInfo: ClusterInfo, val REST_API_VERSION_PATH: String
         return getRbacUser(vNodeCreatorName).body.contains("User '$vNodeCreatorName' not found")
     }
 
+    private fun checkIfNotPreviousVersion(): Boolean {
+        return REST_API_VERSION_PATH != RestApiVersion.C5_1.versionPath && REST_API_VERSION_PATH != RestApiVersion.C5_2.versionPath
+    }
+
     internal val vNodeCreatorClient: HttpsClient by lazy {
         lock.withLock {
             val vNodeCreatorRole = checkVNodeCreatorRoleExists()
-            if (vNodeCreatorRole != null && checkVNodeCreatorUserDoesNotExist()) {
+            if (checkIfNotPreviousVersion() && vNodeCreatorRole != null && checkVNodeCreatorUserDoesNotExist()) {
                 logger.info(
                     "Creating user '$vNodeCreatorName' with role '${vNodeCreatorRole["roleName"].textValue()}'"
                 )
