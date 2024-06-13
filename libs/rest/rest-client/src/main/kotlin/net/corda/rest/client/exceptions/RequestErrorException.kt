a2a6d65112dddb1f7a9784b817534d0e19122dc9
Tony Lawson <tony.lawson@r3.com>
2024-06-13 15:22:12 +0100

    ES-2299: Remove the old rest-client module (#6208)

--- libs/rest/rest-client/src/main/kotlin/net/corda/rest/client/exceptions/RequestErrorException.kt
+++
@@ -1,5 +0,0 @@
-package net.corda.rest.client.exceptions
-
-import net.corda.v5.base.exceptions.CordaRuntimeException
-
-class RequestErrorException(message: String) : CordaRuntimeException(message)
