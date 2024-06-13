de4394d9e282e7277e09176609cfd3258499566c
Anton Subbotin <anton.subbotin@r3.com>
2024-06-13 18:33:38 +0100

    WIP Start comb worker in beforeAll (see TODO)

--- tools/corda-runtime-gradle-plugin/src/endToEndTest/kotlin/net/corda/gradle/plugin/queries/QueriesSmokeTest.kt
+++ tools/corda-runtime-gradle-plugin/src/endToEndTest/kotlin/net/corda/gradle/plugin/queries/QueriesSmokeTest.kt
@@ -1,10 +1,42 @@
 package net.corda.gradle.plugin.queries
 
+
 import net.corda.gradle.plugin.EndToEndTestBase
+import net.corda.gradle.plugin.cordalifecycle.CordaLifecycleHelper
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.io.TempDir
+import java.io.File
 
 class QueriesSmokeTest : EndToEndTestBase() {
+    companion object {
+        private val pidCacheFile: File = File.createTempFile("combined-worker-compose", ".pid").also {
+            it.delete()
+        }
+
+        @JvmStatic
+        @BeforeAll
+        fun startCombinedWorker() {
+            val composeFile = File(this::class.java.getResource("/config/combined-worker-compose.yml")!!.toURI())
+
+            CordaLifecycleHelper().startCombinedWorkerWithDockerCompose(
+                pidCacheFile.absolutePath,
+                composeFile.absolutePath,
+                "corda-cluster",
+                "5.3.0.0-HC01"
+            )
+            // TODO: do this in a thread and wait for the REST API to be available
+        }
+
+        @JvmStatic
+        @AfterAll
+        fun stopCombinedWorker() {
+            CordaLifecycleHelper().stopCombinedWorkerProcess(pidCacheFile.absolutePath)
+        }
+    }
+
     @Test
     fun listVNodesIsSuccessful() {
         appendCordaRuntimeGradlePluginExtension()
