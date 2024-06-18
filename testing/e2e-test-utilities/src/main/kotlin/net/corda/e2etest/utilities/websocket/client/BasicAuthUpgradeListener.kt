6babf82c6be2b90f495504bcdc2fc72971f933e8
Filipe Oliveira <filipe.oliveira@r3.com>
2024-06-18 12:04:32 +0100

    Reverted change.

--- testing/e2e-test-utilities/src/main/kotlin/net/corda/e2etest/utilities/websocket/client/BasicAuthUpgradeListener.kt
+++
@@ -1,8 +0,0 @@
-
-    override fun onHandshakeResponse(response: UpgradeResponse?) {
-    }
-
-    private fun toBasicAuthValue(username: String, password: String): String {
-        return "Basic " + Base64.getEncoder().encodeToString("$username:$password".toByteArray())
-    }
-}
