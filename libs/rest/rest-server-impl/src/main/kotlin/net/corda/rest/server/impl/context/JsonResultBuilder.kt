e8b130605a7aa3d8a8e516fdb9463784ba7ab527
Filipe Oliveira <filipe.oliveira@r3.com>
2024-06-26 13:30:27 +0100

    Fixed failing test due to the charset not being set.

--- libs/rest/rest-server-impl/src/main/kotlin/net/corda/rest/server/impl/context/JsonResultBuilder.kt
+++ libs/rest/rest-server-impl/src/main/kotlin/net/corda/rest/server/impl/context/JsonResultBuilder.kt
@@ -1,5 +1,6 @@
 package net.corda.rest.server.impl.context
 
+import io.javalin.http.ContentType
 import io.javalin.http.Context
 import net.corda.rest.ResponseCode
 import net.corda.rest.response.ResponseEntity
@@ -18,7 +19,7 @@ fun Context.buildJsonResult(result: Any?, returnType: Class<*>) {
             }
         }
         (result as? String) != null ->
-            ctx.contentType(ContextUtils.contentTypeApplicationJson).result(result).status(ResponseCode.OK.statusCode)
+            ctx.contentType(ContentType.APPLICATION_JSON.name).result(result).status(ResponseCode.OK.statusCode)
         result != null -> {
             // If the return type does not specify a response code (is not a HttpResponse) we default the status to 200 - OK.
             ctx.json(result).status(ResponseCode.OK.statusCode)
