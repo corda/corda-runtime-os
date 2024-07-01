0afba85bdc41a9cdd8533dbea3b1186e002dd783
Anton Subbotin <anton.subbotin@r3.com>
2024-07-01 18:33:08 +0100

    Add cpk and flows verification

--- tools/corda-runtime-gradle-plugin/src/smokeTest/kotlin/net/corda/gradle/plugin/network/StaticNetworkTest.kt
+++ tools/corda-runtime-gradle-plugin/src/smokeTest/kotlin/net/corda/gradle/plugin/network/StaticNetworkTest.kt
@@ -1,8 +1,10 @@
 package net.corda.gradle.plugin.network
 
+import net.corda.gradle.plugin.CombinedWorkerHelper.restClient
 import net.corda.gradle.plugin.CombinedWorkerHelper.startCompose
 import net.corda.gradle.plugin.CombinedWorkerHelper.stopCompose
 import net.corda.gradle.plugin.SmokeTestBase
+import net.corda.gradle.plugin.queries.LIST_VNODES_TASK_NAME
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.BeforeAll
@@ -24,16 +26,54 @@ class StaticNetworkTest : SmokeTestBase() {
     }
 
     @Test
-    fun vNodesSetupSucceeds() {
-        val result = executeWithRunner(VNODE_SETUP_TASK_NAME, "--info", "--stacktrace", forwardOutput = true, isStaticNetwork = true)
+    fun setupNetworkVerifyVNodesAndCPIs() {
+        // Create a static network
+        val vNodeSetupResult = executeWithRunner(
+            VNODE_SETUP_TASK_NAME,
+            "--info", "--stacktrace",
+            forwardOutput = true,
+            isStaticNetwork = true
+        )
 
         val vNodeRegisteredMessage = Regex("VNode .+ with shortHash [A-F0-9]+ registered.")
-        assertThat(result.output).containsPattern(vNodeRegisteredMessage.pattern)
+        assertThat(vNodeSetupResult.output).containsPattern(vNodeRegisteredMessage.pattern)
 
-        val expectedCommonNames = setOf("Alice", "Bob", "Charlie", "Dave", "NotaryRep1")
-        val registeredCommonNames = result.output.lines()
-            .filter { it.matches(vNodeRegisteredMessage) }
-            .map { it.split("CN=").last().split(",").first() }.toSet()
-        assertThat(registeredCommonNames).isEqualTo(expectedCommonNames)
+        val expectedCommonNames = listOf("Alice", "Bob", "Charlie", "Dave", "NotaryRep1")
+        val registeredVNodes = vNodeSetupResult.output.lines().filter { it.matches(vNodeRegisteredMessage) }.associate {
+            val commonName = it.split("CN=").last().split(",").first()
+            val shortHash = it.split(" with shortHash ").last().split(" registered.").first()
+            commonName to shortHash
+        }
+        assertThat(registeredVNodes.keys).containsExactlyInAnyOrderElementsOf(expectedCommonNames)
+
+        // List VNodes and verify output
+        val listVNodesResult = executeWithRunner(
+            LIST_VNODES_TASK_NAME,
+            "--info",
+            "--stacktrace",
+            forwardOutput = true,
+            isStaticNetwork = true
+        )
+
+        val vNodes = listVNodesResult.output.lines().filter { it.contains("CN=") }.map {
+            val (cpiName, shortHash) = it.split(Regex("\\s+")).take(2)
+            val commonName = it.split("CN=").last().split(",").first()
+            Triple(cpiName, shortHash, commonName)
+        }
+        assertThat(vNodes).hasSameSizeAs(expectedCommonNames)
+
+        val expectedCpiNames = listOf("MyCorDapp", "NotaryServer")
+        assertThat(vNodes.map { it.first }.toSet()).containsExactlyInAnyOrderElementsOf(expectedCpiNames)
+        assertThat(vNodes.map { it.second }).containsExactlyInAnyOrderElementsOf(registeredVNodes.values)
+        assertThat(vNodes.map { it.third }).containsExactlyInAnyOrderElementsOf(expectedCommonNames)
+
+        // Verify startable flows
+        val myCorDappFlows = restClient.flowInfoClient.getFlowclassHoldingidentityshorthash(registeredVNodes["Alice"]!!)
+        assertThat(myCorDappFlows.flowClassNames).isEmpty()
+
+        val notaryCpks = restClient.cpiClient.getCpi().cpis.filter { it.id.cpiName == "NotaryServer" }.firstOrNull()?.cpks
+        assertThat(notaryCpks).isNotNull
+        assertThat(notaryCpks).isNotEmpty
+        assertThat(notaryCpks).allSatisfy { assertThat(it.id.name).contains("com.r3.corda.notary") }
     }
 }
