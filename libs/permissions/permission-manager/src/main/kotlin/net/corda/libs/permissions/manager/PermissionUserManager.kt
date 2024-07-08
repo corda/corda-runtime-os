9b2ae8f4c0893f3f64ea8512e8e2b171740a340e
aadhavskthvl <aadhav.sakthivel@r3.com>
2024-07-08 16:19:22 +0100

    Added implementation for addPropertyToUser

--- libs/permissions/permission-manager/src/main/kotlin/net/corda/libs/permissions/manager/PermissionUserManager.kt
+++ libs/permissions/permission-manager/src/main/kotlin/net/corda/libs/permissions/manager/PermissionUserManager.kt
@@ -1,6 +1,13 @@
 package net.corda.libs.permissions.manager
 
-import net.corda.libs.permissions.manager.request.*
+import net.corda.libs.permissions.manager.request.AddRoleToUserRequestDto
+import net.corda.libs.permissions.manager.request.ChangeUserPasswordDto
+import net.corda.libs.permissions.manager.request.CreateUserRequestDto
+import net.corda.libs.permissions.manager.request.DeleteUserRequestDto
+import net.corda.libs.permissions.manager.request.GetPermissionSummaryRequestDto
+import net.corda.libs.permissions.manager.request.GetUserRequestDto
+import net.corda.libs.permissions.manager.request.RemoveRoleFromUserRequestDto
+import net.corda.libs.permissions.manager.request.AddPropertyToUserRequestDto
 import net.corda.libs.permissions.manager.response.UserPermissionSummaryResponseDto
 import net.corda.libs.permissions.manager.response.UserResponseDto
 
