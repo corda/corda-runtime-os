938c5326ebbaa5d5874d300ea09a99b41fddf7ed
Emily Bowe <emily.bowe@r3.com>
2024-07-17 13:33:22 +0100

    add exception handling and retry to getTopics in KafkaAdmin

--- libs/messaging/kafka-message-bus-impl/src/main/kotlin/net/corda/messagebus/kafka/admin/KafkaAdmin.kt
+++ libs/messaging/kafka-message-bus-impl/src/main/kotlin/net/corda/messagebus/kafka/admin/KafkaAdmin.kt
@@ -1,11 +1,34 @@
 package net.corda.messagebus.kafka.admin
 
 import net.corda.messagebus.api.admin.Admin
+import net.corda.utilities.retry.Linear
+import net.corda.utilities.retry.tryWithBackoff
+import net.corda.v5.base.exceptions.CordaRuntimeException
 import org.apache.kafka.clients.admin.AdminClient
+import org.slf4j.LoggerFactory
+import java.util.concurrent.ExecutionException
 
 class KafkaAdmin(private val adminClient: AdminClient) : Admin {
+
+    companion object {
+        private val logger = LoggerFactory.getLogger(this::class.java.enclosingClass)
+    }
+
     override fun getTopics(): Set<String> {
-        return adminClient.listTopics().names().get()
+        return try {
+            tryWithBackoff(
+                logger = logger,
+                maxRetries = 3,
+                maxTimeMillis = 3000,
+                backoffStrategy = Linear(200)
+            ) {
+                adminClient.listTopics().names().get()
+            }
+        } catch (e: ExecutionException) {
+            logger.warn("could not get Topics")
+            throw CordaRuntimeException(e.message)
+        }
+
     }
 
     override fun close() {
