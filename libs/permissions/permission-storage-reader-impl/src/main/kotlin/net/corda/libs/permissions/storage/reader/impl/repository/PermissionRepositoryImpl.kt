9b8139cc067eabd8e44fd63f4f4adab06ae00704
Ben Yip <ben.yip@r3.com>
2024-07-22 14:42:01 +0100

    CORE-20736: Update reconcilePermissionSummaries to support groups (#6290)
    * Update reconcilePermissionSummaries to support groups
    * Creates a tree to represent the relationship between groups and users and calculate all permissions for all users

--- libs/permissions/permission-storage-reader-impl/src/main/kotlin/net/corda/libs/permissions/storage/reader/impl/repository/PermissionRepositoryImpl.kt
+++ libs/permissions/permission-storage-reader-impl/src/main/kotlin/net/corda/libs/permissions/storage/reader/impl/repository/PermissionRepositoryImpl.kt
@@ -3,17 +3,18 @@ package net.corda.libs.permissions.storage.reader.impl.repository
 import net.corda.libs.permissions.storage.reader.repository.PermissionRepository
 import net.corda.libs.permissions.storage.reader.repository.UserLogin
 import net.corda.libs.permissions.storage.reader.summary.InternalUserPermissionSummary
+import net.corda.libs.permissions.storage.reader.util.PermissionUserUtil
 import net.corda.orm.utils.transaction
 import net.corda.permissions.model.Group
 import net.corda.permissions.model.Permission
 import net.corda.permissions.model.Role
 import net.corda.permissions.model.User
-import net.corda.permissions.query.dto.InternalPermissionQueryDto
 import net.corda.permissions.query.dto.InternalUserEnabledQueryDto
 import java.time.Instant
 import javax.persistence.EntityManager
 import javax.persistence.EntityManagerFactory
 
+@Suppress("TooManyFunctions")
 class PermissionRepositoryImpl(private val entityManagerFactory: EntityManagerFactory) : PermissionRepository {
 
     override fun findAllUsers(): List<User> {
@@ -48,11 +49,11 @@ class PermissionRepositoryImpl(private val entityManagerFactory: EntityManagerFa
         entityManagerFactory.transaction { entityManager ->
             val timeOfPermissionSummary = Instant.now()
             val userLoginsWithEnabledFlag = findAllUserLoginsAndEnabledFlags(entityManager)
-            val userPermissionsFromRoles = findPermissionsForUsersFromRoleAssignment(entityManager)
+            val userPermissionsFromRolesAndGroups = PermissionUserUtil.calculatePermissionsForUsers(entityManager)
 
-            return aggregatePermissionSummariesForUsers(
+            return PermissionUserUtil.aggregatePermissionSummariesForUsers(
                 userLoginsWithEnabledFlag,
-                userPermissionsFromRoles,
+                userPermissionsFromRolesAndGroups,
                 timeOfPermissionSummary
             )
         }
@@ -74,44 +75,9 @@ class PermissionRepositoryImpl(private val entityManagerFactory: EntityManagerFa
         }
     }
 
-    private fun aggregatePermissionSummariesForUsers(
-        userLogins: List<InternalUserEnabledQueryDto>,
-        userPermissionsFromRoles: Map<UserLogin, List<InternalPermissionQueryDto>>,
-        timeOfPermissionSummary: Instant,
-    ): Map<String, InternalUserPermissionSummary> {
-        return userLogins.associateBy({ it.loginName }) {
-            // rolePermissionsQuery features inner joins so a user without roles won't be present in this map
-            val permissionsInheritedFromRoles = userPermissionsFromRoles[it.loginName] ?: emptyList()
-
-            InternalUserPermissionSummary(it.loginName, it.enabled, permissionsInheritedFromRoles, timeOfPermissionSummary)
-        }
-    }
-
     private fun findAllUserLoginsAndEnabledFlags(em: EntityManager): List<InternalUserEnabledQueryDto> {
         val query = "SELECT NEW net.corda.permissions.query.dto.InternalUserEnabledQueryDto(u.loginName, u.enabled) FROM User u"
         return em.createQuery(query, InternalUserEnabledQueryDto::class.java)
             .resultList
-    }
-
-    private fun findPermissionsForUsersFromRoleAssignment(em: EntityManager): Map<UserLogin, List<InternalPermissionQueryDto>> {
-        val rolePermissionsQuery = """
-            SELECT DISTINCT NEW net.corda.permissions.query.dto.InternalPermissionQueryDto(
-                u.loginName,
-                p.id,
-                p.groupVisibility.id, 
-                p.virtualNode, 
-                p.permissionString, 
-                p.permissionType
-            )
-            FROM User u
-                JOIN RoleUserAssociation rua ON rua.user.id = u.id
-                JOIN Role r ON rua.role.id = r.id
-                JOIN RolePermissionAssociation rpa ON rpa.role.id = r.id
-                JOIN Permission p ON rpa.permission.id = p.id
-            """
-        return em.createQuery(rolePermissionsQuery, InternalPermissionQueryDto::class.java)
-            .resultList
-            .sortedWith(PermissionQueryDtoComparator())
-            .groupBy { it.loginName }
     }
 }
