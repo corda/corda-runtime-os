b7657c3e3d2c617fcce8ec3bf5e2a426299c04a6
Emily Bowe <emily.bowe@r3.com>
2024-07-26 11:27:15 +0100

    apply PR comments to improve test

--- libs/messaging/kafka-message-bus-impl/src/test/kotlin/net/corda/messagebus/kafka/admin/KafkaAdminTest.kt
+++ libs/messaging/kafka-message-bus-impl/src/test/kotlin/net/corda/messagebus/kafka/admin/KafkaAdminTest.kt
@@ -8,7 +8,6 @@ import org.apache.kafka.common.errors.TimeoutException
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
-import org.mockito.Mockito
 import org.mockito.kotlin.mock
 import org.mockito.kotlin.times
 import org.mockito.kotlin.verify
@@ -38,23 +37,18 @@ class KafkaAdminTest {
     @Test
     fun `getTopics will retry an exception and be successful when retries not exceeded`() {
         val adminClient = mock<AdminClient>()
-        val kafkaFuture: KafkaFuture<Set<String>> = mock()
+        val kafkaFuture = mock<KafkaFuture<Set<String>>>()
 
         val topicResult = mock<ListTopicsResult>()
-        Mockito.`when`(adminClient.listTopics()).thenReturn(topicResult)
-        Mockito.`when`(topicResult.names()).thenReturn(kafkaFuture)
+        whenever(topicResult.names()).thenReturn(kafkaFuture)
+        whenever(adminClient.listTopics()).thenReturn(topicResult)
 
         //retries hardcoded in getTopics to max 3 attempts
 
-        var attemptNumber = 1
-        Mockito.`when`(kafkaFuture.get()).thenAnswer {
-            if (attemptNumber <= 2) {
-                attemptNumber++
-                throw (ExecutionException(TimeoutException("timed out")))
-            } else {
-                setOf("topic1")
-            }
-        }
+        whenever(kafkaFuture.get())
+            .thenThrow(ExecutionException(TimeoutException("timed out")))
+            .thenThrow(ExecutionException(TimeoutException("timed out")))
+            .thenReturn(setOf("topic1"))
 
         val admin = KafkaAdmin(adminClient)
 
@@ -64,28 +58,20 @@ class KafkaAdminTest {
     @Test
     fun `getTopics will retry an exception and rethrow when retries exceeded`() {
         val adminClient = mock<AdminClient>()
-        val kafkaFuture: KafkaFuture<Set<String>> = mock()
+        val kafkaFuture = mock<KafkaFuture<Set<String>>>()
 
         val topicResult = mock<ListTopicsResult>()
-        Mockito.`when`(adminClient.listTopics()).thenReturn(topicResult)
-        Mockito.`when`(topicResult.names()).thenReturn(kafkaFuture)
+        whenever(topicResult.names()).thenReturn(kafkaFuture)
+        whenever(adminClient.listTopics()).thenReturn(topicResult)
 
         //retries hardcoded in getTopics to max 3 attempts
 
-        var attemptNumber = 1
-        Mockito.`when`(kafkaFuture.get()).thenAnswer {
-            if (attemptNumber <= 3) {
-                attemptNumber++
-                throw (ExecutionException(TimeoutException("timed out")))
-            } else {
-                setOf("topic1")
-            }
-        }
+        whenever(kafkaFuture.get())
+            .thenThrow(ExecutionException(TimeoutException("timed out")))
 
         val admin = KafkaAdmin(adminClient)
 
         assertThrows<CordaRuntimeException> { admin.getTopics() }
-
         verify(kafkaFuture, times(3)).get()
     }
 }
