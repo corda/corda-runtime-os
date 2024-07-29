70186f4c4e67b442b202464f8da538e9807ed699
Ben Millar <44114751+ben-millar@users.noreply.github.com>
2024-07-29 14:22:57 +0100

    CORE-20797 Adding CordaMessageAPIProducerRequiresReset exception to ExceptionUtils (#6311)
    This is required so that the exception is correctly handled in the Kafka message patterns.

--- libs/messaging/messaging-impl/src/main/kotlin/net/corda/messaging/utils/ExceptionUtils.kt
+++ libs/messaging/messaging-impl/src/main/kotlin/net/corda/messaging/utils/ExceptionUtils.kt
@@ -2,10 +2,12 @@ package net.corda.messaging.utils
 
 import net.corda.messaging.api.exception.CordaMessageAPIFatalException
 import net.corda.messaging.api.exception.CordaMessageAPIIntermittentException
+import net.corda.messaging.api.exception.CordaMessageAPIProducerRequiresReset
 
 object ExceptionUtils {
     val CordaMessageAPIException: Set<Class<out Throwable>> = setOf(
         CordaMessageAPIFatalException::class.java,
-        CordaMessageAPIIntermittentException::class.java
+        CordaMessageAPIIntermittentException::class.java,
+        CordaMessageAPIProducerRequiresReset::class.java
     )
 }
