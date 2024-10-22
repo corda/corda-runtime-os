480847315e4203061c4b2cb4d730d939b4f860e7
Will Vigor <william.vigor@r3.com>
2024-10-22 11:25:28 +0100

    Try and fix osgi test in ledger-consensual-flow

--- libs/ledger/ledger-lib-common-flow/src/main/kotlin/net/corda/ledger/libs/common/flow/impl/transaction/factory/TransactionMetadataFactoryImpl.kt
+++ libs/ledger/ledger-lib-common-flow/src/main/kotlin/net/corda/ledger/libs/common/flow/impl/transaction/factory/TransactionMetadataFactoryImpl.kt
@@ -4,16 +4,14 @@ import net.corda.ledger.common.data.transaction.TransactionMetadataImpl
 import net.corda.ledger.common.data.transaction.WireTransactionDigestSettings
 import net.corda.ledger.common.flow.transaction.factory.TransactionMetadataFactory
 import net.corda.libs.platform.PlatformInfoProvider
-import net.corda.sandbox.type.UsedByFlow
 import net.corda.v5.ledger.common.transaction.CordaPackageSummary
 import net.corda.v5.ledger.common.transaction.TransactionMetadata
-import net.corda.v5.serialization.SingletonSerializeAsToken
 
 class TransactionMetadataFactoryImpl(
     private val platformInfoProvider: PlatformInfoProvider,
     private val getCpkSummaries: () -> List<CordaPackageSummary>,
     private val getCpiSummary: () -> CordaPackageSummary
-) : TransactionMetadataFactory, UsedByFlow, SingletonSerializeAsToken {
+) : TransactionMetadataFactory {
     override fun create(ledgerSpecificMetadata: Map<String, Any>): TransactionMetadata {
         val metadata = mapOf(
             TransactionMetadataImpl.DIGEST_SETTINGS_KEY to WireTransactionDigestSettings.defaultValues,
