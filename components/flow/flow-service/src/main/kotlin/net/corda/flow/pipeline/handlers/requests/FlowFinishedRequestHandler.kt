d64b6c994edb6837164abfbea1e41bf7645e6b92
LWogan <lorcan.wogan@r3.com>
2024-11-18 10:37:06 +0000

    CORE-20909: Ensure flow status does not exceed max message size.

--- components/flow/flow-service/src/main/kotlin/net/corda/flow/pipeline/handlers/requests/FlowFinishedRequestHandler.kt
+++ components/flow/flow-service/src/main/kotlin/net/corda/flow/pipeline/handlers/requests/FlowFinishedRequestHandler.kt
@@ -1,12 +1,15 @@
 package net.corda.flow.pipeline.handlers.requests
 
+import net.corda.avro.serialization.CordaAvroSerializationFactory
 import net.corda.data.flow.state.waiting.WaitingFor
 import net.corda.flow.fiber.FlowIORequest
+import net.corda.flow.pipeline.addTerminationKeyToMeta
 import net.corda.flow.pipeline.events.FlowEventContext
+import net.corda.flow.pipeline.exceptions.FlowFatalException
 import net.corda.flow.pipeline.factory.FlowMessageFactory
 import net.corda.flow.pipeline.factory.FlowRecordFactory
-import net.corda.flow.pipeline.addTerminationKeyToMeta
 import net.corda.flow.pipeline.handlers.requests.helper.getRecords
+import net.corda.flow.state.FlowCheckpoint
 import org.osgi.service.component.annotations.Activate
 import org.osgi.service.component.annotations.Component
 import org.osgi.service.component.annotations.Reference
@@ -18,7 +21,9 @@ class FlowFinishedRequestHandler @Activate constructor(
     @Reference(service = FlowMessageFactory::class)
     private val flowMessageFactory: FlowMessageFactory,
     @Reference(service = FlowRecordFactory::class)
-    private val flowRecordFactory: FlowRecordFactory
+    private val flowRecordFactory: FlowRecordFactory,
+    @Reference(service = CordaAvroSerializationFactory::class)
+    private val cordaAvroSerializationFactory: CordaAvroSerializationFactory
 ) : FlowRequestHandler<FlowIORequest.FlowFinished> {
 
     private companion object {
@@ -26,6 +31,8 @@ class FlowFinishedRequestHandler @Activate constructor(
     }
 
     override val type = FlowIORequest.FlowFinished::class.java
+
+    private val serializer = cordaAvroSerializationFactory.createAvroSerializer<Any>()
 
     override fun getUpdatedWaitingFor(context: FlowEventContext<Any>, request: FlowIORequest.FlowFinished): WaitingFor? {
         return null
@@ -36,6 +43,7 @@ class FlowFinishedRequestHandler @Activate constructor(
         request: FlowIORequest.FlowFinished
     ): FlowEventContext<Any> {
         val checkpoint = context.checkpoint
+        validateResultSize(checkpoint, request)
 
         val status = flowMessageFactory.createFlowCompleteStatusMessage(checkpoint, request.result)
         val records = getRecords(flowRecordFactory, context, status)
@@ -46,5 +54,22 @@ class FlowFinishedRequestHandler @Activate constructor(
         context.flowMetrics.flowCompletedSuccessfully()
         val metaDataWithTermination = addTerminationKeyToMeta(context.metadata)
         return context.copy(outputRecords = context.outputRecords + records, metadata = metaDataWithTermination)
+    }
+
+    /**
+     * The flow status will contain the result string. Ensure it doesn't exceed the max message size allowed.
+     */
+    private fun validateResultSize(checkpoint: FlowCheckpoint, request: FlowIORequest.FlowFinished) {
+        val maxAllowedMessageSize = checkpoint.maxMessageSize
+        val result = request.result
+        if (result != null) {
+            val resultSize = serializer.serialize(result)?.size
+            if (resultSize != null && resultSize > maxAllowedMessageSize) {
+                throw FlowFatalException(
+                    "Flow attempted to return a result that is greater than the max message size allowed. Flow " +
+                            "result size [$resultSize]. Max Allowed Message Size [$maxAllowedMessageSize]"
+                )
+            }
+        }
     }
 }
