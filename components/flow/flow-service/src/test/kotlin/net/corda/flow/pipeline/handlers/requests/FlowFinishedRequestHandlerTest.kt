575bd65f195a5c1e43e351a33fd0873716dfa538
Lorcan Wogan <69468264+LWogan@users.noreply.github.com>
2024-11-20 14:21:05 +0000

    CORE-20909 validate flow result cannot exceed max message size (#6393)
    This PR ensures the flow result does not exceed the max allowed message size for payloads sent to the message bus. The flow result object is passed to the message bus via the FlowStatus object. If the message size is too large this will result in a fatal exception in the mediator and the flows topic partition will become blocked as the pattern will retry message after the worker restarts.

--- components/flow/flow-service/src/test/kotlin/net/corda/flow/pipeline/handlers/requests/FlowFinishedRequestHandlerTest.kt
+++ components/flow/flow-service/src/test/kotlin/net/corda/flow/pipeline/handlers/requests/FlowFinishedRequestHandlerTest.kt
@@ -1,5 +1,7 @@
 package net.corda.flow.pipeline.handlers.requests
 
+import net.corda.avro.serialization.CordaAvroSerializationFactory
+import net.corda.avro.serialization.CordaAvroSerializer
 import net.corda.data.flow.FlowKey
 import net.corda.data.flow.event.mapper.FlowMapperEvent
 import net.corda.data.flow.event.mapper.ScheduleCleanup
@@ -8,10 +10,15 @@ import net.corda.flow.BOB_X500_HOLDING_IDENTITY
 import net.corda.flow.RequestHandlerTestContext
 import net.corda.flow.SESSION_ID_1
 import net.corda.flow.fiber.FlowIORequest
+import net.corda.flow.pipeline.exceptions.FlowFatalException
 import net.corda.messaging.api.records.Record
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.mockito.Mockito.mock
 import org.mockito.kotlin.any
+import org.mockito.kotlin.anyOrNull
 import org.mockito.kotlin.eq
 import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
@@ -22,15 +29,34 @@ class FlowFinishedRequestHandlerTest {
         val FLOW_KEY = FlowKey(SESSION_ID_1, BOB_X500_HOLDING_IDENTITY)
     }
 
-    private val testContext = RequestHandlerTestContext(Any())
     private val flowResult = "ok"
     private val ioRequest = FlowIORequest.FlowFinished(flowResult)
-    private val handler = FlowFinishedRequestHandler(testContext.flowMessageFactory, testContext.flowRecordFactory)
+    private val avroSerializationFactory = mock<CordaAvroSerializationFactory>()
+    private val serializer = mock<CordaAvroSerializer<Any>>()
+    private lateinit var testContext: RequestHandlerTestContext<Any>
+    private lateinit var handler: FlowFinishedRequestHandler
+
+    @BeforeEach
+    fun setup() {
+        whenever(serializer.serialize(any())).thenReturn(byteArrayOf(1, 2, 3))
+        whenever(avroSerializationFactory.createAvroSerializer<Any>(anyOrNull())).thenReturn(serializer)
+        testContext = RequestHandlerTestContext(Any())
+        whenever(testContext.flowCheckpoint.maxMessageSize).thenReturn(100000000)
+        handler = FlowFinishedRequestHandler(testContext.flowMessageFactory, testContext.flowRecordFactory, avroSerializationFactory)
+    }
 
     @Test
     fun `Updates the waiting for to nothing`() {
         val waitingFor = handler.getUpdatedWaitingFor(testContext.flowEventContext, ioRequest)
         assertThat(waitingFor?.value).isNull()
+    }
+
+    @Test
+    fun `Flow Result exceeds max size`() {
+        whenever(testContext.flowCheckpoint.maxMessageSize).thenReturn(1)
+        handler = FlowFinishedRequestHandler(testContext.flowMessageFactory, testContext.flowRecordFactory, avroSerializationFactory)
+
+        assertThrows<FlowFatalException> {  handler.postProcess(testContext.flowEventContext, ioRequest) }
     }
 
     @Test
